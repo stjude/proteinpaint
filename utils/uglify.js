594e7cf15957a6ef28712674e7e204ade6c0e61a
xzhou <xin.zhou@stjude.org>
2017-12-14 14:16:07 -0600

    process one file at a time, do not glob

--- utils/uglify.js
+++ utils/uglify.js
@@ -1,5 +1,5 @@
 if(process.argv.length!=4) {
-	console.log('<in dir> <outdir>')
+	console.log('<input js file to be uglified > <outdir>')
 	process.exit()
 }
 
@@ -7,15 +7,7 @@ if(process.argv.length!=4) {
 const path=require('path')
 const exec=require('child_process').execSync
 
-const indir=process.argv[2]
+const infile=process.argv[2]
 const outdir=process.argv[3]
 
-require('glob').glob(path.join(indir,'*.js'), (err,files)=>{
-	if(err) {
-		console.log(err)
-		return
-	}
-	for(const file of files) {
-		exec('./node_modules/babel-cli/bin/babel.js '+file+' | ./node_modules/uglify-js/bin/uglifyjs --compress --mangle > '+path.join(outdir,path.basename(file)))
-	}
-})
+exec('./node_modules/babel-cli/bin/babel.js '+infile+' | ./node_modules/uglify-js/bin/uglifyjs --compress --mangle > '+path.join(outdir,path.basename(infile)))
