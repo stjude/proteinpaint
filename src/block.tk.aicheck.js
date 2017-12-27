90a629d7485e4fc29e2df50211327d8204efe309
xzhou <xin.zhou@stjude.org>
2017-12-27 08:43:59 -0600

    set coverage max from config menu

--- src/block.tk.aicheck.js
+++ src/block.tk.aicheck.js
@@ -6,7 +6,7 @@ import {event as d3event} from 'd3-selection'
 
 
 /*
-follows bedj track, main & subpanel rendered separately
+follows bigwig track, main & subpanel rendered separately
 */
 
 
@@ -15,6 +15,10 @@ follows bedj track, main & subpanel rendered separately
 
 function makeTk(tk,block) {
 	tk.img=tk.glider.append('image')
+
+	if(!tk.coveragemax) {
+		tk.coveragemax=100
+	}
 
 	if(!tk.vafheight) {
 		tk.vafheight = 50
@@ -99,6 +103,8 @@ function tkarg(tk,block) {
 		rglst:block.tkarg_rglst(),
 		regionspace:block.regionspace,
 		width:block.width,
+
+		coveragemax: tk.coveragemax,
 
 		file:tk.file,
 		url:tk.url,
@@ -281,284 +287,28 @@ export function loadTksubpanel(tk, block, panel) {
 
 
 
-function configPanel(tk, block, holder, loader) {
-	// provide hooks to each component, so that caller can choose to hide certain ones
-	const config={
-		pcolor:{},
-		ncolor:{},
-		pcolor2:{},
-		ncolor2:{},
-			// .row
-			// .lab
-
-		dotplot:{},
-			// .row
-
-		dividefactor:{}
-	}
+function configPanel(tk, block) {
 
 	// height
 	{
-		const row=holder.append('div')
+		const row = tk.tkconfigtip.d.append('div')
 			.style('margin-bottom','15px')
-		row.append('span').html('Height&nbsp;&nbsp;')
+		row.append('span').html('Coverage max:&nbsp;')
 		row.append('input')
-			.attr('size',5)
-			.property('value',tk.barheight)
+			.attr('type','number')
+			.style('width','60px')
+			.property('value',tk.coveragemax)
 			.on('keyup',()=>{
 				if(d3event.code!='Enter') return
 				const s=d3event.target.value
 				if(s=='') return
 				const v=Number.parseInt(s)
 				if(Number.isNaN(v) || v<=1) {
-					alert('track height must be positive integer')
+					alert('coverage max must be positive integer')
 					return
 				}
-				tk.barheight=v
-				loader( client.bwSetting.height )
+				tk.coveragemax=v
+				loadTk( tk, block)
 			})
 	}
-
-	// pcolor
-	config.pcolor.row=holder.append('div').style('margin-bottom','15px')
-	config.pcolor.lab=config.pcolor.row.append('span').text('Positive value color').style('padding-right','10px')
-	config.pcolor.row.append('input').attr('type','color')
-		.property('value',tk.pcolor)
-		.on('change',()=>{
-			tk.pcolor=d3event.target.value
-			loader( client.bwSetting.pcolor )
-		})
-
-	// ncolor
-	config.ncolor.row=holder.append('div').style('margin-bottom','15px')
-	config.ncolor.lab=config.ncolor.row.append('span').text('Negative value color').style('padding-right','10px')
-	config.ncolor.row.append('input').attr('type','color')
-		.property('value',tk.ncolor)
-		.on('change',()=>{
-			tk.ncolor=d3event.target.value
-			loader( client.bwSetting.ncolor )
-		})
-
-	if(!tk.scale.auto) {
-		// pcolor2
-		config.pcolor2.row=holder.append('div').style('margin-bottom','15px')
-		config.pcolor2.lab=config.pcolor2.row.append('span').html('&ge;Max color').style('padding-right','10px')
-		config.pcolor2.row.append('input').attr('type','color')
-			.property('value',tk.pcolor2)
-			.on('change',()=>{
-				tk.pcolor2=d3event.target.value
-				loader( client.bwSetting.pcolor2 )
-			})
-		// ncolor2
-		config.ncolor2.row=holder.append('div').style('margin-bottom','15px')
-		config.ncolor2.lab=config.ncolor2.row.append('span').html('&le;Min color').style('padding-right','10px')
-		config.ncolor2.row.append('input').attr('type','color')
-			.property('value',tk.ncolor2)
-			.on('change',()=>{
-				tk.ncolor2=d3event.target.value
-				loader( client.bwSetting.ncolor2 )
-			})
-	}
-		
-	// y-scale
-	{
-		const row=holder.append('div')
-			.style('margin-bottom','15px')
-		row.append('span').html('Y scale&nbsp;&nbsp;')
-		const ss=row.append('select')
-		const ssop1=ss.append('option').text('automatic')
-		const ssop2=ss.append('option').text('fixed')
-		const ssop3=ss.append('option').text('percentile')
-		ss.on('change',()=>{
-			const si=d3event.target.selectedIndex
-			if(si==0) {
-				fixed.style('display','none')
-				percentile.style('display','none')
-				tk.scale.auto=1
-				loader( client.bwSetting.autoscale )
-				return
-			}
-			if(si==1) {
-				fixed.style('display','block')
-				percentile.style('display','none')
-				return
-			}
-			fixed.style('display','none')
-			percentile.style('display','block')
-		})
-		let usingfixed=false,
-			usingperc=false
-		if(tk.scale.auto) {
-			ssop1.property('selected',1)
-		} else {
-			if(tk.scale.percentile) {
-				usingperc=true
-				ssop3.property('selected',1)
-			} else {
-				usingfixed=true
-				ssop2.property('selected',1)
-			}
-		}
-		// y-scale fixed
-		const fixed=row.append('div').style('margin','10px')
-			.style('display',usingfixed ? 'block' : 'none')
-		{
-			const row1=fixed.append('div')
-			row1.append('span').html('Max&nbsp;').style('font-family','Courier').style('font-size','.9em')
-			const max=row1.append('input').attr('size',5)
-			if(usingfixed) {
-				max.property('value',tk.scale.max)
-			}
-			const row2=fixed.append('div')
-			row2.append('span').html('Min&nbsp;').style('font-family','Courier').style('font-size','.9em')
-			const min=row2.append('input').attr('size',5)
-			if(usingfixed) {
-				min.property('value',tk.scale.min)
-			}
-			row2.append('button').text('Set').style('margin-left','5px').on('click',()=>{
-				const s1=max.property('value')
-				if(s1=='') {
-					return
-				}
-				const v1=Number.parseFloat(s1)
-				if(Number.isNaN(v1)) {
-					alert('invalid max value')
-					return
-				}
-				const s2=min.property('value')
-				if(s2=='') {
-					return
-				}
-				const v2=Number.parseFloat(s2)
-				if(Number.isNaN(v2)) {
-					alert('invalid min value')
-					return
-				}
-				delete tk.scale.auto
-				delete tk.scale.percentile
-				tk.scale.max=v1
-				tk.scale.min=v2
-				loader( client.bwSetting.fixedscale )
-			})
-		}
-		// y-scale percentile
-		const percentile=row.append('div').style('margin-top','6px')
-			.style('display',usingperc ? 'block' : 'none')
-		{
-			percentile.append('span').html('Percentile&nbsp;').style('font-family','Courier').style('font-size','.9em')
-			const input=percentile.append('input').attr('size',5)
-			if(usingperc) {
-				input.property('value',tk.scale.percentile)
-			}
-			const setpercentile=(s)=>{
-				if(s=='') return
-				const v=Number.parseInt(s)
-				if(Number.isNaN(v) || v<=0 || v>100) {
-					alert('percentile should be integer within range 0-100')
-					return
-				}
-				delete tk.scale.auto
-				tk.scale.percentile=v
-				loader( client.bwSetting.percentilescale )
-			}
-			input.on('keyup',()=>{
-				if(d3event.code!='Enter') return
-				setpercentile(input.property('value'))
-			})
-			percentile.append('button').text('Set').style('margin-left','5px').on('click',()=>{
-				setpercentile(input.property('value'))
-			})
-		}
-	}
-
-	// dot plot
-	{
-		config.dotplot.row=holder.append('div')
-			.style('margin-bottom','15px')
-		config.dotplot.row.append('span').html('Dot plot&nbsp;&nbsp;')
-		const s=config.dotplot.row.append('select')
-			.on('change',()=>{
-				const i=d3event.target.selectedIndex
-				if(i==0) {
-					delete tk.dotplotfactor
-				} else {
-					tk.dotplotfactor= Number.parseInt( d3event.target.options[i].innerHTML)
-				}
-				loader( i==0 ? client.bwSetting.nodotplot : client.bwSetting.usedotplot )
-			})
-		let o=s.append('option').text('no')
-		if(!tk.dotplotfactor) {
-			o.property('selected',1)
-		}
-		o=s.append('option').text('5')
-		if(tk.dotplotfactor==5) {
-			o.property('selected',1)
-		}
-		o=s.append('option').text('10')
-		if(tk.dotplotfactor==10) {
-			o.property('selected',1)
-		}
-		o=s.append('option').text('15')
-		if(tk.dotplotfactor==15) {
-			o.property('selected',1)
-		}
-		o=s.append('option').text('20')
-		if(tk.dotplotfactor==20) {
-			o.property('selected',1)
-		}
-	}
-
-
-	// normalization using a divide-by factor
-	config.dividefactor.row = holder.append('div')
-		//.style('margin-bottom','15px')
-	{
-		const id=Math.random().toString()
-		const input = config.dividefactor.row.append('input')
-			.attr('type','checkbox')
-			.attr('id',id)
-		if(!tk.normalize.disable) {
-			input.property('checked',1)
-		}
-		config.dividefactor.row.append('label')
-			.html('&nbsp;Apply normalization')
-			.attr('for',id)
-
-		const folder = config.dividefactor.row.append('div')
-			.style('margin','5px 10px 0px 20px')
-			.style('display', tk.normalize.disable ? 'none' : 'block')
-		folder.append('span')
-			.html('Divide raw value by&nbsp;')
-		const factorinput = folder.append('input')
-			.attr('type','number')
-			.style('width','60px')
-			.property('value', tk.normalize.dividefactor)
-			.on('keyup',()=>{
-				if(d3event.code!='Enter' && d3event.code!='NumpadEnter') return
-				const v = d3event.target.value
-				if(v<=0) {
-					// don't allow
-					return
-				}
-				tk.normalize.dividefactor = v
-				loader( client.bwSetting.usedividefactor )
-			})
-		folder.append('div')
-			.text('Enter a value above zero')
-			.style('font-size','.7em')
-			.style('color','#858585')
-		input.on('change',()=>{
-			if(d3event.target.checked) {
-				client.appear(folder)
-				delete tk.normalize.disable
-				factorinput.property('value',tk.normalize.dividefactor)
-				loader( client.bwSetting.usedividefactor )
-				return
-			}
-			client.disappear(folder)
-			tk.normalize.disable=1
-			loader( client.bwSetting.nodividefactor )
-		})
-	}
-	return config
 }
