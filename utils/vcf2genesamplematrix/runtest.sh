debdcb9cb1c78cba254cec28bb804077821bae18
xzhou <xin.zhou@stjude.org>
2018-03-05 11:40:39 -0600

    allow excludeclass code to be case insensitive

--- utils/vcf2genesamplematrix/runtest.sh
+++ utils/vcf2genesamplematrix/runtest.sh
@@ -1,3 +1,3 @@
 ../../node_modules/.bin/webpack --entry=./source.js --output-filename=bin.js --target=node
 
-node bin.js --genome=hg19 --excludeclass=Intron,exon output.html /home/xzhou/y2
+node bin.js --genome=hg19 --excludeclass=intron,e output.html /home/xzhou/y2
