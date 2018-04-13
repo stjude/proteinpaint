880b5eff4d715947d886001e764de35132755cd2
xzhou <xin.zhou@stjude.org>
2018-04-13 12:10:39 -0500

    create mds for SCD project

---
+++ dataset/scd.js
@@ -0,0 +1,58 @@
+const common=require('../src/common')
+
+
+const samplenamekey = 'sample_name'
+
+
+module.exports={
+
+	genome:'hg19',
+	isMds:true,
+
+	noHandleOnClient:1,
+
+	about:[
+	],
+
+	cohort:{
+		files:[
+			// possible to have file-specific logic
+			{file:'hg38/scd/cnv/sample.table'}
+		],
+		samplenamekey:samplenamekey,
+		tohash:(item, ds)=>{
+			ds.cohort.annotation[ item[samplenamekey] ] = item
+		},
+	},
+
+
+	queries:{
+
+		svcnv:{
+
+
+			name:'SCD germline CNV',
+			showfullmode:true,
+			istrack:true,
+			type:common.tkt.mdssvcnv,
+			file:'hg38/scd/cnv/cnv.gz',
+
+			// cnv
+			valueCutoff:0.2,
+			bplengthUpperLimit:2000000, // limit cnv length to focal events
+
+
+			/*
+			to sort sample groups consistently, on client, not on server
+			*/
+
+
+
+			multihidelabel_vcf:false,
+			multihidelabel_sv:true,
+		},
+
+
+
+	}
+}
