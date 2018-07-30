d0680157b61e61d029153cac1cf4cd45ec991709
xzhou <xin.zhou@stjude.org>
2018-07-30 09:40:52 -0500

    comment

--- scripts/deploy.scmc.sh
+++ scripts/deploy.scmc.sh
@@ -26,3 +26,17 @@ sed "s%$DEVHOST/bin/%http://124.74.133.38:8088/bin/%" < public/bin/proteinpaint.
 tar zcvf sourcecode.tgz tmpbuild/
 
 scp sourcecode.tgz -p 3322 user@124.74.133.38:
+
+
+
+# cd /opt/data/pp/tp/
+# tar cvf test/anno.tar anno/
+# ln -s /opt/data/pp/tp/test/anno.tar /opt/app/pp/es6_proteinpaint/public/
+# tar cvf  test/hg19.tar genomes/hg19.gz*
+# ln -s /opt/data/pp/tp/test/hg19.tar /opt/app/pp/es6_proteinpaint/public/
+
+
+
+# $ wget https://proteinpaint.stjude.org/sourcecode.tgz
+# $ wget https://proteinpaint.stjude.org/anno.tar
+# $ wget https://proteinpaint.stjude.org/hg19.tar
