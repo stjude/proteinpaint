597b77db812a30bc6c3d28662e835f8a2f5fabc7
Zhou <xzhou1@l190186.stjude.sjcrh.local>
2018-10-05 14:18:31 -0500

    install script

---
+++ utils/install.pp.js
@@ -0,0 +1,65 @@
+/*
+to automate the install process to certain degree
+
+Run this script under the ProteinPaint root diretory, as:
+
+$ node utils/install.pp.js
+
+
+Will install some dependencies, and download support files if not available on your system
+
+Requires:
+
+* Node.js 8 or higher
+* npm
+* curl
+
+
+Optional:
+
+* two-column config file, key \t value
+  lines starting with # are ignored
+  keys:
+
+  MAC   - your system is mac, otherwise linux (no windows)
+  TP    - full path of the TP directory
+  CACHE - full path of the cache directory
+  BIN   - full path to a directory for storing dependent binary files and programs
+  URL   - the URL of your PP service
+  GENOMES - prebuilt genomes to be installed on your system, join multiple by comma
+            hg19
+			hg38
+
+
+*/
+
+const fs = require('fs')
+const exec =require('child_process').execSync
+
+const c = {}
+if( process.argv[2] ) {
+	// load installation instructions from external file, optional
+	for(const line of fs.readFileSync(process.argv[2],{encoding:'utf8'}).trim().split('\n')) {
+		if(!line) continue
+		if(line[0]=='#') continue
+		const l = line.split('\t')
+		if(l.length != 2) continue
+		c[ l[0] ] = l[1]
+	}
+}
+
+
+// replace url
+
+
+// bin/
+// get ucsc binaries
+
+
+// tp
+
+// tp/genomes/
+
+
+// for each genome:
+// 
