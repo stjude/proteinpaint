c6670c21a211fc353dc4e8009a6fbc95cbe85448
xzhou <xzhou1@stjude.org>
2018-11-20 12:22:26 -0600

    fillbar can return html

--- src/client.js
+++ src/client.js
@@ -1145,40 +1145,57 @@ export function ensureisblock(b) {
 
 
 export function fillbar(td,v,at) {
-	/*
-	draw a horizontal bar with bg and fg to show percentage
+/*
+draw a horizontal bar with bg and fg to show percentage
 
-	td: holder
-	v: 
-		.f fraction
-		.v1 numerator, optional
-		.v2 denominator
-	at:
-		.width
-		.height
-		.fillbg
-		.fill
-		.readcountcredible
-	*/
-	td.attr('title',
-		(v.f*100).toFixed(0)+'%'+
-		(v.v1!=undefined ? ' ('+v.v1+'/'+v.v2+')' : '')
-		)
+td: holder
+	optional, if missing, will return svg html
+v: 
+	.f fraction
+	.v1 numerator, optional
+	.v2 denominator
+at:
+	optional
+	.width
+	.height
+	.fillbg
+	.fill
+	.readcountcredible
+*/
+	if(!at) at = {}
 	const w=at.width || 40
 	const h=at.height || 12
-	const g=td.append('svg').attr('width',w).attr('height',h)
+
+	let g
+
+	if(td) {
+		td.attr('title',
+			(v.f*100).toFixed(0)+'%'+
+			(v.v1!=undefined ? ' ('+v.v1+'/'+v.v2+')' : '')
+			)
+		g=td.append('svg').attr('width',w).attr('height',h)
+	} else {
+		g = d3select(document.body).append('svg')
+	}
+
+
 	let y=0
 	// fill bg
-	g.append('rect').attr('y',y).attr('width',w).attr('height',h).attr('fill', at.fillbg)
+	g.append('rect').attr('y',y).attr('width',w).attr('height',h).attr('fill', at.fillbg || '#ccccff')
 	// fill fg
-	g.append('rect').attr('y',y).attr('width',w*v.f).attr('height',h).attr('fill',at.fill)
-	if(v.v2<at.readcountcredible) {
+	g.append('rect').attr('y',y).attr('width',w*v.f).attr('height',h).attr('fill',at.fill || '#4d4dff')
+
+	if( at.readcountcredible && v.v2<at.readcountcredible) {
 		// wash with gray
 		const smudge='#545454'
 		const smudge2=.3
 		g.append('rect').attr('y',y).attr('width',w).attr('height',h).attr('fill',smudge).attr('fill-opacity',smudge2)
 	}
-	return g
+
+	if(td) return g
+
+	g.remove()
+	return '<svg width='+w+' height='+h+'>'+g.node().innerHTML+'</svg>'
 }
 
 
