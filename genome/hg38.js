05b0c063adab650d85ee870f0fd202d8fd0473cb
xzhou <xzhou1@stjude.org>
2018-11-28 11:26:49 -0600

    enable clinvar checking in hg38

--- genome/hg38.js
+++ genome/hg38.js
@@ -17,6 +17,36 @@ module.exports={
 		statement_getbyname:'select * from snp146 where name=?',
 		statement_getbycoord:'select * from snp146 where chrom=? and bin=? and chromStart>=? and chromEnd<=?'
 	},
+	clinvarVCF:{
+		file:'hg38/clinvar.hg38.vcf.gz',
+		infokey:'CLNSIG',
+		categories:{
+			'Uncertain_significance':{color:'#aaa',label:'Uncertain significance',textcolor:'white'},
+			'not_provided': {color:'#ccc',label:'Not provided'},
+			'_not_provided':{color:'#ccc',label:'Not provided'},
+			'Benign':{color:'#43ac6a',label:'Benign',textcolor:'white'},
+			'Benign/Likely_benign':{color:'#43ac6a',label:'Benign/Likely benign',textcolor:'white'},
+			'Likely_benign':{color:'#5bc0de',label:'Likely benign',textcolor:'white'},
+			'Likely_pathogenic':{color:'#e99002',label:'Likely pathogenic',textcolor:'white'},
+			'Pathogenic':{color:'#f04124',label:'Pathogenic',textcolor:'white'},
+			'Pathogenic/Likely_pathogenic':{color:'#f04124',label:'Pathogenic/Likely pathogenic',textcolor:'white'},
+			'drug_response':{color:'gold',label:'Drug response',textcolor:'white'},
+			'_drug_response':{color:'gold',label:'Drug response',textcolor:'white'},
+			'Conflicting_interpretations_of_pathogenicity':{color:'#90C3D4',label:'Conflicting interpretations of pathogenicity'},
+			'other':{color:'#ccc',label:'Other'},
+			'_other':{color:'#ccc',label:'Other'},
+			'not_provided':{color:'#ccc',label:'Not provided'},
+			'_not_provided':{color:'#ccc',label:'Not provided'},
+			'risk_factor':{color:'#ccc',label:'Risk factor'},
+			'_risk_factor':{color:'#ccc',label:'Risk factor'},
+			'association':{color:'#ccc',label:'Association'},
+			'_association':{color:'#ccc',label:'Association'},
+			'Affects':{color:'#ccc',label:'Affects'},
+			'_Affects':{color:'#ccc',label:'Affects'},
+			'protective':{color:'#ccc',label:'Protective'},
+			'_protective':{color:'#ccc',label:'Protective'},
+		}
+	},
 	tracks:[
 		{
 			__isgene:true,
