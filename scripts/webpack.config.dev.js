46cb92491e9af9cc710fb8b047ba73a080b8fc70
Edgar Sioson <esioson@stjude.org>
2018-12-06 08:31:12 -0600

    Remove browser-sync dependency; untrack pecan and sjcharts related html files;

--- scripts/webpack.config.dev.js
+++ scripts/webpack.config.dev.js
@@ -6,18 +6,9 @@ const _c=require('../serverconfig')
 config.output.publicPath = (_c.host || 'http://localhost:3000') + '/bin/'
 
 var WebpackNotifierPlugin = require('webpack-notifier')
-//var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 config.plugins=[
 	new webpack.IgnorePlugin(/jquery/),
-	/*
-	new BrowserSyncPlugin({
-    	host: 'localhost', // todo replace this with your hostname
-       port: 3000,
-       //server: { baseDir: ['public'] },
-	   proxy:'http://localhost:3000/'
-     }),
-	 */
 	new WebpackNotifierPlugin()
 ]
 
