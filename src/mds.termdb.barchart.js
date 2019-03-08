4d53314048a251458050a184df67ec75e2fe3c36
jaimin patel <caprizone6@gmail.com>
2019-03-08 09:25:42 -0600

    barchart for single term

--- src/mds.termdb.barchart.js
+++ src/mds.termdb.barchart.js
@@ -1,8 +1,10 @@
 import * as client from './client'
 import * as common from './common'
-//import {axisTop} from 'd3-axis'
-//import {scaleLinear,scaleOrdinal,schemeCategory10} from 'd3-scale'
+import {axisLeft} from 'd3-axis'
+import {format as d3format} from 'd3-format'
+import {scaleLinear,scaleOrdinal,schemeCategory10} from 'd3-scale'
 import {select as d3select,selectAll as d3selectAll,event as d3event} from 'd3-selection'
+import { stringify } from 'querystring';
 
 
 
@@ -28,35 +30,85 @@ export function barchart_make ( arg ) {
 		- log scale
 */
 
-console.log(arg) // for testing only
+	// console.log(arg) // for testing only
 
+	//constant variables
+	let barheight=300,
+	barwidth=20,
+	space=5,
+	axisheight=barheight+5,
+	barspace=2,
+	maxlabelwidth=0,
+	maxvalue=0
+
+	const items_len = arg.items.length
 	const svg = arg.holder.append('svg')
+	const axisg=svg.append('g')
 
 	// set y axis min/max scale
 	const [yscale_min, yscale_max] = set_yscale( arg )
+
+	// also derive label font size
+	const label_fontsize = 15
+
+	const max_label_height = get_max_labelheight( arg, label_fontsize, svg  )
+
+	// set y axis height
+
+	const yaxis_width = 50
 
 	/* plot vertical bars
 	each bar has equal width
 	set bar width based on number of data points to make it comfortable
 	plot item labels under bar, with 45. rotation
 	*/
+	
 	// const barwidth = barchart_setbarwidth( arg )
-	// also derive label font size
-	const label_fontsize = 12
-
-	const max_label_height = get_max_labelheight( arg, label_fontsize, svg  )
-	console.log(max_label_height)
+	svg.attr('width',items_len*(barwidth+barspace)+(space*2)+yaxis_width)
+	.attr('height',axisheight+max_label_height+space)
 
 
-	// set y axis height
+	// Y axis
+	axisg.attr('transform','translate('+yaxis_width+','+space+')')
+		.call(axisLeft().scale(
+			scaleLinear().domain([yscale_max,0]).range([0,barheight])
+			)
+			.tickFormat(d3format('d'))
+		)
+	client.axisstyle({
+		axis:axisg,
+		showline:true,
+		fontsize:barwidth*.8,
+		color:'black'
+	})
 
+	// barplot format
+	let x=yaxis_width+space
+	const sf=barheight/yscale_max
 
-	const bar_space = 2
+	for(let i=0; i<items_len; i++) {
+		const j=arg.items[i]
 
-	const yaxis_width = 50
+		// X axis
+		svg.append('text')
+		.text(j[0])
+		.attr("transform", "translate("+ (x+barwidth/2) +","+ (axisheight+4) +") rotate(-65)")
+		.attr('text-anchor','end')
+		.attr('font-size',label_fontsize)
+		.attr('font-family',client.font)
+		.attr('dominant-baseline','central')
+		
 
+		// bars for barplot
+		svg.append('rect')
+		.attr('x',x)
+		.attr('y',axisheight-sf*j[1])
+		.attr('width',barwidth)
+		.attr('height',sf*j[1])
+		.attr('fill','#901739')
+		x+=barwidth+barspace
+	}
 
-	// set svg dimention
 }
 
 
