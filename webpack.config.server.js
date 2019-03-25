8044a8a5bdc96204b644ceb982a878e804b96cf7
Edgar Sioson <esioson@stjude.org>
2019-03-25 13:33:06 -0500

    Use --env.NODE_ENV=development, if detected, for webpack server mode

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,12 +1,15 @@
 const nodeExternals = require('webpack-node-externals')
 const webpack=require('webpack')
 
-module.exports={
-	target:'node',
-	externals: [nodeExternals()],
-	entry:'./app.js',
-	output:{
-		path: __dirname,
-		filename:'server.js',
-	},
+module.exports=function(env){
+	return {
+		mode: env.NODE_ENV ? env.NODE_ENV : 'production',
+		target:'node',
+		externals: [nodeExternals()],
+		entry:'./app.js',
+		output:{
+			path: __dirname,
+			filename:'server.js',
+		},
+	}
 }
