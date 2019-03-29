ea9d806a4b638c14133397c627a914aa58f6c363
Edgar Sioson <esioson@stjude.org>
2019-03-29 11:05:05 -0500

    Handle missing env argument in webpack server config

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,9 +1,9 @@
 const nodeExternals = require('webpack-node-externals')
 const webpack=require('webpack')
 
-module.exports=function(env){
+module.exports=function(env={}){
 	return {
-		mode: env.NODE_ENV ? env.NODE_ENV : 'production',
+		mode: env && env.NODE_ENV ? env.NODE_ENV : 'production',
 		target:'node',
 		externals: [nodeExternals()],
 		entry:'./app.js',
