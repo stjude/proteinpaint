306991445067173d84b7889458e96272f078462f
xzhou <xzhou1@stjude.org>
2019-05-10 15:24:19 -0500

    updates to ppc deployment

--- scripts/deploy.ppc.sh
+++ scripts/deploy.ppc.sh
@@ -1,29 +1,31 @@
 #npm run ppdev
 
-cp server.js ../proteinpaint/
-cp public/index.html ../proteinpaint/public/
-cp genome/* ../proteinpaint/genome/
-cp dataset/* ../proteinpaint/dataset/
-cp src/common.js src/vcf.js src/bulk* src/tree.js ../proteinpaint/src/
-cp public/bin/* ../proteinpaint/public/bin/
-cp public/dev.html ../proteinpaint/public/
-
-sed 's%http://localhost:3000/bin/%https://ppc.stjude.org/bin/%' < public/bin/proteinpaint.js > ../proteinpaint/public/bin/proteinpaint.js
-
-cd ..
-tar zcvf sourcecode.tgz proteinpaint/
-scp sourcecode.tgz xzhou1@ppc.stjude.org:/opt/app/pp
+cp server.js deploys/ppc/
+cp public/index.html deploys/ppc/
+cp src/common.js src/vcf.js src/bulk* src/tree.js src/mds.termdb.termvaluesetting.js deploys/ppc/src/
+cp genome/* deploys/ppc/genome/
+cp dataset/* deploys/ppc/dataset/
+cp public/bin/* deploys/ppc/public/bin/
+cp package.json deploys/ppc/
 
 
-ssh -t xzhou1@ppc.stjude.org "
-	cd /opt/app/pp
-	tar zxvf sourcecode.tgz
-	mv proteinpaint/server.js .
-	mv proteinpaint/dataset/*js dataset/
-	mv proteinpaint/genome/*js genome/
-	mv proteinpaint/public/index.html public/
-	mv proteinpaint/public/bin/* public/bin/
-	mv proteinpaint/src/* src/
-	rm -rf proteinpaint/ sourcecode.tgz
-	./node_modules/forever/bin/forever stop server.js;./node_modules/forever/bin/forever -o forever.out -e forever.err start server.js --max-old-space-size=81920
-"
+# port 3001 is hardcoded for xin's computer
+sed 's%http://localhost:3001/bin/%https://ppc.stjude.org/bin/%' < public/bin/proteinpaint.js > deploys/ppc/public/bin/proteinpaint.js
+
+cd deploys/
+tar zcvf sourcecode.tgz ppc/
+scp sourcecode.tgz genomeuser@ppc.stjude.org:/opt/app/pp
+
+
+#ssh -t genomeuser@ppc.stjude.org "
+#	cd /opt/app/pp
+#	tar zxvf sourcecode.tgz
+#	mv proteinpaint/server.js .
+#	mv proteinpaint/dataset/*js dataset/
+#	mv proteinpaint/genome/*js genome/
+#	mv proteinpaint/public/index.html public/
+#	mv proteinpaint/public/bin/* public/bin/
+#	mv proteinpaint/src/* src/
+#	rm -rf proteinpaint/ sourcecode.tgz
+#	./node_modules/forever/bin/forever stop server.js;./node_modules/forever/bin/forever -o forever.out -e forever.err start server.js --max-old-space-size=81920
+#"
--- utils/deliver.sh
+++ utils/deliver.sh
@@ -1,6 +1,7 @@
 cp package.json deploys/deliver/proteinpaint/
 cp utils/install.pp.js deploys/deliver/proteinpaint/utils/
 cp utils/binom.R deploys/deliver/proteinpaint/utils/
+cp utils/fisher.R deploys/deliver/proteinpaint/utils/
 cp genome/* deploys/deliver/proteinpaint/genome/
 cp dataset/clinvar* deploys/deliver/proteinpaint/dataset/
 cp public/index.html deploys/deliver/proteinpaint/public/
