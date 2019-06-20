4557542b4f0917f74ae97a0d80532b421afe9ace
jaimin patel <jaimin.patel@stjude.org>
2019-06-20 16:25:25 -0500

    added encodeURIComponent(): tk.sample_termfilter[] JSON string

--- src/mds.termdb.termvaluesetting.ui.js
+++ src/mds.termdb.termvaluesetting.ui.js
@@ -31,7 +31,7 @@ group{}
 
     let vcf_filter_str = false
     if(vcf_filter) {
-        vcf_filter_str = JSON.stringify(to_parameter(vcf_filter))
+        vcf_filter_str = encodeURIComponent(JSON.stringify(to_parameter(vcf_filter)))
     }
 
     update_terms(terms_div)
