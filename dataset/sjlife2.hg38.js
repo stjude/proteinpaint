94668c4b9011adde7969d732b1d84c61046c3f25
xzhou <xzhou1@stjude.org>
2019-08-20 15:09:10 -0500

    grade 0 as computable grade

--- dataset/sjlife2.hg38.js
+++ dataset/sjlife2.hg38.js
@@ -21,6 +21,7 @@ module.exports={
 				events_key:'conditionevents',
 				grade_key: 'grade',
 				grade_labels: [ // computable grades
+					{ grade: 0, label: '0: Normal' },
 					{ grade: 1, label: '1: Mild' },
 					{ grade: 2, label: '2: Moderate' },
 					{ grade: 3, label: '3: Severe' },
@@ -28,7 +29,6 @@ module.exports={
 					{ grade: 5, label: '5: Death' }
 				],
 				uncomputable_grades: {
-					0: 'No symptom',
 					9: 'Unknown status'
 				},
 				comparison_groups: [
