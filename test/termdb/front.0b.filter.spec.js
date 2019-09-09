e724e8783b13c3717d023152584ef6fe3dcb71f4
jaimin patel <jaimin.patel@stjude.org>
2019-09-09 17:34:39 -0500

    categorical and numrical filter tests

--- test/termdb/front.0b.filter.spec.js
+++ test/termdb/front.0b.filter.spec.js
@@ -36,14 +36,8 @@ tape("filter term-value button", function(test) {
 			bar_click_menu: {
 				add_filter: true
 			},
-			plot2restore: {
-				term: termjson["diaggrp"],
-				settings: {
-					currViews: ["barchart"]
-				}
-			},
 			callbacks: {
-				plot: {
+				tree: {
 					postRender: [testFilterDisplay, triggerFilterRemove]
 				}
 			}
@@ -51,82 +45,52 @@ tape("filter term-value button", function(test) {
 	})
 
 	function testFilterDisplay(obj) {
-		test.equal(
-			obj.obj.dom.termfilterdiv.selectAll(".term_name_btn").html(),
-			termfilter.terms[0].term.name,
-			"should Filter term-name and plot clicked from runpp() be same"
-		)
-		test.equal(
-			obj.obj.dom.termfilterdiv
-				.selectAll(".value_btn")
-				.html()
-				.slice(0, -2),
-			termfilter.terms[0].values[0].label,
-			"should Filter value and value supplied from runpp() be same"
-		)
-		test.true(
-			obj.obj.dom.termfilterdiv.selectAll(".term_remove_btn").size() >= 1,
-			"should have 'x' button to remove filter"
-		)
-		test.true(
-			obj.obj.dom.termfilterdiv.selectAll(".add_term_btn").size() >= 1,
-			"should have '+' button to add new term filter"
-		)
+    obj.bus.on("postRender", null)
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".term_name_btn").html(),
+        termfilter.terms[0].term.name,
+        "should Filter term-name and plot clicked from runpp() be same"
+      )
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".value_btn").html().slice(0, -2),
+        termfilter.terms[0].values[0].label,
+        "should Filter value and value supplied from runpp() be same"
+      )
+      test.true(
+        obj.dom.termfilterdiv.selectAll(".term_remove_btn").size() >= 1,
+        "should have 'x' button to remove filter"
+      )
+      test.true(
+        obj.dom.termfilterdiv.selectAll(".add_term_btn").size() >= 1,
+        "should have '+' button to add new term filter"
+      )
+    }, 100)
 	}
 
 	function triggerFilterRemove(obj) {
-		obj.bus.on("postRender", [testFilterRemove, triggerFilterAdd])
-		setTimeout(
-			() =>
-				obj.obj.dom.termfilterdiv
-					.selectAll(".term_remove_btn")
-					.node()
-					.click(),
-			100
-		)
+		obj.bus.on("postRender", [testFilterRemove,triggerFilterAdd])
+		setTimeout(() => obj.dom.termfilterdiv
+			.selectAll(".term_remove_btn")
+			.node()
+			.click(), 400)
 	}
 
 	function testFilterRemove(obj) {
-		test.equal(
-			obj.obj.dom.termfilterdiv.selectAll(".term_name_btn").size(),
-			0,
-			"should remove tvs filter after clicking 'x'"
-		)
-	}
+		test.equal(obj.dom.termfilterdiv.selectAll(".term_name_btn").size(), termfilter.terms.length, "should remove tvs filter after clicking 'x'")
+  }
+  
+  function triggerFilterAdd(obj){
+    obj.bus.on("postRender", testAddTerm)
+    termfilter.terms[0] = {
+      term: { id: "diaggrp", name: "Diagnosis Group", iscategorical: true },
+      values: [{ key: "Acute lymphoblastic leukemia", label: "Acute lymphoblastic leukemia" }]
+    }
+    setTimeout(() => obj.main(), 200)
+  } 
 
-	function triggerFilterAdd(obj) {
-		obj.bus.on("postRender", null)
-		setTimeout(
-			() =>
-				obj.obj.dom.termfilterdiv
-					.selectAll(".add_term_btn")
-					.node()
-					.click(),
-			200
-		)
-		setTimeout(
-			() =>
-				obj.obj.tvstip.d
-					.selectAll(".sja_menuoption")
-					.node()
-					.click(),
-			250
-		)
-		setTimeout(() => obj.obj.tvstip.d.selectAll(".sja_menuoption")._groups[0][1].click(), 350)
-		setTimeout(() => obj.obj.tvstip.d.selectAll(".sja_menuoption")._groups[0][2].click(), 450)
-		setTimeout(() => {
-			const elem = obj.obj.tvstip.d.select(".bars-cell").select("rect")
-			elem.node().dispatchEvent(new Event("click", { bubbles: true }))
-			testAddTerm(obj)
-		}, 1200)
-	}
-
-	function testAddTerm(obj) {
-		test.equal(
-			obj.obj.dom.termfilterdiv.selectAll(".term_name_btn").size(),
-			1,
-			"should add term value filter for clicked term"
-		)
+  function testAddTerm(obj) {
+		test.equal(obj.dom.termfilterdiv.selectAll(".term_name_btn").size(), termfilter.terms.length, "should add filter from data")
 		obj.tip.hide()
 		test.end()
 	}
@@ -134,11 +98,11 @@ tape("filter term-value button", function(test) {
 
 tape("filter term-value button: categorical term", function(test) {
 	test.timeoutAfter(3000)
-	test.plan(2)
+	test.plan(6)
 	const div0 = d3s.select("body").append("div")
 	const termfilter = {
-		show_top_ui: true,
-		terms: [
+    show_top_ui: true,
+    terms: [
 			{
 				term: { id: "diaggrp", name: "Diagnosis Group", iscategorical: true },
 				values: [{ key: "Wilms tumor", label: "Wilms tumor" }]
@@ -159,34 +123,194 @@ tape("filter term-value button: categorical term", function(test) {
 			bar_click_menu: {
 				add_filter: true
 			},
-			plot2restore: {
-				term: termjson["diaggrp"],
-				settings: {
-					currViews: ["barchart"]
-				}
-			},
 			callbacks: {
-				plot: {
-					postRender: [testFilterDisplay /*,triggerChangeCategory ,triggerAddCategory */]
+				tree: {
+					postRender: [testFilterDisplay, triggerChangeNegation ,triggerAddCategory, triggerRemoveCategory]
 				}
 			}
 		}
 	})
 
-	function testFilterDisplay(obj) {
-		obj.bus.on("postRender", null)
-		setTimeout(() => {
-			test.equal(
-				obj.obj.dom.termfilterdiv.selectAll(".condition_btn").size(),
-				1,
-				"should have 'IS' button for categorical filter"
-			)
-			test.equal(
-				obj.obj.dom.termfilterdiv.selectAll(".add_value_btn").size(),
-				1,
-				"should have '+' button to add category to filter"
-			)
-			test.end()
-		}, 100)
+  function testFilterDisplay(obj) {
+    obj.bus.on("postRender", null)
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".condition_btn").size(), 1,
+        "should have negation button for categorical filter"
+      )
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".condition_btn").html(), "IS",
+        "should have 'IS' for negation button for categorical filter"
+      )
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".add_value_btn").size(), 1,
+        "should have '+' button to add category to filter"
+      )
+    },100)
+  }
+
+  function triggerChangeNegation(obj){
+    setTimeout(() => {
+      obj.bus.on("postRender", checkNegationBtnVal)
+      obj.termfilter.terms[0].isnot = true
+      obj.main()
+    }, 200)
+  }
+
+  function checkNegationBtnVal(obj){
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".condition_btn").html(), "IS NOT",
+        "should have 'IS NOT' for negation button after change"
+      )
+    },300)
+  }
+
+  function triggerAddCategory(obj){
+    setTimeout(() => {
+      obj.bus.on("postRender", checkAddedCategory)
+      obj.termfilter.terms[0].values[1] = { key: "Acute lymphoblastic leukemia", label: "Acute lymphoblastic leukemia" }
+      obj.main()
+    }, 400)
+  }
+
+  function checkAddedCategory(obj){
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".value_btn").size(), 
+        termfilter.terms[0].values.length, 
+        "should add category from data"
+      )
+    },500)
+  }
+
+  function triggerRemoveCategory(obj){
+    setTimeout(() => {
+      obj.bus.on("postRender", checkRemovedCategory)
+      obj.termfilter.terms[0].values.pop()
+      obj.main()
+    }, 600)
+  }
+
+  function checkRemovedCategory(obj){
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".value_btn").size(), 
+        termfilter.terms[0].values.length, 
+        "should remove category from data"
+      )
+      test.end()
+    },700)
+  }
+})
+
+tape("filter term-value button: Numerical term", function(test) {
+	test.timeoutAfter(4000)
+	test.plan(6)
+	const div0 = d3s.select("body").append("div")
+	const termfilter = {
+		show_top_ui: true,
+		terms: [
+      {
+        term: { id: "aaclassic_5", name: "Cumulative Alkylating Agent (Cyclophosphamide Equivalent Dose)", unit: "mg/m²", isfloat: true },
+        ranges: [{ stopinclusive: true, start: 1000, stop: 2000}]
+      }
+    ]
 	}
+
+	runproteinpaint({
+		host,
+		holder: div0.node(),
+		noheader: 1,
+		nobox: true,
+		display_termdb: {
+			dslabel: "SJLife",
+			genome: "hg38",
+			default_rootterm: {},
+			termfilter,
+			callbacks: {
+				tree: {
+					postRender: [testFilterDisplay, triggerchangeRange ,triggerAddUnannotatedRange, triggerRemoveRange]
+				}
+			}
+		}
+  })
+
+  function testFilterDisplay(obj){
+    obj.bus.on("postRender", null)
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".term_name_btn").html(),
+        termfilter.terms[0].term.name,
+        "should Filter btn and term-name from runpp() be same"
+      )
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".value_btn").html().split(" ")[0],
+        termfilter.terms[0].ranges[0].start.toString(),
+        "should have value button same as data"
+      )
+      test.true(
+        obj.dom.termfilterdiv.selectAll(".add_value_btn").size() >= 1,
+        "should have '+' button to add unannoated value to filter"
+      )
+    },100)
+  }
+
+  function triggerchangeRange(obj){
+    setTimeout(() => {
+      obj.bus.on("postRender", checkRangeBtn)
+      obj.termfilter.terms[0].ranges[0] = { stopinclusive: true, start: 3000, stop: 4000}
+      obj.main()
+    }, 200)
+  }
+
+  function checkRangeBtn(obj){
+    obj.bus.on("postRender",null)
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".value_btn").html().split(" ")[0],
+        termfilter.terms[0].ranges[0].start.toString(),
+        "should have value button changed from data"
+      )
+    },300)
+  }
+  
+  function triggerAddUnannotatedRange(obj){
+    setTimeout(() => {
+      obj.bus.on("postRender", checkUnannotatedValBtn)
+      obj.termfilter.terms[0].ranges[1] = { is_unannotated:true, value:"-9999", label:"Unknown treatment record"}
+      obj.main()
+    }, 400)
+  }
+
+  function checkUnannotatedValBtn(obj){
+    obj.bus.on("postRender",null)
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".value_btn")._groups[0][1].innerText,
+        termfilter.terms[0].ranges[1].label,
+        "should have unannotated value button added from data"
+      )
+    },600)
+  }
+
+  function triggerRemoveRange(obj){
+    setTimeout(() => {
+      obj.bus.on("postRender", checkRemovedRange)
+      obj.termfilter.terms[0].ranges.pop()
+      obj.main()
+    }, 1200)
+  }
+
+  function checkRemovedRange(obj){
+    obj.bus.on("postRender",null)
+    setTimeout(() => {
+      test.equal(
+        obj.dom.termfilterdiv.selectAll(".value_btn").size(),
+        termfilter.terms[0].ranges.length,
+        "should remove value button altered by data"
+      )
+      test.end()
+    },1400)
+  }
 })
