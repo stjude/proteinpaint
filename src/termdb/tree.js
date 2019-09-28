07ac30a60b0d5c773d306c0387d208aa9156289e
Edgar Sioson <esioson@stjude.org>
2019-09-28 18:07:07 -0500

    Divide termdb tree component code into an instance/data processing class, a renderer methods setter, and an interactivity methods setter

--- src/termdb/tree.js
+++ src/termdb/tree.js
@@ -5,14 +5,38 @@ import { plotInit, plotConfig } from './plot'
 
 const childterm_indent = '30px'
 
+/*
+	Recommended Component Code Organization
+
+	(a) class (produces instance):
+  - all methods expected by rx.api (main, etc)
+	- all data processing code
+
+	(b) setRenderers(self): attaches renderer methods
+	
+	(c) setInteractivity(self): attaches event handlers
+*/
+
 class TdbTree {
+	/*
+	Termdb Tree Component
+	- api-related and data processing code
+	  within this class declaration 
+
+	opts: {
+		holder,
+		callbacks // see bus below
+	}
+*/
 	constructor(app, opts) {
 		this.api = rx.getComponentApi(this)
 		this.notifyComponents = rx.notifyComponents
 		this.app = app
 		this.dom = { holder: opts.holder }
-		// set closure methods to handle conflicting "this" contexts
-		this.notThis(this)
+
+		// attach instance-specific methods via closure
+		setRenderers(this)
+		setInteractivity(this)
 
 		this.components = {
 			plots: {}
@@ -78,35 +102,6 @@ class TdbTree {
 		return terms
 	}
 
-	renderBranch(term, div) {
-		if (!term || !term.terms) return
-		if (!(term.id in this.termsById)) return
-		const expanded = this.app.state().tree.expandedTerms.includes(term.id)
-		if (!expanded) {
-			div.style('display', 'none')
-			// do not update hidden child termdiv's
-			return
-		}
-		div.style('display', 'block')
-
-		const cls = 'termdiv'
-		const childTermIds = term.terms.map(this.bindKey)
-		const divs = div
-			.selectAll('.' + cls)
-			.filter(t => childTermIds.includes(t.id))
-			.data(term.terms, this.bindKey)
-
-		divs.exit().each(this.hideTerm)
-
-		divs.each(this.updateTerm)
-
-		divs
-			.enter()
-			.append('div')
-			.attr('class', cls)
-			.each(this.addTerm)
-	}
-
 	viewPlot(action) {
 		const plot = this.components.plots[action.id]
 		if (plot) plot.main(action)
@@ -120,151 +115,7 @@ class TdbTree {
 			this.components.plots[action.id] = newPlot
 		}
 		if (action.type != 'plot_edit') {
-			const show = action.type == 'plot_add' || action.type == 'plot_show'
-			this.dom.holder
-				.selectAll('.termchilddiv')
-				.filter(term => term.id == action.id)
-				.style('overflow', show ? '' : 'hidden')
-				.style('height', show ? '' : 0)
-				.style('opacity', show ? 1 : 0)
-		}
-	}
-
-	notThis(self) {
-		// cannot use arrow function since the
-		// alternate "this" context is needed
-
-		// this == the d3 selected DOM node
-		self.hideTerm = function(term) {
-			//console.log(138, 'hideTerm', term.id)
-			if (self.app.state().tree.expandedTerms.includes(term.id)) return
-			select(this).style('display', 'none')
-		}
-
-		self.updateTerm = function(term) {
-			//console.log(143, 'updateTerm', term.id)
-			const div = select(this)
-			div.datum(term)
-			const expanded = self.app.state().tree.expandedTerms.includes(term.id)
-			const divs = selectAll(this.childNodes).filter(function() {
-				return !this.className.includes('termchilddiv')
-			})
-
-			divs
-				.select('.termbtn')
-				.datum(term)
-				.html(!expanded ? '+' : '-')
-
-			divs
-				.select('.termlabel')
-				.datum(term)
-				.html(term.name)
-
-			divs
-				.select('.termview')
-				.datum(term)
-				.html('VIEW')
-
-			const plot = self.app.state({ type: 'plot', id: term.id })
-			const isVisible = expanded || (plot && plot.isVisible)
-			const childdiv = divs
-				.select('.termchilddiv')
-				.datum(term)
-				.style('display', expanded ? 'block' : 'none')
-				.style('overflow', isVisible ? '' : 'hidden')
-				.style('height', isVisible ? '' : 0)
-				.style('opacity', isVisible ? 1 : 0)
-		}
-
-		self.addTerm = function(term) {
-			//console.log(176, 'addTerm', term.id)
-			const div = select(this)
-			div
-				.datum(term)
-				.style('display', 'block')
-				.style('margin', term.isleaf ? '' : '2px')
-				.style('padding', '0px 5px')
-				.style('padding-left', term.isleaf ? 0 : '')
-				.style('cursor', 'pointer')
-
-			if (!term.isleaf) {
-				div
-					.append('div')
-					.datum(term)
-					.html('+')
-					.attr('class', 'sja_menuoption termbtn')
-					.style('display', 'inline-block')
-					.style('padding', '4px 9px')
-					.style('background', '#ececec')
-					.style('font-family', 'courier')
-					.on('click', self.toggleTerm)
-			}
-
-			div
-				.append('div')
-				.datum(term)
-				.html(term.name)
-				.attr('class', 'termlabel')
-				.style('display', 'inline-block')
-				.style('text-align', 'center')
-				.style('padding', '5px 5px 5px 5px')
-				.on('click', self.toggleTerm)
-
-			if (term.isleaf) {
-				div
-					.append('div')
-					.attr('class', 'termview')
-					.datum(term)
-					.html('VIEW')
-					.style('display', 'inline-block')
-					.style('display', 'inline-block')
-					.style('border', '1px solid #aaa')
-					.style('padding', '2px 5px')
-					.style('margin-left', '50px')
-					.style('background', '#ececec')
-					.style('font-size', '0.8em')
-					.on('click', self.togglePlot)
-			}
-
-			const childdiv = div
-				.append('div')
-				.datum(term)
-				.attr('class', 'termchilddiv')
-				.style('padding-left', childterm_indent)
-				.style('transition', '0.3s ease')
-
-			const expanded = self.app.state().tree.expandedTerms.includes(term.id)
-			if (expanded) self.renderBranch(term, childdiv)
-		}
-
-		self.toggleTerm = function(term) {
-			event.stopPropagation()
-			if (term.isleaf) return
-			const expanded = self.app.state().tree.expandedTerms.includes(term.id)
-			const type = expanded ? 'tree_collapse' : 'tree_expand'
-			const holder = selectAll(this.parentNode.childNodes).filter(function() {
-				return this.className.includes('termchilddiv')
-			})
-			self.app.dispatch({ type, termId: term.id, term, holder })
-		}
-
-		self.togglePlot = function(term) {
-			event.stopPropagation()
-			const plot = self.app.state().tree.plots[term.id]
-			if (!plot) {
-				// need to assess pros and cons of passing the holder via action versus alternatives
-				const holder = select(select(this).node().parentNode.lastChild)
-				self.app.dispatch({
-					type: 'plot_add',
-					id: term.id,
-					term,
-					holder,
-					config: plotConfig({ term })
-				})
-			} else {
-				const type = !plot || !plot.isVisible ? 'plot_show' : 'plot_hide'
-				self.app.dispatch({ type, id: term.id, term })
-			}
+			this.updatePlotView(action)
 		}
 	}
 
@@ -273,4 +124,202 @@ class TdbTree {
 	}
 }
 
-exports.treeInit = rx.getInitFxn(TdbTree)
+export const treeInit = rx.getInitFxn(TdbTree)
+
+function setRenderers(self) {
+	/*
+		Set static renderer code here for HTML, SVGs, etc 
+
+		Closured reference to object instance as self
+		versus alternative "this" context such as DOM element
+
+		self: a TdbTree instance
+	*/
+	// !!! no free-floating variable declarations here !!!
+	// set properties within the class declarations
+
+	self.renderBranch = (term, div) => {
+		if (!term || !term.terms) return
+		if (!(term.id in self.termsById)) return
+		const expanded = self.app.state().tree.expandedTerms.includes(term.id)
+		if (!expanded) {
+			div.style('display', 'none')
+			// do not update hidden child termdiv's
+			return
+		}
+		div.style('display', 'block')
+
+		const cls = 'termdiv'
+		const childTermIds = term.terms.map(self.bindKey)
+		const divs = div
+			.selectAll('.' + cls)
+			.filter(t => childTermIds.includes(t.id))
+			.data(term.terms, self.bindKey)
+
+		divs.exit().each(self.hideTerm)
+
+		divs.each(self.updateTerm)
+
+		divs
+			.enter()
+			.append('div')
+			.attr('class', cls)
+			.each(self.addTerm)
+	}
+
+	// this == the d3 selected DOM node
+	self.hideTerm = function(term) {
+		//console.log('hideTerm', term.id)
+		if (self.app.state().tree.expandedTerms.includes(term.id)) return
+		select(this).style('display', 'none')
+	}
+
+	self.updateTerm = function(term) {
+		//console.log('updateTerm', term.id)
+		const div = select(this)
+		div.datum(term)
+		const expanded = self.app.state().tree.expandedTerms.includes(term.id)
+		const divs = selectAll(this.childNodes).filter(function() {
+			return !this.className.includes('termchilddiv')
+		})
+
+		divs
+			.select('.termbtn')
+			.datum(term)
+			.html(!expanded ? '+' : '-')
+
+		divs
+			.select('.termlabel')
+			.datum(term)
+			.html(term.name)
+
+		divs
+			.select('.termview')
+			.datum(term)
+			.html('VIEW')
+
+		const plot = self.app.state({ type: 'plot', id: term.id })
+		const isVisible = expanded || (plot && plot.isVisible)
+		const childdiv = divs
+			.select('.termchilddiv')
+			.datum(term)
+			.style('display', expanded ? 'block' : 'none')
+			.style('overflow', isVisible ? '' : 'hidden')
+			.style('height', isVisible ? '' : 0)
+			.style('opacity', isVisible ? 1 : 0)
+	}
+
+	self.addTerm = function addTerm(term) {
+		//console.log('addTerm', term.id)
+		const div = select(this)
+		div
+			.datum(term)
+			.style('display', 'block')
+			.style('margin', term.isleaf ? '' : '2px')
+			.style('padding', '0px 5px')
+			.style('padding-left', term.isleaf ? 0 : '')
+			.style('cursor', 'pointer')
+
+		if (!term.isleaf) {
+			div
+				.append('div')
+				.datum(term)
+				.html('+')
+				.attr('class', 'sja_menuoption termbtn')
+				.style('display', 'inline-block')
+				.style('padding', '4px 9px')
+				.style('background', '#ececec')
+				.style('font-family', 'courier')
+				.on('click', self.toggleTerm)
+		}
+
+		div
+			.append('div')
+			.datum(term)
+			.html(term.name)
+			.attr('class', 'termlabel')
+			.style('display', 'inline-block')
+			.style('text-align', 'center')
+			.style('padding', '5px 5px 5px 5px')
+			.on('click', self.toggleTerm)
+
+		if (term.isleaf) {
+			div
+				.append('div')
+				.attr('class', 'termview')
+				.datum(term)
+				.html('VIEW')
+				.style('display', 'inline-block')
+				.style('display', 'inline-block')
+				.style('border', '1px solid #aaa')
+				.style('padding', '2px 5px')
+				.style('margin-left', '50px')
+				.style('background', '#ececec')
+				.style('font-size', '0.8em')
+				.on('click', self.togglePlot)
+		}
+
+		const childdiv = div
+			.append('div')
+			.datum(term)
+			.attr('class', 'termchilddiv')
+			.style('padding-left', childterm_indent)
+			.style('transition', '0.3s ease')
+
+		const expanded = self.app.state().tree.expandedTerms.includes(term.id)
+		if (expanded) self.renderBranch(term, childdiv)
+	}
+
+	self.updatePlotView = function(action) {
+		const show = action.type == 'plot_add' || action.type == 'plot_show'
+		self.dom.holder
+			.selectAll('.termchilddiv')
+			.filter(term => term.id == action.id)
+			.style('overflow', show ? '' : 'hidden')
+			.style('height', show ? '' : 0)
+			.style('opacity', show ? 1 : 0)
+	}
+}
+
+function setInteractivity(self) {
+	/*
+		Set interactivity code here, for mouseovers, clicks, etc.
+
+		Closured reference to object instance as self
+		versus alternative "this" context such as DOM element
+
+		self: a TdbTree instance
+	*/
+	// !!! no free-floating variable declarations here !!!
+	// use self to create properties
+
+	self.toggleTerm = function(term) {
+		event.stopPropagation()
+		if (term.isleaf) return
+		const expanded = self.app.state().tree.expandedTerms.includes(term.id)
+		const type = expanded ? 'tree_collapse' : 'tree_expand'
+		const holder = selectAll(this.parentNode.childNodes).filter(function() {
+			return this.className.includes('termchilddiv')
+		})
+		self.app.dispatch({ type, termId: term.id, term, holder })
+	}
+
+	self.togglePlot = function(term) {
+		event.stopPropagation()
+		const plot = self.app.state().tree.plots[term.id]
+		if (!plot) {
+			// need to assess pros and cons of passing the holder via action versus alternatives
+			const holder = select(select(this).node().parentNode.lastChild)
+			self.app.dispatch({
+				type: 'plot_add',
+				id: term.id,
+				term,
+				holder,
+				config: plotConfig({ term })
+			})
+		} else {
+			const type = !plot || !plot.isVisible ? 'plot_show' : 'plot_hide'
+			self.app.dispatch({ type, id: term.id, term })
+		}
+	}
+}
