2acd4c8296e567634b3b09552a568539f36746eb
jaimin patel <jaimin.patel@stjude.org>
2019-10-09 10:12:22 -0500

    hide unit from click-menu and tip if value is uncomputable

--- src/mds.termdb.barchart.events.js
+++ src/mds.termdb.barchart.events.js
@@ -12,13 +12,16 @@ export default function getHandlers(self) {
 		// while bar data has all required data including seriesId
 		const term1 = self.terms.term1
 		const term2 = self.terms.term2 ? self.terms.term2 : null
-		const term1unit = term1.unit
+		const uncomp_term1 = term1.values ? Object.values(term1.values).map(v=>v.label) : []
+		const uncomp_term2 = term2 && term2.values ? Object.values(term2.values).map(v=>v.label) : []
+		const term1unit = term1.unit && !uncomp_term1.includes(d.seriesId) ? ' ' + term1.unit : ''
+		const term2unit = term2 && term2.unit && !uncomp_term2.includes(d.dataId) ? ' ' + term2.unit : ''
 		const seriesLabel =
 			(term1.values && d.seriesId in term1.values ? term1.values[d.seriesId].label : d.seriesId ? d.seriesId : d.id) +
-			(term1.unit ? ' ' + term1.unit : '')
+			term1unit
 		const dataLabel =
 			(term2 && term2.values && d.dataId in term2.values ? term2.values[d.dataId].label : d.dataId ? d.dataId : d.id) +
-			(term2 && term2.unit ? ' ' + term2.unit : '')
+			term2unit
 		const icon = !term2
 			? ''
 			: "<div style='display:inline-block; width:14px; height:14px; margin: 2px 3px; vertical-align:top; background:" +
@@ -69,13 +72,16 @@ export default function getHandlers(self) {
 			mouseover(d) {
 				const term1 = self.terms.term1
 				const term2 = self.terms.term2 ? self.terms.term2 : null
-				const term1unit = term1.unit
+				const uncomp_term1 = term1.values ? Object.values(term1.values).map(v=>v.label) : []
+				const uncomp_term2 = term2 && term2.values ? Object.values(term2.values).map(v=>v.label) : []
+				const term1unit = term1.unit && !uncomp_term1.includes(d.seriesId) ? ' ' + term1.unit : ''
+				const term2unit = term2 && term2.unit && !uncomp_term2.includes(d.dataId) ? ' ' + term2.unit : ''
 				const seriesLabel =
 					(term1.values && d.seriesId in term1.values ? term1.values[d.seriesId].label : d.seriesId) +
-					(term1.unit ? ' ' + term1.unit : '')
+					term1unit
 				const dataLabel =
 					(term2 && term2.values && d.dataId in term2.values ? term2.values[d.dataId].label : d.dataId) +
-					(term2 && term2.unit ? ' ' + term2.unit : '')
+					term2unit
 				const icon = !term2
 					? ''
 					: "<div style='display:inline-block; width:14px; height:14px; margin: 2px 3px; vertical-align:top; background:" +
