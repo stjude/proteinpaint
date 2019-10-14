618ec3523ec9f48130de32203932af5534e6097e
xzhou <xzhou1@stjude.org>
2019-10-14 16:58:39 -0500

    initiate modifier handling

--- src/termdb/app.js
+++ src/termdb/app.js
@@ -6,6 +6,20 @@ import {filterInit} from "./filter"
 import {sayerror} from '../client'
 //import {controlsInit} from "./controls"
 
+/*
+opts{}
+.states{}
+	required
+	https://docs.google.com/document/d/1gTPKS9aDoYi4h_KlMBXgrMxZeA_P4GXhWcQdNQs3Yp8/edit
+.modifiers{}
+	optional
+	contains key-fxn() pairs
+	a component will consume a specific modifier (addressed by its key) to alter its behavior
+	can run the callback supplied with results to other apps (e.g. click a term in tree)
+	app and components will refer to the same frozen object of "modifiers{}", in a read-only way
+}
+*/
+
 class TdbApp {
 	constructor(opts, holder) {
 		this.opts = opts
@@ -22,9 +36,9 @@ class TdbApp {
 		try {
 			this.store = storeInit(this.api)
 			this.state = this.store.state()
-
+			const modifiers = validate_modifiers(opts.modifiers)
 			this.components = {
-				tree: treeInit(this.api, {holder: holder.append('div')}),
+				tree: treeInit( this.api, { holder: holder.append('div'), modifiers}),
 				terms: filterInit(this.api, {holder: holder.append("div")})
 			}
 		} catch(e) {
@@ -55,3 +69,10 @@ class TdbApp {
 }
 
 exports.appInit = rx.getInitFxn(TdbApp)
+
+function validate_modifiers(tmp={}){
+	for(const k in tmp) {
+		if(typeof tmp[k] != 'function') throw 'modifier "'+k+'" not a function'
+	}
+	return Object.freeze(tmp)
+}
