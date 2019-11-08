b35d9aeb4bc48dcb73d96f18bdefd5dbe73d5433
xzhou82 <xzhou1@stjude.org>
2019-11-08 15:36:39 -0600

    the "getterm" script that does just one thing

---
+++ src/common/getterm.js
@@ -0,0 +1,29 @@
+import { dofetch2 } from '../client'
+
+/*
+to retrieve the termjson object of one term, using its id
+only works for a termdb-enabled dataset
+
+if the function is attached to an instance with .state{ dslabel, genome }, then simply call:
+	await instance.getterm( ? )
+
+otherwise, do:
+	await getterm( id, dslabel, genome )
+
+*/
+
+const cache = {}
+
+export default async function(termid, dslabel = null, genome = null) {
+	if (!termid) throw 'getterm: termid missing'
+	if (this && this.state) {
+		if (this.state.dslabel) dslabel = this.state.dslabel
+		if (this.state.genome) genome = this.state.genome
+	}
+	if (!dslabel) throw 'getterm: dslabel missing'
+	if (!genome) throw 'getterm: genome missing'
+	const data = await dofetch2(`termdb?dslabel=${dslabel}&genome=${genome}&gettermbyid=${termid}`, {}, cache)
+	if (data.error) throw 'getterm: ' + data.error
+	if (!data.term) throw 'no term found for ' + termid
+	return data.term
+}
