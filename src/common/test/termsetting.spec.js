38c143c744cb08c0a723f7c54f8af28492af1cab
xzhou82 <xzhou1@stjude.org>
2019-12-04 15:31:19 -0600

    intended direct functional test on termsetting

--- src/common/test/termsetting.spec.js
+++ src/common/test/termsetting.spec.js
@@ -2,18 +2,16 @@ const tape = require('tape')
 const d3s = require('d3-selection')
 const termsettingInit = require('../termsetting').termsettingInit
 
-
 /*********
 this is the direct functional testing of the component, without the use of runpp()
 this currently doesn't work
 will need to figure out how to allow node/browserify to work with import
 */
 
-tape('\n',test=>{
+tape('\n', test => {
 	test.pass('-***- termsetting -***-')
 	test.end()
 })
-
 
 const holder0 = d3s.select('body').append('div')
 
@@ -24,70 +22,66 @@ test_usebinsless()
 // general testing that can share a pill
 test_menu()
 
-
 function test_disablereplaceremove() {
 	const pill = termsettingInit({
-		holder0.append('div'),
-		genome:'hg38',
-		dslabel:'SJLife',
-		disable_ReplaceRemove:true,
+		holder: holder0.append('div'),
+		genome: 'hg38',
+		dslabel: 'SJLife',
+		disable_ReplaceRemove: true,
 		debug: true,
-		callback: (data)=>{
+		callback: data => {
 			// some logic here
 		}
 	})
 
 	pill.main({
-		term:{
-			id:'dummy',
-			name:'dummy',
-			iscategorical:true,
-			values:{
-				cat1:{label:'Cat 1'}
+		term: {
+			id: 'dummy',
+			name: 'dummy',
+			iscategorical: true,
+			values: {
+				cat1: { label: 'Cat 1' }
 			}
 		},
-		q:{ groupsetting:{ inuse:false } }
+		q: { groupsetting: { inuse: false } }
 	})
 
 	// trigger click on pill
 
-	tape('disable_ReplaceRemove',test=>{
+	tape('disable_ReplaceRemove', test => {
 		// test if no Replace and Remove buttons in menu
 	})
 }
 
-
 function test_usebinsless() {
 	const pill = termsettingInit({
-		holder0.append('div'),
-		genome:'hg38',
-		dslabel:'SJLife',
+		holder: holder0.append('div'),
+		genome: 'hg38',
+		dslabel: 'SJLife',
 		use_bins_less: true,
 		debug: true,
-		callback: (data)=>{
+		callback: data => {
 			// some logic here
 		}
 	})
 
 	pill.main({
-		term:{
-			id:'dummy',
-			name:'dummy',
-			iscategorical:true,
-			values:{
-				cat1:{label:'Cat 1'}
+		term: {
+			id: 'dummy',
+			name: 'dummy',
+			iscategorical: true,
+			values: {
+				cat1: { label: 'Cat 1' }
 			}
 		},
-		q:{ groupsetting:{ inuse:false } }
+		q: { groupsetting: { inuse: false } }
 	})
 
 	// trigger click on pill
 
-	tape('use_bins_less', test=>{
-		// test to see 
+	tape('use_bins_less', test => {
+		// test to see
 	})
 }
 
-
-function test_menu() {
-}
+function test_menu() {}
