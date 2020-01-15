bca15d6e0802a42df938f80a8ba464b2389017ba
Edgar Sioson <esioson@stjude.org>
2020-01-15 12:14:49 -0600

    More filter tests for each control menu option

--- src/common/test/filter.spec.js
+++ src/common/test/filter.spec.js
@@ -59,12 +59,237 @@ async function addDemographicSexFilter(opts, btn) {
 	termdivSex.querySelector('.bars-cell > rect').dispatchEvent(new Event('click', { bubbles: true }))
 }
 
+function normalizeActiveData(opts) {
+	let activeData = JSON.parse(JSON.stringify(opts.filter.Inner.activeData))
+	// delete UI assigned tracking/binding values from activeData
+	delete activeData.item.$id
+	delete activeData.item.ancestry
+	delete activeData.filter.$id
+	delete activeData.filter.ancestry
+	delete activeData.filter.lst[0].$id
+	delete activeData.filter.lst[0].ancestry
+	return { item: activeData.item, filter: activeData.filter }
+}
+
+function getHighlightedRowCount(menuRows, action) {
+	return menuRows
+		.filter(function(d) {
+			return (
+				(d.action == action && this.style.backgroundColor != 'transparent') ||
+				this.style.backgroundColor != 'transparent'
+			)
+		})
+		.size()
+}
+
 /**************
  test sections
 ***************/
 
 tape('\n', test => {
 	test.pass('-***- common/filter -***-')
+	test.end()
+})
+
+tape('control menu', async test => {
+	const opts = getOpts({
+		filterData: {
+			type: 'tvslst',
+			in: true,
+			join: '',
+			lst: [
+				{
+					type: 'tvs',
+					tvs: {
+						term: {
+							id: 'diaggrp',
+							name: 'Diagnosis Group',
+							iscategorical: true
+						},
+						values: [
+							{
+								key: `Wilm's tumor`,
+								label: `Wilm's tumor`
+							}
+						]
+					}
+				}
+			]
+		}
+	})
+
+	await sleep(150)
+	const tipd = opts.filter.Inner.dom.controlsTip.d
+	const menuRows = tipd.selectAll('tr')
+	await opts.filter.main(opts.filterData)
+
+	const pill0 = opts.holder.select('.sja_pill_wrapper').node()
+	pill0.click()
+	await sleep(50)
+
+	test.notEqual(tipd.node().style.display, 'none', 'should be dsplayed when clicking a pill')
+
+	const editOpt = menuRows.filter(d => d.action == 'edit')
+	test.equal(editOpt.size(), 1, 'should have an Edit option')
+	editOpt.node().click()
+	test.equal(getHighlightedRowCount(menuRows, 'edit'), 1, 'should highlight only the edit row when clicked')
+	test.deepEqual(
+		normalizeActiveData(opts),
+		{ item: opts.filterData.lst[0], filter: opts.filterData },
+		'should set the expected edit activeData'
+	)
+	test.notEqual(
+		opts.filter.Inner.dom.treeTip.d.style('display'),
+		'none',
+		'should display the tree tip when clicking the edit option'
+	)
+
+	const replaceOpt = menuRows.filter(d => d.action == 'replace')
+	test.equal(replaceOpt.size(), 1, 'should have a Replace option')
+	replaceOpt.node().click()
+	test.equal(getHighlightedRowCount(menuRows, 'replace'), 1, 'should highlight only the replace row when clicked')
+	test.deepEqual(
+		normalizeActiveData(opts),
+		{ item: opts.filterData.lst[0], filter: opts.filterData },
+		'should set the expected replace activeData'
+	)
+	test.notEqual(
+		opts.filter.Inner.dom.treeTip.d.style('display'),
+		'none',
+		'should display the tree tip when clicking the replace option'
+	)
+
+	const andOpt = menuRows.filter(d => d.action == 'join-and')
+	test.equal(andOpt.size(), 1, 'should have a +AND option')
+	test.equal(
+		andOpt
+			.selectAll('td:nth-child(2) > span')
+			.filter(function(d, i) {
+				return i > 1 || this.innerHTML == ')'
+			})
+			.size(),
+		0,
+		'should have no higher level +AND join option in a one-pill root filter'
+	)
+	andOpt.node().click()
+	test.equal(getHighlightedRowCount(menuRows, 'join-and'), 1, 'should highlight only the +AND row when clicked')
+	test.deepEqual(
+		normalizeActiveData(opts),
+		{ item: opts.filterData.lst[0], filter: opts.filterData },
+		'should set the expected +AND activeData'
+	)
+	test.notEqual(
+		opts.filter.Inner.dom.treeTip.d.style('display'),
+		'none',
+		'should display the tree tip when clicking the +AND option'
+	)
+
+	const orOpt = menuRows.filter(d => d.action == 'join-or')
+	test.equal(orOpt.size(), 1, 'should have a +OR option')
+	test.equal(
+		orOpt
+			.selectAll('td:nth-child(2) > span')
+			.filter(function(d, i) {
+				return i > 1 || this.innerHTML == ')'
+			})
+			.size(),
+		0,
+		'should have no higher level +OR join option in a one-pill root filter'
+	)
+	orOpt.node().click()
+	test.equal(getHighlightedRowCount(menuRows, 'join-or'), 1, 'should highlight only the +OR row when clicked')
+	test.deepEqual(
+		normalizeActiveData(opts),
+		{ item: opts.filterData.lst[0], filter: opts.filterData },
+		'should set the expected +OR activeData'
+	)
+	test.notEqual(
+		opts.filter.Inner.dom.treeTip.d.style('display'),
+		'none',
+		'should display the tree tip when clicking the +OR option'
+	)
+
+	const negateOpt = menuRows.filter(d => d.action == 'negate')
+	test.equal(negateOpt.size(), 1, 'should have a Negate option')
+	test.equal(
+		negateOpt
+			.selectAll('td:nth-child(2) > span')
+			.filter(function() {
+				return this.innerHTML == ')'
+			})
+			.size(),
+		0,
+		'should not have any clickable parenthesis in the negate option of a single pill filter'
+	)
+	const isnotBeforeClick = opts.filter.Inner.filter.lst[0].tvs.isnot
+	negateOpt.node().firstChild.click()
+	await sleep(30)
+	test.equal(
+		!isnotBeforeClick,
+		opts.filter.Inner.filter.lst[0].tvs.isnot,
+		`should reverse a pill's tvs.isnot value after clicking Negate`
+	)
+
+	const removeOpt = menuRows.filter(d => d.action == 'remove')
+	test.equal(removeOpt.size(), 1, 'should have a remove option')
+	test.equal(
+		removeOpt
+			.selectAll('td:nth-child(2) > span')
+			.filter(function() {
+				return this.innerHTML == ')'
+			})
+			.size(),
+		0,
+		'should not have any clickable parenthesis in the remove option of a single pill filter'
+	)
+
+	pill0.click()
+	await sleep(50)
+	await addDemographicSexFilter(opts, andOpt.node())
+
+	pill0.click()
+	test.equal(
+		andOpt
+			.selectAll('td:nth-child(2) > span')
+			.filter(function(d, i) {
+				return i > 1 || this.innerHTML == ')'
+			})
+			.size(),
+		0,
+		'should have no higher level +AND join option in a two-pill root filter with join=and'
+	)
+	test.equal(
+		orOpt
+			.selectAll('td:nth-child(2) > span')
+			.filter(function(d, i) {
+				return i > 0 || this.innerHTML == ')'
+			})
+			.size(),
+		2,
+		'should have a clickable higher-level +OR option in a two-pill root filter with join=and'
+	)
+	test.equal(
+		negateOpt
+			.selectAll('td:nth-child(2) > span')
+			.filter(function() {
+				return this.innerHTML == ')'
+			})
+			.size(),
+		1,
+		'should have 1 clickable parenthesis in the negate option of a two-pill filter'
+	)
+	test.equal(
+		negateOpt
+			.selectAll('td:nth-child(2) > span')
+			.filter(function() {
+				return this.innerHTML == ')'
+			})
+			.size(),
+		1,
+		'should have 1 clickable parenthesis in the remove option of a two-pill filter'
+	)
+
+	negateOpt.node().firstChild.click()
 	test.end()
 })
 
@@ -90,6 +315,17 @@ tape('empty root filter', async test => {
 		'should hide the filter container div'
 	)
 
+	opts.holder
+		.node()
+		.querySelector('.sja_new_filter_btn')
+		.click()
+	await sleep(50)
+	test.notEqual(
+		opts.filter.Inner.dom.treeTip.d.node().style.display,
+		'none',
+		'should display the tree menu when clicking the +NEW button'
+	)
+
 	// simulate creating the initial filter
 	await addDemographicSexFilter(opts, opts.holder.node().querySelector('.sja_new_filter_btn'))
 	test.equal(opts.filterData.lst.length, 1, 'should create a one-entry filter.lst[]')
@@ -108,6 +344,7 @@ tape('empty root filter', async test => {
 		.click()
 	await sleep(50)
 
+	test.notEqual(tipd.node().style.display, 'none', 'should display the control menu when clicking a pill')
 	// remove the only entry from root filter.lst[]
 	tipd
 		.selectAll('tr')
@@ -160,6 +397,7 @@ tape('root filter with a single-entry', async test => {
 
 	await sleep(150)
 	const tipd = opts.filter.Inner.dom.controlsTip.d
+	const menuRows = tipd.selectAll('tr')
 	await opts.filter.main(opts.filterData)
 
 	test.equal(opts.holder.select('.sja_new_filter_btn').style('display'), 'none', 'should hide the +NEW button')
@@ -185,13 +423,7 @@ tape('root filter with a single-entry', async test => {
 		.click()
 
 	// simulate appending another tvs to the root filter.lst[]
-	await addDemographicSexFilter(
-		opts,
-		tipd
-			.selectAll('tr')
-			.filter(d => d.action == 'join-and')
-			.node()
-	)
+	await addDemographicSexFilter(opts, menuRows.filter(d => d.action == 'join-and').node())
 	test.equal(opts.filterData.lst.length, 2, 'should create a two-entry filter.lst[]')
 	test.equal(opts.holder.select('.sja_new_filter_btn').style('display'), 'none', 'should hide the +NEW button')
 	test.notEqual(
@@ -226,8 +458,7 @@ tape('root filter with a single-entry', async test => {
 		.querySelectorAll('.sja_pill_wrapper')[1]
 		.click()
 	await sleep(100)
-	tipd
-		.selectAll('tr')
+	menuRows
 		.filter(d => d.action == 'remove')
 		.node()
 		.click()
