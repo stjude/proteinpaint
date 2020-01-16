3020bccdad12aea1e3ff63cc59e7a0db39e79b49
Edgar Sioson <esioson@stjude.org>
2020-01-16 17:22:29 -0600

    Start updating the filter specs to match the latest control requirements

--- src/common/test/filter.spec.js
+++ src/common/test/filter.spec.js
@@ -66,8 +66,9 @@ function normalizeActiveData(opts) {
 	delete activeData.item.ancestry
 	delete activeData.filter.$id
 	delete activeData.filter.ancestry
-	delete activeData.filter.lst[0].$id
-	delete activeData.filter.lst[0].ancestry
+	for (const item of activeData.filter.lst) {
+		delete item.$id
+	}
 	return { item: activeData.item, filter: activeData.filter }
 }
 
@@ -91,7 +92,7 @@ tape('\n', test => {
 	test.end()
 })
 
-tape('control menu', async test => {
+tape('control menu, 1 pill', async test => {
 	const opts = getOpts({
 		filterData: {
 			type: 'tvslst',
@@ -159,20 +160,86 @@ tape('control menu', async test => {
 		'should display the tree tip when clicking the replace option'
 	)
 
-	const andOpt = menuRows.filter(d => d.action == 'join-and')
-	test.equal(andOpt.size(), 1, 'should have a +AND option')
+	const joinOpt = menuRows.filter(d => d.action == 'join')
+	test.equal(joinOpt.style('display'), 'none', 'should hide a join option for a single-pill root filter')
+
+	const negateOpt = menuRows.filter(d => d.action == 'negate')
+	test.equal(negateOpt.size(), 1, 'should have a Negate option')
+	const isnotBeforeClick = opts.filter.Inner.filter.lst[0].tvs.isnot
+	negateOpt.node().click()
+	await sleep(30)
 	test.equal(
-		andOpt
-			.selectAll('td:nth-child(2) > span')
-			.filter(function(d, i) {
-				return i > 1 || this.innerHTML == ')'
-			})
-			.size(),
-		0,
-		'should have no higher level +AND join option in a one-pill root filter'
+		!isnotBeforeClick,
+		opts.filter.Inner.filter.lst[0].tvs.isnot,
+		`should reverse a pill's tvs.isnot value after clicking Negate`
 	)
-	andOpt.node().click()
-	test.equal(getHighlightedRowCount(menuRows, 'join-and'), 1, 'should highlight only the +AND row when clicked')
+
+	const removeOpt = menuRows.filter(d => d.action == 'remove')
+	test.equal(removeOpt.size(), 1, 'should have a remove option')
+	opts.filter.Inner.dom.holder.node().click()
+	test.end()
+})
+
+tape('control menu, 2 pills', async test => {
+	const opts = getOpts({
+		filterData: {
+			type: 'tvslst',
+			in: true,
+			join: 'or',
+			lst: [
+				{
+					type: 'tvs',
+					tvs: {
+						term: {
+							id: 'diaggrp',
+							name: 'Diagnosis Group',
+							iscategorical: true
+						},
+						values: [
+							{
+								key: `Wilm's tumor`,
+								label: `Wilm's tumor`
+							}
+						]
+					}
+				},
+				{
+					type: 'tvs',
+					tvs: {
+						term: {
+							id: 'abc',
+							name: 'ABC',
+							iscategorical: true
+						},
+						values: [
+							{
+								key: `123`,
+								label: `123`
+							}
+						]
+					}
+				}
+			]
+		}
+	})
+
+	await sleep(150)
+	const tipd = opts.filter.Inner.dom.controlsTip.d
+	const menuRows = tipd.selectAll('tr')
+	await opts.filter.main(opts.filterData)
+
+	const pill0 = opts.holder.select('.sja_pill_wrapper').node()
+	pill0.click()
+
+	const joinOpt = menuRows.filter(d => d.action == 'join')
+	test.notEqual(joinOpt.style('display'), 'none', 'should display a join option when clicking a pill')
+	test.equal(
+		joinOpt.node().querySelector('td:nth-child(2)').innerHTML,
+		opts.filterData.join == 'or' ? 'AND' : 'OR',
+		'should have a correctly-labeled join option'
+	)
+	joinOpt.node().click()
+	test.equal(getHighlightedRowCount(menuRows, 'join'), 1, 'should highlight only the join-option row when clicked')
 	test.deepEqual(
 		normalizeActiveData(opts),
 		{ item: opts.filterData.lst[0], filter: opts.filterData },
@@ -184,116 +251,103 @@ tape('control menu', async test => {
 		'should display the tree tip when clicking the +AND option'
 	)
 
-	const orOpt = menuRows.filter(d => d.action == 'join-or')
-	test.equal(orOpt.size(), 1, 'should have a +OR option')
+	await addDemographicSexFilter(opts, joinOpt.node())
+	const lst = opts.filter.Inner.filter.lst
 	test.equal(
-		orOpt
-			.selectAll('td:nth-child(2) > span')
-			.filter(function(d, i) {
-				return i > 1 || this.innerHTML == ')'
-			})
-			.size(),
-		0,
-		'should have no higher level +OR join option in a one-pill root filter'
-	)
-	orOpt.node().click()
-	test.equal(getHighlightedRowCount(menuRows, 'join-or'), 1, 'should highlight only the +OR row when clicked')
-	test.deepEqual(
-		normalizeActiveData(opts),
-		{ item: opts.filterData.lst[0], filter: opts.filterData },
-		'should set the expected +OR activeData'
-	)
-	test.notEqual(
-		opts.filter.Inner.dom.treeTip.d.style('display'),
-		'none',
-		'should display the tree tip when clicking the +OR option'
-	)
-
-	const negateOpt = menuRows.filter(d => d.action == 'negate')
-	test.equal(negateOpt.size(), 1, 'should have a Negate option')
-	test.equal(
-		negateOpt
-			.selectAll('td:nth-child(2) > span')
-			.filter(function() {
-				return this.innerHTML == ')'
-			})
-			.size(),
-		0,
-		'should not have any clickable parenthesis in the negate option of a single pill filter'
-	)
-	const isnotBeforeClick = opts.filter.Inner.filter.lst[0].tvs.isnot
-	negateOpt.node().firstChild.click()
-	await sleep(30)
-	test.equal(
-		!isnotBeforeClick,
-		opts.filter.Inner.filter.lst[0].tvs.isnot,
-		`should reverse a pill's tvs.isnot value after clicking Negate`
-	)
-
-	const removeOpt = menuRows.filter(d => d.action == 'remove')
-	test.equal(removeOpt.size(), 1, 'should have a remove option')
-	test.equal(
-		removeOpt
-			.selectAll('td:nth-child(2) > span')
-			.filter(function() {
-				return this.innerHTML == ')'
-			})
-			.size(),
-		0,
-		'should not have any clickable parenthesis in the remove option of a single pill filter'
-	)
-
-	pill0.click()
-	await sleep(50)
-	await addDemographicSexFilter(opts, andOpt.node())
-
-	pill0.click()
-	test.equal(
-		andOpt
-			.selectAll('td:nth-child(2) > span')
-			.filter(function(d, i) {
-				return i > 1 || this.innerHTML == ')'
-			})
-			.size(),
-		0,
-		'should have no higher level +AND join option in a two-pill root filter with join=and'
+		lst[0] && lst[0].join,
+		opts.filter.Inner.filter.join == 'and' ? 'or' : 'and',
+		'should create a subnested filter.join != root filter.join'
 	)
 	test.equal(
-		orOpt
-			.selectAll('td:nth-child(2) > span')
-			.filter(function(d, i) {
-				return i > 0 || this.innerHTML == ')'
-			})
-			.size(),
+		lst[0] && lst[0].type == 'tvslst' && lst[0].lst.length,
 		2,
-		'should have a clickable higher-level +OR option in a two-pill root filter with join=and'
-	)
-	test.equal(
-		negateOpt
-			.selectAll('td:nth-child(2) > span')
-			.filter(function() {
-				return this.innerHTML == ')'
-			})
-			.size(),
-		1,
-		'should have 1 clickable parenthesis in the negate option of a two-pill filter'
-	)
-	test.equal(
-		negateOpt
-			.selectAll('td:nth-child(2) > span')
-			.filter(function() {
-				return this.innerHTML == ')'
-			})
-			.size(),
-		1,
-		'should have 1 clickable parenthesis in the remove option of a two-pill filter'
+		'should create a subnested filter entry under the root filter.lst'
 	)
 
-	negateOpt.node().firstChild.click()
 	test.end()
 })
 
-tape('empty root filter', async test => {
+tape('control menu, join label', async test => {
+	const opts = getOpts({
+		filterData: {
+			type: 'tvslst',
+			in: true,
+			join: 'or',
+			lst: [
+				{
+					type: 'tvs',
+					tvs: {
+						term: {
+							id: 'diaggrp',
+							name: 'Diagnosis Group',
+							iscategorical: true
+						},
+						values: [
+							{
+								key: `Wilm's tumor`,
+								label: `Wilm's tumor`
+							}
+						]
+					}
+				},
+				{
+					type: 'tvs',
+					tvs: {
+						term: {
+							id: 'abc',
+							name: 'ABC',
+							iscategorical: true
+						},
+						values: [
+							{
+								key: `123`,
+								label: `123`
+							}
+						]
+					}
+				}
+			]
+		}
+	})
+
+	await sleep(150)
+	const tipd = opts.filter.Inner.dom.controlsTip.d
+	const menuRows = tipd.selectAll('tr')
+	await opts.filter.main(opts.filterData)
+
+	const joinLabel = opts.filter.Inner.dom.holder.select('.sja_filter_join_label')
+	joinLabel.node().click()
+
+	const editOpt = menuRows.filter(d => d.action == 'edit')
+	test.equal(editOpt.style('display'), 'none', 'should hide the Edit option')
+
+	const replaceOpt = menuRows.filter(d => d.action == 'replace')
+	test.equal(replaceOpt.style('display'), 'none', 'should hide the Replace option')
+
+	const joinOpt = menuRows.filter(d => d.action == 'join')
+	test.notEqual(joinOpt.style('display'), 'none', 'should show the join option')
+	test.equal(
+		joinOpt.node().querySelector('td:nth-child(2)').innerHTML,
+		opts.filterData.join.toUpperCase(),
+		'should correctly label the join option'
+	)
+
+	const negateOpt = menuRows.filter(d => d.action == 'negate')
+	test.notEqual(negateOpt.style('display'), 'none', 'should show the Negate option')
+
+	const removeOpt = menuRows.filter(d => d.action == 'remove')
+	test.notEqual(removeOpt.style('display'), 'none', 'should show the Remove option')
+
+	const origLstLength = opts.filterData.lst.length
+	await addDemographicSexFilter(opts, joinOpt.node())
+	const lst = opts.filter.Inner.filter.lst
+	test.equal(lst.filter(f => f.type != 'tvs').length, 0, 'should not create a subnested filter')
+	test.equal(lst.length, origLstLength + 1, 'should append one item to the root filter.lst[]')
+
+	test.end()
+})
+
+tape.skip('empty root filter', async test => {
 	const opts = getOpts({
 		filterData: {
 			type: 'tvslst',
@@ -368,7 +422,7 @@ tape('empty root filter', async test => {
 	test.end()
 })
 
-tape('root filter with a single-entry', async test => {
+tape.skip('root filter with a single-entry', async test => {
 	const opts = getOpts({
 		filterData: {
 			type: 'tvslst',
@@ -493,7 +547,7 @@ tape('root filter with a single-entry', async test => {
 	test.end()
 })
 
-tape('root filter with nested filters', async test => {
+tape.skip('root filter with nested filters', async test => {
 	const opts = getOpts({
 		filterData: {
 			type: 'tvslst',
