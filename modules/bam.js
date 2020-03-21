3a0f5382c67d05e748cb92b7ae3e423a018d905d
xzhou82 <xzhou1@stjude.org>
2020-03-21 18:01:01 -0500

    work in progress

--- modules/bam.js
+++ modules/bam.js
@@ -5,9 +5,12 @@ const utils = require('./utils')
 const createCanvas = require('canvas').createCanvas
 const spawn = require('child_process').spawn
 const readline = require('readline')
+const basecolor = require('../src/common.js').basecolor
 
 /*
  */
+const fcolor = '#d3d3d3'
+const rcolor = '#D7F1D5'
 
 const serverconfig = __non_webpack_require__('./serverconfig.json')
 const samtools = serverconfig.samtools || 'samtools'
@@ -19,7 +22,13 @@ module.exports = genomes => {
 			if (!req.query.genome) throw '.genome missing'
 			const genome = genomes[req.query.genome]
 			if (!genome) throw 'invalid genome'
-			await do_query(genome, req, res)
+			if (!req.query.regions) throw '.regions[] missing'
+			if (!req.query.stackheight) throw '.stackheight missing'
+			req.query.stackheight = Number.parseInt(req.query.stackheight)
+			if (Number.isNaN(req.query.stackheight)) throw '.stackheight not integer'
+			if (req.query.nochr) req.query.nochr = JSON.parse(req.query.nochr)
+			const result = await do_query(genome, req)
+			res.send(result)
 		} catch (e) {
 			res.send({ error: e.message || e })
 			if (e.stack) console.log(e.stack)
@@ -27,7 +36,7 @@ module.exports = genomes => {
 	}
 }
 
-async function do_query(genome, req, res) {
+async function do_query(genome, req) {
 	const [e, _file, isurl] = app.fileurl(req)
 	if (e) throw e
 	// a query object to collect all the bits
@@ -44,18 +53,43 @@ async function do_query(genome, req, res) {
 	if (q.nochr == undefined) {
 		q.nochr = await app.bam_ifnochr(q.file, genome, q.dir)
 	}
-	if (!req.query.regions) throw '.regions[] missing'
+
 	const regions = JSON.parse(req.query.regions)
 	for (const r of regions) {
 		await query_region(r, q)
+		r.scale = p => Math.ceil((r.width * (p - r.start)) / (r.stop - r.start))
+		r.ntwidth = r.width / (r.stop - r.start)
 	}
-	const canvaswidth = regions[regions.length - 1].xoff + regions[regions.length - 1].width
-	const canvasheight = 100
+
+	const qname2template = parse_all_reads(regions)
+	console.log(regions.reduce((i, j) => i + j.lines.length, 0), 'reads', qname2template.size, 'templates')
+
+	// do stacking
+	const templates = [...qname2template.values()].sort((i, j) => i.start - j.start)
+	const stacks = [] // each value is stop coord of each stack
+	for (const template of templates) {
+		// {start, stop, segments}
+		let stackidx = null
+		for (let i = 0; i < stacks.length; i++) {
+			if (stacks[i] < template.start) {
+				stackidx = i
+				stacks[i] = template.stop
+				break
+			}
+		}
+		if (stackidx == null) {
+			stackidx = stacks.length
+			stacks[stackidx] = template.stop
+		}
+		template.y = stackidx * q.query.stackheight
+	}
+
+	const canvaswidth = regions[regions.length - 1].x + regions[regions.length - 1].width
+	const canvasheight = stacks.length * q.query.stackheight
 	const canvas = createCanvas(canvaswidth, canvasheight)
 	const ctx = canvas.getContext('2d')
-
-	for (const r of regions) {
-		render_region(ctx, r, q)
+	for (const template of qname2template.values()) {
+		render_template(ctx, template, q, regions)
 	}
 
 	const result = {
@@ -64,7 +98,7 @@ async function do_query(genome, req, res) {
 		height: canvasheight,
 		nochr: q.nochr
 	}
-	res.send(result)
+	return result
 }
 
 function query_region(r, q) {
@@ -84,370 +118,216 @@ function query_region(r, q) {
 		rl.on('line', line => {
 			r.lines.push(line)
 		})
-		ps.on('close', () => {
-			rl.close()
+		rl.on('close', () => {
 			resolve()
 		})
 	})
 }
 
-function render_region(ctx, r, q) {
-	const ntwidth = this.width / (this.stop - this.start)
-
-	const readsf = [],
-		readsr = []
-	for (const line of r.lines) {
-		var l = line.split('\t')
-		var pos = parseInt(l[3]) - 1
-		var stop = start
-		var boxes = []
-		var flag = l[1],
-			seq = l[9],
-			cigarstr = l[5]
-		var prev = 0
-		var cum = 0
-		for (var i = 0; i < cigarstr.length; i++) {
-			if (cigarstr[i].match(/[0-9]/)) continue
-			var cigar = cigarstr[i]
-			if (cigar == 'H') {
-				// ignore
-				continue
-			}
-			var len = parseInt(cigarstr.substring(prev, i))
-			var s = ''
-			if (cigar == 'N') {
-				// no seq
-			} else if (cigar == 'P' || cigar == 'D') {
-				// padding or del, no sequence in read
-				for (var j = 0; j < len; j++) {
-					s += '*'
-				}
+function parse_all_reads(regions) {
+	// parse reads from all regions
+	// a template with segments possibly from multiple regions
+	const qname2template = new Map()
+	// one item for each template (read pair)
+	// key: qname
+	// value: template, a list of segments
+	for (let i = 0; i < regions.length; i++) {
+		const r = regions[i]
+		for (const line of r.lines) {
+			const segment = parse_one_segment(line, r, [i])
+			if (!segment || !segment.qname) continue
+			const lastbox = segment.boxes[segment.boxes.length - 1]
+			const temp = qname2template.get(segment.qname)
+			if (temp) {
+				// add this segment to existing template
+				temp.segments.push(segment)
+				temp.stop = lastbox.start + lastbox.len
 			} else {
-				s = seq.substr(cum, len)
-				cum += len
+				qname2template.set(segment.qname, {
+					start: segment.boxes[0].start,
+					stop: lastbox.start + lastbox.len,
+					segments: [segment]
+				})
 			}
-			prev = i + 1
-			switch (cigar) {
-				case '=':
-				case 'M':
-					if (Math.max(pos, this.start) < Math.min(pos + len - 1, this.stop)) {
-						// visible
-						boxes.push({
-							opr: 'M',
-							start: pos,
-							len: len,
-							s: s
-						})
-					}
-					pos += len
-					break
-				case 'I':
-					if (pos > this.start && pos < this.stop) {
-						boxes.push({
-							opr: 'I',
-							start: pos,
-							len: len,
-							s: s
-						})
-					}
-					break
-				case 'D':
-					if (Math.max(pos, this.start) < Math.min(pos + len - 1, this.stop)) {
-						boxes.push({
-							opr: 'D',
-							start: pos,
-							len: len,
-							s: s
-						})
-					}
-					pos += len
-					break
-				case 'N':
-					if (Math.max(pos, this.start) < Math.min(pos + len - 1, this.stop)) {
-						boxes.push({
-							opr: 'N',
-							start: pos,
-							len: len,
-							s: s
-						})
-					}
-					pos += len
-					break
-				case 'X':
-				case 'S':
-					if (Math.max(pos, this.start) < Math.min(pos + len - 1, this.stop)) {
-						boxes.push({
-							opr: cigar,
-							start: pos,
-							len: len,
-							s: s
-						})
-					}
-					pos += len
-					break
-				case 'P':
-					if (pos > this.start && pos < this.stop) {
-						boxes.push({
-							opr: 'P',
-							start: pos,
-							len: len,
-							s: s
-						})
-					}
-					break
-				default:
-					console.log('unknown cigar: ' + cigar)
-			}
-		}
-		if (boxes.length == 0) return
-		var read = {
-			name: l[0],
-			forward: !(flag & 0x10),
-			boxes: boxes
-		}
-		if (read.forward) {
-			readsf.push(read)
-		} else {
-			readsr.push(read)
 		}
 	}
-	var reads = readsf.concat(readsr)
-	//reads.sort((i,j)=>{ return i.boxes[0].start-j.boxes[0].start})
-	var canvas = createCanvas(this.width, reads.length * this.stackheight)
-	var ctx = canvas.getContext('2d')
-	var fontsize = this.stackheight
-	ctx.font = fontsize + 'px arial'
-	ctx.textBaseline = 'middle'
-	var scale = p => Math.ceil((this.width * (p - 0.5 - this.start)) / (this.stop - this.start))
-	reads.forEach((read, i) => {
-		var y = i * this.stackheight
-		var xstop = 0
-		read.boxes.forEach(b => {
-			var x = scale(b.start)
-			switch (b.opr) {
-				case 'P':
-				case 'I':
-					// ignore
-					return
-				case 'N':
-					ctx.strokeStyle = read.forward ? this.fcolor : this.rcolor
-					var y2 = Math.floor(y + this.stackheight / 2) + 0.5
-					ctx.beginPath()
-					ctx.moveTo(x, y2)
-					ctx.lineTo(x + b.len * ntwidth, y2)
-					ctx.stroke()
-					xstop = x + b.len * ntwidth
-					return
-				case 'X':
-					ctx.fillStyle = 'white'
-					ctx.fillText(b.s, x, y + fontsize / 2)
-					return
-				default:
-					ctx.fillStyle = read.forward ? this.fcolor : this.rcolor
-					ctx.fillRect(x, y, b.len * ntwidth, this.stackheight)
-					xstop = x + b.len * ntwidth
-			}
-		})
-		ctx.fillStyle = read.forward ? this.fcolor : this.rcolor
-		ctx.fillText(read.name, xstop, y + fontsize / 2)
-	})
+	return qname2template
 }
 
-function load(dir) {
-	this.name = req.query.name
-	this.start = start
-	this.stop = stop
-	this.chr = req.query.chr
-	this.nochr = nochr
-	this.width = width
-	this.barheight = barheight
-	this.stackheight = stackheight
-	this.fcolor = req.query.fcolor
-	this.rcolor = req.query.rcolor
-	this.mmcolor = req.query.mmcolor
-	this.load = () => {
-		var ps = spawn(
-			samtools,
-			['view', file, (this.nochr ? this.chr.replace('chr', '') : this.chr) + ':' + this.start + '-' + this.stop],
-			{ cwd: dir }
-		)
-		var out = [],
-			out2 = []
-		ps.stdout.on('data', data => {
-			// TODO detect amount of data, terminate if too big
-			out.push(data)
-		})
-		ps.stderr.on('data', data => {
-			out2.push(data)
-		})
-		ps.on('close', code => {
-			if (out2.length > 0) {
-				//res.send({error:out2.join('')})
-				//return
+function parse_one_segment(line, r, ridx) {
+	// line
+	// r, a region
+	// ridx: region array index
+	const l = line.split('\t')
+	const boxes = []
+	const qname = l[0],
+		flag = l[2 - 1],
+		cigarstr = l[6 - 1],
+		rnext = l[7 - 1],
+		pnext = l[8 - 1],
+		seq = l[10 - 1]
+	let pos = Number.parseInt(l[4 - 1]) - 1, // change to 0-based
+		stop = r.start,
+		prev = 0,
+		cum = 0
+	for (let i = 0; i < cigarstr.length; i++) {
+		const cigar = cigarstr[i]
+		if (cigar.match(/[0-9]/)) continue
+		if (cigar == 'H') {
+			// ignore
+			continue
+		}
+		const len = Number.parseInt(cigarstr.substring(prev, i))
+		let s = ''
+		if (cigar == 'N') {
+			// no seq
+		} else if (cigar == 'P' || cigar == 'D') {
+			// padding or del, no sequence in read
+			for (let j = 0; j < len; j++) {
+				s += '*'
 			}
-			var lines = out
-				.join('')
-				.trim()
-				.split('\n')
-			var ntwidth = this.width / (this.stop - this.start)
-			var readsf = [],
-				readsr = []
-			lines.forEach(line => {
-				var l = line.split('\t')
-				var pos = parseInt(l[3]) - 1
-				var stop = start
-				var boxes = []
-				var flag = l[1],
-					seq = l[9],
-					cigarstr = l[5]
-				var prev = 0
-				var cum = 0
-				for (var i = 0; i < cigarstr.length; i++) {
-					if (cigarstr[i].match(/[0-9]/)) continue
-					var cigar = cigarstr[i]
-					if (cigar == 'H') {
-						// ignore
-						continue
-					}
-					var len = parseInt(cigarstr.substring(prev, i))
-					var s = ''
-					if (cigar == 'N') {
-						// no seq
-					} else if (cigar == 'P' || cigar == 'D') {
-						// padding or del, no sequence in read
-						for (var j = 0; j < len; j++) {
-							s += '*'
-						}
-					} else {
-						s = seq.substr(cum, len)
-						cum += len
-					}
-					prev = i + 1
-					switch (cigar) {
-						case '=':
-						case 'M':
-							if (Math.max(pos, this.start) < Math.min(pos + len - 1, this.stop)) {
-								// visible
-								boxes.push({
-									opr: 'M',
-									start: pos,
-									len: len,
-									s: s
-								})
-							}
-							pos += len
-							break
-						case 'I':
-							if (pos > this.start && pos < this.stop) {
-								boxes.push({
-									opr: 'I',
-									start: pos,
-									len: len,
-									s: s
-								})
-							}
-							break
-						case 'D':
-							if (Math.max(pos, this.start) < Math.min(pos + len - 1, this.stop)) {
-								boxes.push({
-									opr: 'D',
-									start: pos,
-									len: len,
-									s: s
-								})
-							}
-							pos += len
-							break
-						case 'N':
-							if (Math.max(pos, this.start) < Math.min(pos + len - 1, this.stop)) {
-								boxes.push({
-									opr: 'N',
-									start: pos,
-									len: len,
-									s: s
-								})
-							}
-							pos += len
-							break
-						case 'X':
-						case 'S':
-							if (Math.max(pos, this.start) < Math.min(pos + len - 1, this.stop)) {
-								boxes.push({
-									opr: cigar,
-									start: pos,
-									len: len,
-									s: s
-								})
-							}
-							pos += len
-							break
-						case 'P':
-							if (pos > this.start && pos < this.stop) {
-								boxes.push({
-									opr: 'P',
-									start: pos,
-									len: len,
-									s: s
-								})
-							}
-							break
-						default:
-							console.log('unknown cigar: ' + cigar)
-					}
+		} else {
+			s = seq.substr(cum, len)
+			cum += len
+		}
+		prev = i + 1
+		switch (cigar) {
+			case '=':
+			case 'M':
+				if (Math.max(pos, r.start) < Math.min(pos + len - 1, r.stop)) {
+					// visible
+					boxes.push({
+						opr: 'M',
+						start: pos,
+						len
+						// is match, no need to provide seq
+					})
 				}
-				if (boxes.length == 0) return
-				var read = {
-					name: l[0],
-					forward: !(flag & 0x10),
-					boxes: boxes
+				pos += len
+				break
+			case 'I':
+				if (pos > r.start && pos < r.stop) {
+					boxes.push({
+						opr: 'I',
+						start: pos,
+						len,
+						s
+					})
 				}
-				if (read.forward) readsf.push(read)
-				else readsr.push(read)
-			})
-			var reads = readsf.concat(readsr)
-			//reads.sort((i,j)=>{ return i.boxes[0].start-j.boxes[0].start})
-			var canvas = createCanvas(this.width, reads.length * this.stackheight)
-			var ctx = canvas.getContext('2d')
-			var fontsize = this.stackheight
-			ctx.font = fontsize + 'px arial'
-			ctx.textBaseline = 'middle'
-			var scale = p => Math.ceil((this.width * (p - 0.5 - this.start)) / (this.stop - this.start))
-			reads.forEach((read, i) => {
-				var y = i * this.stackheight
-				var xstop = 0
-				read.boxes.forEach(b => {
-					var x = scale(b.start)
-					switch (b.opr) {
-						case 'P':
-						case 'I':
-							// ignore
-							return
-						case 'N':
-							ctx.strokeStyle = read.forward ? this.fcolor : this.rcolor
-							var y2 = Math.floor(y + this.stackheight / 2) + 0.5
-							ctx.beginPath()
-							ctx.moveTo(x, y2)
-							ctx.lineTo(x + b.len * ntwidth, y2)
-							ctx.stroke()
-							xstop = x + b.len * ntwidth
-							return
-						case 'X':
-							ctx.fillStyle = 'white'
-							ctx.fillText(b.s, x, y + fontsize / 2)
-							return
-						default:
-							ctx.fillStyle = read.forward ? this.fcolor : this.rcolor
-							ctx.fillRect(x, y, b.len * ntwidth, this.stackheight)
-							xstop = x + b.len * ntwidth
-					}
-				})
-				ctx.fillStyle = read.forward ? this.fcolor : this.rcolor
-				ctx.fillText(read.name, xstop, y + fontsize / 2)
-			})
-			res.send({
-				src: canvas.toDataURL(),
-				height: reads.length * this.stackheight
-			})
-		})
+				break
+			case 'D':
+				if (Math.max(pos, r.start) < Math.min(pos + len - 1, r.stop)) {
+					boxes.push({
+						opr: 'D',
+						start: pos,
+						len,
+						s
+					})
+				}
+				pos += len
+				break
+			case 'N':
+				if (Math.max(pos, r.start) < Math.min(pos + len - 1, r.stop)) {
+					boxes.push({
+						opr: 'N',
+						start: pos,
+						len,
+						s
+					})
+				}
+				pos += len
+				break
+			case 'X':
+			case 'S':
+				if (Math.max(pos, r.start) < Math.min(pos + len - 1, r.stop)) {
+					boxes.push({
+						opr: cigar,
+						start: pos,
+						len,
+						s
+					})
+				}
+				pos += len
+				break
+			case 'P':
+				if (pos > r.start && pos < r.stop) {
+					boxes.push({
+						opr: 'P',
+						start: pos,
+						len,
+						s
+					})
+				}
+				break
+			default:
+				console.log('unknown cigar: ' + cigar)
+		}
 	}
+	if (boxes.length == 0) {
+		// no visible boxes, do not show this segment
+		return
+	}
+	const segment = {
+		qname,
+		boxes,
+		forward: !(flag & 0x10),
+		ridx
+	}
+	return segment
+}
+
+function render_template(ctx, template, q, regions) {
+	// segments: a list of segments consisting this template, maybe at different regions
+	const fontsize = q.query.stackheight
+	//ctx.font = fontsize + 'px arial'
+	//ctx.textBaseline = 'middle'
+	for (let i = 0; i < template.segments.length; i++) {
+		const seg = template.segments[i]
+		plot_segment(ctx, seg, template.y, q, regions)
+		if (i > 0) {
+			const prevseg = template.segments[i - 1]
+			//const prevlastbox =
+		}
+	}
+}
+
+function plot_segment(ctx, segment, y, q, regions) {
+	const r = regions[segment.ridx] // this region where the segment falls into
+	// what if segment spans multiple regions
+
+	let xstop = 0
+	segment.boxes.forEach(b => {
+		const x = r.x + r.scale(b.start)
+		if (b.opr == 'P') {
+			return
+		}
+		if (b.opr == 'I') {
+			// insertion, show a round dot
+			console.log('insertion')
+			return
+		}
+		if (b.opr == 'N') {
+			ctx.strokeStyle = fcolor //segment.forward ? fcolor : rcolor
+			const y2 = Math.floor(y + q.query.stackheight / 2) + 0.5
+			ctx.beginPath()
+			ctx.moveTo(x, y2)
+			ctx.lineTo(x + b.len * r.ntwidth, y2)
+			ctx.stroke()
+			xstop = x + b.len * r.ntwidth
+			return
+		}
+		if (b.opr == 'X' || b.opr == 'S') {
+			//ctx.fillStyle = 'white'
+			//ctx.fillText(b.s, x, y + fontsize / 2)
+			for (let j = 0; j < b.s.length; j++) {
+				ctx.fillStyle = basecolor[b.s[j]] || 'black'
+				ctx.fillRect(x + r.ntwidth * j, y, r.ntwidth, q.query.stackheight)
+			}
+			return
+		}
+		// match
+		ctx.fillStyle = fcolor // segment.forward ? fcolor : rcolor
+		ctx.fillRect(x, y, b.len * r.ntwidth, q.query.stackheight)
+		xstop = x + b.len * r.ntwidth
+	})
 }
--- src/block.tk.bam.js
+++ src/block.tk.bam.js
@@ -73,7 +73,8 @@ export async function loadTk(tk, block) {
 }
 
 async function getData(tk, block) {
-	const lst = ['genome=' + block.genome.name, 'regions=' + JSON.stringify(tk.regions)]
+	const lst = ['genome=' + block.genome.name, 'regions=' + JSON.stringify(tk.regions), 'stackheight=' + tk.stackheight]
+	if ('nochr' in tk) lst.push('nochr=' + tk.nochr)
 	if (tk.file) lst.push('file=' + tk.file)
 	if (tk.url) lst.push('url=' + tk.url)
 	if (tk.indexURL) lst.push('indexURL=' + tk.indexURL)
@@ -84,239 +85,25 @@ async function getData(tk, block) {
 }
 
 function renderTk(tk, block) {
-	tk.glider.selectAll('*').remove()
-
-	for (const p of tk.subpanels) {
-		p.glider
-			.attr('transform', 'translate(0,0)') // it may have been panned
-			.selectAll('*')
-			.remove()
-	}
+	tk.img
+		.attr('xlink:href', tk.data.src)
+		.attr('width', tk.data.width)
+		.attr('height', tk.data.height)
+	tk.nochr = tk.data.nochr
 
 	tk.tklabel.each(function() {
 		tk.leftLabelMaxwidth = this.getBBox().width
 	})
-
-	renderTk_covplot(tk, block)
-	renderTk_fpkm(tk, block)
-
-	// gene fpkm
-
 	block.setllabel()
+
+	tk.height_main = tk.height = tk.data.height
 	tk.height_main += tk.toppad + tk.bottompad
-}
-
-function renderTk_covplot(tk, block) {
-	const noploth = 30 // row height for not showing plot
-	const anyregionwithcovplot = tk.regions.find(r => r.coveragesrc)
-
-	if (anyregionwithcovplot) {
-		// at least 1 region has rna/dna cov plot
-		// position labels accordingly
-		client.axisstyle({
-			axis: tk.rna.coverageaxisg.attr('transform', 'scale(1) translate(0,0)').call(
-				axisLeft()
-					.scale(
-						scaleLinear()
-							.domain([0, tk.rna.coveragemax])
-							.range([tk.rna.coveragebarh, 0])
-					)
-					.tickValues([0, tk.rna.coveragemax])
-			),
-			showline: true
-		})
-		tk.tklabel.attr('y', tk.rna.coveragebarh / 2 - 2)
-		tk.rna.coveragelabel.attr('y', tk.rna.coveragebarh / 2 + 2).attr('transform', 'scale(1)')
-
-		client.axisstyle({
-			axis: tk.dna.coverageaxisg
-				.attr('transform', 'scale(1) translate(0,' + (tk.rna.coveragebarh + tk.barypad) + ')')
-				.call(
-					axisLeft()
-						.scale(
-							scaleLinear()
-								.domain([0, tk.dna.coveragemax])
-								.range([0, tk.dna.coveragebarh])
-						)
-						.tickValues([0, tk.dna.coveragemax])
-				),
-			showline: true
-		})
-		tk.dna.coveragelabel
-			.attr('transform', 'scale(1)')
-			.attr('y', tk.rna.coveragebarh + tk.barypad + tk.dna.coveragebarh / 2)
-			.each(function() {
-				tk.leftLabelMaxwidth = Math.max(tk.leftLabelMaxwidth, this.getBBox().width)
-			})
-
-		tk.height_main = tk.rna.coveragebarh + tk.barypad + tk.dna.coveragebarh
-	} else {
-		// no region has cov plot
-		tk.dna.coverageaxisg.attr('transform', 'scale(0)')
-		tk.rna.coverageaxisg.attr('transform', 'scale(0)')
-		tk.dna.coveragelabel.attr('transform', 'scale(0)')
-		tk.rna.coveragelabel.attr('transform', 'scale(0)')
-
-		tk.height_main = noploth
-	}
-
-	for (const r of tk.regions) {
-		if (r.covplotrangelimit) {
-			// no plot
-			tk.glider
-				.append('text')
-				.text('Zoom in under ' + common.bplen(r.covplotrangelimit) + ' to show coverage plot')
-				.attr('font-size', block.laelfontsize)
-				.attr('text-anchor', 'middle')
-				.attr('x', r.x + r.width / 2)
-				.attr('y', noploth / 2)
-			continue
-		}
-		tk.glider
-			.append('image')
-			.attr('x', r.x)
-			.attr('width', r.width)
-			.attr('height', tk.rna.coveragebarh + tk.barypad + tk.dna.coveragebarh)
-			.attr('xlink:href', r.coveragesrc)
-	}
-}
-
-function renderTk_fpkm(tk, block) {
-	/*
-1. if anything to render across all regions, make axis, else, hide axis & label
-2. for each region, if has gene fpkm, plot; else, show out of bound
-*/
-	const noploth = 30 // row height for not showing plot
-	const anyregionwithfpkm = tk.regions.find(r => !r.fpkmrangelimit)
-
-	let maxfpkm = 0
-	for (const r of tk.regions) {
-		if (r.fpkmrangelimit) continue
-		if (r.genes) {
-			for (const g of r.genes) {
-				if (Number.isFinite(g.fpkm)) maxfpkm = Math.max(maxfpkm, g.fpkm)
-				expressionstat.measure(g, tk.gecfg)
-			}
-		}
-	}
-
-	const y = tk.height_main + tk.yspace1
-
-	if (anyregionwithfpkm && maxfpkm > 0) {
-		client.axisstyle({
-			axis: tk.fpkm.axisg.attr('transform', 'scale(1) translate(0,' + y + ')').call(
-				axisLeft()
-					.scale(
-						scaleLinear()
-							.domain([0, maxfpkm])
-							.range([tk.fpkm.barh, 0])
-					)
-					.tickValues([0, maxfpkm])
-			),
-			showline: true
-		})
-		tk.fpkm.label.attr('y', y + tk.fpkm.barh / 2).attr('transform', 'scale(1)')
-
-		tk.height_main += tk.yspace1 + tk.fpkm.barh
-	} else {
-		// no region has fpkm
-		tk.fpkm.axisg.attr('transform', 'scale(0)')
-		tk.fpkm.label.attr('transform', 'scale(0)')
-		tk.height_main += noploth
-	}
-
-	for (const r of tk.regions) {
-		if (r.fpkmrangelimit) {
-			// no plot
-			tk.glider
-				.append('text')
-				.text('Zoom in under ' + common.bplen(r.fpkmrangelimit) + ' to show gene ' + tk.gecfg.datatype + ' values')
-				.attr('font-size', block.laelfontsize)
-				.attr('text-anchor', 'middle')
-				.attr('x', r.x + r.width / 2)
-				.attr('y', y + noploth / 2)
-			continue
-		}
-		if (!r.genes) continue
-
-		if (maxfpkm == 0) {
-			// within range but still no data
-			continue
-		}
-
-		const rsf = r.width / (r.stop - r.start)
-
-		for (const gene of r.genes) {
-			if (!Number.isFinite(gene.fpkm)) continue
-
-			// plot this gene
-
-			const color = expressionstat.ase_color(gene, tk.gecfg)
-			const boxh = (tk.fpkm.barh * gene.fpkm) / maxfpkm
-
-			let x1, x2
-			if (r.reverse) {
-				x1 = r.x + rsf * (r.stop - Math.min(r.stop, gene.stop))
-				x2 = r.x + rsf * (r.stop - Math.max(r.start, gene.start))
-			} else {
-				x1 = r.x + rsf * (Math.max(r.start, gene.start) - r.start)
-				x2 = r.x + rsf * (Math.min(r.stop, gene.stop) - r.start)
-			}
-
-			const line = tk.glider
-				.append('line')
-				.attr('x1', x1)
-				.attr('x2', x2)
-				.attr('y1', y + tk.fpkm.barh - boxh)
-				.attr('y2', y + tk.fpkm.barh - boxh)
-				.attr('stroke', color)
-				.attr('stroke-width', 2)
-				.attr('stroke-opacity', 0.4)
-			const box = tk.glider
-				.append('rect')
-				.attr('x', x1)
-				.attr('y', y + tk.fpkm.barh - boxh)
-				.attr('width', x2 - x1)
-				.attr('height', boxh)
-				.attr('fill', color)
-				.attr('fill-opacity', 0.2)
-			tk.glider
-				.append('rect')
-				.attr('x', x1)
-				.attr('y', y + tk.fpkm.barh - boxh - 2)
-				.attr('width', x2 - x1)
-				.attr('height', boxh + 2)
-				.attr('fill', 'white')
-				.attr('fill-opacity', 0)
-				.on('mouseover', () => {
-					line.attr('stroke-opacity', 0.5)
-					box.attr('fill-opacity', 0.3)
-					tooltip_genefpkm(gene, tk)
-				})
-				.on('mouseout', () => {
-					line.attr('stroke-opacity', 0.4)
-					box.attr('fill-opacity', 0.2)
-					tk.tktip.hide()
-				})
-		}
-	}
-}
-
-function tooltip_genefpkm(gene, tk) {
-	tk.tktip.clear().show(d3event.clientX, d3event.clientY)
-	const lst = [
-		{
-			k: gene.gene + ' ' + tk.gecfg.datatype,
-			v: gene.fpkm
-		}
-	]
-	const table = client.make_table_2col(tk.tktip.d, lst)
-	expressionstat.showsingleitem_table(gene, tk.gecfg, table)
 }
 
 function makeTk(tk, block) {
 	delete tk.uninitialized
 	tk.img = tk.glider.append('image')
+	if (!tk.stackheight) tk.stackheight = 13 // make it dependent on range size
 
 	tk.tklabel.text(tk.name).attr('dominant-baseline', 'auto')
 
