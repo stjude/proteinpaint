d688e34b2849400fba00b8e6b369545b0fb1d11f
jaimin patel <jaimin.patel@stjude.org>
2020-03-23 17:11:26 -0500

    termsetting: 'custom bins' (in progress)

--- src/common/test/termsetting.spec.js
+++ src/common/test/termsetting.spec.js
@@ -226,7 +226,7 @@ tape('Caterogical term', async test => {
 	test.end()
 })
 
-tape.only('Numerical term', async test => {
+tape('Numerical term', async test => {
 	const opts = getOpts({
 		tsData: {
 			term: {
