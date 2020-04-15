ffc6944671fc65cb02fc1bb20e84c03cdb469f97
xzhou82 <xzhou1@stjude.org>
2020-04-15 09:36:17 -0500

    new script to parse out term order from phenotree

---
+++ utils/sjlife2/phenotree.makeentiretree.sh
@@ -0,0 +1,6 @@
+cp phenotree/matrix.tree phenotree/entire.tree
+tail -n +2 phenotree/ccssctcae.tree >> phenotree/entire.tree
+echo >> phenotree/entire.tree
+tail -n +2 phenotree/sjlifectcae.tree >> phenotree/entire.tree
+echo >> phenotree/entire.tree
+tail -n +2 phenotree/sn.tree >> phenotree/entire.tree
