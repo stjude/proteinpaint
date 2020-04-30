05545f9ccb41c995feed27eebafd6ddb7700d2bc
Edgar Sioson <esioson@stjude.org>
2020-04-30 13:35:52 -0500

    Allow authorization in cross-origin requests for jwt support

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
 	next()
 })
 
