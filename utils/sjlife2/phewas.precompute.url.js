3b3bd24fd9487342fc9f72c788bc8750f63f10a3
xzhou82 <xzhou1@stjude.org>
2020-07-13 10:19:22 -0500

    replace fetch with got ;

--- utils/sjlife2/phewas.precompute.url.js
+++ utils/sjlife2/phewas.precompute.url.js
@@ -1,14 +1,13 @@
-const fetch = require('node-fetch')
+const got = require('got')
 const fs = require('fs')
 const path = require('path')
-//const serverconfig = require('~/proteinpaint/serverconfig.json')
 const serverconfig = require('../../serverconfig.json')
 
 main()
 
 async function main() {
-	const res = await fetch('http://localhost:3001/termdb?genome=hg38&dslabel=SJLife&precompute=1&phewas=1')
-	const data = await res.json()
+	const res = await got('http://localhost:3001/termdb?genome=hg38&dslabel=SJLife&precompute=1&phewas=1')
+	const data = JSON.parse(res.body)
 	if (data.error) throw data.error
 	if (!data.filename) throw 'filename missing'
 	fs.renameSync(path.join(serverconfig.cachedir, data.filename), 'category2vcfsample')
