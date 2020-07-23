e512e26a18dcad0025c38c0e34cc0d293c262d56
Edgar Sioson <esioson@stjude.org>
2020-07-23 15:19:36 -0500

    Set an Expires header field in the server response to trigger browser caching

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+	res.setHeader('Expires', new Date(Date.now() + 30000).toUTCString())
 	next()
 })
 
