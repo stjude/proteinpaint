ae28db47909246fa39de9570e30a6838f1480ef4
Paul <robinpaul85@gmail.com>
2020-07-31 19:52:13 -0500

    Looking for frequency of kmers, not just unique kmers

--- modules/bam.kmer.indel.js
+++ modules/bam.kmer.indel.js
@@ -43,18 +43,25 @@ export async function match_complexvariant(templates, q) {
 	const ref_kmers = build_kmers(refseq, kmer_length)
 	const alt_kmers = build_kmers(altseq, kmer_length)
 
+	const ref_kmers_nodups = new Set(ref_kmers)
+	const alt_kmers_nodups = new Set(alt_kmers)
 	//console.log(ref_kmers)
 	//console.log(alt_kmers)
 
 	const ref_comparisons = []
 	const alt_comparisons = []
+
+	const ref_comparisons2 = []
+	const alt_comparisons2 = []
 	const refaltstatus = []
 	for (const template of templates) {
 		const read_seq = template.segments[0].seq
 		// let cigar_seq = template.segments[0].cigarstr
 		const read_kmers = build_kmers(read_seq, kmer_length)
-		const ref_comparison = jaccard_similarity(read_kmers, ref_kmers)
-		const alt_comparison = jaccard_similarity(read_kmers, alt_kmers)
+		const ref_comparison = jaccard_similarity(read_kmers, ref_kmers, ref_kmers_nodups)
+		const alt_comparison = jaccard_similarity(read_kmers, alt_kmers, alt_kmers_nodups)
+		alt_comparisons2.push(alt_comparison)
+		ref_comparisons2.push(ref_comparison)
 		// console.log("Iteration:",k,read_seq,cigar_seq,ref_comparison,alt_comparison,read_seq.length,refseq.length,altseq.length,read_kmers.length,ref_kmers.length,alt_kmers.length)
 		const diff_score = alt_comparison - ref_comparison
 		if (diff_score < 0) {
@@ -67,6 +74,7 @@ export async function match_complexvariant(templates, q) {
 			}
 		}
 	}
+
 	const ref_cutoff = jStat.percentile(ref_comparisons, percentile_cutoff)
 	const alt_cutoff = jStat.percentile(alt_comparisons, 1 - percentile_cutoff)
 	// console.log(alt_comparisons)
@@ -81,9 +89,13 @@ export async function match_complexvariant(templates, q) {
 		if (refalt == 'ref') {
 			if (ref_comparisons[j] <= ref_cutoff) {
 				// Label read as reference allele
+				templates[i].__tempscore =
+					alt_comparisons2[i].toFixed(4).toString() + ' ' + ref_comparisons2[i].toFixed(4).toString() //+" "+ref_comparisons[j].toFixed(4).toString()
 				type2group[bamcommon.type_supportref].templates.push(templates[i])
 			} else {
 				// Label read as none
+				templates[i].__tempscore =
+					alt_comparisons2[i].toFixed(4).toString() + ' ' + ref_comparisons2[i].toFixed(4).toString() //+" "+ref_comparisons[j].toFixed(4).toString()
 				type2group[bamcommon.type_supportno].templates.push(templates[i])
 			}
 			j++
@@ -92,9 +104,13 @@ export async function match_complexvariant(templates, q) {
 		if (refalt == 'alt') {
 			if (alt_comparisons[k] >= alt_cutoff) {
 				// Label read as alternate allele
+				templates[i].__tempscore =
+					alt_comparisons2[i].toFixed(4).toString() + ' ' + ref_comparisons2[i].toFixed(4).toString() //+" "+alt_comparisons[k].toFixed(4).toString()
 				type2group[bamcommon.type_supportalt].templates.push(templates[i])
 			} else {
 				// Label read as none
+				templates[i].__tempscore =
+					alt_comparisons2[i].toFixed(4).toString() + ' ' + ref_comparisons2[i].toFixed(4).toString() //+" "+alt_comparisons[k].toFixed(4).toString()
 				type2group[bamcommon.type_supportno].templates.push(templates[i])
 			}
 			k++
@@ -133,12 +149,25 @@ function build_kmers(sequence, kmer_length) {
 		// console.log(subseq)
 		kmers.push(subseq)
 	}
-	const kmers_nodups = new Set(kmers)
-	return kmers_nodups
+	// const kmers_nodups = new Set(kmers)
+	return kmers
 }
 
-function jaccard_similarity(kmers1_nodups, kmers2_nodups) {
+function jaccard_similarity(kmers1, kmers2, kmers2_nodups) {
+	const kmers1_nodups = new Set(kmers1)
 	const intersection = new Set([...kmers1_nodups].filter(i => kmers2_nodups.has(i)))
 	const all_kmers = new Set([...kmers1_nodups, ...kmers2_nodups])
-	return intersection.size / all_kmers.size
+	let intersection_length = 0
+	const kmer1_counts = new Map([...kmers1_nodups].map(x => [x, kmers1.filter(y => y === x).length]))
+	const kmer2_counts = new Map([...kmers2_nodups].map(x => [x, kmers2.filter(y => y === x).length]))
+	for (const kmer of intersection) {
+		const kmer1_freq = kmer1_counts.get(kmer)
+		const kmer2_freq = kmer2_counts.get(kmer)
+		if (kmer1_freq >= kmer2_freq) {
+			intersection_length += kmer2_freq
+		} else if (kmer1_freq < kmer2_freq) {
+			intersection_length += kmer1_freq
+		}
+	}
+	return intersection_length / (kmers1.length + kmers2.length - intersection_length)
 }
