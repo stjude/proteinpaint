72822df6d4408d415110f8609633887630ac8e99
Paul <robinpaul85@gmail.com>
2020-08-05 13:39:30 -0500

    Adding ability to write a file containing diff_scores

--- modules/bam.kmer.indel.js
+++ modules/bam.kmer.indel.js
@@ -1,6 +1,7 @@
-const jStat = require('jStat').jStat
+// const jStat = require('jStat').jStat
 const utils = require('./utils')
 const bamcommon = require('./bam.common')
+const fs = require('fs')
 
 export async function match_complexvariant(templates, q) {
 	// TODO
@@ -39,12 +40,6 @@ export async function match_complexvariant(templates, q) {
 
 	// console.log(refallele,altallele,refseq,altseq)
 	const kmer_length = 10 // length of kmer
-	// const percentile_cutoff = 0.75 // Difference in jaccard similarity betwen reference and alternate allele
-	let absolute_cutoff = 0.01 * Math.abs(refallele.length - altallele.length)
-	if (absolute_cutoff > 0.05) {
-		absolute_cutoff = 0.05
-	}
-	console.log('Alternate cutoff:', absolute_cutoff)
 
 	const ref_kmers = build_kmers(refseq, kmer_length)
 	const alt_kmers = build_kmers(altseq, kmer_length)
@@ -54,7 +49,7 @@ export async function match_complexvariant(templates, q) {
 	//console.log(ref_kmers)
 	//console.log(alt_kmers)
 
-	const type2group = bamcommon.make_type2group(q)
+	let kmer_diff_scores = []
 	for (const template of templates) {
 		const read_seq = template.segments[0].seq
 		// let cigar_seq = template.segments[0].cigarstr
@@ -63,29 +58,40 @@ export async function match_complexvariant(templates, q) {
 		const alt_comparison = jaccard_similarity(read_kmers, alt_kmers, alt_kmers_nodups)
 		// console.log("Iteration:",k,read_seq,cigar_seq,ref_comparison,alt_comparison,read_seq.length,refseq.length,altseq.length,read_kmers.length,ref_kmers.length,alt_kmers.length)
 		const diff_score = alt_comparison - ref_comparison
-		if (diff_score < 0) {
-			if (Math.abs(diff_score) >= absolute_cutoff) {
-				// Label as reference
-				template.__tempscore = alt_comparison.toFixed(4).toString() + ' ' + ref_comparison.toFixed(4).toString()
-				type2group[bamcommon.type_supportref].templates.push(template)
-			} else {
-				// Label as none
-				template.__tempscore = alt_comparison.toFixed(4).toString() + ' ' + ref_comparison.toFixed(4).toString()
-				type2group[bamcommon.type_supportno].templates.push(template)
-			}
-		} else {
-			if (diff_score >= 0) {
-				if (Math.abs(diff_score) >= absolute_cutoff) {
-					// Label as alternate
-					template.__tempscore = alt_comparison.toFixed(4).toString() + ' ' + ref_comparison.toFixed(4).toString()
-					type2group[bamcommon.type_supportalt].templates.push(template)
-				} else {
-					// Label as none
-					template.__tempscore = alt_comparison.toFixed(4).toString() + ' ' + ref_comparison.toFixed(4).toString()
-					type2group[bamcommon.type_supportno].templates.push(template)
-				}
-			}
+		kmer_diff_scores.push(diff_score)
+	}
+	//console.log("Original array:",kmer_diff_scores)
+	let kmer_diff_scores_asc = [...kmer_diff_scores]
+
+	kmer_diff_scores_asc.sort((a, b) => a - b) // Sort array in ascending order
+
+	var file = fs.createWriteStream(
+		q.variant.chr + '.' + q.variant.pos + '.' + q.variant.ref + '.' + q.variant.alt + '.txt'
+	)
+	file.on('error', function(err) {
+		/* error handling */
+	})
+	kmer_diff_scores_asc.forEach(function(v) {
+		file.write(v + '\n')
+	})
+	file.end()
+
+	let j = 0
+	const type2group = bamcommon.make_type2group(q)
+	for (const diff_score of kmer_diff_scores) {
+		if (diff_score >= 0) {
+			templates[j].__tempscore = diff_score.toFixed(4).toString()
+			type2group[bamcommon.type_supportalt].templates.push(templates[j])
+		} else if (diff_score <= 0) {
+			templates[j].__tempscore = diff_score.toFixed(4).toString()
+			type2group[bamcommon.type_supportref].templates.push(templates[j])
 		}
+
+		//            else if (diff_score > ref_cutoff && diff_score < alt_cutoff){
+		//                templates[j].__tempscore = diff_score.toFixed(4).toString()
+		//                type2group[bamcommon.type_supportno].templates.push(templates[j])
+		//	    }
+		j++
 	}
 
 	const groups = []
