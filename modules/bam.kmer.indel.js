af6eab40468b6145d75d69345c74f5156d0cb2b3
Paul <robinpaul85@gmail.com>
2020-08-10 17:33:20 -0500

    Determining cutoffs on the basis of local maxima

--- modules/bam.kmer.indel.js
+++ modules/bam.kmer.indel.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 export async function match_complexvariant(templates, q) {
 	// TODO
 	// get flanking sequence, suppose that segments are of same length, use segment length
-	const segbplen = Math.ceil(1.5 * templates[0].segments[0].seq.length)
+	const segbplen = templates[0].segments[0].seq.length
 	// need to verify if the retrieved sequence is showing 1bp offset or not
 	const leftflankseq = (await utils.get_fasta(
 		q.genome,
@@ -51,6 +51,9 @@ export async function match_complexvariant(templates, q) {
 	//console.log(alt_kmers)
 
 	const kmer_diff_scores = []
+	const ref_scores = []
+	const alt_scores = []
+	let i = 0
 	for (const template of templates) {
 		const read_seq = template.segments[0].seq
 		// let cigar_seq = template.segments[0].cigarstr
@@ -60,8 +63,80 @@ export async function match_complexvariant(templates, q) {
 		// console.log("Iteration:",k,read_seq,cigar_seq,ref_comparison,alt_comparison,read_seq.length,refseq.length,altseq.length,read_kmers.length,ref_kmers.length,alt_kmers.length)
 		const diff_score = alt_comparison - ref_comparison
 		kmer_diff_scores.push(diff_score)
+		const item = {
+			value: Math.abs(diff_score),
+			groupID: i
+		}
+		if (diff_score > 0) {
+			alt_scores.push(item)
+		} else if (diff_score <= 0) {
+			ref_scores.push(item)
+		}
+
+		i++
 	}
-	//console.log("Original array:",kmer_diff_scores)
+
+	const ref_indices = determine_maxima(ref_scores)
+	const alt_indices = determine_maxima(alt_scores)
+
+	let index = 0
+	const type2group = bamcommon.make_type2group(q)
+	let kmer_diff_scores_input = []
+	for (const item of ref_indices) {
+		if (item[1] == 'refalt') {
+			if (type2group[bamcommon.type_supportref]) {
+				index = item[0]
+				templates[index].__tempscore = kmer_diff_scores[index].toFixed(4).toString()
+				type2group[bamcommon.type_supportref].templates.push(templates[index])
+				const input_items = {
+					value: kmer_diff_scores[index],
+					groupID: 'ref'
+				}
+				kmer_diff_scores_input.push(input_items)
+			}
+		} else if (item[1] == 'none') {
+			if (type2group[bamcommon.type_supportno]) {
+				index = item[0]
+				templates[index].__tempscore = kmer_diff_scores[index].toFixed(4).toString()
+				type2group[bamcommon.type_supportno].templates.push(templates[index])
+				const input_items = {
+					value: kmer_diff_scores[index],
+					groupID: 'none'
+				}
+				kmer_diff_scores_input.push(input_items)
+			}
+		}
+	}
+
+	for (const item of alt_indices) {
+		if (item[1] == 'refalt') {
+			if (type2group[bamcommon.type_supportalt]) {
+				index = item[0]
+				templates[index].__tempscore = kmer_diff_scores[index].toFixed(4).toString()
+				type2group[bamcommon.type_supportalt].templates.push(templates[index])
+				const input_items = {
+					value: kmer_diff_scores[index],
+					groupID: 'alt'
+				}
+				kmer_diff_scores_input.push(input_items)
+			}
+		} else if (item[1] == 'none') {
+			if (type2group[bamcommon.type_supportno]) {
+				index = item[0]
+				templates[index].__tempscore = kmer_diff_scores[index].toFixed(4).toString()
+				type2group[bamcommon.type_supportno].templates.push(templates[index])
+				const input_items = {
+					value: kmer_diff_scores[index],
+					groupID: 'none'
+				}
+				kmer_diff_scores_input.push(input_items)
+			}
+		}
+	}
+	kmer_diff_scores_input.sort((a, b) => a.value - b.value)
+	console.log('Final array for plotting:', kmer_diff_scores_input)
+	// Please use this array for plotting the scatter plot .values contain the numeric value, .groupID contains ref/alt/none status. You can use red for alt, green for ref and blue for none.
+
 	q.kmer_diff_scores_asc = kmer_diff_scores.slice().sort((a, b) => a - b) // Sort array in ascending order
 	if (features.bamScoreRplot) {
 		const file = fs.createWriteStream(
@@ -70,34 +145,10 @@ export async function match_complexvariant(templates, q) {
 		file.on('error', function(err) {
 			/* error handling */
 		})
-		q.kmer_diff_scores_asc.forEach(function(v) {
-			file.write(v + '\n')
+		kmer_diff_scores_input.forEach(function(v) {
+			file.write(v.value + ',' + v.groupID + '\n')
 		})
 		file.end()
-	}
-
-	let j = 0
-	const type2group = bamcommon.make_type2group(q)
-	for (const diff_score of kmer_diff_scores) {
-		if (diff_score >= 0) {
-			if (type2group[bamcommon.type_supportalt]) {
-				templates[j].__tempscore = diff_score.toFixed(4).toString()
-				type2group[bamcommon.type_supportalt].templates.push(templates[j])
-			}
-		} else if (diff_score <= 0) {
-			if (type2group[bamcommon.type_supportref]) {
-				templates[j].__tempscore = diff_score.toFixed(4).toString()
-				type2group[bamcommon.type_supportref].templates.push(templates[j])
-			}
-		}
-
-		//      else if (diff_score > ref_cutoff && diff_score < alt_cutoff){
-		//              if (type2group[bamcommon.type_supportno]) {
-		//                templates[j].__tempscore = diff_score.toFixed(4).toString()
-		//                type2group[bamcommon.type_supportno].templates.push(templates[j])
-		//	    }
-		//	      }
-		j++
 	}
 
 	const groups = []
@@ -152,4 +203,41 @@ function jaccard_similarity(kmers1, kmers2, kmers2_nodups) {
 		}
 	}
 	return intersection_length / (kmers1.length + kmers2.length - intersection_length)
+}
+
+function determine_maxima(kmer_diff_scores) {
+	kmer_diff_scores.sort((a, b) => a.value - b.value)
+	// console.log(kmer_diff_scores)
+
+	let kmer_diff_scores_input = []
+	for (let i = 0; i < kmer_diff_scores.length; i++) {
+		kmer_diff_scores_input.push([i, kmer_diff_scores[i].value])
+	}
+	const min_value = [0, kmer_diff_scores[0].value]
+	const max_value = [kmer_diff_scores.length - 1, kmer_diff_scores[kmer_diff_scores.length - 1].value]
+	const slope_of_line = (max_value[1] - min_value[1]) / (max_value[0] - min_value[0])
+	console.log(slope_of_line)
+	const intercept_of_line = min_value[1] * slope_of_line
+
+	let distances_from_line = []
+	for (let i = 0; i < kmer_diff_scores.length; i++) {
+		distances_from_line.push(
+			Math.abs(slope_of_line * kmer_diff_scores_input[i][0] - kmer_diff_scores_input[i][1] + intercept_of_line) /
+				Math.sqrt(1 + slope_of_line * slope_of_line)
+		) // distance = abs(a*x+b*y+c)/sqrt(a^2+b^2)
+	}
+	const array_maximum = Math.max(...distances_from_line)
+	// console.log("Array maximum:",array_maximum)
+	const index_array_maximum = distances_from_line.indexOf(array_maximum)
+	// console.log("Max index:",index_array_maximum,"Total length:",kmer_diff_scores.length)
+	let indices = []
+	for (let i = 0; i < kmer_diff_scores.length; i++) {
+		if (i < index_array_maximum) {
+			indices.push([kmer_diff_scores[i].groupID, 'none'])
+		} else if (i >= index_array_maximum) {
+			indices.push([kmer_diff_scores[i].groupID, 'refalt'])
+		}
+	}
+	//console.log("indices:",indices)
+	return indices
 }
