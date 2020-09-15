133fa998e70b6dc0b093ec4f2fb7e2a70eda59f4
Edgar Sioson <esioson@stjude.org>
2020-09-14 21:55:37 -0500

    Link to pp's public dir when it is installed as a module in another project

--- bin.js
+++ bin.js
@@ -7,11 +7,23 @@
 	$ npx proteinpaint
 */
 
-
 // to-do, before starting the server:
 // - merge updates to genomes and/or dataset as needed
 // - option to redownload datasets
 
 // option to use forever-monitor (to-do)
 // - OR -
+
+const fs = require('fs')
+const pkg = require('./package.json')
+
+// use the pp packages' public dir for index.html and /bin bundles
+if (!fs.existsSync('public')) {
+	if (pkg._where) {
+		fs.symlink(pkg._where + '/node_modules/@stjude/proteinpaint/public', 'public', () => {})
+	} else {
+		fs.symlink(__dirname + '/public', 'public', () => {})
+	}
+}
+
 require('./server.js')
