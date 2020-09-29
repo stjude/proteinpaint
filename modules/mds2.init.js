f517b9b8fac11f5366b83c05261e38e753e9b440
xzhou82 <xzhou1@stjude.org>
2020-09-29 14:47:42 -0500

    all errors will be thrown inside pp_init()

--- modules/mds2.init.js
+++ modules/mds2.init.js
@@ -40,16 +40,12 @@ export async function init(ds, genome) {
 	const tk = ds.track
 	if (!tk.name) tk.name = ds.label
 
-	try {
-		may_validate_info_fields(tk)
-		may_validate_population(tk)
-		await may_init_vcf(tk.vcf, genome, ds)
-		await may_init_ld(tk.ld, genome, ds)
-		await may_init_svcnv(tk.svcnv, genome, ds)
-		may_sum_samples(tk)
-	} catch (e) {
-		return e
-	}
+	may_validate_info_fields(tk)
+	may_validate_population(tk)
+	await may_init_vcf(tk.vcf, genome, ds)
+	await may_init_ld(tk.ld, genome, ds)
+	await may_init_svcnv(tk.svcnv, genome, ds)
+	may_sum_samples(tk)
 	if (tk.samples) console.log(ds.label + ': mds2: ' + tk.samples.length + ' samples')
 }
 
