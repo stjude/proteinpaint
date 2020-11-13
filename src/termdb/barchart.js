b59fa93ad97e8d5bb0c70b400bbde607985257fa
Edgar Sioson <siosonel@gmail.com>
2020-11-13 10:35:36 -0600

    Put back the barchart.state.genome, deslabel as it is needed for click handling

--- src/termdb/barchart.js
+++ src/termdb/barchart.js
@@ -61,6 +61,8 @@ class TdbBarchart {
 		const config = appState.tree.plots[this.id]
 		return {
 			isVisible: config.settings.currViews.includes('barchart'),
+			genome: appState.vocab.genome,
+			dslabel: appState.vocab.dslabel,
 			nav: appState.nav,
 			termfilter: appState.termfilter,
 			config: {
