46cd21a11baea1193811f5d08a34b852744c0360
Edgar Sioson <siosonel@gmail.com>
2020-12-07 09:09:07 -0600

    start prototyping a react wrapper

---
+++ wrappers/react/index.js
@@ -0,0 +1,8 @@
+// import * as React from 'react'
+import { runproteinpaint } from '../../src/app'
+import * as common from '../../src/common'
+
+console.log('test ....')
+console.log(common.defaultcolor)
+console.log(common.d3color)
+console.log(common.Partjson)
