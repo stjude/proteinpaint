6b82c045b45fd4ddb7b7fd4d23f4c5ea8aa9dec3
jaimin patel <jaimin.patel@stjude.org>
2020-12-10 12:32:45 -0600

    update pp viz only after checking with shouldComponentUpdate()

--- wrappers/react/proteinpaint.js
+++ wrappers/react/proteinpaint.js
@@ -4,15 +4,22 @@ import { runproteinpaint } from '../../src/app'
 export default class ProteinPaint extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = { data: JSON.parse(localStorage.getItem(this.props.dataKey)) }
 		this.ppHolderRef = React.createRef()
 	}
 	componentDidMount() {
 		const pp_holder = this.ppHolderRef.current
 		this.runpp(pp_holder)
 	}
+	static getDerivedStateFromProps(props) {
+		return { data: JSON.parse(localStorage.getItem(props.dataKey)) }
+	}
+	shouldComponentUpdate(nextProps, nextState) {
+		return (
+			this.state.data.gene != nextState.data.gene || this.state.data.tracks[0].set_id != nextState.data.tracks[0].set_id
+		)
+	}
 	componentDidUpdate() {
-		if (this.state.set_id_editing) return
 		const pp_holder = this.ppHolderRef.current
 		pp_holder.querySelector('.sja_root_holder').remove()
 		this.runpp(pp_holder)
@@ -25,8 +32,8 @@ export default class ProteinPaint extends React.Component {
 		)
 	}
 	runpp(holder) {
-		const data = JSON.parse(localStorage.getItem(this.props.dataKey))
-		console.log(26, data.gene)
-		runproteinpaint(Object.assign({ holder, noheader: true, nobox: true }, data))
+		const ppdata = JSON.parse(JSON.stringify(this.state.data))
+		// console.log(29, ppdata)
+		runproteinpaint(Object.assign({ holder, noheader: true, nobox: true }, ppdata))
 	}
 }
