b011e4059f133c7a89a9fccd45770de74dd77b27
xzhou82 <xzhou1@stjude.org>
2020-12-17 16:34:50 -0600

    new script to assign default binning schemes to terms e.g. "Age at first occurrence ..."

---
+++ utils/sjlife2/binconfig.ageterms.js
@@ -0,0 +1,31 @@
+// quick treatment for terms like "Age at first occurrence, root canal", assuming years
+// does not apply to months
+
+if (process.argv.length != 4) {
+	console.log('<termdb, previous version> <keep/manual.termconfig>; output to stdout')
+	process.exit()
+}
+
+const fs = require('fs')
+const file_termdb = process.argv[2]
+const file_termconfig = process.argv[3]
+const idset = new Set() // term id with config, will not overwrite with auto configs
+for (const line of fs
+	.readFileSync(file_termconfig, { encoding: 'utf8' })
+	.trim()
+	.split('\n')) {
+	console.log(line)
+	idset.add(line.split('\t')[0])
+}
+for (const line of fs
+	.readFileSync(process.argv[2], { encoding: 'utf8' })
+	.trim()
+	.split('\n')) {
+	const l = line.split('\t')
+	const id = l[0]
+	if (idset.has(id)) continue
+	const name = l[1]
+	if (name.startsWith('Age at first ') || name.endsWith(' age')) {
+		console.log(id + '\tbins\tlabel_offset=1;bin_size=10;first_bin.stop=10;last_bin.start=50')
+	}
+}
