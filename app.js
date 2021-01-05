118180b0e5051422f22dba8932f246d04816773e
Edgar Sioson <siosonel@gmail.com>
2021-01-05 15:20:22 -0600

    Reuse the react App.js code in both the test spec and example html page

--- app.js
+++ app.js
@@ -230,6 +230,32 @@ if (serverconfig.debugmode) {
 		res.header('Content-Type', 'text/html')
 		res.send(await fs.readFileSync(file))
 	})
+	app.get(basepath + '/wrappers/test/App.js', async (req, res) => {
+		const file = path.join(process.cwd(), `./src/wrappers/test/App.js`)
+		res.header('Content-Type', 'application/javascript')
+		const content = await fs.readFileSync(file, { encoding: 'utf8' })
+		const lines = content.split('\n')
+		let str = ''
+		// remove import lines
+		for (const line of lines) {
+			let l = line.trim()
+			if (!l.startsWith('import')) {
+				if (l.includes('getPpReact')) {
+					l = l.replace('getPpReact', 'runproteinpaint.wrappers.getPpReact')
+				}
+				if (l.includes('getLolliplotTrack')) {
+					l = l.replace('getLolliplotTrack', 'runproteinpaint.wrappers.getLolliplotTrack')
+				}
+
+				if (l.startsWith('export')) {
+					str += l.substr(l.search(' ')) + '\n'
+				} else {
+					str += l + '\n'
+				}
+			}
+		}
+		res.send(str)
+	})
 }
 
 /****
