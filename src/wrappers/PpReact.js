a2e342786755c10f9f4cdfd7836e4eaae6a6cae6
Edgar Sioson <siosonel@gmail.com>
2021-01-05 18:04:29 -0600

    copy deepEqual() from rx.core to PpReact since tree shaking is not removing unused code in the proteinpaint.js webpack bundle

--- src/wrappers/PpReact.js
+++ src/wrappers/PpReact.js
@@ -1,5 +1,4 @@
 import { runproteinpaint } from '../app'
-import { deepEqual } from '../common/rx.core'
 export { getLolliplotTrack } from './gdc.views'
 
 export function getPpReact(React, getTrack) {
@@ -65,4 +64,23 @@ export function getPpReact(React, getTrack) {
 	}
 
 	return ProteinPaint
+}
+
+function deepEqual(x, y) {
+	if (x === y) {
+		return true
+	} else if (typeof x == 'object' && x != null && (typeof y == 'object' && y != null)) {
+		if (Object.keys(x).length != Object.keys(y).length) {
+			return false
+		}
+
+		for (var prop in x) {
+			if (y.hasOwnProperty(prop)) {
+				if (!deepEqual(x[prop], y[prop])) return false
+			} else {
+				return false
+			}
+		}
+		return true
+	} else return false
 }
