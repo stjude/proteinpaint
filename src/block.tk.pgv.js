2946c167d28d1d0902d5d90b0fa93aed8a59293d
xzhou82 <xzhou1@stjude.org>
2021-02-10 16:10:32 -0600

    change "tkbedj" route back to POST to deal with very long rglst[] problem

--- src/block.tk.pgv.js
+++ src/block.tk.pgv.js
@@ -345,7 +345,7 @@ export function loadTk(tk, block) {
 			if (tk.categories) {
 				arg.categories = tk.categories
 			}
-			const task = client.dofetch2('tkbedj?' + arg2lst(arg)).then(data => {
+			const task = client.dofetch2('tkbedj', { method: 'POST', body: JSON.stringify(arg) }).then(data => {
 				if (data.error) {
 					throw data.error
 				}
@@ -415,8 +415,8 @@ export function loadTk(tk, block) {
 
 		for (const gvtk of tk.genevaluetklst) {
 			const arg = block.tkarg_bedj(gvtk)
-			//const req = new Request(block.hostURL + '/bedjdata', { method: 'POST', body: JSON.stringify(arg) })
-			const task = client.dofetch2('tkbedj?getdata=1&' + arg2lst(arg)).then(data => {
+			arg.getdata = 1
+			const task = client.dofetch2('tkbedj', { method: 'POST', body: JSON.stringify(arg) }).then(data => {
 				if (data.error) throw data.error
 				if (data.items && data.items.length > 0) {
 					for (const i of data.items) {
@@ -1354,13 +1354,4 @@ function gvtklabelclick(gvtk, tk, block) {
 	if (gvtk.multivaluekey) {
 		// to show sample by value type matrix?
 	}
-}
-
-function arg2lst(a) {
-	const lst = []
-	for (const k in a) {
-		const v = a[k]
-		lst.push(k + '=' + encodeURIComponent(typeof v == 'object' ? JSON.stringify(v) : v))
-	}
-	return lst.join('&')
 }
