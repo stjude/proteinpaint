2b46a381473f5e68342911b0fd3865dc187768f7
Colleen Reilly <colleenmeister@gmail.com>
2021-02-19 13:29:30 -0600

    app_div spans page

--- src/app.js
+++ src/app.js
@@ -319,7 +319,7 @@ function makeheader(app, obj, jwt) {
 				.style('display', app_btn_active ? 'inline-block' : 'none')
 				.style('background-color', '#f2f2f2')
 				.style('border-radius','5px')
-				.style('width','85vw')
+				.style('width','95vw')
 
 		async function load_app_div(){
 			if(apps_rendered) return
