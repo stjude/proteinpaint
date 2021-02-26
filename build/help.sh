a303501c6ec3902c5882e8b8e447db199eff9d44
Edgar Sioson <siosonel@gmail.com>
2021-02-25 22:12:21 -0600

    rename the host-helpers dir to just helpers

--- build/help.sh
+++ build/help.sh
@@ -6,7 +6,7 @@
 # ./build/help.js HOST TASK ARGS
 # where 
 # HOST: [username@]domain OR IP-address
-# TASK: any filename under build/host-helpers/, without the file extension
+# TASK: any filename under build/helpers/, without the file extension
 # ARGS: see the allowed arguments in the corresponding TASK script
 ##########################################################################
 
@@ -15,6 +15,6 @@ set -e
 REMOTE=$1
 TASK=$2
 
-rsync -a --delete ./build/host-helpers/ $REMOTE:/opt/app/pp/helpers
+rsync -a --delete ./build/helpers/ $REMOTE:/opt/app/pp/helpers
 
-ssh "$REMOTE" "cd /opt/app/pp; chmod -R 755 helpers; bash -s " -- < ./build/host-helpers/$TASK.sh "${@:3}" 
+ssh "$REMOTE" "cd /opt/app/pp; chmod -R 755 helpers; bash -s " -- < ./build/helpers/$TASK.sh "${@:3}" 
