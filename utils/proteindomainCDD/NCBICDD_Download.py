133c20e754f02d0b2f55c8441cdf2b163803d0f6
Wang <jian.wang@stjude.org>
2021-03-03 10:36:31 -0600

    proteindomainCDD update

--- utils/proteindomainCDD/NCBICDD_Download.py
+++ utils/proteindomainCDD/NCBICDD_Download.py
@@ -7,33 +7,35 @@ Downloading *.protein.gpff.gz files from NCBI for CDD extraction.
 
 import subprocess as sp
 import os,sys
+import nameConvert
 
 usage="python3 "+sys.argv[0]+" <species>"
-spe = """human
-	mouse
-	zebrafish
-"""
 
 if len(sys.argv) <= 1:
 	print(usage)
 	print("Species supported:")
-	print(spe)
+	print(', '.join(nameConvert.SUPSPE()))
 	sys.exit(1)
 
 species = sys.argv[1]
 
 ## species info
-ORG2refDir = {"human":"H_sapiens",
-                "mouse":"M_musculus",
-                "zebrafish":"D_rerio"}
+speDir = nameConvert.DOWNAME(species)
+if not speDir:
+	print(species + ' is not supported yet!')
+	print('Species supported: '+', '.join(nameConvert.SUPSPE()))
+	sys.exit(1)
 
+downloadPath = 'ftp://ftp.ncbi.nih.gov/refseq/' + \
+		speDir + \
+		'/mRNA_Prot/*.protein.gpff.gz'
 
 #generate a directory named 'CDD_NCBI' to store all protein.gpff.gz files
 sp.run('mkdir -p CDD_NCBI_'+species,shell=True)
 os.chdir('CDD_NCBI_'+species)
 
 #Downloading NCBI protein annotation files (protein.gpff.gz)
-sp.run('wget ftp://ftp.ncbi.nih.gov/refseq/'+ORG2refDir[species]+'/mRNA_Prot/*.protein.gpff.gz',shell=True)
+sp.run('wget '+downloadPath,shell=True)
 #sp.run('wget ftp://ftp.ncbi.nih.gov/refseq/release/vertebrate_mammalian/*.protein.gpff.gz',shell=True)
 print('protein annotation file downloading done!')
 
