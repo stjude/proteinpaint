3310f0769c167f3370845f110267f45107653625
Edgar Sioson <siosonel@gmail.com>
2021-04-01 15:46:32 -0500

    add a comment to force prettier pre-commit formatting

--- client/shared/tree.js
+++ client/shared/tree.js
@@ -26,26 +26,19 @@ one item for each child-parent relationship in the hierarchy
 strange issue: https://github.com/stjude/proteinpaint/commit/c36004d47d4374d2ade719c6ef9e2b848f0850dc
 using Map for lp, nodes etc will cause memory issue, thus the use of simple objects
 
+to-do: verify this works after a reorg
 */
-
-
-
 
 const hardcode_root = 'root'
 const hierarchy_spacer = '...'
 
-
-
-
-export function stratinput(lst,levels){
-
-	const lp=Object.create(null)
+export function stratinput(lst, levels) {
+	const lp = Object.create(null)
 	// leaf to parent
 	// k: HM...BALL...sub
 	// v: HM...BALL
 
-
-	const nodes=Object.create(null)
+	const nodes = Object.create(null)
 	/*
 	k: string id of node, e.g. HM...BALL
 	v: node
@@ -54,33 +47,32 @@ export function stratinput(lst,levels){
 			items from input
 	*/
 
-
-	const size=Object.create(null)
+	const size = Object.create(null)
 	// only increment size to leaf nodes, so that root.sum() will work
 	// k: string id of a node, e.g. HM...BALL
 	// v: number of items
-	for(const m of lst) {
-		for(const [i,lev] of levels.entries()) {
-			const thisv=getkey(m,i,levels)
-			const pav=getkey(m,i-1,levels)
-			if(!m[lev.k]) {
+	for (const m of lst) {
+		for (const [i, lev] of levels.entries()) {
+			const thisv = getkey(m, i, levels)
+			const pav = getkey(m, i - 1, levels)
+			if (!m[lev.k]) {
 				// stop at this level
 				// add count to prev level
-				if(i>0) {
+				if (i > 0) {
 					size[pav] += 1
 				}
 				break
 			}
 			lp[thisv] = pav
-			if(!(thisv in size)) {
+			if (!(thisv in size)) {
 				size[thisv] = 0
 			}
-			if(!(thisv in nodes)) {
-				const n={
-					lst:[]
+			if (!(thisv in nodes)) {
+				const n = {
+					lst: []
 				}
-				if(lev.full) {
-					n.full=m[lev.full]
+				if (lev.full) {
+					n.full = m[lev.full]
 				}
 
 				n.id0 = levels[0].k
@@ -97,25 +89,25 @@ export function stratinput(lst,levels){
 				nodes[thisv] = n
 			}
 			nodes[thisv].lst.push(m)
-			if(i==levels.length-1) {
+			if (i == levels.length - 1) {
 				size[thisv] += 1
 			}
 		}
 	}
 
-	const nlst=[{ id:hardcode_root, name:hardcode_root}]
+	const nlst = [{ id: hardcode_root, name: hardcode_root }]
 
-	for(const chid in lp) {
-		const paid=lp[chid]
-		const n=nodes[chid]
-		const fields=chid.split( hierarchy_spacer )
+	for (const chid in lp) {
+		const paid = lp[chid]
+		const n = nodes[chid]
+		const fields = chid.split(hierarchy_spacer)
 		nlst.push({
-			id:chid,
-			parentId:paid,
-			lst:n.lst,
-			value:size[chid],
-			name:fields[fields.length-1], // show this instead of chid
-			full:n.full,
+			id: chid,
+			parentId: paid,
+			lst: n.lst,
+			value: size[chid],
+			name: fields[fields.length - 1], // show this instead of chid
+			full: n.full,
 			id0: n.id0,
 			v0: n.v0,
 			id1: n.id1,
@@ -127,15 +119,13 @@ export function stratinput(lst,levels){
 	return nlst
 }
 
-
-
-function getkey(m,i,levels) {
+function getkey(m, i, levels) {
 	// if i is 0, return 'root'
-	const klst = [ hardcode_root ]
-	for(let j=0; j<i; j++) {
+	const klst = [hardcode_root]
+	for (let j = 0; j < i; j++) {
 		klst.push(m[levels[j].k])
 	}
-	if(i>=0) {
+	if (i >= 0) {
 		klst.push(m[levels[i].k])
 	}
 	return klst.join(hierarchy_spacer)
