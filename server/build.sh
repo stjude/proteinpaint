1cf7331300ae9045c5205fa55909082b00180b93
Edgar Sioson <siosonel@gmail.com>
2021-04-14 18:53:19 -0500

    package server code

---
+++ server/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+###############
+# ARGUMENTS
+###############
+
+if (($# == 0)); then
+	REV="HEAD"
+else
+	REV=$1
+fi
+
+#############################
+# EXTRACT FROM COMMIT
+#############################
+
+./build/tmp.sh $REV
+cd tmppack/server
+npm install
+npx webpack --config=webpack.config.js
+
+##########
+# PACK
+##########
+
+rm -rf public
+rm -f shared
+mv ../client/shared ./
+npm pack 
