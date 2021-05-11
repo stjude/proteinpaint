7410785a2d72f858caa9c678aac99e0079863922
Edgar Sioson <siosonel@gmail.com>
2021-05-11 10:57:53 -0500

    Incorporate GDC Lolliplot testing, using pre-generated server data, in the corrresponding target Dockerfile build

--- server/src/test/routes/cacheMiddleware.js
+++ server/src/test/routes/cacheMiddleware.js
@@ -5,18 +5,18 @@ const path = require('path')
 const crypto = require('crypto')
 const serverconfig = require('../../serverconfig')
 
-module.exports = function setRoutes(app, basepath) {
+module.exports = function setRoutes(app) {
 	app.use((req, res, next) => {
 		const id = crypto
 			.createHash('sha1')
 			.update(`${req.originalUrl} ${JSON.stringify(req.body)}`)
 			.digest('hex')
 		const cacheFile = path.join(serverconfig.binpath + '/src/test/data', id)
-		console.log(11, id, cacheFile)
 
 		try {
 			if (fs.existsSync(cacheFile)) {
 				console.log(`Using cache file ${cacheFile}`)
+				res.header('content-type', 'application/json')
 				res.send(fs.readFileSync(cacheFile))
 			} else {
 				const send = res.send
