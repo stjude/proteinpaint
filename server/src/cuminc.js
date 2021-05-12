1e0201221c04f4c6e2e3b950d6db0686c7898e64
Paul <robinpaul85@gmail.com>
2021-05-12 02:00:53 -0500

    Preprocessing data for CI

--- server/src/cuminc.js
+++ server/src/cuminc.js
@@ -13,45 +13,57 @@ export function handle_incidence(genomes) {
 			const tdb = ds.cohort.termdb
 			if (!tdb) throw 'no termdb for this dataset'
 			const case_data = get_sql_queries_greater(q, ds)
-			const case_patient_ids = case_data.map(i => i.sample)
-			const case_grades = case_data.map(i => i.grade).join('\t')
-			const case_year_to_events = case_data.map(i => Object.values(i)[2]) // Was not able to call using the key name MIN(years_to_event), therefore using Object.values
+			//const case_patient_ids = case_data.map(i => i.sample)
+			//const case_grades = case_data.map(i => i.grade).join('\t')
+			const case_year_to_events = case_data.map(i => Object.values(i)[2])
 
 			const control_data = get_sql_queries_lesser(q, ds)
-			const control_patient_ids = control_data.map(i => i.sample)
-			const control_grades = control_data.map(i => i.grade).join('\t')
+			//const control_patient_ids = control_data.map(i => i.sample)
+			//const control_grades = control_data.map(i => i.grade).join('\t')
 			const control_year_to_events = control_data.map(i => Object.values(i)[2])
 
 			const death_datas = get_sql_queries_greater(q, ds) // set grade = 5 for deaths
-			//let patient_death_times = []
-			//let patient_death_ids = []
-			//if (death_datas.length != 0) { // Everybody is alive/censored
-			//patient_death_status = data.map(i => '0').join('\t')
-			//patient_death_times = data.map(i => 'NA').join('\t')
 			const patient_death_ids = death_datas.map(i => i.sample)
 			const patient_death_times = death_datas.map(i => Object.values(i)[2])
-			//}
+
 			let event_array = []
-			for (let j = 0; j < case_patient_ids.length; j++) {
-				let event = 1
-				for (let i = 0; i < patient_death_ids.length; i++) {
-					if (patient_death_ids[i] == case_patient_ids[j] && patient_death_times[i] <= case_year_to_events[j]) {
+			let group_array = []
+			for (let j = 0; j < case_data.length; j++) {
+				const case_year_to_event = Object.values(case_data[j])[2] // Was not able to call using the key name MIN(years_to_event), therefore using Object.values
+				let event = 0
+				let group = 1
+				if (case_data[j].grade != 0) {
+					event = 1
+				}
+				for (let i = 0; i < death_datas.length; i++) {
+					const patient_death_time = Object.values(death_datas[i])[2]
+					if (death_datas[i].sample == case_data[j].sample && patient_death_time <= case_year_to_event) {
 						event = 2
 					}
 				}
 				event_array.push(event)
+				group_array.push(group)
 			}
 
-			for (let j = 0; j < control_patient_ids.length; j++) {
+			for (let j = 0; j < control_data.length; j++) {
+				const control_year_to_event = Object.values(control_data[j])[2]
 				let event = 0
-				for (let i = 0; i < patient_death_ids.length; i++) {
-					if (patient_death_ids[i] == control_patient_ids[j] && patient_death_times[i] <= control_year_to_events[j]) {
+				let group = 0
+				if (control_data[j].grade != 0) {
+					event = 1
+				}
+				for (let i = 0; i < death_datas.length; i++) {
+					const patient_death_time = Object.values(death_datas[i])[2]
+					if (death_datas[i].sample == control_data[j].sample && patient_death_time <= control_year_to_event) {
 						event = 2
 					}
 				}
 				event_array.push(event)
+				group_array.push(group)
 			}
-			const year_to_events = case_year_to_events.concat(control_year_to_events).join('\t')
+			const year_to_events = case_year_to_events.concat(control_year_to_events).join('_')
+			const events = event_array.join('_')
+			const group = group_array.join('_')
 			//console.log("year_to_events:",year_to_events)
 
 			res.send(case_data)
