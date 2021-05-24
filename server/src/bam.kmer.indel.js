34d06fa9226923f921029c902c6795710af02780
Paul <robinpaul85@gmail.com>
2021-05-24 16:09:19 -0500

    Changes in rust output parsing to get rid of non-alphabetic characters

--- server/src/bam.kmer.indel.js
+++ server/src/bam.kmer.indel.js
@@ -138,23 +138,23 @@ export async function match_complexvariant_rust(q, templates_info) {
 	for (let item of rust_output_list) {
 		if (item.includes('output_gID')) {
 			group_ids = item
-				.replace('/"/g', '')
-				.replace('/,/g', '')
+				.replace(/"/g, '')
+				.replace(/,/g, '')
 				.replace('output_gID:', '')
 				.split(':')
 				//.map(Number)
 				.map(n => Number(n.replace(/\D/g, ''))) // Removing characters that are not digits
 		} else if (item.includes('output_cat')) {
 			categories = item
-				.replace('/"/g', '')
-				.replace('/,/g', '')
+				.replace(/"/g, '')
+				.replace(/,/g, '')
 				.replace('output_cat:', '')
 				.split(':')
 				.map(n => n.replace(/[^a-zA-Z0-9]+/g, '')) // Removing characters that are not alphabets
 		} else if (item.includes('output_diff_scores')) {
 			diff_scores = item
-				.replace('/"/g', '')
-				.replace('/,/g', '')
+				.replace(/"/g, '')
+				.replace(/,/g, '')
 				.replace('output_diff_scores:', '')
 				.split(':')
 				.map(Number)
