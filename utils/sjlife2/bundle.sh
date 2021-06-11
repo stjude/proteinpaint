b901227a4c7f8f011a61f62c09bf311bf61a80ce
xzhou82 <xzhou1@stjude.org>
2021-06-11 13:30:10 -0500

    update bundle script

--- utils/sjlife2/bundle.sh
+++ utils/sjlife2/bundle.sh
@@ -1,2 +1,2 @@
-tar zcvf ../tdb.tgz a* category2vcfsample chronicevents.precomputed termdb termid2htmldef term2subcohort samples.idmap keep/*
+tar zcvf ../tdb.tgz a* category2vcfsample chronicevents.precomputed term* samples.idmap keep/* PRS/*
 scp ../tdb.tgz hpc.stjude.org:~/tp/files/hg38/sjlife/
