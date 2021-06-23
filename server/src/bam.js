9da8ddb95df5f515b8ff3589ac7ad973899152af
Paul <robinpaul85@gmail.com>
2021-06-23 12:51:26 -0500

    Commented out q.variant code in query_regions function

--- server/src/bam.js
+++ server/src/bam.js
@@ -826,16 +826,16 @@ async function query_reads(q) {
 
 	otherwise, query for every region in q.regions
 	*/
-	if (q.variant) {
-		const r = {
-			chr: q.variant.chr,
-			start: q.variant.pos,
-			stop: q.variant.pos + q.variant.ref.length
-		}
-		await query_region(r, q)
-		q.regions[0].lines = r.lines
-		return
-	}
+	//if (q.variant) {
+	//	const r = {
+	//		chr: q.variant.chr,
+	//		start: q.variant.pos,
+	//		stop: q.variant.pos + q.variant.ref.length,
+	//	}
+	//	await query_region(r, q)
+	//	q.regions[0].lines = r.lines
+	//	return
+	//}
 	if (q.sv) {
 		return
 	}
@@ -877,9 +877,9 @@ function query_region(r, q) {
 	r.lines = []
 	return new Promise((resolve, reject) => {
 		let ps = ''
-		console.log(
-			'samtools view ' + q.file + ' ' + (q.nochr ? r.chr.replace('chr', '') : r.chr) + ':' + r.start + '-' + r.stop
-		)
+		//console.log(
+		//	'samtools view ' + q.file + ' ' + (q.nochr ? r.chr.replace('chr', '') : r.chr) + ':' + r.start + '-' + r.stop
+		//)
 		if (q.gdc_case_id) {
 			ps = spawn(samtools, ['view', q.file], { cwd: q.dir })
 		} else {
