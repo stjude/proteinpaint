fdcc514b19288459ad9be67020250ac187840e05
xzhou <xzhou1@stjude.org>
2021-07-19 14:35:51 -0500

    improve kmer test script to isolate data from utility, establish a structure to add in additional tests

--- server/utils/test/rust_indel.spec.js
+++ server/utils/test/rust_indel.spec.js
@@ -7,6 +7,13 @@ const Readable = require('stream').Readable
 ***************/
 const rust_indel_bin = '../rust_indel_cargo/target/release/rust_indel_cargo'
 
+// these are constants. can be overwritten by the same settings in the examples
+const kmer_length = 6,
+	weight_no_indel = 0.1,
+	weight_indel = 10,
+	threshold_slope = 0.1,
+	strictness = 1
+
 /**************
  Test sections
 ***************/
@@ -15,35 +22,159 @@ tape('\n', function(test) {
 	test.end()
 })
 
-let output
-
-//TODO: add useful test for invalid input
 tape('rust indel binary', async function(test) {
-	//Test valid input
-	const validInput =
-		'GTACCCTAGGTGGAACGGCCGCCTTCTCCATTCTCCATGGCCCCACAAGCTTCCCTTCCCCCGGTGCCACCACGACTTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGTAAAGCATTTT-GTACCCTAGGTGGAACGGCCGCCTTCTCCATTCTCCATGGCCCCACAAGCTTCCCTTCCCCCGGTGCCACCACGACTTGACCTGGTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGTAAAGCATTTT-CACGACTTGACCTGGCGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTC-TGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGTAAAGCATTTTCCATTAC-GTAATCTCAGCTACTTGGGAGGCTGAGGCAGGAGAATTGCTTCAACACAAGAGGCGGAGGTTGCAGTCAGTCGAG-CCTGGTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGCTGCT-CCACGACTTGACCTGGTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTT-CTTGACCTGGTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGG:119155749-119155755-119146842-119155742-119155747-119155747:62M88N13M-75M-3M8834N72M-69M88N6M-16S59M-10S65M:119155745:75:T:TTGACCTGG:6:0.1:10:0.1:1:GTACCCTAGGTGGAACGGCCGCCTTCTCCATTCTCCATGGCCCCACAAGCTTCCCTTCCCCCGGTGCCACCACGAC:TGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGTAAAGCATTTT'
+	for (const e of examples) {
+		await runTest(e, test)
+	}
+	test.end()
+})
+
+function runTest(e, test) {
+	// validate data structure of e
+	if (!e.pplink) throw '.pplink missing'
+	if (!e.leftFlank) throw '.leftFlank missing'
+	if (!e.rightFlank) throw '.rightFlank missing'
+	if (!e.seqRef) throw '.seqRef missing'
+	if (!e.seqMut) throw '.seqMut missing'
+	if (!Number.isInteger(e.readlen)) throw '.readlen is not integer'
+	if (!e.variant) throw '.variant{} missing'
+	if (!Number.isInteger(e.variant.pos)) throw '.variant.pos is not integer'
+	if (!e.variant.ref) throw '.variant.ref missing'
+	if (!e.variant.alt) throw '.variant.alt missing'
+	if (!Array.isArray(e.reads)) throw '.reads[] missing'
+	if (e.reads.length == 0) throw '.reads[] empty array'
+	for (const r of e.reads) {
+		if (!r.seq) throw '.seq missing from a read'
+		if (!Number.isInteger(r.pos)) throw '.pos not integer from a read'
+		if (!r.cigar) throw '.cigar missing from a read'
+		if (r.group != 'none' && r.group != 'ref' && r.group != 'alt') throw '.group is not none/ref/alt for a read'
+	}
+
+	// compose input string
+	const input =
+		e.seqRef +
+		'-' +
+		e.seqMut +
+		'-' +
+		e.reads.map(i => i.seq).join('-') +
+		':' +
+		e.reads.map(i => i.pos).join('-') +
+		':' +
+		e.reads.map(i => i.cigar).join('-') +
+		':' +
+		e.variant.pos +
+		':' +
+		e.readlen +
+		':' +
+		e.variant.ref +
+		':' +
+		e.variant.alt +
+		':' +
+		kmer_length +
+		':' +
+		weight_no_indel +
+		':' +
+		weight_indel +
+		':' +
+		threshold_slope +
+		':' +
+		strictness +
+		':' +
+		e.leftFlank +
+		':' +
+		e.rightFlank
+
+	// run rust binary
 	const ps = spawn(rust_indel_bin)
-	Readable.from(validInput).pipe(ps.stdin)
+	Readable.from(input).pipe(ps.stdin)
 	const stdout = []
+	const err = []
+	ps.stderr.on('data', d => err.push(d))
 	ps.stdout.on('data', data => stdout.push(data))
 	ps.on('close', code => {
-		const output = stdout.join('').split('\n')
-		let output_cat = ''
-		let output_gID = ''
-		for (i of output) {
-			if (i.includes('output_cat'))
-				output_cat = i
+		const errmsg = err.join('').trim()
+		if (errmsg) throw errmsg
+
+		let groups, indices
+		for (const line of stdout.join('').split('\n')) {
+			if (line.includes('output_cat')) {
+				groups = line
 					.replace(/"/g, '')
 					.replace(/,/g, '')
 					.replace('output_cat:', '')
-			if (i.includes('output_gID'))
-				output_gID = i
+					.split(':')
+			} else if (line.includes('output_gID')) {
+				indices = line
 					.replace(/"/g, '')
 					.replace(/,/g, '')
 					.replace('output_gID:', '')
+					.split(':')
+					.map(Number)
+			}
 		}
-		test.equal(output_cat, 'none:ref:none:none:alt:alt', 'should match expected output categories')
-		test.equal(output_gID, '2:1:3:0:4:5', 'should match expected output gIDs')
+
+		// detect reads that fail the test
+		for (const idx of indices) {
+			if (e.reads[idx].group != groups[idx]) {
+				test.fail(`Read #${idx + 1} assigned to "${e.reads[idx].group}", should be "${groups[idx]}". ${e.pplink}`)
+			}
+		}
 	})
-	test.end()
-})
+}
+
+const examples = [
+	{
+		pplink:
+			'https://ppr.stjude.org/?genome=hg19&block=1&bamfile=test,proteinpaint_demo/hg19/bam/rna.8bp.insertion.bam&position=chr11:119155611-119155851&variant=chr11.119155746.T.TTGACCTGG',
+		leftFlank: 'GTACCCTAGGTGGAACGGCCGCCTTCTCCATTCTCCATGGCCCCACAAGCTTCCCTTCCCCCGGTGCCACCACGAC',
+		rightFlank: 'TGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGTAAAGCATTTT',
+		seqRef:
+			'GTACCCTAGGTGGAACGGCCGCCTTCTCCATTCTCCATGGCCCCACAAGCTTCCCTTCCCCCGGTGCCACCACGACTTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGTAAAGCATTTT',
+		seqMut:
+			'GTACCCTAGGTGGAACGGCCGCCTTCTCCATTCTCCATGGCCCCACAAGCTTCCCTTCCCCCGGTGCCACCACGACTTGACCTGGTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGTAAAGCATTTT',
+		readlen: 75,
+		variant: {
+			pos: 119155745, // 0-based
+			ref: 'T',
+			alt: 'TTGACCTGG'
+		},
+		reads: [
+			{
+				seq: 'CACGACTTGACCTGGCGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTC',
+				pos: 119155749,
+				cigar: '62M88N13M',
+				group: 'none'
+			},
+			{
+				seq: 'TGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGTAAAGCATTTTCCATTAC',
+				pos: 119155755,
+				cigar: '3M8834N72M',
+				group: 'ref'
+			},
+			{
+				seq: 'GTAATCTCAGCTACTTGGGAGGCTGAGGCAGGAGAATTGCTTCAACACAAGAGGCGGAGGTTGCAGTCAGTCGAG',
+				pos: 119146842,
+				cigar: '3M8834N72M',
+				group: 'none'
+			},
+			{
+				seq: 'CCTGGTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGGCTGCT',
+				pos: 119155742,
+				cigar: '69M88N6M',
+				group: 'none'
+			},
+			{
+				seq: 'CCACGACTTGACCTGGTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTT',
+				pos: 119155747,
+				cigar: '16S59M',
+				group: 'alt'
+			},
+			{
+				seq: 'CTTGACCTGGTGACCTTCTGCCGCAGCGAGTATGTGTTCCCTCAAGTGCTTCTGCTCTTGGAACTGCTTCTAAGG',
+				pos: 119155747,
+				cigar: '10S65M',
+				group: 'alt'
+			}
+		]
+	}
+]
