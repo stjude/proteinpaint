9dba1f1e1dd836b0477139e5e7a9bf375ad5d244
Gavriel Matt <gavriel.matt@stjude.org>
2021-07-22 14:48:08 -0500

    modified termdb.initbinconfig.js and created test/termdb.initbinconfig.spec.js for testing.

--- server/shared/termdb.initbinconfig.js
+++ server/shared/termdb.initbinconfig.js
@@ -1,5 +1,3 @@
-const jStat = require('jstat').jStat
-
 /*
 Initialize a bin configuration for a numeric dataset
 <data>: array of numeric data values
@@ -11,24 +9,22 @@ module.exports = function initBinConfig(data, opts = {}) {
 	for (const x of data) {
 		if (!Number.isFinite(x)) throw 'Cannot compute bin config. Data contains non-numeric values.'
 	}
-	// compute bin size, first bin stop, and last bin start (if applicable)
-	const max = jStat.max(data)
-	const min = jStat.min(data)
-	const binSize = (max - min) / 6
-	const firstBinStop = jStat.percentile(data, 0.05)
-	const eighthBinStop = firstBinStop + binSize * 7
-	let lastBinStart
-	if (max > eighthBinStop) {
-		//upper limit number of bins is 8
-		const eighthBinStart = firstBinStop + binSize * 6
-		lastBinStart = eighthBinStart
-	}
+	// compute the bin size for a maximum bin number of 8
+	data.sort((a, b) => a - b)
+	const l = data.length
+	const min = data[0]
+	const max = data[l - 1]
+	const binSize = (max - min) / 8
+	// first bin stop will equal the 5th percentile of the dataset
+	let p5idx = Math.round(l * 0.05) - 1
+	if (p5idx < 0) p5idx = 0
+	const firstBinStop = data[p5idx]
 	// round the bin values
 	let binSize_rnd, firstBinStop_rnd, lastBinStart_rnd, rounding
-	if (binSize >= 1 && firstBinStop >= 1) {
-		;[binSize_rnd, firstBinStop_rnd, lastBinStart_rnd] = roundIntegers(binSize, firstBinStop, lastBinStart)
+	if (binSize >= 1 && Math.abs(firstBinStop) >= 1) {
+		;[binSize_rnd, firstBinStop_rnd, lastBinStart_rnd] = roundIntegers(binSize, firstBinStop, max)
 	} else {
-		;[binSize_rnd, firstBinStop_rnd, lastBinStart_rnd, rounding] = roundFractions(binSize, firstBinStop, lastBinStart)
+		;[binSize_rnd, firstBinStop_rnd, lastBinStart_rnd, rounding] = roundFractions(binSize, firstBinStop, max)
 	}
 	// generate the bin configuration
 	const binConfig = {
@@ -50,26 +46,28 @@ module.exports = function initBinConfig(data, opts = {}) {
 	}
 }
 
-function roundIntegers(binSize, firstBinStop, lastBinStart) {
+function roundIntegers(binSize, firstBinStop, max) {
 	const binSize_rnd = Number(binSize.toPrecision(1))
-	const binSize_log = Math.floor(Math.log10(binSize))
-	const firstBinStop_log = Math.floor(Math.log10(firstBinStop))
+	const binSize_log = Math.floor(Math.log10(binSize_rnd))
+	const firstBinStop_log = Math.floor(Math.log10(Math.abs(firstBinStop)))
 	let firstBinStop_rnd
 	if (binSize_log <= firstBinStop_log) {
 		firstBinStop_rnd = Math.round(firstBinStop / 10 ** binSize_log) * 10 ** binSize_log
 	} else {
 		firstBinStop_rnd = Math.round(firstBinStop / 10 ** firstBinStop_log) * 10 ** firstBinStop_log
 	}
+	// if the number of bins is greater than 8 after rounding, set the last bin start to restrict the number of bins to 8
 	let lastBinStart_rnd
-	if (lastBinStart) {
-		lastBinStart_rnd = Math.round(lastBinStart / 10 ** binSize_log) * 10 ** binSize_log
+	const eighthBinStop_rnd = firstBinStop_rnd + binSize_rnd * 7
+	if (max > eighthBinStop_rnd) {
+		lastBinStart_rnd = firstBinStop_rnd + binSize_rnd * 6
 	}
 	return [binSize_rnd, firstBinStop_rnd, lastBinStart_rnd]
 }
 
-function roundFractions(binSize, firstBinStop, lastBinStart) {
+function roundFractions(binSize, firstBinStop, max) {
 	const binSize_rnd = Number(binSize.toPrecision(1))
-	const binSize_log = Math.floor(Math.log10(binSize))
+	const binSize_log = Math.floor(Math.log10(binSize_rnd))
 	const firstBinStop_log = Math.floor(Math.log10(Math.abs(firstBinStop)))
 	let digits
 	if (binSize_log <= firstBinStop_log) {
@@ -78,8 +76,12 @@ function roundFractions(binSize, firstBinStop, lastBinStart) {
 		digits = Math.abs(firstBinStop_log)
 	}
 	const firstBinStop_rnd = Number(firstBinStop.toFixed(digits))
+	// if the number of bins is greater than 8 after rounding, set the last bin start to restrict the number of bins to 8
 	let lastBinStart_rnd
-	if (lastBinStart) {
+	const eighthBinStop = firstBinStop_rnd + binSize_rnd * 7
+	const eighthBinStop_rnd = Number(eighthBinStop.toFixed(digits))
+	if (max > eighthBinStop_rnd) {
+		lastBinStart = firstBinStop_rnd + binSize_rnd * 6
 		lastBinStart_rnd = Number(lastBinStart.toFixed(digits))
 	}
 	const rounding = '.' + digits + 'f'
