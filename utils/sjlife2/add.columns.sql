8fa08ba20be43ba24d35ae238a245fa44613f6d3
Edgar Sioson <siosonel@gmail.com>
2021-07-29 10:51:13 -0500

    the sjlife ETL will be updated to not require adding columns to the terms table

--- utils/sjlife2/add.columns.sql
+++
@@ -1,8 +0,0 @@
-
----------------------------------------------
--- add columns to the terms table
----------------------------------------------
-
-ALTER TABLE terms ADD COLUMN type TEXT;
-ALTER TABLE terms ADD COLUMN isleaf INTEGER;
-ALTER TABLE subcohort_terms ADD COLUMN included_types TEXT;
