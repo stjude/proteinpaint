a7bc60f36ac55e1e26e8e47fd45a0c5e67b3ff26
Edgar Sioson <siosonel@gmail.com>
2021-08-06 11:20:13 -0500

    Specify the expected selectors in features.json instead of in example.spec.js

--- client/src/test/examples.spec.js
+++ client/src/test/examples.spec.js
@@ -36,34 +36,6 @@ const eventCallbacks = {
 	// Lollipop: 'postRender.test'
 }
 
-// if a type or Example.name is not found in elemToTest,
-// then the default test is to test for
-// 1 svg element per runpp() call in an example
-const elemToTest = {
-	// for image tests, do not count nativeTracks here as
-	// that is detected in runargs and added to the test automatically
-
-	// by example.name
-	Lollipop: { circle: 10 },
-	'Sample Scatterplot': { circle: 10 },
-	ASE: { rect: 20, line: 20 },
-
-	// by track.type
-	junction: { circle: 2 },
-	mdsjunction: { circle: 2 },
-	bigwig: { image: 1 },
-	aicheck: { image: 1 },
-	profilegenevalue: { image: 1 },
-	bigwigstranded: { image: 1 },
-	bigwig: { image: 1 },
-	bedj: { image: 1 },
-	hicstraw: { image: 1 },
-	bam: { image: 1 },
-	profilegenevalue: { image: 1 }
-}
-
-const excludedExamples = ['Hi-C', 'Single Cell']
-
 /**************
  test sections
 ***************/
@@ -93,12 +65,16 @@ tape('examplejson', test => {
 function runTests(data, test) {
 	// 1. Plan the tests
 	// Track examples that are testable and how they should be tested
-	const testable = []
+	const testable = [],
+		notTested = { hidden: [] }
 	let numPlannedTests = 1 // including the examplejson download
 	for (const x of data.examples) {
-		if (!x.ppcalls || x.hidden || excludedExamples.includes(x.name)) continue
+		if (!x.ppcalls || x.hidden) {
+			notTested.hidden.push(x.name)
+			continue
+		}
 		// to limit to a particular test, uncomment the line below
-		// if (!['ASE'].includes(x.name)) continue
+		// if (!['RNA splice junction of one sample'].includes(x.name)) continue
 
 		if (!Array.isArray(x.ppcalls)) {
 			test.fail('ppcalls should be an array for ' + x.name)
@@ -111,9 +87,18 @@ function runTests(data, test) {
 					// for tracks, use callback tests only if there is one track
 					// otherwise, use timed tests
 					const useCallback = (tracks.length === 1 && tracks[0].type in eventCallbacks) || x.name in eventCallbacks
-					const expected = getExpected({ x, call })
-					numPlannedTests += Object.keys(expected).length
-					testable.push({ x, call, useCallback, expected })
+					/*
+						an example call must have a testSpec: {selector1: count, ...}
+						object in order to trigger testing per call
+					*/
+					const numElemToTest = Object.keys(call?.testSpec?.expected || {}).length
+					if (numElemToTest) {
+						numPlannedTests += numElemToTest
+						testable.push({ x, call, useCallback })
+					} else {
+						if (!(x.name in notTested)) notTested[x.name] = []
+						notTested[x.name].push(call.label || 'call#' + x.ppcalls.indexOf(call) + ': no testSpec')
+					}
 				}
 			}
 		}
@@ -125,11 +110,16 @@ function runTests(data, test) {
 		return
 	}
 
+	if (notTested.hidden.length || Object.keys(notTested).length > 1) {
+		console.info('Not Tested: ', JSON.stringify(notTested, null, 2))
+	}
+
 	test.plan(numPlannedTests)
 	const numTests = testable.length
-	const numSeconds = 6 // number of seconds for each example to fully render
+	// default number of seconds for each example to fully render
+	const numSeconds = 5
 	// since tracks are rendered in parallel, the total render time
-	// is not additive, so lower the expected number of seconds
+	// is not additive, so lower the expected number of seconds to finish
 	test.timeoutAfter(numSeconds * 1000 * Math.max(2, Math.ceil(numTests / 4)))
 
 	// 2. Recursively run tests with a limited number of concurrency
@@ -157,8 +147,8 @@ function runTests(data, test) {
 		const arg = { holder, host: window.location.origin }
 		const callback = tkInstance => {
 			numCallsTested++
-			for (const selector in t.expected) {
-				hasElem(holder, test, t.x.name, selector, t.expected[selector])
+			for (const selector in t.call.testSpec.expected) {
+				hasElem(holder, test, t.x.name, selector, t.call.testSpec.expected[selector])
 			}
 			mayRunOneCall()
 		}
@@ -182,52 +172,18 @@ function runTests(data, test) {
 	function runTimedTest(t) {
 		const holder = getHolder()
 		const arg = { holder, host: window.location.origin }
+		const wait = t.call?.testSpec?.timeout || 5000
 
 		setTimeout(() => {
 			numCallsTested++
-			for (const selector in t.expected) {
-				hasElem(holder, test, t.x.name, selector, t.expected[selector])
+			const label = t.x.name + (t.call.label ? ' ' + t.call.label : '')
+			for (const selector in t.call.testSpec.expected) {
+				hasElem(holder, test, label, selector, t.call.testSpec.expected[selector])
 			}
 			mayRunOneCall()
-		}, numSeconds * 1000)
+		}, wait)
 
 		numCallsStarted++
 		runproteinpaint(Object.assign(arg, t.call.runargs))
 	}
-}
-
-function getExpected(t) {
-	// will delete unused 'svg' or 'image' key later
-	const expected = { svg: 0, image: 0 }
-
-	if (t.call.runargs.nativetracks) expected.image++
-
-	if (t.x.name in elemToTest) {
-		for (const selector in elemToTest[t.x.name]) {
-			if (!(selector in expected)) expected[selector] = 0
-			expected[selector] += elemToTest[t.x.name][selector]
-		}
-	} else {
-		if (t.call.runargs.tracks) {
-			for (const tk of t.call.runargs.tracks) {
-				const label = t.x.name + ' ' + tk.type
-				if (tk.type in elemToTest) {
-					for (const selector in elemToTest[tk.type]) {
-						if (!(selector in expected)) expected[selector] = 0
-						expected[selector] += elemToTest[tk.type][selector]
-					}
-				} else {
-					expected.svg++
-				}
-			}
-		} else {
-			expected.svg++
-		}
-	}
-
-	for (const selector in expected) {
-		if (!expected[selector]) delete expected[selector]
-	}
-
-	return expected
 }
