8e3d3dc3222e40d6f11cfe9dd06c15ccdb9f3142
Edgar Sioson <siosonel@gmail.com>
2021-09-28 17:08:59 -0500

    Use the submitted term1_q when available for /termdb?getcategories=1

--- server/src/termdb.js
+++ server/src/termdb.js
@@ -170,16 +170,18 @@ function trigger_getcategories(q, res, tdb, ds) {
 			break
 		case 'integer':
 		case 'float':
-			arg.term1_q = term.bins.default
+			arg.term1_q = q.term1_q ? q.term1_q : term.bins.default
 			break
 		case 'condition':
-			arg.term1_q = {
-				bar_by_grade: q.bar_by_grade,
-				bar_by_children: q.bar_by_children,
-				value_by_max_grade: q.value_by_max_grade,
-				value_by_most_recent: q.value_by_most_recent,
-				value_by_computable_grade: q.value_by_computable_grade
-			}
+			arg.term1_q = q.term1_q
+				? q.term1_q
+				: {
+						bar_by_grade: q.bar_by_grade,
+						bar_by_children: q.bar_by_children,
+						value_by_max_grade: q.value_by_max_grade,
+						value_by_most_recent: q.value_by_most_recent,
+						value_by_computable_grade: q.value_by_computable_grade
+				  }
 			break
 		default:
 			throw 'unknown term type'
