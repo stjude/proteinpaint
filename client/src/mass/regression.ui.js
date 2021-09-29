0cc302c56417fe0de805d654b52e3aa1c4bc5809
Jaimin Patel <jaimin.patel@stjude.org>
2021-09-29 17:05:02 -0500

    regression ui: show barchart for values and show excluded categories

--- client/src/mass/regression.ui.js
+++ client/src/mass/regression.ui.js
@@ -112,12 +112,14 @@ class MassRegressionUI {
 		if (q.value_by_computable_grade) lst.push('value_by_computable_grade=1')
 		const url = lst.join('&')
 		const data = await dofetch3(url, {}, this.app.opts.fetchOpts)
-        console.log(data)
 		if (data.error) throw data.error
 		d.sampleCounts =
 			d.term.term.type == 'float' || d.term.term.type == 'integer'
 				? data.lst.filter(v => v.range.is_unannotated != true)
 				: data.lst
+        if(d.term.term.type == 'float' || d.term.term.type == 'integer' && data.lst.find(v => v.range.is_unannotated == true)){
+            d.excludeCounts = data.lst.filter(v => v.range.is_unannotated == true)
+        }
 		const totalCount = (d.term.q.totalCount = { included: 0, excluded: 0, total: 0 })
 		data.lst.forEach(v => {
 			if (v.range && v.range.is_unannotated) totalCount.excluded = totalCount.excluded + v.samplecount
@@ -252,10 +254,13 @@ function setRenderers(self) {
 			infoDiv: div.append('div')
 		}
 		d.dom.cutoffDiv = d.dom.infoDiv.append('div')
-		d.dom.term_summmary_div = d.dom.infoDiv.append('div')
+        d.dom.top_info_div = d.dom.infoDiv.append('div')
+		d.dom.term_info_div = d.dom.top_info_div.append('div').style('display','inline-block')
+        d.dom.ref_click_prompt = d.dom.top_info_div.append('div').style('display','inline-block')
 		d.dom.term_values_div = d.dom.infoDiv.append('div')
 		d.dom.values_table = d.dom.term_values_div.append('table')
-		d.dom.ref_click_prompt = d.dom.term_values_div.append('div')
+		d.dom.term_summmary_div = d.dom.term_values_div.append('div')
+        d.dom.excluded_table = d.dom.term_values_div.append('table')
 		updatePill.call(this, d)
 	}
 
@@ -326,60 +331,67 @@ function setRenderers(self) {
 		if (d.section.configKey == 'independent') {
 			if (d.term.term.type == 'float' || d.term.term.type == 'integer') {
 				make_values_table(d)
+                d.dom.term_info_div.html(`Use as ${q.use_as || 'continuous'} variable. </br>`)
 				d.dom.term_summmary_div.html(
-					`Use as ${q.use_as || 'continuous'} variable. </br>
-					${q.totalCount.included} sample included.` +
+					`${q.totalCount.included} sample included.` +
 						(q.totalCount.excluded ? ` ${q.totalCount.excluded} samples excluded.` : '')
 				)
 			} else if (d.term.term.type == 'categorical' || d.term.term.type == 'condition') {
 				const gs = d.term.q.groupsetting || {}
-				let text
-				// d.values is already set by self.setActiveValues() above
-				if (gs.inuse) {
-					text = Object.keys(d.values).length + ' groups.'
-					make_values_table(d)
-				} else {
-					text = Object.keys(d.values).length + (d.term.term.type == 'categorical' ? ' categories.' : ' grades.')
-					make_values_table(d)
-				}
-				text =
-					text +
+                // d.values is already set by self.setActiveValues() above
+				const term_text = 'Use as ' + Object.keys(d.values).length + (gs.inuse ? ' groups.' : ' categories.')
+				make_values_table(d)
+				const summary_text =
 					` ${q.totalCount.included} sample included.` +
-					(q.totalCount.excluded ? ` ${q.totalCount.excluded} samples excluded.` : '')
+					(q.totalCount.excluded ? ` ${q.totalCount.excluded} samples excluded:` : '')
+                d.dom.term_info_div.html(term_text)
 				d.dom.ref_click_prompt
 					.style('padding', '5px 10px')
 					.style('color', '#999')
 					.style('text-transform', 'uppercase')
+                    .style('font-size', '.7em')
 					.text('Click to set a row as reference.')
-				d.dom.term_summmary_div.text(text)
+				d.dom.term_summmary_div.text(summary_text)
 			}
 		} else if (d.section.configKey == 'term') {
 			if (d.term.term.type == 'float' || d.term.term.type == 'integer') make_values_table(d)
 			d.dom.term_summmary_div.text(
 				`${q.totalCount.included} sample included.` +
-					(q.totalCount.excluded ? ` ${q.totalCount.excluded} samples excluded.` : '')
+					(q.totalCount.excluded ? ` ${q.totalCount.excluded} samples excluded:` : '')
 			)
+            // QUICK FIX: hide top_info_div rightnow for linear regression, 
+            // for logistic regression, it needs to be changed as required
+            d.dom.top_info_div.style('display','none')
 		}
+        if(d.excludeCounts !== undefined && d.excludeCounts.length){
+            make_values_table(d, true)
+        }
 	}
 
-	function make_values_table(d) {
+	function make_values_table(d, excluded) {
 		// TODO: is it ok to sort grade by sample count or it should be by grades 0-5?
 		// and what should be reference group, '0: no condition' seems good choice.
-		const tr_data = d.sampleCounts.sort((a, b) => b.samplecount - a.samplecount)
-        const maxCount = Math.max(...tr_data.map(v => v.samplecount), 0)
-        tr_data.forEach(v => v.bar_width_frac = (1 - (maxCount - v.samplecount)/ maxCount))
-		if (!('refGrp' in d) && d.term.q && 'refGrp' in d.term.q) d.refGrp = d.term.q.refGrp
+		const tr_data = excluded ? 
+            d.excludeCounts.sort((a, b) => b.samplecount - a.samplecount) :
+            d.sampleCounts.sort((a, b) => b.samplecount - a.samplecount)
+        
+        if(!excluded){
+            const maxCount = Math.max(...tr_data.map(v => v.samplecount), 0)
+            tr_data.forEach(v => v.bar_width_frac = (1 - (maxCount - v.samplecount)/ maxCount).toFixed(4))
+            if (!('refGrp' in d) && d.term.q && 'refGrp' in d.term.q) d.refGrp = d.term.q.refGrp
+    
+            if (!('refGrp' in d) || !tr_data.find(c => c.key === d.refGrp)) {
+                if (d.term.id in self.refGrpByTermId && tr_data.find(c => c.key === self.refGrpByTermId[d.term.id])) {
+                    d.refGrp = self.refGrpByTermId[d.term.id]
+                } else {
+                    d.refGrp = tr_data[0].key
+                    self.refGrpByTermId[d.term.id] = tr_data[0].key
+                }
+            }
+        }
 
-		if (!('refGrp' in d) || !tr_data.find(c => c.key === d.refGrp)) {
-			if (d.term.id in self.refGrpByTermId && tr_data.find(c => c.key === self.refGrpByTermId[d.term.id])) {
-				d.refGrp = self.refGrpByTermId[d.term.id]
-			} else {
-				d.refGrp = tr_data[0].key
-				self.refGrpByTermId[d.term.id] = tr_data[0].key
-			}
-		}
-
-		const trs = d.dom.values_table
+        const table = excluded ? d.dom.excluded_table : d.dom.values_table
+		const trs = table
 			.style('margin', '10px 5px')
 			.style('border-spacing', '3px')
 			.style('border-collapse', 'collapse')
@@ -397,6 +409,9 @@ function setRenderers(self) {
 			.append('tr')
 			.each(trEnter)
 		//d.values_table.selectAll('tr').sort((a,b) => d.sampleCounts[b.key] - d.sampleCounts[a.key])
+        
+        // change color of excluded_table text
+        if(excluded) d.dom.excluded_table.selectAll('td').style('color','#999')
 	}
 
 	function trEnter(item) {
@@ -421,14 +436,14 @@ function setRenderers(self) {
 
         // sample count bar td
         const bar_td = tr.append('td')
-            .style('padding', '3px 5px')
+            .style('padding', '1px 5px')
 
         // bar_width
         const barWidth = maxBarWidth * item.bar_width_frac
         bar_td.append('div')
            .style('margin','1px 10px')
            .style('width',barWidth + 'px')
-           .style('height','12px')
+           .style('height','15px')
            .style('background-color','#ddd')
             
 
