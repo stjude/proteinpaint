5beff4a761e23d48117bc1ed4a2bf74d511edc36
jaimin patel <jaimin.patel@stjude.org>
2021-10-27 17:53:12 -0500

    termsetting groupset redesign: show groups in seperate div rather than in single table

--- client/src/common/termsetting.groupsetting.js
+++ client/src/common/termsetting.groupsetting.js
@@ -1,4 +1,5 @@
 import { keyupEnter } from '../client'
+import { select } from 'd3-selection'
 
 export function setGroupsettingMethods(self) {
 	self.regroupMenu = function(grp_count, temp_cat_grps) {
@@ -81,6 +82,166 @@ export function setGroupsettingMethods(self) {
 
 		group_ct_select.node().value = default_grp_count
 
+		const groups_holder = group_edit_div.append('div')
+
+		const groups_divs = groups_holder.selectAll(':scope > div').data(groupset.groups)
+
+		groups_divs
+			.enter()
+			.append('div')
+			.each(addGroupDiv)
+
+		// 'Apply' button
+		button_div
+			.append('div')
+			.attr('class', 'apply_btn sja_filter_tag_btn')
+			.style('display', 'inline-block')
+			.style('border-radius', '13px')
+			// .style('padding', '7px 6px')
+			.style('margin', '5px')
+			.style('text-align', 'center')
+			.style('font-size', '.8em')
+			.style('text-transform', 'uppercase')
+			.text('Apply')
+			.on('click', () => {
+				const name_inputs = group_rename_div.node().querySelectorAll('input')
+				//update customset and add to self.q
+				for (const key in cat_grps) {
+					const i = cat_grps[key].group - 1
+					const group = customset.groups[i]
+					if (group) {
+						group.name = name_inputs[i].value
+						group.values.push({ key })
+					}
+				}
+				self.q.type = 'custom-groupset'
+				self.q.groupsetting = {
+					inuse: true,
+					customset: customset
+				}
+				self.dom.tip.hide()
+				self.opts.callback({
+					term: self.term,
+					q: self.q
+				})
+			})
+	}
+
+	function addGroupDiv(group, i) {
+		console.log(group, i)
+
+		const values = self.q.bar_by_children ? self.term.subconditions : self.term.values
+		const cat_grps = JSON.parse(JSON.stringify(values))
+		const group_val_keys = group.values.map(v => v.key.toString())
+		console.log(group_val_keys, cat_grps)
+
+		const group_div = select(this)
+
+		const title_div = group_div
+			.append('div')
+			.style('border-top', '1px solid #bbb')
+			.style('margin-top', '25px')
+
+		title_div
+			.append('div')
+			.style('display', 'inline-block')
+			.style('position', 'relative')
+			.style('top', '-25px')
+			.style('border-radius', '13px')
+			.style('margin', '15px')
+			.style('margin-bottom', '-10px')
+			.style('padding', '3px 10px')
+			.style('text-align', 'center')
+			.style('font-size', '.8em')
+			.style('text-transform', 'uppercase')
+			.style('border', '1px solid #bbb')
+			.style('color', '#999')
+			.style('background-color', '#fff')
+			.text('Group ' + (i + 1))
+
+		const group_rename_div = group_div
+			.append('div')
+			.attr('class', 'group_edit_div')
+			.style('display', 'inline-block')
+
+		group_rename_div
+			.append('label')
+			.attr('for', 'grp_ct')
+			.style('display', 'inline-block')
+			.style('margin-right', '15px')
+			.html('Group name')
+
+		const group_name_input = group_rename_div
+			.append('input')
+			.attr('size', 12)
+			.attr('value', group.name || i + 1)
+			.style('margin', '2px 5px')
+			.style('display', 'inline-block')
+			.style('font-size', '.8em')
+			.style('width', '80px')
+			.on('keyup', () => {
+				if (!keyupEnter()) return
+				// TODO
+				// //update customset and add to self.q
+				// for (const [key, val] of Object.entries(cat_grps)) {
+				// 	for (let j = 0; j < default_grp_count; j++) {
+				// 		if (cat_grps[key].group == j + 1) customset.groups[j].values.push({ key: key })
+				// 	}
+				// }
+
+				// customset.groups[i].name = group_name_input.node().value
+				// self.q.type = 'custom-groupset'
+				// self.q.groupsetting = {
+				// 	inuse: true,
+				// 	customset: customset
+				// 	//predefined_groupset_idx: removed from this q.groupsetting
+				// }
+
+				// self.regroupMenu(default_grp_count, cat_grps)
+			})
+
+		const group_select_div = group_div.append('div').style('margin', '5px')
+
+		const group_table = group_select_div.append('table').style('border-collapse', 'collapse')
+
+		// for each cateogry add new row with radio button for each group and category name
+		for (const [key, val] of Object.entries(cat_grps)) {
+			const cat_tr = group_table
+				.append('tr')
+				.on('mouseover', () => {
+					cat_tr.style('background-color', '#eee')
+				})
+				.on('mouseout', () => {
+					cat_tr.style('background-color', '#fff')
+				})
+
+			cat_tr
+				.append('td')
+				.attr('align', 'center')
+				.style('padding', '2px 5px')
+				.append('input')
+				.attr('type', 'checkbox')
+				.attr('name', key)
+				.attr('value', i)
+				.property('checked', () => {
+					return group_val_keys.includes(key)
+				})
+				.on('click', () => {
+					// TODO
+					// cat_grps[key].group = i + 1
+				})
+
+			cat_tr
+				.append('td')
+				.style('display', 'inline-block')
+				.style('margin', '2px')
+				.style('cursor', 'default')
+				.html(val.label)
+		}
+	}
+
+	// refactor from here
+	/*
 		const group_rename_div = group_edit_div
 			.append('div')
 			.attr('class', 'group_edit_div')
@@ -218,40 +379,5 @@ export function setGroupsettingMethods(self) {
 				.style('cursor', 'default')
 				.html(val.label)
 		}
-
-		// 'Apply' button
-		button_div
-			.append('div')
-			.attr('class', 'apply_btn sja_filter_tag_btn')
-			.style('display', 'inline-block')
-			.style('border-radius', '13px')
-			// .style('padding', '7px 6px')
-			.style('margin', '5px')
-			.style('text-align', 'center')
-			.style('font-size', '.8em')
-			.style('text-transform', 'uppercase')
-			.text('Apply')
-			.on('click', () => {
-				const name_inputs = group_rename_div.node().querySelectorAll('input')
-				//update customset and add to self.q
-				for (const key in cat_grps) {
-					const i = cat_grps[key].group - 1
-					const group = customset.groups[i]
-					if (group) {
-						group.name = name_inputs[i].value
-						group.values.push({ key })
-					}
-				}
-				self.q.type = 'custom-groupset'
-				self.q.groupsetting = {
-					inuse: true,
-					customset: customset
-				}
-				self.dom.tip.hide()
-				self.opts.callback({
-					term: self.term,
-					q: self.q
-				})
-			})
-	}
+		*/
 }
