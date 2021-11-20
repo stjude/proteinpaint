ef2c894ac6885952a083485d93e600db531f7e8b
Edgar Sioson <siosonel@gmail.com>
2021-11-19 18:33:27 -0600

    Fix the helper scripts for pnet and termdb

--- utils/pnet/do.sh
+++ utils/pnet/do.sh
@@ -9,10 +9,23 @@ set -e
 # get the directory of the currently executing script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "updating the db file"
-
+echo "*** updating the db file ***"
+echo "creating tables ..."
 sqlite3 db < $DIR/../termdb/create.sql
+
+echo "loading data into tables ..."
 sqlite3 db < $DIR/load.sql
+
+echo "updating the terms table"
 node $DIR/setterms.js
+
+echo "updating the ancestry table"
+node $DIR/setterms.js
+
+echo "setting the default subcohort ..."
 sqlite3 db < $DIR/../termdb/set-default-subcohort.sql
+
+echo "setting the included types ..."
 sqlite3 db < $DIR/../termdb/set-included-types.sql
+
+echo "done!"
