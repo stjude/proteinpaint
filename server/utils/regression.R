56171df4c355d80978958537b9b5d1827771393f
Gavriel Matt <gavriel.matt@stjude.org>
2021-11-22 09:45:51 -0600

    use json input into regression.R

--- server/utils/regression.R
+++ server/utils/regression.R
@@ -6,25 +6,20 @@
 # Usage
 ###########
 
-# Usage: Rscript regression.R <stdin> [<regression type> <variable classes> <reference categories> <scaling factors> <formula>] <stdout>
+# Usage: Rscript regression.R < jsonIn > jsonOut
 #
 # Parameters:
-#   - <stdin>: [string] tab-delimited table of regression variables streamed from stdin. First line must be a header (i.e. column names). First column must be the outcome variable. All other columns are independent variables. All variables in the table are included in the regression analysis. Here is an example input where the "score" variable will be treated as the outcome variable:
+#   - jsonIn: json-formatted input of regression analysis streamed from standard input.
 #
-#       score   gender   race    age	    treatment1   treatment2
-#       10.56   Male     White   10-20    1            1
-#       7.24    Female   White   10-20    0            0
-#       12.71   Female   Other   20-30    1            0
-#       16.23   Male     Other   20-30    0            0
-#       ...
-#
-#   - arguments:
-#       - <regression type>: [string] type of regression analysis ("linear" or "logistic"). Linear regression should be used if the outcome is a continuous variable. Logistic regression should be used if the outcome is a categorical variable. Currently, logistic regression can only be performed on binary outcome variables (e.g. "no/yes", "no condition/condition", "0/1").
-#       - <variable classes>: [string] a comma-delimited list of R classes for each variable (e.g. "numeric,integer,factor,factor,factor,numeric"). The number and order of classes should match that of variables in the table. Variables with integer or numeric classes will be treated as continuous variables, while variables with a factor class will be treated as categorical variables.
-#       - <reference categories>: [string] a comma-delimited list of reference categories for each variable (same format as <variable classes>). For categorical variables, specify the desired reference category. For continuous variables, use an 'NA' string.
-#       - <scaling factors>: [string] a comma-delimited list of scaling factors for each variable (same format as <variable classes>). Values of each variable will be divided by the corresponding scaling factor. Use the 'NA' string to indicate that a variable should not be scaled.
-#       - <formula>: [string] a formula for fitting the regression model (e.g. 'outcome ~ id0 + id1 + id0:id1')
-#
+#     jsonIn {}
+#     .type: [string] regression type (e.g. "linear" or "logistic")
+#     .formula: [string] formula of regression model (e.g. "outcome ~ id0 + id1 + id2"). All terms in the formula must be present in .terms{}.
+#     .terms: {}
+#       .id: [string] term id (e.g. "outcome", "id0", "id1", etc.). Must be present in .formula.
+#       .type: [string] termdb term type (e.g. "integer", "float", "categorical", or "condition")
+#       .values: [array] data values
+#       .refGrp: [string (optional, but required for categorical/condition term)] reference value of the term
+#       .scale: [number (optional)] scaling factor of the term. All term values will be divided by this number.
 #   - output: 
 #       - <stdout>: [string] output summary statistics of the regression analysis streamed to stdout. The output will consist of different tables of summary statistics where the tables are delimited by header lines (i.e. lines beginning with "#"). Each header line has the following format: #<name of table>
 
@@ -33,49 +28,44 @@
 # Code
 ###########
 
-#TODO: should we use "|" instead of "," for delimiting values within arguments? Use JSON input for arguments.
-
+######## Data preparation ##########
 
 args <- commandArgs(trailingOnly = T)
-if (length(args) != 5){
-  stop("Usage: Rscript regression.R <stdin> [<regression type> <variable classes> <reference categories> <scaling factors> <formula>] <stdout>")
+if (length(args) != 0){
+  stop("Usage: Rscript regression.R < jsonIn > jsonOut")
 }
-regressionType <- args[1]
-variableClasses <- strsplit(args[2], split = ",")[[1]]
-refCategories <- strsplit(args[3], split = ",")[[1]]
-scalingFactors <- strsplit(args[4], split = ",")[[1]]
-formulaStr <- args[5]
+
+library(jsonlite)
 
 
-#Read in input data and assign variable classes
+# read in json input
 con <- file("stdin","r")
-dat <- read.table(con, header = T, sep = "\t", quote = "", colClasses = variableClasses, check.names = F)
+lst <- fromJSON(con, simplifyDataFrame = F, simplifyMatrix = F)
 
-if(length(variableClasses) != ncol(dat)){
-  stop("Number of variable classes does not match the number of variables")
-}
-if(length(refCategories) != ncol(dat)){
-  stop("Number of reference categories does not match the number of variables")
-}
-if(length(scalingFactors) != ncol(dat)){
-  stop("Number of scaling factors does not match the number of variables")
-}
+# prepare a data table to store term values
+data <- as.data.frame(matrix(data = NA, nrow = length(lst$terms[[1]]$values), ncol = length(lst$terms)))
 
-#Assign the reference categories of categorical variables.
-for(i in 1:ncol(dat)) {
-  if(is.factor(dat[,i])) {
-    dat[,i] <- relevel(dat[,i], ref = refCategories[i])
+# populate the data table
+for (i in 1:length(lst$terms)) {
+  term <- lst$terms[[i]]
+  colnames(data)[i] <- term$id
+  if (term$type == "integer" | term$type == "float") {
+    # numeric terms
+    if ("scale" %in% names(term)) {
+      data[,i] <- term$values/term$scale
+    } else {
+      data[,i] <- term$values
+    }
+  } else if (term$type == "categorical" | term$type == "condition") {
+    # categorical/condition terms
+    data[,i] <- relevel(factor(term$values), ref = term$refGrp)
+  } else {
+    stop(paste0("term type '", term$type, "' is not recognized"))
   }
 }
 
-#Scale those variables that have scaling factors
-for(i in 1:length(scalingFactors)) {
-  if(scalingFactors[i] != "NA") {
-    scalingFactor <- as.numeric(scalingFactors[i])
-    dat[,i] <- dat[,i]/scalingFactor
-  }
-}
 
+######### Regression analysis ##########
 
 # function to build coefficients table
 build_coef_table <- function(res_summ) {
@@ -90,19 +80,18 @@ build_coef_table <- function(res_summ) {
 }
 
 # perform the regression analysis
-outcomeVar <- names(dat)[1]
-if (regressionType == "linear"){
+if (lst$type == "linear"){
   # linear regression
-  if (!is.integer(dat[,outcomeVar]) & !is.numeric(dat[,outcomeVar])){
-    stop("outcome variable needs to be numeric for linear regression")
+  if (!is.numeric(data$outcome)){
+    stop("linear regression requires a numeric outcome variable")
   }
   # fit linear model
-  res <- lm(as.formula(formulaStr), data = dat)
+  res <- lm(as.formula(lst$formula), data = data)
   res_summ <- summary(res)
-  # residuals table
+  # prepare residuals table
   residuals_table <- round(data.frame(as.list(fivenum(res_summ$residuals))), 3)
   colnames(residuals_table) <- c("Minimum","1st quartile","Median","3rd quartile","Maximum")
-  # coefficients table
+  # prepare coefficients table
   coefficients_table <- build_coef_table(res_summ)
   colnames(coefficients_table)[1] <- "Beta"
   # compute confidence intervals of beta values
@@ -110,9 +99,9 @@ if (regressionType == "linear"){
   colnames(ci) <- c("95% CI (low)","95% CI (high)")
   coefficients_table <- cbind(coefficients_table, ci)
   coefficients_table <- coefficients_table[,c(1,5,6,2,3,4)]
-  # type III statistics
+  # prepare type III statistics table
   typeIII_table <- as.data.frame(drop1(res, scope = ~., test = "F"))
-  # other summary stats
+  # prepare other summary stats table
   other_table <- c(
     "Residual standard error" = round(res_summ$sigma, 2),
     "Residual degrees of freedom" = round(res$df.residual, 0),
@@ -121,16 +110,16 @@ if (regressionType == "linear"){
     "F-statistic" = round(unname(res_summ$fstatistic[1]), 2),
     "P-value" = signif(unname(pf(res_summ$fstatistic[1], res_summ$fstatistic[2], res_summ$fstatistic[3], lower.tail = F)), 4)
   )
-} else if (regressionType == "logistic"){
+} else if (lst$type == "logistic"){
   # logistic regression
-  if (!is.factor(dat[,outcomeVar])){
-    stop("outcome variable needs to be a factor for logistic regression")
+  if (!is.factor(data$outcome)){
+    stop("logistic regression requires a factor outcome variable")
   }
-  if (nlevels(dat[,outcomeVar]) != 2){
+  if (nlevels(data$outcome) != 2){
     stop("outcome variable is not binary")
   }
   # fit logistic model
-  res <- glm(as.formula(formulaStr), family=binomial(link='logit'), data = dat)
+  res <- glm(as.formula(lst$formula), family=binomial(link='logit'), data = data)
   res_summ <- summary(res)
   # prepare residuals table
   residuals_table <- round(data.frame(as.list(fivenum(res_summ$deviance.resid))), 3)
@@ -145,9 +134,9 @@ if (regressionType == "linear"){
   colnames(ci) <- c("95% CI (low)","95% CI (high)")
   coefficients_table <- cbind(coefficients_table, ci)
   coefficients_table <- coefficients_table[,c(1,6,7,2,3,4,5)]
-  # type III statistics
+  # prepare type III statistics table
   typeIII_table <- as.data.frame(drop1(res, scope = ~., test = "LRT"))
-  # other summary stats
+  # prepare other summary stats table
   other_table <- c(
     "Dispersion parameter" = res_summ$dispersion,
     "Null deviance" = res_summ$null.deviance,
@@ -162,6 +151,7 @@ if (regressionType == "linear"){
 }
 
 
+######### Formatting regression results
 # reformat the coefficients table
 # round the non-p-value columns to 3 decimal places and the p-value columns to 4 significant digits
 pvalueCol <- grepl("^Pr\\(>", colnames(coefficients_table))
