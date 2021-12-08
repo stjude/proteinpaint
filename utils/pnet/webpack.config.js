df3dd35d33d132b0b8ba2c9ea0e20c7a66e77345
Edgar Sioson <siosonel@gmail.com>
2021-12-07 20:50:54 -0600

    Include the setterms bundling for pnet when deploying to ppr

--- utils/pnet/webpack.config.js
+++ utils/pnet/webpack.config.js
@@ -4,11 +4,11 @@ const path = require('path')
 
 module.exports = function(env = {}) {
 	const config = serverConfigFxn(env)
-	;(config.entry = './setterms.js'),
-		(config.output = {
-			path: path.join(__dirname, './'),
-			filename: './setterms.bundle.js'
-		})
+	config.entry = path.join(__dirname, './setterms.js')
+	config.output = {
+		path: path.join(__dirname, './'),
+		filename: './setterms.bundle.js'
+	}
 	config.externals = [
 		nodeExternals({
 			allowlist: [path.join(__dirname, '../../server/src'), path.join(__dirname, '../../server/shared')]
