5de341e7908bd6deb17d96ceb3df597aa00cfd58
xzhou82 <xzhou1@stjude.org>
2021-12-08 12:17:04 -0600

    fix bug in forest plot rendering, update example

--- client/src/plots/regression.results.js
+++ client/src/plots/regression.results.js
@@ -400,6 +400,7 @@ function setRenderers(self) {
 			numbers2array(i.lst)
 		}
 		values.sort((a, b) => a - b) // ascending
+		console.log(values)
 		// all valid numbers are collected into values[]
 
 		// graph dimension
@@ -466,7 +467,8 @@ function setRenderers(self) {
 			}
 
 			const x = scale(mid)
-			if (x != -Infinity && x != Infinity) {
+			if (Number.isFinite(x)) {
+				// guard against infinity values
 				g.append('circle')
 					.attr('cx', x)
 					.attr('cy', height / 2)
@@ -480,12 +482,14 @@ function setRenderers(self) {
 			}
 			const x1 = scale(cilow),
 				x2 = scale(cihigh)
-			g.append('line')
-				.attr('x1', x1)
-				.attr('y1', height / 2)
-				.attr('x2', x2)
-				.attr('y2', height / 2)
-				.attr('stroke', forestcolor)
+			if (Number.isFinite(x1) && Number.isFinite(x2)) {
+				g.append('line')
+					.attr('x1', x1)
+					.attr('y1', height / 2)
+					.attr('x2', x2)
+					.attr('y2', height / 2)
+					.attr('stroke', forestcolor)
+			}
 		}
 		///////// helpers
 		function numbers2array(lst) {
