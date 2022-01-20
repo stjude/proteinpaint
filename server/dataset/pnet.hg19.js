bfd5bf81194a0371801b8d9769eb07cb1064cd7c
Edgar Sioson <siosonel@gmail.com>
2022-01-20 17:18:02 -0600

    Export a dataset function for PNET so that its jsfile can be shared with other serverconfig dataset entries

--- server/dataset/pnet.hg19.js
+++ server/dataset/pnet.hg19.js
@@ -1,10 +1,19 @@
-module.exports = {
-	isMds: true,
+// allow the reuse of this PNET dataset jsfile for other datasets
+// like MB_meta_analysis by using updateAttr: [] items in the corresponding
+// serverconfig dataset entry and by by exporting a dataset generator function
+// instead of exporting a static object
 
-	cohort: {
-		db: {
-			file: 'files/hg19/pnet/clinical/db'
-		},
-		termdb: {}
+module.exports = function() {
+	// not using the usual 'common' argument, just want a fresh dataset object
+	// to allow sharing the jsfile without having to share the same ds bootstrap object
+	return {
+		isMds: true,
+
+		cohort: {
+			db: {
+				file: 'files/hg19/pnet/clinical/db'
+			},
+			termdb: {}
+		}
 	}
 }
