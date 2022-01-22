ded2831b2bf1b50628f081016bd10cb7ef7a325c
xzhou82 <xzhou1@stjude.org>
2022-01-22 14:00:25 -0600

    show snplst table as first row

--- client/src/common/termsetting.snplst.js
+++ client/src/common/termsetting.snplst.js
@@ -16,8 +16,8 @@ self.term{}
 		// following attributes are computed by validation
 		.snpid
 		.invalid
-		.alleles
-		//.validgtcount
+		.allele2count
+		.gt2count
 self.q{}
 	.alleleType: int
 	.geneticModel: int
@@ -69,13 +69,33 @@ and must click Submit to save
 */
 function makeEditMenu(self, div) {
 	// the ui will create following controls, to be accessed upon clicking Submit button
-	let textarea, select_alleleType, select_geneticModel, select_missingGenotype
+	let snplst_table, textarea, select_alleleType, select_geneticModel, select_missingGenotype
 
-	// table of two columns
-	const tr = div
-		.append('table')
-		.style('margin', '15px')
-		.append('tr')
+	// table has two rows
+	const table = div.append('table').style('margin', '15px')
+
+	// first row has 1 <td>, shows snp list
+	if (self.term && self.term.snps) {
+		// show list of snps as well as status for each: {invalid, }
+
+		const td = table
+			.append('tr')
+			.append('td')
+			.attr('colspan', 2)
+			.style('padding-bottom', '20px')
+		snplst_table = td.append('table')
+		renderSnpEditTable(snplst_table)
+
+		td.append('div')
+			.style('opacity', 0.4)
+			.style('font-size', '.7em')
+			.html(
+				'Note: Click on allele to make it effect allele.</br>#samples is the number of samples with at least one valid genotype'
+			)
+	}
+
+	// second row has 2 <td>
+	const tr = table.append('tr')
 
 	// left column
 	const tdleft = tr
@@ -83,9 +103,7 @@ function makeEditMenu(self, div) {
 		.style('vertical-align', 'top')
 		.style('padding-right', '20px')
 
-	const snp_edit_div = tdleft.append('div').style('display', 'none')
-
-	const textarea_title = tdleft
+	tdleft
 		.append('div')
 		.style('display', 'none')
 		.style('opacity', 0.4)
@@ -93,29 +111,6 @@ function makeEditMenu(self, div) {
 		.style('margin-bottom', '5px')
 		.text('ADDITIONAL SNPs')
 
-	if (self.term && self.term.snps) {
-		// show list of snps as well as status for each: {invalid, alleles, validgtcount}
-		snp_edit_div.style('display', 'block')
-		textarea_title.style('display', 'block')
-		snp_edit_div
-			.append('table')
-			.style('font-size', '.8em')
-			.style('padding', '5px')
-
-		renderSnpEditTable()
-
-		snp_edit_div
-			.append('div')
-			.style('opacity', 0.4)
-			.style('font-size', '.7em')
-			.style('margin-bottom', '10px')
-			.html(
-				'Note: Click on allele to make it effect allele.</br>#samples is the number of samples with at least one valid genotype'
-			)
-	}
-
-	// temporary implementation: textarea can be created when no term;
-	// if there's term, may instead create controls for each snp for setting up interactions etc
 	textarea = tdleft
 		.append('textarea')
 		.attr('rows', 5)
@@ -172,7 +167,6 @@ function makeEditMenu(self, div) {
 
 	if (self.term) {
 		// .term and .q is available on the instance; populate UI with values
-		// textarea.text(snp2text(self))
 		select_alleleType.property('selectedIndex', self.q.alleleType)
 		select_geneticModel.property('selectedIndex', self.q.geneticModel)
 		select_missingGenotype.property('selectedIndex', self.q.missingGenotype)
@@ -199,18 +193,25 @@ function makeEditMenu(self, div) {
 		.on('click', async () => {
 			// parse input text
 			const snps = parseSnpFromText(textarea)
-			if (self.term.snps.length == 0 && snps.length == 0) return window.alert('No valid SNPs')
-			// a snplst term is expected to not have a term.id
-			if (!self.term) self.term = {}
-			if (!self.q) self.q = {}
+			if (self.term) {
+				if (!self.term.snps) self.term.snps = [] // possible if term of a different type was there before?
+				// already have term;
+				// any valid input in textarea are added to existing term
+				for (const s of snps) self.term.snps.push(s)
+			} else {
+				// no term; require valid submission in textarea
+				if (!snps.length) return window.alert('No valid SNPs')
+				// have valid input; create new term
+				self.term = { snps } // term does not have id
+				self.q = {} // q does not have mode
+			}
 			// set term type in case the instance had a different term before
 			self.term.type = 'snplst'
-			self.term.snps = self.term.snps.length ? [...self.term.snps, ...snps] : snps
 			self.term.name = getTermName(self.term.snps)
 			d3event.target.disabled = true
 			d3event.target.innerHTML = 'Validating SNPs...'
 			await validateInput(self)
-			// self.term.id is now cache file name
+			//q.cacheid is set
 			self.q.alleleType = select_alleleType.property('selectedIndex')
 			self.q.geneticModel = select_geneticModel.property('selectedIndex')
 			self.q.missingGenotype = select_missingGenotype.property('selectedIndex')
@@ -219,7 +220,7 @@ function makeEditMenu(self, div) {
 			self.runCallback()
 		})
 
-	function renderSnpEditTable() {
+	function renderSnpEditTable(snplst_table) {
 		// allow to delete or add to this list
 		let rowcount = 0,
 			maxAllelCount = 0,
@@ -227,11 +228,9 @@ function makeEditMenu(self, div) {
 		maxAllelCount = Math.max.apply(Math, self.term.snps.map(s => s.allele2count).map(o => Object.keys(o).length))
 		maxGenotypeCount = Math.max.apply(Math, self.term.snps.map(s => s.gt2count).map(o => Object.keys(o).length))
 
-		const splst_table = snp_edit_div.select('table')
+		snplst_table.selectAll('*').remove()
 
-		splst_table.selectAll('*').remove()
-
-		const title_tr = splst_table.append('tr').style('opacity', 0.4)
+		const title_tr = snplst_table.append('tr').style('opacity', 0.4)
 		const col_titles = [
 			{ title: 'SNPs' },
 			{ title: 'Valid' },
@@ -251,7 +250,7 @@ function makeEditMenu(self, div) {
 
 		// SNPs
 		for (const [i, snp] of self.term.snps.entries()) {
-			const tr = splst_table.append('tr').style('background', rowcount++ % 2 ? '#eee' : 'none')
+			const tr = snplst_table.append('tr').style('background', rowcount++ % 2 ? '#eee' : 'none')
 
 			// col 1: rsid
 			tr.append('td').text(snp.rsid)
@@ -310,7 +309,7 @@ function makeEditMenu(self, div) {
 				.text('Remove')
 				.on('click', () => {
 					self.term.snps = self.term.snps.filter(s => s.rsid !== snp.rsid)
-					renderSnpEditTable()
+					renderSnpEditTable(snplst_table)
 				})
 		}
 	}
