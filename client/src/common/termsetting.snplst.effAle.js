026569f3404719493940bdccc5ef3907e0258768
xzhou82 <xzhou1@stjude.org>
2022-01-26 17:44:30 -0600

    create branch and add snp.effAleUse

--- client/src/common/termsetting.snplst.effAle.js
+++ client/src/common/termsetting.snplst.effAle.js
@@ -1,4 +1,6 @@
 /*
+TODO rename file to termsetting.snplst.shared.js
+
 if the "effect allele" is already set for a snp (by user), return it
 otherwise, compute it based on the alleleType setting (from snplst q{})
 
@@ -43,5 +45,69 @@ export function get_refGenotype(alleleType, geneticModel, snp) {
 		const ref_al = snp.alleles.find(i => i.isRef).allele
 		const ref_gt = ref_al + ',' + ref_al
 		return ref_gt
+	}
+}
+
+/*
+tw: {term{ snps[] }, q{}}
+data: returned by vocab getCategories()
+	such data includes sample and allele summaries for snps based on current filter
+	copy these info to tw.snps
+*/
+export function mayRunSnplstTask(tw, data) {
+	// TODO quick fix!!! run arbitrary logic specific to snplst
+	if (tw.term.type != 'snplst') return
+	if (!Array.isArray(data.snps)) throw 'data.snps[] not array'
+	// note!! tw is modified here and is not written to state, but should be fine
+
+	// delete existing sample summaries from snps, in case when a snp is no longer found in latest cohort due to filtering
+	for (const s of tw.term.snps) {
+		delete s.alleles
+		delete s.gt2count
+	}
+
+	// clear old settings
+	tw.q.snp2effAle = {}
+
+	// copy latest sample summaries to tw.term.snps[]
+	for (const s of data.snps) {
+		// { snpid, alleles, gt2count }
+		// if this snp does not have valid content for alleles and gt2count, then it will not be computable
+		const snp = tw.term.snps.find(i => i.snpid == s.snpid)
+		if (!snp) throw 'snp not found by id'
+		snp.alleles = s.alleles
+		snp.gt2count = s.gt2count
+		// to name it differently than snp.effectAllele, which is user-specified
+		snp.effAleUse = get_effectAllele(tw.q.alleleType, snp)
+		tw.q.snp2effAle[snp.snpid] = snp.effAleUse
+	}
+
+	tw.q.numOfSampleWithAnyValidGT = data.numOfSampleWithAnyValidGT
+
+	// if genetic model choice is "by genotype", need to assign refGrp to each snp
+	// create q.snp2refGrp{}
+	if (tw.q.geneticModel == 3) {
+		tw.q.snp2refGrp = {}
+		for (const s of tw.term.snps) {
+			if (s.invalid || !s.gt2count) continue
+			// FIXME "invalid" flag is missing for things supposed to be invalid
+
+			// from gt2count, find the gt that does not contain effect allele as ref group
+			let refGT
+			for (const gt in s.gt2count) {
+				if (!gt.includes(s.effAleUse)) {
+					refGT = gt
+					break
+				}
+			}
+			if (!refGT) {
+				// no gt that doesn't contain effect allele
+				// FIXME quick fix to use a RANDOM gt so it won't break
+				refGT = Object.keys(s.gt2count)[0]
+			}
+			tw.q.snp2refGrp[s.snpid] = refGT
+		}
+	} else {
+		delete tw.q.snp2refGrp
 	}
 }
