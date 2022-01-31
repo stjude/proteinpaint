2d5c8649e2fcce70df851e0a4cad4d77ef9efbc9
Gavriel Matt <gavriel.matt@stjude.org>
2022-01-31 12:05:21 -0600

    modified JSON input/output structure into cuminc.R

--- server/utils/cuminc.R
+++ server/utils/cuminc.R
@@ -1,60 +1,75 @@
-################################
-# Cumulative incidence analysis
-################################
+#################################
+# Cumulative incidence analysis #
+#################################
 
-###########
-# Usage
-###########
+#########
+# Usage #
+#########
 
 # Usage: Rscript cuminc.R < jsonIn > jsonOut
 
 # Parameters:
-#   - jsonIn: [json string] input data streamed from standard input. The json string should not contain any newline characters, because newline characters are treated as separators of json records (trailing newline characters are ok).
-#     
-#     JSON input specifications:
-#     {
-#       "times": [array] times to events
-#       "events": [array] event codes (e.g. 0, 1). An event code of 0 indicates  a censored event. An event code of 1 indicates an event of interest.
-#     }
+#   - jsonIn: [json string] input data streamed from standard input. Each element of array is time/event/series data for a given sample.
+#
+#     [
+#       {
+#         time: [number] time to event
+#         event: [number] event code (0 = censored, 1 = event).
+#         series: [string] series ID. If empty string, then "series" in output json will be "1".
+#       }
+#     ]
 #
 #   - jsonOut: [json string] cumulative incidence results streamed to standard out.
 #
-#     JSON output specifications:
 #     {
-#       "time": [array] times where estimates are computed
-#       "est": [array] estimated cumulative incidence values
-#       "var": [array] variances of cumulative incidence values
-#       "low": [array] 95% confidence intervals - lower bounds
-#       "up": [array] 95% confidence intervals - upper bounds
+#       "estimates": {
+#         <series>: [
+#           {
+#             "time": [array] times where estimates are computed
+#             "est": [array] estimated cumulative incidence values
+#             "var": [array] variances of cumulative incidence values
+#             "low": [array] 95% confidence intervals - lower bounds
+#             "up": [array] 95% confidence intervals - upper bounds
+#           }  
+#         ],
+#       "tests": [
+#          {
+#            "stat": [number] test statistic
+#            "pv": [number] p-value
+#            "df": [number] degrees of freedom
+#          }
+#        ]
+#       }
 #     }
 
 
-###########
-# Code
-###########
+########
+# Code #
+########
 
 library(jsonlite)
 suppressPackageStartupMessages(library(cmprsk))
 
 # data preparation
 con <- file("stdin","r")
-lst <- stream_in(con, verbose = F, simplifyVector = T, simplifyDataFrame = F, simplifyMatrix = F)
-if (length(lst) != 1) stop("input json must have only 1 record")
-lst <- lst[[1]]
-lst$events <- as.factor(lst$events)
+dat <- stream_in(con, verbose = F)
+dat$event <- as.factor(dat$event)
+dat$series <- as.factor(dat$series)
 
 # compute cumulative incidence
-res <- cuminc(ftime = lst$times, fstatus = lst$events, cencode = 0)
-
-# extract cumulative incidence of event of interest
-out <- res[[1]]
-
-# 95% confidence intervals
-low <- out$est - (1.96 * sqrt(out$var))
-low[low < 0] <- 0
-up <- out$est + (1.96 * sqrt(out$var))
-out[["low"]] <- low
-out[["up"]] <- up
+out <- list()
+res <- cuminc(ftime = dat$time, fstatus = dat$event, group = dat$series, cencode = 0)
+out[["estimates"]] <- list()
+for (series in levels(dat$series)) {
+  seriesRes <- as.data.frame(res[[paste(series,1)]])
+  low <- seriesRes$est - (1.96 * sqrt(seriesRes$var))
+  low[low < 0] <- 0
+  up <- seriesRes$est + (1.96 * sqrt(seriesRes$var))
+  seriesRes["low"] <- low
+  seriesRes["up"] <- up
+  out[["estimates"]][[series]] <- seriesRes
+}
+if ("Tests" %in% names(res)) out[["tests"]] <- as.data.frame(res$Tests)
 
 # output results in json format
 cat(toJSON(out, digits = NA, na = "string"), file = "", sep = "")
