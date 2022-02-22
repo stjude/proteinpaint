67a65153bf8303c1e5eaab893187e658a6c8f91c
Edgar Sioson <siosonel@gmail.com>
2022-02-22 13:36:40 -0600

    more tests for dictui parsing

--- client/src/test/dictionary.ui.spec.js
+++ client/src/test/dictionary.ui.spec.js
@@ -36,7 +36,7 @@ tape('levels before name+note, no gaps', function(test) {
 	].join('\n')
 
 	const holder = getHolder()
-	const results = parseTabDelimitedData(holder, tsv) //; console.log(JSON.stringify(results.terms))
+	const results = parseTabDelimitedData(holder, tsv) //console.log(JSON.stringify(results.terms))
 	const expected = [
 		{
 			id: 'A1a',
@@ -149,6 +149,65 @@ tape('levels after name+note, with gap', function(test) {
 	test.end()
 })
 
+tape('empty variable name', function(test) {
+	test.timeoutAfter(100)
+	const tsv = [
+		`level_1\tlevel_2\tlevel_3\tvariable name\tvariable note`,
+		`A\tA.1\tA.1.a\t\t1=Yes; 0=No`,
+		`A\tA.1\tA.1.b\tA1b\t1=Yes; 0=No`,
+		`A\tA.2\tA.2.a\tA2a\t1=Treated; 0=Not treated`,
+		`B\tB.1\tB.1.a\tB1a\t1=Treated; 0=Not treated`
+	].join('\n')
+
+	const holder = getHolder()
+	const results = parseTabDelimitedData(holder, tsv) //; console.log(JSON.stringify(results.terms))
+	const expected = [
+		{
+			id: 'A.1.a',
+			name: 'A.1.a',
+			type: 'categorical',
+			values: { '0': { label: 'No' }, '1': { label: 'Yes' } },
+			groupsetting: { inuse: false },
+			isleaf: true,
+			parent_id: 'A.1'
+		},
+		{
+			id: 'A1b',
+			name: 'A.1.b',
+			type: 'categorical',
+			values: { '0': { label: 'No' }, '1': { label: 'Yes' } },
+			groupsetting: { inuse: false },
+			isleaf: true,
+			parent_id: 'A.1'
+		},
+		{
+			id: 'A2a',
+			name: 'A.2.a',
+			type: 'categorical',
+			values: { '0': { label: 'Not treated' }, '1': { label: 'Treated' } },
+			groupsetting: { inuse: false },
+			isleaf: true,
+			parent_id: 'A.2'
+		},
+		{
+			id: 'B1a',
+			name: 'B.1.a',
+			type: 'categorical',
+			values: { '0': { label: 'Not treated' }, '1': { label: 'Treated' } },
+			groupsetting: { inuse: false },
+			isleaf: true,
+			parent_id: 'B.1'
+		},
+		{ id: 'A', name: 'A', isleaf: false, parent_id: null },
+		{ id: 'A.1', name: 'A.1', isleaf: false, parent_id: 'A' },
+		{ id: 'A.2', name: 'A.2', isleaf: false, parent_id: 'A' },
+		{ id: 'B', name: 'B', isleaf: false, parent_id: null },
+		{ id: 'B.1', name: 'B.1', isleaf: false, parent_id: 'B' }
+	]
+	test.deepEqual(results.terms, expected, 'should use the variable name as term.id')
+	test.end()
+})
+
 tape('repeated level names, same line', function(test) {
 	test.timeoutAfter(100)
 	const tsv = [
@@ -161,6 +220,28 @@ tape('repeated level names, same line', function(test) {
 
 	const holder = getHolder()
 	const message = 'should display an error for repeated level names in the same line'
+	try {
+		const holder = getHolder()
+		const results = parseTabDelimitedData(holder, tsv)
+		test.equal(holder.selectAll('.sja_errorbar').size(), 1, message)
+	} catch (e) {
+		test.equal(holder.selectAll('.sja_errorbar').size(), 1, message)
+	}
+	test.end()
+})
+
+tape('dash between levels', function(test) {
+	test.timeoutAfter(100)
+	const tsv = [
+		`level_1\tlevel_2\tlevel_3\tvariable name\tvariable note`,
+		`A\tA.1\tA.1.a\tA1a\t1=Yes; 0=No`,
+		`A\tA.1\tA.1.b\tA1b\t1=Yes; 0=No`,
+		`A\t-\tA.2.a\tA2a\t1=Treated; 0=Not treated`,
+		`B\tB.1\tB.1.a\tB1a\t1=Treated; 0=Not treated`
+	].join('\n')
+
+	const holder = getHolder()
+	const message = `should display an error for a '-' between level names`
 	try {
 		const holder = getHolder()
 		const results = parseTabDelimitedData(holder, tsv)
