db689619438ca8e0102eb674c195c3bbe040ab42
xzhou82 <xzhou1@stjude.org>
2022-03-31 21:03:05 -0500

    cuminc default break at 3 with fillTW context

--- client/src/plots/cuminc.js
+++ client/src/plots/cuminc.js
@@ -620,7 +620,7 @@ function setInteractivity(self) {
 export async function getPlotConfig(opts, app) {
 	if (!opts.term) throw 'cuminc: opts.term{} missing'
 	try {
-		await fillTermWrapper(opts.term, app.vocabApi, { conditionMode: 'binary' })
+		await fillTermWrapper(opts.term, app.vocabApi, { conditionMode: 'binary', condition_break3: true })
 		if (opts.term2) await fillTermWrapper(opts.term2, app.vocabApi)
 		if (opts.term0) await fillTermWrapper(opts.term0, app.vocabApi)
 	} catch (e) {
