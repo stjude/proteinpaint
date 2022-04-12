bcbff2dc4e2b8a8e4c35c9efb124dbab7afa1a37
Edgar Sioson <siosonel@gmail.com>
2022-04-11 22:11:29 -0500

    improved git repo sync script

--- client/rx/extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WORKDIR=
-if [[ "${pwd}" =~ ^.client\/rx ]]; then
-	echo "this script must be called from the [proteinpaint]/client/rx directory"
-	exit 1
-fi
-
-rsync -av ./ ../../../rx
