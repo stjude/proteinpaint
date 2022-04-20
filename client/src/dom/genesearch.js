d28999851673adf5873d786776cb05d87ad0cc05
xzhou82 <xzhou1@stjude.org>
2022-04-20 12:02:33 -0500

    start to prototype hgvs parsing

--- client/src/dom/genesearch.js
+++ client/src/dom/genesearch.js
@@ -6,11 +6,13 @@ import { invalidcoord, string2pos } from '../coord'
 /*
 
 TODO
-1. allow only searching by gene name, disable coorinput
+-- allow only searching by gene name, disable coord/variant/snp parsing
    this allows to replace gene_searchbox() in client/src/gene.js
    use only shallow (not "deep") query to find gene symbol
    do not map gene to coord
-2. dedup code with block.js
+-- allow to hide searchStat dom
+-- dedup code with block.js
+-- dedup code with app header
 
 ***********************************
 function argument object {}
@@ -34,37 +36,8 @@ function argument object {}
 
 .allowVariant: true
 	optional
-	if true, allow to enter chr.pos.ref.alt
+	if true, allow to enter chr.pos.ref.alt or hgvs (see next section)
 	otherwise, only allow chr:start-stop
-	support hgvs notations for substitution/insertion/deletion
-		only supports "g." for linear genomic reference https://varnomen.hgvs.org/bg-material/refseq/
-		others o. m. c. n. are not supported
-
-		entered positions are 1-based, parse to 0-based
-
-		snv
-			given chr14:g.104780214C>T
-			parse to chr14.104780214.C.T
-
-		mnv
-			given chr2:g.119955155_119955159delinsTTTTT
-			parse to chr2.119955155.AGCTG.TTTTT
-
-		deletion
-			chr17:g.7673802delCGCACCTCAAAGCTGTTC
-			parse to chr17.7673802.CGCACCTCAAAGCTGTTC.-
-
-			chr?:g.33344591del
-			parse to chr?.33344591.A.-
-
-			if allele is present after "del", will use the allele
-			otherwise decide by position/range
-			https://varnomen.hgvs.org/recommendations/DNA/variant/deletion/
-
-		insertion
-			chr5:g.171410539_171410540insTCTG
-			parse to chr5.171410539.-.TCTG
-
 
 .callback()
 	optional
@@ -84,6 +57,39 @@ result object returned by the function
 .ref
 .alt
 	"pos/ref/alt" are included when entered a variant
+
+***********************************
+partial support of hgvs https://varnomen.hgvs.org/recommendations/DNA/
+
+limited to substitution/insertion/deletion on "g."
+other references (o. m. c. n.) are not supported
+
+entered positions are 1-based, parse to 0-based
+
+snv
+	given chr14:g.104780214C>T
+	parse to chr14.104780214.C.T
+
+mnv
+	given chr2:g.119955155_119955159delinsTTTTT
+	parse to chr2.119955155.AGCTG.TTTTT
+	must query ref allele
+
+deletion
+	chr17:g.7673802delCGCACCTCAAAGCTGTTC
+	parse to chr17.7673802.CGCACCTCAAAGCTGTTC.-
+
+	chr?:g.33344591del
+	parse to chr?.33344591.A.-
+	must query ref allele
+
+	if allele is present after "del", will use the allele
+	otherwise decide by position/range
+	https://varnomen.hgvs.org/recommendations/DNA/variant/deletion/
+
+insertion
+	chr5:g.171410539_171410540insTCTG
+	parse to chr5.171410539.-.TCTG
 */
 export function addGeneSearchbox(arg) {
 	const tip = arg.tip,
@@ -115,7 +121,7 @@ export function addGeneSearchbox(arg) {
 				// then no need to match with gene/snp
 
 				if (arg.allowVariant) {
-					const variant = string2variant(v, arg.genome)
+					const variant = await string2variant(v, arg.genome)
 					if (variant) {
 						getResult(variant, 'Valid variant')
 						return
@@ -174,7 +180,7 @@ export function addGeneSearchbox(arg) {
 		if (!v) return
 
 		if (arg.allowVariant) {
-			const variant = string2variant(v, arg.genome)
+			const variant = await string2variant(v, arg.genome)
 			if (variant) {
 				// is valid variant
 				// do not write to result
@@ -341,7 +347,22 @@ export function addGeneSearchbox(arg) {
 	return result //searchbox
 }
 
-function string2variant(v, genome) {
+async function string2variant(v, genome) {
+	// try to parse as simple variant
+	const variant = string2simpleVariant(v, genome)
+	if (variant) {
+		// is a simple variant, return
+		return variant
+	}
+	// try to parse hgvs; if successful, return variant; otherwise return null as not a variant
+	return await string2hgvs(v, genome)
+}
+
+function string2simpleVariant(v, genome) {
+	// try to parse as chr.pos.ref.alt first
+	// this format has conflict with hgvs e.g. NG_012232.1(NM_004006.2):c.93+1G>T
+	// which is in fact not currently supported by this code
+	// if it fails, return false and parse as hgvs next
 	const tmp = v.split('.')
 	if (tmp.length != 4) return
 	const chr = tmp[0]
@@ -356,3 +377,87 @@ function string2variant(v, genome) {
 		alt: tmp[3]
 	}
 }
+
+async function string2hgvs(v, genome) {
+	const tmp = v.split(':g.')
+	if (tmp.length != 2) return
+	const chr = tmp[0]
+	// test with 'delins', 'del', 'ins'
+	if (tmp[1].includes('delins')) {
+		return await hgvs_delins(chr, tmp[1], genome)
+	}
+	if (tmp[1].includes('del')) {
+		return await hgvs_del(chr, tmp[1], genome)
+	}
+	if (tmp[1].includes('ins')) {
+		return hgvs_ins(chr, tmp[1])
+	}
+	return hgvs_snv(chr, tmp[1])
+}
+
+function hgvs_snv(chr, v) {}
+
+function hgvs_ins(chr, v) {
+	//  chr5:g.171410539_171410540insTCTG
+	const [tmppos, altAllele] = v.split('ins')
+	if (!altAllele) return
+	const pos = Number(tmppos.split('_')[0])
+	if (!Number.isInteger(pos)) return
+	return {
+		isVariant: true,
+		chr,
+		pos,
+		ref: '-',
+		alt: altAllele
+	}
+}
+
+async function hgvs_del(chr, v, genome) {
+	//chr17:g.7673802delCGCACCTCAAAGCTGTTC
+	const [tmppos, refAllele] = v.split('del')
+	if (refAllele) {
+		// deleted ref nt is given, simply parse position and done
+		const pos = Number(tmppos.split('_')[0])
+		if (!Number.isInteger(pos)) return
+		return {
+			isVariant: true,
+			chr,
+			pos,
+			ref: refAllele,
+			alt: '-'
+		}
+	}
+	// deleted ref nt is not given. this info is coded in tmppos, either "333" or "333_334"
+	const [t1, t2] = tmppos.split('_')
+	const start = Number(t1)
+	const stop = t2 ? Number(t2) : start
+	if (!Number.isInteger(start) || !Number.isInteger(stop)) return
+	const refAllele2 = await getRefAllele(start - 1, stop, genome)
+	return {
+		isVariant: true,
+		chr,
+		pos,
+		ref: refAllele2,
+		alt: '-'
+	}
+}
+
+async function hgvs_delins(chr, v, genome) {
+	// chr2:g.119955155_119955159delinsTTTTT
+	const [tmppos, altAllele] = v.split('delins')
+	// must contain altAllele
+	if (!altAllele) return
+	const pos = Number(tmppos.split('_')[0])
+	if (!Number.isInteger(pos)) return
+	// request ref allele
+	const refAllele = await getRefAllele(start, stop, genome)
+	return {
+		isVariant: true,
+		chr,
+		pos,
+		ref: refAllele,
+		alt: altAllele
+	}
+}
+
+async function getRefAllele(start, stop, genome) {}
