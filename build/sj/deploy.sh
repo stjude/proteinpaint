142656d3b92df078ff04ccddfd38d1cd96edb9c9
Edgar Sioson <siosonel@gmail.com>
2022-06-30 17:43:46 -0500

    cohort.db.refresh may require shared code

--- build/sj/deploy.sh
+++ build/sj/deploy.sh
@@ -210,6 +210,8 @@ else
 	mv server/utils $APP/
 	mv server/cards $APP/
 	mv server/src/serverconfig.js $APP/src
+	mv shared $APP/
+	# !!! TODO: old version of cohort.db.refresh will not be supported moving forward
 	if [[ "$SUBDOMAIN" == "ppr" ]]; then
 		# may need to support cohort.db.refresh,
 		# as set via serverconfig dataset updateAttr
