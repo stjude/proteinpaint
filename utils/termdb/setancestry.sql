7b8125a90a7122accfac996bfa10629ad1aba43d
Edgar Sioson <siosonel@gmail.com>
2022-07-02 11:53:42 -0500

    fix the setancestry script

--- utils/termdb/setancestry.sql
+++ utils/termdb/setancestry.sql
@@ -1,4 +1,15 @@
+delete from ancestry;
+
 insert into ancestry (term_id, ancestor_id)
-select id, parent_id
-from terms
-where parent_id is not null;
+with recursive a(id, ancestor_id) as (
+	select id, parent_id
+	from terms
+	where parent_id is not null and parent_id != '*' -- and id='Cardiac dysrhythmia' -- for testing
+	union all
+	select a.id, t.parent_id
+	from terms t
+	join a on a.ancestor_id = t.id
+	where t.parent_id is not null and parent_id != '*'
+)
+select * 
+from a;
