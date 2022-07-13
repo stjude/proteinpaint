df47ed7b0346ec678a9cf635ee5cf65e2dc21b99
xzhou82 <xzhou1@stjude.org>
2022-07-13 13:40:30 -0500

    explain design and usage of this ds

--- server/dataset/termdb.test.js
+++ server/dataset/termdb.test.js
@@ -2,6 +2,26 @@ const serverconfig = require('../src/serverconfig.js')
 const fs = require('fs')
 const path = require('path')
 
+/*
+this will automatically create path under tp/ if missing,
+and copy over the sqlite db file from repo to tp/ path
+
+reason:
+- db2 file is anonymized and unindentifiable
+- allow continuous integration test (future todo)
+- ensure TermdbTest/db2 to be fully static and recoverable, to ensure tests work as expected
+
+to enable this dataset on your pp instance, have this entry in hg38 datasets array of your "serverconfig.json":
+
+ { "name": "TermdbTest", "jsfile": "./dataset/termdb.test.js" }
+
+test with survival plot:
+
+http://localhost:3000/?noheader=1&mass={"dslabel":"TermdbTest","genome":"hg38","plots":[{"chartType":"survival","term2":{"id":"diaggrp"},"term":{"id":"efs"}}]}
+
+(this ensures better-sqlite3 and R works on your pp instance)
+*/
+
 const ds = {
 	isMds: true,
 	cohort: {
@@ -18,10 +38,6 @@ const ds = {
 	}
 }
 
-/* reason of copying db2 to server/test/testdata/:
-- allow continuous integration test
-- ensure TermdbTest/db2 to be fully static and recoverable, to ensure tests work as expected
-*/
 const datadir = path.join(serverconfig.tpmasterdir, 'files/hg38/TermdbTest')
 if (!fs.existsSync(datadir)) fs.mkdirSync(datadir) // create missing path
 
