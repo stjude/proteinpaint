758fef6327f60846fa0369604b0daef26223eeb5
Paul <robinpaul85@gmail.com>
2022-08-02 23:26:19 -0500

    Switched to bigtools v0.1.11

--- server/utils/rust/src/bigwig.rs
+++ server/utils/rust/src/bigwig.rs
@@ -20,7 +20,7 @@
  cd .. && cargo build --release && time echo https://proteinpaint.stjude.org/ppdemo/hg19/bigwig/temp.bw,chr17,0,81195210,1140 | target/release/bigwig
  ~/proteinpaint/server/utils/bigWigSummary /Users/rpaul1/proteinpaint/hg19/PCGP/DNA/cov-wgs/SJOS016_D.bw chr17 0 81195210 1140
 
- cd .. && cargo build --release && time echo http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeMapability/wgEncodeCrgMapabilityAlign100mer.bigWig,chr17,39963265,41231940,800 | target/release/bigwig (not working)
+ cd .. && cargo build --release && time echo http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeMapability/wgEncodeCrgMapabilityAlign100mer.bigWig,chr17,0,81195210,1140 | target/release/bigwig (not working)
  ~/proteinpaint/server/utils/bigWigSummary http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeMapability/wgEncodeCrgMapabilityAlign100mer.bigWig chr17 6074169 9086266 1140
 
 */
@@ -49,7 +49,6 @@ Notes:
 use bigtools::bigwig::ZoomHeader;
 use bigtools::bigwigread::BigWigRead;
 use bigtools::utils::file::remote_file::RemoteFile;
-use math::round;
 use std::env;
 use std::io;
 
@@ -82,26 +81,31 @@ fn main() {
     //println!("difference:{}", difference);
     //println!("datapoints:{}", datapoints);
 
-    let mut file_found = false; // Flag to check if the bigwig file was found. If not found, only "File not found" message is displayed.  false: not found, true: found
-
     //println!("datapoints_list length:{:?}", datapoints_list);
 
     let mut is_weburl: bool = false;
-    let mut zoom_headers = Vec::<ZoomHeader>::new();
     if bigwig_file_url.starts_with("http://") == true
         || bigwig_file_url.starts_with("https://") == true
         || bigwig_file_url.starts_with("www.") == true
     {
         // Its a web URL
         let remote_file = RemoteFile::new(&bigwig_file_url);
-        let remote_file2 = remote_file.clone();
         let reader = BigWigRead::from(remote_file);
         match reader {
-            Ok(_) => {
-                file_found = true;
+            Ok(reader) => {
                 is_weburl = true;
-                let reader = BigWigRead::from(remote_file2).unwrap();
-                zoom_headers = reader.info.zoom_headers;
+                let zoom_headers = reader.info.zoom_headers.clone();
+                calculate_datapoints(
+                    zoom_headers,
+                    reader,
+                    &bigwig_file_url,
+                    is_weburl,
+                    datapoints,
+                    difference,
+                    chrom,
+                    start_pos as f64,
+                    stop_pos as f64,
+                );
             }
             Err(_) => {
                 println!("File not found");
@@ -113,15 +117,26 @@ fn main() {
         //let a = &reader.info.zoom_headers;
         match reader {
             Ok(_) => {
-                file_found = true;
                 let reader = BigWigRead::from_file_and_attach(&bigwig_file_url).unwrap();
-                zoom_headers = reader.info.zoom_headers;
+                let zoom_headers = reader.info.zoom_headers.clone();
+                calculate_datapoints(
+                    zoom_headers,
+                    reader,
+                    &bigwig_file_url,
+                    is_weburl,
+                    datapoints,
+                    difference,
+                    chrom,
+                    start_pos as f64,
+                    stop_pos as f64,
+                );
             }
             Err(_) => {
                 println!("File not found");
             }
         }
     }
+    /*
     //println!("zoom_headers:{:?}", zoom_headers);
 
     if file_found == true {
@@ -139,6 +154,7 @@ fn main() {
             stop_pos as f64,
         );
     }
+    */
 }
 
 fn determine_max(n1: f64, n2: f64) -> f64 {
@@ -157,12 +173,13 @@ fn determine_min(n1: f64, n2: f64) -> f64 {
     }
 }
 
+#[allow(dead_code)]
 fn calculate_appropriate_zoom_level(zoom_headers: Vec<ZoomHeader>, difference: f64) -> Option<u32> {
     let mut reduction_levels = Vec::<u32>::new();
     let mut closest_level = Option::<u32>::None; // Zoom level will be none at base-pair resolution
     let mut unity_added = false;
-    let max_entries_parsed_limit = 10000; // Maximum number of entries that should be parsed from bigwig file. A very high number will lead to better accuracy as this will lead to selection of a lower zoom level. In contrast, a lower value will decrease run time at the cost of accuracy.
-                                          // Parsing out various zoom levels from bigwig file
+    let max_entries_parsed_limit = 100000; // Maximum number of entries that should be parsed from bigwig file. A very high number will lead to better accuracy as this will lead to selection of a lower zoom level. In contrast, a lower value will decrease run time at the cost of accuracy.
+                                           // Parsing out various zoom levels from bigwig file
     for reduction_level in zoom_headers
         .into_iter()
         .map(|entry| (entry.reduction_level))
@@ -178,7 +195,7 @@ fn calculate_appropriate_zoom_level(zoom_headers: Vec<ZoomHeader>, difference: f
     //println!("reduction_levels:{:?}", reduction_levels);
     for level in reduction_levels {
         closest_level = Some(level);
-        if round::floor(difference as f64 / level as f64, 0) < max_entries_parsed_limit as f64 {
+        if (difference as f64 / level as f64).floor() < max_entries_parsed_limit as f64 {
             break;
         }
     }
@@ -189,14 +206,13 @@ fn calculate_appropriate_zoom_level(zoom_headers: Vec<ZoomHeader>, difference: f
     closest_level
 }
 
-#[allow(dead_code)]
 fn calculate_appropriate_zoom_level_ucsc(
     zoom_headers: Vec<ZoomHeader>,
     exact_offset: f64,
 ) -> Option<u32> {
     let mut reduction_levels = Vec::<u32>::new();
     let mut closest_level = Option::<u32>::None; // Zoom level will be none at base-pair resolution
-    let desired_reduction: u32 = round::floor((exact_offset as f64) / 2.0, 0) as u32;
+    let desired_reduction: u32 = ((exact_offset as f64) / 2.0).floor() as u32;
     let mut unity_added = false;
     if desired_reduction > 1 {
         // Parsing out various zoom levels from bigwig file
@@ -212,7 +228,7 @@ fn calculate_appropriate_zoom_level_ucsc(
             unity_added = true;
         }
         reduction_levels.reverse();
-        //println!("reduction_levels:{:?}", reduction_levels);
+        println!("reduction_levels:{:?}", reduction_levels);
         let mut closest_diff: u64 = 18446744073709551615; // Highest number allowed by u64 type
         for level in reduction_levels {
             let diff: u64 = (desired_reduction as i64 - level as i64).abs() as u64;
@@ -228,12 +244,16 @@ fn calculate_appropriate_zoom_level_ucsc(
     if closest_level == Some(1) && unity_added == true {
         closest_level = None;
     }
-    //println!("closest_level:{:?}", closest_level);
+    println!("closest_level:{:?}", closest_level);
     closest_level
 }
 
-fn calculate_datapoints(
+fn calculate_datapoints<
+    R: bigtools::utils::file::seekableread::Reopen<S>,
+    S: bigtools::utils::file::seekableread::SeekableRead,
+>(
     zoom_headers: Vec<ZoomHeader>,
+    mut reader: BigWigRead<R, S>,
     bigwig_file_url: &String,
     is_weburl: bool,
     datapoints: u32,
@@ -248,8 +268,8 @@ fn calculate_datapoints(
     let mut datapoints_sum = vec![0.0 as f64; datapoints as usize]; // Sum of all values within a region
     let mut datapoints_num = vec![0 as f64; datapoints as usize]; // Number of all values within a region
 
-    let zoom_level = calculate_appropriate_zoom_level(zoom_headers, difference);
-    //let zoom_level = calculate_appropriate_zoom_level_ucsc(zoom_headers, exact_offset);
+    //let zoom_level = calculate_appropriate_zoom_level(zoom_headers, difference);
+    let zoom_level = calculate_appropriate_zoom_level_ucsc(zoom_headers, exact_offset);
 
     let mut current_pos: f64 = start_pos as f64; // Initializing current_pos to start position
     for _i in 0..datapoints {
@@ -261,14 +281,14 @@ fn calculate_datapoints(
 
     if is_weburl == true {
         // Read from a web URL
-        let remote_file = RemoteFile::new(&bigwig_file_url);
-        let mut reader = BigWigRead::from(remote_file).unwrap();
         match zoom_level {
             Some(level) => {
                 // Using some zoom level
+                let pre = std::time::Instant::now();
                 let bigwig_output = reader
                     .get_zoom_interval(&chrom, start_pos as u32, stop_pos as u32, level)
                     .unwrap();
+                dbg!(pre.elapsed());
                 let mut i = 0;
                 let mut start_region = datapoints_list[i];
                 let mut end_region = datapoints_list[i + 1];
@@ -507,10 +527,10 @@ fn calculate_datapoints(
                 for entry in bigwig_output {
                     match entry {
                         Ok(v) => {
-                            //println!(
-                            //    "start,end,sum,bases_covered:{},{},{},{}",
-                            //    v.start, v.end, v.summary.sum, v.summary.bases_covered
-                            //);
+                            println!(
+                                "start,end,sum,bases_covered:{},{},{},{}",
+                                v.start, v.end, v.summary.sum, v.summary.bases_covered
+                            );
 
                             if v.start == v.end {
                                 continue;
