aa9c706ddd4c5e78b935178afdc15ef26e4ebd55
Edgar Sioson <siosonel@gmail.com>
2022-08-10 18:15:46 -0500

    update the Docker build to use @stjude/proteinpaint-rust

--- build/full/pack.sh
+++ build/full/pack.sh
@@ -10,6 +10,10 @@ npx rollup -c ./client/rollup.config.js
 
 cd rust
 npm pack
+RUSTPKGVER="$(grep version package.json | sed 's/.*"version": "\(.*\)".*/\1/')"
+cd ../server
+echo -e "\nInstalling @stjude/proteinpaint-rust ...\n"
+npm install rust/stjude-proteinpaint-rust-$RUSTPKGVER.tgz
 cd ..
 
 mv package.json package.json.bak
