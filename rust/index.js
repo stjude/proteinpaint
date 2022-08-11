c7ee87d756f13c7259fac8dfef9b0984a2343769
Edgar Sioson <siosonel@gmail.com>
2022-08-11 12:16:45 -0500

    More descriptive messages when catching and emitting run_rust() errors

--- rust/index.js
+++ rust/index.js
@@ -8,21 +8,35 @@ exports.run_rust = function(binfile, input_data) {
 		const ps = spawn(binpath)
 		const stdout = []
 		const stderr = []
-		Readable.from(input_data).pipe(ps.stdin)
+
+		try {
+			Readable.from(input_data).pipe(ps.stdin)
+		} catch (error) {
+			ps.kill()
+			let errmsg = error
+			if (stderr.length) errmsg += `killed run_rust('${binfile}'), stderr: ${stderr.join('').trim()}`
+			reject(errmsg)
+		}
+
 		ps.stdout.on('data', data => stdout.push(data))
 		ps.stderr.on('data', data => stderr.push(data))
 		ps.on('error', err => {
-			console.log('stderr:', stderr)
+			if (stderr.length) console.log(`run_rust('${binfile}') ps.on('error') stderr:`, stderr)
 			reject(err)
 		})
 		ps.on('close', code => {
 			if (code !== 0) reject(`spawned '${binfile}' exited with a non-zero status and this stderr:\n${stderr.join('')}`)
-			else if (stdout.toString().includes('Cannot read bigWig file') == true) {
+			else if (stderr.length) {
+				// handle rust stderr
+				const err = stderr.join('').trim()
+				const errmsg = `run_rust('${binfile}') emitted standard error\n stderr: ${err}`
+				reject(errmsg)
+			} else if (stdout.join('').includes('Cannot read bigWig file') == true) {
 				// When bigfile is not found, the promise should be rejected with message given below
-				reject(stdout.toString())
+				reject(stdout.join(''))
 			} else {
 				//console.log("stdout:",stdout)
-				resolve(stdout.join('').toString())
+				resolve(stdout.join(''))
 			}
 		})
 	})
