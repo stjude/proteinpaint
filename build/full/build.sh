57b19dffc8e13df081e8819604c8ce21f25ce55a
Airen Zaldivar <airenzp@gmail.com>
2022-08-22 12:33:54 -0500

    replacing arg name from platform to architecture and using -a instead of -p

--- build/full/build.sh
+++ build/full/build.sh
@@ -7,18 +7,18 @@ set -e
 ###############
 
 USAGE="Usage:
-./build/full/build.sh [-r] [-b] [-c] [-p]
+./build/full/build.sh [-r] [-b] [-c] [-a]
 
 	-r REV: git revision to checkout, if empty will use the current code state
 	-b BUILDARGS: build variables to pass to the Dockerfile that are not persisted to the built image
 	-c CROSSENV: cross-env options that used prior to npm install
-	-p: platform that should be used to compile native packages
+	-a: architecture that should be used to compile native packages
 "
 REV=latest
 BUILDARGS=""
 CROSSENV=""
-PLATFORM="x86_64"
-while getopts "r:b:c:h:p:" opt; do
+ARCH="x86_64"
+while getopts "r:b:c:h:a:" opt; do
 	case "${opt}" in
 	r)
 		REV=${OPTARG}
@@ -29,8 +29,8 @@ while getopts "r:b:c:h:p:" opt; do
 	c)
 		CROSSENV=${OPTARG}	
 		;;
-	p)
-		PLATFORM=${OPTARG}	
+	a)
+		ARCH=${OPTARG}	
 		;;
 	h)
 		echo $USAGE
@@ -39,7 +39,7 @@ while getopts "r:b:c:h:p:" opt; do
 	
 	esac
 done
-echo "PLATFORM IS $PLATFORM"
+echo "ARCH IS $ARCH"
 #########################
 # EXTRACT REQUIRED FILES
 #########################
@@ -57,11 +57,11 @@ cd tmppack
 #TAG="$(node -p "require('./package.json').version")"
 TAG="$(grep version package.json | sed 's/.*"version": "\(.*\)".*/\1/')"
 echo "building ppbase:$REV image, package version=$TAG"
-docker build  --file ./build/Dockerfile $BUILDARGS --target ppbase --tag ppbase:$REV --build-arg platform="$PLATFORM" .
+docker build  --file ./build/Dockerfile $BUILDARGS --target ppbase --tag ppbase:$REV --build-arg platform="$ARCH" .
 echo "building pprust:$REV image, package version=$TAG"
-docker build  --file ./build/Dockerfile $BUILDARGS --target pprust --tag pprust:$REV --build-arg platform="$PLATFORM" .
+docker build  --file ./build/Dockerfile $BUILDARGS --target pprust --tag pprust:$REV --build-arg platform="$ARCH" .
 echo "generating a build with minimal package jsons"
-docker build  --file ./build/Dockerfile $BUILDARGS --target ppminpkg --tag ppminpkg:$REV --build-arg platform="$PLATFORM" .
+docker build  --file ./build/Dockerfile $BUILDARGS --target ppminpkg --tag ppminpkg:$REV --build-arg platform="$ARCH" .
 
 echo "building pppkg:$REV image, package version=$TAG, can copy /home/root/pp/tmppack/stjude-proteinpaint.tgz as a publishable package"
 docker build  --file ./build/full/Dockerfile $BUILDARGS --target pppkg --tag pppkg:$REV --build-arg IMGVER=$REV --build-arg PKGVER=$TAG --build-arg CROSSENV="$CROSSENV" .
