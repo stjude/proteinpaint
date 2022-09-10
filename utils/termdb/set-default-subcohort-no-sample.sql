5b1b8715337b04d8e310c64c7ee771b51c0a50b2
Edgar Sioson <siosonel@gmail.com>
2022-09-10 18:16:26 -0500

    Prevent duplicate subcohort term entries due to left join returning the same entry, once for annotation and another one for survival term inserts when no sample data is available

--- utils/termdb/set-default-subcohort-no-sample.sql
+++ utils/termdb/set-default-subcohort-no-sample.sql
@@ -4,6 +4,10 @@ insert into subcohort_terms (cohort, term_id, count)
 select '', t.id, count(distinct a.sample)
 from terms t
 left join annotations a ON a.term_id = t.id
+-- prevent unjoined terms from being returned for survival term,
+-- which is done in the last pair of inserts, otherwise
+-- both annotation and survival terms will be duplicated because of left join
+where t.type != 'survival'
 group by t.id;
 
 insert into subcohort_terms (cohort, term_id, count)
@@ -11,12 +15,16 @@ select '', p.ancestor_id, count(distinct a.sample)
 from terms t
 left join annotations a ON a.term_id = t.id
 join ancestry p ON p.term_id = t.id
+-- see comment in the first insert statement above, do not duplicate survival ancestor entries
+where t.type != 'survival'
 group by p.ancestor_id;
 
 insert into subcohort_terms (cohort, term_id, count)
 select '', t.id, count(distinct s.sample)
 from terms t
 left join survival s ON s.term_id = t.id
+-- see comment in the first insert statement above, do not duplicate annotation term entries
+where t.type = 'survival'
 group by t.id;
 
 insert into subcohort_terms (cohort, term_id, count)
@@ -24,4 +32,6 @@ select '', p.ancestor_id, count(distinct s.sample)
 from terms t
 left join survival s ON s.term_id = t.id
 join ancestry p ON p.term_id = t.id
+-- see comment in the first insert statement above, do not duplicate annotation ancestor entries
+where t.type = 'survival'
 group by p.ancestor_id;
