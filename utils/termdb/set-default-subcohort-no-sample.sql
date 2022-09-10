33eb4e3839df91213a777e108c647dd1c7533b4c
Edgar Sioson <siosonel@gmail.com>
2022-09-10 18:27:39 -0500

    farther simplify the subcohort_terms ETL when there are no samples

--- utils/termdb/set-default-subcohort-no-sample.sql
+++ utils/termdb/set-default-subcohort-no-sample.sql
@@ -1,37 +1,14 @@
 delete from subcohort_terms;
 
 insert into subcohort_terms (cohort, term_id, count)
-select '', t.id, count(distinct a.sample)
+select '', t.id, 0
 from terms t
-left join annotations a ON a.term_id = t.id
--- prevent unjoined terms from being returned for survival term,
--- which is done in the last pair of inserts, otherwise
--- both annotation and survival terms will be duplicated because of left join
-where t.type != 'survival'
+-- no need to join to annotation or survival tables,
+-- since there are no samples with annotation or survival data
 group by t.id;
 
 insert into subcohort_terms (cohort, term_id, count)
-select '', p.ancestor_id, count(distinct a.sample)
+select '', p.ancestor_id, 0
 from terms t
-left join annotations a ON a.term_id = t.id
 join ancestry p ON p.term_id = t.id
--- see comment in the first insert statement above, do not duplicate survival ancestor entries
-where t.type != 'survival'
-group by p.ancestor_id;
-
-insert into subcohort_terms (cohort, term_id, count)
-select '', t.id, count(distinct s.sample)
-from terms t
-left join survival s ON s.term_id = t.id
--- see comment in the first insert statement above, do not duplicate annotation term entries
-where t.type = 'survival'
-group by t.id;
-
-insert into subcohort_terms (cohort, term_id, count)
-select '', p.ancestor_id, count(distinct s.sample)
-from terms t
-left join survival s ON s.term_id = t.id
-join ancestry p ON p.term_id = t.id
--- see comment in the first insert statement above, do not duplicate annotation ancestor entries
-where t.type = 'survival'
 group by p.ancestor_id;
