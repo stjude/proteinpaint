f1f5da69c2bb215c34421643eae2f0eed2cd0c21
Edgar Sioson <siosonel@gmail.com>
2022-09-20 15:12:06 -0500

    fix the hooks init script

--- utils/hooks/init.sh
+++ utils/hooks/init.sh
@@ -3,6 +3,7 @@
 mv -f .git/hooks/pre-commit .git/hooks/pre-commit-bkup
 cd .git/hooks
 ln -s ../../utils/hooks/pre-commit pre-commit
+cd ../..
 
 INSTALL_PYTHON=/usr/local/opt/python@3.9/bin/python3.9
 ARGS=(hook-impl --config=.pre-commit-config.yaml --hook-type=pre-commit)
