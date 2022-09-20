c94af92835827601b2411840e2becf8179a744b6
kgangwan <kgangwan@stjude.org>
2022-09-20 11:54:27 -0500

    added db and sampleidmap to clinical folder

--- server/dataset/all-pharmacotyping.hg38.js
+++ server/dataset/all-pharmacotyping.hg38.js
@@ -5,7 +5,7 @@ module.exports = {
 	// termdb as a generic interface
 	// getters will be added to abstract the detailed implementations
 	termdb: {
-		dictionary: { dbFile: 'files/hg38/ALL-pharmacotyping/db'},
+		dictionary: { dbFile: 'files/hg38/ALL-pharmacotyping/clinical/db'},
 		termid2totalsize2: {}
 	}
 }
