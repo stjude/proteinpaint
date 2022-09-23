b6c006f691dcc253d73e31e091c5c19416eb748d
congyu-lu <clu@stjude.com>
2022-09-23 13:41:10 -0500

    added more tests in parseBedTest.js

--- server/src/test/bedj.parseBedTest.js
+++ server/src/test/bedj.parseBedTest.js
@@ -1,12 +1,12 @@
 const tape = require('tape')
 const parseBedLine = require('../bedj.parseBed').parseBedLine
 
-tape('\n', function(test) {
-	test.pass('-***- server/bedj.parseBed test -***-')
-	test.end()
-})
+// tape('\n', function (test) {
+// 	test.pass('-***- server/bedj.parseBed test -***-')
+// 	test.end()
+// })
 
-tape('test...', function(test) {
+tape('test #1: parsing a line from hg38 gencodeV41.bb file', function(test) {
 	const bedLine = [
 		'chr17',
 		'7661778',
@@ -72,7 +72,233 @@ tape('test...', function(test) {
 		],
 		utr3: [[7661778, 7661938]]
 	}
+	test.deepEqual(parseBedLine(bedLine), expected)
+	test.end()
+})
 
+tape('test #2: parsing a line from dbsnp.hg19.bb file', function(test) {
+	const bedLine = [
+		'chr17',
+		'7579867',
+		'7579868',
+		'rs766765429',
+		'A',
+		'1',
+		'G,',
+		'0',
+		'12',
+		'-inf,2.39328e-05,7.96381e-06,1.67766e-05,-inf,3.19591e-05,-inf,-inf,-inf,-inf,-inf,-inf,',
+		',A,A,A,,A,,,,,,,',
+		',G,G,G,,G,,,,,,,',
+		'1819',
+		'snv',
+		'clinvar,clinvarBenign,rareSome,rareAll,overlapDiffClass,',
+		'58095997340',
+		'266'
+	]
+	const expected = { name: 'rs766765429' }
+	test.deepEqual(parseBedLine(bedLine), expected)
+	test.end()
+})
+
+tape('test #3: parsing a line from lrg.bb file', async function(test) {
+	const bedLine = [
+		'chr17',
+		'7681070',
+		'7705502',
+		'LRG_375',
+		'0',
+		'+',
+		'7681070',
+		'7705502',
+		'0',
+		'1',
+		'24432,',
+		'0,',
+		'',
+		'',
+		'24432',
+		'25522',
+		'WRAP53',
+		'NG_028245.1',
+		'RAPID: Resource of Asian Primary Immunodeficiency Diseases',
+		'http://web16.kazusa.or.jp/rapid/browseByPIDGenes',
+		'2013-01-16'
+	]
+	const expected = {
+		name: 'WRAP53',
+		isoform: 'LRG_375',
+		strand: '+',
+		exon: [[7681070, 7705502]],
+		rnalen: 24432,
+		cdslen: 24432,
+		codingstart: 7681070,
+		codingstop: 7705502,
+		coding: [[7681070, 7705502]]
+	}
+	test.deepEqual(parseBedLine(bedLine), expected)
+	test.end()
+})
+
+tape('test #4: parsing the line for gene IGKC from hg38 gencodeV41.bb file', function(test) {
+	const bedLine = [
+		'chr2',
+		'88857160',
+		'88857683',
+		'ENST00000390237.2',
+		'0',
+		'-',
+		'88857360',
+		'88857683',
+		'789624',
+		'1',
+		'523,',
+		'0,',
+		'uc061lpw.1',
+		'none',
+		'none',
+		'1,',
+		'none',
+		'IGKC',
+		'A0A5H1ZRQ3',
+		'none',
+		'coding',
+		'havana_ig_gene_homo_sapiens',
+		'IG_C_gene',
+		'Ensembl_canonical,appris_principal_1,basic,cds_start_NF,mRNA_start_NF,overlapping_locus',
+		'2',
+		'canonical,basic,all'
+	]
+	const expected = {
+		name: 'IGKC',
+		isoform: 'ENST00000390237',
+		strand: '-',
+		exon: [[88857160, 88857683]],
+		rnalen: 523,
+		category: 'coding',
+		cdslen: 323,
+		codingstart: 88857360,
+		codingstop: 88857683,
+		coding: [[88857360, 88857683]],
+		utr3: [[88857160, 88857360]],
+		startCodonFrame: 1
+	}
+	test.deepEqual(parseBedLine(bedLine), expected)
+	test.end()
+})
+
+tape('test #5: parsing a line from genCC.bb', function(test) {
+	const bedLine = [
+		'chr17',
+		'7668420',
+		'7687490',
+		'TP53 MONDO:0005089',
+		'0',
+		'-',
+		'7668420',
+		'7687490',
+		'56,161,105',
+		'ENST00000269305.9',
+		'ENSG00000141510.18',
+		'NM_000546.6',
+		'GENCC_000104-HGNC_11998-MONDO_0005089-HP_0000006-GENCC_100002',
+		'HGNC:11998',
+		'TP53',
+		'MONDO:0005089',
+		'sarcoma',
+		'MONDO:0005089',
+		'sarcoma',
+		'GENCC:100002',
+		'Strong',
+		'HP:0000006',
+		'Autosomal dominant',
+		'GENCC:000104',
+		'Genomics England PanelApp',
+		'HGNC:11998',
+		'TP53',
+		'MONDO:0005089',
+		'SARCOMA',
+		'HP:0000006',
+		'Autosomal dominant inheritance',
+		'GENCC:000104',
+		'Genomics England PanelApp',
+		'GENCC:100002',
+		'Strong',
+		'2021-02-11 13:01:34',
+		'https://panelapp.genomicsengland.co.uk/panels/734',
+		'',
+		'27050224, 28338660',
+		'',
+		'000104.pa734.v1.11.hgnc:11998.m1.p1',
+		'2021-03-31'
+	]
+	const expected = { name: 'TP53 MONDO:0005089', strand: '-' }
+	test.deepEqual(parseBedLine(bedLine), expected)
+	test.end()
+})
+
+tape('test #6: parsing a line from dbSnp153BadCoords.bb, four columns', function(test) {
+	const bedLine = ['chr17', '22078995', '22078998', 'rs587632675']
+	const expected = { name: 'rs587632675' }
+	test.deepEqual(parseBedLine(bedLine), expected)
+	test.end()
+})
+
+tape('test #7: parsing a line from hgmd.bb, strand == .', function(test) {
+	const bedLine = [
+		'chr17',
+		'7687375',
+		'7687376',
+		'TP53:CS002469',
+		'0',
+		'.',
+		'7687375',
+		'7687376',
+		'0,0,0',
+		'TP53',
+		'CS002469',
+		'splicing variant'
+	]
+	const expected = { name: 'TP53:CS002469' }
+	test.deepEqual(parseBedLine(bedLine), expected)
+	test.end()
+})
+
+tape('test #8: parsing a line from interactions.bb', function(test) {
+	const bedLine = [
+		'chr17',
+		'7687439',
+		'7703502',
+		'WRAP53: TP53,DKC1,SMN2,SMN1,TRNAU1AP,NCBP2,NOP10,NHP2,SNRPC,PRPF4',
+		'32',
+		'.',
+		'7687439',
+		'7703502',
+		'0,0,128'
+	]
+	const expected = {
+		name: 'WRAP53: TP53,DKC1,SMN2,SMN1,TRNAU1AP,NCBP2,NOP10,NHP2,SNRPC,PRPF4'
+	}
+	test.deepEqual(parseBedLine(bedLine), expected)
+	test.end()
+})
+
+tape('test#9: parsing a line from miRnaAtlasSample1.bb', function(test) {
+	const bedLine = [
+		'chr17',
+		'8088066',
+		'8088083',
+		'hsa-miR-4314',
+		'777',
+		'+',
+		'MI0015846',
+		'24',
+		'8.57,114.92,54.01,75.90,120.94,19.11,73.37,11.06,81.16,23.25,7.38,101.05,58.32,16.78,79.95,3.11,175.32,78.50,92.89,37.40,62.77,62.39,96.46,64.69',
+		'322760',
+		'418',
+		'0.67'
+	]
+	const expected = { name: 'hsa-miR-4314', strand: '+' }
 	test.deepEqual(parseBedLine(bedLine), expected)
 	test.end()
 })
