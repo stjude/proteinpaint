85a491b46eb2958eac7e3a18edd3f826e2a5934c
Edgar Sioson <siosonel@gmail.com>
2022-10-15 20:21:46 -0500

    add datasets to pp-irt and fix deployment issues

---
+++ sj/pp-irt/index.js
@@ -0,0 +1,3 @@
+// this package automatically starts the server when loaded
+// !!! TODO: pp-server should return the express app for better control !!!
+const app = require('@stjude/proteinpaint-server')
