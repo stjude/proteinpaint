d2cb7fe925ff6b46c6d7ea1fe49815b6b0161b8c
aacic <aacic@stjude.org>
2022-11-08 12:03:56 -0600

    Extract webpack base config.

---
+++ server/webpack.base.config.js
@@ -0,0 +1,58 @@
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+const fs = require('fs')
+
+let babelrc
+try {
+	babelrc = fs.readFileSync(path.join(__dirname, '.babelrc'))
+	babelrc = JSON.parse(babelrc)
+	if (process.env.PP_MODE?.startsWith('container')) {
+		babelrc.presets[0][1].targets.node = 16
+	}
+} catch (e) {
+	throw e
+}
+
+module.exports = function(env = {}) {
+	// the env object is passed to webpack cli call by
+	// adding --env NODE_ENV='...', --env devtool='...', etc
+	return {
+		// see https://v4.webpack.js.org/configuration/mode/
+		//
+		// note that webpack applies commonly used plugins
+		// by mode, such as the terser plugin to
+		// minify production bundles, so there are less
+		// manual configuration settings needed here
+		//
+		mode: env.NODE_ENV ? env.NODE_ENV : 'production',
+		target: 'node',
+		externals: [
+			nodeExternals({
+				allowlist: [/\/src\//, /d3-*/, 'internmap', 'delaunator', 'robust-predicates'],
+				additionalModuleDirs: [path.resolve(__dirname, '../node_modules')]
+			})
+		],
+		externalsPresets: {
+			node: true
+		},
+		entry: path.join(__dirname, './src/app.js'),
+		output: {
+			path: path.join(__dirname, './'),
+			filename: 'server.js'
+		},
+		module: {
+			strictExportPresence: true,
+			rules: [
+				{
+					test: /\.js$/,
+					use: [
+						{
+							loader: 'babel-loader',
+							options: babelrc
+						}
+					]
+				}
+			]
+		}
+	}
+}
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,63 +1,25 @@
-const nodeExternals = require('webpack-node-externals')
-const webpack = require('webpack')
 const path = require('path')
-const fs = require('fs')
-
-const babelrc = require('./utils/babel/readBabelConfig').readBabelConfig(__dirname, 16)
+const baseConfig = require('./webpack.base.config')
 
 module.exports = function(env = {}) {
-	// the env object is passed to webpack cli call by
-	// adding --env NODE_ENV='...', --env devtool='...', etc
-	return {
-		// see https://v4.webpack.js.org/configuration/mode/
-		//
-		// note that webpack applies commonly used plugins
-		// by mode, such as the terser plugin to
-		// minify production bundles, so there are less
-		// manual configuration settings needed here
-		//
-		mode: env.NODE_ENV ? env.NODE_ENV : 'production',
-		target: 'node',
-		externals: [
-			nodeExternals({
-				allowlist: [/\/src\//, /d3-*/, 'internmap', 'delaunator', 'robust-predicates'],
-				additionalModuleDirs: [path.resolve(__dirname, '../node_modules')]
-			})
-		],
-		externalsPresets: {
-			node: true
-		},
-		entry: path.join(__dirname, './src/app.js'),
-		output: {
-			path: path.join(__dirname, './'),
-			filename: 'server.js'
-		},
-		module: {
-			strictExportPresence: true,
-			rules: [
-				{
-					test: /\.js$/,
-					use: [
-						{
-							loader: 'babel-loader',
-							options: babelrc
-						}
-					]
-				}
-			]
-		},
-		// see https://v4.webpack.js.org/configuration/devtool/ for option details
-		//
-		// devtool: 'eval' is fastest to build/rebuild, no files are written,
-		// but the line number in stack traces may be a little bit off
-		//
-		// devtool: 'source-map' is slowest to build/rebuild, but
-		// line numbers in stack traces are accurate
-		//
-		devtool: env.devtool ? env.devtool : env.NODE_ENV == 'development' ? 'source-map' : false,
-		infrastructureLogging: {
-			appendOnly: false,
-			level: 'info'
-		}
+	const config = baseConfig(env)
+	config.entry = path.join(__dirname, './src/app.js')
+	config.output = {
+		path: path.join(__dirname, './'),
+		filename: 'server.js'
 	}
+	// see https://v4.webpack.js.org/configuration/devtool/ for option details
+	//
+	// devtool: 'eval' is fastest to build/rebuild, no files are written,
+	// but the line number in stack traces may be a little bit off
+	//
+	// devtool: 'source-map' is slowest to build/rebuild, but
+	// line numbers in stack traces are accurate
+	//
+	config.devtool = env.devtool ? env.devtool : env.NODE_ENV === 'development' ? 'source-map' : false
+	config.infrastructureLogging = {
+		appendOnly: false,
+		level: 'info'
+	}
+	return config
 }
