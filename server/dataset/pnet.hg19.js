fa9481162bb5701a844c7e4f06886ec5af3de83f
Airen Zaldivar <airenzp@gmail.com>
2022-11-09 09:24:08 -0600

    Restored TSNE Category as default

--- server/dataset/pnet.hg19.js
+++ server/dataset/pnet.hg19.js
@@ -23,7 +23,7 @@ module.exports = function() {
 						name: 'Methylome TSNE',
 						dimension: 2, // 2d requires x/y, 3d requires x/y/z
 						file: 'files/hg19/pnet/classification/pnet_apr13_tnse.txt',
-						colorTW: { term: { name: 'TP53', type: 'geneVariant' } }
+						colorTW: { id: 'TSNE Category' }
 
 						// allow additional config for this plot
 						// allow additional config for this plot
