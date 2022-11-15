105f47c946868651497b15884bc4e1c05d3e66a6
Airen Zaldivar <airenzp@gmail.com>
2022-11-15 14:32:32 -0600

    Added header and footer

--- client/termsetting/handlers/samplelst.js
+++ client/termsetting/handlers/samplelst.js
@@ -5,24 +5,34 @@ import { Menu } from '#dom/menu'
 export function getHandler(self) {
 	return {
 		showEditMenu(div) {
-			div.style('padding', '4px')
+			const group1 = self.q.groups[0]
 			const group2 = self.q.groups[1]
+
+			div
+				.style('padding', '4px')
+				.append('div')
+				.html('&nbsp;' + group1.name)
+				.style('font-size', '0.7rem')
+			const tableDiv = div.append('div')
+			div
+				.append('div')
+				.html('<b>Others</b> group excludes these samples')
+				.style('font-size', '0.7rem')
 			if (group2.name === 'Others') {
 				showSamples()
 
 				function showSamples() {
-					div.selectAll('*').remove()
-					const values = self.q.groups[0].values
+					tableDiv.selectAll('*').remove()
 					const rows = []
-					for (const value of values) rows.push([{ value: value }])
+					for (const value of group1.values) rows.push([{ value: value }])
 					const columns = [{ label: 'Sample' }]
 					renderTable({
 						rows,
 						columns,
-						div,
+						div: tableDiv,
 						deleteCallback: i => {
 							group2.values.splice(i, 1)
-							values.splice(i, 1)
+							group1.values.splice(i, 1)
 							showSamples()
 							self.runCallback()
 						}
