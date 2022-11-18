b6e5ab23d6c8b1f2c0b884a451ee5a0f2c5568ed
Edgar Sioson <siosonel@gmail.com>
2022-11-17 21:00:46 -0600

    export the TermdbTest dataset as a function to allow isolated use in different genomes

--- server/dataset/termdb.test.js
+++ server/dataset/termdb.test.js
@@ -84,89 +84,91 @@ http://localhost:3000/?noheader=1&mass={"dslabel":"TermdbTest","genome":"hg38","
 (this ensures better-sqlite3 and R works on your pp instance)
 */
 
-const ds = {
-	isMds: true,
-	cohort: {
-		db: {
-			file: 'files/hg38/TermdbTest/db2'
-		},
-		termdb: {
-			survivalplot: {
-				term_ids: ['efs', 'os'],
-				xUnit: 'years',
-				codes: [{ value: 0, name: '' }, { value: 1, name: 'censored' }]
+module.exports = function() {
+	const ds = {
+		isMds: true,
+		cohort: {
+			db: {
+				file: 'files/hg38/TermdbTest/db2'
 			},
-
-			minTimeSinceDx: 5, // enrollment in sjlife requires 5 years since cancer diagnosis
-
-			ageStartTermId: 'agedx', // term id for starting age of patients
-			// for cox outcome with timeScale='age'
-			// starting age of patients is age at cancer diagnosis
-
-			ageEndOffset: 0.00274, // number of years to offset ending age of patients
-			// for cox outcome with timeScale='age'
-			// 1 day (i.e. 1/365 or 0.00274) needs to be added
-			// to age_end to prevent age_end = age_start (which
-			// would cause regression analysis to fail in R)
-
-			selectCohort: {
-				// wrap term.id into a term json object so as to use it in tvs;
-				// the term is not required to exist in termdb
-				// term.id is specific to this dataset, should not use literally in client/server code but always through a variable
-				term: {
-					id: 'subcohort',
-					type: 'categorical'
+			termdb: {
+				survivalplot: {
+					term_ids: ['efs', 'os'],
+					xUnit: 'years',
+					codes: [{ value: 0, name: '' }, { value: 1, name: 'censored' }]
 				},
-				prompt: 'To get started with the Clinical Browser, select the survivor population you wish to browse.',
-				values: [
-					// <ul><li> for items, with a radio button for each.
-					{
-						keys: ['SJLIFE'],
-						label: 'St. Jude Lifetime Cohort (SJLIFE)',
-						shortLabel: 'SJLIFE',
-						isdefault: true,
-						cssSelector: 'tbody > tr > td:nth-child(2)'
-					},
-					{
-						keys: ['CCSS'],
-						label: 'Childhood Cancer Survivor Study (CCSS)',
-						shortLabel: 'CCSS',
-						cssSelector: 'tbody > tr > td:nth-child(3)'
-					},
-					{
-						keys: ['SJLIFE', 'CCSS'],
-						label: 'Combined SJLIFE+CCSS',
-						shortLabel: 'SJLIFE+CCSS',
-						cssSelector: 'tbody > tr > td:nth-child(2), tbody > tr > td:nth-child(3)',
-						// show note under label in smaller text size
-						note:
-							'The combined cohorts are limited to those variables that are comparable between the two populations. For example, selecting this category does not allow browsing of clinically-ascertained variables, which are only available in SJLIFE.'
-					}
-				],
-				highlightCohortBy: 'cssSelector',
-				htmlinfo: cohorthtmltable
-			},
 
-			dataDownloadFailHelpLink: 'https://university.stjude.cloud/docs/visualization-community/data-download/'
-		},
-		scatterplots: {
-			plots: [
-				{
-					name: 'Methylome TSNE',
-					dimension: 2,
-					file: 'files/hg38/TermdbTest/tnse.txt',
-					term: { id: 'diaggrp' }
-				}
-			]
+				minTimeSinceDx: 5, // enrollment in sjlife requires 5 years since cancer diagnosis
+
+				ageStartTermId: 'agedx', // term id for starting age of patients
+				// for cox outcome with timeScale='age'
+				// starting age of patients is age at cancer diagnosis
+
+				ageEndOffset: 0.00274, // number of years to offset ending age of patients
+				// for cox outcome with timeScale='age'
+				// 1 day (i.e. 1/365 or 0.00274) needs to be added
+				// to age_end to prevent age_end = age_start (which
+				// would cause regression analysis to fail in R)
+
+				selectCohort: {
+					// wrap term.id into a term json object so as to use it in tvs;
+					// the term is not required to exist in termdb
+					// term.id is specific to this dataset, should not use literally in client/server code but always through a variable
+					term: {
+						id: 'subcohort',
+						type: 'categorical'
+					},
+					prompt: 'To get started with the Clinical Browser, select the survivor population you wish to browse.',
+					values: [
+						// <ul><li> for items, with a radio button for each.
+						{
+							keys: ['SJLIFE'],
+							label: 'St. Jude Lifetime Cohort (SJLIFE)',
+							shortLabel: 'SJLIFE',
+							isdefault: true,
+							cssSelector: 'tbody > tr > td:nth-child(2)'
+						},
+						{
+							keys: ['CCSS'],
+							label: 'Childhood Cancer Survivor Study (CCSS)',
+							shortLabel: 'CCSS',
+							cssSelector: 'tbody > tr > td:nth-child(3)'
+						},
+						{
+							keys: ['SJLIFE', 'CCSS'],
+							label: 'Combined SJLIFE+CCSS',
+							shortLabel: 'SJLIFE+CCSS',
+							cssSelector: 'tbody > tr > td:nth-child(2), tbody > tr > td:nth-child(3)',
+							// show note under label in smaller text size
+							note:
+								'The combined cohorts are limited to those variables that are comparable between the two populations. For example, selecting this category does not allow browsing of clinically-ascertained variables, which are only available in SJLIFE.'
+						}
+					],
+					highlightCohortBy: 'cssSelector',
+					htmlinfo: cohorthtmltable
+				},
+
+				dataDownloadFailHelpLink: 'https://university.stjude.cloud/docs/visualization-community/data-download/'
+			},
+			scatterplots: {
+				plots: [
+					{
+						name: 'Methylome TSNE',
+						dimension: 2,
+						file: 'files/hg38/TermdbTest/tnse.txt',
+						term: { id: 'diaggrp' }
+					}
+				]
+			}
 		}
 	}
+
+	const targetDir = path.join(serverconfig.binpath, 'test/tp/files/hg38/TermdbTest')
+	const datadir = path.join(serverconfig.tpmasterdir, 'files/hg38/TermdbTest')
+
+	if (!targetDir.endsWith(datadir) && !fs.existsSync(datadir)) {
+		fs.symlinkSync(targetDir, datadir)
+	}
+
+	return ds
 }
-
-const targetDir = path.join(serverconfig.binpath, 'test/tp/files/hg38/TermdbTest')
-const datadir = path.join(serverconfig.tpmasterdir, 'files/hg38/TermdbTest')
-
-if (!targetDir.endsWith(datadir) && !fs.existsSync(datadir)) {
-	fs.symlinkSync(targetDir, datadir)
-}
-
-module.exports = ds
