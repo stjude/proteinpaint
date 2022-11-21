074d3788824fab83f99b51885a50dc14efd63f54
aacic <aacic@stjude.org>
2022-11-21 10:09:44 -0600

    Set node env to test.

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -66,26 +66,23 @@ module.exports = env => {
 			})
 
 		case 'development':
-			return merge(
-				commonConfig,
-				(developmentConfig = {
-					mode: 'development',
-					entry: path.join(__dirname, './src/app.js'),
-					output: {
-						path: path.join(__dirname, './'),
-						filename: 'server.js'
-					},
-					// see https://v4.webpack.js.org/configuration/devtool/ for option details
-					//
-					// devtool: 'eval' is fastest to build/rebuild, no files are written,
-					// but the line number in stack traces may be a little bit off
-					//
-					// devtool: 'source-map' is slowest to build/rebuild, but
-					// line numbers in stack traces are accurate
-					//
-					devtool: 'source-map'
-				})
-			)
+			return merge(commonConfig, {
+				mode: 'development',
+				entry: path.join(__dirname, './src/app.js'),
+				output: {
+					path: path.join(__dirname, './'),
+					filename: 'server.js'
+				},
+				// see https://v4.webpack.js.org/configuration/devtool/ for option details
+				//
+				// devtool: 'eval' is fastest to build/rebuild, no files are written,
+				// but the line number in stack traces may be a little bit off
+				//
+				// devtool: 'source-map' is slowest to build/rebuild, but
+				// line numbers in stack traces are accurate
+				//
+				devtool: 'source-map'
+			})
 
 		case 'test':
 			return merge(commonConfig, {
