24e756ed943cab81c0103a55cb226e860f5d7e50
Xin Zhou <xzhou82@gmail.com>
2022-12-06 16:27:13 -0600

    improve code and use GBM by default if no filter

--- client/src/launchGdcMatrix.js
+++ client/src/launchGdcMatrix.js
@@ -12,11 +12,14 @@ designed to work for Oncoprint app in GDC Analysis Tools Framework
 
 arg = {}
 	runpp() argument object
+
 	.filter0{}
 		the filter object from GDC Cohort Builder
 		{"op":"and","content":[{"op":"in","content":{"field":"cases.primary_site","value":["breast","bronchus and lung"]}}]}
+
 	.genes[]
 		future: list of genes to launch matrix
+
 holder
 genomes = { hg38 : {} }
 
@@ -38,30 +41,46 @@ export async function init(arg, holder, genomes) {
 	const genome = genomes[gdcGenome]
 	if (!genome) throw gdcGenome + ' missing'
 
-	if (arg.genes) {
-		if (!Array.isArray(arg.genes) || arg.genes.length == 0) throw '.genes[] is not non-empty array'
-		return await launchMatrix(arg.genes, arg, holder, genome)
-	}
+	// always require a gdc cohort filter
+	const gdcCohort = getGdcCohort(arg)
 
-	// default genes not provided
+	const genes = await getGenes(arg, gdcCohort)
 
+	// TODO limit number of cases, backend?
+	return await launchMatrix(genes, arg, holder, genome)
+}
+
+function getGdcCohort(arg) {
 	if (arg.filter0) {
-		// cohort filter is provided. query top genes to use in default matrix
+		// already given a filter, return without validation
 		if (typeof arg.filter0 != 'object') throw 'filter0 not object'
-		const data = await dofetch3(
-			`gdc_filter2topGenes?genome=${gdcGenome}&filter0=${encodeURIComponent(JSON.stringify(arg.filter0))}`
-		)
-		if (data.error) throw data.error
-		if (!data.genes) throw 'no top genes found'
-		return await launchMatrix(data.genes, arg, holder, genome)
+		return arg.filter0
+	}
+	// no filter. as discussed Dec/6, 2022, use GBM as a default
+	return {
+		op: 'and',
+		content: [{ op: 'in', content: { field: 'cases.disease_type', value: ['Gliomas'] } }]
+	}
+}
+
+async function getGenes(arg, gdcCohort) {
+	if (arg.genes) {
+		// genes are predefined
+		if (!Array.isArray(arg.genes) || arg.genes.length == 0) throw '.genes[] is not non-empty array'
+		return arg.genes
 	}
 
-	throw 'todo may show disease select'
+	// genes are not predefined. query to get top genes using the current cohort
+	const data = await dofetch3(
+		`gdc_filter2topGenes?genome=${gdcGenome}&filter0=${encodeURIComponent(JSON.stringify(gdcCohort))}`
+	)
+	if (data.error) throw data.error
+	if (!data.genes) throw 'no top genes found using the cohort filter'
+	return data.genes
 }
 
 async function launchMatrix(genes, arg, holder, genome) {
-	// slicing is for temp testing
-	const termlst = genes.slice(5, 10).map(i => {
+	const termlst = genes.map(i => {
 		return { term: { name: i, type: 'geneVariant' } }
 	})
 	const _ = await import('#mass/app')
