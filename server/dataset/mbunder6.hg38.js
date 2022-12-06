d2d01d661f81ed906597692a38475244ce736a48
congyu-lu <111394589+congyu-lu@users.noreply.github.com>
2022-12-06 15:57:04 -0600

    add mbunder6 dataset (#1603)
    Co-authored-by: congyu-lu <clu@stjude.com>

---
+++ server/dataset/mbunder6.hg38.js
@@ -0,0 +1,22 @@
+module.exports = function() {
+	return {
+		isMds: true,
+
+		cohort: {
+			allowedChartTypes: ['summary', 'matrix'],
+			mutationset: [
+				{
+					snvindel: 'sdhanda/mb_portal/BT_database/mb_review/SNVindel_firstcase.tsv',
+					cnv: 'sdhanda/mb_portal/BT_database/mb_review/CNV_data_firstcase.tsv',
+					fusion: 'sdhanda/mb_portal/BT_database/mb_review/fusion_firstcase.tsv'
+				}
+			],
+			db: {
+				file: 'files/hg38/mbunder6/clinical/db'
+			},
+			termdb: {
+				allowedTermTypes: ['geneVariant']
+			}
+		}
+	}
+}
