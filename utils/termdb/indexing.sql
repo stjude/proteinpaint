1bbfd350117116ee5284cd3ddc53ccb95e0ff603
Airen Zaldivar <airenzp@gmail.com>
2022-12-12 13:40:24 -0600

    Added missing indexes

--- utils/termdb/indexing.sql
+++ utils/termdb/indexing.sql
@@ -3,9 +3,20 @@
 update terms set parent_id=null where parent_id='';
 
 create index terms_n on terms(name);
+
+create index ancestry_pid on ancestry(ancestor_id);
+
+create index a_termid on annotations(term_id);
 create index a_value on annotations(value);
+
+create index c_termid on chronicevents(term_id);
 create index c_grade on chronicevents(grade);
+
+CREATE INDEX p_termid on precomputed(term_id);
 CREATE INDEX p_value_for on precomputed(value_for);
+
 CREATE INDEX subcohort_terms_cohort ON subcohort_terms(cohort);
-CREATE INDEX subcohort_terms_term_id ON subcohort_terms(term_id);
+CREATE INDEX subcohort_terms_termid ON subcohort_terms(term_id);
+
+CREATE INDEX survival_term ON survival(term_id);
 
