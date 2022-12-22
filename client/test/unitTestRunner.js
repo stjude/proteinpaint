6d7115cddbca4788b1608ad1f648b43a50dab77d
Edgar Sioson <siosonel@gmail.com>
2022-12-21 21:33:01 -0600

    merge client initUnit code into unitTestRunner to lessen indirection

--- client/test/unitTestRunner.js
+++ client/test/unitTestRunner.js
@@ -1,9 +1,13 @@
-export function unittest() {
-	importInitTests()
-}
+// the native fetch API will not know the default host or might
+// not have access to the localStorage, so
+// testHost will be used as default as needed
+window.testHost = 'http://localhost:3000'
+sessionStorage.setItem('hostURL', window.testHost)
 
-async function importInitTests() {
-	var context = require.context('../', true, /\.unit.spec.js$/)
-	context.keys().forEach(context)
-	module.exports = context
-}
+// polyfill only for testing
+if (!window.structuredClone) window.structuredClone = val => JSON.parse(JSON.stringify(val))
+
+const context = require.context('../', true, /\.unit.spec.js$/)
+console.log(6, context)
+context.keys().forEach(context)
+module.exports = context
