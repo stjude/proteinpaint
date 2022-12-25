6139657fb8c86704c86a941162a037966f5825eb
Xin Zhou <xzhou82@gmail.com>
2022-12-24 22:25:32 -0600

    move unused script to old/

--- utils/sjlife2/load.sql
+++ utils/sjlife2/load.sql
@@ -8,10 +8,16 @@ insert into samples(id) select distinct(id) from sampleidmap;
 
 .import termdb terms
 
+-- adding ad-hoc terms to "terms" table: id/name/parent_id/jsondata/child_order/type/isleaf
+-- that are not part of phenotree
+--
+-- parent terms (* and $ROOT$) are not real terms and not declared in terms table, preventing foreign key check to work
+
 -- only do this if cohort selection is enabled
 -- using * for parent id of subcohort makes it hidden from the tree (but still searchable by name)
 INSERT INTO terms VALUES ('subcohort', 'Cohort', '*', '{"name":"Cohort","type":"categorical","values":{"SJLIFE":{"label":"SJLIFE"},"CCSS":{"label":"CCSS"}}}', 0, NULL, 0);
 
+-- admix coefficient from 3 ancestries
 INSERT INTO terms VALUES ('ASA', 'ASA', '*', '{"name":"ASA","type":"float"}', 0, "float", 0);
 INSERT INTO terms VALUES ('CEU', 'CEU', '*', '{"name":"CEU","type":"float"}', 0, "float", 0);
 INSERT INTO terms VALUES ('YRI', 'YRI', '*', '{"name":"YRI","type":"float"}', 0, "float", 0);
