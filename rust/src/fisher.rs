7149605c6547937bd9de562d154a6726796066b4
Paul <robinpaul85@gmail.com>
2023-01-09 14:22:46 -0600

    Implemented Bonferonni correction, selecting fisher/chisq based on expected value, filtering low sample entries when low_sample_size=true

--- rust/src/fisher.rs
+++ rust/src/fisher.rs
@@ -4,7 +4,9 @@
 // Results are written in JSON format to stdout.
 
 // Input JSON specifications:
-// { fdr: Flag to calculate adjusted p-value using Benjamini-Hochberg correction (optional)
+// { fdr: Flag to calculate adjusted p-value using Benjamini-Hochberg correction (true/false) (optional)
+//   bon: Flag to calculate adjusted p-value using Bonferroni correction (true/false) (optional)
+//   low_sample_size: Flag to check if entries with low sample size need to be ignored (true/false) (optional)
 //   input:[{
 //     index: Index of the entry
 //        n1:
@@ -15,7 +17,7 @@
 // }
 
 /*
-    Suggested by Qian: 
+    Suggested by Qian:
     1. Using <5 in each cell is a conventional rule of thumb for choosing chi-square or Fisher’s exact test.
     2. The association test may have extremely low power if a group (row sum or column sum of 2x2 table) is too small compared to
     the total participants, regardless of the value in each cell. Including too many non-powerful tests in this portal may affect
@@ -56,9 +58,13 @@
 
 // cd ~/proteinpaint/rust && cargo build --release && json='{"fdr":true,"input":[{"index":0,"n1":214,"n2":2057,"n3":134,"n4":1954},{"index":1,"n1":134,"n2":1954,"n3":214,"n4":2057},{"index":2,"n1":1863,"n2":225,"n3":1935,"n4":336},{"index":3,"n1":1935,"n2":336,"n3":1863,"n4":225},{"index":4,"n1":106,"n2":2165,"n3":74,"n4":2014},{"index":5,"n1":74,"n2":2014,"n3":106,"n4":2165},{"index":6,"n1":1,"n2":987,"n3":3,"n4":897},{"index":7,"n1":3,"n2":748,"n3":4,"n4":977}]}' && time echo "$json" | target/release/fisher
 
+// cd ~/proteinpaint/rust && cargo build --release && json='{"fdr":true, "low_sample_size":true,"input":[{"index":0,"n1":214,"n2":2057,"n3":134,"n4":1954},{"index":1,"n1":134,"n2":1954,"n3":214,"n4":2057},{"index":2,"n1":1863,"n2":225,"n3":1935,"n4":336},{"index":3,"n1":1935,"n2":336,"n3":1863,"n4":225},{"index":4,"n1":106,"n2":2165,"n3":74,"n4":2014},{"index":5,"n1":74,"n2":2014,"n3":106,"n4":2165},{"index":6,"n1":1,"n2":987,"n3":3,"n4":897},{"index":7,"n1":3,"n2":748,"n3":4,"n4":977}]}' && time echo "$json" | target/release/fisher
+
+// cd ~/proteinpaint/rust && cargo build --release && json='{"bon":true, "low_sample_size":true,"input":[{"index":0,"n1":214,"n2":2057,"n3":134,"n4":1954},{"index":1,"n1":134,"n2":1954,"n3":214,"n4":2057},{"index":2,"n1":1863,"n2":225,"n3":1935,"n4":336},{"index":3,"n1":1935,"n2":336,"n3":1863,"n4":225},{"index":4,"n1":106,"n2":2165,"n3":74,"n4":2014},{"index":5,"n1":74,"n2":2014,"n3":106,"n4":2165},{"index":6,"n1":1,"n2":987,"n3":3,"n4":897},{"index":7,"n1":3,"n2":748,"n3":4,"n4":977}]}' && time echo "$json" | target/release/fisher
+
 // cd ~/proteinpaint/rust && cargo build --release && json='{"fdr":true,"input":[{"index":0,"n1":214,"n2":2057,"n3":134,"n4":1954},{"index":1,"n1":134,"n2":1954,"n3":214,"n4":2057},{"index":2,"n1":1863,"n2":225,"n3":1935,"n4":336},{"index":3,"n1":1935,"n2":336,"n3":1863,"n4":225},{"index":4,"n1":106,"n2":2165,"n3":74,"n4":2014},{"index":5,"n1":74,"n2":2014,"n3":106,"n4":2165}]}' && time echo "$json" | target/release/fisher
 
-// Example of json input missing the fdr flag
+// Example of json input missing the fdr and bon flag
 
 // cd ~/proteinpaint/rust && cargo build --release && json='{"input":[{"index":0,"n1":514,"n2":626,"n3":45,"n4":106},{"index":1,"n1":11,"n2":948,"n3":364,"n4":292},{"index":2,"n1":129,"n2":951,"n3":531,"n4":268},{"index":3,"n1":677,"n2":40,"n3":11,"n4":837},{"index":4,"n1":947,"n2":937,"n3":245,"n4":817},{"index":5,"n1":589,"n2":889,"n3":934,"n4":400},{"index":6,"n1":5,"n2":119,"n3":278,"n4":641},{"index":7,"n1":873,"n2":113,"n3":771,"n4":109},{"index":8,"n1":495,"n2":69,"n3":759,"n4":884},{"index":9,"n1":266,"n2":192,"n3":686,"n4":761},{"index":10,"n1":484,"n2":814,"n3":754,"n4":521},{"index":11,"n1":50,"n2":615,"n3":357,"n4":470},{"index":12,"n1":416,"n2":109,"n3":472,"n4":462},{"index":13,"n1":535,"n2":935,"n3":969,"n4":35},{"index":14,"n1":605,"n2":667,"n3":553,"n4":359},{"index":15,"n1":483,"n2":719,"n3":879,"n4":254},{"index":16,"n1":940,"n2":32,"n3":259,"n4":373},{"index":17,"n1":228,"n2":565,"n3":154,"n4":155},{"index":18,"n1":23,"n2":57,"n3":232,"n4":238},{"index":19,"n1":356,"n2":39,"n3":771,"n4":887},{"index":20,"n1":481,"n2":307,"n3":776,"n4":952},{"index":21,"n1":463,"n2":202,"n3":57,"n4":218},{"index":22,"n1":658,"n2":68,"n3":431,"n4":774},{"index":23,"n1":334,"n2":266,"n3":266,"n4":677},{"index":24,"n1":97,"n2":544,"n3":532,"n4":863},{"index":25,"n1":562,"n2":313,"n3":725,"n4":574}]}' && time echo "$json" | target/release/fisher
 
@@ -128,29 +134,80 @@ fn main() {
             let variants: &JsonValue = &json_string["input"]; // Putting each variant in a separate element of vector
 
             //let fisher_limit: u32 = json_string["fisher_limit"].as_u32().unwrap(); // Cutoff for sum of all four numbers to decide whether to use fisher or chisq test
-            let individual_fisher_limit: u32 = 5;
+            let individual_fisher_limit: f64 = 5.0;
             let fdr_string = &json_string["fdr"].to_owned();
+            let bon_string = &json_string["bon"].to_owned();
+            let low_sample_size_string = &json_string["low_sample_size"].to_owned();
             //println!("fdr_string:{}", fdr_string);
-            if fdr_string.is_null() == true {
+
+            let mut fdr_bool = false;
+            match fdr_string.as_bool() {
+                Some(fdr) => match fdr {
+                    true => fdr_bool = true,
+                    false => {}
+                },
+                None => {}
+            }
+            let mut low_sample_size_bool = false;
+            match low_sample_size_string.as_bool() {
+                Some(low_sample_size) => match low_sample_size {
+                    true => low_sample_size_bool = true,
+                    false => {}
+                },
+                None => {}
+            }
+            let mut bon_bool = false;
+            match bon_string.as_bool() {
+                Some(bon) => match bon {
+                    true => bon_bool = true,
+                    false => {}
+                },
+                None => {}
+            }
+
+            if fdr_bool == false && bon_bool == false {
                 // Check if FDR calculation is required or not
-                calculate_fisher_chisq_test(variants, individual_fisher_limit, false);
-            } else if fdr_string.is_boolean() == true {
-                // Check if FDR calculation is required or not
-                if fdr_string.as_bool() == Some(true) {
-                    calculate_fisher_chisq_test(variants, individual_fisher_limit, true);
-                } else if fdr_string.as_bool() == Some(false) {
-                    calculate_fisher_chisq_test(variants, individual_fisher_limit, false);
-                }
+                calculate_fisher_chisq_test(
+                    variants,
+                    individual_fisher_limit,
+                    false,
+                    false,
+                    low_sample_size_bool,
+                );
+            } else if fdr_bool == true && bon_bool == false {
+                calculate_fisher_chisq_test(
+                    variants,
+                    individual_fisher_limit,
+                    true,
+                    false,
+                    low_sample_size_bool,
+                );
+            } else if fdr_bool == false && bon_bool == true {
+                calculate_fisher_chisq_test(
+                    variants,
+                    individual_fisher_limit,
+                    false,
+                    true,
+                    low_sample_size_bool,
+                );
+            } else if fdr_bool == true && bon_bool == true {
+                println!("Choose either Bonferronni correction or Benjamini-Hochberg correction");
             } else {
-                calculate_fisher_chisq_test(variants, individual_fisher_limit, true);
-                //println!("variants:{:?}", variants);
+                // Should not happen
+                println!("Unidentified case");
             }
         }
         Err(error) => println!("Incorrect json: {}", error),
     }
 }
 
-fn calculate_fisher_chisq_test(variants: &JsonValue, individual_fisher_limit: u32, fdr: bool) {
+fn calculate_fisher_chisq_test(
+    variants: &JsonValue,
+    individual_fisher_limit: f64,
+    fdr: bool,
+    bon: bool,
+    low_sample_size: bool,
+) {
     let mut p_values_list = Vec::<PValueIndexes>::new();
     let mut num_of_tests: f64 = 0.0; // Number of test actually carried out ignoring those which have low sample-size
     for i in 0..variants.len() {
@@ -174,14 +231,15 @@ fn calculate_fisher_chisq_test(variants: &JsonValue, individual_fisher_limit: u3
                     || (n2 + n4) as f64 / total < 0.03
                     || (n3 + n4) as f64 / total < 0.03)
                     == true
+                && low_sample_size == true
             {
                 // Sample size too low for doing association test
                 //p_value_original = None;
                 fisher_chisq_test_string = "NA".to_string();
-            } else if n1 > individual_fisher_limit
-                && n2 > individual_fisher_limit
-                && n3 > individual_fisher_limit
-                && n4 > individual_fisher_limit
+            } else if calculate_expected_value(1, n1, n2, n3, n4, total) > individual_fisher_limit
+                && calculate_expected_value(2, n1, n2, n3, n4, total) > individual_fisher_limit
+                && calculate_expected_value(3, n1, n2, n3, n4, total) > individual_fisher_limit
+                && calculate_expected_value(4, n1, n2, n3, n4, total) > individual_fisher_limit
             {
                 fisher_chisq_test = 2; // Setting test = chi-sq
                 let _fisher_chisq_test_final;
@@ -223,20 +281,26 @@ fn calculate_fisher_chisq_test(variants: &JsonValue, individual_fisher_limit: u3
         }
     }
 
-    // Calculate Benjamini Hochberg correction
+    // Calculate Benjamini Hochberg/Bonferroni correction
     match fdr {
         true => benjamini_hochberg_correction(p_values_list, num_of_tests),
         false => {
-            let mut output_string = "[".to_string();
-            for i in 0..p_values_list.len() {
-                output_string += &serde_json::to_string(&p_values_list[i]).unwrap();
-                if i != p_values_list.len() - 1 {
-                    output_string += &",".to_string();
+            match bon {
+                true => bonferroni_correction(p_values_list, num_of_tests),
+
+                false => {
+                    let mut output_string = "[".to_string();
+                    for i in 0..p_values_list.len() {
+                        output_string += &serde_json::to_string(&p_values_list[i]).unwrap();
+                        if i != p_values_list.len() - 1 {
+                            output_string += &",".to_string();
+                        }
+                    }
+                    output_string += &"]".to_string();
+                    //println!("output:{}", json::stringify(output_string));
+                    println!("{}", output_string);
                 }
             }
-            output_string += &"]".to_string();
-            //println!("output:{}", json::stringify(output_string));
-            println!("{}", output_string);
         }
     }
 }
@@ -310,4 +374,70 @@ fn benjamini_hochberg_correction(mut p_values_list: Vec<PValueIndexes>, num_of_t
     }
     output_string += &"]".to_string();
     println!("{}", output_string);
+}
+
+fn bonferroni_correction(p_values_list: Vec<PValueIndexes>, num_of_tests: f64) {
+    //println!("num_of_tests:{}", num_of_tests);
+    let mut adjusted_p_values = Vec::<AdjustedPValueIndexes>::new();
+    for i in 0..p_values_list.len() {
+        let mut adjusted_p_value: Option<f64> = None;
+        match p_values_list[i].p_value {
+            Some(p_val) => {
+                //println!("p_val:{}", p_val);
+                let adjusted_p_val: f64 = p_val / num_of_tests; // In bonferroni correction, dividing p_value by number of tests (excluding those with low sample sizes)
+                adjusted_p_value = Some(adjusted_p_val);
+            }
+            None => {}
+        }
+
+        adjusted_p_values.push(AdjustedPValueIndexes {
+            index: p_values_list[i].index,
+            n1: p_values_list[i].n1,
+            n2: p_values_list[i].n2,
+            n3: p_values_list[i].n3,
+            n4: p_values_list[i].n4,
+            p_value: p_values_list[i].p_value,
+            adjusted_p_value: adjusted_p_value,
+            fisher_chisq: p_values_list[i].fisher_chisq.clone(),
+        });
+    }
+
+    // Sorting original p-values by their original indexes in ascending order
+    adjusted_p_values
+        .as_mut_slice()
+        .sort_by(|a, b| (a.index).partial_cmp(&b.index).unwrap_or(Ordering::Equal));
+
+    // Printing original and adjusted p-values
+
+    let mut output_string = "[".to_string();
+    for i in 0..adjusted_p_values.len() {
+        output_string += &serde_json::to_string(&adjusted_p_values[i]).unwrap();
+        if i != adjusted_p_values.len() - 1 {
+            output_string += &",".to_string();
+        }
+    }
+    output_string += &"]".to_string();
+    println!("{}", output_string);
+}
+
+fn calculate_expected_value(
+    position: u32, // position must be between 1 and 4
+    n1: u32,
+    n2: u32,
+    n3: u32,
+    n4: u32,
+    total: f64,
+) -> f64 {
+    let mut expected_value: f64 = 0.0;
+    //total: f64 = (n1 + n2 + n3 + n4) as f64;
+    if position == 1 {
+        expected_value = ((n1 + n2) as f64) * ((n1 + n3) as f64) / total;
+    } else if position == 2 {
+        expected_value = ((n1 + n2) as f64) * ((n2 + n4) as f64) / total;
+    } else if position == 3 {
+        expected_value = ((n1 + n3) as f64) * ((n3 + n4) as f64) / total;
+    } else if position == 4 {
+        expected_value = ((n2 + n4) as f64) * ((n3 + n4) as f64) / total;
+    }
+    expected_value
 }
