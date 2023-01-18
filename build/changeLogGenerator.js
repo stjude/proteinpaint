f4041530274b324be9fe791de32caa97946ed6d6
PP CI <pp@stjude.org>
2023-01-18 14:12:55 -0600

    Add change log generator.

---
+++ build/changeLogGenerator.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+const changeLogFile = '../CHANGELOG.md'
+const oldChangeLogText = fs.readFileSync(changeLogFile).toString('utf-8')
+
+const releaseTextFile = '../release.txt'
+const releaseLogText = fs.readFileSync(releaseTextFile).toString('utf-8')
+
+if (releaseLogText === '') {
+	throw Error(`Release text in ${releaseTextFile} is empty`)
+}
+
+const header = '# Change Log\n' + '\n' + 'All notable changes to this project will be documented in this file.'
+
+const changeLogWithoutHeader = oldChangeLogText.replace(header, '')
+
+const version = require('../package.json').version
+
+const newChangeLog = `${header} 
+## ${version}
+
+${releaseLogText}
+${changeLogWithoutHeader}`
+
+fs.writeFileSync(changeLogFile, newChangeLog)
+fs.writeFileSync(releaseTextFile, '')
