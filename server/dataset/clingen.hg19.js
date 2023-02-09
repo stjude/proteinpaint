88925826dcd22964333cd1c65faee44fb5d37e76
Jian Wang <32489359+josephpowi@users.noreply.github.com>
2023-02-09 15:49:48 -0600

    add dataset/clingen.hg19.js (#1837)
    Co-authored-by: Wang <jwang7@d210581.stjude.sjcrh.local>

---
+++ server/dataset/clingen.hg19.js
@@ -0,0 +1,63 @@
+module.exports = {
+	isMds3: true,
+	genome: 'hg19',
+
+	// termdb as a generic interface
+	// getters will be added to abstract the detailed implementations
+	cohort: {
+		// expose matrix for testing. should be okay to merge to master as for now the ash dataset is not exposed from mass ui
+		allowedChartTypes: ['summary', 'matrix'],
+
+		db: { file: 'hg19/clingen/db' },
+		termdb: {
+			termid2totalsize2: {}
+		}
+	},
+	variant2samples: {
+		variantkey: 'ssm_id', // required, tells client to return ssm_id for identifying variants
+
+		// list of term ids as sample details
+		twLst: [
+			{ id: 'Lineage', q: {} }
+			//{ id: 'Primary_subtype', q: {} },
+			//{ id: 'Secondary_subtype', q: {} },
+			//{ id: 'Sex', q: {} },
+			//{ id: 'Age', q: {} },
+			//{ id: 'race', q: {} }
+		],
+
+		// small list of terms for sunburst rings
+		//sunburst_twLst: [{ id: 'Lineage', q: {} }, { id: 'Primary_subtype', q: {} }],
+		sunburst_twLst: [{ id: 'Lineage', q: {} }],
+
+		// quick fix: flag to indicate availability of these fields, so as to create new columns in sample table
+		sampleHasSsmReadDepth: true, // corresponds to .ssm_read_depth{} of a sample
+		sampleHasSsmTotalNormal: true // corresponds to .totalNormal:int of a sample
+
+		// either of sample_id_key or sample_id_getter will be required for making url link for a sample
+		//sample_id_key: 'case_id',
+
+		//url: {}
+	},
+	queries: {
+		// temporary fix for genomeBrowser app to show gene model
+		defaultBlock2GeneMode: true,
+
+		snvindel: {
+			forTrack: true,
+			byrange: {
+				bcffile: 'hg19/clingen/clinGen.hg19.bcf.gz'
+			}
+		},
+		svfusion: {
+			byrange: {
+				file: 'hg19/clingen/clinGen.svfusion.hg19.gz'
+			}
+		}
+		/*
+		cnvpileup:{},
+		geneexpression: {
+		},
+		*/
+	}
+}
