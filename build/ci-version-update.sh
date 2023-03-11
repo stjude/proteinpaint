27b0bb44f04e2a74d2317ef663d48164fc293e25
Aleksandar Acic <aacic@stjude.org>
2023-03-10 19:25:00 -0600

    Push a test branch from CI.

--- build/ci-version-update.sh
+++ build/ci-version-update.sh
@@ -181,5 +181,5 @@ git config --global user.name "PPTeam CI"
 git add --all
 git commit -m "$COMMITMSG"
 git tag $TAG
-#git push origin master
-#git push origin $TAG
+git push origin release-test
+git push origin $TAG
