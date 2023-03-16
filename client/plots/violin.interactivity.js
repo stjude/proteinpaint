fd217e5f33f53b321863945b4885914e41614946
kgangwan <kgangwan@stjude.org>
2023-03-16 16:35:12 -0500

    refactoring

--- client/plots/violin.interactivity.js
+++ client/plots/violin.interactivity.js
@@ -1,97 +1,194 @@
 import { filterJoin, getFilterItemByTag } from '../filter/filter'
 import { renderTable } from '#dom/table'
+import { to_svg } from '#src/client'
 
-export function displayLabelClickMenu(t1, t2, plot, event, self) {
-	displayLabelClickMenu.called = true
-	displayMenu(t1, t2, self, plot, event, null, null)
-}
+export function setInteractivity(self) {
+	self.download = function() {
+		if (!self.state) return
 
-export async function displayBrushMenu(t1, t2, self, plot, selection, scale, isH) {
-	const start = isH ? scale.invert(selection[0]) : scale.invert(selection[1])
-	const end = isH ? scale.invert(selection[1]) : scale.invert(selection[0])
-	displayBrushMenu.called = true
-	displayMenu(t1, t2, self, plot, event, start, end)
-	// const brushValues = plot.values.filter(i => i > start && i < end)
-}
+		// has to be able to handle multichart view
+		// const mainGs = []
+		// const translate = { x: undefined, y: undefined }
+		// const titles = []
+		// let maxw = 0,
+		// 	maxh = 0,
+		// 	tboxh = 0
+		// let prevY = 0,
+		// 	numChartsPerRow = 0
 
-function displayMenu(t1, t2, self, plot, event, start, end) {
-	self.app.tip.d.selectAll('*').remove()
-
-	const options = []
-
-	if (displayLabelClickMenu.called === true) {
-		if (t2) {
-			if (t1.term.type === 'categorical') {
-				options.push({
-					label: `Add filter: ${plot.label.split(',')[0]}`,
-					callback: getAddFilterCallback(t1, t2, self, plot, 'term1')
-				})
-			} else {
-				options.push({
-					label: `Add filter: ${plot.label.split(',')[0]}`,
-					callback: getAddFilterCallback(t1, t2, self, plot, 'term2')
-				})
-			}
-			//On label clicking, display 'Hide' option to hide plot.
-			options.push({
-				label: `Hide: ${plot.label}`,
-				callback: () => {
-					const termNum = t2 ? 'term2' : null
-					const term = self.config[termNum]
-					const isHidden = true
-					self.app.dispatch({
-						type: 'plot_edit',
-						id: self.id,
-						config: {
-							[termNum]: {
-								isAtomic: true,
-								id: term.id,
-								term: term.term,
-								q: getUpdatedQfromClick(plot, t2, isHidden)
-							}
-						}
-					})
-				}
-			})
-		}
-		if (self.config.settings.violin.displaySampleIds && self.state.hasVerifiedToken) {
-			options.push({
-				label: `List samples`,
-				callback: async () => listSamples(self, event, t1, t2, plot, self.data.min, self.data.max * 2)
-			})
-		}
-		displayLabelClickMenu.called = false
-	} else if (displayBrushMenu.called === true) {
-		options.push({
-			label: `Add filter: ${start.toFixed(1)} < x < ${end.toFixed(1)}`,
-			callback: getAddFilterCallback(t1, t2, self, plot, start, end)
+		self.dom.violinDiv.selectAll('.sjpp-violin-plot').each(function() {
+			to_svg(this, 'violin', { apply_dom_styles: true })
 		})
-
-		if (self.config.settings.violin.displaySampleIds && self.state.hasVerifiedToken) {
-			options.push({
-				label: `List samples`,
-				callback: async () => listSamples(self, event, t1, t2, plot, start, end)
-			})
-		}
-		displayBrushMenu.called = false
 	}
 
-	//show menu options for label clicking and brush selection
-	self.app.tip.d
-		.append('div')
-		.selectAll('div')
-		.data(options)
-		.enter()
-		.append('div')
-		.attr('class', 'sja_menuoption')
-		.text(d => d.label)
-		.on('click', (event, d) => {
-			self.app.tip.hide()
-			d.callback()
-			self.dom.tableHolder.style('display', 'none')
+	self.displayLabelClickMenu = function(t1, t2, plot, event, self) {
+		self.displayLabelClickMenu.called = true
+		self.displayMenu(t1, t2, self, plot, event, null, null)
+	}
+
+	self.displayBrushMenu = function(t1, t2, self, plot, selection, scale, isH) {
+		const start = isH ? scale.invert(selection[0]) : scale.invert(selection[1])
+		const end = isH ? scale.invert(selection[1]) : scale.invert(selection[0])
+		self.displayBrushMenu.called = true
+		self.displayMenu(t1, t2, self, plot, event, start, end)
+		// const brushValues = plot.values.filter(i => i > start && i < end)
+	}
+
+	self.displayMenu = function(t1, t2, self, plot, event, start, end) {
+		self.app.tip.d.selectAll('*').remove()
+
+		const options = []
+
+		if (self.displayLabelClickMenu.called === true) {
+			if (t2) {
+				if (t1.term.type === 'categorical') {
+					options.push({
+						label: `Add filter: ${plot.label.split(',')[0]}`,
+						callback: getAddFilterCallback(t1, t2, self, plot, 'term1')
+					})
+				} else {
+					options.push({
+						label: `Add filter: ${plot.label.split(',')[0]}`,
+						callback: getAddFilterCallback(t1, t2, self, plot, 'term2')
+					})
+				}
+				//On label clicking, display 'Hide' option to hide plot.
+				options.push({
+					label: `Hide: ${plot.label}`,
+					callback: () => {
+						const termNum = t2 ? 'term2' : null
+						const term = self.config[termNum]
+						const isHidden = true
+						self.app.dispatch({
+							type: 'plot_edit',
+							id: self.id,
+							config: {
+								[termNum]: {
+									isAtomic: true,
+									id: term.id,
+									term: term.term,
+									q: getUpdatedQfromClick(plot, t2, isHidden)
+								}
+							}
+						})
+					}
+				})
+			}
+			if (self.config.settings.violin.displaySampleIds && self.state.hasVerifiedToken) {
+				options.push({
+					label: `List samples`,
+					callback: async () => self.listSamples(self, event, t1, t2, plot, self.data.min, self.data.max * 2)
+				})
+			}
+			self.displayLabelClickMenu.called = false
+		} else if (self.displayBrushMenu.called === true) {
+			options.push({
+				label: `Add filter: ${start.toFixed(1)} < x < ${end.toFixed(1)}`,
+				callback: getAddFilterCallback(t1, t2, self, plot, start, end)
+			})
+
+			if (self.config.settings.violin.displaySampleIds && self.state.hasVerifiedToken) {
+				options.push({
+					label: `List samples`,
+					callback: async () => self.listSamples(self, event, t1, t2, plot, start, end)
+				})
+			}
+			self.displayBrushMenu.called = false
+		}
+
+		//show menu options for label clicking and brush selection
+		self.app.tip.d
+			.append('div')
+			.selectAll('div')
+			.data(options)
+			.enter()
+			.append('div')
+			.attr('class', 'sja_menuoption')
+			.text(d => d.label)
+			.on('click', (event, d) => {
+				self.app.tip.hide()
+				d.callback()
+				self.dom.tableHolder.style('display', 'none')
+			})
+
+		self.app.tip.show(event.clientX, event.clientY)
+	}
+
+	self.listSamples = async function(self, event, t1, t2, plot, start, end) {
+		const tvslst = getTvsLst(t1, t2, plot, start, end)
+		const term = t1.q?.mode === 'continuous' ? t1 : t2
+		const filter = {
+			type: 'tvslst',
+			join: 'and',
+			lst: [self.state.termfilter.filter, tvslst],
+			in: true
+		}
+		const opts = {
+			terms: [term],
+			filter
+		}
+		//getAnnotatedSampleData is used to retrieve sample id's and values (see matrix.js).
+		const data = await self.app.vocabApi.getAnnotatedSampleData(opts)
+		self.displaySampleIds(event, self, term, data)
+	}
+
+	self.displaySampleIds = function(event, self, term, data) {
+		self.app.tip.clear()
+		if (!data?.samples) return
+		const sampleIdArr = []
+		for (const [c, k] of Object.entries(data.samples)) {
+			const sampleIdObj = {}
+			if (data.refs.bySampleId[c]) {
+				sampleIdObj[data.refs.bySampleId[c]] = k[term.$id].value
+			}
+			sampleIdArr.push([{ value: Object.keys(sampleIdObj) }, { value: Object.values(sampleIdObj) }])
+		}
+		const tableDiv = self.app.tip.d.append('div')
+		const columns = [{ label: 'Sample Id' }, { label: 'Value' }]
+		const rows = sampleIdArr
+
+		renderTable({
+			rows,
+			columns,
+			div: tableDiv,
+			showLines: false,
+			maxWidth: '30vw',
+			maxHeight: '25vh',
+			resize: true,
+			showLines: true
 		})
 
-	self.app.tip.show(event.clientX, event.clientY)
+		self.app.tip.show(event.clientX, event.clientY)
+	}
+
+	self.labelHideLegendClicking = function(t2, plot, self) {
+		self.dom.legendDiv.selectAll('.sjpp-htmlLegend').on('click', event => {
+			event.stopPropagation()
+			const d = event.target.__data__
+			if (t2) {
+				for (const key of Object.keys(t2?.q?.hiddenValues)) {
+					if (d.text === key) {
+						delete self.config.term2.q.hiddenValues[key]
+					}
+				}
+				const termNum = self.config.term2 ? 'term2' : null
+				const term = self.config[termNum]
+				const isHidden = false
+				self.app.dispatch({
+					type: 'plot_edit',
+					id: self.id,
+					config: {
+						[termNum]: {
+							isAtomic: true,
+							id: term.id,
+							term: term.term,
+							q: getUpdatedQfromClick(plot, t2, isHidden)
+						}
+					}
+				})
+			}
+		})
+	}
 }
 
 function getAddFilterCallback(t1, t2, self, plot, rangeStart, rangeStop) {
@@ -106,7 +203,7 @@ function getAddFilterCallback(t1, t2, self, plot, rangeStart, rangeStop) {
 		if (t1.term.type === 'categorical') {
 			createTvsLstValues(t1, plot, tvslst, 0)
 
-			if (displayBrushMenu.called === true) {
+			if (self.displayBrushMenu.called === true) {
 				createTvsLstRanges(t2, tvslst, rangeStart, rangeStop, 1)
 			}
 		} else if (
@@ -128,17 +225,17 @@ function getAddFilterCallback(t1, t2, self, plot, rangeStart, rangeStop) {
 						: null
 				}
 			]
-			if (displayBrushMenu.called === true) {
+			if (self.displayBrushMenu.called === true) {
 				createTvsLstRanges(t1, tvslst, rangeStart, rangeStop, 1)
 			}
 		} else {
 			createTvsLstValues(t2, plot, tvslst, 0)
-			if (displayBrushMenu.called === true) {
+			if (self.displayBrushMenu.called === true) {
 				createTvsLstRanges(t1, tvslst, rangeStart, rangeStop, 1)
 			}
 		}
 	} else {
-		if (displayBrushMenu.called === true) {
+		if (self.displayBrushMenu.called === true) {
 			createTvsLstRanges(t1, tvslst, rangeStart, rangeStop, 0)
 		}
 	}
@@ -163,53 +260,6 @@ function getUpdatedQfromClick(plot, term, isHidden = false) {
 	if (isHidden) q.hiddenValues[id] = 1
 	else delete q.hiddenValues[id]
 	return q
-}
-
-async function listSamples(self, event, t1, t2, plot, start, end) {
-	const tvslst = getTvsLst(t1, t2, plot, start, end)
-	const term = t1.q?.mode === 'continuous' ? t1 : t2
-	const filter = {
-		type: 'tvslst',
-		join: 'and',
-		lst: [self.state.termfilter.filter, tvslst],
-		in: true
-	}
-	const opts = {
-		terms: [term],
-		filter
-	}
-	//getAnnotatedSampleData is used to retrieve sample id's and values (see matrix.js).
-	const data = await self.app.vocabApi.getAnnotatedSampleData(opts)
-	displaySampleIds(event, self, term, data)
-}
-
-function displaySampleIds(event, self, term, data) {
-	self.app.tip.clear()
-	if (!data?.samples) return
-	const sampleIdArr = []
-	for (const [c, k] of Object.entries(data.samples)) {
-		const sampleIdObj = {}
-		if (data.refs.bySampleId[c]) {
-			sampleIdObj[data.refs.bySampleId[c]] = k[term.$id].value
-		}
-		sampleIdArr.push([{ value: Object.keys(sampleIdObj) }, { value: Object.values(sampleIdObj) }])
-	}
-	const tableDiv = self.app.tip.d.append('div')
-	const columns = [{ label: 'Sample Id' }, { label: 'Value' }]
-	const rows = sampleIdArr
-
-	renderTable({
-		rows,
-		columns,
-		div: tableDiv,
-		showLines: false,
-		maxWidth: '30vw',
-		maxHeight: '25vh',
-		resize: true,
-		showLines: true
-	})
-
-	self.app.tip.show(event.clientX, event.clientY)
 }
 
 function createTvsLstValues(term, plot, tvslst, lstIdx) {
@@ -279,33 +329,4 @@ function getTvsLst(t1, t2, plot, rangeStart, rangeStop) {
 		}
 	} else createTvsLstRanges(t1, tvslst, rangeStart, rangeStop, 0)
 	return tvslst
-}
-
-export function labelHideLegendClicking(t2, plot, self) {
-	self.dom.legendDiv.selectAll('.sjpp-htmlLegend').on('click', event => {
-		event.stopPropagation()
-		const d = event.target.__data__
-		if (t2) {
-			for (const key of Object.keys(t2?.q?.hiddenValues)) {
-				if (d.text === key) {
-					delete self.config.term2.q.hiddenValues[key]
-				}
-			}
-			const termNum = self.config.term2 ? 'term2' : null
-			const term = self.config[termNum]
-			const isHidden = false
-			self.app.dispatch({
-				type: 'plot_edit',
-				id: self.id,
-				config: {
-					[termNum]: {
-						isAtomic: true,
-						id: term.id,
-						term: term.term,
-						q: getUpdatedQfromClick(plot, t2, isHidden)
-					}
-				}
-			})
-		}
-	})
 }
