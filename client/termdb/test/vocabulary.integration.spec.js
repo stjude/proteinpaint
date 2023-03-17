71ae6f20bce0c48ca1e8c8aea6cdd62068756d47
Colleen Reilly <creilly@stjude.org>
2023-03-17 15:41:52 -0500

    fixed integration test; new vocab tests; new TestApp

--- client/termdb/test/vocabulary.integration.spec.js
+++ client/termdb/test/vocabulary.integration.spec.js
@@ -32,14 +32,13 @@ function getHolder() {
  test sections
 **************
 vocabInit()
-	vocabInit(), default
-	getVocab(), custom
-	Missing .state
+    vocabInit(), default
+    getVocab(), custom
+    Missing .state
 TermdbVocab
-	TermdbVocab()
+    TermdbVocab()
 FrontendVocab
-	FrontendVocab()
-	Missing state.vocab
+    Missing state.vocab
 */
 
 tape('\n', function(test) {
@@ -49,6 +48,8 @@ tape('\n', function(test) {
 
 tape('vocabInit(), default', test => {
 	/* Tests TermdbVocab indirectly */
+	test.timeoutAfter(100)
+
 	const app = {
 		opts: {
 			state: {
@@ -72,19 +73,46 @@ tape('vocabInit(), default', test => {
 		'function',
 		'should have a vocab.getNestedChartSeriesData function'
 	)
+	test.equal(
+		typeof vocabApi.mayFillInMissingCatValues,
+		'function',
+		'should have a vocab.mayFillInMissingCatValues function'
+	)
+	test.equal(typeof vocabApi.getTdbDataUrl, 'function', 'should have a vocab.getTdbDataUrl function')
+	test.equal(typeof vocabApi.syncTermData, 'function', 'should have a vocab.syncTermData function')
 	test.equal(typeof vocabApi.getRegressionData, 'function', 'should have a vocab.getRegressionData function')
 	test.equal(typeof vocabApi.findTerm, 'function', 'should have a vocab.findTerm function')
+	test.equal(typeof vocabApi.getTermInfo, 'function', 'should have a vocab.getTermInfo function')
 	test.equal(typeof vocabApi.getCohortSampleCount, 'function', 'should have a vocab.getCohortSampleCount function')
 	test.equal(typeof vocabApi.getFilteredSampleCount, 'function', 'should have a vocab.getFilteredSampleCount function')
+	test.equal(typeof vocabApi.getViolinPlotData, 'function', 'should have a vocab.getViolinPlotData function')
+	test.equal(typeof vocabApi.getPercentile, 'function', 'should have a vocab.getPercentile function')
+	test.equal(typeof vocabApi.getDescrStats, 'function', 'should have a vocab.getDescrStats function')
+	test.equal(typeof vocabApi.getterm, 'function', 'should have a vocab.getterm function')
+	test.equal(typeof vocabApi.graphable, 'function', 'should have a vocab.graphable function')
+	test.equal(typeof vocabApi.getCategories, 'function', 'should have a vocab.getCategories function')
+	test.equal(
+		typeof vocabApi.getNumericUncomputableCategories,
+		'function',
+		'should have a vocab.getNumericUncomputableCategories function'
+	)
+	test.equal(typeof vocabApi.getConditionCategories, 'function', 'should have a vocab.getConditionCategories function')
+	test.equal(typeof vocabApi.validateSnps, 'function', 'should have a vocab.validateSnps function')
+	test.equal(typeof vocabApi.get_variantFilter, 'function', 'should have a vocab.get_variantFilter function')
+	test.equal(typeof vocabApi.getAnnotatedSampleData, 'function', 'should have a vocab.getAnnotatedSampleData function')
+	test.equal(typeof vocabApi.getTwMinCopy, 'function', 'should have a vocab.getTwMinCopy function')
+	test.equal(typeof vocabApi.getTermTypes, 'function', 'should have a vocab.getTermTypes function')
+	test.equal(typeof vocabApi.getLDdata, 'function', 'should have a vocab.getLDdata function')
+	test.equal(typeof vocabApi.getScatterData, 'function', 'should have a vocab.getScatterData function')
+	test.equal(typeof vocabApi.getCohortsData, 'function', 'should have a vocab.getCohortsData function')
+	test.equal(typeof vocabApi.getMds3queryDetails, 'function', 'should have a vocab.getMds3queryDetails function')
 
-	//getPercentile
-	//syncTermData
-	//getAnnotatedSampleData
 	test.end()
 })
 
 tape('getVocab(), custom', test => {
 	/* Tests FrontendVocab indirectly */
+	test.timeoutAfter(100)
 
 	runpp({
 		state: {
@@ -104,28 +132,39 @@ tape('getVocab(), custom', test => {
 			'function',
 			'should have a vocab.getNestedChartSeriesData function'
 		)
-		//test.equal(typeof app.vocabApi.getRegressionData, 'function', 'should have a vocab.getRegressionData function')
+		test.equal(typeof app.vocabApi.syncTermData, 'function', 'should have a vocab.syncTermData function')
 		test.equal(typeof app.vocabApi.findTerm, 'function', 'should have a vocab.findTerm function')
+		test.equal(typeof app.vocabApi.getTermInfo, 'function', 'should have a vocab.getTermInfo function')
 		test.equal(
 			typeof app.vocabApi.getCohortSampleCount,
 			'function',
 			'should have a vocab.getCohortSampleCount function'
 		)
+		// Frontend.getFilteredSampleCount() returns { samplecount: 'TBD' }??
+		test.equal(typeof app.vocabApi.getDensityPlotData, 'function', 'should have a vocab.getDensityPlotData function')
+		test.equal(typeof app.vocabApi.getPercentile, 'function', 'should have a vocab.getPercentile function')
+		test.equal(typeof app.vocabApi.getDescrStats, 'function', 'should have a vocab.getDescrStats function')
+		test.equal(typeof app.vocabApi.getterm, 'function', 'should have a vocab.getterm function')
+		test.equal(typeof app.vocabApi.getCategories, 'function', 'should have a vocab.getCategories function')
 		test.equal(
-			typeof app.vocabApi.getFilteredSampleCount,
+			typeof app.vocabApi.getNumericUncomputableCategories,
 			'function',
-			'should have a vocab.getFilteredSampleCount function'
+			'should have a vocab.getNumericUncomputableCategories function'
 		)
+		test.equal(
+			typeof app.vocabApi.getConditionCategories,
+			'function',
+			'should have a vocab.getConditionCategories function'
+		)
+		test.equal(typeof app.vocabApi.graphable, 'function', 'should have a vocab.graphable function')
+		test.equal(typeof app.vocabApi.q_to_param, 'function', 'should have a vocab.q_to_param function')
 
-		//syncTermData
-		//getDensityPlotData
-		//getPercentile
 		test.end()
 	}
 })
 
 tape('Missing .state', test => {
-	test.timeoutAfter(3000)
+	test.timeoutAfter(100)
 
 	const app = {
 		opts: {
@@ -144,8 +183,23 @@ tape('Missing .state', test => {
 	test.end()
 })
 
+/* TermdbVocab tests 
+
+//getTdbDataUrl
+//syncTermData
+
+//getPercentile
+//syncTermData
+//getAnnotatedSampleData
+    
+*/
+tape('\n', function(test) {
+	test.pass('-***- TermdbVocab Tests -***-')
+	test.end()
+})
+
 tape.skip('TermdbVocab()', test => {
-	test.timeoutAfter(3000)
+	test.timeoutAfter(100)
 
 	const app = {
 		opts: {
@@ -162,63 +216,21 @@ tape.skip('TermdbVocab()', test => {
 		fetchOpts: app.opts.fetchOpts
 	})
 
-	//getTdbDataUrl
-	//syncTermData
-
 	test.end()
 })
 
-tape('FrontendVocab()', test => {
-	test.timeoutAfter(3000)
-	const app = {
-		state: {
-			vocab: {
-				terms: vocabData.terms,
-				selectCohort: {
-					term: { id: 'subcohort', type: 'categorical' },
-					values: [
-						{
-							keys: ['ABC'],
-							label: 'ABC Lifetime Cohort (ABC)',
-							shortLabel: 'ABC',
-							isdefault: true
-						},
-						{
-							keys: ['XYZ'],
-							label: 'XYZ Cancer Survivor Study (XYZ)',
-							shortLabel: 'XYZ'
-						}
-					]
-				}
-			}
-		}
-	}
+/* FrontendVocab tests 
 
-	const frontendVocabApi = new FrontendVocab({
-		app,
-		state: app.state,
-		fetchOpts: app.fetchOpts
-	})
-
-	test.ok(
-		Array.isArray(frontendVocabApi.getTermdbConfig().supportedChartTypes),
-		'Should return supportedChartTypes from vocab.getTermdbConfig'
-	)
-	const selectCohort = frontendVocabApi.getTermdbConfig().selectCohort
-	const validateSelectCohort =
-		typeof selectCohort.term == 'object' &&
-		selectCohort.term.id &&
-		selectCohort.term.type &&
-		Array.isArray(selectCohort.values)
-			? true
-			: false
-	test.ok(validateSelectCohort, 'Should include all required keys for .selectCohort')
-
+//syncTermData
+//getDensityPlotData
+*/
+tape('\n', function(test) {
+	test.pass('-***- FrontendVocab Tests -***-')
 	test.end()
 })
 
 tape('Missing state.vocab', test => {
-	test.timeoutAfter(3000)
+	test.timeoutAfter(100)
 
 	const app = {
 		opts: {
