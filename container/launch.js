73f9cf41e087de31c9301a8609e5fbc4eaf6b962
Edgar Sioson <siosonel@gmail.com>
2023-03-19 17:14:38 -0500

    fix container build tags; simply download a prebuilt docker image in container package postinstall

--- container/launch.js
+++ container/launch.js
@@ -6,15 +6,15 @@ const spawnSync = require('child_process').spawnSync
 // from the current working directory
 spawnSync(`rm -rf public/cards`)
 
-const mode = process.argv[2]
+const mode = process.argv[2] || 'full'
 const run = path.join(__dirname, 'run.sh')
 
 try {
-	const img = mode ? `pp${mode}:latest` : `ppfull:latest`
+	const img = `ghcr.io/stjude/pp${mode}:latest`
 	console.log(`starting ${img} ...`)
-	const out = spawnSync('bash', [run, img, 'pp'], {encoding: 'utf-8'})
+	const out = spawnSync('bash', [run, img, 'pp'], { encoding: 'utf-8' })
 	if (out.stderr) throw out.stderr
 	else console.log(out.stdout)
-} catch(e) {
+} catch (e) {
 	console.error(e)
 }
