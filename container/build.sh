255e9f2fc27ea0b59f96bd76d84ce26193d85e57
Edgar <siosonel@gmail.com>
2023-03-21 14:09:53 -0700

    fix container build tags; simply download a prebuilt docker image (#36)
    * fix container build tags; simply download a prebuilt docker image in container package postinstall
    
    * mount a dataset dir to the container, either empty or containing user-supplied dataset js files

--- container/build.sh
+++ container/build.sh
@@ -7,15 +7,15 @@ set -euxo pipefail
 ###############
 
 USAGE="Usage:
-	./build.sh [-m] [-r] [-b] [-c]
+	./build.sh [-m] [-r] [-b] [-c] [-t]
 
 	-m MODE: string to loosely indicate the build environment.
 			 - defaults to an empty string
-			 - notdev indicates build outside of the repo or dev environment
+			 - 'pkg' is reserved to indicate a package build, outside of the repo or dev environment
 			 - will be used as a prefix for the image name
 	-b BUILDARGS: build variables to pass to the Dockerfile that are not persisted to the built image
 	-c CROSSENV: cross-env options that are used prior to npm install
-	-t GIT_PAT: git personal access token to use in .npmrc
+	-t GIT_PAT: github personal access token to use in .npmrc
 "
 BUILDARGS=""
 CROSSENV=""
@@ -25,9 +25,6 @@ while getopts "m:r:b:c:h:x:t:" opt; do
 	case "${opt}" in
 	m)
 		MODE=${OPTARG}
-		;;
-	r)
-		REV=${OPTARG}
 		;;
 	b)
 		BUILDARGS=${OPTARG}
@@ -49,8 +46,8 @@ while getopts "m:r:b:c:h:x:t:" opt; do
 	esac
 done
 
-if [[ "$MODE" == "notdev" && -d ../client ]]; then
-	echo "post-install build skipped within repo"
+if [[ "$MODE" == "pkg" && -d "../client" ]]; then
+	echo "post-install pkg build skipped within repo"
 	exit 0
 fi
 
@@ -66,29 +63,57 @@ if [[ ${ARCH} == "arm64" ]]; then ARCH="aarch64"; fi
 # Docker build
 #########################
 # !!! FOR TESTING ONLY --- REMOVE .npmrc BEFORE PUSHING !!!
-# !!! once PP is open-sourced, the .npmrc should only have the registry URL for the @stjude namespace !!!
-echo "@stjude:registry=https://npm.pkg.github.com/" > .npmrc
-if [[ $GIT_PAT != "" ]]; then
-  # pragma: allowlist nextline secret
-	echo "//npm.pkg.github.com/:_authToken="$GIT_PAT | cat - .npmrc > temp && mv temp .npmrc
+# !!! once PP uses a public npm registry that does not require a token, 
+# then at most the .npmrc should only have the registry URL for the @stjude namespace !!!
+if [[ "$GIT_PAT" != "" ]]; then
+	echo "@stjude:registry=https://npm.pkg.github.com/" > .npmrc
+	# pragma: allowlist nextline secret
+	echo "//npm.pkg.github.com/:_authToken=$GIT_PAT" >> .npmrc
+elif [[ ! -f .npmrc ]]; then
+	if [[ ! -f ~/.npmrc ]]; then
+		echo "either an .npmrc in the home or working directory, or a [-t Github personal access token] argument, is required"
+		exit 1
+	fi
+	cp ~/.npmrc .
 fi
 
+IMGVER="$(node -p "require('./package.json').version")"
 # assumes that the branch head is currently checked out
-REV=latest # $(git rev-parse --short HEAD)
-TAG="$(node -p "require('./package.json').version")"
+IMGREV="head"
+set +e
+HASH=$(git rev-parse --short HEAD 2>/dev/null)
+set -e
+if [[ "$HASH" != "" ]]; then
+	IMGREV="$HASH"
+fi
 SERVERPKGVER="$(node -p "require('./package.json').containerDeps.server")"
 FRONTPKGVER="$(node -p "require('./package.json').containerDeps.front")"
 
-echo "building ppbase:$REV image, package version=$TAG"
-docker build . --file ./Dockerfile --target ppbase --tag "${MODE}ppbase:latest" --tag "${MODE}ppbase:$REV" --build-arg ARCH="$ARCH" $BUILDARGS
+echo "building ${MODE}ppbase image"
+docker build . --file ./Dockerfile --target ppbase --tag "${MODE}ppbase:latest" --build-arg ARCH="$ARCH" $BUILDARGS
 
-echo "building pprust:$REV image, package version=$TAG"
-docker build . --file ./Dockerfile --target pprust --tag "${MODE}pprust:latest" --tag "${MODE}ppbase:$REV" --build-arg ARCH="$ARCH" $BUILDARGS
+echo "building ${MODE}pprust image"
+docker build . --file ./Dockerfile --target pprust --tag "${MODE}pprust:latest" --build-arg ARCH="$ARCH" $BUILDARGS
 
-echo "building ppserver:$REV image, package version=$TAG"
-docker build . --file ./Dockerfile --target ppserver --tag "${MODE}ppserver:latest" --tag "${MODE}ppserver:$TAG" --tag "${MODE}ppserver:$REV" --build-arg IMGVER=$REV --build-arg SERVERPKGVER=$SERVERPKGVER --build-arg CROSSENV="$CROSSENV" $BUILDARGS
+echo "building ${MODE}ppserver image"
+docker build . --file ./Dockerfile --target ppserver --tag "${MODE}ppserver:latest" --build-arg IMGVER=$IMGVER --build-arg IMGREV=$IMGREV --build-arg SERVERPKGVER=$SERVERPKGVER --build-arg CROSSENV="$CROSSENV" $BUILDARGS
 
-echo "building ppfull:$REV image, package version=$TAG"
-docker build . --file ./Dockerfile --target ppapp --tag "${MODE}ppfull:latest" --tag "${MODE}ppfull:$TAG" --tag "${MODE}ppfull:$REV" --build-arg IMGVER=$REV --build-arg SERVERPKGVER=$SERVERPKGVER --build-arg FRONTPKGVER=$FRONTPKGVER --build-arg CROSSENV="$CROSSENV" $BUILDARGS
+echo "building ${MODE}ppfull image"
+docker build . --file ./Dockerfile --target ppfull --tag "${MODE}ppfull:latest" --build-arg IMGVER=$IMGVER --build-arg IMGREV=$IMGREV --build-arg SERVERPKGVER=$SERVERPKGVER --build-arg FRONTPKGVER=$FRONTPKGVER --build-arg CROSSENV="$CROSSENV" $BUILDARGS
 
-[ "$(ls -Alq $(pwd)/tmppack | grep -q . )" ] || rm -r $PWD/tmppack
+# in non-dev/repo environment, may automatically add extra tags
+if [[ "$MODE" != "" ]]; then
+	for target in server full; do
+		docker tag "${MODE}pp${target}:latest" "${MODE}pp${target}:$IMGVER"
+	done
+
+	if [[ "$HASH" != "" ]]; then
+		for target in base rust server full; do
+			docker tag "${MODE}pp${target}:latest" "${MODE}pp${target}:$HASH"
+		done
+	fi
+fi
+
+if [[ "$(ls -Alq $PWD/tmppack | grep -q . )" ]]; then
+	rm -r $PWD/tmppack
+fi
