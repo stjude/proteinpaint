0a0eaf82b758658cbfc84ab40ff9184130c3c198
Airen Zaldivar <airenzp@gmail.com>
2023-03-29 12:01:27 -0500

    Fix to check second group others using in. Removed group name from the q and read from the values

--- server/src/termdb.sql.samplelst.js
+++ server/src/termdb.sql.samplelst.js
@@ -4,24 +4,17 @@ export const sampleLstSql = {
 			samples,
 			samplesString
 		for (const [i, group] of tw.q.groups.entries()) {
+			const name = Object.keys(tw.term.values)[i]
 			samples = group.values.map(value => value.sampleId)
 			samplesString = samples.map(() => '?').join(',')
-			if (i == 1 && group.name == 'Others') {
-				sql += `
-				SELECT id as sample, ? as key, ? as value
-				FROM sampleidmap
-				WHERE sample NOT IN (${samplesString})`
-				values.push(group.name, group.name, ...samples)
-				break
-			}
 
 			sql += `SELECT id as sample, ? as key, ? as value
 				FROM sampleidmap
-				WHERE sample IN (${samplesString})
+				WHERE sample ${group.in ? '' : 'NOT'} IN (${samplesString})
 			`
 			if (i != tw.q.groups.length - 1) sql += 'UNION ALL '
 
-			values.push(group.name, group.name, ...samples)
+			values.push(name, name, ...samples)
 		}
 		return { sql: `${tablename} AS (${sql})`, tablename }
 	}
