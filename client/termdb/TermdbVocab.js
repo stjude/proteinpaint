15e8bb22cf9beb0b1d48f354a138f649b8d0b01c
Airen Zaldivar <airenzp@gmail.com>
2023-03-29 18:42:28 -0500

    Fixed matrix bug

--- client/termdb/TermdbVocab.js
+++ client/termdb/TermdbVocab.js
@@ -802,6 +802,7 @@ export class TermdbVocab extends Vocab {
 		} else {
 			copy.term.name = tw.term.name
 			copy.term.type = tw.term.type
+			copy.term.values = tw.term.values
 		}
 		return copy
 	}
