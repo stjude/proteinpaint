74d501524f44cb6610b3f795d3512295f0871b8d
Edgar Sioson <siosonel@gmail.com>
2023-04-02 15:30:03 -0500

    support click to trigger the svg scroll and improve the styling

--- client/dom/svg.scroll.js
+++ client/dom/svg.scroll.js
@@ -4,22 +4,11 @@ export function svgScroll(_opts) {
 	if (!_opts.holder) throw `missing svgScroll.opts.holder argument`
 	const defaults = {
 		height: 12,
-		zoomLevel: 1
+		zoomLevel: 1,
+		opacity: 0.7
 	}
 
 	const opts = Object.assign({}, defaults, _opts)
-
-	opts.holder.attr('display', 'none')
-
-	// assumes horizontal for now
-	const rect = opts.holder
-		.append('rect')
-		.style('height', opts.height)
-		.style('stroke', '#999')
-		.style('stroke-width', 1)
-		.style('fill', '#fff')
-
-	const ref = {}
 
 	function scrollInit(e) {
 		ref.x = e.clientX
@@ -47,18 +36,76 @@ export function svgScroll(_opts) {
 			.on('mouseup.sjppSvgScroll', null)
 	}
 
+	function scrollByClick(e) {
+		ref.x = e.clientX
+		const i = e.clientX < slideElem.getBoundingClientRect().x ? -1 : 1
+		scrollStop({ clientX: Math.round(ref.x + i * ref.arrowDx) })
+	}
+
+	opts.holder
+		.attr('display', 'none')
+		.attr('opacity', opts.opacity)
+		.on('mouseover', () => opts.holder.attr('opacity', 1))
+		.on('mouseout', () => opts.holder.attr('opacity', opts.opacity))
+
+	// assumes horizontal for now
+	const rect = opts.holder
+		.append('rect')
+		.style('height', opts.height)
+		.style('stroke', 'none')
+		//.style('stroke-width', 1)
+		//.attr('rx', opts.height / 2)
+		.style('fill', '#fff')
+		.on('click', scrollByClick)
+
+	const line = opts.holder
+		.append('line')
+		.style('stroke', '#ccc')
+		.style('stroke-width', 1)
+		.on('click', scrollByClick)
+
+	const leftArrow = opts.holder
+		.append('path')
+		.attr('y', -opts.height)
+		.attr('d', `M0,${opts.height / 2}L${opts.height},0L${opts.height},${opts.height}Z`)
+		.style('stroke', '#ccc')
+		.style('stroke-width', 1)
+		//.attr('rx', opts.height / 2)
+		.style('fill', '#ccc')
+		.on('click', e => {
+			ref.x = e.clientX
+			scrollStop({ clientX: Math.round(ref.x - ref.arrowDx) })
+		})
+
+	const rtArrow = opts.holder
+		.append('path')
+		.attr('y', -opts.height)
+		.style('stroke', '#ccc')
+		.style('stroke-width', 1)
+		//.attr('rx', opts.height / 2)
+		.style('fill', '#ccc')
+		.attr('d', `M0,0L${opts.height},${opts.height / 2}L0,${opts.height}Z`)
+		.on('click', e => {
+			ref.x = e.clientX
+			scrollStop({ clientX: Math.round(ref.x + ref.arrowDx) })
+		})
+
+	const sliderHt = opts.height - 4
 	const slider = opts.holder
 		.append('rect')
-		.attr('y', 1)
-		.attr('height', opts.height - 2)
+		.attr('y', 2)
+		.attr('height', sliderHt)
 		.attr('stroke', '#aaa')
 		.attr('stroke-width', 1)
 		.attr('fill', '#ccc')
-		.attr('rx', opts.height / 2)
+		.attr('rx', sliderHt / 2)
 		.on('mousedown', scrollInit)
 		.on('mousemove', scrollMove)
 		.on('mouseup', scrollStop)
-	//.on('mouseout', scrollStop)
+
+	const slideElem = slider.node()
+
+	const ref = {}
 
 	const api = {
 		update(_opts) {
@@ -71,7 +118,18 @@ export function svgScroll(_opts) {
 				return
 			}
 			opts.holder.attr('transform', `translate(${opts.x},${opts.y})`).attr('display', '')
+
 			rect.attr('width', v)
+
+			line
+				.attr('x1', 0)
+				.attr('x2', v)
+				.attr('y1', opts.height / 2)
+				.attr('y2', opts.height / 2)
+
+			leftArrow.attr('transform', `translate(0,0)`)
+
+			rtArrow.attr('transform', `translate(${v - opts.height},0)`)
 
 			if (v != prev.v || t != prev.t) {
 				// multiply visibleWidth by the ratio of v:t,
@@ -91,6 +149,7 @@ export function svgScroll(_opts) {
 
 			ref.maxDx = opts.visibleWidth - ref.sliderX - ref.sliderWidth
 			ref.minDx = -ref.sliderX
+			ref.arrowDx = Math.round(0.1 * opts.visibleWidth)
 			delete ref.x
 		}
 	}
