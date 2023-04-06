d124e9d69e3199cd3e74cba217279babb8646ebe
Colleen Reilly <creilly@stjude.org>
2023-04-06 13:25:43 -0500

    menu unit test and planned tests

--- client/dom/test/menu.unit.spec.js
+++ client/dom/test/menu.unit.spec.js
@@ -17,14 +17,114 @@ function getHolder() {
 
 /**************
  test sections
-***************/
+**************
+
+new Menu()
+
+*/
 
 tape('\n', test => {
 	test.pass('-***- dom/menu -***-')
 	test.end()
 })
 
-tape.only('Launch Menu', test => {
-	const x = new Menu()
+tape('new Menu()', test => {
+	test.timeoutAfter(100)
+
+	const testMenu = new Menu()
+
+	test.equal(typeof testMenu.d, 'object', 'Should have a menu.d object')
+	test.equal(typeof testMenu.clear, 'function', 'Should have a menu.clear() function')
+	test.equal(typeof testMenu.show, 'function', 'Should have a menu.show() function')
+	test.equal(typeof testMenu.showunder, 'function', 'Should have a menu.showunder() function')
+	test.equal(typeof testMenu.hide, 'function', 'Should have a menu.hide() function')
+	test.equal(typeof testMenu.fadeout, 'function', 'Should have a menu.fadeout() function')
+	test.equal(typeof testMenu.toggle, 'function', 'Should have a menu.toggle() function')
+	test.equal(typeof testMenu.getCustomApi, 'function', 'Should have a menu.getCustomApi() function')
+
+	test.end()
+})
+
+tape.only('clear()', test => {
+	test.timeoutAfter(500)
+
+	const holder = getHolder()
+
+	const testMenu1 = new Menu({
+		parent_menu: holder
+	})
+	testMenu1.d.append('div').text('Test')
+
+	// if (test._ok) holder.remove()
+	test.end()
+})
+
+tape.skip('show()', test => {
+	test.timeoutAfter(500)
+
+	// const holder = getHolder()
+	const testMenu = new Menu()
+
+	// if (test._ok) holder.remove()
+	test.end()
+})
+
+tape.skip('showunder()', test => {
+	test.timeoutAfter(500)
+
+	// const holder = getHolder()
+	const testMenu = new Menu()
+
+	// if (test._ok) holder.remove()
+	test.end()
+})
+
+tape.skip('showunderoffset()', test => {
+	test.timeoutAfter(500)
+
+	// const holder = getHolder()
+	const testMenu = new Menu()
+
+	// if (test._ok) holder.remove()
+	test.end()
+})
+
+tape.skip('hide()', test => {
+	test.timeoutAfter(500)
+
+	// const holder = getHolder()
+	const testMenu = new Menu()
+
+	// if (test._ok) holder.remove()
+	test.end()
+})
+
+tape.skip('fadeout()', test => {
+	test.timeoutAfter(500)
+
+	// const holder = getHolder()
+	const testMenu = new Menu()
+
+	// if (test._ok) holder.remove()
+	test.end()
+})
+
+tape.skip('toggle()', test => {
+	test.timeoutAfter(500)
+
+	// const holder = getHolder()
+	const testMenu = new Menu()
+
+	// if (test._ok) holder.remove()
+	test.end()
+})
+
+tape.skip('getCustomApi()', test => {
+	test.timeoutAfter(500)
+
+	// const holder = getHolder()
+	const testMenu = new Menu()
+
+	// if (test._ok) holder.remove()
 	test.end()
 })
