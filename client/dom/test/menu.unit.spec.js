6b3a7920055418f4e71343da32f58c7444e1b545
Colleen Reilly <creilly@stjude.org>
2023-04-07 15:35:15 -0500

    More `menu.unit` tests

--- client/dom/test/menu.unit.spec.js
+++ client/dom/test/menu.unit.spec.js
@@ -1,6 +1,7 @@
 import tape from 'tape'
 import * as d3s from 'd3-selection'
-import { Menu } from '#dom/Menu'
+import { Menu } from '#dom/menu'
+import { detectOne } from '../../test/test.helpers.js'
 
 /*************************
  reusable helper functions
@@ -15,11 +16,17 @@ function getHolder() {
 		.style('margin', '5px')
 }
 
+const longText =
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+
 /**************
  test sections
 **************
 
 new Menu()
+show(), clear(), and hide(), no args
+clear() with arg
+show() with args
 
 */
 
@@ -34,6 +41,7 @@ tape('new Menu()', test => {
 	const testMenu = new Menu()
 
 	test.equal(typeof testMenu.d, 'object', 'Should have a menu.d object')
+	test.equal(typeof testMenu.dnode, 'object', 'Should have a menu.dnode (i.e. d.node()) object')
 	test.equal(typeof testMenu.clear, 'function', 'Should have a menu.clear() function')
 	test.equal(typeof testMenu.show, 'function', 'Should have a menu.show() function')
 	test.equal(typeof testMenu.showunder, 'function', 'Should have a menu.showunder() function')
@@ -41,31 +49,122 @@ tape('new Menu()', test => {
 	test.equal(typeof testMenu.fadeout, 'function', 'Should have a menu.fadeout() function')
 	test.equal(typeof testMenu.toggle, 'function', 'Should have a menu.toggle() function')
 	test.equal(typeof testMenu.getCustomApi, 'function', 'Should have a menu.getCustomApi() function')
+	test.equal(typeof testMenu.destroy, 'function', 'Should have a menu.destroy() function')
 
+	testMenu.destroy()
 	test.end()
 })
 
-tape.only('clear()', test => {
+tape('show(), clear(), and hide(), no args', async test => {
 	test.timeoutAfter(500)
+	//Test basic function collectively to use freely in later tests
 
-	const holder = getHolder()
-
-	const testMenu1 = new Menu({
-		parent_menu: holder
-	})
-	testMenu1.d.append('div').text('Test')
-
-	// if (test._ok) holder.remove()
-	test.end()
-})
-
-tape.skip('show()', test => {
-	test.timeoutAfter(500)
-
-	// const holder = getHolder()
 	const testMenu = new Menu()
+	testMenu.d
+		.append('div')
+		.text('Test')
+		.style('display', 'block')
+	testMenu.show()
 
-	// if (test._ok) holder.remove()
+	let appendedDiv
+	appendedDiv = testMenu.dnode.childNodes[0]
+	test.ok(appendedDiv.style.display == 'block' && appendedDiv.innerText == 'Test', `Should display 'Test' tip`)
+
+	testMenu.clear()
+	appendedDiv = testMenu.dnode.childNodes[0]
+	test.ok(!appendedDiv && testMenu.d, `Should remove appended 'Test' div but empty menu tip persists`)
+
+	testMenu.hide()
+	test.ok(testMenu.dnode.style.display == 'none', `Should not display menu`)
+
+	testMenu.destroy()
+	test.end()
+})
+
+tape.skip('clear() with arg', test => {
+	test.timeoutAfter(500)
+
+	const testMenu = new Menu({ clearSelector: '.sjpp-menu-test-div' })
+	testMenu.d
+		.append('div')
+		.text('Main div - should persist')
+		.style('display', 'block')
+
+	testMenu.d
+		.append('div')
+		.attr('class', 'sjpp-menu-test-div')
+		.text('Clear div - should clear')
+		.style('display', 'block')
+
+	testMenu.show()
+	const x = testMenu.d.select('.sjpp-menu-test-div')
+	console.log(x)
+	// testMenu.clear()
+
+	test.end()
+})
+
+tape('show() with args', async test => {
+	test.timeoutAfter(500)
+
+	const testMenu = new Menu()
+	testMenu.d
+		.style('max-width', '20vw')
+		.style('max-height', '10vh')
+		.style('overflow', 'hidden')
+		.append('div')
+		.text('Test')
+		.style('display', 'block')
+
+	let posNum
+
+	//left (x) position
+	posNum = 50
+	testMenu.show(posNum)
+	test.ok(
+		testMenu.dnode.style.left == `${posNum + testMenu.offsetX}px` && testMenu.dnode.style.top == '',
+		`Should show menu left, under header, shifted by .offsetX = ${testMenu.offsetX}px`
+	)
+
+	//top (y) position
+	testMenu.show('', posNum)
+	test.ok(
+		testMenu.dnode.style.left == '20px' && testMenu.dnode.style.top == `${posNum + testMenu.offsetY}px`,
+		`Should show menu at the top, above header, shifted by .offsetY = ${testMenu.offsety}px`
+	)
+
+	// left & top (x & y) position
+	posNum = 100
+	testMenu.show(posNum, posNum)
+	test.ok(
+		testMenu.dnode.style.left == `${posNum + testMenu.offsetX}px` &&
+			testMenu.dnode.style.top == `${posNum + testMenu.offsetY}px`,
+		`Should show menu top left corner, over header, shifted by .offsetX = ${testMenu.offsetX}px & .offsetY = ${testMenu.offsetY}px`
+	)
+
+	//No shift (i.e. additional 20px)
+	testMenu.show(posNum, posNum, false)
+	test.ok(
+		testMenu.dnode.style.left == `${posNum}px` && testMenu.dnode.style.top == `${posNum}px`,
+		`Should show menu top left corner without .offsetX or .offsetY added to left or top, respectively.`
+	)
+
+	//False down appears to have no effect on show()(?)
+	// posNum = 200
+	// testMenu.show(posNum, posNum, false, false)
+	// console.log(testMenu.dnode.style)
+
+	//With scroll
+	posNum = 200
+	testMenu.d.append('div').text(longText)
+	testMenu.show(posNum, posNum, false)
+	test.ok(
+		testMenu.dnode.style.left == `${posNum + window.scrollX}px` &&
+			testMenu.dnode.style.top == `${posNum + window.scrollY}px`,
+		`Should show menu position with window.scrollX = ${window.scrollX} & window.scrollY = ${window.scrollY}`
+	)
+
+	testMenu.destroy()
 	test.end()
 })
 
@@ -80,16 +179,6 @@ tape.skip('showunder()', test => {
 })
 
 tape.skip('showunderoffset()', test => {
-	test.timeoutAfter(500)
-
-	// const holder = getHolder()
-	const testMenu = new Menu()
-
-	// if (test._ok) holder.remove()
-	test.end()
-})
-
-tape.skip('hide()', test => {
 	test.timeoutAfter(500)
 
 	// const holder = getHolder()
