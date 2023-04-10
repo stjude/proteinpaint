c8b12a4664ac394375f1ce04f738c8dad7a70121
Aleksandar Acic <aacic@stjude.org>
2023-04-10 16:54:51 -0500

    Extend logic.

--- client/plots/disco_new/Ring.ts
+++ client/plots/disco_new/Ring.ts
@@ -1,4 +1,8 @@
 class Ring {
     private radius: number;
     private elements: Array<PlotElement>
+    constructor(radius: number, elements: Array<PlotElement>) {
+        this.radius = radius;
+        this.elements = elements
+    }
 }
