f0db9d7f3107471508d17a3d95a597549ecccf50
Aleksandar Acic <aacic@stjude.org>
2023-04-26 11:13:13 -0500

    Add LabelsRenderer.ts

--- client/plots/disco_new/renderer/DiscoRenderer.ts
+++ client/plots/disco_new/renderer/DiscoRenderer.ts
@@ -1,8 +1,17 @@
-import IRenderer from "#plots/disco_new/viewmodel/IRenderer";
-import * as d3 from "d3";
+import IRenderer from "#plots/disco_new/renderer/IRenderer";
 import ViewModel from "#plots/disco_new/viewmodel/ViewModel";
+import ArcRenderer from "#plots/disco_new/renderer/ArcRenderer";
+import LabelsRenderer from "#plots/disco_new/renderer/LabelsRenderer";
 
 export class DiscoRenderer implements IRenderer {
+    private arcRenderer: ArcRenderer;
+    private labelsRenderer: LabelsRenderer;
+
+    constructor() {
+        this.arcRenderer = new ArcRenderer()
+        this.labelsRenderer = new LabelsRenderer()
+    }
+
     render(holder: any, viewModel: ViewModel) {
         const svg = holder.append('svg')
             .attr('width', viewModel.width)
@@ -12,53 +21,10 @@ export class DiscoRenderer implements IRenderer {
             .attr('class', "mainG")
             .attr('transform', `translate(${viewModel.width / 2},${viewModel.height / 2})`);
 
-        const ring = viewModel.rings[0]
 
-        const data = ring.elements
+        this.arcRenderer.render(mainG, viewModel)
 
-        const pie = d3.pie<number>()
-            .padAngle(0.002)
-            .value(d => d.size)
-            .sort(null);
-
-        const arcData = pie(data);
-
-        const arc = d3.arc<d3.PieArcDatum<number>>()
-            .innerRadius(ring.innerRadius)
-            .outerRadius(ring.outerRadius);
-
-        const arcs = mainG.append("g")
-
-        arcs.selectAll("path")
-            .data(arcData)
-            .enter()
-            .append("path")
-            .attr("d", arc)
-            .attr("fill", "black")
-
-        arcs.selectAll("text")
-            .data(arcData)
-            .enter()
-            .append("text")
-            .each(function (d) {
-                d.angle = (d.startAngle + d.endAngle) / 2
-                d.ccAngle = d.angle - Math.PI / 2
-            })
-            .attr("transform", (d) => `translate(${arc.centroid(<any>d)}) rotate(${(d.angle * 180) / Math.PI - 90})${d.angle > Math.PI ? 'rotate(180)' : ''}`)
-            .attr("dy", "0.35em")
-            .attr("text-anchor", "middle")
-            .text(d => d.data.key)
-            .style("fill", "white")
-
-
-        const labels = mainG.append("g")
-
-        // labels.append("text").attr("x", 0)
-        //     .attr("y", 0)
-        //     .attr("fill", "#000000")
-        //     .attr("font-size", "16")
-        //     .attr("text-anchor", "middle")
-        //     .text("Hello, world!")
+        this.labelsRenderer.render(mainG, viewModel)
     }
 }
 
