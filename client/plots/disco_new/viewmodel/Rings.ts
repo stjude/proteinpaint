4b0da0234c55fd03756c3a94e24f7e912da88d64
Aleksandar Acic <aacic@stjude.org>
2023-05-02 18:41:22 -0500

    Animate labels transition.

--- client/plots/disco_new/viewmodel/Rings.ts
+++ client/plots/disco_new/viewmodel/Rings.ts
@@ -1,13 +1,13 @@
 import Label from "#plots/disco_new/viewmodel/Label";
 import Ring from "#plots/disco_new/viewmodel/Ring";
 import Chromosome from "#plots/disco_new/viewmodel/Chromosome";
+import Labels from "#plots/disco_new/viewmodel/Labels";
 
 export default class Rings {
 
-    labelsRing: Ring<Label>
+    labelsRing: Labels<Label>
     chromosomesRing: Ring<Chromosome>
-
-    constructor(labelsRing: Ring<Label>, chromosomesRing: Ring<Chromosome>) {
+    constructor(labelsRing: Labels<Label>, chromosomesRing: Ring<Chromosome>) {
         this.labelsRing = labelsRing;
         this.chromosomesRing = chromosomesRing;
     }
