77d37a452260c87be38de6c180625f1b941f5448
Airen Zaldivar <airenzp@gmail.com>
2023-05-15 13:38:45 -0500

    If there is a label use the label for the category, not the key

--- server/src/termdb.scatter.js
+++ server/src/termdb.scatter.js
@@ -271,6 +271,7 @@ function processSample(dbSample, sample, tw, categoryMap, category) {
 	if (tw.term.type == 'geneVariant') assignGeneVariantValue(dbSample, sample, tw, categoryMap, category)
 	else {
 		value = dbSample?.[tw.id || tw.term.name]?.key
+		if (tw.term.values?.[value]?.label) value = tw.term.values?.[value]?.label
 		sample.hidden[category] = tw.q.hiddenValues ? dbSample?.[tw.id]?.key in tw.q.hiddenValues : false
 		if (value) {
 			sample[category] = value.toString()
