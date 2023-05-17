f99aebe6ab56a79754d6dc2f043adc7a1e832a59
Airen Zaldivar <airenzp@gmail.com>
2023-05-17 10:33:57 -0500

    Added genomes. Testing that the msigdb button is shown if possible

--- client/dom/genesetSearch.js
+++ client/dom/genesetSearch.js
@@ -1,5 +1,6 @@
 export function initGenesetSearch({ holder, genome, callback, geneList, mode, vocabApi }) {
-	const div = holder.append('div').style('width', '50%')
+	console.log(genome)
+	const div = holder.append('div').style('width', '50vw')
 	const headerDiv = div.append('div')
 	headerDiv.append('input').attr('placeholder', 'Search genes')
 	headerDiv.append('button').text('Search')
@@ -7,8 +8,21 @@ export function initGenesetSearch({ holder, genome, callback, geneList, mode, vo
 		.append('div')
 		.style('display', 'inline-block')
 		.style('float', 'right')
-	rightDiv.append('button').text('Load MSigDB gene set')
+	rightDiv.append('input').attr('type', 'checkbox')
+	rightDiv.append('span').html('Use only cancer census genes')
+	if (genome?.termdbs?.msigdb)
+		rightDiv
+			.append('button')
+			.attr('name', 'msigdbBt')
+			.text('Load MSigDB gene set')
 	rightDiv.append('button').text('Clear')
 
-	const genesDiv = div.append('div')
+	const genesDiv = div.append('div').html('...')
+	const submitDiv = div
+		.append('div')
+		//.style('display', 'inline-block')
+		//.style('float', 'right')
+		.append('button')
+		.text('Submit')
+		.on('click', callback)
 }
