b5f687a60182bbea932b7961f7d1c64d42a1dbbc
Airen Zaldivar <airenzp@gmail.com>
2023-06-01 11:28:30 -0500

    removing prints

--- server/src/termdb.matrix.js
+++ server/src/termdb.matrix.js
@@ -165,7 +165,6 @@ function divideTerms(lst) {
 	const dict = [],
 		nonDict = []
 	for (const tw of lst) {
-		console.log(tw)
 		const type = tw.term.type
 		if (type == 'snplst' || type == 'snplocus' || type == 'geneVariant') {
 			nonDict.push(tw)
