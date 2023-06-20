478649efaaebb25feb6335f5ee23df5d4cfba2b6
Aleksandar Acic <aacic@stjude.org>
2023-06-20 22:51:36 +0200

    Increase legend font.

--- client/plots/disco_new/renderer/LegendRenderer.ts
+++ client/plots/disco_new/renderer/LegendRenderer.ts
@@ -1,36 +1,41 @@
-import Legend from "#plots/disco_new/viewmodel/Legend";
-import svgLegend from "#dom/svg.legend"
-import LegendJSONMapper from "#plots/disco_new/mapper/LegendJSONMapper";
+import Legend from '#plots/disco_new/viewmodel/Legend'
+import svgLegend from '#dom/svg.legend'
+import LegendJSONMapper from '#plots/disco_new/mapper/LegendJSONMapper'
 
 export default class LegendRenderer {
-    private legendJSONMapper: LegendJSONMapper;
+	private legendJSONMapper: LegendJSONMapper
+	private fontSize: number
 
-    constructor(capped: number, onClickCallback: (d: any, t: any) => void) {
-        this.legendJSONMapper = new LegendJSONMapper(capped, onClickCallback)
-    }
+	constructor(capped: number, fontSize: number, onClickCallback: (d: any, t: any) => void) {
+		this.fontSize = fontSize
+		this.legendJSONMapper = new LegendJSONMapper(capped, onClickCallback)
+	}
 
-    render(holder: any, legend: Legend, xOffset: number, svgw, svgh ) {
-        const svgLegendRenderer = svgLegend({
-            holder: holder.append('g'),
-            rectFillFxn: d => d.color,
-            iconStroke: '#aaa'
-        })
+	render(holder: any, legend: Legend, xOffset: number, svgw, svgh) {
+		const svgLegendRenderer = svgLegend({
+			holder: holder.append('g'),
+			rectFillFxn: (d) => d.color,
+			iconStroke: '#aaa',
+		})
 
-        // TODO calculate legend dimensions
+		// TODO calculate legend dimensions
 
-        const d =  {
-            xOffset : xOffset
-        }
+		const d = {
+			xOffset: xOffset,
+		}
 
-        const data = this.legendJSONMapper.map(legend)
+		const data = this.legendJSONMapper.map(legend)
 
-        svgLegendRenderer(data, {
-            settings: Object.assign({},{
-                svgw:svgw,
-                svgh: svgh,
-                dimensions: d,
-                fontsize: 9,
-            })
-        })
-    }
+		svgLegendRenderer(data, {
+			settings: Object.assign(
+				{},
+				{
+					svgw: svgw,
+					svgh: svgh,
+					dimensions: d,
+					fontsize: this.fontSize,
+				}
+			),
+		})
+	}
 }
