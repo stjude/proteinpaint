45e6f152ec6e4974ab34ec5aa44a0b6d73f28a71
Colleen Reilly <creilly@stjude.org>
2023-06-20 17:17:31 -0500

    Migrate `../termsetting/handlers/numeric.binary` to typescript; initial typing effort

--- client/termsetting/handlers/numeric.binary.ts
+++ client/termsetting/handlers/numeric.binary.ts
@@ -4,6 +4,7 @@ import { get_bin_label, get_bin_range_equation } from '#shared/termdb.bins'
 import { make_one_checkbox } from '#dom/checkbox'
 import { getPillNameDefault } from '#termsetting'
 import { convertViolinData } from '#filter/tvs.numeric'
+import { PillData, RangeEntry, TermSettingInstance } from '#shared/types'
 
 /*
 ********************** EXPORTED
@@ -24,9 +25,9 @@ getHandler(self)
 	processCustomBinInputs()
 */
 
-export function getHandler(self) {
+export function getHandler(self: TermSettingInstance) {
 	return {
-		getPillName(d) {
+		getPillName(d: PillData) {
 			return getPillNameDefault(self, d)
 		},
 
@@ -34,7 +35,7 @@ export function getHandler(self) {
 			return { text: 'binary' }
 		},
 
-		async showEditMenu(div) {
+		async showEditMenu(div: any) {
 			self.num_obj = {}
 
 			self.num_obj.plot_size = {
@@ -44,8 +45,8 @@ export function getHandler(self) {
 				ypad: 20
 			}
 			try {
-				const d = await self.vocabApi.getViolinPlotData({
-					termid: self.term.id,
+				const d = await self.vocabApi!.getViolinPlotData({
+					termid: self.term!.id,
 					filter: self.filter,
 					svgw: self.num_obj.plot_size.width,
 					orientation: 'horizontal',
@@ -78,7 +79,7 @@ export function getHandler(self) {
 
 			self.dom.rangeAndLabelDiv = self.dom.bins_table.append('div')
 
-			renderBoundaryInputDivs(self, self.q.lst)
+			renderBoundaryInputDivs(self, self.q!.lst)
 
 			const btndiv = div.append('div').style('padding', '3px 10px')
 
@@ -87,15 +88,15 @@ export function getHandler(self) {
 				.style('margin', '5px')
 				.html('Apply')
 				.on('click', async () => {
-					delete self.q.startinclusive
-					delete self.q.stopinclusive
-					delete self.q.bin_size
-					delete self.q.first_bin
-					delete self.q.last_bin
-					self.q.lst = processCustomBinInputs(self)
-					self.numqByTermIdModeType[self.term.id].binary = JSON.parse(JSON.stringify(self.q))
-					self.q.mode = 'binary'
-					self.runCallback()
+					delete self.q!.startinclusive
+					delete self.q!.stopinclusive
+					delete self.q!.bin_size
+					delete self.q!.first_bin
+					delete self.q!.last_bin
+					self.q!.lst = processCustomBinInputs(self)
+					self.numqByTermIdModeType[self.term!.id!].binary = JSON.parse(JSON.stringify(self.q))
+					self.q!.mode = 'binary'
+					self.runCallback!()
 				})
 
 			btndiv
@@ -106,21 +107,22 @@ export function getHandler(self) {
 					// delete self.q and create new with default
 					// TODO: rightnow reset will devide bins at max-min/2 as per logic at line 134
 					// if it must be reset at median, the logic must be changned
-					delete self.q
-					delete self.numqByTermIdModeType[self.term.id]
+					delete self.q 
+					delete self.numqByTermIdModeType[self.term!.id!]
 					getHandler(self).showEditMenu(self.dom.num_holder)
 				})
 		}
 	}
 }
 
-function setqDefaults(self) {
-	const dd = self.num_obj.density_data
+function setqDefaults(self: TermSettingInstance) {
+	const dd = self.num_obj!.density_data!
 	const boundry_value = self.q && self.q.lst && self.q.lst.length ? self.q.lst[0].stop : undefined
 	const cache = self.numqByTermIdModeType
+	if (!self.term) throw `Missing .term{} [numeric.binary, setqDefaults()]`
 	const t = self.term
-	if (!cache[t.id]) cache[t.id] = {}
-	if (!cache[t.id].binary) {
+	if (!cache[t.id!]) cache[t.id!] = {}
+	if (!cache[t.id!].binary) {
 		// automatically derive a cutoff to generate binary bins
 		const cutoff =
 			boundry_value !== undefined
@@ -129,7 +131,7 @@ function setqDefaults(self) {
 				? dd.minvalue + (dd.maxvalue - dd.minvalue) / 2
 				: dd.maxvalue
 
-		cache[t.id].binary = {
+		cache[t.id!].binary = {
 			mode: 'binary',
 			type: 'custom-bin',
 			modeBinaryCutoffType: 'normal', // default value
@@ -149,10 +151,10 @@ function setqDefaults(self) {
 	}
 
 	if (!self.q || self.q.mode !== 'binary') self.q = {}
-	const cacheCopy = JSON.parse(JSON.stringify(cache[t.id].binary))
+	const cacheCopy = JSON.parse(JSON.stringify(cache[t.id!].binary))
 	self.q = Object.assign(cacheCopy, self.q)
-	if (self.q.lst) {
-		self.q.lst.forEach(bin => {
+	if (self.q!.lst) {
+		self.q!.lst.forEach(bin => {
 			if (!('label' in bin)) bin.label = get_bin_label(bin, self.q)
 			if (!('range' in bin)) bin.range = get_bin_range_equation(bin, self.q)
 		})
@@ -160,7 +162,7 @@ function setqDefaults(self) {
 	//*** validate self.q ***//
 }
 
-async function renderCuttoffInput(self) {
+async function renderCuttoffInput(self: TermSettingInstance) {
 	// binary mode unqiue UI
 	self.dom.cutoff_div
 		.append('span')
@@ -179,72 +181,72 @@ async function renderCuttoffInput(self) {
 		.style('width', '100px')
 		.attr('type', 'number')
 		.style('margin-right', '10px')
-		.attr('value', self.q.modeBinaryCutoffType == 'normal' ? self.q.lst[0].stop : self.q.modeBinaryCutoffPercentile)
+		.attr('value', self.q!.modeBinaryCutoffType == 'normal' ? self.q!.lst![0].stop : self.q!.modeBinaryCutoffPercentile)
 		.on('change', handleChange)
 
 	self.dom.customBinBoundaryPercentileCheckbox = make_one_checkbox({
 		holder: self.dom.cutoff_div,
 		labeltext: 'Use percentile',
-		checked: self.q.modeBinaryCutoffType == 'percentile',
+		checked: self.q!.modeBinaryCutoffType == 'percentile',
 		divstyle: { display: 'inline-block' },
 		callback: handleCheckbox
 	})
 
-	async function handleChange() {
-		const value = +this.value
-		if (self.q.modeBinaryCutoffType == 'normal') {
+	async function handleChange(this: any) {
+		const value: number = +this.value
+		if (self.q!.modeBinaryCutoffType == 'normal') {
 			updateUI(value)
-		} else if (self.q.modeBinaryCutoffType == 'percentile') {
+		} else if (self.q!.modeBinaryCutoffType == 'percentile') {
 			// using percentile, value is a percentile number
 			if (value < 1 || value > 99) {
 				window.alert('Invalid percentile value: enter integer between 1 and 99')
 				return
 			}
-			self.q.modeBinaryCutoffPercentile = value
+			self.q!.modeBinaryCutoffPercentile = value
 			await setPercentile()
 		} else {
 			throw 'invalid modeBinaryCutoffType value'
 		}
 	}
 
-	function updateUI(cutoff) {
+	function updateUI(cutoff: number) {
 		// cutoff is the actual data value, not percentile
-		self.q.lst[0].stop = cutoff
-		self.q.lst[1].start = cutoff
-		self.q.lst.forEach(bin => {
+		self.q!.lst![0].stop = cutoff
+		self.q!.lst![1].start = cutoff
+		self.q!.lst!.forEach(bin => {
 			bin.label = get_bin_label(bin, self.q)
 			bin.range = get_bin_range_equation(bin, self.q)
 		})
 		setDensityPlot(self)
-		renderBoundaryInputDivs(self, self.q.lst)
+		renderBoundaryInputDivs(self, self.q!.lst)
 	}
 
 	async function handleCheckbox() {
-		self.q.modeBinaryCutoffType = self.q.modeBinaryCutoffType == 'percentile' ? 'normal' : 'percentile'
-		if (self.q.modeBinaryCutoffType == 'normal') {
-			const v = self.q.lst[0].stop
+		self.q!.modeBinaryCutoffType = self.q!.modeBinaryCutoffType == 'percentile' ? 'normal' : 'percentile'
+		if (self.q!.modeBinaryCutoffType == 'normal') {
+			const v = self.q!.lst![0].stop as number
 			self.dom.customBinBoundaryInput.property('value', v)
 			updateUI(v)
-		} else if (self.q.modeBinaryCutoffType == 'percentile') {
+		} else if (self.q!.modeBinaryCutoffType == 'percentile') {
 			// switched to percentile
-			self.dom.customBinBoundaryInput.property('value', self.q.modeBinaryCutoffPercentile)
+			self.dom.customBinBoundaryInput.property('value', self.q!.modeBinaryCutoffPercentile)
 			await setPercentile()
 		} else {
 			throw 'invalid modeBinaryCutoffType value'
 		}
 	}
 	async function setPercentile() {
-		const data = await self.opts.vocabApi.getPercentile(self.term.id, [self.q.modeBinaryCutoffPercentile], self.filter)
+		const data = await self.opts.vocabApi.getPercentile(self.term!.id!, [self.q!.modeBinaryCutoffPercentile!], self.filter!)
 		updateUI(data.values[0])
 	}
 }
 
-function processCustomBinInputs(self) {
+function processCustomBinInputs(self: TermSettingInstance) {
 	const startinclusive = self.dom.boundaryInput.property('value') == 'startinclusive'
 	const stopinclusive = self.dom.boundaryInput.property('value') == 'stopinclusive'
 	const inputDivs = self.dom.bins_table.node().querySelectorAll('input')
-	let prevBin
-	const val = self.q.lst[0].stop // should not get value from dom.customBinBoundaryInput as value can be percentile
+	//let prevBin
+	const val = self.q!.lst![0].stop // should not get value from dom.customBinBoundaryInput as value can be percentile
 
 	const bins = [
 		{
@@ -262,7 +264,7 @@ function processCustomBinInputs(self) {
 	]
 
 	// assign bin labels
-	bins.forEach((bin, i) => {
+	bins.forEach((bin: RangeEntry, i: number) => {
 		// may use user assigned labels if not empty string
 		const label = inputDivs[i].value
 		bin.label = label ? label : get_bin_label(bin, self.q)
