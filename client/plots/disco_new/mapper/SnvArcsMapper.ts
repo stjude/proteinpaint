26fd2383451935c9297407e782f4e8644be36ac7
Aleksandar Acic <aacic@stjude.org>
2023-06-21 14:11:32 +0200

    Format gene row in the tooltip like: Gene: ${gene} Show mutation type.

--- client/plots/disco_new/mapper/SnvArcsMapper.ts
+++ client/plots/disco_new/mapper/SnvArcsMapper.ts
@@ -1,69 +1,73 @@
-import Reference from "./Reference";
-import Data from "./Data";
-import SnvArc from "#plots/disco_new/viewmodel/SnvArc";
-import MLabel from "./MLabel";
-import SnvLegendElement from "#plots/disco_new/viewmodel/SnvLegendElement";
+import Reference from './Reference'
+import Data from './Data'
+import SnvArc from '#plots/disco_new/viewmodel/SnvArc'
+import MLabel from './MLabel'
+import SnvLegendElement from '#plots/disco_new/viewmodel/SnvLegendElement'
 
 export default class SnvArcsMapper {
+	snvClassMap: Map<string, SnvLegendElement> = new Map()
 
-    snvClassMap: Map<string, SnvLegendElement> = new Map()
+	private settings: any
+	private sampleName: string
+	private reference: Reference
 
-    private settings: any;
-    private sampleName: string;
-    private reference: Reference;
+	private onePxArcAngle: number
+	private bpx: number
 
-    private onePxArcAngle: number;
-    private bpx: number;
+	constructor(settings: any, sampleName: string, reference: Reference) {
+		this.settings = settings
+		this.sampleName = sampleName
+		this.reference = reference
 
-    constructor(settings: any, sampleName: string, reference: Reference) {
-        this.settings = settings
-        this.sampleName = sampleName
-        this.reference = reference
+		// number of base pairs per pixel
+		this.bpx = Math.floor(
+			this.reference.totalSize / (this.reference.totalChromosomesAngle * settings.rings.svnInnerRadius)
+		)
+		this.onePxArcAngle = 1 / settings.rings.svnInnerRadius
+	}
 
-        // number of base pairs per pixel
-        this.bpx = Math.floor(this.reference.totalSize / (this.reference.totalChromosomesAngle * settings.rings.svnInnerRadius))
-        this.onePxArcAngle = 1 / (settings.rings.svnInnerRadius)
-    }
+	map(exonicSnvDataMap: Map<number, Array<Data>>): Array<SnvArc> {
+		const snvArray: Array<SnvArc> = []
+		for (const angle of exonicSnvDataMap.keys()) {
+			const array = exonicSnvDataMap.get(angle)
+			if (array) {
+				const arraySize = array.length
 
-    map(exonicSnvDataMap: Map<number, Array<Data>>): Array<SnvArc> {
-        const snvArray: Array<SnvArc> = []
-        for (const angle of exonicSnvDataMap.keys()) {
-            const array = exonicSnvDataMap.get(angle)
-            if (array) {
-                const arraySize = array.length
+				for (let i = 0; i < array.length; i++) {
+					const data = array[i]
+					const snvLegendElement = this.snvClassMap.get(data.mClass)
+					if (snvLegendElement) {
+						this.snvClassMap.set(data.mClass, this.createSnvLegend(data.mClass, ++snvLegendElement.count))
+					} else {
+						this.snvClassMap.set(data.mClass, this.createSnvLegend(data.mClass, 1))
+					}
 
-                for (let i = 0; i < array.length; i++) {
-                    const data = array[i]
-                    const snvLegendElement = this.snvClassMap.get(data.mClass)
-                    if (snvLegendElement) {
-                        this.snvClassMap.set(data.mClass, this.createSnvLegend(data.mClass, ++snvLegendElement.count))
-                    } else {
-                        this.snvClassMap.set(data.mClass, this.createSnvLegend(data.mClass, 1))
-                    }
+					const startAngle = angle
+					const endAngle = angle + this.onePxArcAngle
 
-                    const startAngle = angle
-                    const endAngle = angle + this.onePxArcAngle
+					const mLabel = MLabel.getInstance().mlabel ? MLabel.getInstance().mlabel[data.mClass] : undefined
 
-                    const arc = new SnvArc(startAngle,
-                        endAngle,
-                        this.settings.rings.svnInnerRadius + (i * this.settings.rings.svnWidth / arraySize),
-                        this.settings.rings.svnInnerRadius + ((i + 1) * this.settings.rings.svnWidth / arraySize),
-                        MLabel.getInstance().mlabel ? MLabel.getInstance().mlabel[data.mClass].color : '#000',
-                        data.gene,
-                        data.mClass,
-                        data.mname,
-                        data.chr,
-                        data.pos
-                    )
-                    snvArray.push(arc)
-                }
-            }
-        }
-        return snvArray
-    }
+					const arc = new SnvArc(
+						startAngle,
+						endAngle,
+						this.settings.rings.svnInnerRadius + (i * this.settings.rings.svnWidth) / arraySize,
+						this.settings.rings.svnInnerRadius + ((i + 1) * this.settings.rings.svnWidth) / arraySize,
+						mLabel.color,
+						data.gene,
+						mLabel.label,
+						data.mname,
+						data.chr,
+						data.pos
+					)
+					snvArray.push(arc)
+				}
+			}
+		}
+		return snvArray
+	}
 
-    private createSnvLegend(dataClass: string, count: number) {
-        const mClass = MLabel.getInstance().mlabel[dataClass]
-        return new SnvLegendElement(mClass.label, mClass.color, count)
-    }
+	private createSnvLegend(dataClass: string, count: number) {
+		const mClass = MLabel.getInstance().mlabel[dataClass]
+		return new SnvLegendElement(mClass.label, mClass.color, count)
+	}
 }
