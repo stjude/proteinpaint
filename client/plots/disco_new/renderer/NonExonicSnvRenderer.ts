e169ffdc6154d7f6d18376d66e14436b0761a2c8
Aleksandar Acic <aacic@stjude.org>
2023-06-21 14:11:32 +0200

    Format gene row in the tooltip like: Gene: ${gene} Show mutation type.

--- client/plots/disco_new/renderer/NonExonicSnvRenderer.ts
+++ client/plots/disco_new/renderer/NonExonicSnvRenderer.ts
@@ -1,29 +1,32 @@
-import IRenderer from "./IRenderer";
-import * as d3 from "d3";
-import SnvArc from "#plots/disco_new/viewmodel/SnvArc";
-import Arc from "#plots/disco_new/viewmodel/Arc";
-import MenuProvider from "./MenuProvider";
+import IRenderer from './IRenderer'
+import * as d3 from 'd3'
+import SnvArc from '#plots/disco_new/viewmodel/SnvArc'
+import Arc from '#plots/disco_new/viewmodel/Arc'
+import MenuProvider from './MenuProvider'
 
 export default class NonExonicSnvRenderer implements IRenderer {
-    render(holder: any,  elements: Array<Arc>) {
-        const arcGenerator = d3.arc<SnvArc>();
+	render(holder: any, elements: Array<Arc>) {
+		const arcGenerator = d3.arc<SnvArc>()
 
-        const arcs = holder.append("g")
+		const arcs = holder.append('g')
 
-        const menu = MenuProvider.create()
+		const menu = MenuProvider.create()
 
-        arcs.selectAll("path")
-            .data(elements)
-            .enter()
-            .append("path")
-            .attr('d', (d: SnvArc) => arcGenerator(d))
-            .attr("fill", (d: SnvArc) => d.color)
-            .on('mouseover', (mouseEvent: MouseEvent, arc: SnvArc) => {
-                menu.d.style("color", arc.color).html(`${arc.text} <br />${arc.mname} <br /> ${arc.dataClass} <br /> ${arc.chr}:${arc.pos}`)
-                menu.showunder(mouseEvent.target)
-            })
-            .on('mouseout', () => {
-                menu.hide()
-            })
-    }
+		arcs
+			.selectAll('path')
+			.data(elements)
+			.enter()
+			.append('path')
+			.attr('d', (d: SnvArc) => arcGenerator(d))
+			.attr('fill', (d: SnvArc) => d.color)
+			.on('mouseover', (mouseEvent: MouseEvent, arc: SnvArc) => {
+				menu.d
+					.style('color', arc.color)
+					.html(`Gene: ${arc.text} <br />${arc.mname} <br /> ${arc.dataClass} <br /> ${arc.chr}:${arc.pos}`)
+				menu.showunder(mouseEvent.target)
+			})
+			.on('mouseout', () => {
+				menu.hide()
+			})
+	}
 }
