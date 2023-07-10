2a943cedaf784876fc2eba0c842a9e170a82978e
Airen Zaldivar <airenzp@gmail.com>
2023-07-10 14:46:46 -0500

    Showing hardcoded data

--- client/plots/profileBarchart.js
+++ client/plots/profileBarchart.js
@@ -39,31 +39,92 @@ class profileBarchart {
 			if (_d.samples[k].sampleName == this.state.config.sampleName) data = _d.samples[k]
 		}
 		if (!data) throw 'no data returned for sample'
-		const table = this.dom.holder.append('table')
-		// header
-		{
-			const tr = table.append('tr')
-			tr.append('td').text(this.state.config.sampleName)
-			for (const c of this.state.config.columnNames) tr.append('td').text(c)
+		const holder = this.dom.holder.append('div')
+		const config = this.state.config
+		const svg = holder.append('svg').attr('width', config.svgw).attr('height', config.svgh)
+		//this.drawRect(svg, 100, 100, '#aaa')
+		// const table = this.dom.holder.append('table')
+		// // header
+		// {
+		// 	const tr = table.append('tr')
+		// 	tr.append('td').text(this.state.config.sampleName)
+		// 	for (const c of this.state.config.columnNames) tr.append('td').text(c)
+		// }
+		// for (const row of this.state.config.rows) {
+		// 	const tr = table.append('tr')
+		// 	tr.append('td').text(row.name)
+		// 	for (const tw of row.twlst) {
+		// 		const td = tr.append('td')
+		// 		const value = data[tw.$id]?.value
+		// 		// TODO barplot
+		// 		if (Number.isFinite(value)) {
+		// 			td.text(value)
+		// 		}
+		// 	}
+		// }
+		let x = 400
+		let y = 100
+		let stepx = 600
+		let step = 30
+		svg
+			.append('text')
+			.attr('transform', `translate(${x}, ${50})`)
+			.attr('text-anchor', 'end')
+			.text(`${config.sampleName} Component`)
+		for (const c of config.columnNames) {
+			svg
+				.append('text')
+				.attr('transform', `translate(${x}, ${75})`)
+				.attr('text-anchor', 'end')
+				.style('font-weight', 'bold')
+				.text(`${c}%`)
+			x += stepx
 		}
+
 		for (const row of this.state.config.rows) {
-			const tr = table.append('tr')
-			tr.append('td').text(row.name)
+			x = 400
 			for (const tw of row.twlst) {
-				const td = tr.append('td')
-				const value = data[tw.$id]?.value
-				// TODO barplot
-				if (Number.isFinite(value)) {
-					td.text(value)
-				}
+				const color = '#aaa'
+				drawRect(x, y, color, tw)
+				x += stepx
 			}
+			y += step
+		}
+
+		function drawRect(x, y, color, tw) {
+			console.log(tw)
+			const value = data[tw.$id]?.value
+
+			if (value) {
+				const width = (value / 100) * 100
+				svg
+					.append('g')
+					.attr('transform', `translate(${x + 10}, ${y})`)
+					.append('rect')
+					.attr('x', 0)
+					.attr('y', 0)
+					.attr('width', width)
+					.attr('height', 20)
+					.attr('fill', color)
+				svg
+					.append('text')
+					.attr('transform', `translate(${x + width + 50}, ${y + 15})`)
+					.attr('text-anchor', 'end')
+					.text(`${value}%`)
+			}
+
+			svg
+				.append('text')
+				.attr('transform', `translate(${x}, ${y + 15})`)
+				.attr('text-anchor', 'end')
+				.text(tw.term.name)
 		}
 	}
 }
 
 export async function getPlotConfig(opts, app) {
 	try {
-		const defaults = {}
+		const defaults = { svgw: 1200, svgh: 550 }
 		const config = copyMerge(defaults, opts)
 		for (const row of config.rows) {
 			for (const t of row.twlst) {
