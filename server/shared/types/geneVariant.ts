57c05c75d63732657ed3c425e0862dbc9d1749c6
Colleen Reilly <creilly@stjude.org>
2023-07-12 19:27:10 -0500

    New `types/geneVariant.ts`; applied to `handlers/geneVariant.ts`

---
+++ server/shared/types/geneVariant.ts
@@ -0,0 +1,33 @@
+import { TermWrapper, BaseQ, Term } from './termdb'
+import { TermSettingInstance } from './termsetting'
+
+/*
+--------EXPORTED--------
+GeneVariantQ
+GeneVariantTermWrapper
+GeneVariantTermSettingInstance
+
+*/
+
+export type GeneVariantQ = BaseQ & {
+	// termType: 'geneVariant'
+	cnvGainCutoff?: number
+	cnvMaxLength?: number
+	cnvMinAbsValue?: number
+	cnvLossCutoff?: number
+	exclude: any //an array maybe?
+}
+
+export type GeneVariantTermWrapper = TermWrapper & {
+	q: GeneVariantQ
+	term: GeneVariantTerm
+}
+
+type GeneVariantTerm = Term & {
+	isoform: string
+}
+
+export type GeneVariantTermSettingInstance = TermSettingInstance & {
+	q: GeneVariantQ
+	term: GeneVariantTerm
+}
