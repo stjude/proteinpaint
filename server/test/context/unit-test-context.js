708e89059f61e23a153e67e8af46c3eebe5f51c4
Edgar Sioson <siosonel@gmail.com>
2023-08-02 16:46:57 -0500

    use typia to validate data against a declared type

--- server/test/context/unit-test-context.js
+++ server/test/context/unit-test-context.js
@@ -1,2 +1,3 @@
-const context = require.context('../../', true, /\.unit.spec.js$/)
+console.log(1, process.argv)
+const context = require.context('../../', true, /health.unit.spec.js$/)
 require('../../utils/webpack/exportContext.js').exportContext(context)
