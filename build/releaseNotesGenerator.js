c02753f3c92860d2bc2b822eb5e37e123a33ceec
Edgar Sioson <siosonel@gmail.com>
2023-08-30 18:15:05 -0500

    feat: improve the release text detection and generator, to minimize potential conflicts when merging

--- build/releaseNotesGenerator.js
+++ build/releaseNotesGenerator.js
@@ -1,73 +1,64 @@
-const fs = require('fs')
+/*
+	test by running a commit 
+*/
 
-// reading from files instead of as string arguments to avoid escaping
-const releaseTextFile = process.argv[2]
-const currentLines = fs.readFileSync(releaseTextFile).toString('utf-8').trim().split('\n')
-const currentNotes = currentLines.filter(d => d && true).map(d => d.trim())
+const fs = require('fs')
+const spawnSync = require('child_process').spawnSync
+
+const GenTitle = 'General:'
+const FeatTitle = 'Features:'
+const FixTitle = 'Fixes:'
+
 const commitFile = process.argv[3]
 const commitMsg = fs.readFileSync(commitFile).toString('utf-8').trim()
+const commitLines = commitMsg
+	.split('\n')
+	.map(l => l.trim())
+	.filter(l => l.startsWith('feat: ') || l.startsWith('fix: '))
+if (!commitLines.length) process.exit()
 
-// keyword convention loosely follows https://www.conventionalcommits.org/en/v1.0.0/
-// however, breaking changes is not processed here, that would require
-// manual review, handling, coordination, and should not be automated?
-//
-// TODO: support (scope) syntax, like fix(matrix): or feat(disco):
-//
-const features = [commitMsg]
-	.filter(m => m.toLowerCase().startsWith('feat: '))
-	.map(getStringAfter1stSpace)
-	.filter(filterListItem)
-	.map(getListItem)
+// use the release.txt from origin/master to help minimize merge conflict
+const ps = spawnSync('curl', ['-s', 'https://raw.githubusercontent.com/stjude/proteinpaint/master/release.txt'], {
+	encoding: 'utf-8'
+})
+if (ps.stderr) throw remoteReleaseTxt.stderr
+const remoteReleaseTxt = ps.stdout
+const remoteSections = getSections(remoteReleaseTxt)
 
-const fixes = [commitMsg]
-	.filter(m => m.toLowerCase().startsWith('fix: '))
-	.map(getStringAfter1stSpace)
-	.filter(filterListItem)
-	.map(getListItem)
+// reading from files instead of as string arguments to avoid escaping
+const localReleaseTxtFile = process.argv[2]
+const localReleaseTxt = fs.readFileSync(localReleaseTxtFile).toString('utf-8').trim()
+const currSections = getSections(localReleaseTxt, remoteSections)
 
-const FeatTitle = 'Enhancements'
-const FixTitle = 'Fixes'
-const firstSubsectionTitle = currentNotes.includes(FeatTitle) ? FeatTitle : FixTitle
-const i = currentNotes.indexOf(firstSubsectionTitle) //; console.log(firstSubsectionTitle)
-const generalSection = currentNotes.slice(0, i)
-const subsections = currentNotes.slice(i)
-// okay for these subsection titles to not exist
-const j = currentNotes.indexOf(FixTitle)
-const featureSection = firstSubsectionTitle == FeatTitle ? subsections.slice(0, i) : ''
-const fixSection = firstSubsectionTitle == FixTitle ? subsections : subsections.split(FixTitle)[1]
-
-let notes = [] //; console.log({generalSection, featureSection, features, fixSection, fixes})
-if (generalSection.length) {
-	const GeneralTitle = `General`
-	if (!currentNotes.includes(GeneralTitle) && generalSection.length) notes.push(GeneralTitle)
-	notes.push(...generalSection)
-	if (notes.length) notes.push('')
-}
-if (featureSection || features.length) {
-	if (featureSection[0] != FeatTitle) notes.push(FeatTitle)
-	if (featureSection) notes.push(...featureSection)
-	if (features.length) notes.push(...features)
-	if (notes.length) notes.push('')
-}
-if (fixSection || fixes.length) {
-	if (fixSection[0] != FixTitle) notes.push(FixTitle)
-	if (fixSection) notes.push(...fixSection)
-	if (fixes.length) notes.push(...fixes)
-	if (notes.length) notes.push('')
+for (const line of commitLines) {
+	if (line.startsWith('feat: ')) currSections[FeatTitle].push('- ' + line.slice(6).trim())
+	if (line.startsWith('fix: ')) currSections[FixTitle].push('- ' + line.slice(5).trim())
 }
 
-fs.writeFileSync(releaseTextFile, notes.join('\n'))
+const notes = []
+for (const title in currSections) {
+	const lines = currSections[title]
+	if (lines.length) notes.push(title, ...lines, '')
+}
+
+const content = notes.join('\n')
+fs.writeFileSync(localReleaseTxtFile, content)
 console.log(notes.join('\n'))
 
-function getStringAfter1stSpace(str) {
-	return str.slice(str.indexOf(' ') + 1)
-}
-
-function getListItem(str) {
-	return '- ' + str
-}
-
-function filterListItem(str) {
-	//console.log(63, str, currentNotes)
-	return !currentNotes.includes(str)
+function getSections(releaseTxt, _bySection = null) {
+	const bySection = _bySection || {
+		[GenTitle]: [],
+		[FeatTitle]: [],
+		[FixTitle]: []
+	}
+	const lines = releaseTxt.split('\n').map(l => l.trim())
+	let currSection = bySection[GenTitle]
+	for (const line of lines) {
+		if (line in bySection) {
+			currSection = bySection[line]
+		} else if (line && !currSection.includes(line)) {
+			currSection.push(line)
+		}
+	}
+	return bySection
 }
