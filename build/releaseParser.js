261d00e0ec22161bf99433bada13af25511b7e20
Edgar Sioson <siosonel@gmail.com>
2023-09-02 00:00:39 -0500

    devops: support more release note section titles and their corresponding commit keywords

--- build/releaseParser.js
+++ build/releaseParser.js
@@ -1,25 +1,45 @@
-const GenTitle = 'General:'
-const FeatTitle = 'Features:'
-const FixTitle = 'Fixes:'
+const GenTitle = 'General:' // catch-all, will be used if there are no headings in the releaseTxt argument to getSections()
+const FeatTitle = 'Features:' // enhancements, prototypes
+const FixTitle = 'Fixes:' // bug fixes
+const DevOpsTitle = 'DevOps:' // engineering tasks, includes build, CI, refactor, performance, tests
+const BreakTitle = 'Breaking Change:' // anything that changes the runproteinpaint() or server API; TODO: how about new system deps or support files?
+const DocsTitle = 'Documentation:' // includes chores, this change category should not trigger a release by itself?
+
+const titles = [BreakTitle, GenTitle, FeatTitle, FixTitle, DevOpsTitle, DocsTitle]
 
 module.exports = {
-	GenTitle,
-	FeatTitle,
-	FixTitle,
-	getSections(releaseTxt, _bySection = null) {
-		const bySection = _bySection || {
-			[GenTitle]: [],
-			[FeatTitle]: [],
-			[FixTitle]: []
-		}
+	keywordsToTitle: {
+		breaking: BreakTitle,
+		general: GenTitle,
+		feat: FeatTitle,
+		fix: FixTitle,
+		devops: DevOpsTitle,
+		docs: DocsTitle
+	},
+	getSections(releaseTxt, _bySection = {}) {
+		const bySection = Object.assign(
+			{
+				[BreakTitle]: [],
+				[GenTitle]: [],
+				[FeatTitle]: [],
+				[FixTitle]: [],
+				[DevOpsTitle]: [],
+				[DocsTitle]: []
+			},
+			_bySection
+		)
+
 		const lines = releaseTxt.split('\n').map(l => l.trim())
 		let currSection = bySection[GenTitle]
 		for (const line of lines) {
-			if (line in bySection) {
+			// l can be a section title or entry
+			const matchingTitle = titles.find(t => t.toLowerCase().startsWith(line.toLowerCase()))
+			const l = matchingTitle || line
+			if (l in bySection) {
 				// switch to the next section
-				currSection = bySection[line]
-			} else if (line?.startsWith('- ') && !currSection.includes(line)) {
-				currSection.push(line)
+				currSection = bySection[l]
+			} else if (l?.startsWith('- ') && !currSection.includes(l)) {
+				currSection.push(l)
 			}
 		}
 		return bySection
