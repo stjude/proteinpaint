efce1d5ca2e5ff9df0cc0e2b472670dbf4bd0fb8
Edgar Sioson <siosonel@gmail.com>
2023-09-07 15:55:50 -0500

    separate augen into its own workspace

---
+++ augen/README.md
@@ -0,0 +1,18 @@
+# Augen
+
+Automatic generator of typescript-based server routes, unit tests, and API documentation.
+
+## Install
+
+```bash
+npm install
+```
+
+## Develop
+
+```
+
+```
+
+## Test
+
---
+++ augen/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const augent = require('./src/augen.js')
+const fs = require('fs')
+const path = require('path')
+
+const cmd = process.argv[2]
+if (!['typeCheckers', 'apiJson'].includes(cmd)) throw `cmd='${cmd}' not supported`
+
+const dir = process.argv[3]
+if (!dir) throw `missing dir argument`
+if (!fs.existsSync(dir)) throw `Not found: dir='${dir}'`
+
+const fromPath = process.argv[4]
+
+const files = fs.readdirSync(dir)
+;(async () => {
+	const fileRoutes = files.map(file => ({ file, route: require(`${dir}/${file}`) }))
+	if (cmd == 'typeCheckers') {
+		if (!fromPath) throw `missing fromPath`
+		const content = augent[cmd](fileRoutes, fromPath)
+		console.log(content)
+	}
+	if (cmd == 'apiJson') {
+		const content = augent[cmd](fileRoutes)
+		console.log(content)
+	}
+})()
