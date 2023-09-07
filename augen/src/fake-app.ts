96112ed452d6ce205af6b53a5cc6efc0363c37ff
Edgar Sioson <siosonel@gmail.com>
2023-09-07 15:55:50 -0500

    separate augen into its own workspace

---
+++ augen/src/fake-app.ts
@@ -0,0 +1,27 @@
+type AnyFunction = (res: any, req: any) => any
+
+function getApp({ api }) {
+	const app = {
+		routes: {},
+		get(path: string, handler: AnyFunction) {
+			app.setRoute(path, handler, 'get')
+		},
+		post(path: string, handler: AnyFunction) {
+			app.setRoute(path, handler, 'post')
+		},
+		all(path: string, handler: AnyFunction) {
+			app.setRoute(path, handler, 'get')
+			app.setRoute(path, handler, 'post')
+		},
+		setRoute(path: string, handler: AnyFunction, method) {
+			if (!method) throw `missing route method`
+			if (!app.routes[path]) app.routes[path] = {}
+			app.routes[path][method] = handler
+		}
+	}
+	for (const method in api.methods) {
+		const m = api.methods[method]
+		app[method](api.endpoint, m.init({ app, genomes }))
+	}
+	return app
+}
