31a9e9ed4a7b0f4ed547a687f5cf17ff474ad599
Aleksandar Acic <aacic@stjude.org>
2023-09-12 15:22:20 -0500

    Start ppserver docker image.

--- container/ci/ci-run.sh
+++ container/ci/ci-run.sh
@@ -1,3 +1,3 @@
 ./pack.sh
-./build2.sh -z full
-./run.sh ppfull:latest # or ppserver:latest, requires minimal serverconfig.json
+./build2.sh -z ppserver
+./run.sh ppserver:latest # or ppserver:latest, requires minimal serverconfig.json
