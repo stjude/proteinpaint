b680e68b134b7c84b40359e8a04aca94bae7674c
Colleen Reilly <creilly@stjude.org>
2023-09-13 15:48:56 -0500

    Reusable, descriptive throw message

--- client/dom/sayerror.js
+++ client/dom/sayerror.js
@@ -1,5 +1,14 @@
 import { disappear } from './animation'
 
+/*
+---------Exported---------
+sayerror()
+	- Displays error message in new, closable, red div 
+
+throwMsgWithFilePathAndFnName()
+	- Generic throw message that includes the file and function name in this format: `Message [fileName functionName()]
+*/
+
 export function sayerror(holder, msg) {
 	const div = holder.append('div').attr('class', 'sja_errorbar')
 	// msg can contain injected XSS, so never do .html(msg)
@@ -10,4 +19,29 @@ export function sayerror(holder, msg) {
 		.on('click', () => {
 			disappear(div, true)
 		})
+}
+
+export function throwMsgWithFilePathAndFnName(message) {
+	try {
+		throw new Error()
+	} catch (error) {
+		const stackLines = error.stack.split('\n')
+		const callerLine = stackLines[1].trim()
+
+		// Extract the file name and function name
+		const regex = /\s*at\s+(.*)\s+\((.*).proteinpaint.js:(\d+):(\d+)\)/
+		const match = callerLine.match(regex)
+
+		if (match) {
+			const functionName = match[1]
+			const fileName = match[2]
+				.replace(/^.*\/bin\//, '') //remove window.origin
+				.replace(/_([^_]+)$/, '.$1') //readable file extension
+				.replaceAll(/_/g, '/') //readable file path pattern
+			const errorMsg = `${message} [${fileName} ${functionName}()]`
+			throw new Error(errorMsg)
+		} else {
+			throw new Error(message)
+		}
+	}
 }
