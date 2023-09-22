b9dbaf0da4e981ae613772c253ec17384b9a2fe2
Xin Zhou <xzhou82@gmail.com>
2023-09-22 12:10:00 -0500

    wip logic to parse simple tabular text data for snv/sv/cnv

--- client/plots/disco/launch.adhoc.ts
+++ client/plots/disco/launch.adhoc.ts
@@ -1,20 +1,29 @@
 import { Genome } from '#shared/types/index'
 
-/**
-from multiple possible data sources, parse all into an array and return
-app drawer will supply data for "snvText, svText, cnvText"
-while the "File" and "Url" can come from url parameters e.g. host?disco=1&snvFile=path/to/file
-
-Move these types to shared/server if used across multiple files
-
-*/
 type MutationListEntry = {
-	/**json list of data points, each element is an event with "dt:int" and ready to be used by disco */
+	/**json list of alteration events, each element is an event with "dt:int" and ready to be used by disco
+	FIXME different dt elements require different attributes but this design merge all into one type, can improve later
+	dt=1 {chr, position, gene, class}
+	dt=2 {chrA, posA, chrB, posB}
+	dt=4 {chr, start, stop}
+	*/
 	dt: number
-	chr: string
-	position: number
-	gene: string
-	class: string
+	chr?: string
+	position?: number
+	gene?: string
+	class?: string
+
+	// cnv
+	start?: number
+	stop?: number
+
+	// svfusion
+	chrA?: string
+	chrB?: string
+	posA?: number
+	posB?: number
+	geneA?: string
+	geneB?: string
 }
 
 type DiscoPlotArgs = {
@@ -37,9 +46,9 @@ type DiscoPlotArgs = {
 	/**tabular text of sv/fusion data, with follow columns
 	1. chrA
 	2. posA
-	3. chrB
-	4. posB
-	5. geneA
+	3. geneA
+	4. chrB
+	5. posB
 	6. geneB
 	each line is parsed into an object:
 	{dt:2, chrA, posA, geneA, chrB, posB, geneB} */
@@ -62,7 +71,11 @@ type DiscoPlotArgs = {
 //... more datatypes can be added later
 
 /**
- *  TODO add description about func
+ * from multiple possible data sources, parse mutation events and put everything into one array and return, for disco rendering
+ * app drawer will supply data for "snvText, svText, cnvText"
+ * while the "File" and "Url" can come from url parameters e.g. host?disco=1&snvFile=path/to/file
+ * TODO collect data error and display on ui
+ * TODO later make this script a native part of disco, so that runpp() directly invokes plot.app.js...
  * @param arg
  * @param genomeObj
  * @param holder
@@ -70,7 +83,12 @@ type DiscoPlotArgs = {
  */
 
 export async function launch(arg: DiscoPlotArgs, genomeObj: Genome, holder: Element) {
-	const mlst = await getMlst(arg)
+	const [mlst, errors] = await getMlst(arg)
+
+	if (errors?.length) {
+		// indicate errors on ui
+	}
+
 	const opts = {
 		holder,
 		vocabApi: {
@@ -114,20 +132,109 @@ export async function launch(arg: DiscoPlotArgs, genomeObj: Genome, holder: Elem
  * @param arg
  * @returns
  */
-
 async function getMlst(arg: DiscoPlotArgs) {
 	if (Array.isArray(arg.mlst)) {
-		// has preformatted in
-		return arg.mlst
+		// has preformatted in runpp()
+		return [arg.mlst as MutationListEntry[], null]
 	}
 
 	// parse data from text and files and append to one mlst[] array
-	const mlst = []
-	if (arg.snvText) {
-		// TODO parse and append to mlst
+	const mlst = [] as MutationListEntry[]
+	const errors = []
+
+	if (arg.snvText) parseSnvText(arg.snvText, mlst, errors)
+
+	if (arg.snvFile) {
+	}
+	if (arg.snvUrl) {
 	}
 
-	// ...
+	if (arg.svText) parseSvText(arg.svText, mlst, errors)
+	if (arg.svFile) {
+	}
+	if (arg.svUrl) {
+	}
 
-	return mlst
+	if (arg.cnvText) parseCnvText(arg.cnvText, mlst, errors)
+	if (arg.cnvFile) {
+	}
+	if (arg.cnvUrl) {
+	}
+
+	return [mlst, errors]
+}
+
+function parseSnvText(text: string, mlst: MutationListEntry[], errors: string[]) {
+	// TODO share a parser for snvindel text file with samples (with header line and non-fixed columns), but should not require sample here
+	for (const line of text.trim().split('\n')) {
+		const l = line.split('\t')
+
+		if (l.length != 5) {
+			// TODO collect err into errors[]
+			continue
+		}
+
+		let m
+		try {
+			m = {
+				dt: 1,
+				chr: l[0],
+				position: Number(l[1]),
+				gene: l[2],
+				mname: l[3],
+				class: l[4]
+			} as MutationListEntry
+		} catch (e) {
+			continue
+		}
+		mlst.push(m)
+	}
+}
+
+function parseSvText(text: string, mlst: MutationListEntry[], errors: string[]) {
+	for (const line of text.trim().split('\n')) {
+		const l = line.split('\t')
+		if (l.length != 6) {
+			// TODO collect err
+			continue
+		}
+		let m
+		try {
+			m = {
+				dt: 2,
+				chrA: l[0],
+				posA: Number(l[1]),
+				geneA: l[2],
+				chrB: l[3],
+				posB: Number(l[4]),
+				geneB: l[5]
+			} as MutationListEntry
+		} catch (e) {
+			continue
+		}
+		mlst.push(m)
+	}
+}
+
+function parseCnvText(text: string, mlst: MutationListEntry[], errors: string[]) {
+	for (const line of text.trim().split('\n')) {
+		const l = line.split('\t')
+		if (l.length != 4) {
+			// TODO err
+			continue
+		}
+		let m
+		try {
+			m = {
+				dt: 4,
+				chr: l[0],
+				start: Number(l[1]),
+				stop: Number(l[2]),
+				value: Number(l[3])
+			} as MutationListEntry
+		} catch (e) {
+			continue
+		}
+		mlst.push(m)
+	}
 }
