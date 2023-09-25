2ad0ac34cd51bc18912241e07e07fb574699150b
Airen Zaldivar <airenzp@gmail.com>
2023-09-25 11:57:58 -0500

    Fixed bug

--- client/plots/sampleScatter.interactivity.js
+++ client/plots/sampleScatter.interactivity.js
@@ -100,7 +100,7 @@ export function setInteractivity(self) {
 				const td = row.append('td')
 				if (showShape) {
 					const color = showColor ? self.getColor(d, chart) : self.config.colorTW ? 'gray' : self.settings.defaultColor
-					const shape = showColor && !showShape ? self.getShape(chart, d, 1, true) : self.getShape(chart, d)
+					const shape = showColor ? self.getShape(chart, d, 1, true) : self.getShape(chart, d)
 					const width = value.length * 9 + 60
 					const svg = td.append('svg').attr('width', width).attr('height', '35px')
 					const g = svg.append('g').attr('transform', 'translate(10, 16)')
