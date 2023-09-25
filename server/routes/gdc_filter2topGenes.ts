81a4a2eb4dea8aa2a14d1e47ce14f679da5fa493
Xin Zhou <xzhou82@gmail.com>
2023-09-25 16:53:06 -0500

    create new route gdc_filter2topGenes by moving some code

---
+++ server/routes/gdc_filter2topGenes.ts
@@ -0,0 +1,130 @@
+import { GdcTopMutatedGeneResponse } from '#shared/types/routes/gdc_filter2topGenes.ts'
+import path from 'path'
+import got from 'got'
+
+const apihost = process.env.PP_GDC_HOST || 'https://api.gdc.cancer.gov'
+
+export const api = {
+	endpoint: 'gdc_filter2topGenes',
+	methods: {
+		get: {
+			init({ genomes }) {
+				/*
+				genomes parameter is currently not used
+				could be used later to:
+				- verify hg38/GDC is on this instance and otherwise disable this route..
+				- perform conversion on gene name/id for future on needs
+				*/
+
+				return async (req: any, res: any): Promise<void> => {
+					try {
+						const genes = await getGenes(req.query)
+						const payload = { genes } as GdcTopMutatedGeneResponse
+						res.send(payload)
+					} catch (e: any) {
+						res.send({ status: 'error', error: e.message || e })
+					}
+				}
+			},
+			request: {
+				typeId: null
+				//valid: default to type checker
+			},
+			response: {
+				typeId: 'gdc_filter2topGenes'
+				// will combine this with type checker
+				//valid: (t) => {}
+			}
+		}
+	}
+}
+
+/*
+req.query {
+	filter // optional gdc GFF cohort filter, invisible and read only
+		FIXME should there be pp filter too?
+	geneFilter?: str
+	maxGenes: int
+}
+
+mayAddCGC2filter() are copied to
+/utils/gdc/topSSMgenes.js
+and hosted on https://proteinpaint.stjude.org/GDC/
+*/
+async function getGenes(q: any) {
+	let _f = { op: 'and', content: [] } // allow blank filter to test geneset edit ui (without filter)
+	if (q.filter) {
+		if (typeof q.filter != 'object') throw 'filter not object'
+		_f = q.filter
+	}
+	const response = await got.post(path.join(apihost, '/analysis/top_mutated_genes_by_project'), {
+		headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+		body: JSON.stringify({
+			size: q.maxGenes || 50,
+			fields: 'symbol',
+			filters: mayAddCGC2filter(_f, q.geneFilter)
+		})
+	})
+	const re = JSON.parse(response.body)
+	const genes = []
+	for (const hit of re.data.hits) {
+		if (!hit.symbol) continue
+		genes.push(hit.symbol)
+	}
+	return genes
+}
+
+/*
+str:
+	stringified gdc filter object, should not include the "genes.is_cancer_gene_census" filter element
+geneFilter: str
+	if = 'CGC', insert following element into the filter and return stringified obj
+
+	{
+		"op":"and",
+		"content":[
+			{
+				"content":{ "field":"genes.is_cancer_gene_census", "value":["true"] },
+				"op":"in"
+			}
+		]
+	}
+*/
+function mayAddCGC2filter(f: any, geneFilter?: string) {
+	// reformulate f into f2
+	// f may be "in" or "and". f2 is always "and", in order to join in additional filters
+	let f2
+
+	if (f.op == 'in') {
+		// wrap f into f2
+		f2 = { op: 'and', content: [f] }
+	} else if (f.op == 'and') {
+		// no need to wrap
+		f2 = f
+	} else {
+		throw 'f.op not "in" or "and"'
+	}
+
+	// per Phil on 12/16/2022, following filters ensure to return IDH1 as 1st gene for gliomas
+	f2.content.push({
+		op: 'NOT',
+		content: {
+			field: 'ssms.consequence.transcript.annotation.vep_impact',
+			value: 'missing'
+		}
+	})
+	f2.content.push({
+		op: 'in',
+		content: {
+			field: 'ssms.consequence.transcript.consequence_type',
+			value: ['missense_variant', 'frameshift_variant', 'start_lost', 'stop_lost', 'stop_gained']
+		}
+	})
+
+	if (geneFilter == 'CGC') {
+		// using CGC genes, add in filter
+		f2.content.push({ op: 'in', content: { field: 'genes.is_cancer_gene_census', value: ['true'] } })
+	}
+
+	return f2
+}
--- server/src/mds3.gdc.js
+++ server/src/mds3.gdc.js
@@ -40,8 +40,6 @@ getheaders
 **************** route handlers
 handle_gdc_ssms
 	getSingleSampleMutations
-handle_filter2topGenes
-	get_filter2topGenes
 		mayAddCGC2filter
 
 **************** internal
@@ -1972,113 +1970,6 @@ async function getSingleSampleMutations(query, ds, genome) {
 	}
 
 	return mlst
-}
-
-export function handle_filter2topGenes(genomes) {
-	return async (req, res) => {
-		/* query{}
-		.genome: required
-		.filter0: required
-		.geneFilter: str, required
-		*/
-		try {
-			const genome = genomes[req.query.genome]
-			if (!genome) throw 'invalid genome'
-			res.send({
-				genes: await get_filter2topGenes({
-					filter: req.query.filter0,
-					geneFilter: req.query.geneFilter,
-					maxGenes: req.query.maxGenes
-				})
-			})
-		} catch (e) {
-			if (e.stack) console.log(e.stack)
-			res.send({ error: e.message || e })
-		}
-	}
-}
-
-/*
-get_filter2topGenes() and mayAddCGC2filter() are copied to
-/utils/gdc/topSSMgenes.js
-and hosted on https://proteinpaint.stjude.org/GDC/
-*/
-async function get_filter2topGenes({ filter, geneFilter, maxGenes }) {
-	let _f = { op: 'and', content: [] } // allow blank filter to test geneset edit ui (without filter)
-	if (filter) {
-		if (typeof filter != 'object') throw 'filter not object'
-		_f = filter
-	}
-	const response = await got.post(path.join(apihost, '/analysis/top_mutated_genes_by_project'), {
-		headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-		body: JSON.stringify({
-			size: maxGenes || 50,
-			fields: 'symbol',
-			filters: mayAddCGC2filter(_f, geneFilter)
-		})
-	})
-	const re = JSON.parse(response.body)
-	const genes = []
-	for (const hit of re.data.hits) {
-		if (!hit.symbol) continue
-		genes.push(hit.symbol)
-	}
-	return genes
-}
-
-/*
-str:
-	stringified gdc filter object, should not include the "genes.is_cancer_gene_census" filter element
-geneFilter: str
-	if = 'CGC', insert following element into the filter and return stringified obj
-
-	{
-		"op":"and",
-		"content":[
-			{
-				"content":{ "field":"genes.is_cancer_gene_census", "value":["true"] },
-				"op":"in"
-			}
-		]
-	}
-*/
-function mayAddCGC2filter(f, geneFilter) {
-	// reformulate f into f2
-	// f may be "in" or "and". f2 is always "and", in order to join in additional filters
-	let f2
-
-	if (f.op == 'in') {
-		// wrap f into f2
-		f2 = { op: 'and', content: [f] }
-	} else if (f.op == 'and') {
-		// no need to wrap
-		f2 = f
-	} else {
-		throw 'f.op not "in" or "and"'
-	}
-
-	// per Phil on 12/16/2022, following filters ensure to return IDH1 as 1st gene for gliomas
-	f2.content.push({
-		op: 'NOT',
-		content: {
-			field: 'ssms.consequence.transcript.annotation.vep_impact',
-			value: 'missing'
-		}
-	})
-	f2.content.push({
-		op: 'in',
-		content: {
-			field: 'ssms.consequence.transcript.consequence_type',
-			value: ['missense_variant', 'frameshift_variant', 'start_lost', 'stop_lost', 'stop_gained']
-		}
-	})
-
-	if (geneFilter == 'CGC') {
-		// using CGC genes, add in filter
-		f2.content.push({ op: 'in', content: { field: 'genes.is_cancer_gene_census', value: ['true'] } })
-	}
-
-	return f2
 }
 
 /*
