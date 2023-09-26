9038e2d0a59bea8a136a9e596a648bfe94a5d0d1
Airen Zaldivar <airenzp@gmail.com>
2023-09-26 09:09:03 -0500

    First working version

--- client/mass/store.js
+++ client/mass/store.js
@@ -463,6 +463,7 @@ function validatePlot(p, vocabApi) {
 		} else if (p.chartType == 'profilePolar' || p.chartType == 'polar') {
 		} else if (p.chartType == 'DEanalysis') {
 		} else if (p.chartType == 'sampleView') {
+		} else if (p.chartType == 'profileRadar') {
 		} else {
 			validateGenericPlot(p, vocabApi)
 		}
