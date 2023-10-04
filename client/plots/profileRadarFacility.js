248b057253c141c89fce4f2b63d2aad6eb0eb873
Airen Zaldivar <airenzp@gmail.com>
2023-10-03 23:15:14 -0500

    Improved legend

--- client/plots/profileRadarFacility.js
+++ client/plots/profileRadarFacility.js
@@ -271,7 +271,7 @@ class profileRadarFacility {
 			)
 		this.legendG
 			.append('g')
-			.attr('transform', `translate(${x / 2}, ${y - 5})`)
+			.attr('transform', `translate(${0}, ${y - 5})`)
 			.append('circle')
 			.attr('r', 4)
 			.attr('fill', color)
