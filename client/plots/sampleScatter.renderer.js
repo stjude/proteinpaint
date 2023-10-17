453af52857aaf5271fb9c268b7a018ac5b62fac5
Airen Zaldivar <airenzp@gmail.com>
2023-10-17 12:50:01 -0500

    Added zoom and pan

--- client/plots/sampleScatter.renderer.js
+++ client/plots/sampleScatter.renderer.js
@@ -107,7 +107,7 @@ export function setRenderers(self) {
 		/* eslint-enable */
 
 		if (self.is3D) self.render3DSerie(chart)
-		else if (self.is2DThree) self.render2DSerieThree(chart)
+		else if (self.is2DLarge) self.render2DSerieLarge(chart)
 		else {
 			renderSerie(chart, s.duration)
 			self.renderLegend(chart)
@@ -268,9 +268,10 @@ export function setRenderers(self) {
 		self.mayRenderRegression()
 	}
 
-	self.render2DSerieThree = async function (chart) {
+	self.render2DSerieLarge = async function (chart) {
 		const THREE = await import('three')
-		const OrbitControls = await import('three/addons/controls/OrbitControls.js')
+		const DragControls = await import('three/examples/jsm/controls/DragControls.js')
+
 		chart.chartDiv.selectAll('*').remove()
 		self.canvas = chart.chartDiv.append('canvas').node()
 		self.canvas.width = self.settings.svgw * 1.5
@@ -281,30 +282,16 @@ export function setRenderers(self) {
 		const far = 1000
 		const camera = new THREE.PerspectiveCamera(fov, 1, near, far)
 		const scene = new THREE.Scene()
-		const controls = new OrbitControls.OrbitControls(camera, self.canvas)
-		controls.update()
 		camera.position.set(0, 0, 1)
 		camera.lookAt(scene.position)
-		//const axesHelper = new THREE.AxesHelper(3)
-		//scene.add(axesHelper)
 		camera.updateMatrix()
 		const whiteColor = new THREE.Color('rgb(255,255,255)')
 		scene.background = whiteColor
 
-		const light = new THREE.DirectionalLight(whiteColor, 2)
-		//light.position.set(2, 1, 5)
-		//scene.add(light)
 		const geometry = new THREE.BufferGeometry()
-		const vertices = []
-		const colors = []
-		for (const sample of chart.data.samples) {
-			let x = (chart.xAxisScale(sample.x) - chart.xScaleMin) / self.canvas.width
-			let y = (chart.yAxisScale(sample.y) - chart.yScaleMax) / -self.canvas.height
-			let z = (chart.zAxisScale(sample.z) - chart.zScaleMin) / self.settings.svgd
-			vertices.push(x - 0.5, y + 0.5, z)
-			const color = new THREE.Color(rgb(self.getColor(sample, chart)).toString())
-			colors.push(color.r, color.g, color.b)
-		}
+		const pointer = new THREE.Vector2()
+		const { vertices, colors } = getVertices()
+
 		geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
 		geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
 		const tex = getCircle(128)
@@ -317,9 +304,28 @@ export function setRenderers(self) {
 		})
 
 		const particles = new THREE.Points(geometry, material)
-		scene.add(particles)
 
+		scene.add(particles)
 		const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: self.canvas })
+		const controls = new DragControls.DragControls([particles], camera, renderer.domElement)
+
+		document.addEventListener('mousewheel', event => {
+			camera.position.z += event.deltaY / 500
+		})
+
+		function getVertices() {
+			const vertices = []
+			const colors = []
+			for (const sample of chart.data.samples) {
+				let x = (chart.xAxisScale(sample.x) - chart.xScaleMin) / self.canvas.width
+				let y = (chart.yAxisScale(sample.y) - chart.yScaleMax) / -self.canvas.height
+				let z = (chart.zAxisScale(sample.z) - chart.zScaleMin) / self.settings.svgd
+				vertices.push(x - pointer.x - 0.5, y - pointer.y + 0.5, z)
+				const color = new THREE.Color(rgb(self.getColor(sample, chart)).toString())
+				colors.push(color.r, color.g, color.b)
+			}
+			return { vertices, colors }
+		}
 
 		function getCircle(size) {
 			const c = document.createElement('canvas')
@@ -337,10 +343,8 @@ export function setRenderers(self) {
 
 		function animate() {
 			requestAnimationFrame(animate)
-
-			// required if controls.enableDamping or controls.autoRotate are set to true
-			controls.update()
-
+			camera.zoom = self.k
+			camera.updateProjectionMatrix()
 			renderer.render(scene, camera)
 		}
 		animate()
@@ -390,7 +394,6 @@ export function setRenderers(self) {
 
 		function animate() {
 			requestAnimationFrame(animate)
-
 			// required if controls.enableDamping or controls.autoRotate are set to true
 			controls.update()
 
@@ -736,15 +739,21 @@ export function setRenderers(self) {
 		}
 
 		function zoomIn() {
-			for (const chart of self.charts) zoom.scaleBy(chart.mainG.transition().duration(750), 1.5)
+			for (const chart of self.charts)
+				if (self.is2DLarge) self.k = self.k + 0.25
+				else zoom.scaleBy(chart.mainG.transition().duration(750), 1.5)
 		}
 
 		function zoomOut() {
-			for (const chart of self.charts) zoom.scaleBy(chart.mainG.transition().duration(750), 0.5)
+			for (const chart of self.charts)
+				if (self.is2DLarge) self.k = self.k - 0.25
+				else zoom.scaleBy(chart.mainG.transition().duration(750), 0.5)
 		}
 
 		function resetToIdentity() {
-			for (const chart of self.charts) chart.mainG.transition().duration(750).call(zoom.transform, zoomIdentity)
+			for (const chart of self.charts)
+				if (self.is2DLarge) self.k = 1
+				else chart.mainG.transition().duration(750).call(zoom.transform, zoomIdentity)
 		}
 
 		function toggle_lasso() {
