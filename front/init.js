25606d775b97f4cc0f300cb67dace760b56aa04a
Aleksandar Acic <aacic@stjude.org>
2023-11-06 13:45:45 -0600

    Move cards from server to front module.

--- front/init.js
+++ front/init.js
@@ -4,9 +4,12 @@
 const path = require('path')
 const ps = require('child_process')
 const fs = require('fs')
+const { execSync } = require('child_process')
 
 const URLPATH = process.argv[2] || '.'
 const CWD = process.cwd()
+
+console.log('CWD', CWD)
 try {
 	if (!fs.existsSync(`${CWD}/public`)) {
 		console.log(`making a public directory at ${CWD}`)
@@ -21,6 +24,12 @@ try {
 		// TODO: node 12 in pp-prt does not support rmSync,
 		// should update as part of v16 upgrade
 		fs.rmdirSync(`${CWD}/public/bin`, { recursive: true, force: true }, () => {})
+	}
+	if (!fs.existsSync(`${CWD}/public/cards`)) {
+		console.log(`Coping cards into public/cards folder`)
+		execSync(`cp -r ${CWD}/node_modules/@sjcrh/proteinpaint-front/public/cards ${CWD}/public/cards`, {
+			stdio: 'inherit'
+		})
 	}
 	const tar = ps.spawnSync('tar', [`-xzf`, `${__dirname}/bundles.tgz`, `-C`, `${CWD}`], { encoding: 'utf8' })
 	if (tar.stderr) throw tar.stderr
