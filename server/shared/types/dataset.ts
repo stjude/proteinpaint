42466d1418c12843ab6976dce93c7d03b81b1140
siosonel <siosonel@gmail.com>
2023-11-07 11:23:10 -0600

    fix the PCBySubcohort type definition

--- server/shared/types/dataset.ts
+++ server/shared/types/dataset.ts
@@ -507,9 +507,11 @@ type RestrictAncestriesEntry = {
 	tvs: Tvs
 	PCcount: number
 
-	// TODO declare that either PCTermId or PCBySubcohort is required
+	// TODO declare that either PCTermId or X is required
 	PCTermId?: string
-	PCBySubcohort?: {}
+	PCBySubcohort?: {
+		[subcohortId: string]: any
+	}
 }
 
 /*** types supporting Cohort type ***/
