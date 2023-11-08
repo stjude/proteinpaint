ce072c4135917326def16376c861d2f3877ebe9c
Colleen Reilly <creilly@stjude.org>
2023-11-08 15:48:20 -0600

    Improve hicstat types

--- server/shared/types/routes/hicstat.ts
+++ server/shared/types/routes/hicstat.ts
@@ -3,20 +3,26 @@ export type HicstatRequest = {
 	file?: string
 	/** Remote HiC file URL */
 	url?: string // FIXME use ts to enforce that either file or url must be given, abort if none is given
-	/** Position of first locus, in the format of chr:start:stop */
+}
 
-	pos1: string
-	/** Position of second locus, in the format of chr:start:stop */
-	pos2: string // portal code must validate pos1 and pos2 values, to prevent xxx:456-321
-	/** Resolution */
-	resolution: number
-	/** If is in fragment resolution */
-	isfrag?: boolean
+type Normalization = 'VC' | 'VC_SQRT' | 'KR' | 'NONE'
 
-	/** Normalization method for the queried data */
-	nmeth: string
-	/** Query data type, define enum of observed/oe/expected */
-	//datatype: string
-	/** Minimum value cutoff */
-	mincutoff: number
+export type HicstatResponse = {
+	error?: string
+	/** Only versions 7-9 are acceptable */
+	version: 7 | 8 | 9
+	'Genome ID': string
+	Chromosomes: {
+		/** Index of chr 1 through 22 */
+		[index: number]: number
+		All: number
+		X: number
+		Y: number
+		M: number
+	}
+	/** Orders Chromosomes keys (see above) */
+	chrorder: number[]
+	'Base pair-delimited resolutions': number[]
+	'Fragment-delimited resolutions': number[]
+	normalization: Normalization[]
 }
