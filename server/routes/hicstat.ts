d6595a122078b6fda89eee7683b3b974263eaa3d
Colleen Reilly <creilly@stjude.org>
2023-11-09 12:59:50 -0600

    Finalize typing in `server/src/hicstat.ts` and fix import statement

--- server/routes/hicstat.ts
+++ server/routes/hicstat.ts
@@ -1,5 +1,5 @@
 import { fileurl, file_is_readable } from '#src/utils.js'
-import { do_hicstat } from '#src/hicstat.js'
+import { do_hicstat } from '#src/hicstat.ts'
 import { HicstatRequest, HicstatResponse, HicstatRequestWithValidation } from '#shared/types/routes/hicstat.ts'
 
 export const api = {
@@ -38,26 +38,18 @@ export const api = {
 function init() {
 	return async (req: HicstatRequestWithValidation, res: any): Promise<void> => {
 		try {
-			await handle_hicstat(req, res)
-		} catch (e) {
-			console.log(`hicstat error: ${e} [server/routes/hicstat.ts handle_hicstat()]`)
+			const [e, file, isurl] = fileurl(req)
+			if (e) throw 'illegal file name'
+			if (!isurl) {
+				await file_is_readable(file)
+			}
+			const out = await do_hicstat(file, isurl)
+			res.send({ out })
+		} catch (e: any) {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			res.send({ error: e?.message || e })
+			if (e instanceof Error && e.stack) console.log(e)
 		}
-	}
-}
-
-async function handle_hicstat(req: HicstatRequest, res: any) {
-	try {
-		const [e, file, isurl] = fileurl(req)
-		if (e) throw 'illegal file name'
-		if (!isurl) {
-			await file_is_readable(file)
-		}
-		const out = (await do_hicstat(file, isurl)) as HicstatResponse
-		res.send({ out })
-	} catch (e: any) {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		res.send({ error: e?.message || e })
-		if (e instanceof Error && e.stack) console.log(e)
 	}
 }
