f8d638e3d465c84e4dda196e8cb294979fd78dac
Colleen Reilly <creilly@stjude.org>
2023-11-10 11:35:29 -0600

    Corrected import statement

--- client/src/block.tk.hicstraw.adaptor.ts
+++ client/src/block.tk.hicstraw.adaptor.ts
@@ -1,42 +1,36 @@
 export function hicstrawfromtemplate(tk: any, template: any) {
-	if(tk.textdata) {
-
-		if(!tk.textdata.raw) return '.textdata.raw missing'
-		if(typeof(tk.textdata.raw)!='string') return '.textdata.raw should be string'
+	if (tk.textdata) {
+		if (!tk.textdata.raw) return '.textdata.raw missing'
+		if (typeof tk.textdata.raw != 'string') return '.textdata.raw should be string'
 		delete template.enzyme
-
-	} else if(tk.bedfile || tk.bedurl) {
-
+	} else if (tk.bedfile || tk.bedurl) {
 		// bed file
-
 	} else {
-
 		// by hic straw file
-		if(!template.file && !template.url) return 'none of the data sources available: text data, bedj file, or juicebox file'
+		if (!template.file && !template.url)
+			return 'none of the data sources available: text data, bedj file, or juicebox file'
 	}
-	if(template.domainoverlay) {
-		if(!template.domainoverlay.file && !template.domainoverlay.url) return 'file or url missing for domainoverlay'
+	if (template.domainoverlay) {
+		if (!template.domainoverlay.file && !template.domainoverlay.url) return 'file or url missing for domainoverlay'
 		tk.domainoverlay = {}
-		for(const k in template.domainoverlay) {
+		for (const k in template.domainoverlay) {
 			tk.domainoverlay[k] = template.domainoverlay[k]
 		}
 	}
-	// the "hic" object to work with hic.straw.js
+	// the "hic" object to work with hic.straw.ts
 	tk.hic = {
-		enzyme:template.enzyme
+		enzyme: template.enzyme
 	}
 	delete tk.enzyme
 	return null
 }
 
-
 export function hicstrawmaketk(tk: any) {
-	tk.uninitialized=true
+	tk.uninitialized = true
 }
 
-
 export function hicstrawload(tk: any, block: any) {
-	import('./block.tk.hicstraw.ts').then(module=>{
-		module.loadTk(tk,block)
+	import('./block.tk.hicstraw.ts').then(module => {
+		module.loadTk(tk, block)
 	})
 }
