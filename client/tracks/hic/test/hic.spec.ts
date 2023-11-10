a2af1643b39bb3b69c925e289cfb715ecc9e3dae
Colleen Reilly <creilly@stjude.org>
2023-11-10 12:03:01 -0600

    Initial effort to create a ts type file; new hic test spec file (not for CI)

---
+++ client/tracks/hic/test/hic.spec.ts
@@ -0,0 +1,52 @@
+import tape from 'tape'
+import { HicRunProteinPaintTrackArgs } from '../../../types/hic.ts'
+import * as d3s from 'd3-selection'
+import { runproteinpaint } from '../../../test/front.helpers.js'
+//import { sleep, detectOne, detectGte } from '../../../test/test.helpers.js'
+
+function getHolder() {
+	return d3s
+		.select('body')
+		.append('div')
+		.style('border', '1px solid #aaa')
+		.style('padding', '5px')
+		.style('margin', '5px')
+		.node()
+}
+
+tape('\n', function (test) {
+	test.pass('-***- tracks/hic -***-')
+	test.end()
+})
+
+tape('Render Hi-C track', function (test) {
+	// test.plan(1)
+	test.timeoutAfter(3000)
+	const holder = getHolder() as HTMLDivElement
+
+	runproteinpaint({
+		holder,
+		block: true,
+		nobox: 1,
+		noheader: 1,
+		genome: 'hg19',
+		position: 'chr7:13749862-20841903',
+		nativetracks: 'RefGene',
+		tracks: [
+			{
+				type: 'hicstraw',
+				file: 'proteinpaint_demo/hg19/hic/hic_demo.hic',
+				name: 'Hi-C Demo',
+				percentile_max: 95,
+				mincutoff: 1,
+				pyramidup: 1,
+				enzyme: 'MboI',
+				normalizationmethod: 'VC'
+			} as HicRunProteinPaintTrackArgs
+		]
+	})
+	test.pass('Rendered Hi-C track')
+
+	// if (test._ok) holder.remove()
+	test.end()
+})
--- client/types/hic.ts
+++ client/types/hic.ts
@@ -1,11 +1,33 @@
+import { BaseTrackArgs } from './tracks.ts'
 import { Normalization } from '#shared/types/routes/hicstat.ts'
 
-export type HicstrawArgs = {
-	jwt: any
+type SharedArgs = {
 	/** HiC file path from tp/ */
 	file?: string
 	/** Remote HiC file URL */
 	url?: string
+}
+
+type RestrictionEnzyme = 'Dpnll' | 'EcoRl' | 'Hindlll' | 'MboI' | 'Ncol'
+
+export type HicRunProteinPaintTrackArgs = BaseTrackArgs &
+	SharedArgs & {
+		/** Specifies the track type
+		 * TODO: maybe move 'type' to BaseBlockArgs
+		 */
+		type: 'hicstraw'
+		name: string
+		percentile_max: number
+		mincutoff: number
+		/** Indicates whether the tip of the track points up or down */
+		pyramidup: number | boolean
+		enzyme: RestrictionEnzyme
+		/** Normalization method for the queried data */
+		normalizationmethod: Normalization
+	}
+
+export type HicstrawArgs = SharedArgs & {
+	jwt: any
 	pos1: string
 	pos2: string
 	/** Normalization method for the queried data */
