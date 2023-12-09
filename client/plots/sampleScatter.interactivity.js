64fb73475f45a1b2d29f5d6754520b640aee5320
Airen Zaldivar <airenzp@gmail.com>
2023-12-08 18:54:58 -0600

    Ordered samples considering important mutations

--- client/plots/sampleScatter.interactivity.js
+++ client/plots/sampleScatter.interactivity.js
@@ -34,6 +34,8 @@ export function setInteractivity(self) {
 		samples.sort((s1, s2) => {
 			if (!('sampleId' in s1)) return 1
 			if (s1.category.includes('Wildtype') || s1.category.includes('Not tested')) return 1
+			if (s1.shape.includes('Wildtype') || s1.shape.includes('Not tested')) return 1
+
 			return -1
 		})
 		if (samples.length == 0) return
