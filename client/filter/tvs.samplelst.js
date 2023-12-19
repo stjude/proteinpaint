100d2b44e6f66a7450e7ebad01b4262dc302548d
Airen Zaldivar <airenzp@gmail.com>
2023-12-19 11:47:02 -0600

    Fixed tvs.samplelst editor. Pending to update filter after updating tvs

--- client/filter/tvs.samplelst.js
+++ client/filter/tvs.samplelst.js
@@ -9,30 +9,54 @@ async function fillMenu(self, div, tvs) {
 	div.selectAll('*').remove()
 	div = div.append('div')
 	div.style('font-size', '0.8em')
-	div.append('div').style('margin', '10px').html(`<b>Select <i>${tvs.term.name}</i> samples:</b>`)
 	const rows = []
+	let samples, name
 	for (const field in tvs.term.values) {
-		const samples = tvs.term.values[field].list
-		for (const sample of samples) rows.push([{ value: sample.sample }])
+		name = field
+		samples = tvs.term.values[field].list //Only two possible groups and the second one contains the samples not in the first one
+		addTable(div, tvs, field)
+		break
 	}
+	div
+		.append('div')
+		.append('div')
+		.style('display', 'inline-block')
+		.style('float', 'right')
+		.style('padding', '6px 20px')
+		.append('button')
+		.attr('class', 'sjpp_apply_btn sja_filter_tag_btn')
+		.text('Apply')
+		.on('click', () => {
+			for (const field in tvs.term.values) {
+				const value = tvs.term.values[field]
+				value.list = value.list.filter(s => !('checked' in s) || s.checked)
+				break //only the first group is edited
+			}
+			self.opts.callback(tvs)
+		})
+}
+
+export function addTable(div, tvs, field) {
+	div
+		.style('padding', '6px')
+		.append('div')
+		.style('margin', '10px')
+		.style('font-size', '0.8rem')
+		.html(`<b> ${field}</b>.`)
+
+	const value = tvs.term.values[field]
+	const rows = []
+	for (const sample of value.list) rows.push([{ value: sample.sample }])
 	const columns = [{ label: 'Sample' }]
 	renderTable({
 		rows,
 		columns,
 		div,
-		maxWidth: '250px',
-		buttons: [
-			{
-				text: 'APPLY',
-				class: 'sjpp_apply_btn sja_filter_tag_btn',
-				callback: indexes => {
-					const values = []
-					for (const index of indexes) values.push(tvs.values[index])
-					tvs.values = values
-					self.opts.callback(tvs)
-				}
-			}
-		],
+		maxWidth: '30vw',
+		maxHeight: '40vh',
+		noButtonCallback: (index, node) => {
+			value.list[index].checked = node.checked
+		},
 		striped: false,
 		showHeader: false,
 		selectAll: true
@@ -40,8 +64,12 @@ async function fillMenu(self, div, tvs) {
 }
 
 function term_name_gen(d) {
-	const name = 'sample'
-	return name
+	let n
+	for (const group in d.term.values) {
+		n = d.term.values[group].list.length
+		break
+	}
+	return `n=${n}`
 }
 
 function get_pill_label(tvs) {
