1a6fdb0d7a200362cc947c69b50f7403d881fe24
siosonel <siosonel@gmail.com>
2024-01-03 14:29:40 -0600

    detect if bins exists for a numeric term, before processing its legend data

--- client/plots/matrix.legend.js
+++ client/plots/matrix.legend.js
@@ -93,13 +93,15 @@ export function getLegendData(legendGroups, refs, self) {
 				}
 			} else {
 				if (f.tvs.term.type == 'integer' || f.tvs.term.type == 'float') {
-					for (const v of f.tvs.ranges) {
-						const termValues = Object.values(t.ref.bins)
-						legend.values[v.name] = {
-							key: v.name,
-							label: termValues.find(vl => vl.name == v.name)?.name || v.name,
-							fill: termValues.find(vl => vl.name == v.name)?.color,
-							crossedOut: true
+					if (t.ref?.bins) {
+						for (const v of f.tvs.ranges) {
+							const termValues = Object.values(t.ref.bins)
+							legend.values[v.name] = {
+								key: v.name,
+								label: termValues.find(vl => vl.name == v.name)?.name || v.name,
+								fill: termValues.find(vl => vl.name == v.name)?.color,
+								crossedOut: true
+							}
 						}
 					}
 				} else {
