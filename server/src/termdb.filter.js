53e0aa674981129aaecf18b45c55957b0b6e41a6
Gavriel Matt <gavriel.matt@stjude.org>
2024-01-17 15:13:58 -0600

    verify ids of filter terms

--- server/src/termdb.filter.js
+++ server/src/termdb.filter.js
@@ -47,6 +47,8 @@ export async function getFilterCTEs(filter, ds, CTEname = 'f') {
 			// .CTEname: str
 		} else if (!item.tvs) {
 			throw `filter item should have a 'tvs' or 'lst' property`
+		} else if (!ds.cohort.termdb.q.termjsonByOneid(item.tvs.term.id)) {
+			throw `missing dictionary term for filter item id=${item.tvs.term.id}`
 		} else if (item.tvs.term.type == 'categorical') {
 			f = get_categorical(item.tvs, CTEname_i)
 			// .CTEs: []
