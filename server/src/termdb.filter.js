b222d55a04d6f8988586a7d0a04cb5e4d250c6df
Gavriel Matt <gavriel.matt@stjude.org>
2024-01-18 14:45:00 -0600

    change error message for invalid id

--- server/src/termdb.filter.js
+++ server/src/termdb.filter.js
@@ -48,7 +48,7 @@ export async function getFilterCTEs(filter, ds, CTEname = 'f') {
 		} else if (!item.tvs) {
 			throw `filter item should have a 'tvs' or 'lst' property`
 		} else if (!ds.cohort.termdb.q.termjsonByOneid(item.tvs.term.id)) {
-			throw `missing dictionary term for filter item id=${item.tvs.term.id}`
+			throw 'invalid term id in tvs'
 		} else if (item.tvs.term.type == 'categorical') {
 			f = get_categorical(item.tvs, CTEname_i)
 			// .CTEs: []
