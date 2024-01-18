00a4cc22a71eaecfea181f5a390a22d701582e78
Airen Zaldivar <airenzp@gmail.com>
2024-01-18 11:33:44 -0600

    Tested that when the filter is Mexico one slice has percentage 58

--- client/mass/test/profile.spec.js
+++ client/mass/test/profile.spec.js
@@ -68,16 +68,29 @@ tape('Test polar', function (test) {
 	openPlot('profilePolar', runTests)
 
 	async function runTests(plot) {
-		console.log(plot)
-		//plot.on('postRender.test', null)
+		plot.on('postRender.test', null)
 
 		const self = plot.Inner
 		const svg = self.dom.plotDiv.select('svg')
 		const tableDiv = self.dom.tableDiv
-		console.log(svg)
 		test.true(!svg.empty(), `Should render one svg`)
 		test.true(svg.selectAll('path').size() > 0, `Should render several slices`)
 		test.true(!tableDiv.select('table').empty(), `Should have a table`)
+		const matched = await detectLst({
+			target: self.dom.plotDiv.node(),
+			selector: 'path',
+
+			trigger() {
+				self.app.dispatch({ type: 'plot_edit', id: self.id, config: { settings: { country: 'Mexico' } } })
+			},
+			matcher(mutations) {
+				return mutations
+			}
+		})
+		const slice = matched.find(
+			m => m.target.__data__.module == 'National Context' && self.getPercentage(m.target.__data__) == 58
+		)
+		test.true(slice != null, `Should find a slice for National Context with percentage 58`)
 
 		if (test._ok) self.app.destroy()
 		test.end()
