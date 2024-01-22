2192891038a65fcab175ab2b3ba55da782937851
congyu-lu <clu@stjude.com>
2024-01-22 12:52:02 -0600

    disable edit for matrix geneVariant term setting

--- client/plots/matrix.interactivity.js
+++ client/plots/matrix.interactivity.js
@@ -441,6 +441,8 @@ function setTermActions(self) {
 	self.getMenuOptions = function (t) {
 		return self.chartType == 'hierCluster' && t.grp.name === self.config.settings.hierCluster?.termGroupName
 			? '{remove,}'
+			: t.tw.term.type == 'geneVariant'
+			? '{replace,remove}'
 			: '*'
 	}
 
