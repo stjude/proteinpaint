513be21b01feb07912e91050abe3dcff6e871f86
congyu-lu <clu@stjude.com>
2024-01-26 11:20:58 -0600

    do not disable submit button when not in expression mode

--- client/dom/genesetEdit.ts
+++ client/dom/genesetEdit.ts
@@ -300,8 +300,8 @@ export function showGenesetEdit(arg: showGenesetEditArg) {
 		api.dom.clearBtn.property('disabled', !geneList?.length)
 		const hasChanged = origNames !== JSON.stringify(geneList.map(t => t.gene).sort())
 		api.dom.restoreBtn?.property('disabled', !hasChanged)
-		// disable submit button when gene list not changed or empty
-		api.dom.submitBtn.property('disabled', !hasChanged || !geneList?.length)
+		// disable submit button when gene list not changed or is empty in expression mode
+		api.dom.submitBtn.property('disabled', !hasChanged || (mode == 'expression' && !geneList?.length))
 		if (hasChanged) submitBtn.node().focus()
 	}
 
