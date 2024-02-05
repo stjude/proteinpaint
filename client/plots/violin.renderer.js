3400adf43f67848eefbc2ae3d589db4bc1deba12
Airen Zaldivar <airenzp@gmail.com>
2024-02-05 16:23:55 -0600

    Reduced spaces when rendering plots by using d3 curveMonotoneX

--- client/plots/violin.renderer.js
+++ client/plots/violin.renderer.js
@@ -1,6 +1,6 @@
 import { axisLeft, axisTop } from 'd3-axis'
 import { scaleLinear, scaleLog } from 'd3-scale'
-import { area, curveBasis, line } from 'd3-shape'
+import { curveMonotoneX, line } from 'd3-shape'
 import { getColors } from '#shared/common'
 import { brushX, brushY } from 'd3-brush'
 import { renderTable } from '../dom/table'
@@ -321,12 +321,12 @@ export default function setViolinRenderer(self) {
 		let areaBuilder
 		if (isH) {
 			areaBuilder = line()
-				.curve(curveBasis)
+				.curve(curveMonotoneX)
 				.x(d => svg.axisScale(d.x0))
 				.y(d => wScale(d.density))
 		} else {
 			areaBuilder = line()
-				.curve(curveBasis)
+				.curve(curveMonotoneX)
 				.x(d => wScale(d.density))
 				.y(d => svg.axisScale(d.x0))
 		}
