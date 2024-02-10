e08d4a8ccb4c817b20a7b5406a58372664e037df
siosonel <siosonel@gmail.com>
2024-02-10 12:21:52 -0600

    include types for the raw input data to urljson encoder() in the unit tests

--- server/shared/test/urljson.unit.spec.ts
+++ server/shared/test/urljson.unit.spec.ts
@@ -1,5 +1,5 @@
 import tape from 'tape'
-import { encode, decode } from '../urljson'
+import { encode, decode, UrlJsonRaw, UrlJsonEncoded } from '../urljson'
 
 /*************************
  reusable helper functions
@@ -16,28 +16,28 @@ function preprocess(params) {
 // Test that the original values are recovered with the expected type
 
 tape('string values', test => {
-	const params = { a: 'abc', b: '123', c: '{}', d: '[]', e: 'false', f: 'true', g: 'null', h: 'undefined' }
+	const params: UrlJsonRaw = { a: 'abc', b: '123', c: '{}', d: '[]', e: 'false', f: 'true', g: 'null', h: 'undefined' }
 	const encodedParams = encode(structuredClone(params))
 	test.deepEqual(params, decode(preprocess(encodedParams)), 'should correct encode and decode string values')
 	test.end()
 })
 
 tape('numeric values', test => {
-	const params = { a: 1, b: 0.99, c: 1e2 }
+	const params: UrlJsonRaw = { a: 1, b: 0.99, c: 1e2 }
 	const encodedParams = encode(structuredClone(params))
 	test.deepEqual(params, decode(preprocess(encodedParams)), 'should correct encode and decode numeric values')
 	test.end()
 })
 
 tape('boolean values', test => {
-	const params = { a: true, b: false }
+	const params: UrlJsonRaw = { a: true, b: false }
 	const encodedParams = encode(structuredClone(params))
 	test.deepEqual(params, decode(preprocess(encodedParams)), 'should correct encode and decode boolean values')
 	test.end()
 })
 
 tape('array values', test => {
-	const params = { a: ['xyz', 1, 0, true, false, null, 'null', undefined, { b: 5 }] }
+	const params: UrlJsonRaw = { a: ['xyz', 1, 0, true, false, null, 'null', undefined, { b: 5 }] }
 	const encodedParams = encode(structuredClone(params))
 	// Special handling of undefined value in an array, which JSON.stringify() converts to null
 	params.a[params.a.indexOf(undefined)] = null
@@ -46,7 +46,7 @@ tape('array values', test => {
 })
 
 tape('object values', test => {
-	const params = { a: {}, b: { 1: 'test', nested: { x: 99 } }, c: ['xyz', true], d: undefined, e: null }
+	const params: UrlJsonRaw = { a: {}, b: { 1: 'test', nested: { x: 99 } }, c: ['xyz', true], d: undefined, e: null }
 	const encodedParams = encode(structuredClone(params))
 	// special handling of undefined value in an object, which JSON.stringify deletes from the object
 	for (const key in params) {
