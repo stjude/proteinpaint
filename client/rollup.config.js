5486c5946dfbe54184756d034d1169f4a90cca4b
siosonel <siosonel@gmail.com>
2024-02-11 01:31:26 -0600

    configure the rollup typescript plugin before commonjs

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -32,14 +32,14 @@ export default [
 				extensions: ['.js', '.ts']
 			}),
 			json(),
+			typescript({
+				filterRoot: '../'
+			}),
 			commonjs({
 				extensions: ['.js', '.ts']
 			}),
 			postcss({
 				plugins: [postcssImport()]
-			}),
-			typescript({
-				filterRoot: '../'
 			}),
 			dynamicImportVars(),
 			// for GDC webpack 3 use case: do not use terser by running
