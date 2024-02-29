a4e08f86d1c6dae51fd5ed660a1939057489966e
siosonel <siosonel@gmail.com>
2024-02-29 13:10:10 -0600

    create a route template

---
+++ server/routes/_template_.ts
@@ -0,0 +1,29 @@
+export const api: any = {
+	// route endpoint
+	// - no need for trailing slash
+	// - should be a noun (method is based on HTTP GET, POST, etc)
+	// - don't add 'Data' as response is assumed to be data
+	endpoint: '...',
+	methods: {
+		get: {
+			init,
+			request: {
+				typeId: 'any'
+			},
+			response: {
+				typeId: 'any'
+			}
+		},
+		post: {
+			alternativeFor: 'get',
+			init
+		}
+	}
+}
+
+function init({ genomes }) {
+	return async function (req, res) {
+		// to avoid linter unused var errors
+		console.log(genomes, req, res)
+	}
+}
