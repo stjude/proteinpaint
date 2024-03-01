3336dbc5dcaf5d029a15403d1c0994535595bc24
Colleen Reilly <creilly@stjude.org>
2024-03-01 16:18:59 -0600

    Functioning notification cascade based on user changes in the control panel

--- client/tracks/hic/store.ts
+++ client/tracks/hic/store.ts
@@ -7,6 +7,7 @@ class HicStore {
 	}
 	actions: any
 	state: any
+	views = ['genome', 'chrpair', 'detail', 'horizontal']
 
 	constructor() {
 		this.type = 'store'
@@ -19,13 +20,21 @@ class HicStore {
 export const hicStoreInit = getStoreInit(HicStore)
 
 HicStore.prototype.actions = {
-	view_change(action) {
+	view_change(action: { view: string; config: any }) {
 		if (!action.view) throw Error('view_change: missing view')
+		if (!this.views.some((v: string) => v == action.view)) throw Error(`view_change: unknown view = ${action.view}`)
 		this.state.currView = action.view
 	},
-	view_update(action) {
-		if (!action.view) throw Error('view_update: missing view')
-		const opts = Object.assign(this.state[action.view], action.config)
-		this.state[action.view] = opts
+	view_update(action: { view?: string; config: any }) {
+		if (action.view) {
+			//Target only a specific view
+			if (!this.views.some((v: string) => v == action.view)) throw Error(`view_update: unknown view = ${action.view}`)
+			const opts = Object.assign(this.state[action.view], action.config)
+			this.state[action.view] = opts
+		} else {
+			//Update any value of the state
+			const opts = Object.assign(this.state, action.config)
+			this.state = opts
+		}
 	}
 }
