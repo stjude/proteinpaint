1afc8e3ce9e9ee764c32a537602011b294a87152
gavrielm <gmatt@stjude.org>
2024-03-12 12:05:16 -0500

    PP code to handle 'hideTitle' flag from dataset (#1434)
    * handle the 'hideTitle' flag in the pp code
    
    * check for presence of 'title' in 'ds.cohort' instead of checking for 'hideTitle'
    
    ---------
    
    Co-authored-by: Gavriel Matt <gavriel.matt@stjude.org>

--- server/routes/termdb.config.ts
+++ server/routes/termdb.config.ts
@@ -88,7 +88,7 @@ function make(q, res, ds, genome) {
 		isGeneSetTermdb: tdb.isGeneSetTermdb,
 		lollipop: tdb.lollipop,
 		urlTemplates: tdb.urlTemplates,
-		title: ds.cohort.title || ds.label
+		title: 'title' in ds.cohort ? ds.cohort.title : ds.label
 	}
 	// optional attributes
 	// when missing, the attribute will not be present as "key:undefined"
