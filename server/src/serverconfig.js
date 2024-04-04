2a386a513c9ebffcafbfa6a27f370faa56e8e35f
siosonel <siosonel@gmail.com>
2024-04-04 14:02:42 -0500

    handle missing serverconfig.URL when detecting trailing slash

--- server/src/serverconfig.js
+++ server/src/serverconfig.js
@@ -160,7 +160,7 @@ if (serverconfig.allow_env_overrides) {
 	}
 }
 
-if (serverconfig.URL.endsWith('/')) serverconfig.URL = serverconfig.URL.slice(0, -1)
+if (serverconfig.URL?.endsWith('/')) serverconfig.URL = serverconfig.URL.slice(0, -1)
 
 // always change selected configuration paths in a container
 if (process.env.PP_MODE?.startsWith('container')) {
