fcc60db96d30ec7f551ff4eb4bf4cbd0410b76fd
siosonel <siosonel@gmail.com>
2024-04-07 21:23:32 -0500

    support drag-drop of matrix tiebreaker labels to resort tiebreaker data within a sort priority

--- client/plots/test/matrix.sorterUi.unit.spec.js
+++ client/plots/test/matrix.sorterUi.unit.spec.js
@@ -59,6 +59,37 @@ tape('default setup', async test => {
 	test.end()
 })
 
+tape('section visibility toggling', async test => {
+	const { uiApi, controls, config, parent, opts } = await getControls()
+	const s = config.settings.matrix
+	const ui = uiApi.Inner
+	const activeOptionBeforeDrag = structuredClone(ui.activeOption)
+	const thead = opts.holder
+		.selectAll('thead')
+		.filter(d => d?.types?.includes('geneVariant'))
+		.node()
+
+	test.equal(
+		thead.nextSibling.style.display,
+		'none',
+		'should have a hiddden table section for gene mutation before toggling'
+	)
+
+	thead.firstChild.firstChild.click()
+
+	test.equal(
+		opts.holder
+			.selectAll('thead')
+			.filter(d => d?.types?.includes('geneVariant'))
+			.node().nextSibling.style.display,
+		'',
+		'should have a visible table section for gene mutation after toggling'
+	)
+
+	if (test._ok) uiApi.destroy()
+	test.end()
+})
+
 tape('simulated section drag/drop', async test => {
 	const { uiApi, controls, config, parent, opts } = await getControls()
 	const s = config.settings.matrix
@@ -70,6 +101,14 @@ tape('simulated section drag/drop', async test => {
 		.node()
 	const sectionData = th.__data__
 	ui.trackDraggedSection.call(th, {}, sectionData)
+
+	// since this simulated test does not trigger actual drag and drop,
+	// must make sure that the correct event handler is tested here
+	test.equal(
+		select(th.parentNode.parentNode).on('drop'),
+		ui.adjustSortPriority,
+		'should attach the correct drop handler for section thead'
+	)
 
 	const i = ui.activeOption.sortPriority.indexOf(sectionData)
 	ui.adjustSortPriority({}, ui.activeOption.sortPriority[i + 1])
@@ -86,6 +125,71 @@ tape('simulated section drag/drop', async test => {
 		activeOptionBeforeDrag.sortPriority.reverse(),
 		config.settings.matrix.sortOptions[s.sortSamplesBy].sortPriority,
 		'should adjust the sortPriority/table after clicking apply'
+	)
+
+	if (test._ok) uiApi.destroy()
+	test.end()
+})
+
+tape('simulated tiebreaker drag/drop', async test => {
+	const { uiApi, controls, config, parent, opts } = await getControls()
+	const s = config.settings.matrix
+	const ui = uiApi.Inner
+	const activeOptionBeforeDrag = structuredClone(ui.activeOption)
+	const thead0 = opts.holder
+		.selectAll('thead')
+		.filter(d => d?.types?.includes('geneVariant'))
+		.node()
+
+	thead0.firstChild.firstChild.click()
+
+	const thead1 = opts.holder
+		.selectAll('thead')
+		.filter(d => d?.types?.includes('geneVariant'))
+		.node()
+
+	const trs = thead1.nextSibling.querySelectorAll('tr') //thead.__data__.tiebreakers // select(thead.nextSibling).selectAll('tr').each(d => )
+
+	// since this simulated test does not trigger actual drag and drop,
+	// must make sure that the correct event handler is tested here
+
+	test.equal(
+		select(trs[0]).on('drop'),
+		ui.adjustTieBreakers,
+		'should attach the correct drop handler for tiebreaker label'
+	)
+
+	const tbData = trs[0].__data__
+	ui.trackDraggedTieBreaker.call(trs[0], { target: trs[0].firstChild.nextSibling }, tbData)
+
+	const activeTieBreakers = ui.activeOption.sortPriority[0].tiebreakers
+	const i = activeTieBreakers.indexOf(tbData)
+	console.log(activeTieBreakers[i + 1].label)
+	ui.adjustTieBreakers({ preventDefault: () => undefined }, activeTieBreakers[i + 1])
+
+	const thead2 = opts.holder
+		.selectAll('thead')
+		.filter(d => d?.types?.includes('geneVariant'))
+		.node()
+	const trs2 = [...thead2.nextSibling.querySelectorAll('tr')]
+	test.deepEqual(
+		trs2.slice(0, 2).map(elem => elem.__data__),
+		activeOptionBeforeDrag.sortPriority[0].tiebreakers.slice(0, 2).reverse(),
+		'should visibly switch the first two tiebreaker rows'
+	)
+
+	test.deepEqual(
+		activeOptionBeforeDrag.sortPriority[0].tiebreakers,
+		config.settings.matrix.sortOptions[s.sortSamplesBy].sortPriority[0].tiebreakers,
+		'should not adjust the tiebreakers before clicking apply, after drag/drop'
+	)
+
+	ui.apply()
+
+	test.deepEqual(
+		activeTieBreakers,
+		config.settings.matrix.sortOptions[s.sortSamplesBy].sortPriority[0].tiebreakers,
+		'should adjust the tiebreakers after clicking apply'
 	)
 
 	if (test._ok) uiApi.destroy()
