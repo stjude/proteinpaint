42139e9f7888858d56a6c9f129bba3f8b417753f
robinpaul85 <robinpaul85@gmail.com>
2024-05-01 12:28:58 -0500

    Output only top 100 pathways

--- server/shared/types/routes/genesetOverrepresentation.ts
+++ server/shared/types/routes/genesetOverrepresentation.ts
@@ -1,12 +1,12 @@
 export type genesetOverrepresentationRequest = {
-	sample_genes: string
-	background_genes: string
-	genome: string
-	geneSetGroup: string
+	sample_genes: string // Sample genes to be queried
+	background_genes: string // Background genes against which the sample genes will be queried
+	genome: string // Genome build
+	geneSetGroup: string // Type of GO to be queried e.g MF, CC, BP
 }
 
 export type genesetOverrepresentationResponse = {
-	pathway_name: string
-	p_value_original: number
-	p_value_adjusted: number
+	pathway_name: string // Name of pathway
+	p_value_original: number // Original p-value
+	p_value_adjusted: number // Adjusted p-value
 }
