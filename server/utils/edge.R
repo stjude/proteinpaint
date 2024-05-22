2e713050d2e10e8fafadfe6ff3d29a764945a01d
robinpaul85 <robinpaul85@gmail.com>
2024-05-22 11:45:44 -0500

    Passed min_count, min_total_count to edgeR

--- server/utils/edge.R
+++ server/utils/edge.R
@@ -1,3 +1,9 @@
+# Usage: echo <in_json> | Rscript edge.R > <out_json>
+
+#   in_json: [string] input data in JSON format. Streamed through stdin.
+#   out_json: [string] clustering results in JSON format. Streamed to stdout.
+
+
 # json='{"case":"SJMB066856,SJMB069601,SJMB030827,SJMB030838,SJMB031131,SJMB031227,SJMB077221,SJMB077223","control":"SJMB069596,SJMB069587,SJMB074736,SJMB030488,SJMB030825,SJMB031110,SJMB032998,SJMB033002","input_file":"/Users/rpaul1/pp_data/files/hg38/sjmb12/rnaseq/geneCounts.txt"}' && time echo $json | Rscript edge.R
 
 # json='{"case":"SJMB030827,SJMB030838,SJMB064540,SJMB064538,SJMB064520,SJMB064535,SJMB031131,SJMB031227","control":"SJMB030488,SJMB030825,SJMB064537,SJMB064510,SJMB064533,SJMB064534,SJMB031110","input_file":"/Users/rpaul1/pp_data/files/hg38/sjmb12/rnaseq/geneCounts.txt"}' && time echo $json | Rscript edge.R
@@ -28,21 +34,28 @@ suppressWarnings({
     suppressPackageStartupMessages(library(dplyr))
 })
 
-infile <- file("stdin")
-input <- fromJSON(infile)
-print (input$output_path)
+con <- file("stdin", "r")
+json <- readLines(con, warn=FALSE)
+close(con)
+input <- fromJSON(json)
+#print (input)
+#print (input$output_path)
 cases <- unlist(strsplit(input$case, ","))
 controls <- unlist(strsplit(input$control, ","))
 combined <- c("geneID","geneSymbol",cases,controls)
 #data %>% select(all_of(combined))
-read_file_time_start <- Sys.time()
+#read_file_time_start <- Sys.time()
+suppressWarnings({
+  suppressMessages({
 read_counts <- read_tsv(input$input_file, col_names = TRUE, col_select = combined)
+  })
+})
 geneIDs <- unlist(read_counts[1])
 geneSymbols <- unlist(read_counts[2])
 read_counts <- select(read_counts, -geneID)
 read_counts <- select(read_counts, -geneSymbol)
-read_file_time_stop <- Sys.time()
-print (read_file_time_stop - read_file_time_start)
+#read_file_time_stop <- Sys.time()
+#print (read_file_time_stop - read_file_time_start)
 
 diseased <- rep("Diseased", length(cases))
 control <- rep("Control", length(controls))
@@ -50,16 +63,20 @@ conditions <- c(diseased, control)
 tabs <- rep("\t",length(geneIDs))
 gene_id_symbols <- paste0(geneIDs,tabs,geneSymbols)
 y <- DGEList(counts = as.matrix(read_counts), group = conditions, genes = gene_id_symbols)
-keep <- filterByExpr(y)
+keep <- filterByExpr(y, min.count = input$min_count, min.total.count = input$min_total_count)
 y <- y[keep, keep.lib.sizes = FALSE]
 y <- calcNormFactors(y, method = "TMM")
 #print (y)
-calculate_DE_time_start <- Sys.time()
-dge <- estimateDisp(y = y)
+#calculate_DE_time_start <- Sys.time()
+suppressWarnings({
+  suppressMessages({
+      dge <- estimateDisp(y = y)
+  })
+})
 et <- exactTest(object = dge)
 calculate_DE_time_stop <- Sys.time()
-print ("Time to calculate DE")
-print (calculate_DE_time_stop - calculate_DE_time_start)
+#print ("Time to calculate DE")
+#print (calculate_DE_time_stop - calculate_DE_time_start)
 #print (et)
 logfc <- et$table$logFC
 logcpm <- et$table$logCPM
@@ -76,11 +93,13 @@ names(output)[3] <- "fold_change"
 names(output)[4] <- "original_p_value"
 names(output)[5] <- "adjusted_p_value"
 
-output_json <- toJSON(output)
-print ("output_json")
-output_file <- paste0(input$output_path,"/r_output.txt")
-print (output_file)
-cat(output_json, file = output_file)
+toJSON(output)
+#output_json <- toJSON(output)
+#print ("output_json")
+#output_file <- paste0(input$output_path,"/r_output.txt")
+#print (output_file)
+#cat(output_json, file = output_file)
+
 #top_degs = topTags(object = et, n = "Inf")
 #print ("top_degs")
 #print (top_degs)
