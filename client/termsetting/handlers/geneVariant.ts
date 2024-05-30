9b785b31c5b7590b3d272ac50528d087ccd328b4
Gavriel Matt <gavriel.matt@stjude.org>
2024-05-30 13:58:33 -0500

    Support groupsetting in geneVariant edit UI

--- client/termsetting/handlers/geneVariant.ts
+++ client/termsetting/handlers/geneVariant.ts
@@ -1,7 +1,7 @@
 import { select } from 'd3-selection'
 import { mclass, dt2label, dtsnvindel, dtcnv, dtsv, dtfusionrna } from '../../shared/common'
 import { VocabApi, GeneVariantTermSettingInstance, GeneVariantTW } from '../../shared/types/index'
-import { PredefinedGroupSetting } from '../../shared/types/terms/term'
+import { make_radios } from '#dom/radiobutton'
 
 /* 
 instance attributes
@@ -135,44 +135,6 @@ export function fillTW(tw: GeneVariantTW, vocabApi: VocabApi) {
 	if (!tw.q.groupsetting) tw.q.groupsetting = {}
 	delete tw.q.groupsetting.disabled
 	if (!('inuse' in tw.q.groupsetting)) tw.q.groupsetting.inuse = false
-	if (tw.q.groupsetting.inuse) {
-		// groupsetting is active
-		const gs = tw.q.groupsetting as PredefinedGroupSetting
-		/* is the following necessary? (copied from client/termsetting/handlers/categorical.ts). useIndex does not seem to be used in the codebase.
-		if (
-			gs.lst &&
-			//Typescript emits error that .useIndex could be undefined
-			gs.useIndex &&
-			//Fix checks if property is present
-			gs.useIndex >= 0 &&
-			gs.lst[gs.useIndex]
-		) {
-			gs.predefined_groupset_idx = gs.useIndex
-		}*/
-		gs.predefined_groupset_idx = 0
-
-		// specify a single dt
-		const ds_dts = [] // dts specified in dataset
-		for (const query of Object.keys(vocabApi.termdbConfig.queries)) {
-			if (query == 'snvindel') ds_dts.push(dtsnvindel)
-			else if (query == 'cnv') ds_dts.push(dtcnv)
-			else if (query == 'svfusion') ds_dts.push(dtfusionrna)
-			else if (query == 'sv') ds_dts.push(dtsv) // TODO: is this correct?
-			else continue
-		}
-		if (!tw.q.dt) tw.q.dt = ds_dts[0] // default dt will be first in dataset
-		if (!(tw.q.dt in dt2label)) throw 'invalid dt'
-		if (!ds_dts.includes(tw.q.dt)) throw 'dt not supported in dataset'
-
-		// specify a single origin
-		// TODO: verify that 'vocabApi.termdbConfig.assayAvailability.byDt[dt].byOrigin' will always be defined in dataset when dt has multiple origins
-		if (vocabApi.termdbConfig.assayAvailability?.byDt[tw.q.dt]?.byOrigin) {
-			// dt has multiple origins in dataset
-			// so an origin must be specified
-			if (!tw.q.origin) tw.q.origin = 'somatic' /*'germline'*/
-			if (!(tw.q.origin in vocabApi.termdbConfig.assayAvailability.byDt[tw.q.dt].byOrigin)) throw 'invalid dt origin'
-		}
-	}
 
 	{
 		// apply optional ds-level configs for this specific term
@@ -213,6 +175,130 @@ function makeEditMenu(self: GeneVariantTermSettingInstance, _div: any) {
 	const div = _div.append('div').style('padding', '5px').style('cursor', 'pointer')
 
 	div.append('div').style('font-size', '1.2rem').text(self.term.name)
+
+	const optsDiv = div.append('div').style('margin-top', '10px')
+	const groupsDiv = div.append('div').style('margin-left', '30px').style('display', 'none')
+	const dtDiv = groupsDiv.append('div').style('margin-top', '15px')
+	const originDiv = groupsDiv.append('div').style('margin-top', '15px')
+	const groupsetDiv = groupsDiv.append('div').style('margin-top', '15px')
+
+	// TODO: add apply button. Try to implement in barchart.
+
+	// radio buttons for whether or not to group variants
+	optsDiv.append('div').style('font-weight', 'bold').text('Group variants')
+	const radios = make_radios({
+		holder: optsDiv,
+		options: [
+			{ label: 'No variant grouping', value: false, checked: !self.q.groupsetting.inuse },
+			{ label: 'Assign variants to groups', value: true, checked: self.q.groupsetting.inuse }
+		],
+		callback: v => {
+			if (v) {
+				self.q.groupsetting.inuse = true
+				makeRadiosForGrouping()
+			} else {
+				self.q.groupsetting.inuse = false
+				delete self.q.dt
+				delete self.q.origin
+				groupsDiv.style('display', 'none')
+			}
+		}
+	})
+	if (radios.inputs.filter(':checked').datum().value) makeRadiosForGrouping()
+
+	// make radio buttons for grouping variants
+	function makeRadiosForGrouping() {
+		groupsDiv.style('display', 'block')
+		makeDtRadios()
+		mayMakeOriginRadios()
+		makeGroupsetRadios()
+	}
+
+	// radio buttons for data type
+	function makeDtRadios() {
+		dtDiv.selectAll('*').remove()
+		dtDiv.append('div').style('font-weight', 'bold').text('Variant type')
+		const ds_dts = getDsDts(self.vocabApi.termdbConfig.queries)
+		if (!self.q.dt) self.q.dt = ds_dts[0]
+		make_radios({
+			holder: dtDiv,
+			options: ds_dts.map(dt => ({ label: dt2label[dt], value: dt, checked: dt == self.q.dt })),
+			callback: v => {
+				self.q.dt = v
+				mayMakeOriginRadios()
+				makeGroupsetRadios()
+			}
+		})
+	}
+
+	// radio buttons for variant origin
+	function mayMakeOriginRadios() {
+		const byOrigin = self.vocabApi.termdbConfig.assayAvailability?.byDt[self.q.dt]?.byOrigin
+		if (!byOrigin) {
+			originDiv.style('display', 'none')
+			return
+		}
+		if (!self.q.origin || !(self.q.origin in byOrigin)) self.q.origin = Object.keys(byOrigin)[0]
+		originDiv.style('display', 'block')
+		originDiv.selectAll('*').remove()
+		originDiv.append('div').style('font-weight', 'bold').text('Variant origin')
+		make_radios({
+			holder: originDiv,
+			options: Object.keys(byOrigin).map(origin => ({
+				label: byOrigin[origin].label,
+				value: origin,
+				checked: origin == self.q.origin
+			})),
+			callback: v => {
+				self.q.origin = v
+			}
+		})
+	}
+
+	// radio buttons for variant grouping
+	function makeGroupsetRadios() {
+		// specify the indices of the predefined groupsets that are
+		// relevant to the dt, for example the 'Protein-changing vs. rest'
+		// groupset is relevant to SNV/indel and SV mutations but not
+		// to CNV mutations
+		const groupset_idxs =
+			self.q.dt == dtsnvindel || self.q.dt == dtsv
+				? [0, 1, 2]
+				: self.q.dt == dtcnv
+				? [0]
+				: self.q.dt == dtfusionrna
+				? [0, 1]
+				: null
+		// render the relevant predefined groupsets as radio buttons
+		if (
+			!self.q.groupsetting.predefined_groupset_idx ||
+			!groupset_idxs.includes(self.q.groupsetting.predefined_groupset_idx)
+		)
+			self.q.groupsetting.predefined_groupset_idx = groupset_idxs[0]
+		groupsetDiv.selectAll('*').remove()
+		groupsetDiv.append('div').style('font-weight', 'bold').text('Variant grouping')
+		make_radios({
+			holder: groupsetDiv,
+			options: groupset_idxs.map(i => {
+				const groupset = self.term.groupsetting.lst[i]
+				return { label: groupset.name, value: i, checked: i == self.q.groupsetting.predefined_groupset_idx }
+			}),
+			callback: v => {
+				self.q.groupsetting.predefined_groupset_idx = v
+			}
+		})
+	}
+
+	// Apply button
+	div
+		.append('button')
+		.style('margin-top', '10px')
+		.text('Apply')
+		.on('click', () => {
+			self.runCallback()
+		})
+
+	/*
 	const applyBtn = div
 		.append('button')
 		.property('disabled', true)
@@ -291,5 +377,18 @@ function makeEditMenu(self: GeneVariantTermSettingInstance, _div: any) {
 						.style('cursor', 'pointer')
 						.text(d.label)
 				})
-		})
+		})*/
+}
+
+// get dts specified in dataset
+function getDsDts(ds_queries) {
+	const ds_dts = []
+	for (const query of Object.keys(ds_queries)) {
+		if (query == 'snvindel') ds_dts.push(dtsnvindel)
+		else if (query == 'cnv') ds_dts.push(dtcnv)
+		else if (query == 'svfusion') ds_dts.push(dtfusionrna)
+		else if (query == 'sv') ds_dts.push(dtsv) // TODO: is this correct?
+		else continue
+	}
+	return ds_dts
 }
