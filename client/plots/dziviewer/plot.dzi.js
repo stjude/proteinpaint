ad660aef598863d5f0de18f4086443cc241f30fa
Aleksandar Acic <aacic@stjude.org>
2024-06-05 16:09:06 -0500

    Refactoring

--- client/plots/dziviewer/plot.dzi.js
+++ client/plots/dziviewer/plot.dzi.js
@@ -1,3 +1,23 @@
+/* A plot for the displaying Deep Zoom Images.
+"DZI" or "Deep Zoom Image" is a file format developed by Microsoft for efficiently displaying large images by dividing them into smaller segments,
+allowing users to pan and zoom with high performance and minimal loading time.
+
+
+dslabel=str
+	as on vocab.dslabel
+
+holder
+	holder div
+
+genomeObj={}
+	client side genome obj
+
+sample_id
+
+dzimages = Array<str>
+	list of paths to DZI files
+
+*/
 export default async function (dslabel, holder, genomeObj, sample_id, dzimages) {
 	const loadingDiv = holder.append('div').style('margin', '20px').text('Loading...')
 
