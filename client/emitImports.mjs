5930ac8b212f763dc4b1090ba74da5f657172746
siosonel <siosonel@gmail.com>
2024-06-13 17:06:25 -0500

    rename hic integration test; do not create an open initial test if there are not matching specs

--- client/emitImports.mjs
+++ client/emitImports.mjs
@@ -26,7 +26,7 @@ const specs = glob.sync(`./**/test/${namePattern}`, { cwd: __dirname })
 console.log(`import { matchSpecs, specsMatched } from './matchSpecs.js'`)
 console.log(`import tape from 'tape'`)
 
-console.log(`
+const initialTest = `
 // keep an initial test open until all spec modules have been loaded,
 // to prevent an early-loaded and very fast test from closing the
 // tape harness and ignoring late-loaded tests 
@@ -42,17 +42,20 @@ tape('loading of all import(spec)', test => {
 		test.end()
 	}
 })
-`)
+`
+// only keep an initial test open if there are matching specs to test
+if (specs.length) console.log(initialTest)
+
 console.log(`const promises = []`)
 // do not await on the dynamic import(), tape seems to collects all tests
 // within a given time so that they can all run in sequence, otherwise
 // if awaited, only the first spec file will run (tape seems to consider 
 // all tests done at that point)
 specs.forEach(f => console.log(`if (matchSpecs('${f}')) promises.push(import('../${f}'))`))
+
 console.log(`// this resolves after all test modules are loaded, 
 // but likely before all test code are fully evaluated and completed 
-Promise.all(promises).then(()=>assertAllTestLoaded())
-console.log('*** done ***')
+Promise.all(promises).then(()=>assertAllTestLoaded?.()).catch(e => {throw e})
 `)
 
 console.log(`export function getSpecs() { return specsMatched }`)
