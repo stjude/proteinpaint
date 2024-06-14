36a00862d48909178905fd76a36841531fcc5ff6
siosonel <siosonel@gmail.com>
2024-06-14 15:11:23 -0500

    emit a tape bundle for dev tests in-browser

--- client/dev.sh
+++ client/dev.sh
@@ -4,6 +4,10 @@ set -euxo pipefail
 
 rm -rf ./dist
 
+# TODO: 
+# use a good esbuild node polyfill plugin to avoid having to use webpack,
+# to bundle and supplies tape lib with missing node libs  
+npx webpack --config=./webpack.tape.config.mjs
 node emitImports.mjs > ./test/internals-dev.js
 ln -sf $(pwd)/dist ../public/bin/
 ENV=dev node esbuild.config.mjs
