815b1f709c1586eafde6b994365f84d56a721b73
robinpaul85 <robinpaul85@gmail.com>
2024-06-14 15:02:15 -0500

    Created table for gsea

--- server/shared/types/routes/genesetEnrichment.ts
+++ server/shared/types/routes/genesetEnrichment.ts
@@ -5,8 +5,16 @@ export type genesetEnrichmentRequest = {
 	geneSetGroup: string // Type of GO to be queried e.g MF, CC, BP
 }
 
+type pathway_attributes = {
+	es: number
+	nes: number
+	geneset_size: number
+	leading_edge: string
+	pvalue: number
+	sidak: number
+	fdr: number
+}
+
 export type genesetEnrichmentResponse = {
-	pathway_name: string // Name of pathway
-	p_value_original: number // Original p-value
-	p_value_adjusted: number // Adjusted p-value
+	pathway: pathway_attributes[]
 }
