7069ecbe62a9baaf83b92d759a1f3f13ddcab348
siosonel <siosonel@gmail.com>
2024-06-24 15:31:08 -0500

    always regenerate the public/bin/proteinpaint.js symlink in dev

--- client/dev.sh
+++ client/dev.sh
@@ -3,7 +3,12 @@
 set -euxo pipefail
 
 rm -rf ./dist
-
 node emitImports.mjs > ./test/internals-dev.js
+
+if [[ ! -d ../public/bin ]]; then
+	mkdir ../public/bin
+fi
 ln -sf $(pwd)/dist ../public/bin/
+ln -sf $(pwd)/../front/src/app.js ../public/bin/proteinpaint.js
+
 ENV=dev node esbuild.config.mjs
