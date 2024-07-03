21a3770bf211582f2b75240d15e7d6e2b0ed81be
Xin Zhou <xzhou82@gmail.com>
2024-07-03 10:55:37 -0500

    expose DEgenes to termdbconfig

--- server/routes/termdb.config.ts
+++ server/routes/termdb.config.ts
@@ -231,13 +231,6 @@ function addNonDictionaryQueries(c, ds: Mds3WithCohort, genome) {
 	}
 	if (q.singleCell) {
 		// samples and data are required properties
-		const plots: any[] =
-			'plots' in q.singleCell.data
-				? (q.singleCell.data as SingleCellDataNative).plots.map(p => ({
-						name: p.name,
-						colorColumn: p.colorColumn.name
-				  }))
-				: []
 		q2.singleCell = {
 			samples: {
 				firstColumnName: q.singleCell.samples.firstColumnName,
@@ -247,12 +240,17 @@ function addNonDictionaryQueries(c, ds: Mds3WithCohort, genome) {
 			data: {
 				sameLegend: q.singleCell.data.sameLegend,
 				refName: q.singleCell.data.refName,
-				plots
+				plots: q.singleCell.data.plots.map(p => {
+					return { name: p.name, colorColumn: p.colorColumn.name }
+				})
 			}
 		}
+		// optional data types
 		if (q.singleCell.geneExpression) {
-			// optional data type
 			q2.singleCell.geneExpression = {}
+		}
+		if (q.singleCell.DEgenes) {
+			q2.singleCell.DEgenes = { columnName: q.singleCell.DEgenes.columnName }
 		}
 	}
 	if (q.images) {
