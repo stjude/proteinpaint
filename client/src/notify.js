f07b1da62d4bb40c1cd4bd180ea1726be187168d
siosonel <siosonel@gmail.com>
2024-07-10 10:53:46 -0500

    track messages as files, instead of using http post, to trigger server-sent events

--- client/src/notify.js
+++ client/src/notify.js
@@ -1,43 +1,46 @@
 import { select, selectAll } from 'd3-selection'
 
-const src = new EventSource('/notifications')
+// server-sent events
+const sse = new EventSource('/sse')
+const notifyDiv = select('body')
+	.append('div')
+	.style('position', 'fixed')
+	.style('top', `16px`)
+	.style('right', '10px')
+	.style('font-size', '1.2em')
+	.style('background-color', 'rgba(250, 250, 250, 0.75)')
 
 let lastTimeStamp = 0
-src.onmessage = event => {
-	const chunks = event.data.split('\n\n')
-	const data = chunks.map(JSON.parse)
-	console.log(data)
+sse.onmessage = event => {
+	const data = JSON.parse(event.data) //; console.log(data)
 	const cls = 'sjpp-notification-banner'
-	const divs = select('body')
-		.selectAll(`.${cls}`)
-		.data(data, d => d.key)
+	const divs = notifyDiv.selectAll(`:scope>div`).data(data, d => d.key)
 
 	divs.exit().remove()
 	divs
-		.style('color', d => (d.status == 'ok' ? 'green' : 'red'))
+		.style('color', d => d.color || (d.status == 'ok' ? 'green' : 'red'))
+		.style('border', d => `1px solid ${d.color || '#000'}`)
 		.html(d => `${d.key}: ${d.message}`)
 		.each(function (d) {
-			console.log(18, d)
-			if (d.status == 'ok')
-				setTimeout(() => select(this).transition().duration(2500).style('opacity', 0).remove(), 5000)
+			if (d.duration)
+				setTimeout(() => select(this).transition().duration(d.duration).style('opacity', 0).remove(), d.duration)
 		})
 	divs
 		.enter()
 		.append('div')
 		.attr('class', cls)
-		.style('position', 'fixed')
-		.style('top', (d, i) => `${(i + 1) * 16}px`)
-		.style('right', '10px')
-		.style('color', d => (d.status == 'ok' ? 'green' : 'red'))
+		.style('margin', '5px')
+		.style('padding', '5px')
+		.style('border', d => `1px solid ${d.color || '#000'}`)
+		.style('color', d => d.color || (d.status == 'ok' ? 'green' : 'red'))
 		.html(d => `${d.key}: ${d.message}`)
 		.on('click', function (d) {
-			select(this).remove()
+			//select(this).remove()
 		})
 		.each(function (d) {
-			console.log(32, d)
-			if (d.status == 'ok')
-				setTimeout(() => select(this).transition().duration(2500).style('opacity', 0).remove(), 5000)
+			if (d.duration)
+				setTimeout(() => select(this).transition().duration(d.duration).style('opacity', 0).remove(), d.duration)
 		})
 }
 
-src.onerror = err => console.log(err)
+sse.onerror = err => console.log(err)
