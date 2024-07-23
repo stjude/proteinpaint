068bc5da9ed0ed0ccf8b3243c262542948327f65
robinpaul85 <robinpaul85@gmail.com>
2024-07-23 15:20:52 -0500

    Added typedoc annotation to geneset overrepresentation

--- server/shared/types/routes/genesetOverrepresentation.ts
+++ server/shared/types/routes/genesetOverrepresentation.ts
@@ -1,12 +1,19 @@
 export type genesetOverrepresentationRequest = {
-	sample_genes: string // Sample genes to be queried
-	background_genes: string // Background genes against which the sample genes will be queried
-	genome: string // Genome build
-	geneSetGroup: string // Type of GO to be queried e.g MF, CC, BP
+	/**  Sample genes to be queried */
+	sample_genes: string
+	/** Background genes against which the sample genes will be queried */
+	background_genes: string
+	/** Genome build */
+	genome: string
+	/** Type of GO to be queried e.g MF, CC, BP */
+	geneSetGroup: string
 }
 
 export type genesetOverrepresentationResponse = {
-	pathway_name: string // Name of pathway
-	p_value_original: number // Original p-value
-	p_value_adjusted: number // Adjusted p-value
+	/** Name of pathway */
+	pathway_name: string
+	/** Original p-value */
+	p_value_original: number
+	/** Adjusted p-value */
+	p_value_adjusted: number
 }
