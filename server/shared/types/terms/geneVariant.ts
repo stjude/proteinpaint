e2b15e917dfed0785196ac55ecac8f91d66053fe
siosonel <siosonel@gmail.com>
2024-08-01 15:19:27 -0500

    type fixes to geneVariant fillTW

--- server/shared/types/terms/geneVariant.ts
+++ server/shared/types/terms/geneVariant.ts
@@ -13,26 +13,53 @@ export type GeneVariantQ = BaseQ & {
 
 type GeneVariantBaseTerm = BaseTerm & { groupsetting: GroupSettingTerm }
 
-type GeneVariantCoordTerm = GeneVariantBaseTerm & {
-	kind: 'coord'
+type Coord = {
 	chr: string
 	start: number
 	stop: number
 }
 
-export type GeneVariantGeneTerm = GeneVariantBaseTerm & {
-	kind: 'gene'
-	gene: string
-	chr?: string
-	start?: number
-	stop?: number
-}
+type GeneVariantCoordTerm = GeneVariantBaseTerm &
+	Coord & {
+		kind: 'coord'
+	}
+
+export type GeneVariantGeneTerm =
+	| (GeneVariantBaseTerm & {
+			kind: 'gene'
+			gene: string
+	  })
+	| (GeneVariantBaseTerm & {
+			kind: 'gene'
+			gene: string
+	  } & Coord)
 
 export type GeneVariantTerm = GeneVariantGeneTerm | GeneVariantCoordTerm
+
+export type LooseGeneTerm = GeneVariantBaseTerm & {
+	kind?: 'gene'
+	gene?: string
+	name: string
+}
+
+export type LooseCoordTerm = GeneVariantBaseTerm & {
+	kind?: 'coord'
+	name?: string
+	chr: string
+	start: number
+	stop: number
+}
+
+export type LooseGeneVariantTerm = LooseGeneTerm | LooseCoordTerm
 
 export type GeneVariantTW = BaseTW & {
 	q: GeneVariantQ
 	term: GeneVariantTerm
+}
+
+export type LooseGeneVariantTW = BaseTW & {
+	q: Partial<GeneVariantQ>
+	term: LooseGeneVariantTerm
 }
 
 export type GeneVariantTermSettingInstance = TermSettingInstance & {
@@ -40,4 +67,8 @@ export type GeneVariantTermSettingInstance = TermSettingInstance & {
 	term: GeneVariantTerm
 	category2samplecount: any
 	groupSettingInstance?: any
+}
+
+type ValuesQ = {
+	type?: 'values'
 }
