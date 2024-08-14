db748a95360fba77c39c6c79756d2a321f317575
PPTeam CI <PPTeam@STJUDE.ORG>
2024-08-14 15:29:41 -0500

    fix: Fix wsi request type issue

--- server/shared/types/routes/samplewsimages.ts
+++ server/shared/types/routes/samplewsimages.ts
@@ -2,8 +2,9 @@ export type GetSampleWSImagesRequest = {
 	genome: string
 	dslabel: string
 	sample_id: string
+	wsimage: string
 }
 
 export type GetSampleWSImagesResponse = {
-	sampleWSImages: string
+	sampleWSImages: string[]
 }
