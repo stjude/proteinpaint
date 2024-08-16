2e63fbc3bfd4fe220d5c629d4952afdbe484e4c3
robinpaul85 <robinpaul85@gmail.com>
2024-08-16 16:55:27 -0500

    Passed gene_db to rust script

--- server/routes/genesetOverrepresentation.ts
+++ server/routes/genesetOverrepresentation.ts
@@ -1,9 +1,12 @@
 //import fs from 'fs'
 import {
 	genesetOverrepresentationRequest,
-	genesetOverrepresentationResponse
+	genesetOverrepresentationResponse,
+	gene_overrepresentation_input
 } from '../shared/types/routes/genesetOverrepresentation.ts'
 import { run_rust } from '@sjcrh/proteinpaint-rust'
+import serverconfig from '#src/serverconfig.js'
+import path from 'path'
 
 export const api = {
 	endpoint: 'genesetOverrepresentation',
@@ -24,6 +27,9 @@ export const api = {
 
 function init({ genomes }) {
 	return async (req: any, res: any): Promise<void> => {
+		//console.log("gene_db:",path.join(serverconfig.tpmasterdir,genomes[req.query.genome].genedb.dbfile))
+		//console.log("req.query.genome:",req.query.genome)
+		//console.log("msigdb:",genomes[req.query.genome].termdbs.msigdb.cohort.db.connection.name)
 		try {
 			//console.log("req.query:",req.query)
 			const results = await run_genesetOverrepresentation_analysis(
@@ -43,9 +49,13 @@ async function run_genesetOverrepresentation_analysis(q: genesetOverrepresentati
 	if (!genomes[q.genome].termdbs) throw 'termdb database is not available for ' + q.genome
 	const gene_overrepresentation_input = {
 		sample_genes: q.sample_genes,
-		background_genes: q.background_genes,
-		db: genomes[q.genome].termdbs.msigdb.cohort.db.connection.name,
-		gene_set_group: q.geneSetGroup
+		msigdb: genomes[q.genome].termdbs.msigdb.cohort.db.connection.name,
+		gene_set_group: q.geneSetGroup,
+		gene_db: path.join(serverconfig.tpmasterdir, genomes[q.genome].genedb.dbfile)
+	} as gene_overrepresentation_input
+
+	if (q.background_genes) {
+		gene_overrepresentation_input.background_genes = q.background_genes
 	}
 
 	//fs.writeFile('test.txt', JSON.stringify(gene_overrepresentation_input), function (err) {
