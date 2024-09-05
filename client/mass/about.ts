d42e87f98a38939492443386bc0fddeabd6f69c1
Colleen Reilly <creilly@stjude.org>
2024-09-05 17:50:15 -0500

    Only add about component if data present and ensure plots only launch in the charts tab

--- client/mass/about.ts
+++ client/mass/about.ts
@@ -5,17 +5,25 @@ import { MassApp } from './types/mass'
 type MassAboutOpts = {
 	app: MassApp
 	holder: Elem
+	obj: any
 }
 
 class MassAbout {
 	type: string
 	app: MassApp
 	holder: Elem
+	obj: any
 
 	constructor(opts: MassAboutOpts) {
 		this.type = 'about'
 		this.app = opts.app
 		this.holder = opts.holder
+		this.obj = opts.obj
+	}
+
+	main() {
+		const messageDiv = this.holder
+		messageDiv.html(this.obj.html)
 	}
 }
 
