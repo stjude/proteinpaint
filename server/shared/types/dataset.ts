e6e2638e55161b0fac4dba17854d6ec621e1622f
Robin Paul <rpaul1@stjude.org>
2024-09-10 11:05:42 -0500

    Fixed JSON error in rust code

--- server/shared/types/dataset.ts
+++ server/shared/types/dataset.ts
@@ -419,7 +419,7 @@ export type GeneExpressionQuery = GeneExpressionQueryGdc | GeneExpressionQueryNa
 
 export type SingleCellGeneExpressionNative = {
 	src: 'native'
-	/** path to R rds files, each is a gene-by-cell matrix for a sample, with ".rdx" suffix. missing files are detected and handled */
+	/** path to R rds or HDF5 files, each is a gene-by-cell matrix for a sample, with ".rdx" suffix. missing files are detected and handled */
 	folder: string
 	/** HDF5 or RDS file, will deprecate RDS file later */
 	storage_type: 'HDF5' | 'RDS'
