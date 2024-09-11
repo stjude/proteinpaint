3ec6a4a73ed8e028cfdf9c84cf94cf82ed8e7c61
Airen Zaldivar <airenzp@gmail.com>
2024-09-11 16:25:05 -0500

    check if there is ancestry data, not if the table is present

--- server/src/termdb.matrix.js
+++ server/src/termdb.matrix.js
@@ -461,7 +461,7 @@ export async function getAnnotationRows(q, termWrappers, filter, CTEs, values, t
 			const tw = termWrappers[i]
 			const sampleType = getSampleType(tw.term, q.ds)
 			let query
-			if (onlyChildren && parentType == sampleType && q.ds.cohort.db.tables.has('sample_ancestry'))
+			if (onlyChildren && parentType == sampleType && q.ds.cohort.termdb.hasAncenstry)
 				query = ` select sa.sample_id as sample, key, value, ? as term_id
 				 from sample_ancestry sa join ${t.tablename} on sa.ancestor_id = sample
 				${filter ? ` WHERE sample_id IN ${filter.CTEname} ` : ''}`
