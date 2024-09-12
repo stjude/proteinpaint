970f0ee7e7fb811f5d2958a607320525e5ab7cb7
Colleen Reilly <creilly@stjude.org>
2024-09-12 13:04:40 -0500

    WIP: Enable more shapes

--- client/mds3/skewer.render.shapes.ts
+++ client/mds3/skewer.render.shapes.ts
@@ -5,7 +5,7 @@ export function renderSkewerShapes(tk: any, skewer: any, shapeG: Elem, modefold:
 		.append('path')
 		.attr('d', d => skewer.shape[1].calculatePath(getPathDimensions(skewer.shape[0], d)))
 		.attr('fill', skewer.shape[1].isFilled ? d => tk.color4disc(d.mlst[0]) : 'white')
-		.attr('stroke', d => tk.color4disc(d.mlst[0]))
+		.attr('stroke', skewer.shape[1].isFilled ? 'white' : d => tk.color4disc(d.mlst[0]))
 }
 
 function getPathDimensions(key: string, d: any) {
@@ -13,6 +13,12 @@ function getPathDimensions(key: string, d: any) {
 	switch (key) {
 		case 'emptyVerticalRectangle':
 			return { width: d.radius * 1.4, height: d.radius * 2 }
+		case 'emptyTriangle':
+			return { width: d.radius * 1.75, height: d.radius * 1.75 }
+		case 'filledSquare':
+			return { width: d.radius * 1.7, height: d.radius * 1.7 }
+		case 'emptySquare':
+			return { width: d.radius * 1.7, height: d.radius * 1.7 }
 		default:
 			'Invalid shape key'
 	}
