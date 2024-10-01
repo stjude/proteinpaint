de2867f930ec2c4ff257c9b03fae1556b754d1b9
congyu-lu <clu@stjude.com>
2024-10-01 16:09:53 -0500

    fix SingleCellDataGdc_typeError

--- server/shared/types/dataset.ts
+++ server/shared/types/dataset.ts
@@ -470,7 +470,7 @@ export type SingleCellDataGdc = {
 	src: 'gdcapi'
 	sameLegend: boolean
 	get?: (q: any) => any
-	refName: string
+	refName?: string
 	plots: GDCSingleCellPlot[]
 	width?: number
 	height?: number
