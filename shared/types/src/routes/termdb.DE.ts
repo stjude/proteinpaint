efd86858728abba7aca080772f1f8234f00ba4b2
robinpaul85 <robinpaul85@gmail.com>
2024-10-15 15:54:52 -0500

    Tentative implementation of using HDF5 in DE analysis

--- shared/types/src/routes/termdb.DE.ts
+++ shared/types/src/routes/termdb.DE.ts
@@ -9,8 +9,27 @@ export type DERequest = {
 	min_count: number
 	/** Minimum total read count required for each sample */
 	min_total_count: number
+	/** Storage_type for storing data. Will deprecate text files */
+	storage_type?: 'text' | 'HDF5'
 	/** Method of DE used wilcoxon/edgeR */
 	method?: string
+}
+
+export type ExpressionInput = {
+	/** Case samples separated by , */
+	case: string
+	/** Control samples separated by , */
+	control: string
+	/** data_type instructs rust to carry out differential gene expression analysis */
+	data_type: 'do_DE'
+	/** File containing raw gene counts for DE analysis */
+	input_file: string
+	/** Relative cpm cutoff for filtering a gene compared to all samples and genes in dataset */
+	min_count: number
+	/** Minimum total read count required for each sample */
+	min_total_count: number
+	/** Type of storage file: HDF5 or text.Text will be deprecated in the future */
+	storage_type?: 'HDF5' | 'text'
 }
 
 export type DEResponse = {
