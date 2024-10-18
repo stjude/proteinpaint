b68ee0c598e01b43ccc56b608432a693ef0d09ad
Colleen Reilly <creilly@stjude.org>
2024-10-18 13:21:59 -0500

    WIP: Add y scale above box plot

--- client/plots/boxplot/Model.ts
+++ client/plots/boxplot/Model.ts
@@ -1,3 +1,44 @@
+type Term = { [index: string]: any }
+type DataRequestOpts = { term: Term; term0?: Term; term2?: Term; filter: any; ssid?: string | number }
+
 export class Model {
-	//data
+	config: any
+	state: any
+	app: any
+	constructor(config, state, app) {
+		this.config = config
+		this.state = state
+		this.app = app
+	}
+
+	async getData() {
+		const reqOpts = this.getDataRequestOpts(this.config, this.state)
+		const result = await this.app.vocabApi.getNestedChartSeriesData(reqOpts)
+		const data = result.data
+		this.app.vocabApi.syncTermData(this.config, data)
+		return data
+	}
+
+	getDataRequestOpts(config, state) {
+		const opts: DataRequestOpts = { term: config.term, filter: state.termfilter.filter }
+		if (config.term2) opts.term2 = config.term2
+		if (config.term0) opts.term0 = config.term0
+		if (state.ssid) opts.ssid = state.ssid
+		return opts
+	}
+
+	// processData(data) {
+	// 	let binmax = 0
+	// 	const lst = data.refs.cols.map(t1 => {
+	// 		const d = data.charts[0].serieses.find(d => d.seriesId == t1)
+	// 		if (!d) return null
+	// 		if (binmax < d.max) binmax = d.max
+	// 		return {
+	// 			label: t1,
+	// 			value: d.total,
+	// 			boxplot: d.boxplot
+	// 		}
+	// 	})
+	// 	return [lst, binmax]
+	// }
 }
