8046cbe38a73a70d6287459ac98d64191d4b3173
Colleen Reilly <creilly@stjude.org>
2024-10-22 11:11:14 -0500

    Additional types; rm outdated code and reorg

--- client/plots/boxplot/Model.ts
+++ client/plots/boxplot/Model.ts
@@ -1,10 +1,12 @@
+import type { MassAppApi } from '../../mass/types/mass'
+
 type Term = { [index: string]: any }
 type DataRequestOpts = { term: Term; term0?: Term; term2?: Term; filter: any; ssid?: string | number }
 
 export class Model {
 	config: any
 	state: any
-	app: any
+	app: MassAppApi
 	constructor(config, state, app) {
 		this.config = config
 		this.state = state
@@ -12,11 +14,6 @@ export class Model {
 	}
 
 	async getData() {
-		const reqOpts = this.getDataRequestOpts(this.config, this.state)
-		// const result = await this.app.vocabApi.getNestedChartSeriesData(reqOpts)
-		// const data2 = result.data
-		// this.app.vocabApi.syncTermData(this.config, data2)
-
 		const boxPlotDataArgs: any = {
 			tw: this.config.term,
 			filter: this.state.termfilter.filter
@@ -24,8 +21,6 @@ export class Model {
 		if (this.config.term2) boxPlotDataArgs.divideTw = this.config.term2
 
 		const data = await this.app.vocabApi.getBoxPlotData(boxPlotDataArgs)
-		this.app.vocabApi.syncTermData(this.config, data)
-		// console.log(25, 'getBoxPlotData', data)
 		return data
 	}
 
@@ -36,19 +31,4 @@ export class Model {
 		if (state.ssid) opts.ssid = state.ssid
 		return opts
 	}
-
-	// processData(data) {
-	// 	let binmax = 0
-	// 	const lst = data.refs.cols.map(t1 => {
-	// 		const d = data.charts[0].serieses.find(d => d.seriesId == t1)
-	// 		if (!d) return null
-	// 		if (binmax < d.max) binmax = d.max
-	// 		return {
-	// 			label: t1,
-	// 			value: d.total,
-	// 			boxplot: d.boxplot
-	// 		}
-	// 	})
-	// 	return [lst, binmax]
-	// }
 }
