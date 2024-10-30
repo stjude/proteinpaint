1a2b04cbb6aa8d1442aebc59c45ecc967cea868f
Airen Zaldivar <airenzp@gmail.com>
2024-10-30 16:32:45 -0500

    First draft splitting the image handling into 3 categories: L, F and T

--- client/plots/brainImaging.js
+++ client/plots/brainImaging.js
@@ -24,7 +24,14 @@ class BrainImaging {
 			.style('vertical-align', 'top')
 			.style('padding', '10px')
 		const contentHolder = rightDiv.append('div').style('vertical-align', 'top')
-		this.dom = { headerHolder, contentHolder }
+		const table = contentHolder.append('table')
+		const headerTr = table.append('tr')
+		const contentTr = table.append('tr')
+		const tdL = contentTr.append('td')
+		const tdF = contentTr.append('td')
+		const tdT = contentTr.append('td')
+
+		this.dom = { headerHolder, contentHolder, headerTr, tdL, tdF, tdT, imagesF: [], imagesL: [], imagesT: [] }
 		this.addSliders(state.config.settings.brainImaging)
 
 		const configInputsOptions = this.getConfigInputsOptions()
@@ -41,81 +48,81 @@ class BrainImaging {
 	}
 
 	addSliders(settings) {
-		const headerHolder = this.dom.headerHolder
-		headerHolder.append('label').attr('for', 'saggital').text('Sagittal:')
-		this.dom.saggitalSlider = headerHolder
+		const tr = this.dom.headerTr
+		let td = tr.append('td')
+		td.append('label').attr('for', 'saggital').text('Sagittal:')
+		this.dom.saggitalSlider = td
 			.append('input')
 			.attr('id', 'saggital')
 			.attr('type', 'range')
 			.attr('min', 0)
 			.attr('max', 192)
 			.attr('value', settings.brainImageL)
-			.style('width', '200px')
 			.on('change', e => {
-				const settings = { brainImageL: e.target.value }
-				this.editBrainImage(settings)
+				this.editBrainImage('brainImageL', e.target.value)
 			})
-		this.dom.saggitalInput = headerHolder
+		this.dom.saggitalInput = td
 			.append('input')
 			.attr('type', 'number')
 			.attr('min', 0)
 			.attr('max', 192)
 			.attr('value', settings.brainImageL)
 			.on('change', e => {
-				const settings = { brainImageL: e.target.value }
-				this.editBrainImage(settings)
+				this.editBrainImage('brainImageL', e.target.value)
 			})
 			.style('vertical-align', 'top')
 
-		headerHolder.append('label').attr('for', 'coronal').text('Coronal:').style('margin-left', '30px')
-		this.dom.coronalSlider = headerHolder
+		td = tr.append('td')
+
+		td.append('label').attr('for', 'coronal').text('Coronal:')
+		this.dom.coronalSlider = td
 			.append('input')
 			.attr('type', 'range')
 			.attr('min', 0)
 			.attr('max', 228)
 			.attr('value', settings.brainImageF)
-			.style('width', '200px')
 			.on('change', e => {
-				const settings = { brainImageF: e.target.value }
-				this.editBrainImage(settings)
+				this.editBrainImage('brainImageF', e.target.value)
 			})
-		this.dom.coronalInput = headerHolder
+		this.dom.coronalInput = td
 			.append('input')
 			.attr('type', 'number')
 			.attr('min', 0)
 			.attr('max', 192)
 			.attr('value', settings.brainImageF)
 			.on('change', e => {
-				const settings = { brainImageF: e.target.value }
-				this.editBrainImage(settings)
+				this.editBrainImage('brainImageF', e.target.value)
 			})
 			.style('vertical-align', 'top')
-		headerHolder.append('label').attr('for', 'axial').text('Axial:').style('margin-left', '30px')
-		this.dom.axialSlider = headerHolder
+
+		td = tr.append('td')
+
+		td.append('label').attr('for', 'axial').text('Axial:')
+		this.dom.axialSlider = td
 			.append('input')
 			.attr('id', 'axial')
 			.attr('type', 'range')
 			.attr('min', 0)
 			.attr('max', 192)
 			.attr('value', settings.brainImageT)
-			.style('width', '200px')
 			.on('change', e => {
-				const settings = { brainImageT: e.target.value }
-				this.editBrainImage(settings)
+				this.editBrainImage('brainImageT', e.target.value)
 			})
-		this.dom.axialInput = headerHolder
+		this.dom.axialInput = td
 			.append('input')
 			.attr('type', 'number')
 			.attr('min', 0)
 			.attr('max', 192)
 			.attr('value', settings.brainImageT)
 			.on('change', e => {
-				const settings = { brainImageT: e.target.value }
-				this.editBrainImage(settings)
+				this.editBrainImage('brainImageT', e.target.value)
 			})
 	}
 
-	editBrainImage(settings) {
+	editBrainImage(key, value) {
+		if (!value) return
+		const settings = { [key]: Number(value) }
+
 		this.app.dispatch({ type: 'plot_edit', id: this.id, config: { settings: { brainImaging: settings } } })
 	}
 
@@ -187,56 +194,95 @@ class BrainImaging {
 		return mandatoryConfigInputOptions
 	}
 
-	async main() {
-		const settings = this.state.config.settings.brainImaging
-
-		//settings may be edited by the slider or the input, so we update the sliders and inputs to reflect the current settings
-		this.dom.saggitalSlider.node().value = settings.brainImageL
-		this.dom.saggitalInput.node().value = settings.brainImageL
-		this.dom.coronalSlider.node().value = settings.brainImageF
-		this.dom.coronalInput.node().value = settings.brainImageF
-		this.dom.axialSlider.node().value = settings.brainImageT
-		this.dom.axialInput.node().value = settings.brainImageT
-
-		const body = {
-			genome: this.state.genome,
-			dslabel: this.state.dslabel,
-			refKey: this.state.config.queryKey,
-			l: settings.brainImageL,
-			f: settings.brainImageF,
-			t: settings.brainImageT,
-			selectedSampleFileNames: this.state.config.selectedSampleFileNames
-		}
-		const data = await dofetch3('brainImaging', { body })
-		if (data.error) throw data.error
-
-		const dataUrl = await data.brainImage
-		this.dataUrls = [dataUrl] //later on update with urls obtained after divide by is added, or for testing purposes use [dataUrl, dataUrl]
-		this.images = []
-		this.dom.contentHolder.selectAll('*').remove()
-		const width = this.state.RefNIdata.width
-		const height = this.state.RefNIdata.height
-		for (const dataUrl of this.dataUrls) {
-			const image = this.dom.contentHolder
-				.append('div')
-				.style('display', 'block')
-				.style('vertical-align', 'top')
-				.append('img')
-				.style('border', '5px solid #aaa')
-				.attr('src', dataUrl)
-				.attr('width', width)
-				.attr('height', height)
-			this.images.push(image)
-		}
-	}
-
 	getState(appState) {
 		const config = appState.plots.find(p => p.id === this.id)
+		const updateL = config.settings.brainImaging.brainImageL != this.settings?.brainImageL
+		const updateF = config.settings.brainImaging.brainImageF != this.settings?.brainImageF
+		const updateT = config.settings.brainImaging.brainImageT != this.settings?.brainImageT
 		return {
 			config,
 			dslabel: appState.vocab.dslabel,
 			genome: appState.vocab.genome,
-			RefNIdata: appState.termdbConfig.queries.NIdata[config.queryKey]
+			RefNIdata: appState.termdbConfig.queries.NIdata[config.queryKey],
+			updateL,
+			updateF,
+			updateT
+		}
+	}
+
+	async main() {
+		this.settings = this.state.config.settings.brainImaging
+
+		//settings may be edited by the slider or the input, so we update the sliders and inputs to reflect the current settings
+		this.dom.saggitalSlider.node().value = this.settings.brainImageL
+		this.dom.saggitalInput.node().value = this.settings.brainImageL
+		this.dom.coronalSlider.node().value = this.settings.brainImageF
+		this.dom.coronalInput.node().value = this.settings.brainImageF
+		this.dom.axialSlider.node().value = this.settings.brainImageT
+		this.dom.axialInput.node().value = this.settings.brainImageT
+		if (this.state.updateL) {
+			const body = {
+				genome: this.state.genome,
+				dslabel: this.state.dslabel,
+				refKey: this.state.config.queryKey,
+				l: this.settings.brainImageL,
+				selectedSampleFileNames: this.state.config.selectedSampleFileNames
+			}
+			const data = await dofetch3('brainImaging', { body })
+			if (data.error) throw data.error
+
+			this.dataUrlL = [await data.brainImage]
+			if (this.dom.imagesL.length != this.dataUrlL.length) {
+				this.dom.tdL.selectAll('*').remove()
+				this.dom.imagesL = []
+				for (const url of this.dataUrlL) {
+					const img = this.dom.tdL.append('img').style('border', '5px solid #aaa').attr('src', url)
+					this.dom.imagesL.push(img)
+				}
+			} else for (const url of this.dataUrlL) this.dom.imagesL.forEach((img, i) => img.attr('src', url))
+		}
+
+		if (this.state.updateF) {
+			const body = {
+				genome: this.state.genome,
+				dslabel: this.state.dslabel,
+				refKey: this.state.config.queryKey,
+				f: this.settings.brainImageF,
+				selectedSampleFileNames: this.state.config.selectedSampleFileNames
+			}
+			const data = await dofetch3('brainImaging', { body })
+			if (data.error) throw data.error
+
+			this.dataUrlF = [await data.brainImage]
+			if (this.dom.imagesF.length != this.dataUrlF.length) {
+				this.dom.tdF.selectAll('*').remove()
+				this.dom.imagesF = []
+				for (const url of this.dataUrlF) {
+					const img = this.dom.tdF.append('img').style('border', '5px solid #aaa').attr('src', url)
+					this.dom.imagesF.push(img)
+				}
+			} else for (const url of this.dataUrlF) this.dom.imagesF.forEach((img, i) => img.attr('src', url))
+		}
+		if (this.state.updateT) {
+			const body = {
+				genome: this.state.genome,
+				dslabel: this.state.dslabel,
+				refKey: this.state.config.queryKey,
+				t: this.settings.brainImageT,
+				selectedSampleFileNames: this.state.config.selectedSampleFileNames
+			}
+			const data = await dofetch3('brainImaging', { body })
+			if (data.error) throw data.error
+
+			this.dataUrlT = [await data.brainImage]
+			if (this.dom.imagesT.length != this.dataUrlT.length) {
+				this.dom.tdT.selectAll('*').remove()
+				this.dom.imagesT = []
+				for (const url of this.dataUrlT) {
+					const img = this.dom.tdT.append('img').style('border', '5px solid #aaa').attr('src', url)
+					this.dom.imagesT.push(img)
+				}
+			} else for (const url of this.dataUrlT) this.dom.imagesT.forEach((img, i) => img.attr('src', url))
 		}
 	}
 }
