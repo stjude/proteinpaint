4860ad272005467b1e548886eb9a4d06ae4f98eb
siosonel <siosonel@gmail.com>
2024-11-06 18:23:58 -0600

    separate brain imaging from brain imaging samples types defs

--- shared/types/src/routes/brainImaging.ts
+++ shared/types/src/routes/brainImaging.ts
@@ -20,21 +20,6 @@ export type BrainImagingRequest = {
 	overlayTW?: CategoricalTW
 }
 
-export type BrainImagingSamplesRequest = {
-	/** a user-defined genome label in the serverconfig.json, hg38, hg19, mm10, etc */
-	genome: string
-	/** a user-defined dataset label in the serverconfig.json, such as ClinVar, SJLife, GDC, etc */
-	dslabel: string
-	/** a user-defined brain template label in dataset file, such as Ref1, Ref2 */
-	refKey: string
-}
-
-export type BrainImagingSamplesResponse = {
-	samples: BrainSample[]
-}
-
-export type BrainSample = { [key: string]: string }
-
 export type BrainImagingResponse = {
 	/** the brain imaging plot */
 	brainImage: string
