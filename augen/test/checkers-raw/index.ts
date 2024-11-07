44f3653b0bfa88f2a6b697e4dd33713253fc0d5f
siosonel <siosonel@gmail.com>
2024-11-06 21:44:51 -0600

    track typescript as a devDep in shared/types

--- augen/test/checkers-raw/index.ts
+++
@@ -1,5 +0,0 @@
-import { createValidate } from 'typia'
-import { AbcRequest, AbcResponse } from '../types/abc.ts'
-
-export const validAbcRequest = createValidate<AbcRequest>()
-export const validAbcResponse = createValidate<AbcResponse>()
