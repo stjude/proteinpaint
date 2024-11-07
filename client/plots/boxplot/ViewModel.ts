5a0894c2b84ea9c8feb195e1a0891f286b37a672
Colleen Reilly <creilly@stjude.org>
2024-11-07 14:46:32 -0600

    Clean up code in the view model

--- client/plots/boxplot/ViewModel.ts
+++ client/plots/boxplot/ViewModel.ts
@@ -9,9 +9,6 @@ import type { BoxplotSettings } from './Boxplot'
  */
 
 export class ViewModel {
-	config: any
-	data: any
-	settings: BoxplotSettings
 	/** Top padding for the svg */
 	readonly topPad = 20
 	/** Bottom padding for the svg */
@@ -23,14 +20,11 @@ export class ViewModel {
 	/** Increasing padding to space out the boxplots and determine position */
 	incrTopPad = 40
 	constructor(config: any, data: any, settings: BoxplotSettings) {
-		this.settings = settings
-		this.config = config
-
 		if (!data || !data.plots.length) return
 		const viewData: any = structuredClone(data)
 
-		const totalLabelWidth = viewData.maxLabelLgth * 4 + this.settings.labelPad + this.horizPad
-		const totalRowHeight = this.settings.rowHeight + this.settings.rowSpace
+		const totalLabelWidth = viewData.maxLabelLgth * 4 + settings.labelPad + this.horizPad
+		const totalRowHeight = settings.rowHeight + settings.rowSpace
 
 		/** Add more plot dimensions here
 		 * Eventually should calculate the difference between vertical and
@@ -38,14 +32,14 @@ export class ViewModel {
 		 */
 
 		viewData.plotDim = {
-			//Add 1 to the max so the upper line to boxplot isn't cutoff
-			domain: [viewData.absMin, viewData.absMax + 1],
-			incrTopPad: this.incrTopPad,
-			svgWidth: this.settings.boxplotWidth + totalLabelWidth + this.horizPad,
+			//Add 1 to the max is big enough so the upper line to boxplot isn't cutoff
+			domain: [viewData.absMin, viewData.absMax <= 1 ? viewData.absMax : viewData.absMax + 1],
+			svgWidth: settings.boxplotWidth + totalLabelWidth + this.horizPad,
 			svgHeight: viewData.plots.length * totalRowHeight + this.topPad + this.bottomPad + this.incrTopPad,
 			title: {
-				x: totalLabelWidth + this.settings.boxplotWidth / 2,
-				y: this.topPad + this.incrTopPad / 2
+				x: totalLabelWidth + settings.boxplotWidth / 2,
+				y: this.topPad + this.incrTopPad / 2,
+				text: config.term.q.mode == 'continuous' ? config.term.term.name : config.term2.term.name
 			},
 			yAxis: {
 				x: totalLabelWidth,
@@ -69,8 +63,6 @@ export class ViewModel {
 			plot.y = this.topPad + this.incrTopPad
 			this.incrTopPad += totalRowHeight
 		}
-		viewData.plotTitle =
-			this.config.term.q.mode == 'continuous' ? this.config.term.term.name : this.config.term2.term.name
 
 		return viewData
 	}
