e59511f3a5edd60a375290bff53157aaeed18b4a
siosonel <siosonel@gmail.com>
2024-11-07 13:03:30 -0600

    replace *.ts exports into *.js in the types package

--- shared/types/build.sh
+++ shared/types/build.sh
@@ -7,5 +7,7 @@ mkdir srcjs
 npx esbuild src/*.ts --platform=node --outdir=srcjs --format=esm
 npx esbuild src/**/*.ts --platform=node --outdir=srcjs --format=esm
 grep -lr ".ts\"" srcjs | xargs sed -i.bk -e "s|.ts\"|.js\"|g"
+cp package.json srcjs/
+sed -i.bk -e "s|.ts\"|.js\"|g" srcjs/package.json
 rm -rf srcjs/*.bk  
 rm -rf srcjs/**/*.bk
