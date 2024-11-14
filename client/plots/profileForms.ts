862bcb6f490119bf4b93b3cad2cd251eea3ed2cd
Airen Zaldivar <airenzp@gmail.com>
2024-11-14 13:05:14 -0600

    First working version

--- client/plots/profileForms.ts
+++ client/plots/profileForms.ts
@@ -1,13 +1,12 @@
 import { getCompInit, copyMerge } from '../rx/index.js'
 import { getProfilePlotConfig, profilePlot, getDefaultProfilePlotSettings } from './profilePlot.js'
 import { fillTwLst } from '#termsetting'
-import { axisLeft, axisTop } from 'd3-axis'
+import { axisTop } from 'd3-axis'
 import { scaleLinear as d3Linear } from 'd3-scale'
 import { loadFilterTerms } from './profilePlot.js'
-import { render } from '#src/block.mds2.vcf.plain'
 
 export class profileForms extends profilePlot {
-	id: string
+	id: any
 	type: string
 	opts: { [key: string]: any }
 	svg: any
@@ -20,7 +19,6 @@ export class profileForms extends profilePlot {
 	xAxisScale: any
 	shift: any
 	twLst: any
-
 	constructor(opts) {
 		super()
 		this.opts = opts
@@ -32,36 +30,24 @@ export class profileForms extends profilePlot {
 		const rightDiv = this.dom.rightDiv
 		const config = structuredClone(appState.plots.find(p => p.id === this.id))
 		const settings = config.settings.profileForms
-
-		const questionHolder = rightDiv.append('div')
-		questionHolder.append('span').text('Question: ').style('vertical-align', 'top')
-		const selectQuestion = questionHolder.append('select').property('multiple', true)
-
-		selectQuestion.on('change', () => {
-			const id = selectQuestion.node().value
-			this.app.dispatch({ type: 'plot_edit', id: this.id, config: { question: id } })
-		})
-		const shift = 700
-		const shiftTop = 30
+		rightDiv.append('h3').text(config.module)
+		const shift = 750
+		const shiftTop = 40
 		const svg = rightDiv
+			.style('padding', '10px')
 			.append('svg')
 			.attr('width', settings.svgw + shift + 10)
-			.attr('height', settings.svgh + shift + 10)
+			.attr('height', settings.svgh + shiftTop * 2)
 		const mainG = svg.append('g').attr('transform', `translate(${shift}, ${shiftTop})`)
-		const xAxisG = svg.append('g').attr('transform', `translate(${shift}, ${shiftTop})`)
-		const yAxisG = svg.append('g').attr('transform', `translate(${shift}, ${shiftTop})`)
-		this.xAxisScale = d3Linear().domain([0, 100]).range([0, settings.svgw])
-		const xAxisBottom = axisTop(this.xAxisScale)
-		const yAxisScale = d3Linear()
-			.domain([0, config.terms.length + 1])
-			.range([0, settings.svgh])
-		const yAxisLeft = axisLeft(yAxisScale).ticks(config.terms.length)
-		xAxisG.call(xAxisBottom)
-		yAxisG.call(yAxisLeft)
+		const gridG = svg.append('g').attr('transform', `translate(${shift}, ${shiftTop})`)
 
+		const xAxisG = svg.append('g').attr('transform', `translate(${shift}, ${shiftTop / 2})`)
+		this.xAxisScale = d3Linear().domain([0, 100]).range([0, settings.svgw])
+		xAxisG.call(axisTop(this.xAxisScale))
 		this.dom = copyMerge(this.dom, {
+			svg,
 			mainG,
-			selectQuestion
+			gridG
 		})
 		this.twLst = config.terms.concat(config.scTerms)
 	}
@@ -69,18 +55,7 @@ export class profileForms extends profilePlot {
 	async main() {
 		super.main()
 		await this.setControls()
-		this.renderHeaderOptions()
 		this.renderPlot()
-	}
-
-	renderHeaderOptions() {
-		this.dom.selectQuestion
-			.selectAll('option')
-			.data(this.config.terms)
-			.enter()
-			.append('option')
-			.text((d, i) => i + 1 + '. ' + d.term.name)
-			.attr('value', d => d.term.id)
 	}
 
 	getPercentsDict(tw, samples): { [key: string]: number } {
@@ -89,8 +64,7 @@ export class profileForms extends profilePlot {
 		//if defined in the settings a site is provided and the user can decide what to see, otherwise it is admin view and if the site was set sampleData is not null
 		const percentageDict = {}
 		for (const sample of samples) {
-			let termData = sample[tw.$id].value
-			termData = termData.slice(1, -1) //Removed string quoutes
+			const termData = sample[tw.$id].value
 			const percents: { [key: string]: number } = JSON.parse(termData)
 			for (const key in percents) {
 				const value = percents[key]
@@ -118,28 +92,34 @@ export class profileForms extends profilePlot {
 		this.dom.mainG.selectAll('*').remove()
 
 		const samples = this.settings.site ? [this.data.samples[this.settings.site]] : this.data.lst
-		let i = 1
-		const step = this.settings.svgh / (this.state.config.terms.length + 1)
-		const height = 40
-
+		const height = 30
+		let y = 0
 		for (const tw of this.state.config.terms) {
 			const percents: { [key: string]: number } = this.getPercentsDict(tw, samples)
 			const scTerm = this.state.config.scTerms.find(t => t.term.id.includes(tw.term.id))
 			const scPercents: { [key: string]: number } = this.getSCPercentsDict(scTerm, samples)
 			const scPercentKeys = Object.keys(scPercents).sort((a, b) => a.localeCompare(b))
-			const y = this.settings.svgh - i * step - height / 2
 			const scTotal = Object.values(scPercents).reduce((a, b) => a + b, 0)
 
-			this.renderRects(percents, y - height / 2, height, scTotal == 1 ? scPercentKeys : [])
-			if (scTotal > 1) this.renderRects(scPercents, y + height / 2, height, [])
-
-			i++
+			this.renderRects(percents, y, height, scTotal == 1 ? scPercentKeys : [])
+			if (scTotal > 1) {
+				y += height
+				this.renderRects(scPercents, y, height, [])
+			}
+			this.dom.mainG
+				.append('text')
+				.attr('x', -15)
+				.attr('y', scTotal > 1 ? y : y + height / 2)
+				.text(tw.term.name)
+				.attr('text-anchor', 'end')
+				.attr('font-size', '0.9em')
+			y += height + 20
 		}
+		this.renderLines(y - 20) //last padding not needed
 	}
 
 	renderRects(percents: { [key: string]: number }, y: number, height: number, scPercentKeys: string[]) {
 		const percentsOrdered = Object.keys(percents).sort((a, b) => -a.localeCompare(b))
-		console.log(percentsOrdered, y, height)
 		const total = Object.values(percents).reduce((a, b) => a + b, 0)
 		let x = 0
 		for (const key of percentsOrdered) {
@@ -164,12 +144,35 @@ export class profileForms extends profilePlot {
 			x += width
 		}
 	}
+
+	renderLines(y: number) {
+		this.dom.gridG.selectAll('*').remove()
+		const width = this.settings.svgw
+		const color = 'lightgray'
+		const opacity = 0.5
+		const bins = 4
+		const size = width / bins
+		let x
+		const gridG = this.dom.gridG
+		for (let i = 0; i <= bins; i++) {
+			x = i * size
+			gridG
+				.append('line')
+				.attr('x1', x)
+				.attr('x2', x)
+				.attr('y1', 0)
+				.attr('y2', y)
+				.style('stroke', color)
+				.style('stroke-opacity', opacity)
+				.style('stroke-dasharray', '5, 5')
+		}
+	}
 }
 
 export async function getPlotConfig(opts, app) {
 	let config = getProfilePlotConfig(app, opts)
-	if (config) config = config.find(t => t.module == opts.tw.term.name)
-	else config = {}
+	config = config.find(t => t.module == opts.tw.term.name)
+
 	config.settings = getDefaultProfileFormsSettings()
 	config = copyMerge(structuredClone(config), opts)
 	await fillTwLst(config.terms, app.vocabApi)
@@ -179,12 +182,16 @@ export async function getPlotConfig(opts, app) {
 }
 
 export function getDefaultProfileFormsSettings() {
-	return {
+	const settings = {
 		controls: {
 			isOpen: false
 		},
-		profileForms: copyMerge({ svgw: 400, svgh: 280 }, getDefaultProfilePlotSettings())
+		profileForms: { svgw: 300, svgh: 380 }
 	}
+	const profilePlotSettings = getDefaultProfilePlotSettings()
+	settings.profileForms = copyMerge(settings.profileForms, profilePlotSettings)
+
+	return settings
 }
 
 export const profileFormsInit = getCompInit(profileForms)
