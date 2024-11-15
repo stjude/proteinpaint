495cfcb69d181a1304c1a832dc27e356f5286ff1
Colleen Reilly <creilly@stjude.org>
2024-11-15 13:55:30 -0600

    Use icon instead of rect for plots in the legend

--- client/plots/boxplot/view/LegendRender.ts
+++ client/plots/boxplot/view/LegendRender.ts
@@ -27,13 +27,20 @@ export class LegendRenderer {
 			legendItem
 				.append('div')
 				.style('display', 'inline-block')
-				.style('min-width', '12px')
-				.style('height', '12px')
-				.style('background-color', item.color)
-				.style('border', `1px solid ${rgb(item.color).darker(1)}`)
+				// 	.style('min-width', '12px')
+				// 	.style('height', '12px')
+				// 	.style('background-color', item.color)
+				// 	.style('border', `1px solid ${rgb(item.color).darker(1)}`)
 				.style('margin-right', '3px')
-				.style('top', '1px')
+				// .style('top', '1px')
 				.style('position', 'relative')
+				.style('vertical-align', 'middle')
+				.html(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${rgb(item.color).darker(
+				1
+			)}" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16" style="vertical-align: middle; display: block; margin: auto;">
+                <path stroke="#000" stroke-width="0.25" fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
+                <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
+                </svg>`)
 		}
 		legendItem
 			.append('div')
