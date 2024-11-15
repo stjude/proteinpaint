11aaed0485063f601291539ca0d4ca7a89ba1ec7
Colleen Reilly <creilly@stjude.org>
2024-11-15 13:14:49 -0600

    Add tip to dom

--- client/plots/boxplot/view/BoxPlotLabelMenu.ts
+++ client/plots/boxplot/view/BoxPlotLabelMenu.ts
@@ -4,8 +4,7 @@ import type { BoxPlotInteractions } from '../interactions/BoxPlotInteractions'
 import { renderTable } from '#dom'
 
 export class BoxPlotLabelMenu {
-	tip = new Menu({ padding: '' })
-	constructor(plot, app: MassAppApi, interactions: BoxPlotInteractions) {
+	constructor(plot, app: MassAppApi, interactions: BoxPlotInteractions, tip: Menu) {
 		const options = [
 			//TODO: Filter option? Group?
 			{
@@ -19,12 +18,12 @@ export class BoxPlotLabelMenu {
 				text: `List samples`,
 				isVisible: (state: MassState) => state.termdbConfig.displaySampleIds && app.vocabApi.hasVerifiedToken(),
 				callback: async () => {
-					this.tip.clear().showunder(plot.boxplot.labelG.node())
+					tip.clear().showunder(plot.boxplot.labelG.node())
 					const min = plot.descrStats.find(s => s.id === 'min').value
 					const max = plot.descrStats.find(s => s.id === 'max').value
 					const rows = await interactions.listSamples(plot, min, max)
 
-					const tableDiv = this.tip.d.append('div')
+					const tableDiv = tip.d.append('div')
 					const columns = [{ label: 'Sample' }, { label: 'Value' }]
 
 					renderTable({
@@ -40,16 +39,16 @@ export class BoxPlotLabelMenu {
 			}
 		]
 		plot.boxplot.labelG.on('click', () => {
-			this.tip.clear().showunder(plot.boxplot.labelG.node())
+			tip.clear().showunder(plot.boxplot.labelG.node())
 			const state = app.getState()
 			for (const opt of options) {
 				if (!opt.isVisible(state)) continue
-				this.tip.d
+				tip.d
 					.append('div')
 					.classed('sja_menuoption', true)
 					.text(opt.text)
 					.on('click', () => {
-						this.tip.hide()
+						tip.hide()
 						opt.callback()
 					})
 			}
