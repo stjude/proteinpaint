5b783cd4e6e913dd6c482187e357a405b3533e52
siosonel <siosonel@gmail.com>
2024-11-15 13:40:21 -0600

    require import type syntax in eslint, as applicable

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
 	rules: {
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/no-unused-vars': 'warn'
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/consistent-type-imports': 'error'
 	}
 }
