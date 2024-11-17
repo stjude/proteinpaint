9aa1a865d1fd34165d03561a069f0e646e7887ec
siosonel <siosonel@gmail.com>
2024-11-16 20:29:44 -0600

    improve shared/types package scripts and readme

--- shared/types/build.sh
+++ shared/types/build.sh
@@ -4,4 +4,8 @@ set -euxo pipefail
 
 npx tsx emitCheckers.ts 
 npx typia generate --input ./checkers --output ./dist
+# esbuild will emit js files from ts files,
+# note that package.json:files[] only include dist/*.js;
 node esbuild.config.mjs
+# after packing, should remove dist/*.js except dist/index.js
+# see package.json:scripts.postpack
--- shared/types/dev.mjs
+++ shared/types/dev.mjs
@@ -1,8 +1,12 @@
 import { execSync } from 'child_process'
 
 // - in dev: tsx will be used to run server code and automatically
-// 					 transpile imported dist/*.ts files;
+// 					 transpile imported dist/*.ts files
+//           (no need to generate dist/*.js files, but copy over checkers/index.js to dist/);
 //
 // - in prod build: esbuild will be required to generate static dist/*.js files
 //
 execSync(`npx typia generate --input ./checkers --output ./dist`)
+// typia does not emit js files, must manually copy index.js
+// which is referenced as package.json:exports."./checkers" subpath alias
+execSync(`cp ./checkers/index.js ./dist`)
