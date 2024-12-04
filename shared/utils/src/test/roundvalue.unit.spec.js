6e4da093b972ecc62f0ef1362eda8b0542eae0ab
Colleen Reilly <creilly@stjude.org>
2024-12-03 18:07:05 -0600

    Updated roundValue.js unit tests

--- shared/utils/src/test/roundvalue.unit.spec.js
+++ shared/utils/src/test/roundvalue.unit.spec.js
@@ -1,6 +1,12 @@
 import tape from 'tape'
 import { roundValueAuto, roundValue2 } from '../roundValue.js'
 
+/**
+ * Tests
+ * 		roundValueAuto()
+ * 		roundValue2()
+ */
+
 /**************
  test sections
 ***************/
@@ -9,72 +15,121 @@ tape('\n', function (test) {
 	test.end()
 })
 
-tape('roundValue tests', function (test) {
-	let value = 1.23456789e-10
-	let rounded = 1.2e-10
-	test.equal(roundValueAuto(value), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), value, `is unchanged`)
+tape('roundValueAuto()', function (test) {
+	let value, rounded, formatted
+
+	value = 1.23456789e-10
+	rounded = 1.2e-10
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
 
 	value = 1.2345
 	rounded = 1.23
-	test.equal(roundValueAuto(value), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), rounded, `should return ${rounded}`)
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
 
 	value = 10549.23556789
-	rounded = '1.1e+4'
-	let r2 = 10549
-	test.equal(roundValueAuto(value, true), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), r2, `should return ${r2}`)
+	rounded = 10549.24
+	formatted = '1.1e+4'
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+	test.equal(
+		roundValueAuto(value, true),
+		formatted,
+		`Should return formatted value=${formatted} for input value=${value}`
+	)
 
 	value = 1549.23556789
 	rounded = 1549.24
-	r2 = 1549
-	test.equal(roundValueAuto(value), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), r2, `should return ${r2}`)
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
 
 	value = 549.23556789
 	rounded = 549.24
-	r2 = 549
-	test.equal(roundValueAuto(value), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), r2, `should return ${r2}`)
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
 
 	value = -89378.345862
-	rounded = '-8.9e+4'
-	r2 = -89378
-	test.equal(roundValueAuto(value, true), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), r2, `should return ${r2}`)
+	rounded = -89378.35
+	formatted = '-8.9e+4'
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+	test.equal(
+		roundValueAuto(value, true),
+		formatted,
+		`Should return formatted value=${formatted} for input value=${value}`
+	)
 
 	value = -0.006
-	test.equal(roundValueAuto(value), value, `should return ${value}`)
-	test.equal(roundValue2(value), value, `should return ${value}`)
+	test.equal(roundValueAuto(value), value, `Should return input value=${value} unchanged`)
 
 	value = 1000
-	test.equal(roundValueAuto(value), value, `should return ${value}`)
-	test.equal(roundValue2(value), value, `should return ${value}`)
+	test.equal(roundValueAuto(value), value, `Should return input value=${value} unchanged`)
 
 	value = 1000.1234
 	rounded = 1000.12
-	r2 = 1000
-	test.equal(roundValueAuto(value), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), r2, `should return ${r2}`)
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
 
 	value = -0.0001
-	test.equal(roundValueAuto(value), value, `should return ${rounded}`)
-	test.equal(roundValue2(value), value, `should return ${value}`)
+	test.equal(roundValueAuto(value), value, `Should return rounded value=${rounded} for input value=${value}`)
 
 	value = 0.00001
-	test.equal(roundValueAuto(value), value, `should return ${value}`)
-	test.equal(roundValue2(value), value, `should return ${value}`)
+	test.equal(roundValueAuto(value), value, `Should return input value=${value} unchanged`)
 
 	value = 0.000056
 	rounded = 5.6e-5
-	test.equal(roundValueAuto(value), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), rounded, `should return ${rounded}`)
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
 
 	value = 0.0000005
 	rounded = 5e-7
-	test.equal(roundValueAuto(value), rounded, `should return ${rounded}`)
-	test.equal(roundValue2(value), value, `should return ${value}`)
+	test.equal(roundValueAuto(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+
+	test.end()
+})
+
+tape('roundValue2()', function (test) {
+	let value, rounded
+
+	value = 1.23456789e-10
+	test.equal(roundValue2(value), value, `Should return input value=${value} unchanged`)
+
+	value = 1.2345
+	rounded = 1.23
+	test.equal(roundValue2(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+
+	value = 10549.23556789
+	rounded = 10549
+	test.equal(roundValue2(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+
+	value = 1549.23556789
+	rounded = 1549
+	test.equal(roundValue2(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+
+	value = 549.23556789
+	rounded = 549
+	test.equal(roundValue2(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+
+	value = -89378.345862
+	rounded = -89378
+	test.equal(roundValue2(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+
+	value = -0.006
+	test.equal(roundValue2(value), value, `Should return input value=${value} unchanged`)
+
+	value = 1000
+	test.equal(roundValue2(value), value, `Should return input value=${value} unchanged`)
+
+	value = 1000.1234
+	rounded = 1000
+	test.equal(roundValue2(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+
+	value = -0.0001
+	test.equal(roundValue2(value), value, `Should return input value=${value} unchanged`)
+
+	value = 0.00001
+	test.equal(roundValue2(value), value, `Should return input value=${value} unchanged`)
+
+	value = 0.000056
+	rounded = 5.6e-5
+	test.equal(roundValue2(value), rounded, `Should return rounded value=${rounded} for input value=${value}`)
+
+	value = 0.0000005
+	rounded = 5e-7
+	test.equal(roundValue2(value), value, `Should return input value=${value} unchanged`)
 
 	test.end()
 })
