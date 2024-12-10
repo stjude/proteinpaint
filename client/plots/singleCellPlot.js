77d248ba9a8cadb58ae8c4508a0f28804e05ea06
Airen Zaldivar <airenzp@gmail.com>
2024-12-10 11:26:30 -0600

    Controls handling

--- client/plots/singleCellPlot.js
+++ client/plots/singleCellPlot.js
@@ -6,7 +6,7 @@ import { zoom as d3zoom, zoomIdentity } from 'd3-zoom'
 import { controlsInit } from './controls'
 import { downloadSingleSVG } from '../common/svg.download.js'
 import { select } from 'd3-selection'
-import { rgb } from 'd3'
+import { max, rgb } from 'd3'
 import { roundValueAuto } from '#shared/roundValue.js'
 import { TermTypes } from '#shared/terms.js'
 import { ColorScale, icons as icon_functions, addGeneSearchbox, renderTable, sayerror, Menu } from '#dom'
@@ -218,7 +218,6 @@ class singleCellPlot {
 
 		document.addEventListener('scroll', event => this?.tip?.hide())
 		select('.sjpp-output-sandbox-content').on('scroll', event => this.tip.hide())
-		await this.setControls(state)
 	}
 
 	renderShowPlots(showDiv, state) {
@@ -504,7 +503,8 @@ class singleCellPlot {
 		})
 	}
 
-	async setControls(state) {
+	async setControls() {
+		this.dom.controlsHolder.selectAll('*').remove()
 		const inputs = [
 			{
 				label: 'Chart width',
@@ -521,15 +521,21 @@ class singleCellPlot {
 				settingsKey: 'svgh',
 				min: 300,
 				max: 1000
-			},
-			{
-				label: 'Show grid',
-				type: 'checkbox',
+			}
+		]
+		if (this.largePlot) {
+			inputs.push({
+				label: 'Sample size',
+				type: 'number',
 				chartType: 'singleCellPlot',
-				settingsKey: 'showGrid',
-				boxLabel: ''
-			},
-			{
+				settingsKey: 'sampleSizeThree',
+				title: 'Sample size',
+				min: 0.001,
+				max: 0.1,
+				step: 0.001
+			})
+		} else {
+			inputs.push({
 				label: 'Sample size',
 				type: 'number',
 				chartType: 'singleCellPlot',
@@ -538,8 +544,16 @@ class singleCellPlot {
 				min: 1,
 				max: 3,
 				step: 0.5
-			}
-		]
+			}),
+				inputs.push({
+					label: 'Show grid',
+					boxLabel: '',
+					type: 'checkbox',
+					chartType: 'singleCellPlot',
+					settingsKey: 'showGrid',
+					title: 'Show grid'
+				})
+		}
 
 		this.components = {
 			controls: await controlsInit({
@@ -584,7 +598,7 @@ class singleCellPlot {
 		this.data = await this.getData()
 		this.dom.loadingDiv.style('display', 'none')
 		this.renderPlots(this.data)
-
+		await this.setControls()
 		if (this.dom.header) this.dom.header.html(` ${this.state.config.sample || this.samples[0].sample} single cell data`)
 	}
 
@@ -669,6 +683,7 @@ class singleCellPlot {
 		for (const plot of result.plots) {
 			this.plots.push(plot)
 			plot.cells = [...plot.noExpCells, ...plot.expCells]
+			if (plot.cells.length > maxSamplesD3) this.largePlot = true
 			plot.id = plot.name.replace(/\s+/g, '')
 			this.renderPlot(plot)
 		}
@@ -1202,7 +1217,7 @@ class singleCellPlot {
 		geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
 		const tex = getCircle(128)
 		const material = new THREE.PointsMaterial({
-			size: this.settings.sampleSize * 0.001,
+			size: this.settings.sampleSizeThree,
 			sizeAttenuation: true,
 			transparent: true,
 			opacity: this.settings.opacity,
@@ -1216,29 +1231,31 @@ class singleCellPlot {
 		const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: plot.canvas, preserveDrawingBuffer: true })
 		renderer.setPixelRatio(window.devicePixelRatio)
 
-		if (this.settings.showGrid) {
-			// Line Geometry
-			const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffd3d3d3 })
+		// The grid should be out of the zoom, commented out until handled
 
-			const lines = 5
-			let x = -1
-			const step = 0.5
-			for (let i = 0; i < 5; i++) {
-				let points = []
-				points.push(new THREE.Vector3(x, 1, 0))
-				points.push(new THREE.Vector3(x, -1, 0))
-				let lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-				let line = new THREE.Line(lineGeometry, lineMaterial)
-				scene.add(line)
-				points = []
-				points.push(new THREE.Vector3(-1, x, 0))
-				points.push(new THREE.Vector3(1, x, 0))
-				lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-				line = new THREE.Line(lineGeometry, lineMaterial)
-				scene.add(line)
-				x += step
-			}
-		}
+		// if (this.settings.showGrid) {
+		// 	// Line Geometry
+		// 	const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffd3d3d3 })
+
+		// 	const lines = 5
+		// 	let x = -1
+		// 	const step = 0.5
+		// 	for (let i = 0; i < 5; i++) {
+		// 		let points = []
+		// 		points.push(new THREE.Vector3(x, 1, 0))
+		// 		points.push(new THREE.Vector3(x, -1, 0))
+		// 		let lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+		// 		let line = new THREE.Line(lineGeometry, lineMaterial)
+		// 		scene.add(line)
+		// 		points = []
+		// 		points.push(new THREE.Vector3(-1, x, 0))
+		// 		points.push(new THREE.Vector3(1, x, 0))
+		// 		lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+		// 		line = new THREE.Line(lineGeometry, lineMaterial)
+		// 		scene.add(line)
+		// 		x += step
+		// 	}
+		// }
 
 		// Line Object
 
@@ -1304,7 +1321,8 @@ export async function getPlotConfig(opts, app) {
 		const config = {
 			hiddenClusters: [],
 			settings: {
-				singleCellPlot: settings
+				singleCellPlot: settings,
+				controls: { isOpen: false }
 			},
 			startColor: {},
 			stopColor: {},
@@ -1325,6 +1343,7 @@ export function getDefaultSingleCellSettings() {
 		svgh: 900,
 		showGrid: true,
 		sampleSize: 1.5,
+		sampleSizeThree: 0.001,
 		threeFOV: 55,
 		opacity: 0.8
 	}
