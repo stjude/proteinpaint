51ee43898c45705ec0aa342900a4cb77f0437469
Colleen Reilly <creilly@stjude.org>
2024-12-10 09:32:19 -0600

    Fix erroneous test and organize tests into sections

--- client/src/databrowser/test/databrowser.unit.spec.js
+++ client/src/databrowser/test/databrowser.unit.spec.js
@@ -1,30 +1,32 @@
 import tape from 'tape'
 import { parseDictionary } from '../dictionary.parse'
-import * as d3s from 'd3-selection'
 
 /****************
- Dictionary Tests
+Tests
 
-levels before variable, type, and categories - no gaps
-levels after variable, type, categories - with gap
-empty variable
-extra, non essential column
-no level columns
-repeated level names, same line
-dash between levels
-missing type
-repeated intermediate terms for diff branches, whole dataset
-missing Variable header
-missing Type header
-missing parent_id header
-missing name header
-missing type header
-missing values header
-blank or dash in required data column
-missing k=v in values (dictionary format)
-uncomputable category is a string but not number
-uncomputable category is empty string but not number
-add additional attributes to term
+- Phenotree parsing tests
+	levels before variable, type, and categories - no gaps
+	levels after variable, type, categories - with gap
+	empty variable
+	extra, non essential column
+	no level columns
+	repeated level names, same line
+	dash between levels
+	missing type
+	repeated intermediate terms for diff branches, whole dataset
+	missing Variable header
+	missing Type header
+	uncomputable category is a string but not number
+	uncomputable category is empty string but not number
+	add additional attributes to term
+
+- Data dictionary parsing tests
+	missing parent_id header
+	missing name header
+	missing type header
+	missing values header
+	blank or dash in required data column
+	missing k=v in values (dictionary format)
 
 *****************/
 
@@ -32,6 +34,10 @@ tape('\n', function (test) {
 	test.pass('-***- dictionary, phenotree parsing -***-')
 	test.end()
 })
+
+/**************
+Phenotree tests
+***************/
 
 tape('levels before variable, type, and categories - no gaps', function (test) {
 	test.timeoutAfter(100)
@@ -354,21 +360,22 @@ tape('no level columns', function (test) {
 	test.end()
 })
 
-tape.skip('repeated level names, same line', function (test) {
+tape('repeated level names, same line', function (test) {
 	test.timeoutAfter(100)
 	const tsv = [
 		`level_1\tlevel_2\tlevel_3\tvariable\ttype\tcategories`,
-		`A\tA.1\tA.1.a\tA1a\tcategorical\t{ "0": { "label": "No" }, "1": { "label": "Yes" } }`,
+		`A\tA\tA.1.a\tA1a\tcategorical\t{ "0": { "label": "No" }, "1": { "label": "Yes" } }`,
 		`A\tA.1\tA.1.b\tA1b\tcategorical\t{ "0": { "label": "No" }, "1": { "label": "Yes" } }`,
 		`A\tA.2\tA.2.a\tA2a\tcategorical\t{ "0": {"label": "Not treated" }, "1": { "label": "Treated" } }`,
 		`B\tB.1\tB.1.a\tB1a\tcategorical\t{ "0": {"label": "Not treated" }, "1": { "label": "Treated" } }`
 	].join('\n')
 
-	const message = 'should display an error for repeated level names in the same line'
+	const message = 'Should display an error for repeated level names in the same line'
 	try {
-		const results = parseDictionary(tsv)
+		parseDictionary(tsv) //parsePhenoTree()
+		test.fail(message)
 	} catch (e) {
-		test.fail(message + ': ' + e)
+		test.pass(message + ': ' + e)
 	}
 	test.end()
 })
@@ -473,6 +480,118 @@ tape('missing Type header', function (test) {
 	}
 	test.end()
 })
+
+tape('uncomputable category is a string but not number', function (test) {
+	test.timeoutAfter(100)
+	// user can totally encode missing numeric values with words like "unk". our system requires those to be coded as number instead otherwise it crashes our sql query. in below "unk" key is rejected
+	const tsv = [`variable\ttype\tcategories`, `A1a\tinteger\t{ "unk": { "label": "unknown" } }`].join('\n')
+
+	const message = 'Should throw on uncomputable category not being number'
+	try {
+		parseDictionary(tsv)
+		test.fail(message)
+	} catch (e) {
+		test.pass(message + ': ' + e)
+	}
+	test.end()
+})
+
+tape('uncomputable category is empty string but not number', function (test) {
+	test.timeoutAfter(100)
+	const tsv = [`variable\ttype\tcategories`, `A1a\tinteger\t{ "": { "label": "empty!!" } }`].join('\n')
+
+	const message = 'Should throw on uncomputable category not being number'
+	try {
+		parseDictionary(tsv)
+		test.fail(message)
+	} catch (e) {
+		test.pass(message + ': ' + e)
+	}
+	test.end()
+})
+
+tape('add unit to term', function (test) {
+	test.timeoutAfter(100)
+	const tsv = [
+		`Level_1\tLevel_2\tLevel_3\tLevel_4\tLevel_5\tVariable\ttype\tCategories\tUnit`,
+		`Root\tL2\tL3\t-\t-\tdiaggrp\tcategorical\t`,
+		`Root\tAge at Sample\t-\t-\t-\tsnp6_sample_age\tinteger\t{\"999\":{\"label\":\"N/A:CCSS\"}}\tyears`
+	].join('\n')
+	const message = `Should add unit to term`
+	try {
+		const results = parseDictionary(tsv)
+		const expected = [
+			{
+				id: 'diaggrp',
+				name: 'L3',
+				type: 'categorical',
+				values: {},
+				groupsetting: {
+					disabled: true
+				},
+				child_order: 1,
+				isleaf: true,
+				parent_id: 'L2'
+			},
+			{
+				id: 'snp6_sample_age',
+				name: 'Age at Sample',
+				type: 'integer',
+				groupsetting: undefined,
+				values: {
+					999: {
+						label: 'N/A:CCSS',
+						uncomputable: true
+					}
+				},
+				child_order: 2,
+				isleaf: true,
+				unit: 'years',
+				parent_id: 'Root'
+			},
+			{
+				id: 'Root',
+				name: 'Root',
+				isleaf: false,
+				child_order: 1,
+				parent_id: null
+			},
+			{
+				id: 'L2',
+				name: 'L2',
+				isleaf: false,
+				child_order: 1,
+				parent_id: 'Root'
+			}
+		]
+		test.deepEqual(results.terms, expected, message)
+	} catch (e) {
+		test.fail(message + ': ' + e)
+	}
+
+	test.end()
+})
+
+tape('add additional attributes to term', function (test) {
+	test.timeoutAfter(100)
+	const tsv = [
+		`Level_1\tLevel_2\tVariable\ttype\tCategories\tadditional attributes`,
+		`Root\tTerm 2\tterm2\tinteger\t{\"999\":{\"label\":\"N/A:CCSS\"}}\t{"xx":"yy"}`
+	].join('\n')
+	try {
+		const results = parseDictionary(tsv)
+		const term = results.terms.find(i => i.id == 'term2')
+		test.ok(term, 'term is found by id=term2')
+		test.equal(term.xx, 'yy', 'new property found: term.xx=yy')
+	} catch (e) {
+		test.fail('additional attributes: ' + e)
+	}
+	test.end()
+})
+
+/*********************
+Data Dictionary tests
+*********************/
 
 tape('missing parent_id header', function (test) {
 	test.timeoutAfter(100)
@@ -594,114 +713,6 @@ tape('missing k=v in values (dictionary format)', function (test) {
 		test.fail(message)
 	} catch (e) {
 		test.pass(message + ': ' + e)
-	}
-	test.end()
-})
-
-tape('uncomputable category is a string but not number', function (test) {
-	test.timeoutAfter(100)
-	// user can totally encode missing numeric values with words like "unk". our system requires those to be coded as number instead otherwise it crashes our sql query. in below "unk" key is rejected
-	const tsv = [`variable\ttype\tcategories`, `A1a\tinteger\t{ "unk": { "label": "unknown" } }`].join('\n')
-
-	const message = 'Should throw on uncomputable category not being number'
-	try {
-		parseDictionary(tsv)
-		test.fail(message)
-	} catch (e) {
-		test.pass(message + ': ' + e)
-	}
-	test.end()
-})
-
-tape('uncomputable category is empty string but not number', function (test) {
-	test.timeoutAfter(100)
-	const tsv = [`variable\ttype\tcategories`, `A1a\tinteger\t{ "": { "label": "empty!!" } }`].join('\n')
-
-	const message = 'Should throw on uncomputable category not being number'
-	try {
-		parseDictionary(tsv)
-		test.fail(message)
-	} catch (e) {
-		test.pass(message + ': ' + e)
-	}
-	test.end()
-})
-
-tape('add unit to term', function (test) {
-	test.timeoutAfter(100)
-	const tsv = [
-		`Level_1\tLevel_2\tLevel_3\tLevel_4\tLevel_5\tVariable\ttype\tCategories\tUnit`,
-		`Root\tL2\tL3\t-\t-\tdiaggrp\tcategorical\t`,
-		`Root\tAge at Sample\t-\t-\t-\tsnp6_sample_age\tinteger\t{\"999\":{\"label\":\"N/A:CCSS\"}}\tyears`
-	].join('\n')
-	const message = `Should add unit to term`
-	try {
-		const results = parseDictionary(tsv)
-		const expected = [
-			{
-				id: 'diaggrp',
-				name: 'L3',
-				type: 'categorical',
-				values: {},
-				groupsetting: {
-					disabled: true
-				},
-				child_order: 1,
-				isleaf: true,
-				parent_id: 'L2'
-			},
-			{
-				id: 'snp6_sample_age',
-				name: 'Age at Sample',
-				type: 'integer',
-				groupsetting: undefined,
-				values: {
-					999: {
-						label: 'N/A:CCSS',
-						uncomputable: true
-					}
-				},
-				child_order: 2,
-				isleaf: true,
-				unit: 'years',
-				parent_id: 'Root'
-			},
-			{
-				id: 'Root',
-				name: 'Root',
-				isleaf: false,
-				child_order: 1,
-				parent_id: null
-			},
-			{
-				id: 'L2',
-				name: 'L2',
-				isleaf: false,
-				child_order: 1,
-				parent_id: 'Root'
-			}
-		]
-		test.deepEqual(results.terms, expected, message)
-	} catch (e) {
-		test.fail(message + ': ' + e)
-	}
-
-	test.end()
-})
-
-tape('add additional attributes to term', function (test) {
-	test.timeoutAfter(100)
-	const tsv = [
-		`Level_1\tLevel_2\tVariable\ttype\tCategories\tadditional attributes`,
-		`Root\tTerm 2\tterm2\tinteger\t{\"999\":{\"label\":\"N/A:CCSS\"}}\t{"xx":"yy"}`
-	].join('\n')
-	try {
-		const results = parseDictionary(tsv)
-		const term = results.terms.find(i => i.id == 'term2')
-		test.ok(term, 'term is found by id=term2')
-		test.equal(term.xx, 'yy', 'new property found: term.xx=yy')
-	} catch (e) {
-		test.fail('additional attributes: ' + e)
 	}
 	test.end()
 })
