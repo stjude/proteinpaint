300c82a109b184f572c38a242cbf30084a876fdc
Airen Zaldivar <airenzp@gmail.com>
2024-12-10 00:08:44 -0600

    Cleanup in 2D large scatter and 3D

--- client/plots/sampleScatter.rendererThree.js
+++ client/plots/sampleScatter.rendererThree.js
@@ -32,19 +32,18 @@ export function setRenderersThree(self) {
 		const far = 1000
 		const camera = new THREE.PerspectiveCamera(fov, 1, near, far)
 		const scene = new THREE.Scene()
-		camera.position.set(0, 0, 1)
+		camera.position.set(0, 0, 1.5)
 		camera.lookAt(scene.position)
 		camera.updateMatrix()
 		const whiteColor = new THREE.Color('rgb(255,255,255)')
 		scene.background = whiteColor
 
 		const geometry = new THREE.BufferGeometry()
-		const pointer = new THREE.Vector2()
 		const { vertices, colors } = getVertices()
 
 		geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
 		geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
-		const tex = getCircle(128)
+		const tex = self.getCircle(128)
 		const material = new THREE.PointsMaterial({
 			size: self.settings.threeSize,
 			sizeAttenuation: true,
@@ -67,33 +66,21 @@ export function setRenderersThree(self) {
 		})
 
 		function getVertices() {
+			const xAxisScale = chart.xAxisScale.range([-1, 1])
+			const yAxisScale = chart.yAxisScale.range([-1, 1])
 			const vertices = []
 			const colors = []
 			for (const sample of chart.data.samples) {
 				const opacity = self.getOpacity(sample)
 				if (opacity == 0) continue
-				let x = (chart.xAxisScale(sample.x) - chart.xScaleMin) / self.canvas.width
-				let y = (chart.yAxisScale(sample.y) - chart.yScaleMax) / -self.canvas.height
-				let z = (chart.zAxisScale(sample.z) - chart.zScaleMin) / self.settings.svgd
-				vertices.push(x - 0.5, y + 0.7, z)
+				let x = xAxisScale(sample.x)
+				let y = yAxisScale(sample.y)
+				let z = 0
+				vertices.push(x, y, z)
 				const color = new THREE.Color(rgb(self.getColor(sample, chart)).toString())
 				colors.push(color.r, color.g, color.b)
 			}
 			return { vertices, colors }
-		}
-
-		function getCircle(size) {
-			const c = document.createElement('canvas')
-			c.width = size
-			c.height = size
-			const ctx = c.getContext('2d')
-			ctx.clearRect(0, 0, size, size)
-			ctx.fillStyle = 'white'
-			ctx.beginPath()
-			ctx.arc(size / 2, size / 2, size / 2, 0, 2 * Math.PI)
-			ctx.fill()
-			const tex = new THREE.CanvasTexture(c)
-			return tex
 		}
 
 		function animate() {
@@ -103,6 +90,20 @@ export function setRenderersThree(self) {
 			renderer.render(scene, camera)
 		}
 		animate()
+	}
+
+	self.getCircle = function (size) {
+		const c = document.createElement('canvas')
+		c.width = size
+		c.height = size
+		const ctx = c.getContext('2d')
+		ctx.clearRect(0, 0, size, size)
+		ctx.fillStyle = 'white'
+		ctx.beginPath()
+		ctx.arc(size / 2, size / 2, size / 2, 0, 2 * Math.PI)
+		ctx.fill()
+		const tex = new THREE.CanvasTexture(c)
+		return tex
 	}
 
 	self.render3DSerie = async function (chart) {
@@ -131,41 +132,53 @@ export function setRenderersThree(self) {
 		const controls = new OrbitControls.OrbitControls(camera, self.canvas)
 		controls.enableZoom = false
 		controls.update()
-		camera.position.set(1.5, 0.5, 2)
+		camera.position.set(1, 1, 2)
 		camera.lookAt(scene.position)
 
 		if (self.settings.showAxes) {
 			const axesHelper = new THREE.AxesHelper(1)
 			scene.add(axesHelper)
 			self.addLabels(scene, chart)
-
-			// const grid = new THREE.GridHelper(1)
-			// grid.position.x = 0.5
-			// grid.position.z = 0.5
-			// scene.add(grid)
 		}
 		camera.updateMatrix()
 		const whiteColor = new THREE.Color('rgb(255,255,255)')
 		scene.background = whiteColor
 
-		const light = new THREE.DirectionalLight(whiteColor, 2)
-		light.position.set(0.1, 0.1, 2)
-		scene.add(light)
-		const geometry = new THREE.SphereGeometry(0.005, 32)
+		//const geometry = new THREE.SphereGeometry(0.003, 64)
+		const xAxisScale = chart.xAxisScale.range([0, 1])
+		const yAxisScale = chart.yAxisScale.range([0, 1])
+		const zAxisScale = chart.zAxisScale.range([0, 1])
+
+		const vertices = []
+		const colors = []
 		for (const sample of chart.data.samples) {
 			const opacity = self.getOpacity(sample)
 			if (opacity == 0) continue
-			let x = chart.xMax == chart.xMin ? 0 : Math.abs((sample.x - chart.xMin) / (chart.xMax - chart.xMin))
-			let y = chart.yMax == chart.yMin ? 0 : Math.abs((sample.y - chart.yMin) / (chart.yMax - chart.yMin))
-			let z = chart.zMax == chart.zMin ? 0 : Math.abs((sample.z - chart.zMin) / (chart.zMax - chart.zMin))
+			let x = xAxisScale(sample.x)
+			let y = yAxisScale(sample.y)
+			let z = zAxisScale(sample.z)
 			const color = new THREE.Color(rgb(self.getColor(sample, chart)).toString())
-			const material = new THREE.MeshLambertMaterial({ color })
-			const circle = new THREE.Mesh(geometry, material)
-			scene.add(circle)
-			circle.position.set(x, y, z)
-			scene.add(circle)
+			vertices.push(x, y, z)
+			colors.push(color.r, color.g, color.b)
 		}
 
+		const geometry = new THREE.BufferGeometry()
+
+		geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
+		geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+		const tex = self.getCircle(128)
+		const material = new THREE.PointsMaterial({
+			size: self.settings.threeSize * 3,
+			sizeAttenuation: true,
+			transparent: true,
+			opacity: self.settings.opacity,
+			map: tex,
+			vertexColors: true
+		})
+
+		const particles = new THREE.Points(geometry, material)
+
+		scene.add(particles)
 		const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: self.canvas, preserveDrawingBuffer: true })
 		renderer.setPixelRatio(window.devicePixelRatio)
 		//document.addEventListener( 'pointermove', onPointerMove );
@@ -219,7 +232,7 @@ export function setRenderersThree(self) {
 			const textGeo = new TextGeometry(text, {
 				font,
 				size: 0.02,
-				height: 0.005,
+				height: 0.002,
 				curveSegments: 8,
 				bevelEnabled: false
 			})
