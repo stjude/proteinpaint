87ff4105641b9c5bf667299b32deb47a0908921e
Gavriel Matt <gavriel.matt@stjude.org>
2024-12-20 11:52:19 -0600

    correctly pass usecase to findTermByName()

--- server/src/initGdc.termdb.js
+++ server/src/initGdc.termdb.js
@@ -637,7 +637,7 @@ function makeTermdbQueries(ds, id2term) {
 		return terms
 	}
 
-	q.findTermByName = async (searchStr, vocab, treeFilter = null, usecase = null) => {
+	q.findTermByName = async (searchStr, vocab, usecase = null, treeFilter = null) => {
 		searchStr = searchStr.toLowerCase() // convert to lowercase
 		// replace space with _ to match with id of terms
 		if (searchStr.includes(' ')) searchStr = searchStr.replace(/\s/g, '_')
