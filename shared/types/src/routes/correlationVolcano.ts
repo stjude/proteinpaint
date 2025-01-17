282f452996ec718f1f77bf5e5cba90b476647d78
Xin Zhou <xzhou82@gmail.com>
2025-01-17 09:30:55 -0600

    bug fix

--- shared/types/src/routes/correlationVolcano.ts
+++ shared/types/src/routes/correlationVolcano.ts
@@ -28,7 +28,7 @@ export type CorrelationVolcanoResponse = {
 		tw$id: string
 		/** number of samples analyzed. samples not having complete data for all terms will be excluded, thus size may be lower than current cohort */
 		sampleSize: number
-	}
+	}[]
 }
 
 export const CorrelationVolcanoPayload: RoutePayload = {
