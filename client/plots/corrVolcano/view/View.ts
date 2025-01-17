7055c87dcaf1e9d958ea323906c170654d823bfa
Colleen Reilly <creilly@stjude.org>
2025-01-17 14:29:44 -0600

    Basic correlation volcano plot renders with test data

--- client/plots/corrVolcano/view/View.ts
+++ client/plots/corrVolcano/view/View.ts
@@ -1,13 +1,48 @@
+import { axisstyle } from '#src/client'
+import { axisBottom, axisLeft } from 'd3-axis'
+
+/** Using the data formated in ViewModel, renders the correlation
+ * volcano plot. */
 export class View {
 	dom: any
 	viewData: any
+	readonly defaultRadius = 5
 	constructor(dom, viewData) {
 		this.viewData = viewData
+		this.dom = dom
 
-		this.renderDom()
+		const plotDim = viewData.plotDim
+		this.renderDom(plotDim)
 	}
 
-	renderDom() {
-		//TODO
+	renderDom(plotDim) {
+		this.dom.svg.transition().attr('width', plotDim.svg.width).attr('height', plotDim.svg.height)
+
+		this.renderScale(plotDim.yScale, true)
+		this.renderScale(plotDim.xScale)
+
+		this.renderVariables(this.viewData.variableItems)
+	}
+
+	renderScale(scale, isLeft = false) {
+		const scaleG = this.dom.svg.append('g').attr('transform', `translate(${scale.x}, ${scale.y})`)
+		axisstyle({
+			axis: scaleG.call(isLeft ? axisLeft(scale.scale) : axisBottom(scale.scale)),
+			color: 'black',
+			showline: true
+		})
+		// return scaleG
+	}
+
+	renderVariables(variableItems) {
+		for (const v of variableItems) {
+			this.dom.svg
+				.append('circle')
+				.attr('stroke', v.color)
+				.attr('fill', v.color)
+				.attr('cx', v.x)
+				.attr('cy', v.y)
+				.attr('r', this.defaultRadius)
+		}
 	}
 }
--- client/plots/corrVolcano/viewModel/ViewModel.ts
+++ client/plots/corrVolcano/viewModel/ViewModel.ts
@@ -1,33 +1,69 @@
+import { scaleLinear } from 'd3-scale'
+
 /** TODO, clean this up */
 export class ViewModel {
 	config: any
 	data: any
 	settings: any
 	viewData: any
+	readonly topPad = 40
+	readonly rightPad = 60
+	readonly bottomPad = 40
 	constructor(config, data, settings, variableTwLst) {
-		const plotDim = this.setPlotDimensions(data, config, settings)
+		/** Test data for development until the server code is ready
+		 * Will remove
+		 */
+		data.variableItems = [
+			{
+				tw$id: 'TDQWmvitw0gPLX7I9iWYAVcgJG4-',
+				correlation: 0.5,
+				pvalue: 0.025,
+				sampleSize: 1
+			},
+			{
+				tw$id: '0idPJ69kmHlL5kcaEBOlZmKmasg-',
+				correlation: -0.5,
+				pvalue: 0.002,
+				sampleSize: 2
+			}
+		]
+		const absYMax = data.variableItems.sort((a, b) => b.pvalue - a.pvalue)[0].pvalue
+		const plotDim = this.setPlotDimensions(settings, absYMax)
 
 		this.viewData = {
-			plotDim: this.setPlotDimensions(data, config, settings),
-			variables: this.setVariablesData(data, config, settings, variableTwLst)
+			plotDim,
+			variableItems: this.setVariablesData(data, variableTwLst, plotDim)
 		}
 	}
 
-	setPlotDimensions(data, config, settings) {
-		const plotDim = {
+	setPlotDimensions(settings, absYMax: number) {
+		return {
 			svg: {
-				height: settings.height,
-				width: settings.width
+				height: settings.height + this.topPad + this.bottomPad,
+				width: settings.width + this.rightPad
 			},
-			xDomain: [-1, 1],
-			xRange: [0, settings.width]
+			xScale: {
+				scale: scaleLinear().domain([-1, 1]).range([0, settings.width]),
+				x: this.rightPad,
+				y: settings.height + this.topPad
+			},
+			yScale: {
+				scale: scaleLinear().domain([0, absYMax]).range([settings.height, 0]),
+				x: this.rightPad,
+				y: this.topPad
+			}
 		}
 	}
 
-	setVariablesData(data, config, settings, variableTwLst) {
-		for (const v of data.variables) {
+	setVariablesData(data, variableTwLst, plotDim) {
+		for (const v of data.variableItems) {
 			v.color = v.correlation > 0 ? 'blue' : 'red'
 			v.label = variableTwLst.find(t => t.$id == v.tw$id).term.name
+			v.x = plotDim.xScale.scale(v.correlation) + this.rightPad
+			v.y = plotDim.yScale.scale(v.pvalue) + this.topPad
+
+			console.log(v)
 		}
+		return data.variableItems
 	}
 }
