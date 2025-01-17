46ba1117f7f31ec6b87d3a937bdbda1b851a26af
Colleen Reilly <creilly@stjude.org>
2025-01-17 15:27:42 -0600

    Render p values in log scale with dividing line between pos and neg values

--- client/plots/corrVolcano/viewModel/ViewModel.ts
+++ client/plots/corrVolcano/viewModel/ViewModel.ts
@@ -1,34 +1,44 @@
-import { scaleLinear } from 'd3-scale'
+import type { TermWrapper } from '#types'
+import { scaleLinear, scaleLog } from 'd3-scale'
+import type { CorrVolcanoSettings } from '../CorrelationVolcano'
+
+export type ViewData = {
+	plotDim: any
+	variableItems: any
+}
 
 /** TODO, clean this up */
 export class ViewModel {
-	config: any
-	data: any
-	settings: any
-	viewData: any
+	// config: any
+	// data: any
+	// settings: CorrVolcanoSettings
+	viewData: ViewData
 	readonly topPad = 40
-	readonly rightPad = 60
-	readonly bottomPad = 40
-	constructor(config, data, settings, variableTwLst) {
+	/** Only one side, left or right */
+	readonly horizPad = 60
+	readonly bottomPad = 20
+	constructor(data, settings: CorrVolcanoSettings, variableTwLst: TermWrapper[]) {
 		/** Test data for development until the server code is ready
 		 * Will remove
 		 */
 		data.variableItems = [
 			{
-				tw$id: 'TDQWmvitw0gPLX7I9iWYAVcgJG4-',
+				tw$id: 'TDQWmvitw0gPLX7I9iWYAVcgJG4-', //pragma: allowlist secret
 				correlation: 0.5,
 				pvalue: 0.025,
 				sampleSize: 1
 			},
 			{
-				tw$id: '0idPJ69kmHlL5kcaEBOlZmKmasg-',
+				tw$id: '0idPJ69kmHlL5kcaEBOlZmKmasg-', //pragma: allowlist secret
 				correlation: -0.5,
 				pvalue: 0.002,
 				sampleSize: 2
 			}
 		]
-		const absYMax = data.variableItems.sort((a, b) => b.pvalue - a.pvalue)[0].pvalue
-		const plotDim = this.setPlotDimensions(settings, absYMax)
+		const sortedValues = data.variableItems.map(v => v.pvalue).sort((a, b) => a - b)
+		const absYMax = sortedValues[sortedValues.length - 1]
+		const absYMin = sortedValues[0]
+		const plotDim = this.setPlotDimensions(settings, absYMax, absYMin)
 
 		this.viewData = {
 			plotDim,
@@ -36,33 +46,36 @@ export class ViewModel {
 		}
 	}
 
-	setPlotDimensions(settings, absYMax: number) {
+	setPlotDimensions(settings, absYMax: number, absYMin: number) {
 		return {
 			svg: {
 				height: settings.height + this.topPad + this.bottomPad,
-				width: settings.width + this.rightPad
+				width: settings.width + this.horizPad * 2
 			},
 			xScale: {
 				scale: scaleLinear().domain([-1, 1]).range([0, settings.width]),
-				x: this.rightPad,
+				x: this.horizPad,
 				y: settings.height + this.topPad
 			},
 			yScale: {
-				scale: scaleLinear().domain([0, absYMax]).range([settings.height, 0]),
-				x: this.rightPad,
+				scale: scaleLog().domain([absYMin, absYMax]).range([settings.height, 0]),
+				x: this.horizPad,
 				y: this.topPad
+			},
+			divideLine: {
+				x: this.horizPad + settings.width / 2,
+				y1: settings.height + this.topPad,
+				y2: this.topPad
 			}
 		}
 	}
 
 	setVariablesData(data, variableTwLst, plotDim) {
-		for (const v of data.variableItems) {
-			v.color = v.correlation > 0 ? 'blue' : 'red'
-			v.label = variableTwLst.find(t => t.$id == v.tw$id).term.name
-			v.x = plotDim.xScale.scale(v.correlation) + this.rightPad
-			v.y = plotDim.yScale.scale(v.pvalue) + this.topPad
-
-			console.log(v)
+		for (const item of data.variableItems) {
+			item.color = item.correlation > 0 ? 'blue' : 'red'
+			item.label = variableTwLst.find(t => t.$id == item.tw$id).term.name
+			item.x = plotDim.xScale.scale(item.correlation) + this.horizPad
+			item.y = plotDim.yScale.scale(item.pvalue) + this.topPad
 		}
 		return data.variableItems
 	}
