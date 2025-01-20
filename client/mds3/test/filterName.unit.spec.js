12f6571f0d31f9919ec18d8268714c82c2b26480
Xin Zhou <xzhou82@gmail.com>
2025-01-19 22:15:08 -0600

    fix: getFilterName ignores cohort tvs

--- client/mds3/test/filterName.unit.spec.js
+++ client/mds3/test/filterName.unit.spec.js
@@ -6,10 +6,35 @@ tape('\n', test => {
 	test.end()
 })
 
+// TODO multivalue
+
 const catTerm = { type: 'categorical', name: 'TermA', values: { A: { label: 'Aname' } } }
+const cohortTvs = {
+	tag: 'cohortFilter',
+	type: 'tvs',
+	tvs: {
+		term: { name: 'Cohort', type: 'multivalue', id: 'subcohort', isleaf: false, groupsetting: { disabled: true } },
+		values: [{ key: 'ABC', label: 'ABC' }]
+	}
+}
 
 const fCat = {
 	lst: [
+		{
+			type: 'tvs',
+			tvs: {
+				term: catTerm,
+				values: [{ key: 'A' }]
+			}
+		}
+	]
+}
+
+const fCohort = { lst: [cohortTvs] }
+
+const fCatCohort = {
+	lst: [
+		cohortTvs,
 		{
 			type: 'tvs',
 			tvs: {
@@ -47,21 +72,28 @@ tape('getFilterName', test => {
 	test.timeoutAfter(100)
 	//test.plan(3)
 
-	// combined string is short enough for term name to be included
-	test.equal(getFilterName(fCat), catTerm.name + ': Aname')
+	test.equal(getFilterName(fCohort), 'No filter', 'filter with just cohortFilter is shown as empty')
 
-	catTerm.name = 'Term11111112222222' // name too long and it no longer appears
-	test.equal(getFilterName(fCat), 'Aname')
+	test.equal(
+		getFilterName(fCat),
+		catTerm.name + ': Aname',
+		'combined string is short enough for term name to be included'
+	)
+
+	catTerm.name = 'Term11111112222222'
+	test.equal(getFilterName(fCat), 'Aname', 'combined name is too long thus only show category name')
+
+	test.equal(getFilterName(fCatCohort), 'Aname', 'tvs with tag=cohortFilter is ignored')
 
 	test.equal(getFilterName(fInt), '10<x<20')
 
 	test.equal(getFilterName(fFloat), '1.1<x<2.6')
 
-	fFloat.lst[0].tvs.term.type = 'geneExpression' // pretend the floating range filter is gene exp
-	test.equal(getFilterName(fFloat), '1.1<x<2.6') // and it's treated same as float
+	fFloat.lst[0].tvs.term.type = 'geneExpression'
+	test.equal(getFilterName(fFloat), '1.1<x<2.6', 'geneExpression tvs')
 
-	fFloat.lst[0].tvs.term.type = 'metaboliteIntensity' // pretend the floating range filter is metabolite
-	test.equal(getFilterName(fFloat), '1.1<x<2.6') // and it's treated same as float
+	fFloat.lst[0].tvs.term.type = 'metaboliteIntensity'
+	test.equal(getFilterName(fFloat), '1.1<x<2.6', 'metaboliteIntensity tvs')
 
 	test.end()
 })
