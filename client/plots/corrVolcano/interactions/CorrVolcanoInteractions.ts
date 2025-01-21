538a8c48bbd666b92b3b18cb70b6ba89a3f1e4cc
Colleen Reilly <creilly@stjude.org>
2025-01-21 12:28:50 -0600

    Enable launching the sample scatter plot by clicking on the dot

--- client/plots/corrVolcano/interactions/CorrVolcanoInteractions.ts
+++ client/plots/corrVolcano/interactions/CorrVolcanoInteractions.ts
@@ -1,9 +1,31 @@
 export class CorrVolcanoInteractions {
 	app: any
 	dom: any
-	constructor(app, dom) {
+	id: string
+	variableTwLst: any
+	constructor(app, dom, id, variableTwLst) {
 		this.app = app
 		this.dom = dom
+		this.id = id
+		//TODO: should be in the state somehow
+		this.variableTwLst = variableTwLst
+	}
+
+	//When clicking on dot, launch the sample scatter by gene and drug
+	launchSampleScatter(item) {
+		const config = this.app.getState()
+		const plot = config.plots.find(p => p.id === this.id)
+		const scatterConfig = {
+			chartType: 'sampleScatter',
+			name: 'Sample Scatter',
+			term: { term: plot.featureTw.term },
+			term2: this.variableTwLst.find((t: any) => t.$id === item.tw$id).term,
+			filter: config.termfilter.filter
+		}
+		this.app.dispatch({
+			type: 'plot_create',
+			config: scatterConfig
+		})
 	}
 
 	clearDom() {
