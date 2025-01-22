950dc9156244e200c7ad527d7c1cf595683ee91f
Colleen Reilly <creilly@stjude.org>
2025-01-22 11:41:09 -0600

    New unit tests for the corr volcano view model

---
+++ client/plots/corrVolcano/test/corrVolcano.unit.spec.ts
@@ -0,0 +1,174 @@
+import tape from 'tape'
+import { ViewModel } from '../viewModel/ViewModel'
+import type { CorrVolcanoSettings } from '../CorrelationVolcanoTypes'
+
+/**
+ * Tests
+ *   - Default ViewModel
+ */
+
+const mockData = {
+	variableItems: [
+		{
+			tw$id: 'test$id2',
+			sampleSize: 726,
+			correlation: 0.07,
+			original_pvalue: 0.0593,
+			adjusted_pvalue: 0.431
+		},
+		{
+			tw$id: 'test$id3',
+			sampleSize: 373,
+			correlation: -0.0088,
+			original_pvalue: 0.8657,
+			adjusted_pvalue: 0.8657
+		}
+	]
+}
+
+const mockSettings = {
+	antiCorrColor: '#ff0000',
+	corrColor: '#0000ff',
+	isAdjustedPValue: false,
+	method: 'pearson',
+	threshold: 0.05,
+	height: 500,
+	width: 500
+} satisfies CorrVolcanoSettings
+
+const mockVariableTwLst = [
+	{
+		$id: 'test$id2',
+		id: 'Asparaginase_normalizedLC50',
+		term: {
+			name: 'Asparaginase LC50 (normalized)'
+		},
+		type: 'NumTWRegularBin'
+	},
+	{
+		$id: 'test$id3',
+		id: 'Bortezomib_normalizedLC50',
+		term: {
+			name: 'Bortezomib LC50 (normalized)'
+		},
+		type: 'NumTWRegularBin'
+	}
+]
+
+const mockConfig = {
+	id: '1',
+	chartType: 'correlationVolcano',
+	featureTw: {
+		$id: 'test$id',
+		term: {
+			id: 'testid',
+			type: 'geneExpression',
+			gene: 'KRAS',
+			name: 'KRAS'
+		},
+		q: {}
+	},
+	settings: {
+		correlationVolcano: mockSettings
+	}
+}
+
+/**************
+ test sections
+***************/
+
+tape('\n', function (test) {
+	test.pass('-***- plots/correlationVolcano -***-')
+	test.end()
+})
+
+tape('Default ViewModel', test => {
+	test.timeoutAfter(100)
+
+	const viewModel = new ViewModel(mockConfig, mockData, mockSettings, mockVariableTwLst as any)
+	test.equal(viewModel.defaultMinRadius, 5, `Should set default minimum radius to 5`)
+	test.equal(viewModel.defaultMaxRadius, 20, `Should set default maximum radius to 20`)
+	test.equal(viewModel.bottomPad, 60, `Should set bottom padding to 60`)
+	test.equal(viewModel.horizPad, 70, `Should set horizontal padding to 70`)
+	test.equal(viewModel.topPad, 40, `Should set top padding to 40`)
+
+	const expectedViewData = {
+		plotDim: {
+			svg: { height: 660, width: 640 },
+			title: { text: 'KRAS Gene Expression', x: 320, y: 20 },
+			xAxisLabel: { x: 320, y: 600 },
+			yAxisLabel: { x: 23.333333333333332, y: 290 },
+			xScale: { x: 70, y: 540 },
+			yScale: { x: 70, y: 40 },
+			divideLine: { x: 320, y1: 540, y2: 40 },
+			thresholdLine: { y: 55.15435619536254, x1: 70, x2: 570 }
+		},
+		variableItems: [
+			{
+				tw$id: 'test$id2',
+				sampleSize: 726,
+				correlation: 0.07,
+				original_pvalue: 0.0593,
+				adjusted_pvalue: 0.431,
+				transformed_original_pvalue: 1.2269453066357374,
+				color: '#0000ff',
+				label: 'Asparaginase LC50 (normalized)',
+				x: 547.2727272727273,
+				y: 81.66666666666669,
+				radius: 20
+			},
+			{
+				tw$id: 'test$id3',
+				sampleSize: 373,
+				correlation: -0.0088,
+				original_pvalue: 0.8657,
+				adjusted_pvalue: 0.8657,
+				transformed_original_pvalue: 0.06263258248271039,
+				color: '#ff0000',
+				label: 'Bortezomib LC50 (normalized)',
+				x: 291.42857142857144,
+				y: 498.3333333333333,
+				radius: 5
+			}
+		],
+		legendData: [
+			{ label: 373, x: 25, y: 30, radius: 5 },
+			{ label: 726, x: 25, y: 70, radius: 20 }
+		]
+	}
+
+	test.deepEqual(
+		viewModel.viewData.plotDim.svg,
+		expectedViewData.plotDim.svg,
+		`Should set the svg dimensions as expected`
+	)
+	test.deepEqual(viewModel.viewData.plotDim.title, expectedViewData.plotDim.title, `Should set the title as expected`)
+	test.deepEqual(
+		viewModel.viewData.plotDim.xAxisLabel,
+		expectedViewData.plotDim.xAxisLabel,
+		`Should set the x-axis label as expected`
+	)
+	test.deepEqual(
+		viewModel.viewData.plotDim.yAxisLabel,
+		expectedViewData.plotDim.yAxisLabel,
+		`Should set the y-axis label as expected`
+	)
+	test.deepEqual(
+		viewModel.viewData.plotDim.divideLine,
+		expectedViewData.plotDim.divideLine,
+		`Should set the divide line as expected`
+	)
+	test.deepEqual(
+		viewModel.viewData.plotDim.thresholdLine,
+		expectedViewData.plotDim.thresholdLine,
+		`Should set the threshold line as expected`
+	)
+	test.deepEqual(
+		viewModel.viewData.variableItems,
+		expectedViewData.variableItems,
+		`Should set the variable items as expected`
+	)
+	test.deepEqual(viewModel.viewData.legendData, expectedViewData.legendData, `Should set the legend data as expected`)
+
+	test.end()
+})
