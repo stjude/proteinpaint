d9b014f2c9af65434ad3fb5e2c38754316ed2ca7
Colleen Reilly <creilly@stjude.org>
2025-01-23 16:38:34 -0600

    Initial effort to create a reusable circle reference component for plot legends

---
+++ client/dom/test/LegendCircleReference.unit.spec.ts
@@ -0,0 +1,42 @@
+import tape from 'tape'
+import { select } from 'd3-selection'
+import { LegendCircleReference } from '#dom'
+
+/*************************
+ reusable helper functions
+**************************/
+function getHolder() {
+	return select('body').append('div').style('max-width', '800px').style('border', '1px solid #555')
+}
+
+/**************
+ test sections
+***************/
+tape('\n', function (test) {
+	test.pass('-***- dom/LegendCircleReference -***-')
+	test.end()
+})
+
+tape('Default LegendCircleReference', test => {
+	test.timeoutAfter(100)
+
+	const holder: any = getHolder()
+	const svg = holder.append('svg').attr('width', 300).attr('height', 300)
+
+	new LegendCircleReference({
+		g: svg.append('g').attr('transform', 'translate(30, 30)'),
+		inputMax: 4,
+		inputMin: 0.5,
+		isAscending: true,
+		maxRadius: 20,
+		minRadius: 5,
+		prompt: 'Prompt',
+		title: 'Test',
+		x: 40,
+		y: 40
+	})
+
+	// test.equal(ui.g.selectAll('*').size(), 0, 'Should render 0 elements')
+
+	test.end()
+})
