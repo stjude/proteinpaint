c65bab3d15969112b5175ca35d6f598269e8d6af
aacic <aacic@stjude.org>
2025-01-27 15:00:57 -0600

    Revert wsimagesloaded.ts

--- client/plots/wsiviewer/wsimagesloaded.ts
+++ client/plots/wsiviewer/wsimagesloaded.ts
@@ -3,8 +3,8 @@ import { dofetch3 } from '#common/dofetch'
 export default async function wsiViewerImageFiles(o) {
 	const data = await dofetch3('samplewsimages', {
 		body: {
-			genome: o.app.opts.state.genome,
-			dslabel: o.app.opts.state.dslabel,
+			genome: o.app.opts.state.vocab.genome,
+			dslabel: o.app.opts.state.vocab.dslabel,
 			sample_id: o.app.opts.state.sample_id
 		}
 	})
