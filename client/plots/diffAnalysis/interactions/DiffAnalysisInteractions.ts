9678595880152744ed430190d7036a89a75f9e78
Colleen Reilly <creilly@stjude.org>
2025-02-20 12:43:55 -0600

    Enable downloading the plot or the p value table

--- client/plots/diffAnalysis/interactions/DiffAnalysisInteractions.ts
+++ client/plots/diffAnalysis/interactions/DiffAnalysisInteractions.ts
@@ -1,9 +1,35 @@
+import { downloadTable } from '#dom'
+import { to_svg } from '#src/client'
 import type { DiffAnalysisDom } from '../DiffAnalysisTypes'
 
 export class DiffAnalysisInteractions {
 	dom: DiffAnalysisDom
+	pValueTableData: any
 	constructor(dom: DiffAnalysisDom) {
 		this.dom = dom
+		this.pValueTableData = []
+	}
+
+	download() {
+		this.dom.tip.clear().showunder(this.dom.controls.select('div').node())
+		const opts = [
+			{
+				text: 'Download plot',
+				callback: () => {
+					const svg = this.dom.svg.node() as Node
+					to_svg(svg, `boxplot`, { apply_dom_styles: true })
+				}
+			},
+			{
+				text: 'Download p value table',
+				callback: () => {
+					downloadTable(this.pValueTableData.rows, this.pValueTableData.columns)
+				}
+			}
+		]
+		for (const opt of opts) {
+			this.dom.tip.d.append('div').attr('class', 'sja_menuoption').text(opt.text).on('click', opt.callback)
+		}
 	}
 
 	clearDom() {
