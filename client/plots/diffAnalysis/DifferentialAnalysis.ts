ed62c168e88fac5af50dde43a1f51e6081d4a4d1
Colleen Reilly <creilly@stjude.org>
2025-02-20 11:47:10 -0600

    Implemented basic controls

--- client/plots/diffAnalysis/DifferentialAnalysis.ts
+++ client/plots/diffAnalysis/DifferentialAnalysis.ts
@@ -2,7 +2,7 @@ import type { BasePlotConfig, MassAppApi, MassState } from '#mass/types/mass'
 import { RxComponentInner } from '../../types/rx.d'
 import { getCompInit, copyMerge } from '#rx'
 import { Menu } from '#dom'
-// import { controlsInit } from '../controls'
+import { controlsInit } from '../controls'
 import type { DiffAnalysisDom, DiffAnalysisOpts, DiffAnalysisSettings } from './DiffAnalysisTypes'
 import { DiffAnalysisInteractions } from './interactions/DiffAnalysisInteractions'
 import { Model } from './model/Model'
@@ -22,7 +22,7 @@ export class DifferentialAnalysis extends RxComponentInner {
 		}
 		const holder = opts.holder.classed('sjpp-diff-analysis-main', true)
 		const controls = opts.controls ? holder : holder.append('div')
-		const div = holder.append('div').style('padding', '5px')
+		const div = holder.append('div').style('padding', '5px').style('display', 'inline-block')
 		const errorDiv = div.append('div').attr('id', 'sjpp-diff-analysis-error').style('opacity', 0.75)
 		const svg = div.append('svg').style('display', 'inline-block').attr('id', 'sjpp-diff-analysis-svg')
 		this.dom = {
@@ -30,9 +30,11 @@ export class DifferentialAnalysis extends RxComponentInner {
 			div,
 			error: errorDiv,
 			svg,
+			xAxis: svg.append('g').attr('id', 'sjpp-diff-analysis-xAxis'),
+			yAxis: svg.append('g').attr('id', 'sjpp-diff-analysis-yAxis'),
 			xAxisLabel: svg.append('text').attr('id', 'sjpp-diff-analysis-xAxisLabel').attr('text-anchor', 'middle'),
 			yAxisLabel: svg.append('text').attr('id', 'sjpp-diff-analysis-yAxisLabel').attr('text-anchor', 'middle'),
-			plot: svg.append('rect').attr('id', 'sjpp-diff-analysis-plot'),
+			plot: svg.append('g').attr('id', 'sjpp-diff-analysis-plot'),
 			tip: new Menu({ padding: '' })
 		}
 
@@ -64,34 +66,119 @@ export class DifferentialAnalysis extends RxComponentInner {
 		}
 	}
 
+	async setControls() {
+		const inputs = [
+			{
+				label: 'Minimum Read Count',
+				type: 'number',
+				chartType: 'differentialAnalysis',
+				settingsKey: 'minCount',
+				title: 'The smallest number of reads required for a gene to be considered in the analysis',
+				min: 0,
+				max: 10000
+			},
+			{
+				label: 'Minimum Total Read Count',
+				type: 'number',
+				chartType: 'differentialAnalysis',
+				settingsKey: 'minTotalCount',
+				title: 'The smallest total number of reads required for a gene to be considered in the analysis',
+				min: 0,
+				max: 10000
+			},
+			{
+				label: 'P value Significance (Linear Scale)',
+				type: 'number',
+				chartType: 'differentialAnalysis',
+				settingsKey: 'pValue',
+				title: 'The p-value threshold to determine statistical significance',
+				min: 0,
+				max: 1
+			},
+			{
+				label: 'Fold Change (Log Scale)',
+				type: 'number',
+				chartType: 'differentialAnalysis',
+				settingsKey: 'foldChangeCutoff',
+				title: 'The fold change threshold to determine biological significance',
+				min: -10,
+				max: 10
+			},
+			{
+				label: 'P value',
+				type: 'radio',
+				chartType: 'differentialAnalysis',
+				settingsKey: 'pValueType',
+				title: 'Toggle between original and adjusted pvalues for volcano plot',
+				options: [
+					{ label: 'adjusted', value: 'adjusted' },
+					{ label: 'original', value: 'original' }
+				]
+			},
+			{
+				label: 'Variable Genes Cutoff',
+				type: 'number',
+				chartType: 'differentialAnalysis',
+				settingsKey: 'varGenesCutoff',
+				title: 'Top number of genes with the highest variability to include in analysis',
+				min: 1000,
+				max: 4000
+			},
+			{
+				label: 'Show P value Table',
+				type: 'checkbox',
+				chartType: 'differentialAnalysis',
+				settingsKey: 'showPValueTable',
+				title: 'Show table with both original and adjusted p values for all significant genes',
+				boxLabel: ''
+			}
+		]
+
+		this.components.controls = await controlsInit({
+			app: this.app,
+			id: this.id,
+			holder: this.dom.controls.attr('class', 'pp-termdb-plot-controls').style('display', 'inline-block'),
+			inputs
+		})
+	}
+
+	async init() {
+		await this.setControls()
+	}
+
 	async main() {
 		const config = structuredClone(this.state.config)
 		if (config.chartType != this.type) return
 
-		const model = new Model(this.app, config, config.settings.differentialAnalysis)
-		const response = await model.getData()
-		if (!response || response.error) {
-			this.interactions.clearDom()
-			this.dom.error.text(response.error || 'No data returned from server')
+		try {
+			const settings = config.settings.differentialAnalysis
+			/** Fetch data */
+			const model = new Model(this.app, config, settings)
+			const response = await model.getData()
+			if (!response || response.error) {
+				this.interactions.clearDom()
+				this.dom.error.text(response.error || 'No data returned from server')
+			}
+			if (this.dom.header) {
+				const samplelst = config.samplelst.groups
+				this.dom.header.title.text(`${samplelst[0].name} vs ${samplelst[1].name} `)
+			}
+			/** Format response into an object for rendering */
+			const view = new ViewModel(config, response, settings)
+			/** Render formatted data */
+			new View(this.dom, this.interactions, view.viewData)
+		} catch (e: any) {
+			if (e instanceof Error) console.error(e.message || e)
+			else if (e.stack) console.log(e.stack)
+			throw e
 		}
-
-		if (this.dom.header) {
-			const samplelst = config.samplelst.groups
-			this.dom.header.title.text(`${samplelst[0].name} vs ${samplelst[1].name} `)
-		}
-
-		const settings = config.settings.differentialAnalysis
-		/** Format response into an object for rendering */
-		const view = new ViewModel(config, response, settings)
-		/** Render formatted data */
-		new View(this.dom, this.interactions, view.viewData)
 	}
 }
 
 export const DiffAnalysisInit = getCompInit(DifferentialAnalysis)
 export const componentInit = DiffAnalysisInit
 
-function getDefaultDiffAnalysisSettings(overrides: Partial<DiffAnalysisSettings>): DiffAnalysisSettings {
+function getDefaultDiffAnalysisSettings(overrides = {}): DiffAnalysisSettings {
 	const defaults: DiffAnalysisSettings = {
 		foldChangeCutoff: 0,
 		height: 400,
@@ -99,6 +186,7 @@ function getDefaultDiffAnalysisSettings(overrides: Partial<DiffAnalysisSettings>
 		minTotalCount: 15,
 		pValue: 0.05,
 		pValueType: 'adjusted',
+		showPValueTable: false,
 		varGenesCutoff: 3000,
 		width: 400
 	}
--- client/plots/diffAnalysis/interactions/DiffAnalysisInteractions.ts
+++ client/plots/diffAnalysis/interactions/DiffAnalysisInteractions.ts
@@ -7,7 +7,10 @@ export class DiffAnalysisInteractions {
 	}
 
 	clearDom() {
+		this.dom.div.selectAll('table').remove()
 		this.dom.plot.selectAll('*').remove()
+		this.dom.xAxis.selectAll('*').remove()
 		this.dom.yAxisLabel.text('')
+		this.dom.yAxis.selectAll('*').remove()
 	}
 }
