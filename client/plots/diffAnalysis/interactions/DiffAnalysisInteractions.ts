6fd81860d216274a95da41c734d26246828f1d13
Colleen Reilly <creilly@stjude.org>
2025-02-20 14:05:09 -0600

    Implemented action button above the plot to launch gsea, mv'ing option out of config controls

--- client/plots/diffAnalysis/interactions/DiffAnalysisInteractions.ts
+++ client/plots/diffAnalysis/interactions/DiffAnalysisInteractions.ts
@@ -1,13 +1,23 @@
+import type { MassAppApi } from '#mass/types/mass'
 import { downloadTable } from '#dom'
 import { to_svg } from '#src/client'
 import type { DiffAnalysisDom } from '../DiffAnalysisTypes'
 
 export class DiffAnalysisInteractions {
+	app: MassAppApi
 	dom: DiffAnalysisDom
+	id: string
 	pValueTableData: any
-	constructor(dom: DiffAnalysisDom) {
+	constructor(app: MassAppApi, id: string, dom: DiffAnalysisDom) {
+		this.app = app
 		this.dom = dom
+		this.id = id
 		this.pValueTableData = []
+	}
+
+	setVar(app, id) {
+		this.app = app
+		this.id = id
 	}
 
 	download() {
@@ -32,8 +42,47 @@ export class DiffAnalysisInteractions {
 		}
 	}
 
+	launchGSEA(value: string, foldChangeCutoff: number, data: any) {
+		const sample_genes: any = []
+		const background_genes: any = []
+
+		for (const d of data) {
+			if (d.gene_symbol.length > 0) {
+				// Do not include blank rows
+				if (value == 'upregulated' && foldChangeCutoff < Math.abs(d.fold_change) && d.fold_change > 0) {
+					sample_genes.push(d.gene_symbol)
+				}
+				if (value == 'downregulated' && foldChangeCutoff < Math.abs(d.fold_change) && d.fold_change < 0) {
+					sample_genes.push(d.gene_symbol)
+				}
+				if (value == 'both' && foldChangeCutoff < Math.abs(d.fold_change)) {
+					sample_genes.push(d.gene_symbol)
+				}
+				background_genes.push(d.gene_symbol)
+			}
+		}
+
+		const geneORAparams = {
+			sample_genes: sample_genes.toString(),
+			background_genes: background_genes.toString(),
+			genome: this.app.vocabApi.opts.state.vocab.genome,
+			ora_request_type: value,
+			num_samples_genes: sample_genes.length,
+			num_background_genes: background_genes.length
+		}
+
+		this.app.dispatch({
+			type: 'plot_create',
+			config: {
+				chartType: 'geneORA',
+				geneORAparams
+			}
+		})
+	}
+
 	clearDom() {
 		this.dom.div.selectAll('table').remove()
+		this.dom.actions.selectAll('*').remove()
 		this.dom.plot.selectAll('*').remove()
 		this.dom.xAxis.selectAll('*').remove()
 		this.dom.yAxisLabel.text('')
