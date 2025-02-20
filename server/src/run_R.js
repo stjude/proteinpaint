43be97bca1c99794d8c8cbc71d69d61273a1e2cb
Andrew Willems <andrew.willems@stjude.org>
2025-02-20 12:43:47 -0600

    Putting back original run_R.js from master

--- server/src/run_R.js
+++ server/src/run_R.js
@@ -11,95 +11,56 @@ Standard output of the R script is returned.
 */
 
 import fs from 'fs'
-import serverconfig from './serverconfig.js'
+import serverconfig from './serverconfig'
 import { spawn } from 'child_process'
 import { Readable } from 'stream'
 
 export default async function run_R(path, data, args) {
 	try {
 		await fs.promises.stat(path)
-	} catch (_e) {
+	} catch (e) {
 		throw `${path} does not exist`
 	}
-
 	return new Promise((resolve, reject) => {
 		const _stdout = []
 		const _stderr = []
 		// spawn R child process
 		const sp = spawn(serverconfig.Rscript, args ? [path, ...args] : [path])
-
-		// Handle process errors
-		sp.on('error', err => {
-			sp.stdin.end() // Ensure stdin is closed
-			reject(err)
-		})
-
 		if (data) {
 			// stream input data into R
 			try {
-				// Create input stream from data array
 				const input = data.endsWith('\n') ? data : data + '\n' // R expects a final end-of-line marker
-				// At this point we know data is an array due to validation above
-				// const input = data.join('\n') + '\n'
-				const readableStream = Readable.from(input)
-
-				// Handle stream errors
-				readableStream.on('error', err => {
-					sp.stdin.end()
-					reject(err)
-				})
-
-				// Pipe with proper error handling
-				readableStream.pipe(sp.stdin)
-
-				// Handle pipe finish
-				readableStream.on('end', () => {
-					sp.stdin.end()
-				})
+				Readable.from(input).pipe(sp.stdin)
 			} catch (e) {
-				sp.stdin.end()
 				sp.kill()
 				let errmsg = e
 				const stderr = _stderr.join('').trim()
 				if (stderr) errmsg += `\nR stderr: ${stderr}`
 				reject(errmsg)
 			}
-		} else if (args && args.length > 0) {
-			try {
-				const fileContent = fs.readFileSync(args[0], 'utf8')
-				sp.stdin.write(fileContent)
-				sp.stdin.end() // Explicitly end stdin after writing
-			} catch (_e) {
-				sp.stdin.end()
-				sp.kill()
-				reject(`Error reading file: ${_e.message}`)
-			}
-		} else {
-			// No input data - close stdin immediately
-			sp.stdin.end()
 		}
-
-		// Collect output
+		// store stdout and stderr from R
 		sp.stdout.on('data', data => _stdout.push(data))
 		sp.stderr.on('data', data => _stderr.push(data))
-
-		// Handle process completion
+		sp.on('error', err => reject(err))
+		// return stdout and stderr when R process closes
 		sp.on('close', code => {
 			const stdout = _stdout.join('').trim()
 			const stderr = _stderr.join('').trim()
-
 			if (code !== 0) {
+				// handle non-zero exit status
 				let errmsg = `R process exited with non-zero status code=${code}`
 				if (stdout) errmsg += `\nR stdout: ${stdout}`
 				if (stderr) errmsg += `\nR stderr: ${stderr}`
-				return reject(errmsg)
+				reject(errmsg)
 			}
-
 			if (stderr) {
-				return reject(`R process emitted standard error\nR stderr: ${stderr}`)
+				// handle R stderr
+				const errmsg = `R process emitted standard error\nR stderr: ${stderr}`
+				reject(errmsg)
 			}
-
-			resolve(stdout ? stdout.split('\n') : [])
+			// return standard out from R
+			resolve(stdout)
 		})
 	})
 }
