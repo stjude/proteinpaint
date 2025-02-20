896ffd8a17a2e181d0d496cf01eb7f8cded53fb7
Airen Zaldivar <airenzp@gmail.com>
2025-02-20 15:01:45 -0600

    Removed print

--- server/src/termdb.matrix.js
+++ server/src/termdb.matrix.js
@@ -161,7 +161,6 @@ async function getSampleData(q, ds, onlyChildren = false) {
 				filter0: q.filter0
 			}
 			const data = await q.ds.queries[tw.term.type].get(args)
-			console.log('data', data)
 			for (const sampleId in data.term2sample2value[tw.term.name]) {
 				if (!(sampleId in samples)) {
 					samples[sampleId] = { sample: sampleId }
