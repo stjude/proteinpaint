54554e155769fe4334ddaf45688e500af91f5e0a
robinpaul85 <robinpaul85@gmail.com>
2025-02-21 15:25:14 -0600

    R/rust scripts do not strictly output JSON

--- server/routes/correlationVolcano.ts
+++ server/routes/correlationVolcano.ts
@@ -105,9 +105,21 @@ async function compute(q: CorrelationVolcanoRequest, ds: any, genome: any) {
 	//})
 
 	const time1 = Date.now()
-	const output = {
-		terms: JSON.parse(await run_R(path.join(serverconfig.binpath, 'utils', 'corr.R'), JSON.stringify(input)))
+	let r_json
+	const r_output = await run_R(path.join(serverconfig.binpath, 'utils', 'corr.R'), JSON.stringify(input))
+	for (const line of r_output.split('\n')) {
+		// Split lines using new line character
+		if (line.startsWith('output_string:')) {
+			r_json = JSON.parse(line.replace('output_string:', ''))
+		} else {
+			// Prints other lines being printed in the R code for testing/debugging
+			mayLog(line)
+		}
 	}
+	const output = {
+		terms: r_json
+	}
+
 	mayLog('Time taken to run correlation analysis:', Date.now() - time1)
 	for (const t of output.terms) {
 		const t2 = {
