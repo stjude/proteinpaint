2ec8b4d545dbcc5df678ad9c013f5d642f307ed3
Aleksandar Acic <aacic@stjude.org>
2025-03-04 11:37:12 -0600

    Update container dev image.

---
+++ container/dev/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DEFAULT_TAG="latest"
+TAG="${1:-$DEFAULT_TAG}"
+
+PLATFORM=""
+ARCH=$( uname -m )
+if [[ ${ARCH} == "arm64" ]]; then
+	ARCH="aarch64";
+	PLATFORM="--platform=linux/arm64"
+fi
+
+# The podman buildx build command
+docker buildx build . \
+  --file ./Dockerfile \
+  --tag "ghcr.io/stjude/devcontainer:$TAG" \
+  $PLATFORM \
+  --build-arg ARCH="$ARCH"
