47cfae3780e62e8d38046892ccefea5483288c81
siosonel <siosonel@gmail.com>
2025-03-04 17:24:48 -0600

    use puppeteer instead of tape:run for client unit test

--- client/test.sh
+++ client/test.sh
@@ -10,22 +10,29 @@ if [[ "$NAMEPATTERN" == *"integration"* ]]; then
 	./test/pretest.js $TESTHOST
 fi
 
+if [[ ! -f "./test/tape.bundle.js" ]]; then
+	# NOTES:
+  # - webpack bundling is still needed in github CI, but not for local dev/test spec bundles
+	# - assume that the tape lib rarely changes in local testing environment;
+	#   this does not affect the CI environment, where the runner will install
+	#   from a freshly cloned repo and will always have to create the tape.bundle
+	#
+	# TODO: 
+	# - use a good esbuild node polyfill plugin to avoid having to use webpack,
+	#   to bundle and supply tape lib with missing node libs
+	npx webpack --config=./webpack.tape.config.mjs
+fi
+
 rm -rf ../public/bin/test
 
 TESTFILE=test/internals-test.js
 node emitImports.mjs $NAMEPATTERN > ./$TESTFILE
 
-if [[ ! -f "./test/tape.bundle.js" ]]; then 
-	# assume that the tape lib rarely changes in local testing environment;
-	# this does not affect the CI environment, where the runner will install
-	# from a freshly cloned repo and will always have to create the tape.bundle
-	# TODO: 
-	# use a good esbuild node polyfill plugin to avoid having to use webpack,
-	# to bundle and supply tape lib with missing node libs  
-	npx webpack --config=./webpack.tape.config.mjs
-fi
-
-
 ENV=test node esbuild.config.mjs
-INITJS="window.testHost='$TESTHOST';import('/bin/test/_.._/test/internals-test.js');"
-echo "$INITJS" | npx tape-run --static ../public
+
+if [[ "$NAMEPATTERN" == *"unit"* ]]; then
+	npm run test:puppet
+else
+	INITJS="window.testHost='$TESTHOST';import('/bin/test/_.._/test/internals-test.js');"
+	echo "$INITJS" | npx tape-run --static ../public
+fi
