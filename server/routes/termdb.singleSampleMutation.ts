c43c1df0500a207de38fae36c728700c5461cdab
Xin Zhou <xzhou82@gmail.com>
2025-03-06 14:46:36 -0600

    delete old code

--- server/routes/termdb.singleSampleMutation.ts
+++ server/routes/termdb.singleSampleMutation.ts
@@ -46,14 +46,9 @@ export async function validate_query_singleSampleMutation(ds: any, genome: any) 
 		gdcValidate_query_singleSampleMutation(ds, genome)
 	} else if (_q.src == 'native') {
 		// using a folder to store text files for individual samples
-		// file names are integer sample id
+		// file names are string sample name
 		_q.get = async (q: TermdbSingleSampleMutationRequest) => {
-			/* as mds3 client may not be using integer sample id for now,
-			the argument is string id and has to be mapped to integer id
-			*/
-			const fileName = q.sample
-
-			const file = path.join(serverconfig.tpmasterdir, _q.folder, fileName.toString())
+			const file = path.join(serverconfig.tpmasterdir, _q.folder, q.sample)
 			try {
 				await fs.promises.stat(file)
 			} catch (e: any) {
