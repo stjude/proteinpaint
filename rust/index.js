8841067331920ecedaea5bb88a4eae20518a7c7e
Edgar <siosonel@gmail.com>
2025-03-14 22:54:37 -0500

    detect and kill long running pp rust processes (#2959)
    * detect and kill long running pp rust processes
    
    * comment out testing console.log's in rust/index
    
    * do not trigger the interval to kill long-running rust processes outside of a container, as it would prevent test CI from closing properly
    
    * only run detectAndKill interval when gdcmaf route is called, implying gdd dataset and not being in CI environment; fix "target/gdcmaf" to "target/release/gdcmaf"

--- rust/index.js
+++ rust/index.js
@@ -1,7 +1,10 @@
 const path = require('path'),
-	spawn = require('child_process').spawn,
+	{ spawn, exec } = require('child_process'),
 	Readable = require('stream').Readable,
-	Transform = require('stream').Transform
+	Transform = require('stream').Transform,
+	{ promisify } = require('util')
+
+const execPromise = promisify(exec)
 
 exports.run_rust = function (binfile, input_data) {
 	return new Promise((resolve, reject) => {
@@ -43,8 +46,15 @@ exports.run_rust = function (binfile, input_data) {
 	})
 }
 
+let detectInterval
+
 exports.stream_rust = function (binfile, input_data, emitJson) {
 	const binpath = path.join(__dirname, '/target/release/', binfile)
+	// we only want to run this interval loop inside a container, not in dev/test CI
+	if (!detectInterval && binfile == 'gdcmaf') {
+		detectInterval = setInterval(detectAndKillLongRunningRustProcess, 60000) // in millseconds
+	}
+
 	const ps = spawn(binpath)
 	const stderr = []
 	try {
@@ -54,11 +64,10 @@ exports.stream_rust = function (binfile, input_data, emitJson) {
 		//reader.on('error', ps.stderr.pipe)
 		//return reader
 	} catch (error) {
-		ps.kill()
-		let errmsg = error
+		console.log(error)
+		//let errmsg = error
 		//if (stderr.length) errmsg += `killed run_rust('${binfile}'), stderr: ${stderr.join('').trim()}`
-		//reject(errmsg)
-		console.log(59, error)
+		//console.log(errmsg)
 	}
 
 	const childStream = new Transform({
@@ -67,15 +76,16 @@ exports.stream_rust = function (binfile, input_data, emitJson) {
 			callback()
 		}
 	})
+
 	ps.stdout.pipe(childStream)
 	ps.stderr.on('data', data => stderr.push(data))
-	ps.on('close', code => { //console.log(72, stderr.length)
+	ps.on('close', code => {
 		if (stderr.length) {
 			// handle rust stderr
 			const errors = stderr.join('').trim().split('\n').map(JSON.parse)
 			//const errmsg = `!!! stream_rust('${binfile}') stderr: !!!`
 			//console.log(errmsg, errors)
-			emitJson({errors})
+			emitJson({ errors })
 		} else {
 			emitJson({ ok: true, status: 'ok', message: 'Processing complete' })
 		}
@@ -83,9 +93,61 @@ exports.stream_rust = function (binfile, input_data, emitJson) {
 	ps.on('error', err => {
 		//console.log(74, `stream_rust().on('error')`, err)
 		const errors = stderr.join('').trim().split('\n').map(JSON.parse)
-		emitJson({errors})
+		emitJson({ errors })
 	})
-	// below will duplicate ps.on('close') event above
+
+	// displayRustProcesses(binpath, 1) // use only for testing
+
+	// on('end') will duplicate ps.on('close') event above
 	// childStream.on('end', () => console.log(`-- childStream done --`))
+	// this may duplicate ps.on('error'), unless the error happened within the transformation
+	childStream.on('error', err => console.log('stream_rust childStream.on(error)', err))
 	return childStream
+}
+
+// to test, run `sleep 5555` in 1 or more terminal windows,
+// which should be all killed when making a request to gdc/MafBuild
+// const srcpath = 'sleep 5555' // uncomment to test and comment out below
+const srcpath = path.join(__dirname, 'target/release/gdcmaf')
+const PROCESS_TIMEOUT = 5 * 60 // in seconds
+
+async function detectAndKillLongRunningRustProcess() {
+	try {
+		// console.log(`\n--- detecting and killing long running rust streams (>${PROCESS_TIMEOUT} seconds) using exec() ---`)
+		const ps = await execPromise(`ps -eo pid,etime,command | grep '${srcpath}'`, {
+			encoding: 'utf-8',
+			stdio: 'inherit'
+		})
+		if (!ps.stderr && typeof ps.stdout === 'string') {
+			const lines = ps.stdout.trim().split('\n')
+			for (const line of lines) {
+				if (!line || typeof line != 'string') continue
+				if (line.includes(srcpath)) {
+					const [pid, etime, ...rest] = line
+						.split(' ')
+						.filter(f => !!f)
+						.map(v => v.trim()) //; console.log(132, rest)
+					if (rest.includes('grep')) continue
+					const [sec, min, hour] = etime.split(':').reverse() //; console.log(134, etime.split(':').reverse(), line)
+					const elapsed = Number(sec) + 60 * Number(min) + 3600 * (Number(hour) || 0) //; console.log(127, {elapsed})
+					if (elapsed >= PROCESS_TIMEOUT) {
+						//console.log(136, 'killing process')
+						execPromise(`kill -9 ${pid}`, (_, out) => console.log(out))
+					}
+				}
+			}
+		}
+		// console.log('--- after detecting/killing long running process --')
+		// displayRustProcesses(srcpath)
+		// console.log('--- done detecting/killing long running process --\n')
+	} catch (e) {
+		//console.log(140, e)
+		// okay to not have any process to kill
+		//console.log(e)
+	}
+}
+
+function displayRustProcesses(binpath, killTimeout = 0) {
+	exec(`ps -eo comm,pid,etime | grep "${binpath}"`, { encoding: 'utf-8' }, (err, out) => console.log(out))
+	if (killTimeout) setTimeout(detectAndKillLongRunningRustProcess, killTimeout)
 }
