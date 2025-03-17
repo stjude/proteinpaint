688c8307941dbba290318a39de0a3743de20cba4
siosonel <siosonel@gmail.com>
2025-03-17 13:23:46 -0500

    assign cleared interval to undefined

--- rust/index.js
+++ rust/index.js
@@ -115,8 +115,8 @@ function trackByPid(pid, name, maxElapsed = 300000) {
 	trackedPids.set(pid, { name, expires: Date.now() + maxElapsed })
 	if (!psKillInterval) psKillInterval = setInterval(killExpiredProcess, PSKILL_INTERVAL_MS)
 	// uncomment below to test
-	// console.log([...trackedPids.entries()])
-	// setTimeout(killExpiredProcess, 5) // uncomment for testing only
+	console.log([...trackedPids.entries()])
+	setTimeout(killExpiredProcess, 5) // uncomment for testing only
 }
 
 //
@@ -139,13 +139,5 @@ function killExpiredProcess() {
 		} catch (err) {
 			console.log(`unable to kill ${label}`, err)
 		}
-	}
-
-	// psKillInterval should not run when there are no tracked pids,
-	// and restarted in trackByPid() as needed
-	try {
-		if (!trackedPids.size && psKillInterval) clearInterval(psKillInterval)
-	} catch (e) {
-		console.log(e)
 	}
 }
