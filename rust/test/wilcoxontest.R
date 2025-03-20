62fc6805956591b4c152063563d60fba6fe7386f
robinpaul85 <robinpaul85@gmail.com>
2025-03-20 11:49:35 -0500

    Added more documentation to wilcoxontest.R

--- rust/test/wilcoxontest.R
+++ rust/test/wilcoxontest.R
@@ -1,9 +1,12 @@
+# This script generates random values for two vectors for testing the rust wilcoxon test. This also generates the R wilcoxon p-value using wilcox.test(). Then the p-values from both implementations are plotted using plot_wilcoxon.R
 # Syntax: time Rscript wilcoxontest.R | ../target/release/wilcoxon
 library(jsonlite)
 
 generate_data <- function(iter) {
-    group1_values <- rnorm(500)  # Generate 20 random numbers from a normal distribution
-    group2_values <- runif(500)  # Generate 10 random numbers from a uniform distribution
+    n1 <- 500 # Generate n1 random numbers from a normal distribution
+    n2 <- 500    # Generate n2 random numbers from a uniform distribution
+    group1_values <- rnorm(n1)
+    group2_values <- runif(n2)
     pvalue <- wilcox.test(group1_values,group2_values)$p.value
     # Create a list to store the vectors
     my_df <- list(group1_id = paste0("group1_id_iter",iter), group1_values = group1_values, group2_id = paste0("group2_id_iter",iter), group2_values = group2_values, R_pvalue = pvalue)
@@ -12,7 +15,7 @@ generate_data <- function(iter) {
 }
 
 
-num_tests <- 1
+num_tests <- 1 # Increase the number to get more cases to be plotted by plot_wilcoxon.R
 initial_df <- list()
 for (i in 1:num_tests) {
     new_df <- generate_data(i)
