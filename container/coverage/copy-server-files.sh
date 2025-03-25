5623b7174760b172eec430ad4bab2c91d04b6764
siosonel <siosonel@gmail.com>
2025-03-24 22:02:43 -0500

    divide generate.sh script into smaller codes for easier reuse with docker build-push-action

---
+++ container/coverage/copy-server-files.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -exo pipefail
+
+##############################
+# COPY SOURCE CODE, ARTIFACTS
+##############################
+
+rm -rf server
+mkdir server
+
+# will use the ppserver:latest @sjcrh/proteinpaint-server/package.json,
+# which has the applicable abs path to packed tgz for updated workspaces 
+# cp -r ../../server/package.json server/
+cp -r ../../server/coverage.js server/
+cp -r ../ci/serverconfig.json server/
+cp -r ../../server/emitImports.js server/
+cp -r ../../server/routes server/
+cp -r ../../server/src server/
+cp -r ../../server/test server/
+cp -r ../../server/utils server/
+
+mkdir server/genome
+cp -r ../../server/genome/*.ts server/genome
+npx esbuild "server/genome/*.ts" --platform=node --outdir=server/genome --format=esm
+mkdir server/dataset
+cp -r ../../server/dataset/*.ts server/dataset
+npx esbuild "server/dataset/*.ts" --platform=node --outdir=server/dataset --format=esm
+
+serverParentDir=/home/runner/work/proteinpaint/proteinpaint
+if [[ ! -d "$serverParentDir" ]]; then
+  sed -i.bak "s|$serverParentDir\/|$PWD\/|g" server/serverconfig.json
+fi
