cf4b7db5c195b3523ab43dd37fbec4799626ef5b
siosonel <siosonel@gmail.com>
2025-03-29 12:37:29 -0500

    move augen test to augen/src/test

--- augen/src/test/augen.unit.spec.js
+++ augen/src/test/augen.unit.spec.js
@@ -1,18 +1,18 @@
-import { testApi } from '../src/tester.ts'
+import { testApi } from '#src/tester.ts'
 import { readdirSync } from 'fs'
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
-import * as checkers from './checkers/index.ts'
+import * as checkers from './toyApp/checkers/index.ts'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 runTests()
 
 async function runTests() {
-	const files = readdirSync(join(__dirname, './routes'))
+	const files = readdirSync(join(__dirname, './toyApp/routes'))
 	const endpoints = files.filter(f => f.endsWith('.ts'))
 	for (const f of endpoints) {
-		const route = await import(`./routes/${f}`)
+		const route = await import(`./toyApp/routes/${f}`)
 		testApi(route, f, checkers)
 	}
 }
