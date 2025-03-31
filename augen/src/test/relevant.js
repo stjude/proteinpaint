c42b64136c75b034ab45c572e6f7dc9c1ea26e74
siosonel <siosonel@gmail.com>
2025-03-31 14:59:59 -0500

    reuse runRelevantSpecs() for augen spec:coverage

--- augen/src/test/relevant.js
+++ augen/src/test/relevant.js
@@ -1,4 +1,5 @@
-import { getClosestSpec, emitRelevantSpecCovDetails, gitProjectRoot, publicSpecsDir } from '../closestSpec.js'
+import { getClosestSpec } from '../closestSpec.js'
+import { runRelevantSpecs } from '../runRelevantSpecs.js'
 import path from 'path'
 import tape from 'tape'
 import fs from 'fs'
@@ -6,69 +7,21 @@ import { execSync } from 'child_process'
 
 const dirname = path.join(import.meta.dirname, '../..')
 const relevantSubdirs = ['src']
-const opts = {
-	// changedFiles: ['tvs.js', 'tvs.categorical.js', 'tvs.numeric.js', 'FilterPrompt.js'].map(f => `client/filter/${f}`),
-	// changedFiles: ['handlers/snp.ts'].map(f => `client/termsetting/${f}`)
-	ignore: ['src/toyApp']
+
+if (process.argv.includes('-p')) {
+	const opts = {
+		workspace: 'augen',
+		specs: getRelevantAugenSpecs(),
+		dirname
+	}
+	runRelevantSpecs(opts)
 }
-
-export const reportDir = path.join(dirname, '/.coverage')
-
-export const extractFiles = {
-	html: `${publicSpecsDir}/augen-relevant.html`,
-	markdown: `${publicSpecsDir}/augen-relevant.md`
-}
-
-if (process.argv.includes('-p')) runRelevantSpecs()
 
 export function getRelevantAugenSpecs() {
+	const opts = {
+		// changedFiles: ['tvs.js', 'tvs.categorical.js', 'tvs.numeric.js', 'FilterPrompt.js'].map(f => `client/filter/${f}`),
+		// changedFiles: ['handlers/snp.ts'].map(f => `client/termsetting/${f}`)
+		ignore: ['src/toyApp']
+	}
 	return getClosestSpec(dirname, relevantSubdirs, opts)
-}
-
-async function runRelevantSpecs() {
-	const publicSpecsAugenDir = `${publicSpecsDir}/augen`
-	if (fs.existsSync(publicSpecsAugenDir)) fs.rmSync(publicSpecsAugenDir, { force: true, recursive: true })
-
-	const specs = getRelevantAugenSpecs()
-	if (!specs.matched.length) {
-		if (Object.keys(specs.matchedByFile).length) {
-			// TODO: may require a matching spec for all relevant files
-			console.log('No matching spec for changed files')
-		}
-		process.exit(0)
-	}
-
-	const c8opts = `--all --src=src --experimental-monocart -r=v8 -r=html -r=json -r=markdown-summary -r=markdown-details -o=./.coverage`
-
-	try {
-		const html = []
-		const markdowns = []
-		let title
-		for (const spec of specs.matched) {
-			fs.rmSync(reportDir, { force: true, recursive: true })
-			const testLog = execSync(`npx c8 ${c8opts} tsx ${path.join(dirname, spec)}`, { encoding: 'utf8' })
-			console.log(testLog)
-			if (fs.existsSync(reportDir)) {
-				const extracts = emitRelevantSpecCovDetails({
-					workspace: 'augen',
-					relevantSpecs: specs,
-					reportDir,
-					testedSpecs: [spec]
-				})
-				if (!title) title = extracts.title
-				html.push(extracts.html)
-				markdowns.push(extracts.markdown)
-			}
-		}
-		if (html.length) {
-			const combinedHtml = `<h3>${title}</h3>\n` + html.join('\n')
-			fs.writeFileSync(extractFiles.html, combinedHtml, { encoding: 'utf8' })
-			const combinedMarkdown = markdowns.join('\n')
-			fs.writeFileSync(extractFiles.markdown, combinedMarkdown, { encoding: 'utf8' })
-		}
-	} catch (e) {
-		console.log(`\n!!! augen runRelevantSpecs() error !!!\n`, e, '\n')
-		//test.fail(`Error running relevant augen specs`)
-		//test.end()
-	}
 }
