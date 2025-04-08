1726657bb96ffe489edf70e3e7ea95103fde1669
Colleen Reilly <creilly@stjude.org>
2025-04-08 15:47:17 -0500

    Renders sc sample table component on load

--- client/plots/sc/SC.ts
+++ client/plots/sc/SC.ts
@@ -1,18 +1,18 @@
 import { RxComponentInner } from '../../types/rx.d'
-import { Menu } from '#dom'
 import type { BasePlotConfig, MassAppActions, MassState } from '#mass/types/mass'
 import type { Div } from '../../types/d3'
 import { getCompInit, copyMerge } from '#rx'
 import type { SCConfig, SCConfigOpts, SCViewerOpts } from './SCTypes'
 import { SCRenderer } from './SCRenderer'
+import { getDefaultSCSampleTableSettings } from '../scSampleTable'
 
-export class SCViewer extends RxComponentInner {
+class SCViewer extends RxComponentInner {
 	readonly type = 'sc'
 	components: {
 		plots: { [key: string]: any }
 	}
 	plotsDiv: { [key: string]: Div }
-	plotsControlsDiv: { [key: string]: Div }
+	// plotsControlsDiv: { [key: string]: Div }
 	renderer?: SCRenderer
 
 	constructor(opts: SCViewerOpts) {
@@ -20,8 +20,8 @@ export class SCViewer extends RxComponentInner {
 		this.components = {
 			plots: {}
 		}
-		const holder = opts.holder.classed('sjpp-sc-main', true)
-		const div = holder
+		const div = opts.holder
+			.classed('sjpp-sc-main', true)
 			.append('div')
 			.style('padding', '5px')
 			.style('display', 'inline-block')
@@ -29,14 +29,14 @@ export class SCViewer extends RxComponentInner {
 		const tabsDiv = div.append('div').attr('id', 'sjpp-sc-tabs').style('display', 'inline-block')
 		const plots = div.append('div').attr('id', 'sjpp-sc-tabs-content')
 		this.dom = {
-			controls: opts.controls,
 			div,
 			tabsDiv,
-			plots,
-			tip: new Menu({ padding: '' })
+			plots
 		}
-		this.plotsControlsDiv = {}
+		// this.plotsControlsDiv = {}
 		this.plotsDiv = {}
+
+		if (opts.header) this.dom.header = opts.header.html(`SINGLE CELL`).style('font-size', '0.9em')
 	}
 
 	getState(appState: MassState) {
@@ -63,24 +63,24 @@ export class SCViewer extends RxComponentInner {
 
 	async init(appState: MassState) {
 		const state = this.getState(appState)
-		const config = structuredClone(state.config)
-
-		this.renderer = new SCRenderer(state, config)
+		this.renderer = new SCRenderer(this.app, this.dom, state)
 	}
 
 	async setComponent(config: SCConfig) {
 		let _
-		//Add components
+		if (config.childType == 'scSampleTable') _ = await import(`#plots/scSampleTable.ts`)
+		if (config.childType == 'differentialAnalysis') _ = await import(`#plots/diffAnalysis/DifferentialAnalysis.ts`)
+		if (config.childType == 'violin') _ = await import(`#plots/violin.js`)
 
-		this.plotsControlsDiv[config.childType] = this.dom.controls.append('div')
+		// this.plotsControlsDiv[config.childType] = this.dom.controls.append('div')
 		this.plotsDiv[config.childType] = this.dom.plots.append('div')
 
 		const opts = {
 			app: this.app,
 			holder: this.plotsDiv[config.childType],
 			id: this.id,
-			parent: this.api,
-			controls: this.plotsControlsDiv[config.childType]
+			parent: this.api
+			// controls: this.plotsControlsDiv[config.childType]
 		}
 		this.components.plots[config.childType] = await _.componentInit(opts)
 	}
@@ -97,11 +97,11 @@ export class SCViewer extends RxComponentInner {
 			const chart = this.components.plots[childType]
 			if (chart.type != config.childType) {
 				this.plotsDiv[chart.type].style('display', 'none')
-				this.plotsControlsDiv[chart.type].style('display', 'none')
+				// this.plotsControlsDiv[chart.type].style('display', 'none')
 			}
 		}
 		this.plotsDiv[config.childType].style('display', '')
-		this.plotsControlsDiv[config.childType].style('display', '')
+		// this.plotsControlsDiv[config.childType].style('display', '')
 
 		this.renderer.update(config)
 	}
@@ -113,9 +113,12 @@ export const componentInit = SCInit
 export function getPlotConfig(opts: SCConfigOpts) {
 	const config = {
 		chartType: 'sc',
-		childType: 'table',
-		settings: {}
-	} as SCConfig
+		childType: 'scSampleTable',
+		settings: {
+			controls: { isOpen: false },
+			scSampleTable: getDefaultSCSampleTableSettings(opts.overrides || {})
+		}
+	} as any
 
 	return copyMerge(config, opts)
 }
--- client/plots/sc/SCRenderer.ts
+++ client/plots/sc/SCRenderer.ts
@@ -1,13 +1,95 @@
-export class SCRenderer {
-	state: any
-	config: any
+import { Tabs } from '#dom'
+import type { MassAppApi, MassState } from '#mass/types/mass'
+import { SCConfig } from './SCTypes'
 
-	constructor(state, config) {
+export class SCRenderer {
+	app: MassAppApi
+	config: SCConfig
+	dom: any
+	state: MassState
+	tabs: Tabs
+	tabsData: any
+
+	constructor(app, dom, state) {
+		this.app = app
 		this.state = state
-		this.config = config
+		this.config = structuredClone(state.config)
+		this.dom = dom
+		this.tabsData = this.getTabsOptions()
+		this.tabs = new Tabs({ holder: this.dom.tabsDiv, tabs: this.tabsData })
+		this.tabs.main()
+	}
+
+	getTabsOptions() {
+		// const scQueries = this.state.termdbConfig.queries.singleCell
+		const tabs = [
+			{
+				active: this.config.childType === 'scSampleTable',
+				id: 'scSampleTable',
+				label: 'Samples',
+				isVisible: () => true,
+				getPlotConfig: () => {
+					return {
+						childType: 'scSampleTable'
+					}
+				},
+				callback: this.tabCallback
+			}
+			// {
+			//     active: this.config.childType === 'differentialAnalysis',
+			//     id: 'differentialAnalysis',
+			//     label: 'Differential Expression',
+			//     isVisible: () => scQueries?.DEgenes,
+			//     getPlotConfig: () => {
+			//         return {
+			//             //Need to figure out how to launch a child plot
+			//             //that is also a parent component
+			//             childType: 'differentialAnalysis'
+			//         }
+			//     },
+			//     callback: this.tabCallback
+			// },
+			// {
+			//     active: this.config.childType === 'violin',
+			//     id: 'violin',
+			//     label: 'Summary',
+			//     isVisible: () => true,
+			//     getPlotConfig: () => {
+			//         return {
+			//             childType: 'violin'
+			//         }
+			//     },
+			//     callback: this.tabCallback
+			// },
+			//This might be a reusable holder just for images.
+			// {
+			//     active: this.config.childType === 'scImages',
+			//     id: 'scImages',
+			//     label: scQueries?.images?.label || 'Images',
+			//     isVisible: () => scQueries?.images,
+			//     getPlotConfig: () => {
+			//         return {
+			//             childType: 'scImages'
+			//         }
+			//     },
+			//     callback: this.tabCallback
+			// },
+		]
+		return tabs
+	}
+
+	tabCallback = async (event, tab) => {
+		if (!tab || !tab.id) return
+		const plotConfig = tab.getPlotConfig()
+		await this.app.dispatch({
+			type: 'plot_edit',
+			id: this.config.id,
+			config: plotConfig
+		})
 	}
 
 	update(config) {
-		//TODO
+		const activeTabIndex = this.tabsData.findIndex(tab => tab.id == config.childType)
+		this.tabs.update(activeTabIndex)
 	}
 }
