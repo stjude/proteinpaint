51e5096f97d98ccc4594f31c8de4f01d88614e35
Wang <jwang7@L230176>
2025-04-09 22:55:12 -0500

    gsea.py is migrated

--- server/routes/genesetEnrichment.ts
+++ server/routes/genesetEnrichment.ts
@@ -2,9 +2,9 @@ import type { GenesetEnrichmentRequest, GenesetEnrichmentResponse, RouteApi } fr
 import { genesetEnrichmentPayload } from '#types/checkers'
 import fs from 'fs'
 import path from 'path'
-import { spawn } from 'child_process'
 import { Readable } from 'stream'
 import serverconfig from '#src/serverconfig.js'
+import { run_python } from '@sjcrh/proteinpaint-python'
 
 export const api: RouteApi = {
 	endpoint: 'genesetEnrichment',
@@ -79,11 +79,7 @@ async function run_genesetEnrichment_analysis(
 	//	// For catching input to rust pipeline, in case of an error
 	//	if (err) return console.log(err)
 	//})
-
-	const gsea_output: any = await run_gsea(
-		`${serverconfig.binpath}/utils/gsea.py`,
-		'/' + JSON.stringify(genesetenrichment_input) // "/" is needed for python to accept the bracket "{" as a bracket
-	)
+	const gsea_output: any = await run_python('gsea.py', '/' + JSON.stringify(genesetenrichment_input))
 
 	let result
 	let data_found = false
@@ -114,55 +110,4 @@ async function run_genesetEnrichment_analysis(
 	} else {
 		throw ``
 	}
-}
-
-async function run_gsea(path, data) {
-	try {
-		await fs.promises.stat(path)
-	} catch (_) {
-		throw `${path} does not exist`
-	}
-	return new Promise((resolve, reject) => {
-		const _stdout: any[] = []
-		const _stderr: any[] = []
-		// spawn python process
-		const sp = spawn(serverconfig.python, [path])
-		//console.log("data:",data)
-		if (data) {
-			// stream input data into python
-			try {
-				const input = data.endsWith('\n') ? data : data + '\n' // python expects a final end-of-line marker
-				Readable.from(input).pipe(sp.stdin)
-			} catch (e) {
-				sp.kill()
-				let errmsg = e
-				const stderr = _stderr.join('').trim()
-				if (stderr) errmsg += `\npython stderr: ${stderr}`
-				reject(errmsg)
-			}
-		}
-		// store stdout and stderr from python
-		sp.stdout.on('data', data => _stdout.push(data))
-		sp.stderr.on('data', data => _stderr.push(data))
-		sp.on('error', err => reject(err))
-		// return stdout and stderr when python process closes
-		sp.on('close', code => {
-			const stdout = _stdout.join('').trim()
-			const stderr = _stderr.join('').trim()
-			if (code !== 0) {
-				// handle non-zero exit status
-				let errmsg = `python process exited with non-zero status code=${code}`
-				if (stdout) errmsg += `\npython stdout: ${stdout}`
-				if (stderr) errmsg += `\npython stderr: ${stderr}`
-				reject(errmsg)
-			}
-			if (stderr) {
-				// handle python stderr
-				const errmsg = `python process emitted standard error\npython stderr: ${stderr}`
-				reject(errmsg)
-			}
-			// return standard out from python
-			resolve(stdout)
-		})
-	})
 }
