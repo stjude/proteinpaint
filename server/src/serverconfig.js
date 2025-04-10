f9f3868ca2934577a189d5ced57b82365764b6ee
siosonel <siosonel@gmail.com>
2025-04-10 18:31:43 -0500

    add back python workspace handling in container/pack.sh

--- server/src/serverconfig.js
+++ server/src/serverconfig.js
@@ -61,7 +61,7 @@ if (!serverconfig.clustalo) serverconfig.clustalo = 'clustalo'
 if (!serverconfig.Rscript) serverconfig.Rscript = 'Rscript'
 if (!serverconfig.gfServer) serverconfig.gfServer = 'gfServer'
 if (!serverconfig.gfClient) serverconfig.gfClient = 'gfClient'
-if (!serverconfig.python) serverconfig.python = 'python3'
+// note: server/src/app.ts uses `setPythonBinPath()` from `@scjrch/proteinpaint-python`
 // NOTE: will set other cmd paths that require binpath after it's filled-in below
 
 /******************
