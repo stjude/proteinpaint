bb3f0532e183ba95522892e0e8abeb8bddb94658
Airen Zaldivar <airenzp@gmail.com>
2025-04-11 14:26:35 -0500

    Added date functions and tests

--- shared/utils/src/test/date.unit.spec.js
+++ shared/utils/src/test/date.unit.spec.js
@@ -1,6 +1,5 @@
 import tape from 'tape'
-import { roundValueAuto, roundValue2, decimalPlacesUntilFirstNonZero } from '../roundValue.js'
-import { getDateStrFromNumber, getNumberFromDateStr } from '../terms.js'
+import { getDateStrFromNumber, getNumberFromDateStr, getDateFromNumber, getNumberFromDate } from '../terms.js'
 /**
  * Tests
  * convert date to number
@@ -15,7 +14,25 @@ tape('\n', function (test) {
 	test.end()
 })
 
-tape('conver date to number', function (test) {
+tape('convert date to number', function (test) {
+	//2023.5 is the first of July 2023.
+	const date = new Date(2023, 6, 1) // July 1, 2023
+	const num = getNumberFromDate(date)
+	test.equal(num, 2023.5, `The number for date ${date.toDateString()} should be 2023.5`)
+
+	test.end()
+})
+
+tape('convert number to date', function (test) {
+	const num = 2023.5
+	const date = getDateFromNumber(num)
+	const str = date.toDateString()
+	const expectedDateStr = new Date(2023, 6, 1).toDateString()
+	test.equal(expectedDateStr, str, `The date for number ${num} should be ${expectedDateStr} and  it is ${str}`)
+	test.end()
+})
+
+tape('convert date str to number', function (test) {
 	//2023.5 is the first of July 2023.
 	const str = '2023-07-01'
 	const num = getNumberFromDateStr(str)
@@ -23,7 +40,7 @@ tape('conver date to number', function (test) {
 
 	test.end()
 })
-tape('convert number to date', function (test) {
+tape('convert number to date str', function (test) {
 	const num = 2023.5
 	const str = getDateStrFromNumber(num)
 	test.equal(
