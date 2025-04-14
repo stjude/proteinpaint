1e56d9e0f5919feb2a9df5a126a7e9d7ab846f8b
Airen Zaldivar <airenzp@gmail.com>
2025-04-14 11:51:07 -0500

    Check that the tabs are not hidden because then tab is 0 and hides the content

--- client/mass/app.js
+++ client/mass/app.js
@@ -134,7 +134,10 @@ class MassApp {
 	async main() {
 		await this.api.vocabApi.main()
 		//Do not show the plots below the about tab
-		this.dom.plotDiv.style('display', this.state.nav?.activeTab == 0 ? 'none' : 'block')
+		this.dom.plotDiv.style(
+			'display',
+			this.state.nav?.header_mode != 'hidden' && this.state.nav?.activeTab == 0 ? 'none' : 'block'
+		)
 
 		const newPlots = {}
 		let sandbox
