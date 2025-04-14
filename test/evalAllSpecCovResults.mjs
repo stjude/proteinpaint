02163c08eb74c82facba4fa7fd939a0065847975
siosonel <siosonel@gmail.com>
2025-04-14 12:50:46 -0500

    force augen spec coverage to fail to test ci displayed message and commit

--- test/evalAllSpecCovResults.mjs
+++ test/evalAllSpecCovResults.mjs
@@ -1,12 +1,20 @@
-import { evalSpecCovResults } from '@sjcrh/augen'
+import { evalSpecCovResults, publicSpecsDir } from '@sjcrh/augen'
+import path from 'path'
+import fs from 'fs'
 
 const workspaces = ['client', 'server', 'shared/utils', 'augen']
+
+const failedTextsFile = path.join(publicSpecsDir, 'failedTexts.txt')
 
 if (process.argv[2]) evalAllSpecCovResults(Number(process.argv[2]))
 
 export async function evalAllSpecCovResults(errorCode = 0) {
 	const failures = []
 	const relevantWs = []
+	const failedTexts = []
+
+	if (fs.existsSync(failedTextsFile)) fs.rmSync(failedTextsFile, { force: true })
+
 	for (const workspace of workspaces) {
 		const result = await evalSpecCovResults({ workspace })
 		if (result.workspace) relevantWs.push(result.workspace)
@@ -15,9 +23,19 @@ export async function evalAllSpecCovResults(errorCode = 0) {
 			// console.log(`\n!!! ${workspace} failed spec coverage !!!`)
 			// console.log(result.failedCoverage, '\n')
 			failures.push(result)
+			if (result.failedCoverage) {
+				const text = []
+				for (const [file, cov] of Object.entries(result.failedCoverage)) {
+					if (cov.lowestPct?.diff < 0) text.push(`${file} lowestPct:` + JSON.stringify(cov.lowestPct))
+					if (cov.averagePct?.diff < 0) text.push(`${file} averagePct:` + JSON.stringify(cov.averagePct))
+				}
+				if (text.length) failedTexts.push(workspace, text.join('\n'))
+			}
 		}
 	}
-
+	if (failedTexts.length) {
+		fs.writeFileSync(failedTextsFile, failedTexts.join('\n'))
+	}
 	if (errorCode) {
 		if (failures.length) process.exit(failures.length ? errorCode : 0)
 	}
