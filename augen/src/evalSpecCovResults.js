e405b6484d6a19f4623ef9f3e2e45bf0ad9b27a0
siosonel <siosonel@gmail.com>
2025-04-14 12:50:46 -0500

    force augen spec coverage to fail to test ci displayed message and commit

--- augen/src/evalSpecCovResults.js
+++ augen/src/evalSpecCovResults.js
@@ -68,8 +68,9 @@ export async function evalSpecCovResults({ workspace, jsonExtract }) {
 		if (!failedCoverage.size) {
 			console.log(`\n👏 ${workspace} branch coverage test PASSED! 🎉`)
 			console.log('--- Percent coverage was maintained or improved across relevant files! ---\n')
-			const branch = execSync(`git rev-parse --abbrev-ref HEAD`)
-			if (branch == 'master') {
+			const branch = execSync(`git rev-parse --abbrev-ref HEAD`).trim()
+			console.log(71, 'branch=', branch)
+			if (branch == 'master' || branch == 'spec-coverage-ci') {
 				fs.writeFileSync(covFile, JSON.stringify(relevantCoverage, null, '  '))
 				execSync(`git add ${covFile}`)
 			}
