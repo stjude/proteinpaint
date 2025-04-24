49fc1df1d5babf465e78a424165205113ae71e78
Xin Zhou <xzhou82@gmail.com>
2025-04-24 18:34:02 -0500

    shared summarize_mclass to consistently compute legend for both native and custom data

--- shared/utils/src/mds3tk.js
+++ shared/utils/src/mds3tk.js
@@ -1,3 +1,5 @@
+import { dtcnv } from './common.js'
+
 // this script should contain mds3 track-related stuff shared between client and backend
 
 /*
@@ -14,3 +16,36 @@ svfusion: dt + chr + pos + strand + pairlstidx + mname
 the separator must avoid conflicting with characters from gene names, and can be changed based on needs
 */
 export const ssmIdFieldsSeparator = '__'
+
+/*
+input: array of mixture of ssm, svfusion and cnv
+
+output: sorted array. each element: [ class/dt, count of m ]
+*/
+export function summarize_mclass(mlst) {
+	const m2c = new Map() // k: mclass, v: {}
+	const cnvs = []
+	for (const m of mlst) {
+		if (m.dt == dtcnv) {
+			cnvs.push(m)
+			continue // process cnv later
+		}
+		// snvindel has m.class=str, svfusion has only dt=int
+		const key = m.class || m.dt
+		m2c.set(key, 1 + (m2c.get(key) || 0))
+	}
+
+	if (cnvs.length) {
+		if (Number.isFinite(cnvs[0].value)) {
+			// first cnv uses numeric value (assumes all the same). record by dt
+			m2c.set(dtcnv, cnvs.length)
+		} else {
+			// cnv not numeric and uses class; record by each class
+			for (const c of cnvs) {
+				if (!c.class) continue // should not happen
+				m2c.set(c.class, 1 + (m2c.get(c.class) || 0))
+			}
+		}
+	}
+	return [...m2c].sort((i, j) => j[1] - i[1])
+}
