f4edb44cfbd8e29aabfa3b0ccb7fa51009c259ac
Colleen Reilly <creilly@stjude.org>
2025-04-29 09:00:53 -0500

    Replace `private` with hash

--- server/src/Cache.ts
+++ server/src/Cache.ts
@@ -27,23 +27,23 @@ type CacheOpts = {
 
 export class Cache {
 	/** a pending timeout reference from setTimeout that calls mayDeleteCacheFiles */
-	private cacheCheckTimeout: NodeJS.Timeout | undefined | number
+	#cacheCheckTimeout: NodeJS.Timeout | undefined | number
 	cachedir: string
 	public checkWait: number
-	private fileExtensions: string[]
+	#fileExtensions: string[]
 	/** in milliseconds */
-	private maxAge: number
+	#maxAge: number
 	/** in bytes */
-	private maxSize: number
-	private nextCheckTime = 0
+	#maxSize: number
+	#nextCheckTime = 0
 
 	constructor(opts: CacheOpts = {}) {
-		this.cacheCheckTimeout = 0
+		this.#cacheCheckTimeout = 0
 		this.cachedir = opts.cachedir || serverconfig.cachedir
 		this.checkWait = opts.checkWait || 1 * 60 * 1000
-		this.fileExtensions = opts.fileExtensions || []
-		this.maxAge = opts.maxAge || 2 * 60 * 60 * 1000
-		this.maxSize = opts.maxSize || 5e9
+		this.#fileExtensions = opts.fileExtensions || []
+		this.#maxAge = opts.maxAge || 2 * 60 * 60 * 1000
+		this.#maxSize = opts.maxSize || 5e9
 	}
 
 	mayResetCacheCheckTimeout(wait = 0) {
@@ -51,22 +51,22 @@ export class Cache {
 		if (process.argv.includes('validate')) return
 
 		const checkTime = Date.now() + wait
-		if (this.cacheCheckTimeout) {
-			if (this.nextCheckTime && this.nextCheckTime <= checkTime + 5) return
+		if (this.#cacheCheckTimeout) {
+			if (this.#nextCheckTime && this.#nextCheckTime <= checkTime + 5) return
 			else {
-				clearTimeout(this.cacheCheckTimeout)
-				this.cacheCheckTimeout = undefined
+				clearTimeout(this.#cacheCheckTimeout)
+				this.#cacheCheckTimeout = undefined
 			}
 		}
-		this.nextCheckTime = checkTime
+		this.#nextCheckTime = checkTime
 		console.log(`will trigger mayDeleteCacheFiles() in ${wait} ms`)
-		this.cacheCheckTimeout = setTimeout(this.mayDeleteCacheFiles.bind(this), wait)
+		this.#cacheCheckTimeout = setTimeout(this.mayDeleteCacheFiles.bind(this), wait)
 	}
 
 	async mayDeleteCacheFiles() {
 		console.log(`checking for cached files to delete ...`)
 		try {
-			const minTime = Date.now() - this.maxAge
+			const minTime = Date.now() - this.#maxAge
 			const filenames = await fs.promises.readdir(this.cachedir)
 			const files: { path: any; time: any; size: any; deleted?: any }[] = [] // keep list of undeleted files. may need to rank them and delete old ones ranked by age
 			let totalSize = 0,
@@ -74,7 +74,7 @@ export class Cache {
 				totalCount = 0,
 				deletedCount = 0
 			for (const filename of filenames) {
-				if (this.fileExtensions.length && !this.fileExtensions.some(ext => filename.endsWith(ext))) continue
+				if (this.#fileExtensions.length && !this.#fileExtensions.some(ext => filename.endsWith(ext))) continue
 				totalCount++
 				const fp = path.join(this.cachedir, filename)
 				const s = await fs.promises.stat(fp)
@@ -94,7 +94,7 @@ export class Cache {
 				totalSize += s.size
 			}
 			files.sort((i, j) => j.time - i.time) // descending
-			if (totalSize >= this.maxSize) {
+			if (totalSize >= this.#maxSize) {
 				/*
                 storage use is still above limit, deleting files just older than cutoff is not enough
                 a lot of recent requests may have deposited lots of cache files
@@ -109,15 +109,15 @@ export class Cache {
 					deletedCount++
 					deletedSize += f.size
 					totalSize -= f.size
-					if (totalSize < this.maxSize) break
+					if (totalSize < this.#maxSize) break
 				}
 			}
 			console.log(
 				`deleted ${deletedCount} of ${totalCount} cached files (${deletedSize} bytes deleted, ${totalSize} remaining)`
 			)
 			// empty out the following tracking variables
-			this.cacheCheckTimeout = undefined
-			this.nextCheckTime = 0
+			this.#cacheCheckTimeout = undefined
+			this.#nextCheckTime = 0
 			const nextFile = totalSize && files.find(f => !f.deleted)
 			if (nextFile) {
 				// trigger another mayDeleteCachefile() call with setTimeout,
@@ -125,7 +125,7 @@ export class Cache {
 				// or much sooner if the max cache size is currently exceeded
 				const wait =
 					this.checkWait +
-					Math.round(totalSize >= this.maxSize ? 0 : Math.max(0, nextFile.time + this.maxAge - Date.now()))
+					Math.round(totalSize >= this.#maxSize ? 0 : Math.max(0, nextFile.time + this.#maxAge - Date.now()))
 				this.mayResetCacheCheckTimeout(wait)
 			}
 		} catch (e) {
