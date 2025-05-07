bced14c8dbf5dd60db3a67decf52a1ece74262dd
Robin Paul <robin.paul@stjude.org>
2025-05-07 14:52:32 -0500

    hierCluster accepts gene cutoffs from genome file

--- shared/types/src/genome.ts
+++ shared/types/src/genome.ts
@@ -9,115 +9,117 @@ Genome
 */
 
 type GeneDb = {
-	dbfile: string
+    dbfile: string
 }
 
 type TermDbs = {
-	[key: string]: TermDbsEntry
+    [key: string]: TermDbsEntry
 }
 
 type TermDbsEntry = {
-	label: string
-	cohort: Cohort
+    label: string
+    cohort: Cohort
+    analysisGenesetGroups: { label: string, value: string }[]
+    geneORAparam: { minCutoff: number, maxCutoff: number }
 }
 
 type DbStatement = {
-	dbfile: string
-	statement: string
+    dbfile: string
+    statement: string
 }
 
 type Snp = {
-	bigbedfile: string
+    bigbedfile: string
 }
 
 type FimoMotif = {
-	db: string
-	annotationfile: string
+    db: string
+    annotationfile: string
 }
 
 type TrackCategoryEntry = {
-	color: string
-	label: string
+    color: string
+    label: string
 }
 
 type TrackCategories = {
-	[index: string]: TrackCategoryEntry | undefined
+    [index: string]: TrackCategoryEntry | undefined
 }
 
 type Track = {
-	__isgene?: boolean
-	translatecoding?: boolean
-	file: string
-	type: string
-	name: string
-	categories?: TrackCategories
-	stackheight?: number
-	stackspace?: number
-	vpad?: number
-	color?: string
-	onerow?: boolean
+    __isgene?: boolean
+    translatecoding?: boolean
+    file: string
+    type: string
+    name: string
+    categories?: TrackCategories
+    stackheight?: number
+    stackspace?: number
+    vpad?: number
+    color?: string
+    onerow?: boolean
 }
 
 type DefaultCoord = {
-	chr: string
-	start: number
-	stop: number
-	gene?: string
+    chr: string
+    start: number
+    stop: number
+    gene?: string
 }
 
 type GeneSet = {
-	name: string
-	lst: string[]
+    name: string
+    lst: string[]
 }
 
 type HicEnzymeFragment = {
-	enzyme: string
-	file: string
+    enzyme: string
+    file: string
 }
 
 type HicDomainSetEntry = {
-	name: string
-	longname: string
-	file: string
+    name: string
+    longname: string
+    file: string
 }
 
 type HicDomainSet = {
-	[index: string]: HicDomainSetEntry
+    [index: string]: HicDomainSetEntry
 }
 
 type HicDomainGrpEntry = {
-	name: string
-	reference: string
-	sets: HicDomainSet
+    name: string
+    reference: string
+    sets: HicDomainSet
 }
 
 type HicDomainGroups = {
-	[index: string]: HicDomainGrpEntry
+    [index: string]: HicDomainGrpEntry
 }
 
 type HicDomain = {
-	[index: string]: HicDomainGroups
+    [index: string]: HicDomainGroups
 }
 
 //Separated to force g.tracks as required, see hgvirus.ts
 export type MinGenome = {
-	isMinGenome?: boolean
-	species: string
-	genomefile: string
-	genedb: GeneDb
-	defaultcoord: DefaultCoord
-	hicenzymefragment?: HicEnzymeFragment[]
-	majorchr: string
+    isMinGenome?: boolean
+    species: string
+    genomefile: string
+    genedb: GeneDb
+    defaultcoord: DefaultCoord
+    hicenzymefragment?: HicEnzymeFragment[]
+    majorchr: string
 }
 
 export type Genome = MinGenome & {
-	termdbs?: TermDbs
-	proteindomain?: DbStatement
-	repeatmasker?: DbStatement
-	snp?: Snp
-	fimo_motif?: FimoMotif
-	tracks?: Track[]
-	geneset?: GeneSet[]
-	hicdomain?: HicDomain
-	minorchr?: string
+    termdbs?: TermDbs
+    proteindomain?: DbStatement
+    repeatmasker?: DbStatement
+    snp?: Snp
+    fimo_motif?: FimoMotif
+    tracks?: Track[]
+    geneset?: GeneSet[]
+    hicdomain?: HicDomain
+    minorchr?: string
 }
