5e31b5b7fb6fc44cefd19d358eb6215f0d1b308e
Colleen Reilly <creilly@stjude.org>
2025-05-08 14:03:38 -0500

    Initial unit tests for cache

--- server/src/Cache.ts
+++ server/src/Cache.ts
@@ -5,7 +5,7 @@ import path from 'path'
 export class Cache {
 	readonly defaultCacheDirs = [
 		/** Temporarily stores sliced bam files */
-		{ dir: 'bam' },
+		{ dir: 'bam', fileExtensions: new Set(['.bam', '.bai']) },
 		/** In use? */
 		{ dir: 'genome' },
 		/** Temporarily stores pickle files for gsea */
@@ -20,14 +20,15 @@ export class Cache {
 		{
 			dir: 'snpgt',
 			fileNameRegexp: /[^\w]/, // client-provided cache file name matching with this are denied
-			sampleColumn: 6 // in cache file, sample column starts from 7th column
+			sampleColumn: 6, // in cache file, sample column starts from 7th column
+			onlyCache: true //Denote to use cache, not cachedir
 		}
 	]
 
 	async init() {
 		// create sub directories under cachedir, and register path in serverconfig
 		for (const subdir of this.defaultCacheDirs) {
-			if (Object.keys(subdir).length > 1) {
+			if (subdir.onlyCache) {
 				//cache objs are defined as cache_<dir> in serverconfig
 				serverconfig[`cache_${subdir.dir}`] = {
 					dir: await this.mayCreateSubdirInCache(subdir.dir),
@@ -63,27 +64,40 @@ export class Cache {
 	deleteCacheFiles() {
 		// Allow the serverconfig to override the default interval of 1 minute
 		// Useful for CI
-		const cacheMonitor = serverconfig.features?.cacheMonitor
-		const interval = cacheMonitor.intervalOverride || 1000 * 60
+		const cacheMonitor = serverconfig?.features?.cacheMonitor
+		const interval = cacheMonitor?.intervalOverride ?? 1000 * 60
 		// Clear the subdirectories periodically, defined by the interval
 		// and optional cacheMonitor settings in serverconfig
-		setInterval(async () => {
+		const runInterval = setInterval(async () => {
 			console.log('Checking for cached files to delete ...')
 			for (const subdir of this.defaultCacheDirs) {
 				//Only enable gsea for now
-				if (subdir.dir !== 'gsea') continue
-				const maxAge = cacheMonitor?.[subdir.dir].maxAge || 1000 * 60 * 60 * 2 // 2 hours
-				const maxSize = cacheMonitor?.[subdir.dir].maxSize || 5e9 // 5 GB
-				await this.mayDeleteCacheFiles(maxAge, maxSize, serverconfig[`cache_${subdir.dir}`], interval)
+				// if (subdir.dir !== 'gsea') continue
+				const maxAge = cacheMonitor?.[subdir.dir]?.maxAge ?? 1000 * 60 * 60 * 2 // 2 hours
+				const maxSize = cacheMonitor?.[subdir.dir]?.maxSize ?? 5e9 // 5 GB
+				const configRef = subdir.onlyCache
+					? serverconfig[`cache_${subdir.dir}`]
+					: serverconfig[`cachedir_${subdir.dir}`]
+				await this.mayDeleteCacheFiles(maxAge, maxSize, configRef, subdir, interval)
 			}
 		}, interval)
+
+		return {
+			stop: () => clearInterval(runInterval)
+		}
 	}
 
-	async mayDeleteCacheFiles(maxAge: number, maxSize: number, subdir, interval: number) {
+	async mayDeleteCacheFiles(maxAge: number, maxSize: number, configRef, subdir, interval: number) {
+		const filePath = configRef?.dir || configRef
+		console.log(filePath)
 		console.log(`checking for cached ${subdir.dir} files to delete ...`)
 		try {
 			const minTime = Date.now() - maxAge
-			const filenames = await fs.promises.readdir(subdir.dir)
+			const filenames = await fs.promises.readdir(filePath)
+			if (filenames.length == 0) {
+				console.log(`No ${subdir.dir} cached files to delete`)
+				return
+			}
 			// keep list of undeleted files. may need to rank them and delete old ones ranked by age
 			const files: { path: any; time: any; size: any; deleted?: any }[] = []
 			let totalSize = 0,
@@ -91,9 +105,9 @@ export class Cache {
 				totalCount = 0,
 				deletedCount = 0
 			for (const filename of filenames) {
-				if (subdir.fileExtensions.size && !subdir.fileExtensions.has(path.extname(filename))) continue
+				if (subdir?.fileExtensions?.size && !subdir.fileExtensions.has(path.extname(filename))) continue
 				totalCount++
-				const fp = path.join(subdir, filename)
+				const fp = path.join(filePath, filename)
 				const s = await fs.promises.stat(fp)
 				if (!s.isFile()) continue
 				const time = s.mtimeMs
@@ -113,10 +127,10 @@ export class Cache {
 			files.sort((i, j) => j.time - i.time) // descending
 			if (totalSize >= maxSize) {
 				/*
-					storage use is still above limit, deleting files just older than cutoff is not enough
-					a lot of recent requests may have deposited lots of cache files
-					must delete more old files ranked by age
-					*/
+				storage use is still above limit, deleting files just older than cutoff is not enough
+				a lot of recent requests may have deposited lots of cache files
+				must delete more old files ranked by age
+				*/
 				const minMtime = Date.now() - interval
 				for (const f of files) {
 					// do not delete files too soon that it may affect a current file read
@@ -130,10 +144,10 @@ export class Cache {
 				}
 			}
 			console.log(
-				`deleted ${deletedCount} of ${totalCount} cached files (${deletedSize} bytes deleted, ${totalSize} remaining)`
+				`deleted ${deletedCount} of ${totalCount} ${subdir.dir} cached files (${deletedSize} bytes deleted, ${totalSize} remaining)`
 			)
 		} catch (e) {
-			console.error('Error in mayDeleteCacheFiles(): ' + e)
+			console.error(`Error in mayDeleteCacheFiles() for ${subdir.dir}: ${e}`)
 		}
 	}
 }
