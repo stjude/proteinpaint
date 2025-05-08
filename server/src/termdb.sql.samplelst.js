66762c3a1d95f4f9945e5c8659c95b2e74e6a8a6
Airen Zaldivar <airenzp@gmail.com>
2025-05-08 16:38:46 -0500

    Passed parameters for the query in the values

--- server/src/termdb.sql.samplelst.js
+++ server/src/termdb.sql.samplelst.js
@@ -8,15 +8,18 @@ export const sampleLstSql = {
 			if (!('in' in group)) group.in = true
 			samples = group.values.map(value => value.sampleId || value.sample)
 			const type = samples[0] ? ds.sampleId2Type.get(samples[0]) : ''
-			samplesString = samples.join(',') //later on need to unify the list handling in samplelst
-
+			samplesString = Array(samples.length).fill('?').join(',') //later on need to unify the list handling in samplelst
 			sql += `SELECT id as sample, ? as key, ? as value
 				FROM sampleidmap
 				WHERE sample ${group.in ? '' : 'NOT'} IN (${samplesString})  `
-			if (ds.cohort.db.tableColumns['sampleidmap'].includes('sample_type')) sql += `and sample_type = ${type} `
+			values.push(group.name, group.name)
+			values.push(...samples)
+			if (ds.cohort.db.tableColumns['sampleidmap'].includes('sample_type')) {
+				sql += `and sample_type = ? `
+				values.push(type)
+			}
 
 			if (i != tw.q.groups.length - 1) sql += 'UNION ALL '
-			values.push(group.name, group.name)
 		}
 		return { sql: `${tablename} AS (${sql})`, tablename }
 	}
