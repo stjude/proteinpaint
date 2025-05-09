91c73b5333362d65d8c1bdfb946b02ba816c95a6
Airen Zaldivar <airenzp@gmail.com>
2025-05-08 22:00:18 -0500

    Fixed multivalue query

--- server/src/termdb.sql.multivalue.js
+++ server/src/termdb.sql.multivalue.js
@@ -1,5 +1,6 @@
 export const multivalueCTE = {
-	getCTE(tablename, tw) {
+	getCTE(tablename, tw, values) {
+		values.push(tw.term.id)
 		return {
 			sql: `${tablename} AS (
 				SELECT 
@@ -7,7 +8,7 @@ export const multivalueCTE = {
 					value as key, 
 					value
 				FROM anno_multivalue
-				WHERE term_id='${tw.term.id}'
+				WHERE term_id=?
 			)`,
 			tablename
 		}
