d82556e93283633d403b480fb1045c6c31bb9d7c
siosonel <siosonel@gmail.com>
2025-05-13 11:00:24 -0500

    update server readme and clean up unused scripts

--- server/test/pretest.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import serverconfig from '../src/serverconfig.js'
-
-const hg38 = serverconfig.genomes && serverconfig.genomes.find(g => g.name == 'hg38-test')
-if (!hg38 || !hg38.datasets || !hg38.datasets.find(d => d.name == 'TermdbTest')) {
-	throw 'Missing the TermdbTest dataset entry in the hg38 genome of serverconfig.json'
-}
