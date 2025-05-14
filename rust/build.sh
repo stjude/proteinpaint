6c72e6800a0ddefdc0b2c21c75c3b2da03f6e01b
Aleksandar Acic <aacic@stjude.org>
2025-05-14 12:19:54 -0500

    Move Dockerfile to rust.

---
+++ rust/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DEFAULT_TAG="latest"
+TAG="${1:-$DEFAULT_TAG}"
+
+PLATFORM=""
+ARCH=$( uname -m )
+if [[ ${ARCH} == "arm64" ]]; then
+	ARCH="x86_64";
+  PLATFORM="--platform=linux/amd64"
+# Enable this if you want to build for arm64
+#	ARCH="aarch64";
+#	PLATFORM="--platform=linux/arm64"
+fi
+
+# The podman buildx build command
+docker buildx build . \
+  --file ./Dockerfile \
+  --tag "ghcr.io/stjude/rust:$TAG" \
+  $PLATFORM
