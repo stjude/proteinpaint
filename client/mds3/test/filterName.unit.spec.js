a8da94f83d9db28d6eff765860537f447130b3c1
Xin Zhou <xzhou82@gmail.com>
2025-05-14 09:39:49 -0500

    fix: handle geneVariant dtTerms in getFilterName()

--- client/mds3/test/filterName.unit.spec.js
+++ client/mds3/test/filterName.unit.spec.js
@@ -6,6 +6,42 @@ tape('\n', test => {
 	test.end()
 })
 
+tape('getFilterName', test => {
+	test.timeoutAfter(100)
+
+	test.equal(getFilterName(fCohort), 'No filter', 'filter with just cohortFilter is shown as empty')
+
+	test.equal(
+		getFilterName(fCat),
+		catTerm.name + ': Aname',
+		'combined string is short enough for term name to be included'
+	)
+
+	catTerm.name = 'Term11111112222222'
+	test.equal(getFilterName(fCat), 'Aname', 'combined name is too long thus only show category name')
+
+	test.equal(getFilterName(fCatCohort), 'Aname', 'tvs with tag=cohortFilter is ignored')
+
+	test.equal(getFilterName(fInt), '10<x<20')
+
+	test.equal(getFilterName(fFloat), '1.1<x<2.6')
+
+	fFloat.lst[0].tvs.term.type = 'geneExpression'
+	test.equal(getFilterName(fFloat), '1.1<x<2.6', 'geneExpression tvs')
+
+	fFloat.lst[0].tvs.term.type = 'metaboliteIntensity'
+	test.equal(getFilterName(fFloat), '1.1<x<2.6', 'metaboliteIntensity tvs')
+
+	test.equal(getFilterName(fSamplelst), 'testsamplegroup', 'samplelst tvs')
+	test.equal(getFilterName(fdtSnv), 'xx SNV/indel', 'dtsnvindel')
+	test.equal(getFilterName(fdtCnv), 'xx CNV', 'dtcnv')
+	test.equal(getFilterName(fdtFusion), 'xx Fusion RNA', 'dtcnv')
+	test.equal(getFilterName(fdtSv), 'xx SV', 'dtsv')
+
+	test.end()
+})
+
+// test data
 // TODO multivalue
 
 const catTerm = { type: 'categorical', name: 'TermA', values: { A: { label: 'Aname' } } }
@@ -77,35 +113,44 @@ const fSamplelst = {
 		}
 	]
 }
+const fdtSnv = {
+	lst: [
+		{
+			type: 'tvs',
+			tvs: {
+				term: { type: 'dtsnvindel', geneVariantTerm: { name: 'xx' } }
+			}
+		}
+	]
+}
 
-tape('getFilterName', test => {
-	test.timeoutAfter(100)
-	//test.plan(3)
-
-	test.equal(getFilterName(fCohort), 'No filter', 'filter with just cohortFilter is shown as empty')
-
-	test.equal(
-		getFilterName(fCat),
-		catTerm.name + ': Aname',
-		'combined string is short enough for term name to be included'
-	)
-
-	catTerm.name = 'Term11111112222222'
-	test.equal(getFilterName(fCat), 'Aname', 'combined name is too long thus only show category name')
-
-	test.equal(getFilterName(fCatCohort), 'Aname', 'tvs with tag=cohortFilter is ignored')
-
-	test.equal(getFilterName(fInt), '10<x<20')
-
-	test.equal(getFilterName(fFloat), '1.1<x<2.6')
-
-	fFloat.lst[0].tvs.term.type = 'geneExpression'
-	test.equal(getFilterName(fFloat), '1.1<x<2.6', 'geneExpression tvs')
-
-	fFloat.lst[0].tvs.term.type = 'metaboliteIntensity'
-	test.equal(getFilterName(fFloat), '1.1<x<2.6', 'metaboliteIntensity tvs')
-
-	test.equal(getFilterName(fSamplelst), 'testsamplegroup', 'samplelst tvs')
-
-	test.end()
-})
+const fdtCnv = {
+	lst: [
+		{
+			type: 'tvs',
+			tvs: {
+				term: { type: 'dtcnv', geneVariantTerm: { name: 'xx' } }
+			}
+		}
+	]
+}
+const fdtFusion = {
+	lst: [
+		{
+			type: 'tvs',
+			tvs: {
+				term: { type: 'dtfusion', geneVariantTerm: { name: 'xx' } }
+			}
+		}
+	]
+}
+const fdtSv = {
+	lst: [
+		{
+			type: 'tvs',
+			tvs: {
+				term: { type: 'dtsv', geneVariantTerm: { name: 'xx' } }
+			}
+		}
+	]
+}
