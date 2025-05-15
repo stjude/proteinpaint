eb846c326d4a27cf97524fafc61a175f6fd3f5b5
Colleen Reilly <creilly@stjude.org>
2025-05-15 15:19:43 -0500

    WIP: Implement tabbing left and right of annotations.

--- shared/types/src/dataset.ts
+++ shared/types/src/dataset.ts
@@ -979,7 +979,7 @@ export type WSImages = {
 	/**  ds supplied */
 	getWSIAnnotations?: (sampleName: string, wsiImage: string) => Promise<string[]>
 	/**  ds supplied */
-	getZoomInPoints?: (sampleName: string, wsiImage: string) => Promise<Array<[number, number]>>
+	getZoomInPoints?: (sampleName: string, wsiImage: string, index: number) => Promise<Array<[number, number]>>
 }
 
 /*** types supporting Termdb ***/
