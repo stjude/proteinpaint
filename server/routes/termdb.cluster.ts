cd09d9b6ec825f3abec1e5ceeae0bf6fadccb569
siosonel <siosonel@gmail.com>
2025-05-20 11:41:57 -0500

    fix the doneCaching detection in affected termdb routes

--- server/routes/termdb.cluster.ts
+++ server/routes/termdb.cluster.ts
@@ -48,7 +48,7 @@ function init({ genomes }) {
 			if (!g) throw 'invalid genome name'
 			const ds = g.datasets[q.dslabel]
 			if (!ds) throw 'invalid dataset name'
-			if (!ds.__gdc?.doneCaching)
+			if (ds.__gdc && !ds.__gdc?.doneCaching)
 				throw 'The server has not finished caching the case IDs: try again in about 2 minutes.'
 			if ([TermTypes.GENE_EXPRESSION, TermTypes.METABOLITE_INTENSITY, NUMERIC_DICTIONARY_TERM].includes(q.dataType)) {
 				if (!ds.queries?.[q.dataType] && q.dataType !== NUMERIC_DICTIONARY_TERM)
