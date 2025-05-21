c30fa4290b548c000237636f6b83a4207a5cb0c0
Gavriel Matt <gmatt@stjude.org>
2025-05-21 11:55:44 -0500

    fix parent term name in dt tvs

--- client/filter/tvs.dt.js
+++ client/filter/tvs.dt.js
@@ -7,6 +7,6 @@ Base TVS handler for dt terms
 export const handler = Object.assign({}, catHandler, { term_name_gen })
 
 function term_name_gen(d) {
-	const name = d.term.geneVariantTerm ? `${d.term.geneVariantTerm.name} ${d.term.name}` : d.term.name
+	const name = d.term.parentTerm ? `${d.term.parentTerm.name} ${d.term.name}` : d.term.name
 	return name.length < 31 ? name : '<label title="' + name + '">' + name.substring(0, 28) + '...' + '</label>'
 }
