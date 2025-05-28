c389760fe8e7636fb9efe563dfddfa4471d7c325
Wang <jwang7@L230176>
2025-05-28 13:14:45 -0500

    add mafOptions to gdcGRIN2.rs

--- rust/src/gdcGRIN2.rs
+++ rust/src/gdcGRIN2.rs
@@ -1,3 +1,15 @@
+/*
+  This script download cohort maf files from GDC, concatenate them into a single file that includes user specified columns.
+
+  Input JSON:
+    caseFiles
+    mafOptions: For SNVindel filtering
+  Output mutations as JSON array.
+
+  Example of usage:
+    echo '{"caseFiles": {"MP2PRT-PATFJE": {"maf": "26ea7b6f-8bc4-4e83-ace1-2125b493a361"},"MP2PRT-PAPIGD": {"maf": "653d7458-f4af-4328-a1ce-3bbf22a2e347"}},"mafOptions": {"minTotalDepth": 10,"minAltAlleleCount": 2}}' | ./target/release/gdcGRIN2
+*/
+
 use flate2::read::GzDecoder;
 use futures::StreamExt;
 use memchr::memchr;
@@ -23,14 +35,37 @@ struct DataType {
     maf: Option<String>,
 }
 
+// Define the structure for mafOptions
+#[derive(Deserialize, Debug)]
+struct MafOptions {
+    #[serde(rename = "minTotalDepth")]
+    min_total_depth: i32,
+    #[serde(rename = "minAltAlleleCount")]
+    min_alt_allele_count: i32,
+}
+
+// Define the top-level input structure
+#[derive(Deserialize, Debug)]
+struct InputData {
+    #[serde(rename = "caseFiles")]
+    case_files: HashMap<String, DataType>,
+    #[serde(rename = "mafOptions")]
+    maf_options: Option<MafOptions>,
+}
+
 // Function to parse TSV content
 // CNV:
 // Select cnv columns ["Chromosome","Start","End","Segment_Mean"]
 // Segment_Mean >= 0.2 => gain; Segment_Mean <= -0.2 => loss
 // MAF:
 // Select MAF columns ["Chromosome","Start_Position","End_Position"]
-fn parse_content(content: &str, case_id: &str, data_type: &str) -> Result<Vec<Vec<String>>, (String, String, String)> {
-    //) -> Result<String, (String, String, String)> {
+fn parse_content(
+    content: &str,
+    case_id: &str,
+    data_type: &str,
+    min_total_depth: i32,
+    min_alt_allele_count: i32,
+) -> Result<Vec<Vec<String>>, (String, String, String)> {
     let lines = content.lines();
     let mut parsed_data = Vec::new();
     //let mut parsed_data: String = String::new();
@@ -42,7 +77,7 @@ fn parse_content(content: &str, case_id: &str, data_type: &str) -> Result<Vec<Ve
         columns = vec!["Chromosome", "Start", "End", "Segment_Mean"]
     } else if data_type == "maf" {
         header_mk = "Hugo_Symbol";
-        columns = vec!["Chromosome", "Start_Position", "End_Position"]
+        columns = vec!["Chromosome", "Start_Position", "End_Position", "t_depth", "t_alt_count"]
     };
     let mut header: Vec<String> = Vec::new(); // GDC file header
     for line in lines {
@@ -63,13 +98,14 @@ fn parse_content(content: &str, case_id: &str, data_type: &str) -> Result<Vec<Ve
                 }
             }
         } else {
-            let mut keep_ck: bool = true;
+            let mut keep_ck: bool = true; // keep or discard this line
             let cont_lst: Vec<String> = line.split("\t").map(|s| s.to_string()).collect();
             let mut out_lst: Vec<String> = Vec::new();
             // add sample ID first
             out_lst.push(case_id.to_string());
             for x in columns_indices.iter() {
                 let mut element = cont_lst[*x].to_string();
+                // filter CNV based on segment mean value
                 if data_type == "cnv" && &header[*x] == "Segment_Mean" {
                     // convert to f32 (segment_mean)
                     let seg_mean = match element.parse::<f32>() {
@@ -89,9 +125,33 @@ fn parse_content(content: &str, case_id: &str, data_type: &str) -> Result<Vec<Ve
                 }
                 out_lst.push(element);
             }
+            // filter snvindel based on t_depth and t_alt_count
             // add lsn.type to snv
             if data_type == "maf" {
-                out_lst.push("mutation".to_string());
+                // Parse t_depth and t_alt_count to i32
+                // t_depth
+                let alle_depth = match out_lst[4].parse::<i32>() {
+                    Ok(value) => value,
+                    Err(_) => {
+                        let error_msg = "Failed to convert t_depth to i32.".to_string();
+                        return Err((case_id.to_string(), data_type.to_string(), error_msg));
+                    }
+                };
+                // t_alt_count
+                let alt_count = match out_lst[5].parse::<i32>() {
+                    Ok(value) => value,
+                    Err(_) => {
+                        let error_msg = "Failed to convert t_alt_count to i32.".to_string();
+                        return Err((case_id.to_string(), data_type.to_string(), error_msg));
+                    }
+                };
+                // filtering snvindels based on t_depth and t_alt_count
+                if alle_depth >= min_total_depth && alt_count >= min_alt_allele_count {
+                    out_lst = out_lst[0..4].to_vec();
+                    out_lst.push("mutation".to_string());
+                } else {
+                    keep_ck = false;
+                }
             }
             if keep_ck {
                 parsed_data.push(out_lst);
@@ -112,7 +172,12 @@ fn parse_content(content: &str, case_id: &str, data_type: &str) -> Result<Vec<Ve
 
 // Function to download data
 //async fn download_data(data4dl: HashMap<String,DataType>, host: &str) -> Vec<Result<(String, String), (String, String)>> {
-async fn download_data(data4dl: HashMap<String, DataType>, host: &str) -> () {
+async fn download_data(
+    data4dl: HashMap<String, DataType>,
+    host: &str,
+    min_total_depth: i32,
+    min_alt_allele_count: i32,
+) -> () {
     // Generate URLs from data4dl, handling optional cnv and maf
     let data_urls = data4dl
         .into_iter()
@@ -198,27 +263,29 @@ async fn download_data(data4dl: HashMap<String, DataType>, host: &str) -> () {
         .buffer_unordered(10)
         .for_each(|result| async {
             match result {
-                Ok((case_id, data_type, content)) => match parse_content(&content, &case_id, &data_type) {
-                    Ok(parsed_data) => match serde_json::to_string(&parsed_data) {
-                        Ok(json) => println!("{}", json),
-                        Err(e) => {
+                Ok((case_id, data_type, content)) => {
+                    match parse_content(&content, &case_id, &data_type, min_total_depth, min_alt_allele_count) {
+                        Ok(parsed_data) => match serde_json::to_string(&parsed_data) {
+                            Ok(json) => println!("{}", json),
+                            Err(e) => {
+                                let error = ErrorEntry {
+                                    case: format!("{}: {}", case_id, data_type),
+                                    error: format!("Failed to convert data to JSON {}", e),
+                                };
+                                let error_js = serde_json::to_string(&error).unwrap();
+                                eprintln!("{}", error_js);
+                            }
+                        },
+                        Err((cid, dtp, error)) => {
                             let error = ErrorEntry {
-                                case: format!("{}: {}", case_id, data_type),
-                                error: format!("Failed to convert data to JSON {}", e),
+                                case: format!("{}: {}", cid, dtp),
+                                error,
                             };
                             let error_js = serde_json::to_string(&error).unwrap();
                             eprintln!("{}", error_js);
                         }
-                    },
-                    Err((cid, dtp, error)) => {
-                        let error = ErrorEntry {
-                            case: format!("{}: {}", cid, dtp),
-                            error,
-                        };
-                        let error_js = serde_json::to_string(&error).unwrap();
-                        eprintln!("{}", error_js);
                     }
-                },
+                }
                 Err((case_id, data_type, error)) => {
                     let error = ErrorEntry {
                         case: format!("{}: {}", case_id, data_type),
@@ -249,7 +316,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await;
 
     // Handle the result of the input timeout operation
-    let input_js: HashMap<String, DataType> = match result {
+    let input_js: InputData = match result {
         Ok(Ok(buffer)) => match serde_json::from_str(&buffer) {
             Ok(js) => js,
             Err(e) => {
@@ -290,8 +357,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
+    // Check if case_files is present and not empty
+    if input_js.case_files.is_empty() {
+        let stdin_error = ErrorEntry {
+            case: String::new(),
+            error: "caseFiles is missing or empty in input data.".to_string(),
+        };
+        let stdin_error_js = serde_json::to_string(&stdin_error).unwrap();
+        writeln!(io::stderr(), "{}", stdin_error_js).expect("Failed to output stderr!");
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "caseFiles is missing or empty from input JSON.",
+        )) as Box<dyn std::error::Error>);
+    };
+    let case_files = input_js.case_files;
+
+    // Set default maf_options
+    let (min_total_depth, min_alt_allele_count) = match input_js.maf_options {
+        Some(options) => (options.min_total_depth, options.min_alt_allele_count),
+        None => (10, 2), // Default values
+    };
+
     // Download data
-    download_data(input_js, HOST).await;
+    download_data(case_files, HOST, min_total_depth, min_alt_allele_count).await;
 
     Ok(())
 }
