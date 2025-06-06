4d20b34c763cb9a55e6c29c816aa37802f2bd3aa
congyu-lu <clu@stjude.com>
2025-06-06 16:11:38 -0500

    expect get() return cases passed filter0

--- server/src/bam.gdc.js
+++ server/src/bam.gdc.js
@@ -325,7 +325,7 @@ async function getBamfileByFileId(id, field, q) {
 	return re
 }
 
-export function getQueryApi(ds) {
+function getQueryApi(ds) {
 	// Should be generated once per dataset, in this case once for gdc only
 	if (queryApi) throw `The gdc queryApi has already been set.`
 
