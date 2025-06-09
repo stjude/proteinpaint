9d8973897f032c3de6d36ce38041f450469568ed
Colleen Reilly <creilly@stjude.org>
2025-06-09 14:41:27 -0500

    Add color to at annotations table

--- client/plots/wsiviewer/viewModelNew/ViewModel.ts
+++ client/plots/wsiviewer/viewModelNew/ViewModel.ts
@@ -34,18 +34,28 @@ export class ViewModel {
 	setAnnonationsTableData(viewData: any, imageData: WSImage) {
 		if (!imageData?.annotationsData?.length) return
 
-		//Remove filter() after development
-		const annotationsRows: any = imageData
-			.annotationsData!.filter((_, i) => i < 30)
-			.map((d, i) => {
-				return [{ value: i }, { value: d.zoomCoordinates }, { value: roundValue(d.uncertainty, 4) }, { value: d.class }]
-			})
+		const annotationsRows: any = imageData.annotationsData!.map((d, i) => {
+			const color = imageData?.classes?.find(c => c.label === d.class)?.color || ''
+
+			return [
+				{ value: i }, // Index
+				{ value: d.zoomCoordinates },
+				{ value: roundValue(d.uncertainty, 4) },
+				{
+					html: `<span style="display:inline-block;width:10px;height:20px;background-color:${color};border:grey 1px solid;"></span>`
+				}, //Show the color next to the class
+				{ value: d.class },
+				{ value: '' } // Annotated class will be set later
+			]
+		})
 
 		const columns = [
 			{ label: 'Index', sortable: true, align: 'center' },
 			{ label: 'Coordinates' },
 			{ label: 'Uncertainty', sortable: true },
-			{ label: 'Model-Predicted Class', sortable: true }
+			{ label: '', align: 'center' }, //Show the color next to the class
+			{ label: 'Model-Predicted Class', sortable: true },
+			{ label: 'Annotated Class', sortable: true }
 		]
 
 		viewData.annotations = {
@@ -62,7 +72,9 @@ export class ViewModel {
 		for (const c of imageData.classes) {
 			classRows.push([
 				{ value: c.label },
-				{ html: `<span style="display:inline-block;width:20px;height:20px;background-color:${c.color}"></span>` },
+				{
+					html: `<span style="display:inline-block;width:20px;height:20px;background-color:${c.color};border:grey 1px solid;"></span>`
+				},
 				{ value: c.shortcut.replace('Key', '').replace('Digit', '') }
 			])
 			shortcuts.push(c.shortcut)
