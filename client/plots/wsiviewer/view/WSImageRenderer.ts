a5e580cfed592587678743ee8ffd95b139a5fd7f
Colleen Reilly <creilly@stjude.org>
2025-06-11 15:38:36 -0500

    Show user class confirmations and changes in the annotation table

--- client/plots/wsiviewer/view/WSImageRenderer.ts
+++ client/plots/wsiviewer/view/WSImageRenderer.ts
@@ -77,6 +77,31 @@ export class WSImageRenderer {
 					tr.style('background-color', index === rowIdx ? selectedColor : origColor)
 				})
 
+				this.buffers.tmpClass.addListener((tmpClass: { label: string; color: string }) => {
+					if (this.buffers.annotationsIdx.get() === rowIdx) {
+						const spanHtml = `<span style="display:inline-block;width:12px;height:18px;background-color:${tmpClass.color};border:grey 1px solid;"></span>`
+
+						/** Need to update the data source. Otherwise user changes
+						 * disappear on sort. */
+						this.imageViewData.annotations!.rows[rowIdx][4].html = spanHtml
+						this.imageViewData.annotations!.rows[rowIdx][5].value = tmpClass.label
+
+						const predictedClassTd = tr.select('td:nth-child(4)')
+						const colorTd = tr.select('td:nth-child(5)')
+						const annotationTd = tr.select('td:nth-child(6)')
+
+						if (tmpClass.label != 'Confirmed') {
+							predictedClassTd.style('text-decoration', 'line-through').style('color', 'grey')
+						} else {
+							predictedClassTd.style('text-decoration', 'none').style('color', 'black')
+						}
+						colorTd.select('span').remove()
+						colorTd.html(spanHtml)
+						//Padding overrides the default padding
+						annotationTd.style('padding', '0px').text(tmpClass.label)
+					}
+				})
+
 				tr.on('click', () => {
 					this.buffers.annotationsIdx.set(rowIdx)
 					const coords = [this.imageViewData.annotations!.rows[rowIdx][1].value] as unknown as [number, number][]
