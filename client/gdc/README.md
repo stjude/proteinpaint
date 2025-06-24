d8471edd2b80e9df8e81f25fab71ddd7871c1135
siosonel <siosonel@gmail.com>
2025-06-23 21:27:30 -0500

    fix: clear all fetched server data when the username has changed in bindProteinPaint()

--- client/gdc/README.md
+++ client/gdc/README.md
@@ -1,5 +1,7 @@
 # GDC plot launchers
 
+## Background
+
 These code are specifically built for various PP features to work in GDC https://portal.gdc.cancer.gov/
 Each file corresponds to a card in the GDC Analysis Center (separately coded in GDC Frontend Framework https://github.com/NCI-GDC/gdc-frontend-framework)
 
@@ -23,3 +25,12 @@ and such purpose-specific key will trigger client/src/app.js to load a script fi
 and this script file will load a PP plot which works for both gdc and non-gdc datasets.
 
 These scripts will supply gdc-specific data and configurations.
+
+## GDC login handling
+
+In SJ portal logins, we always refresh the app fully with window.location.reload() or similar,
+so that `/genomes` and `/termdb/config` responses with `{dsAuth}` and `{requiredAuth, clientAuthResult}`
+are correct when starting mass app. So, the difference from non-GDC usage/portals is that `client/gdc/` 
+code use `.update()` and not a full `runpp()` refresh, since GDC portal is reactive with lots of irrelevant
+dispatches that has to be ignored - otherwise repetitive full refreshes would blink a lot and may create
+duplicate tracks due to race condition (`bindProteinPaint()` was created to correctly handle these issues).
--- server/src/test/routes/gdc.js
+++ server/src/test/routes/gdc.js
@@ -20,6 +20,15 @@ export default async function setRoutes(app, basepath) {
 	// so delay setting these optional routes (this is done in server/src/auth.js also)
 	await sleep(0)
 
+	app.get('/auth', logIn)
+	app.post('/auth', logIn)
+
+	app.get('/auth/user', getAuth)
+	app.post('/auth/user', getAuth)
+
+	app.get('/auth/logout', logOut)
+	app.post('/auth/logout', logOut)
+
 	app.post('/gdc/ssid', async (req, res) => {
 		const q = req.body
 		const i = sessions.indexOf(q.token)
@@ -76,4 +85,42 @@ export default async function setRoutes(app, basepath) {
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+const pastExpiration =
+	'sessionid=; Domain=.gdc.cancer.gov; expires=Sat, 01 Jan 2000 12:00:00 GMT; GMT; HttpOnly; Max-Age=1800; Path=/;HttpOnly; SameSite=Lax; Secure'
+let loggedIn = false,
+	numTries = 0
+
+function logIn(req, res) {
+	numTries++
+	if (numTries === 1) {
+		res.header('Set-Cookie', pastExpiration)
+		return // skip the initial page load
+	}
+	loggedIn = true
+	getAuth(req, res)
+}
+
+function logOut(req, res) {
+	loggedIn = false
+	// must be in the past
+	res.header('Set-Cookie', pastExpiration)
+	res.send({ status: 'ok' })
+}
+
+function getAuth(req, res) {
+	// res.header(
+	// 	'Access-Control-Allow-Origin',
+	// 	req.get('origin') || req.get('referrer') || req.protocol + '://' + req.get('host').split(':')[0] || '*'
+	// )
+	if (!loggedIn) {
+		res.status(401)
+		res.header('Set-Cookie', pastExpiration)
+		res.end()
+		return
+	}
+	// this cookie is copied from the response header after logging in to portal.gdc.cancer.gov
+	res.header('Set-Cookie', '')
+	res.send({})
 }
