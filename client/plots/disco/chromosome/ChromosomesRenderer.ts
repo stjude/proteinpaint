414fb9867efadaff7093d2d7fe32f188feb1db8e
ItsVictorLP <victorlopezperez357@gmail.com>
2025-06-27 12:29:34 -0500

    Added comments to code from previous commit

--- client/plots/disco/chromosome/ChromosomesRenderer.ts
+++ client/plots/disco/chromosome/ChromosomesRenderer.ts
@@ -37,20 +37,29 @@ export default class ChromosomesRenderer implements IRenderer {
 			.attr('d', arc)
 			.attr('fill', 'black')
 			.on('mousemove', (event: MouseEvent, d: d3.PieArcDatum<Chromosome>) => {
+				// Get mouse pointer coordinates relative to the arcs group
 				const [x, y] = d3.pointer(event, arcs.node())
+				// Convert Cartesian coordinates to polar angle (in radians)
 				let angle = Math.atan2(y, x) + Math.PI / 2
+				// Normalize angle to be within [0, 2π]
 				if (angle < 0) angle += 2 * Math.PI
+				// Compute how far along this arc segment the angle falls (fraction between 0 and 1)
 				const frac = Math.max(0, Math.min(1, (angle - d.data.startAngle) / (d.data.endAngle - d.data.startAngle)))
+				// Interpolate position along chromosome based on angle
 				const pos = Math.round(d.data.start + frac * d.data.size)
+				// Update tooltip text with chromosome name and position (formatted with commas)
 				menu.d.text(`Chr ${d.data.text}: Position: ${pos.toLocaleString()}`)
+				// Display tooltip at cursor position
 				menu.show(event.x, event.y)
 			})
 			.on('mouseover', event => {
+				// When hovering over an arc, highlight it with an orange border
 				d3.select(event.currentTarget as SVGElement)
 					.attr('stroke', 'orange')
 					.attr('stroke-width', 1)
 			})
 			.on('mouseout', event => {
+				// Remove highlight and hide tooltip when mouse leaves the arc
 				d3.select(event.currentTarget as SVGElement)
 					.attr('stroke', null)
 					.attr('stroke-width', null)
