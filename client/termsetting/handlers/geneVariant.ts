2cb8a33385a8ec2368ceaf840276f91cdf56392a
Gavriel Matt <gmatt@stjude.org>
2025-07-10 12:34:23 -0500

    geneVariant default to groupsetting in summary plot

--- client/termsetting/handlers/geneVariant.ts
+++ client/termsetting/handlers/geneVariant.ts
@@ -102,7 +102,10 @@ async function makeEditMenu(self: GeneVariantTermSettingInstance, _div: any) {
 		}
 	})
 
-	if (self.usecase?.detail == 'term0' || self.usecase?.detail == 'term2' || self.opts.geneVariantEditMenuOnlyGrp) {
+	if (
+		(self.usecase?.detail && ['term', 'term0', 'term2'].includes(self.usecase.detail)) ||
+		self.opts.geneVariantEditMenuOnlyGrp
+	) {
 		// hide option for turning off groupsetting
 		optsDiv.style('display', 'none')
 		groupsDiv.style('margin-top', '10px')
