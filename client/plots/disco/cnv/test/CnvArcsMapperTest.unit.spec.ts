e725bc06e4a4615db73b5e3e06514873e7dbe744
Colleen Reilly <creilly@stjude.org>
2025-07-17 11:17:29 -0500

    Code clean up; add documentation

--- client/plots/disco/cnv/test/CnvArcsMapperTest.unit.spec.ts
+++ client/plots/disco/cnv/test/CnvArcsMapperTest.unit.spec.ts
@@ -4,7 +4,6 @@ import Reference from '#plots/disco/chromosome/Reference.ts'
 import CnvArcsMapper from '#plots/disco/cnv/CnvArcsMapper.ts'
 import DataMapper from '#plots/disco/data/DataMapper.ts'
 import { CnvRenderingType } from '#plots/disco/cnv/CnvRenderingType.ts'
-import { hg38 } from '../../../../test/testdata/genomes'
 
 const overriders = { padAngle: 0.0 }
 const settings = discoDefaults(overriders)
@@ -18,39 +17,39 @@ const chromosomes = {
 const reference = new Reference(settings, chromosomes)
 
 const rawData = [
-		{
-			chr: 'chr1',
-			dt: 4,
-			start: 0,
-			stop: 100,
-			value: -1
-		},
-		{
-			chr: 'chr2',
-			dt: 4,
-			start: 0,
-			stop: 100,
-			value: 6
-		}
-	]
+	{
+		chr: 'chr1',
+		dt: 4,
+		start: 0,
+		stop: 100,
+		value: -1
+	},
+	{
+		chr: 'chr2',
+		dt: 4,
+		start: 0,
+		stop: 100,
+		value: 6
+	}
+]
 
-	const dataHolder = new DataMapper(settings, reference, sampleName, []).map(rawData)
+const dataHolder = new DataMapper(settings, reference, sampleName, []).map(rawData)
 
-	const data = dataHolder.cnvData
-	const cnvArcsMapper = new CnvArcsMapper(
-		10,
-		5,
-		settings,
-		'Sample',
-		reference,
-		dataHolder.percentilePositive,
-		dataHolder.percentileNegative,
-		dataHolder.cappedCnvMaxAbsValue,
-		dataHolder.cnvMaxPercentileAbs,
-		'Unit',
-		CnvRenderingType.bar
-	)
-	const arcs = cnvArcsMapper.map(data)
+const data = dataHolder.cnvData
+const cnvArcsMapper = new CnvArcsMapper(
+	10,
+	5,
+	settings,
+	'Sample',
+	reference,
+	dataHolder.percentilePositive,
+	dataHolder.percentileNegative,
+	dataHolder.cappedCnvMaxAbsValue,
+	dataHolder.cnvMaxPercentileAbs,
+	'Unit',
+	CnvRenderingType.bar
+)
+const arcs = cnvArcsMapper.map(data)
 
 test('CnvArcsMapper.map() should return an array of CnvArc objects', t => {
 	t.equal(arcs.length, 2, 'Number of arcs should be equal to the number of data items')
