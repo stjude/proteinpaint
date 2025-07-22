8c2fd76cbe6ef21d4edd8cc59bf3163b62ebd76f
Colleen Reilly <creilly@stjude.org>
2025-07-22 11:19:48 -0500

    Render filter UI on new project creation; update ad hoc build script to match filter needs.

--- client/plots/aiHistoTool/view/CreateProjectRender.ts
+++ client/plots/aiHistoTool/view/CreateProjectRender.ts
@@ -1,9 +1,30 @@
-export class CreateProjectRender {
-	terms: any[]
-	dom: any
+import { filterPromptInit } from '#filter'
 
-	constructor(terms: any[], dom: any) {
-		this.terms = terms
+export class CreateProjectRender {
+	dom: any
+	app: any
+
+	constructor(dom: any, app: any) {
 		this.dom = dom
+		this.app = app
+	}
+
+	render() {
+		const filterDiv = this.dom.holder
+			.append('div')
+			.attr('id', 'sjpp-ai-histo-tool-filter-div')
+			.attr('class', 'sjpp-deletable-ai-histo-div')
+
+		const filter = filterPromptInit({
+			holder: filterDiv,
+			emptyLabel: 'Add fitler',
+			vocabApi: this.app.vocabApi,
+			termdbConfig: this.app.vocabApi.termdbConfig,
+			debug: true,
+			callback: () => {
+				console.log('Filter applied')
+			}
+		})
+		filter.main({ type: 'tvslst', join: '', lst: [] })
 	}
 }
