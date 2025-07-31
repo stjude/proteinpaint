a89ad842a9a2fdbb047cd819d3d37d4307f75b3b
Airen Zaldivar <airenzp@gmail.com>
2025-07-31 17:16:25 -0500

    Checked null filter before joining

--- server/src/termdb.matrix.js
+++ server/src/termdb.matrix.js
@@ -372,7 +372,8 @@ export async function getSamplesPerFilter(q, ds) {
 	q.ds = ds
 	const samples = {}
 	for (const id in q.filters) {
-		const filter = q.filters[id] ? filterJoin([q.filter, q.filters[id]]) : q.filter
+		let filter = q.filters[id]
+		if (q.filter) filter = filterJoin([q.filter, q.filters[id]])
 		const result = (await get_samples({ filter, __protected__: q.__protected__ }, q.ds)).map(i => i.id)
 		samples[id] = Array.from(new Set(result))
 	}
