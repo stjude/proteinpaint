fb3d2364245a66ba16eebea05ed1b01099c5ba77
Airen Zaldivar <airenzp@gmail.com>
2025-08-07 14:11:55 -0500

    Renamed missing files

--- shared/types/checkers/termdb.filterTermValues.ts
+++ shared/types/checkers/termdb.filterTermValues.ts
@@ -1,8 +1,8 @@
 // this file was auto-generated using 'npm run generate' or 'npm run dev' from the shared/types dir
 import { createValidate } from 'typia'
-import type { FilterTermValuesRequest, FilterTermValuesResponse } from '../src/routes/filterTermValues.ts'
+import type { FilterTermValuesRequest, FilterTermValuesResponse } from '../src/routes/termdb.filterTermValues.ts'
 
-export { FilterTermValuesPayload } from '../src/routes/filterTermValues.ts'
+export { FilterTermValuesPayload } from '../src/routes/termdb.filterTermValues.ts'
 
 export const validFilterTermValuesRequest = createValidate<FilterTermValuesRequest>()
 export const validFilterTermValuesResponse = createValidate<FilterTermValuesResponse>()
