f07acb8f752f9b0beedb52cddf8195aafd85f600
Airen Zaldivar <airenzp@gmail.com>
2025-08-19 12:25:36 -0500

    If the data is filtered down to a sample should be a user sample

--- server/routes/termdb.profileFormScores.ts
+++ server/routes/termdb.profileFormScores.ts
@@ -49,7 +49,7 @@ async function getScoresDict(query, ds) {
 		},
 		ds
 	)
-	const lst = Object.values(data.samples)
+	const lst: any[] = Object.values(data.samples)
 	let sites = lst.map((s: any) => {
 		return { label: data.refs.bySampleId[s.sample].label, value: s.sample }
 	})
@@ -59,6 +59,12 @@ async function getScoresDict(query, ds) {
 	})
 	if (query.userSites) {
 		sites = sites.filter(s => query.userSites.includes(s.label))
+		if (lst.length == 1 && !sites.length) {
+			const siteId: number = lst[0].sample
+			const site = data.refs.bySampleId[siteId].label
+			const hospital = query.facilityTW.term.values[site].label
+			throw `The access to the hospital ${hospital} is denied`
+		}
 	}
 
 	let sitesSelected: any[] = []
