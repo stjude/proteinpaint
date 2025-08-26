70ff82e38f3bf9e64e9c983f4863545701e8c445
Colleen Reilly <creilly@stjude.org>
2025-08-26 15:52:06 -0500

    New end point for deleting annotations

---
+++ server/src/runSQLHelpers.ts
@@ -0,0 +1,39 @@
+import type Database from 'better-sqlite3'
+
+/** Run multiple SQL statements in a transaction
+ * More performant than running .prepare().run() each time */
+export function runMultiStmtSQL(
+	connection: Database.Database,
+	stmts: { sql: string; params: any[] }[],
+	errorText = 'fetch'
+) {
+	const transaction = connection.transaction((batch: typeof stmts) => {
+		for (const { sql, params = [] } of batch) {
+			//Reuse the same prepared statement for memory efficiency
+			const sqlStmt = connection.prepare(sql)
+			for (const item of params) {
+				sqlStmt.run(item)
+			}
+		}
+	})
+
+	try {
+		transaction(stmts)
+	} catch (e: any) {
+		console.error(`Error executing SQL transaction for ${errorText}: ${e.message || e}`)
+		throw new Error(`Failed to ${errorText}`)
+	}
+}
+
+/** Run only one SQL statement at a time */
+export function runSQL(connection: Database.Database, sql: string, params: string[] = [], errorText = 'fetch') {
+	try {
+		if (!params.length) {
+			return connection.prepare(sql).all()
+		}
+		return connection.prepare(sql).run(params)
+	} catch (e: any) {
+		console.error(`Error executing SQL for ${errorText}: ${e.message || e}`)
+		throw new Error(`Failed to ${errorText}`)
+	}
+}
