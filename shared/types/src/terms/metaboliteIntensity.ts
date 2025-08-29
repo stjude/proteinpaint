edb351025f8292969c0d1896940f158a5c7503f8
siosonel <siosonel@gmail.com>
2025-08-29 17:07:33 -0500

    add date and metaboliteIntensity to NumericTerm definition as a whole, instead of adding to dict term definition

--- shared/types/src/terms/metaboliteIntensity.ts
+++ shared/types/src/terms/metaboliteIntensity.ts
@@ -1,31 +1,28 @@
-import type { TermWrapper } from './tw.ts'
-import type { BaseQ } from './term.ts'
-import type { NumericTerm } from './numeric.ts'
+import type { BaseTW, NumericBaseTerm, NumericQ } from '../index'
 import type { TermSettingInstance } from '../termsetting.ts'
 
 /*
 --------EXPORTED--------
-MetaboliteIntensityQ
 MetaboliteIntensityTermWrapper
 MetaboliteIntensityTermSettingInstance
 
 */
 
-export type MetaboliteIntensityQ = BaseQ & {
-	mode: 'continuous'
-}
-
-export type MetaboliteIntensityTW = TermWrapper & {
-	q: MetaboliteIntensityQ
+export type MetaboliteIntensityTW = BaseTW & {
+	type: string // 'NumTWCont'
+	q: NumericQ
 	term: MetaboliteIntensityTerm
 }
 
-export type MetaboliteIntensityTerm = NumericTerm & {
+export type MetaboliteIntensityTerm = NumericBaseTerm & {
+	name?: string
+	type: 'metaboliteIntensity'
 	metabolite: string
 	bins: any
+	unit?: string
 }
 
 export type MetaboliteIntensityTermSettingInstance = TermSettingInstance & {
-	q: MetaboliteIntensityQ
+	q: NumericQ
 	term: MetaboliteIntensityTerm
 }
