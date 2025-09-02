be9f9112fe44e4e540573771b6f4515e8425a5fd
Airen Zaldivar <airenzp@gmail.com>
2025-09-02 14:59:55 -0500

    Renamed getName2Svg to getChartDict. Passed parent in the chart dict. Applied parent style in the report

--- client/dom/downloadMenu.ts
+++ client/dom/downloadMenu.ts
@@ -3,15 +3,14 @@ import { downloadSingleSVG, downloadAggregatedSVG } from '../common/svg.download
 
 export class DownloadMenu {
 	menu: Menu
-	name2svg: any
+	name2svg: { [name: string]: { svg: any; parent: any } }
 	holder: any
 	multipleSVGs: boolean
 	filename: string
 
-	constructor(name2svg, holder, filename = 'charts') {
+	constructor(name2svg, filename = 'charts') {
 		this.menu = new Menu({ padding: '0px' })
 		this.name2svg = name2svg
-		this.holder = holder
 		this.multipleSVGs = Object.keys(name2svg).length > 1
 		this.filename = filename.replace(/\s/g, '_')
 	}
@@ -24,7 +23,7 @@ export class DownloadMenu {
 			.attr('class', 'sja_menuoption sja_sharp_border')
 			.text('PDF')
 			.on('click', () => {
-				downloadSVGsAsPdf(this.name2svg, this.filename, this.holder)
+				downloadSVGsAsPdf(this.name2svg, this.filename)
 				this.menu.hide()
 			})
 		menuDiv
@@ -32,7 +31,7 @@ export class DownloadMenu {
 			.attr('class', 'sja_menuoption sja_sharp_border')
 			.text('SVG')
 			.on('click', () => {
-				downloadAggregatedSVG(this.name2svg, this.filename, this.holder)
+				downloadAggregatedSVG(this.name2svg, this.filename)
 				this.menu.hide()
 			})
 
@@ -42,15 +41,15 @@ export class DownloadMenu {
 				.attr('class', 'sja_menuoption sja_sharp_border')
 				.text('Multiple SVG')
 				.on('click', () => {
-					for (const [name, svg] of Object.entries(this.name2svg))
-						downloadSingleSVG(svg, name.replace(/\s/g, '_'), this.holder)
+					for (const [name, chart] of Object.entries(this.name2svg))
+						downloadSingleSVG(chart.svg.node(), name.replace(/\s/g, '_'), chart.parent)
 					this.menu.hide()
 				})
 		this.menu.show(x - 20, y - 10)
 	}
 }
 
-export async function downloadSVGsAsPdf(name2svg, filename, parent) {
+export async function downloadSVGsAsPdf(name2svg, filename) {
 	const JSPDF = await import('jspdf')
 	const { jsPDF } = JSPDF
 	/*
@@ -59,7 +58,7 @@ export async function downloadSVGsAsPdf(name2svg, filename, parent) {
 	Therefore, a simple import 'svg2pdf.js' without curly braces is all that is needed to apply its functionality. 
 	 */
 	await import('svg2pdf.js') // This import extends jsPDF with SVG functionality
-	const doc = new jsPDF('p', 'pt', 'a4') // p for portrait, l for landscape, points, A4 size
+	const doc = new jsPDF('portrait', 'pt', 'a4') // p for portrait, l for landscape, points, A4 size
 	doc.setFontSize(12)
 	const pageWidth = doc.internal.pageSize.getWidth() - 10
 	const pageHeight = doc.internal.pageSize.getHeight() - 10
@@ -68,22 +67,22 @@ export async function downloadSVGsAsPdf(name2svg, filename, parent) {
 	let y = 50
 	const x = 30
 	const ratio = 72 / 96 //convert pixels to pt
-	const styles: any[] = []
-	if (parent) {
-		const svgStyles = window.getComputedStyle(parent)
-		for (const [prop, value] of Object.entries(svgStyles)) {
-			if (prop.startsWith('font')) styles.push({ prop, value })
+
+	for (const [name, chart] of entries) {
+		const parent = chart.parent
+		const svg = chart.svg.node().cloneNode(true) //clone to avoid modifying the original
+		if (parent) {
+			const svgStyles = window.getComputedStyle(parent)
+			for (const [prop, value] of Object.entries(svgStyles)) {
+				if (prop.startsWith('font')) svg.style[prop] = value
+			}
 		}
-	}
-	for (const [name, svgObj] of entries) {
-		const svg = svgObj.node()
-		for (const style of styles) {
-			svg.style[style.prop] = style.value
-		}
+		parent.appendChild(svg) //Added otherwise does not print, will remove later
+
 		const rect = svg.getBoundingClientRect()
 		svg.setAttribute('viewBox', `0 0 ${rect.width} ${rect.height}`)
-		const width = Math.min(pageWidth, rect.width * ratio) - 20
-		const height = Math.min(pageHeight, rect.height * ratio) - 20
+		const width = Math.min(pageWidth, rect.width * ratio) - 10
+		const height = Math.min(pageHeight, rect.height * ratio) - 10
 		if (y + height > pageHeight - 20) {
 			doc.addPage()
 			y = 50
@@ -91,6 +90,7 @@ export async function downloadSVGsAsPdf(name2svg, filename, parent) {
 		doc.text(name, x + 10, y - 20)
 		await doc.svg(svg, { x, y, width, height })
 		y = y + height + 50
+		parent.removeChild(svg)
 	}
 	doc.save(filename + '.pdf')
 }
