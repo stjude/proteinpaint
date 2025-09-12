1597a59a90eb6435bc910c0c840be210a84639f8
Wang <jwang7@L230176>
2025-09-12 16:04:48 -0500

    add newformat flag to RnaseqGeneCount

--- shared/types/src/dataset.ts
+++ shared/types/src/dataset.ts
@@ -703,6 +703,8 @@ type RnaseqGeneCount = {
 	samplesFile?: string
 	/** Storage_type for storing data (HDF5) */
 	storage_type: 'HDF5'
+	/** temporary h5 file format flag */
+	newformat?: boolean
 }
 
 /** the metabolite query */
