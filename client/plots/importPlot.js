a967a1898045760fb3a9653dfc7ce04f6c13a03d
Gavriel Matt <gmatt@stjude.org>
2025-09-12 11:26:49 -0500

    Add comment

--- client/plots/importPlot.js
+++ client/plots/importPlot.js
@@ -2,7 +2,8 @@ export async function importPlot(chartType, notFoundMessage = '') {
 	// TODO: move to dynamic import of exact plot names here, instead of string-pattern,
 	// so that the bundler does not have to guess code file extension, directory names and letter casing
 	switch (chartType) {
-		/*case 'genomeBrowser':
+		/*// enable this when gb/GB.ts is ready to replace genomeBrowser.js
+		case 'genomeBrowser':
 			return await import('./gb/GB.ts')*/
 		case 'AIProjectAdmin':
 			return await import('./aiProjectAdmin/AIProjectAdmin.ts')
