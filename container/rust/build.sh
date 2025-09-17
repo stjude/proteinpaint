0e0bc991428d976a4047b2d6aad2224ee669b8fc
Aleksandar Acic <aacic@stjude.org>
2025-09-17 11:29:21 -0500

    Change docker image to ghcr.io/stjude/rust:latest

--- container/rust/build.sh
+++ container/rust/build.sh
@@ -16,5 +16,5 @@ fi
 # The podman buildx build command
 docker buildx build . \
   --file ./Dockerfile \
-  --tag "ghcr.io/stjude/rust_debian_testing:$TAG" \
+  --tag "ghcr.io/stjude/rust:$TAG" \
   $PLATFORM
