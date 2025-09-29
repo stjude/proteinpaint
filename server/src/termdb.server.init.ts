8292014dd3090b52177473072781df06dedeba37
siosonel <siosonel@gmail.com>
2025-09-29 18:57:11 -0500

    reuse ds checkAccessToSampleData() in getSingleSampleDatta()

--- server/src/termdb.server.init.ts
+++ server/src/termdb.server.init.ts
@@ -3,7 +3,6 @@ import { authApi } from './auth.js'
 import { isUsableTerm } from '#shared/termdb.usecase.js'
 import { DEFAULT_SAMPLE_TYPE, numericTypes } from '#shared/terms.js'
 import type { isSupportedChartCallbacks } from '#types'
-import { getFilterCTEs } from './termdb.filter.js'
 //import { interpolateSqlValues } from './termdb.sql.js'
 
 /*
@@ -548,25 +547,20 @@ export function server_init_db_queries(ds) {
 		return supportedChartTypes
 	}
 
-	q.getSingleSampleData = async function (sampleId, term_ids = [], filter, ds) {
-		const sampleTypes = new Set(term_ids.map(id => ds.cohort.termdb.term2SampleType.get(id)))
-		const filterCTEs = await getFilterCTEs(filter, ds, sampleTypes)
-		//check that the sample is accessible
-		if (filterCTEs) {
-			const params = [...filterCTEs.values, sampleId]
-			const query = `
-			WITH
-			${filterCTEs.filters}
-			select id 
-			from sampleidmap 
-			where id=? and id in ${filterCTEs.CTEname}
-			`
-			//console.log(interpolateSqlValues(query, params))
-			const rows = cn.prepare(query).all(params)
-			if (rows.length == 0) throw 'No accessible data found for the sample provided'
-		}
-		const termClause = !term_ids.length ? '' : `and term_id in (${term_ids.map(() => '?').join(',')})`
+	q.getSingleSampleData = async function (q, ds) {
+		const { sampleId, term_ids } = q
 
+		if (ds.cohort.termdb.checkAccessToSampleData) {
+			const samples = ds.cohort.db.connection.prepare(`SELECT name FROM sampleidmap WHERE id=?`).all(sampleId)
+			// pass sampleNames since portal token does not know internal sample ID-to-name mapping
+			const access = ds.cohort.termdb.checkAccessToSampleData?.(q, {
+				sampleCount: 1,
+				sampleNames: samples.map(s => s.name)
+			})
+			if (!access.canAccess) throw access.message || 'No accessible data found for the sample provided'
+		}
+
+		const termClause = !term_ids.length ? '' : `and term_id in (${term_ids.map(() => '?').join(',')})`
 		const query = `
 		select term_id, value, jsondata from ( select term_id, value 
 		from anno_categorical 
