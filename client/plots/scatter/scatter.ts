5c48991249c715acbdf8eb7f7f63a032c081fca7
Airen Zaldivar <airenzp@gmail.com>
2025-10-01 20:32:25 -0500

    Consider local filter in the survival and the scatters

--- client/plots/scatter/scatter.ts
+++ client/plots/scatter/scatter.ts
@@ -79,7 +79,7 @@ export class Scatter extends PlotBase implements RxComponent {
 			throw `No plot with id='${this.id}' found. Did you set this.id before this.api = getComponentApi(this)?`
 		}
 		const parentConfig: any = appState.plots.find(p => p.id === this.parentId)
-		const termfilter = getCombinedTermFilter(appState, parentConfig?.filter)
+		const termfilter = getCombinedTermFilter(appState, config.filter || parentConfig?.filter)
 
 		return {
 			config,
