b5ff8034ed2e71775a598c2755887552ebda2a62
Robin Paul <rpaul1@stjude.org>
2025-10-09 16:18:03 -0500

    temp9

--- rust/src/test_ai.rs
+++ rust/src/test_ai.rs
@@ -7,29 +7,43 @@ fn main() {}
 mod tests {
     use serde_json;
     use std::fs::{self};
-    use std::path::Path; // Importing custom rig module for invoking SJ GPU server
-
-    //#[tokio::test]
-    #[ignore]
+    use std::path::Path;
     struct UserQuery {
         user_prompt: String,
         has_gene_expression: bool,
         expected_json: String,
+        action_type: ActionType,
     }
 
+    enum ActionType {
+        // The (ground truth) action type of the query e.g. Summary, DE, survival etc.
+        Summary(),
+        DE(),
+    }
+
+    //#[tokio::test]
+    #[ignore]
     async fn user_prompts() {
         let user_prompts = vec![
             UserQuery {
                 user_prompt: String::from("Show molecular subtypes for men greater than 60 yrs"),
                 has_gene_expression: true,
+                action_type: ActionType::Summary(),
+                expected_json: String::from(
+                    "{\"action\":\"summary\",\"filter\":[{\"Categorical\":{\"term\":\"Sex\",\"value\":\"Male\"}},{\"Numeric\":{\"greaterThan\":60.0,\"lessThan\":null,\"term\":\"Age\"}}],\"summaryterms\":[{\"clinical\":\"Molecular subtype\"}]}",
+                ),
             },
             UserQuery {
                 user_prompt: String::from("Show TP53 gene expression between genders"),
                 has_gene_expression: true,
+                action_type: ActionType::Summary(),
+                expected_json: String::from(
+                    "{\"action\":\"summary\",\"summaryterms\":[{\"clinical\":\"Sex\"},{\"geneExpression\":\"TP53\"}]}",
+                ),
             },
         ];
-        let termdbtestdb: &str = "../../server/test/tp/files/hg38/TermdbTest/db";
-        let genedb: &str = "../../server/test/tp/anno/genes.hg38.test.db";
+        let termdbtestdb: &str = "../server/test/tp/files/hg38/TermdbTest/db";
+        let genedb: &str = "../server/test/tp/anno/genes.hg38.test.db";
         let temperature: f64 = 0.01;
         let max_new_tokens: usize = 512;
         let top_p: f32 = 0.95;
@@ -56,7 +70,7 @@ mod tests {
         let llm_backend_type = super::super::llm_backend::Sj();
 
         for user_input in user_prompts {
-            let final_output = super::super::run_pipeline(
+            let llm_output = super::super::run_pipeline(
                 &user_input.user_prompt,
                 comp_model.clone(),
                 embedding_model.clone(),
@@ -69,6 +83,15 @@ mod tests {
                 user_input.has_gene_expression,
             )
             .await;
+
+            match user_input.action_type {
+                ActionType::Summary() => {
+                    let llm_json_value: super::super::SummaryType = serde_json::from_str(&llm_output.unwrap()).expect("Did not get a valid JSON of type {action: summary, summaryterms:[{clinical: term1}, {geneExpression: gene}], filter:[{term: term1, value: value1}]} from the LLM");
+                    let expected_json_value: super::super::SummaryType = serde_json::from_str(&user_input.expected_json).expect("Did not get a valid JSON of type {action: summary, summaryterms:[{clinical: term1}, {geneExpression: gene}], filter:[{term: term1, value: value1}]} from the LLM");
+                    assert_eq!(llm_json_value, expected_json_value);
+                }
+                ActionType::DE() => {}
+            }
         }
     }
 }
