5b4c9953d5332c5b4161f4402fcfa32b8f76afa0
Robin Paul <robin.paul@stjude.org>
2025-10-14 16:53:36 -0500

    Implemented method to parse testdata from dataset json in ai test file

--- rust/src/test_ai.rs
+++ rust/src/test_ai.rs
@@ -5,6 +5,7 @@ fn main() {}
 
 #[cfg(test)]
 mod tests {
+    use crate::{AiJsonFormat, Charts};
     use serde_json;
     use std::fs::{self};
     use std::path::Path;
@@ -21,7 +22,32 @@ mod tests {
         DE(),
     }
 
-    //#[tokio::test]
+    #[derive(PartialEq, Debug, Clone, schemars::JsonSchema, serde::Serialize, serde::Deserialize)]
+    struct ServerConfig {
+        tpmasterdir: String,
+        llm_backend: String,
+        sj_apilink: String,
+        sj_comp_model_name: String,
+        sj_embedding_model_name: String,
+        ollama_apilink: String,
+        ollama_comp_model_name: String,
+        ollama_embedding_model_name: String,
+        genomes: Vec<Genomes>,
+    }
+
+    #[derive(PartialEq, Debug, Clone, schemars::JsonSchema, serde::Serialize, serde::Deserialize)]
+    struct Genomes {
+        name: String,
+        datasets: Vec<Dataset>,
+    }
+
+    #[derive(PartialEq, Debug, Clone, schemars::JsonSchema, serde::Serialize, serde::Deserialize)]
+    struct Dataset {
+        name: String,
+        aifiles: Option<String>, // For now aifiles are defined only for certain datasets
+    }
+
+    #[tokio::test]
     #[ignore]
     async fn user_prompts() {
         let user_prompts = vec![
@@ -54,44 +80,78 @@ mod tests {
         let data = fs::read_to_string(absolute_path).unwrap();
 
         // Parse the JSON data
-        let json: serde_json::Value = serde_json::from_str(&data).unwrap();
+        let serverconfig: ServerConfig = serde_json::from_str(&data).expect("JSON not in serverconfig.json format");
+        println!("serverconfig:{:?}", serverconfig);
 
-        // Initialize Myprovider client
-        let sjprovider_host = json["sj_apilink"].as_str().unwrap();
-        let sjprovider_embedding_model = json["sj_embedding_model_name"].as_str().unwrap();
-        let sjprovider_comp_model = json["sj_comp_model_name"].as_str().unwrap();
+        for genome in &serverconfig.genomes {
+            for dataset in &genome.datasets {
+                match &dataset.aifiles {
+                    Some(ai_json_file) => {
+                        println!("Testing dataset:{}", dataset.name);
+                        let ai_json_file_path = String::from("../../") + ai_json_file;
+                        let ai_json_file = Path::new(&ai_json_file_path);
 
-        let sj_client = super::super::sjprovider::Client::builder()
-            .base_url(sjprovider_host)
-            .build()
-            .expect("SJ server not found");
-        let embedding_model = sj_client.embedding_model(sjprovider_embedding_model);
-        let comp_model = sj_client.completion_model(sjprovider_comp_model);
-        let llm_backend_type = super::super::llm_backend::Sj();
+                        // Read the file
+                        let ai_data = fs::read_to_string(ai_json_file).unwrap();
+                        // Parse the JSON data
+                        let ai_json: AiJsonFormat =
+                            serde_json::from_str(&ai_data).expect("AI JSON file does not have the correct format");
 
-        for user_input in user_prompts {
-            let llm_output = super::super::run_pipeline(
-                &user_input.user_prompt,
-                comp_model.clone(),
-                embedding_model.clone(),
-                llm_backend_type.clone(),
-                temperature,
-                max_new_tokens,
-                top_p,
-                termdbtestdb,
-                genedb,
-                user_input.has_gene_expression,
-            )
-            .await;
+                        let genedb = String::from(&serverconfig.tpmasterdir) + &"/" + &ai_json.genedb;
+                        let dataset_db = String::from(&serverconfig.tpmasterdir) + &"/" + &ai_json.db;
 
-            match user_input.action_type {
-                ActionType::Summary() => {
-                    let llm_json_value: super::super::SummaryType = serde_json::from_str(&llm_output.unwrap()).expect("Did not get a valid JSON of type {action: summary, summaryterms:[{clinical: term1}, {geneExpression: gene}], filter:[{term: term1, value: value1}]} from the LLM");
-                    let expected_json_value: super::super::SummaryType = serde_json::from_str(&user_input.expected_json).expect("Did not get a valid JSON of type {action: summary, summaryterms:[{clinical: term1}, {geneExpression: gene}], filter:[{term: term1, value: value1}]} from the LLM");
-                    assert_eq!(llm_json_value, expected_json_value);
+                        for chart in ai_json.charts.clone() {
+                            match chart {
+                                Charts::Summary(testdata) => {
+                                    for ques_ans in testdata.TestData {
+                                        let user_input = ques_ans.question;
+                                    }
+                                }
+                                Charts::DE(testdata) => {} // To do
+                            }
+                        }
+                    }
+                    None => {}
                 }
-                ActionType::DE() => {}
             }
         }
+
+        // Initialize Myprovider client
+        //let sjprovider_host = json["sj_apilink"].as_str().unwrap();
+        //let sjprovider_embedding_model = json["sj_embedding_model_name"].as_str().unwrap();
+        //let sjprovider_comp_model = json["sj_comp_model_name"].as_str().unwrap();
+
+        //let sj_client = super::super::sjprovider::Client::builder()
+        //    .base_url(sjprovider_host)
+        //    .build()
+        //    .expect("SJ server not found");
+        //let embedding_model = sj_client.embedding_model(sjprovider_embedding_model);
+        //let comp_model = sj_client.completion_model(sjprovider_comp_model);
+        //let llm_backend_type = super::super::llm_backend::Sj();
+
+        //for user_input in user_prompts {
+        //    let llm_output = super::super::run_pipeline(
+        //        &user_input.user_prompt,
+        //        comp_model.clone(),
+        //        embedding_model.clone(),
+        //        llm_backend_type.clone(),
+        //        temperature,
+        //        max_new_tokens,
+        //        top_p,
+        //        termdbtestdb,
+        //        genedb,
+        //        user_input.has_gene_expression,
+        //    )
+        //    .await;
+
+        //    match user_input.action_type {
+        //        ActionType::Summary() => {
+        //            let llm_json_value: super::super::SummaryType = serde_json::from_str(&llm_output.unwrap()).expect("Did not get a valid JSON of type {action: summary, summaryterms:[{clinical: term1}, {geneExpression: gene}], filter:[{term: term1, value: value1}]} from the LLM");
+        //            let expected_json_value: super::super::SummaryType = serde_json::from_str(&user_input.expected_json).expect("Did not get a valid JSON of type {action: summary, summaryterms:[{clinical: term1}, {geneExpression: gene}], filter:[{term: term1, value: value1}]} from the LLM");
+        //            assert_eq!(llm_json_value, expected_json_value);
+        //        }
+        //        ActionType::DE() => {}
+        //    }
+        //}
     }
 }
