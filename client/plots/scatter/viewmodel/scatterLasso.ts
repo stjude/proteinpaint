a996b760fd9a0e400628c9350e5043bc759b50be
Airen Zaldivar <airenzp@gmail.com>
2025-10-14 16:08:19 -0500

    Use right term for the x value label

--- client/plots/scatter/viewmodel/scatterLasso.ts
+++ client/plots/scatter/viewmodel/scatterLasso.ts
@@ -174,7 +174,7 @@ export class ScatterLasso {
 		const columns: TableColumn[] = []
 		const first = group.items[0]
 		if ('sample' in first) columns.push(formatCell('Sample', 'label'))
-		if (this.scatter.config.term) columns.push(formatCell(this.scatter.config.colorTW.term.name, 'label'))
+		if (this.scatter.config.term) columns.push(formatCell(this.scatter.config.term.term.name, 'label'))
 		if (this.scatter.config.term2) columns.push(formatCell(this.scatter.config.term2.term.name, 'label'))
 		if (this.scatter.config.colorTW) columns.push(formatCell(this.scatter.config.colorTW.term.name, 'label'))
 		if (this.scatter.config.shapeTW) columns.push(formatCell(this.scatter.config.shapeTW.term.name, 'label'))
