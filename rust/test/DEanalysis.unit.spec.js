6b40c6a15417c2faac11f50248499cb0c2c69734
Wang <jwang7@L230176>
2025-10-19 23:01:22 -0500

    fix unit test DEanalysis.unit.spec.js

--- rust/test/DEanalysis.unit.spec.js
+++ rust/test/DEanalysis.unit.spec.js
@@ -23,7 +23,7 @@ tape('rust DE wilcoxon unit test', async function (test) {
 		control:
 			'2646,2674,2688,2744,2758,2786,2814,2842,2856,2884,2912,2926,2954,2968,2996,3010,3038,3080,3094,3122,3164,3220,3248,3416,3430,3458',
 		data_type: 'do_DE',
-		input_file: serverconfig.binpath + '/test/tp/files/hg38/TermdbTest/rnaseq/TermdbTest.geneCounts.h5',
+		input_file: serverconfig.binpath + '/test/tp/files/hg38/TermdbTest/rnaseq/TermdbTest.geneCounts.new.h5',
 		cachedir: serverconfig.cachedir,
 		min_count: 10,
 		min_total_count: 15,
@@ -41,54 +41,54 @@ tape('rust DE wilcoxon unit test', async function (test) {
 		}
 	)
 	// Gene1
-	const gene1_id = 'ENSG00000169962'
-	const gene1_out = out.find(user => user.gene_id === gene1_id)
-	const gene1_exp_out = JSON.parse(expJson).find(user => user.gene_id === gene1_id)
+	const gene1_name = 'TAS1R3'
+	const gene1_out = out.find(user => user.gene_name === gene1_name)
+	const gene1_exp_out = JSON.parse(expJson).find(user => user.gene_name === gene1_name)
 	test.ok(
 		gene1_out.original_p_value - gene1_exp_out.original_p_value < p_value_cutoff,
-		`For ${gene1_id}, original pvalue=${gene1_out.original_p_value}, expected pvalue=${gene1_exp_out.original_p_value}`
+		`For ${gene1_name}, original pvalue=${gene1_out.original_p_value}, expected pvalue=${gene1_exp_out.original_p_value}`
 	)
 	test.ok(
 		gene1_out.adjusted_p_value - gene1_exp_out.adjusted_p_value < p_value_cutoff,
-		`For ${gene1_id}, original adj_pvalue=${gene1_out.adjusted_p_value}, expected adj_pvalue=${gene1_exp_out.adjusted_p_value}`
+		`For ${gene1_name}, original adj_pvalue=${gene1_out.adjusted_p_value}, expected adj_pvalue=${gene1_exp_out.adjusted_p_value}`
 	)
 	test.ok(
 		gene1_out.fold_change - gene1_exp_out.fold_change < fold_change_cutoff,
-		`For ${gene1_id}, original fold change=${gene1_out.fold_change}, expected fold change=${gene1_exp_out.fold_change}`
+		`For ${gene1_name}, original fold change=${gene1_out.fold_change}, expected fold change=${gene1_exp_out.fold_change}`
 	)
 
 	// Gene2
-	const gene2_id = 'ENSG00000230368'
-	const gene2_out = out.find(user => user.gene_id === gene2_id)
-	const gene2_exp_out = JSON.parse(expJson).find(user => user.gene_id === gene2_id)
+	const gene2_name = 'FAM41C'
+	const gene2_out = out.find(user => user.gene_name === gene2_name)
+	const gene2_exp_out = JSON.parse(expJson).find(user => user.gene_name === gene2_name)
 	test.ok(
 		gene2_out.original_p_value - gene2_exp_out.original_p_value < p_value_cutoff,
-		`For ${gene2_id}, original pvalue=${gene2_out.original_p_value}, expected pvalue=${gene2_exp_out.original_p_value}`
+		`For ${gene2_name}, original pvalue=${gene2_out.original_p_value}, expected pvalue=${gene2_exp_out.original_p_value}`
 	)
 	test.ok(
 		gene2_out.adjusted_p_value - gene2_exp_out.adjusted_p_value < p_value_cutoff,
-		`For ${gene2_id}, original adj_pvalue=${gene2_out.adjusted_p_value}, expected adj_pvalue=${gene2_exp_out.adjusted_p_value}`
+		`For ${gene2_name}, original adj_pvalue=${gene2_out.adjusted_p_value}, expected adj_pvalue=${gene2_exp_out.adjusted_p_value}`
 	)
 	test.ok(
 		gene2_out.fold_change - gene2_exp_out.fold_change < fold_change_cutoff,
-		`For ${gene2_id}, original fold change=${gene2_out.fold_change}, expected fold change=${gene2_exp_out.fold_change}`
+		`For ${gene2_name}, original fold change=${gene2_out.fold_change}, expected fold change=${gene2_exp_out.fold_change}`
 	)
 
 	// Gene3
-	const gene3_id = 'ENSG00000067606'
-	const gene3_out = out.find(user => user.gene_id === gene3_id)
-	const gene3_exp_out = JSON.parse(expJson).find(user => user.gene_id === gene3_id)
+	const gene3_name = 'PRKCZ'
+	const gene3_out = out.find(user => user.gene_name === gene3_name)
+	const gene3_exp_out = JSON.parse(expJson).find(user => user.gene_name === gene3_name)
 	test.ok(
 		gene3_out.original_p_value - gene3_exp_out.original_p_value < p_value_cutoff,
-		`For ${gene3_id}, original pvalue=${gene3_out.original_p_value}, expected pvalue=${gene3_exp_out.original_p_value}`
+		`For ${gene3_name}, original pvalue=${gene3_out.original_p_value}, expected pvalue=${gene3_exp_out.original_p_value}`
 	)
 	test.ok(
 		gene3_out.adjusted_p_value - gene3_exp_out.adjusted_p_value < p_value_cutoff,
-		`For ${gene3_id}, original adj_pvalue=${gene3_out.adjusted_p_value}, expected adj_pvalue=${gene3_exp_out.adjusted_p_value}`
+		`For ${gene3_name}, original adj_pvalue=${gene3_out.adjusted_p_value}, expected adj_pvalue=${gene3_exp_out.adjusted_p_value}`
 	)
 	test.ok(
 		gene3_out.fold_change - gene3_exp_out.fold_change < fold_change_cutoff,
-		`For ${gene3_id}, original fold change=${gene3_out.fold_change}, expected fold change=${gene3_exp_out.fold_change}`
+		`For ${gene3_name}, original fold change=${gene3_out.fold_change}, expected fold change=${gene3_exp_out.fold_change}`
 	)
 	test.end()
 })
@@ -96,10 +96,11 @@ tape('rust DE wilcoxon unit test', async function (test) {
 // This tests the rust gene counts HDF5 query
 tape('rust DE sample search test from raw gene counts HDF5 file', async function (test) {
 	const inJson = {
-		input_file: serverconfig.binpath + '/test/tp/files/hg38/TermdbTest/rnaseq/TermdbTest.geneCounts.h5',
-		data_type: 'get_samples'
+		hdf5_file: serverconfig.binpath + '/test/tp/files/hg38/TermdbTest/rnaseq/TermdbTest.geneCounts.new.h5',
+		validate: true
 	}
-	const Rustout = await run_rust('DEanalysis', JSON.stringify(inJson))
+	const Rustout = await run_rust('readH5', JSON.stringify(inJson))
+	const RustoutJson = JSON.parse(Rustout)
 
 	const expJson = fs.readFileSync(
 		path.join(serverconfig.binpath + '/test/tp/files/hg38/TermdbTest', 'TermdbTest_DE_samples_exp_output.json'),
@@ -108,6 +109,12 @@ tape('rust DE sample search test from raw gene counts HDF5 file', async function
 		}
 	)
 
-	test.deepEqual(Rustout, expJson, 'Test rust DE sample search from raw gene counts HDF5 should match expected output')
+	const expectedArray = expJson.trim().split(',')
+
+	test.deepEqual(
+		RustoutJson.samples,
+		expectedArray,
+		'Test rust DE sample search from raw gene counts HDF5 should match expected output'
+	)
 	test.end()
 })
