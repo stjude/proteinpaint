315f0c52f96096bc49f8f36a31c3d8e41f79964b
Gavriel Matt <gmatt@stjude.org>
2025-10-23 12:10:01 -0500

    Migrate ViewModel to View to be able to detect this.blockInstance. Fix conditions for filling config.subMds3TkFilters[]

--- client/plots/gb/interactions/Interactions.ts
+++ client/plots/gb/interactions/Interactions.ts
@@ -20,33 +20,7 @@ export class Interactions {
 		})
 	}
 
-	// TODO: need to test this
-	maySaveTrackUpdatesToState = async (blockInstance, state) => {
-		/* following changes will be saved in state:
-		- when a mds3 subtk is created/updated, its tk.filterObj should be saved to state so it can be recovered from session
-		- a facet track is removed by user via block ui */
-		if (!blockInstance) return
-		const config = structuredClone(state.config)
-		for (const t of blockInstance.tklst) {
-			if (t.type == 'mds3' && t.filterObj) {
-				if (state.filter) {
-					if (JSON.stringify(t.filterObj) == JSON.stringify(state.filter)) {
-						// this tk filter is identical as state (mass global filter). this means the tk is the "main" tk and the filter was auto-added via mass global filter.
-						// do not add such filter in subMds3TkFilters[], that will cause an issue of auto-creating unwanted subtk on global filter change
-						continue
-					}
-				} else {
-					if (!config.subMds3TkFilters) config.subMds3TkFilters = []
-					config.subMds3TkFilters.push(t.filterObj)
-					// filter0?
-				}
-			}
-		}
-		if (config.trackLst?.activeTracks) {
-			// active facet tracks are inuse; if user deletes such tracks from block ui, must update state
-			const newLst = config.trackLst.activeTracks.filter(n => blockInstance.tklst.find(i => i.name == n))
-			config.trackLst.activeTracks = newLst
-		}
+	saveToState = async config => {
 		await this.app.save({
 			type: 'plot_edit',
 			id: this.id,
--- client/plots/gb/view/View.ts
+++ client/plots/gb/view/View.ts
@@ -1,6 +1,6 @@
 import { first_genetrack_tolist } from '#common/1stGenetk'
 import { Tabs } from '#dom'
-import { filterInit } from '#filter/filter'
+import { filterInit, filterJoin } from '#filter'
 import { appInit } from '#termdb/app'
 import { mayUpdateGroupTestMethodsIdx } from '../interactions/Interactions.ts'
 
@@ -10,6 +10,8 @@ export class View {
 	dom: any
 	opts: any
 	interactions: any
+	groupSampleCounts: any
+	pop2average: any
 	blockInstance: any
 	filterUI: any
 	constructor(state, data, dom, opts, interactions) {
@@ -22,9 +24,162 @@ export class View {
 	}
 
 	async main() {
+		const tklst = await this.generateTracks()
 		this.mayRenderGroups()
 		this.mayRenderVariantFilter()
-		await this.launchBlockWithTracks(this.data.tklst)
+		await this.launchBlockWithTracks(tklst)
+	}
+
+	async generateTracks() {
+		// handle multiple possibilities of generating genome browser tracks
+
+		const tklst: any = [] // list of tracks to be shown in block
+
+		if (this.state.config.snvindel?.shown) {
+			// show snvindel-based mds3 tk
+			if (this.data) {
+				// variant data has been precomputed
+				// launch custom mds3 tk to show the variants
+				// TODO move computing logic to official mds3 tk and avoid tricky workaround using custom tk
+				const tk = await this.launchCustomMds3tk(this.data)
+				if (tk) {
+					// tricky! will not return obj when block is already shown, since it updates data for existing tk obj
+					tklst.push(tk)
+				}
+			} else {
+				// official mds3 tk without precomputed tk data
+				const tk: any = {
+					type: 'mds3',
+					dslabel: this.state.vocab.dslabel,
+					onClose: async () => {
+						// on closing subtk, the filterObj corresponding to the subtk will be "removed" from subMds3TkFilters[], by regenerating the array
+						await this.maySaveTrackUpdatesToState()
+					},
+					// for showing disco etc as ad-hoc sandbox, persistently in the mass plotDiv, rather than a menu
+					newChartHolder: this.opts.plotDiv
+				}
+				// any cohort filter for this tk
+				{
+					const lst: any = []
+					// register both global filter and local filter to pass to mds3 data queries
+					if (this.state.filter?.lst?.length) lst.push(this.state.filter)
+					if (this.state.config.snvindel.filter) lst.push(this.state.config.snvindel.filter)
+					if (lst.length == 1) {
+						tk.filterObj = structuredClone(lst[0])
+					} else if (lst.length > 1) {
+						tk.filterObj = filterJoin(lst)
+					}
+					// TODO this will cause mds3 tk to show a leftlabel to indicate the filtering, which should be hidden
+				}
+				tklst.push(tk)
+
+				if (this.state.config?.subMds3TkFilters) {
+					for (const subFilter of this.state.config.subMds3TkFilters) {
+						// for every element, create a new subtk
+						const t2: any = {
+							type: 'mds3',
+							dslabel: this.state.vocab.dslabel,
+							// for showing disco etc as ad-hoc sandbox, persistently in the mass plotDiv, rather than a menu
+							newChartHolder: this.opts.plotDiv
+						}
+						if (this.state.filter?.lst?.length) {
+							// join sub filter with global
+							t2.filterObj = filterJoin([this.state.filter, subFilter])
+						} else {
+							// no global. only sub
+							t2.filterObj = structuredClone(subFilter)
+						}
+						tklst.push(t2)
+					}
+				}
+			}
+		}
+		if (this.state.config?.trackLst?.activeTracks?.length) {
+			// include active facet tracks
+			for (const n of this.state.config.trackLst.activeTracks) {
+				for (const f of this.state.config.trackLst.facets) {
+					for (const t of f.tracks) {
+						if (t.name == n) tklst.push(t)
+					}
+				}
+			}
+		}
+		if (this.state.config.ld?.tracks) {
+			for (const t of this.state.config.ld.tracks) {
+				if (t.shown) tklst.push(t)
+			}
+		}
+		return tklst
+	}
+
+	async launchCustomMds3tk(data) {
+		this.mayGetSampleCounts(data)
+
+		if (this.blockInstance) {
+			// block already launched. update data on the tk and rerender
+			const t2 = this.blockInstance.tklst.find(i => i.type == 'mds3')
+			t2.custom_variants = data.mlst
+
+			// details.groups[] may have changed. update label and tooltip callback etc, of tk numeric axis view mode object
+			furbishViewModeWithSnvindelComputeDetails(
+				this,
+				t2.skewer.viewModes.find(i => i.type == 'numeric')
+			)
+
+			t2.load()
+			return
+		}
+
+		const nm = {
+			// numeric mode object; to fill in based on snvindel.details{}
+			type: 'numeric',
+			inuse: true,
+			byAttribute: 'nm_axis_value'
+		}
+		furbishViewModeWithSnvindelComputeDetails(this, nm)
+		const tk = {
+			type: 'mds3',
+			// despite having custom data, still provide dslabel for the mds3 tk to function as an official dataset
+			dslabel: this.state.vocab.dslabel,
+			name: 'Variants',
+			custom_variants: data.mlst,
+			skewerModes: [nm]
+		}
+		return tk
+	}
+
+	mayGetSampleCounts(data) {
+		if (Number.isInteger(data.totalSampleCount_group1) || Number.isInteger(data.totalSampleCount_group2)) {
+			this.groupSampleCounts = [data.totalSampleCount_group1, data.totalSampleCount_group2]
+			this.pop2average = data.pop2average
+		}
+	}
+
+	maySaveTrackUpdatesToState = async () => {
+		/* following changes will be saved in state:
+		- when a mds3 subtk is created/updated, its tk.filterObj should be saved to state so it can be recovered from session
+		- a facet track is removed by user via block ui */
+		if (!this.blockInstance) return
+		const config = structuredClone(this.state.config)
+		for (const t of this.blockInstance.tklst) {
+			if (t.type == 'mds3' && t.filterObj) {
+				if (JSON.stringify(t.filterObj) == JSON.stringify(this.state.filter)) {
+					// this tk filter is identical as state (mass global filter). this means the tk is the "main" tk and the filter was auto-added via mass global filter.
+					// do not add such filter in subMds3TkFilters[], that will cause an issue of auto-creating unwanted subtk on global filter change
+					continue
+				} else {
+					if (!config.subMds3TkFilters) config.subMds3TkFilters = []
+					config.subMds3TkFilters.push(t.filterObj)
+					// filter0?
+				}
+			}
+		}
+		if (config.trackLst?.activeTracks) {
+			// active facet tracks are inuse; if user deletes such tracks from block ui, must update state
+			const newLst = config.trackLst.activeTracks.filter(n => this.blockInstance.tklst.find(i => i.name == n))
+			config.trackLst.activeTracks = newLst
+		}
+		await this.interactions.saveToState(config)
 	}
 
 	mayRenderGroups() {
@@ -237,7 +392,7 @@ export class View {
 			.style('margin-left', '10px')
 			.style('opacity', 0.6)
 
-		if (this.data.pop2average) {
+		if (this.pop2average) {
 			// info available, computed for the other group comparing against this population, display here
 			if (this.state.config.snvindel.details.groups[groupIdx == 1 ? 0 : 1]?.type == 'filter') {
 				/*
@@ -249,8 +404,8 @@ export class View {
 				e.g. when info-vs-population, despite the _partialData is still there, must not render it
 				*/
 				const lst: any = []
-				for (const k in this.data.pop2average) {
-					const value = this.data.pop2average[k]
+				for (const k in this.pop2average) {
+					const value = this.pop2average[k]
 					if (!Number.isFinite(value)) continue // if there are no samples involved in current view, admix value is null
 					lst.push(`${k}=${value.toFixed(2)}`)
 				}
@@ -305,7 +460,7 @@ export class View {
 
 		this.filterUI[groupIdx].main(this.getJoinedFilter(group))
 		div.select('.sjpp-gb-filter-count').remove()
-		const count = this.data.groupSampleCounts?.[groupIdx]
+		const count = this.groupSampleCounts?.[groupIdx]
 
 		if (Number.isInteger(count)) {
 			// quick fix! sample count for this group is already present from partial data, create field to display
@@ -528,16 +683,9 @@ export class View {
 			nobox: true,
 			tklst,
 			debugmode: this.opts.debug,
-			/*
-            // use onsetheight but not onloadalltk_always, so callback will be called on all tk updates, including removing tk
-            //onloadalltk_always:
-            onsetheight: () => {
-                // TODO on any tk update, collect tk config and save to state so they are recoverable from session
-                // FIXME this is not called at protein mode
-                this.maySaveTrackUpdatesToState()
-            }
-            */
-			onAddRemoveTk: () => this.interactions.maySaveTrackUpdatesToState(this.blockInstance, this.state)
+			onAddRemoveTk: async () => {
+				await this.maySaveTrackUpdatesToState()
+			}
 		}
 		if (this.state.config.blockIsProteinMode) {
 			// must be in protein mode and requires gene symbol
@@ -565,4 +713,51 @@ export class View {
 		const _ = await import('#src/block')
 		this.blockInstance = new _.Block(arg)
 	}
+}
+
+/* given group configuration, determine: numeric track axis label
+- viewmode.label as axis label of numeric mode
+- viewmode.tooltipPrintValue()
+*/
+function furbishViewModeWithSnvindelComputeDetails(self, viewmode) {
+	delete viewmode.tooltipPrintValue
+
+	const [g1, g2] = self.state.config.snvindel.details.groups
+	if (g1 && g2) {
+		if (g1.type == 'info' || g2.type == 'info') {
+			// either group is info field. value type can only be value difference
+			viewmode.label = 'Value difference'
+			return
+		}
+		// none of the group is info field. each group should derive AF and there can be different ways of comparing it from two groups
+		const testMethod =
+			self.state.config.snvindel.details.groupTestMethods[self.state.config.snvindel.details.groupTestMethodsIdx]
+		viewmode.label = testMethod.axisLabel || testMethod.name
+		if (testMethod.name == 'Allele frequency difference') {
+			// callback returns value separated by ' = ', which allows this to be also displayed in itemtable.js
+			viewmode.tooltipPrintValue = m => [{ k: 'AF diff', v: m.nm_axis_value }]
+		} else if (testMethod.name == "Fisher's exact test") {
+			viewmode.tooltipPrintValue = m => [{ k: 'p-value', v: m.p_value }]
+		}
+		return
+	}
+
+	// only 1 group
+	if (g1.type == 'info') {
+		const f = self.state.config.variantFilter?.terms?.find(i => i.id == g1.infoKey)
+		viewmode.label = f?.name || g1.infoKey
+		viewmode.tooltipPrintValue = m => [{ k: viewmode.label, v: m.info[g1.infoKey] }]
+		return
+	}
+	if (g1.type == 'filter') {
+		viewmode.label = 'Allele frequency'
+		viewmode.tooltipPrintValue = m => [{ k: 'Allele frequency', v: m.nm_axis_value }]
+		return
+	}
+	if (g1.type == 'population') {
+		viewmode.label = 'Allele frequency'
+		viewmode.tooltipPrintValue = m => [{ k: 'Allele frequency', v: m.nm_axis_value }]
+		return
+	}
+	throw 'unknown type of the only group'
 }
