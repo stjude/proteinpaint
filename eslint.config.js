ab7fe9ffaef27b5209fd7edd070f6871838a7ade
siosonel <siosonel@gmail.com>
2025-10-28 10:57:31 -0500

    specify js env in pre-commit and pre-push hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'eslint/config'
 import tsParser from '@typescript-eslint/parser'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import js from'@eslint/js'
-import fs from'fs'
-import path from'path'
-import { FlatCompat } from'@eslint/eslintrc'
+import js from '@eslint/js'
+import fs from 'fs'
+import path from 'path'
+import { FlatCompat } from '@eslint/eslintrc'
+import globals from 'globals'
 
 const __dirname = import.meta.dirname
 const compat = new FlatCompat({
@@ -15,7 +16,21 @@ const compat = new FlatCompat({
 const sjppDir = path.join(__dirname, '../../sjpp')
 const errOrWarn = fs.existsSync(sjppDir) ? 'error' : 'warn'
 
+const envGlobals =
+	process.env.JSENV == 'node'
+		? globals.node
+		: process.env.JSENV == 'browser'
+		? globals.browser
+		: globals['shared-node-browser']
+
+if ('AudioWorkletGlobalScope ' in envGlobals) {
+	// fix bug in globals package
+	envGlobals.AudioWorkletGlobalScope = envGlobals['AudioWorkletGlobalScope ']
+	delete envGlobals['AudioWorkletGlobalScope ']
+}
+
 export default defineConfig([
+	//js.configs.recommended,
 	{
 		ignores: [
 			'**/node_modules/**/*',
@@ -26,10 +41,10 @@ export default defineConfig([
 			'client/types/test/d3.type.spec.ts',
 			'container/coverage/server/*',
 			'public/',
-			'python/',
+			//'python/',
 			'shared/types/dist/*',
 			'shared/types/src/test/numeric.type.spec.ts',
-			'**/.*',
+			//'**/.*',
 			'**/bin/*',
 			'**/*bundle.*'
 		]
@@ -37,20 +52,18 @@ export default defineConfig([
 	{
 		files: ['**/*.ts'],
 		languageOptions: {
-			parser: tsParser
+			parser: tsParser,
+			globals: { ...envGlobals }
 		},
-
 		plugins: {
 			'@typescript-eslint': typescriptEslint
 		},
-
 		extends: compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'),
-
 		rules: {
+			'no-undef': 'error',
 			'no-unused-private-class-members': 'off',
 			'@typescript-eslint/no-explicit-any': 'off',
 			'@typescript-eslint/no-non-null-assertion': 'off',
-
 			'@typescript-eslint/no-unused-vars': [
 				errOrWarn,
 				{
@@ -59,7 +72,6 @@ export default defineConfig([
 					argsIgnorePattern: '^_'
 				}
 			],
-
 			'@typescript-eslint/consistent-type-imports': errOrWarn
 		}
 	}
