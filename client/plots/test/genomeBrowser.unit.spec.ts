9116666e6f814fb2371473425be615809962190e
Xin Zhou <xzhou82@gmail.com>
2025-10-29 08:50:20 -0500

    fix for gb unit test

--- client/plots/test/genomeBrowser.unit.spec.ts
+++ client/plots/test/genomeBrowser.unit.spec.ts
@@ -29,55 +29,31 @@ const vocabApi: any = {
 }
 
 tape('computeBlockModeFlag()', test => {
-	console.log('priority 1: with override f(config,true)')
-
-	config.blockIsProteinMode = undefined
-	computeBlockModeFlag(config, true)
-	test.equal(config.blockIsProteinMode, true, 'override from undefined to true')
-
-	config.blockIsProteinMode = false
-	computeBlockModeFlag(config, true)
-	test.equal(config.blockIsProteinMode, true, 'override from false to true')
-
-	config.blockIsProteinMode = true
-	computeBlockModeFlag(config, false)
-	test.equal(config.blockIsProteinMode, false, 'override from true to false')
-
-	console.log('priority 2: no override and has default f(config)')
-
-	config.blockIsProteinMode = false
-	computeBlockModeFlag(config)
-	test.equal(config.blockIsProteinMode, false, 'remains false')
-
-	config.blockIsProteinMode = true
-	computeBlockModeFlag(config)
-	test.equal(config.blockIsProteinMode, true, 'remains true')
-
-	console.log('priority 3: no override and default, auto compute f(config, null, vocabApi)')
-	delete config.blockIsProteinMode // MUST delete flag before every of following test
-	computeBlockModeFlag(config, null, vocabApi)
+	// MUST delete flag before every of following test
+	delete config.blockIsProteinMode
+	computeBlockModeFlag(config, vocabApi)
 	test.equal(config.blockIsProteinMode, false, 'no gbRestrictMode, no geneSearchResult, auto set to false')
 
 	delete config.blockIsProteinMode
 	config.geneSearchResult.geneSymbol = 'xx'
-	computeBlockModeFlag(config, null, vocabApi)
+	computeBlockModeFlag(config, vocabApi)
 	test.equal(config.blockIsProteinMode, true, 'no gbRestrictMode, has gene symbol, auto set to true')
 
 	delete config.blockIsProteinMode
 	vocabApi.termdbConfig.queries.gbRestrictMode = 'protein'
-	computeBlockModeFlag(config, null, vocabApi)
+	computeBlockModeFlag(config, vocabApi)
 	test.equal(config.blockIsProteinMode, true, 'gbRestrictMode=protein, auto set to true')
 
 	delete config.blockIsProteinMode
 	vocabApi.termdbConfig.queries.gbRestrictMode = 'genomic'
-	computeBlockModeFlag(config, null, vocabApi)
+	computeBlockModeFlag(config, vocabApi)
 	test.equal(config.blockIsProteinMode, false, 'gbRestrictMode=genomic, auto set to false')
 
 	delete config.blockIsProteinMode
 	vocabApi.termdbConfig.queries.gbRestrictMode = 'invalid'
 	test.throws(
 		function () {
-			computeBlockModeFlag(config, null, vocabApi)
+			computeBlockModeFlag(config, vocabApi)
 		},
 		/unknown gbRestrictMode/,
 		'throws on unknown gbRestrictMode'
