4c7fbcef363a6a4844877619292fe0ca2f974bba
siosonel <siosonel@gmail.com>
2025-10-31 11:52:58 -0500

    do not exit early in ci-version-update.sh, since it will stop the whole ci step when called in global scope

--- build/ci-version-update.sh
+++ build/ci-version-update.sh
@@ -83,72 +83,71 @@ if [[ "$EXISTINGTAG" != "" ]]; then
   # assume an interrupted release that needs to be resumed
   # if the tag exists, then the commit must also exist since a 
   # new tag is pushed after the branch commit
-  exit 0
-fi
+else
+  COMMITMSG="$TAG $UPDATED"
+  echo "$COMMITMSG"
+  echo "committing version change ..."
+  git config --global user.email "PPTeam@STJUDE.ORG"
+  git config --global user.name "PPTeam CI"
+  git add --all
+  git commit -m "$COMMITMSG"
 
-COMMITMSG="$TAG $UPDATED"
-echo "$COMMITMSG"
-echo "committing version change ..."
-git config --global user.email "PPTeam@STJUDE.ORG"
-git config --global user.name "PPTeam CI"
-git add --all
-git commit -m "$COMMITMSG"
+  git tag $TAG # the absolute reference to the repo state at the time of build
 
-git tag $TAG # the absolute reference to the repo state at the time of build
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  git reset --hard HEAD~1 # go back one commit on the current branch
+                          # to allow local branch to get fast-forwarded after pull, if applicable;
+                          # tag will now be ahead of branch by one commit
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git reset --hard HEAD~1 # go back one commit on the current branch
-                        # to allow local branch to get fast-forwarded after pull, if applicable;
-                        # tag will now be ahead of branch by one commit
+  git pull # may already be at current tip or new commits may have been added
+  # at this point, the local commit history will look like one of these
+  # (A)
+  # fast-forward         (B)
+  # where both are       merge
+  # at same commit     
+  #                          branch fast-forwarded to a new commit from remote
+  # tag,branch           tag |
+  #    |                    \| 
+  #    |                     |
+  #    |                     |
+  #
 
-git pull # may already be at current tip or new commits may have been added
-# at this point, the local commit history will look like one of these
-# (A)
-# fast-forward         (B)
-# where both are       merge
-# at same commit     
-#                          branch fast-forwarded to a new commit from remote
-# tag,branch           tag |
-#    |                    \| 
-#    |                     |
-#    |                     |
-#
+  # check in case another CI bumped the version in github while this CI was running
+  PULLED_VERSION="$(node -p "require('./package.json').version")"
+  if [[ "$PULLED_VERSION" != "$PREVIOUS_VERSION" ]]; then
+    echo "!!! Error: !!!"
+    echo "the remote version has changed from '$PREVIOUS_VERSION' to '$PULLED_VERSION' while this CI workflow/action was running"
+    echo "!!! ------ !!!"
+    exit 1
+  fi
 
-# check in case another CI bumped the version in github while this CI was running
-PULLED_VERSION="$(node -p "require('./package.json').version")"
-if [[ "$PULLED_VERSION" != "$PREVIOUS_VERSION" ]]; then
-  echo "!!! Error: !!!"
-  echo "the remote version has changed from '$PREVIOUS_VERSION' to '$PULLED_VERSION' while this CI workflow/action was running"
-  echo "!!! ------ !!!"
-  exit 1
-fi
+  # !!! DO NOT REBASE !!!
+  git merge $TAG # can either fast-forward, or merge if there are new commits from remote
+  # after merge,
+  # (A) tag and branch will remain at the same commit if no new branch commits were pulled from remote
+  #     - OR -
+  # (B) if new commits were pulled, the commit history will now look like this
+  #
+  #     branch, after tag is merged into a new commit tip from remote
+  #    /|
+  # tag |
+  #    \| 
+  #     |
+  #     |
+  # 
 
-# !!! DO NOT REBASE !!!
-git merge $TAG # can either fast-forward, or merge if there are new commits from remote
-# after merge,
-# (A) tag and branch will remain at the same commit if no new branch commits were pulled from remote
-#     - OR -
-# (B) if new commits were pulled, the commit history will now look like this
-#
-#     branch, after tag is merged into a new commit tip from remote
-#    /|
-# tag |
-#    \| 
-#     |
-#     |
-# 
+  if [[ "$PWD" != *"/sjpp/"* ]]; then
+    # must be in the remote ci environment
+    git push origin $BRANCH # synchronize branch tip with remote, may already be up-to-date 
+                            # or will update remote commit history to look like (B) after git merge diagram
+    git push origin $TAG    # remote should not have this tag yet, should error if it's already there (non-fast-forward)
 
-if [[ "$PWD" != *"/sjpp/"* ]]; then
-  # must be in the remote ci environment
-  git push origin $BRANCH # synchronize branch tip with remote, may already be up-to-date 
-                          # or will update remote commit history to look like (B) after git merge diagram
-  git push origin $TAG    # remote should not have this tag yet, should error if it's already there (non-fast-forward)
-
-  # IMPORTANT:
-  # subsequent steps after this script must use the tagged commit, since the pulled branch tip may have moved
-  git reset --hard $TAG # if the branch tip did not move after pull, then nothing changes with this reset;
-                 # if it moved, this will revert to the tagged commit and the commit history will look like (A) in the diagram above after git pull
-else 
-  # /sjpp/ in the path means a dev environment
-  echo "to undo release steps in dev, run: 'git reset --hard HEAD^1 && git tag -d $TAG'"
+    # IMPORTANT:
+    # subsequent steps after this script must use the tagged commit, since the pulled branch tip may have moved
+    git reset --hard $TAG # if the branch tip did not move after pull, then nothing changes with this reset;
+                   # if it moved, this will revert to the tagged commit and the commit history will look like (A) in the diagram above after git pull
+  else 
+    # /sjpp/ in the path means a dev environment
+    echo "to undo release steps in dev, run: 'git reset --hard HEAD^1 && git tag -d $TAG'"
+  fi
 fi
