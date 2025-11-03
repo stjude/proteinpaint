ee9fb2a16846e0567af5b61f1f18119b5c90a507
Airen Zaldivar <airenzp@gmail.com>
2025-11-03 09:44:27 -0600

    Pass color to pattern rect

--- client/plots/profileBarchart.js
+++ client/plots/profileBarchart.js
@@ -103,7 +103,7 @@ class profileBarchart extends profilePlot {
 			.append('path')
 			.attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
 			.attr('stroke-width', 1)
-			.attr('stroke', 'gray')
+			.attr('stroke', color)
 
 		let x
 		let y
