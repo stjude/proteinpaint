00885a357bebd8aa38777ae5eeb1bd5ec5a6a374
Andrew Willems <andrew.willems@stjude.org>
2025-11-05 14:56:15 -0600

    Test to check more data types. Test to check that when all dts are unchecked the run button is disabled

--- client/plots/grin2/test/grin2.integration.spec.ts
+++ client/plots/grin2/test/grin2.integration.spec.ts
@@ -92,6 +92,163 @@ tape('grin2 fusion-only', function (test) {
 	}
 })
 
+// TODO: No SV data in termdbtest. Will write one once we have some
+
+tape('grin2 cnv-only', function (test) {
+	runpp({
+		state: {
+			plots: [{ chartType: 'grin2' }]
+		},
+		grin2: {
+			callbacks: {
+				'postRender.test': runCNVTest
+			}
+		}
+	})
+
+	async function runCNVTest(g) {
+		try {
+			// Use the DOM handles
+			const snvInput = g.Inner.dom.snvindelCheckbox.node() as HTMLInputElement
+			const cnvInput = g.Inner.dom.cnvCheckbox.node() as HTMLInputElement
+			const fusionInput = g.Inner.dom.fusionCheckbox.node() as HTMLInputElement
+			const svInput = g.Inner.dom.svCheckbox.node() as HTMLInputElement
+
+			test.ok(snvInput && cnvInput && fusionInput && svInput, 'All lesion data type checkbox inputs are present')
+
+			// Toggle checkboxes to cnv only
+			snvInput.checked = false
+			snvInput.dispatchEvent(new Event('input', { bubbles: true }))
+			cnvInput.checked = true
+			cnvInput.dispatchEvent(new Event('input', { bubbles: true }))
+			fusionInput.checked = false
+			fusionInput.dispatchEvent(new Event('input', { bubbles: true }))
+			svInput.checked = false
+			svInput.dispatchEvent(new Event('input', { bubbles: true }))
+
+			// Run analysis
+			g.Inner.dom.runButton.node().dispatchEvent(new Event('click', { bubbles: true }))
+
+			// Confirm plot rendered
+			const svg = await detectOne({
+				elem: g.Inner.dom.div.node(),
+				selector: '[data-testid="sjpp-manhattan"]'
+			})
+			test.ok(svg, 'CNV-only run rendered <svg>')
+
+			if (test['_ok']) g.Inner.app.destroy()
+		} catch (e) {
+			test.fail(e instanceof Error ? e.message : String(e))
+		} finally {
+			test.end()
+		}
+	}
+})
+
+tape('grin2 snvindel-only', function (test) {
+	runpp({
+		state: {
+			plots: [{ chartType: 'grin2' }]
+		},
+		grin2: {
+			callbacks: {
+				'postRender.test': runSNVIndelTest
+			}
+		}
+	})
+
+	async function runSNVIndelTest(g) {
+		try {
+			// Use the DOM handles
+			const snvInput = g.Inner.dom.snvindelCheckbox.node() as HTMLInputElement
+			const cnvInput = g.Inner.dom.cnvCheckbox.node() as HTMLInputElement
+			const fusionInput = g.Inner.dom.fusionCheckbox.node() as HTMLInputElement
+			const svInput = g.Inner.dom.svCheckbox.node() as HTMLInputElement
+
+			test.ok(snvInput && cnvInput && fusionInput && svInput, 'All lesion data type checkbox inputs are present')
+
+			// Toggle checkboxes to snvindel only
+			snvInput.checked = true
+			snvInput.dispatchEvent(new Event('input', { bubbles: true }))
+			cnvInput.checked = false
+			cnvInput.dispatchEvent(new Event('input', { bubbles: true }))
+			fusionInput.checked = false
+			fusionInput.dispatchEvent(new Event('input', { bubbles: true }))
+			svInput.checked = false
+			svInput.dispatchEvent(new Event('input', { bubbles: true }))
+
+			// Run analysis
+			g.Inner.dom.runButton.node().dispatchEvent(new Event('click', { bubbles: true }))
+
+			// Confirm plot rendered
+			const svg = await detectOne({
+				elem: g.Inner.dom.div.node(),
+				selector: '[data-testid="sjpp-manhattan"]'
+			})
+			test.ok(svg, 'SNVINDEL-only run rendered <svg>')
+
+			if (test['_ok']) g.Inner.app.destroy()
+		} catch (e) {
+			test.fail(e instanceof Error ? e.message : String(e))
+		} finally {
+			test.end()
+		}
+	}
+})
+
+tape('grin2 all-data-types-unchecked disables run button', function (test) {
+	runpp({
+		state: {
+			plots: [{ chartType: 'grin2' }]
+		},
+		grin2: {
+			callbacks: {
+				'postRender.test': runDtUncheckedTest
+			}
+		}
+	})
+
+	async function runDtUncheckedTest(g) {
+		try {
+			const snvInput = g.Inner.dom.snvindelCheckbox.node() as HTMLInputElement
+			const cnvInput = g.Inner.dom.cnvCheckbox.node() as HTMLInputElement
+			const fusionInput = g.Inner.dom.fusionCheckbox.node() as HTMLInputElement
+			const svInput = g.Inner.dom.svCheckbox.node() as HTMLInputElement
+
+			test.ok(snvInput && cnvInput && fusionInput && svInput, 'All data type checkboxes exist')
+
+			// Uncheck all data types
+			snvInput.checked = false
+			snvInput.dispatchEvent(new Event('input', { bubbles: true }))
+
+			cnvInput.checked = false
+			cnvInput.dispatchEvent(new Event('input', { bubbles: true }))
+
+			fusionInput.checked = false
+			fusionInput.dispatchEvent(new Event('input', { bubbles: true }))
+
+			svInput.checked = false
+			svInput.dispatchEvent(new Event('input', { bubbles: true }))
+
+			// Check the checkboxes are actually unchecked
+			test.equal(snvInput.checked, false, 'SNV unchecked')
+			test.equal(cnvInput.checked, false, 'CNV unchecked')
+			test.equal(fusionInput.checked, false, 'Fusion unchecked')
+			test.equal(svInput.checked, false, 'SV unchecked')
+
+			// Check Run button is disabled
+			const runBtn = g.Inner.dom.runButton.node() as HTMLButtonElement
+			test.equal(runBtn.disabled, true, 'Run button is disabled when no data types selected')
+
+			if (test['_ok']) g.Inner.app.destroy()
+		} catch (e) {
+			test.fail(e instanceof Error ? e.message : String(e))
+		} finally {
+			test.end()
+		}
+	}
+})
+
 /*************************
  reusable helper functions
 **************************/
