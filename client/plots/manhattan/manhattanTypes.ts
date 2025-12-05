5e22392e19280636759990dbefcdb122ce6f5273
Andrew Willems <andrew.willems@stjude.org>
2025-12-05 16:26:51 -0600

    Initial attempt of removing svg circles from plot

---
+++ client/plots/manhattan/manhattanTypes.ts
@@ -0,0 +1,15 @@
+export interface ManhattanPoint {
+	chrom: string
+	color: string
+	end: number
+	gene: string
+	nsubj: number
+	pixel_x: number
+	pixel_y: number
+	pos: number
+	q_value: number
+	start: number
+	type: string
+	x: number
+	y: number
+}
