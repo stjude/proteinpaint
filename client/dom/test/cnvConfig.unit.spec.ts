d85fa9d746cda95b8a996fa49a119c64970c8d12
Gavriel Matt <gmatt@stjude.org>
2025-12-08 11:40:59 -0600

    Make cnv cutoffs optional

--- client/dom/test/cnvConfig.unit.spec.ts
+++ client/dom/test/cnvConfig.unit.spec.ts
@@ -6,10 +6,12 @@ import { select } from 'd3-selection'
 test sections:
     - basic render
     - input values
+	- null max length input
+	- no gain/loss cutoffs
+	- no cutoffs
     - apply callback
     - wildtype toggle
     - no wildtype toggle
-    - null max length input
 */
 
 tape('\n', test => {
@@ -55,6 +57,77 @@ tape('input values', test => {
 	test.equal(gainInput.property('value'), '0.5', 'gain input has correct initial value')
 	test.equal(lossInput.property('value'), '-0.5', 'loss input has correct initial value')
 	test.equal(maxLenInput.property('value'), '500', 'max length input has correct initial value')
+
+	holder.remove()
+	test.end()
+})
+
+tape('null max length input', test => {
+	const holder = select(document.body).append('div')
+	let newConfig
+
+	renderCnvConfig({
+		holder,
+		cnvGainCutoff: 0.5,
+		cnvLossCutoff: -0.5,
+		cnvMaxLength: null,
+		callback: config => (newConfig = config)
+	})
+
+	const maxLenInput = holder.select('input[data-testid="sjpp-cnv-length-input"]')
+	test.equal(maxLenInput.property('value'), '-1', 'max length input shows -1 when initialized as null')
+
+	// apply with -1
+	const applyBtn = holder.select('button').node() as HTMLButtonElement
+	applyBtn.click()
+
+	test.equal(newConfig.cnvMaxLength, null, 'callback normalizes -1 to null')
+
+	// apply with new value
+	maxLenInput.property('value', 500)
+	applyBtn.click()
+	test.equal(newConfig.cnvMaxLength, 500, 'callback receives updated max length')
+
+	holder.remove()
+	test.end()
+})
+
+tape('no gain/loss cutoffs', test => {
+	const holder = select(document.body).append('div')
+
+	renderCnvConfig({
+		holder,
+		cnvMaxLength: 500,
+		callback: () => {}
+	})
+
+	const gainInput = holder.select('input[data-testid="sjpp-cnv-gain-input"]').node() as HTMLInputElement
+	const lossInput = holder.select('input[data-testid="sjpp-cnv-loss-input"]').node() as HTMLInputElement
+	const maxLenInput = holder.select('input[data-testid="sjpp-cnv-length-input"]').node() as HTMLInputElement
+
+	test.notOk(gainInput, 'gain input should not be rendered')
+	test.notOk(lossInput, 'loss input should not be rendered')
+	test.equal(maxLenInput.value, '500', 'max length input has correct initial value')
+
+	holder.remove()
+	test.end()
+})
+
+tape('no cutoffs', test => {
+	const holder = select(document.body).append('div')
+
+	renderCnvConfig({
+		holder,
+		callback: () => {}
+	})
+
+	const gainInput = holder.select('input[data-testid="sjpp-cnv-gain-input"]').node() as HTMLInputElement
+	const lossInput = holder.select('input[data-testid="sjpp-cnv-loss-input"]').node() as HTMLInputElement
+	const maxLenInput = holder.select('input[data-testid="sjpp-cnv-length-input"]').node() as HTMLInputElement
+
+	test.notOk(gainInput, 'gain input should not be rendered')
+	test.notOk(lossInput, 'loss input should not be rendered')
+	test.notOk(maxLenInput, 'max length input should not be rendered')
 
 	holder.remove()
 	test.end()
@@ -133,36 +206,6 @@ tape('no wildtype toggle', test => {
 
 	const wtCheckbox = holder.select('input[type="checkbox"]').node()
 	test.notOk(wtCheckbox, 'wildtype checkbox not rendered when WTtoggle=false')
-
-	holder.remove()
-	test.end()
-})
-
-tape('null max length input', test => {
-	const holder = select(document.body).append('div')
-	let newConfig
-
-	renderCnvConfig({
-		holder,
-		cnvGainCutoff: 0.5,
-		cnvLossCutoff: -0.5,
-		cnvMaxLength: null,
-		callback: config => (newConfig = config)
-	})
-
-	const maxLenInput = holder.select('input[data-testid="sjpp-cnv-length-input"]')
-	test.equal(maxLenInput.property('value'), '-1', 'max length input shows -1 when initialized as null')
-
-	// apply with -1
-	const applyBtn = holder.select('button').node() as HTMLButtonElement
-	applyBtn.click()
-
-	test.equal(newConfig.cnvMaxLength, null, 'callback normalizes -1 to null')
-
-	// apply with new value
-	maxLenInput.property('value', 500)
-	applyBtn.click()
-	test.equal(newConfig.cnvMaxLength, 500, 'callback receives updated max length')
 
 	holder.remove()
 	test.end()
