1eafd2be03b6f544c2b5b21fabfb14f7fc0fdba3
siosonel <siosonel@gmail.com>
2025-12-10 11:16:31 -0600

    fix: detect navigable svg text and g elements in lollipop SSM and matrix plots, to include in navigation by keyboard

--- client/src/app.observer.js
+++ client/src/app.observer.js
@@ -1,14 +1,26 @@
 import { select as d3select } from 'd3-selection'
 
-export const focusableSjClasses = [
+export const clickableSjClasses = [
 	'sja_menuoption',
 	'sja_clb',
 	'sja_clbtext',
 	'sja_clbtext2',
 	'sjpp-dslabel',
 	'sjpp-active-tiny-button',
-	'sja_inset_a'
+	'sja_inset_a',
+	'sjpp-matrix-label',
+	'sjpp-matrix-divide-by-label',
+	'sjpp-matrix-legend-g'
 ]
+
+// buttons, inputs, a (link) and other elements have native support
+// for tab and Enter-to-click keyboard navigation; the tagNames below
+// are for elements that do not natively support keyboard navigation,
+// and will be detected as requiring support if they have one of the
+// clickableSjClasses
+const svgClickable = new Set(['text' /*, 'rect'*/])
+const targetTagNames = new Set(['DIV', 'SPAN', 'TEXT', 'LABEL', 'g', ...[...svgClickable]])
+
 export const renderWait = 500
 const observedElems = new WeakSet()
 
@@ -33,8 +45,6 @@ export function observeElem(elem) {
 	observer.observe(elem, { childList: true, subtree: true })
 }
 
-const targetTagNames = new Set(['DIV', 'SPAN', 'TEXT', 'LABEL'])
-
 const observer = new MutationObserver(function (mutationsList, observer) {
 	const relevantElems = new Set()
 	for (const mutation of mutationsList) {
@@ -53,13 +63,29 @@ const observer = new MutationObserver(function (mutationsList, observer) {
 	setTimeout(() => {
 		for (const elem of relevantElems) {
 			if (elem.getAttribute('tabindex') !== null) continue
-			if (!focusableSjClasses.find(cls => elem.classList.contains(cls))) continue
+			const selem = d3select(elem)
+			if (
+				!svgClickable.has(elem.tagName) &&
+				!clickableSjClasses.find(cls => elem.classList?.contains(cls)) &&
+				!selem.on('click')
+			)
+				continue
 			//console.log(36, elem.tabIndex, elem.getAttribute('tabindex'))
-			d3select(elem)
-				.attr('tabindex', 0)
+			selem
+				// svg elems will be grouped
+				.attr('tabindex', 0) //svgClickable.has(elem.tagName) ? -1 : 0)
 				.on('keyup', event => {
-					if (event.key == 'Enter') elem.click()
+					if (event.key == 'Enter') {
+						const box = event.target.getBoundingClientRect()
+						const clientX = box.x + 0.5 * box.width
+						const clientY = box.y + box.height
+						//event.target.dispatchEvent(new MouseEvent('mousemove', { bubbles: true, clientX, clientY }))
+						event.target.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, clientX, clientY }))
+						event.target.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, clientX, clientY }))
+						event.target.dispatchEvent(new MouseEvent('click', { bubbles: true, clientX, clientY }))
+					}
 				})
 		}
+		relevantElems.clear()
 	}, renderWait)
 })
