6fc0749fb83d821b2742f5bb6bc0e38e1bcb2c40
Xin Zhou <xzhou82@gmail.com>
2025-12-11 11:58:41 -0600

    wip clean up type and client

--- shared/types/src/routes/termdb.chat.ts
+++ shared/types/src/routes/termdb.chat.ts
@@ -6,46 +6,22 @@ export type ChatRequest = {
 	genome: string
 	dslabel: string
 	filter?: Filter
-	filter0?: any // gdc
 	/** user prompt */
 	prompt: string
 	__protected__: any
 }
 
-export type numeric_filter = {
-	lower: number
-	upper: number
+type HtmlResponse = {
+	type: 'html'
+	/** for display only */
+	html: string
 }
-
-export type categorical_filter = {
-	name: string
+type PlotResponse = {
+	type: 'plot'
+	/** plot state */
+	plot: object
 }
-
-export type filter_variable = {
-	name: string
-	variable_type: 'float' | 'categories'
-	cutoff: numeric_filter | categorical_filter
-}
-
-export type summary = {
-	group_categories: string
-	overlay?: string
-	divide_by?: string
-	filter?: filter_variable
-}
-
-export type plot = {
-	/** Different chart types currently supported by the AI chatbot, will add types for the other charts when they will be implemented */
-	chartType: summary | 'survival' | 'hierarchial' | 'snv_indel' | 'cnv' | 'variant_calling' | 'none' | 'dge'
-}
-
-export type ChatResponse = {
-	type: 'html' | 'plot' | 'json'
-	/** when type=html, this value is set meaning server returns a chat response */
-	html?: string
-	/** when type=plot. value is json */
-	plot?: any
-}
+export type ChatResponse = HtmlResponse | PlotResponse
 
 export const ChatPayload: RoutePayload = {
 	request: {
