52c8d404dcf336e927115aeb4b676291f2d3a289
Colleen Reilly <creilly@stjude.org>
2025-12-16 09:59:39 -0600

    Mv mock box plot data to new file

--- client/plots/boxplot/test/boxplot.unit.spec.ts
+++ client/plots/boxplot/test/boxplot.unit.spec.ts
@@ -1,7 +1,7 @@
 import type { MassAppApi } from '#mass/types/mass'
 import tape from 'tape'
-import { termjson } from '../../../test/testdata/termjson'
-import { ViewModel } from '../viewModel/ViewModel'
+import { getBoxPlotMockData } from './mockBoxPlotData'
+// import { ViewModel } from '../viewModel/ViewModel'
 // import { LegendDataMapper } from '../viewModel/LegendDataMapper'
 import { ListSamples } from '../interactions/ListSamples'
 
@@ -17,128 +17,32 @@ Tests:
 See unit tests for #dom/boxplot for rendering unit tests
 */
 
-function getMockData() {
-	const mockDescrStats1 = {
-		min: { key: 'min', label: 'Min', value: 20 },
-		max: { key: 'max', label: 'Max', value: 100 },
-		median: { key: 'median', label: 'Median', value: 60 }
-	}
-
-	const mockDescrStats2 = {
-		min: { key: 'min', label: 'Min', value: 0 },
-		max: { key: 'max', label: 'Max', value: 60 },
-		median: { key: 'median', label: 'Median', value: 30 }
-	}
-
-	const mockSettings = {
-		plotLength: 20,
-		color: 'blue',
-		displayMode: 'default',
-		labelPad: 10,
-		isLogScale: false,
-		isVertical: false,
-		orderByMedian: false,
-		rowHeight: 20,
-		rowSpace: 10,
-		useDefaultSettings: true,
-		removeOutliers: false,
-		showAssocTests: true
-	}
-
-	const mockConfig = {
-		chartType: 'summary',
-		childType: 'boxplot',
-		groups: [],
-		id: 'test_test',
-		term: {
-			term: JSON.parse(JSON.stringify(termjson['agedx'])),
-			q: { mode: 'continuous', descrStats: mockDescrStats1 }
-		},
-		term2: { term: JSON.parse(JSON.stringify(termjson['sex'])), q: { descrStats: mockDescrStats2 } },
-		settings: { boxplot: mockSettings }
-	}
-
-	const mockPlot1 = {
-		key: '1',
-		seriesId: '1',
-		boxplot: {
-			w1: 0.002739726,
-			w2: 22.747930234,
-			p05: 0.9205479452,
-			p25: 3.4712328767,
-			p50: 7.4410958904,
-			p75: 11.78630137,
-			p95: 16.849315068,
-			iqr: 8.3150684933,
-			out: [{ value: 1 }],
-			label: '1, n=1'
-		},
-		labColor: 'black',
-		color: 'blue',
-		descrStats: mockDescrStats1,
-		x: 278,
-		y: 155
-	}
-
-	const mockPlot2 = {
-		key: '2',
-		seriesId: '2',
-		boxplot: {
-			w1: 0.002739726,
-			w2: 22.747930234,
-			p05: 0.9205479452,
-			p25: 3.4712328767,
-			p50: 7.4410958904,
-			p75: 11.78630137,
-			p95: 16.849315068,
-			iqr: 8.3150684933,
-			out: [],
-			label: '2, n=2'
-		},
-		labColor: 'black',
-		color: '#e75480',
-		descrStats: mockDescrStats2,
-		x: 278,
-		y: 155
-	}
-
-	const mockData: any = {
-		chartId: '',
-		plots: [mockPlot1, mockPlot2],
-		absMin: 0,
-		absMax: 100,
-		uncomputableValues: [{ label: 'test', value: 1 }]
-	}
-
-	return { mockDescrStats1, mockDescrStats2, mockSettings, mockConfig, mockPlot1, mockPlot2, mockData }
-}
-
-function getViewModel() {
-	const { mockConfig, mockData, mockSettings } = getMockData()
-	return {
-		viewModel: new ViewModel(mockConfig, mockData, mockSettings, 400, false),
-		mockConfig,
-		mockData,
-		mockSettings
-	}
-}
+// function getViewModel() {
+// 	const { mockConfig, mockData, mockSettings } = getBoxPlotMockData()
+// 	return {
+// 		viewModel: new ViewModel(mockConfig, mockData, mockSettings, 400, false),
+// 		mockConfig,
+// 		mockData,
+// 		mockSettings
+// 	}
+// }
 
 tape('\n', function (test) {
 	test.comment('-***- plots/boxplot/ViewModel -***-')
 	test.end()
 })
 
-tape('Default ViewModel()', function (test) {
-	test.timeoutAfter(100)
+// tape('Default ViewModel()', function (test) {
+// 	test.timeoutAfter(100)
 
-	const { viewModel } = getViewModel()
-	test.equal(typeof viewModel.viewData, 'object', `Should create a viewData object`)
-	test.equal(typeof viewModel.viewData.plotDim, 'object', `Should create a plotDim object`)
-	test.equal(viewModel.viewData.plots.length, 2, `Should create 2 plots`)
-	test.equal(typeof viewModel.viewData.legend, 'object', `Should create a legend object`)
+// 	const { viewModel } = getViewModel()
+// 	test.equal(typeof viewModel.viewData, 'object', `Should create a viewData object`)
+// 	test.equal(typeof viewModel.viewData.plotDim, 'object', `Should create a plotDim object`)
+// 	test.equal(viewModel.viewData.plots.length, 2, `Should create 2 plots`)
+// 	test.equal(typeof viewModel.viewData.legend, 'object', `Should create a legend object`)
 
-	test.end()
-})
+// 	test.end()
+// })
 
 // tape('ViewModel.setPlotDimensions()', function (test) {
 // 	test.timeoutAfter(100)
@@ -238,7 +142,7 @@ tape('Default ListSamples()', test => {
 	test.plan(7)
 
 	const mockApp: MassAppApi = {} as MassAppApi
-	const { mockConfig, mockPlot1 } = getMockData()
+	const { mockConfig, mockPlot1 } = getBoxPlotMockData()
 	const mockState: any = {
 		plots: [mockConfig],
 		termfilter: { filter: 'test' }
@@ -263,7 +167,7 @@ tape('Default ListSamples()', test => {
 })
 
 tape('ListSamples() with invalid plot', test => {
-	const { mockConfig, mockPlot1 } = getMockData()
+	const { mockConfig, mockPlot1 } = getBoxPlotMockData()
 	const mockApp: MassAppApi = {} as MassAppApi
 	const mockState: any = {
 		plots: [mockConfig],
@@ -281,7 +185,7 @@ tape('ListSamples() with invalid plot', test => {
 })
 
 tape('ListSamples.getTvsLst()', test => {
-	const { mockConfig, mockPlot1 } = getMockData()
+	const { mockConfig, mockPlot1 } = getBoxPlotMockData()
 	const mockApp: MassAppApi = {} as MassAppApi
 	const mockState: any = {
 		plots: [mockConfig],
