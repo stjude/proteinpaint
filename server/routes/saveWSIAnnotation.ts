f7429e453db344ceac57e394112751e26ab67bda
Aleksandar Acic <32873451+aacic@users.noreply.github.com>
2025-12-17 21:59:19 -1000

    Fix sql. (#4041)

--- server/routes/saveWSIAnnotation.ts
+++ server/routes/saveWSIAnnotation.ts
@@ -103,8 +103,13 @@ function validateQuery(ds: any, connection: Database.Database) {
 
 			// Query first user id from project_users table, later replace with actual user management
 			const userRow = connection
-				.prepare(`SELECT id FROM project_users WHERE project_id = ? ORDER BY id ASC LIMIT 1`)
-				.get(projectId) as { id: number } | undefined
+				.prepare(
+					`SELECT id
+						  FROM project_users
+						  ORDER BY id
+						  LIMIT 1`
+				)
+				.get() as { id: number } | undefined
 
 			const userId = userRow?.id
 
