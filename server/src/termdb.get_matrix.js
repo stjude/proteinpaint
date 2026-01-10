9ccf2770ce36b050822e81ace723c6e6f41ecade
siosonel <siosonel@gmail.com>
2026-01-10 13:14:12 -0600

    tidy up get_matrix() code

---
+++ server/src/termdb.get_matrix.js
@@ -0,0 +1,187 @@
+import { getData } from './termdb.matrix.js'
+import serverconfig from './serverconfig.js'
+import { authApi } from './auth.js'
+
+const maxStrLength = 5.12e8 // based on a 64-bit hard constraint in V8 for string processing
+
+// if '$' exists as a data object property, it indicates stripped properties
+// that should be rehydrated on the client side
+const $codes = Object.freeze({
+	// Below are data object properties that have the same strict value as another object property.
+	// The server will strip these object properties, and the client will rehydrate by copying from
+	// another property based such as `dataObj.key`.
+	copyAs: Object.freeze({
+		1: 'value',
+		2: 'label'
+	}),
+	// Below are object properties that need to be merged/applied back to a dehydrated data object.
+	// All the keys from a corresponding code object below will be deleted by the server.
+	objAssign: Object.freeze({
+		// will freeze these when generating $objAssign helper object below
+		1: { class: 'Blank', label: 'Not tested' },
+		2: { class: 'Blank', label: 'Not tested', origin: 'germline' },
+		3: { class: 'Blank', label: 'Not tested', origin: 'somatic' },
+		4: { class: 'WT', label: 'Wildtype' },
+		5: { class: 'WT', label: 'Wildtype', origin: 'germline' },
+		6: { class: 'WT', label: 'Wildtype', origin: 'somatic' }
+	})
+})
+
+// below creates an easy way to easily detect copyable data properties
+// should look like [[1, 'value'], [2, 'label']]
+const $copyAs = Object.freeze(Object.entries($codes.copyAs).map(c => Object.freeze([parseInt(c[0]), c[1]])))
+
+// below is a helper object to quickly find a matching $code for geneVariant data values
+// should look like {
+//   Blank: {
+//     '': 1,
+//     germline: 2,
+//     somatic: 3
+//   },
+//   WT: {
+//     '': 4,
+//     germline: 5,
+//     somatic: 6
+//   }
+// }
+// not hardcoding, but basing off refs.$codes.objAssign entry to guarantee correct mapping
+const $objAssign = {}
+for (const [code, m] of Object.entries($codes.objAssign)) {
+	Object.freeze(m)
+	if (!$objAssign[m.class]) $objAssign[m.class] = {}
+	$objAssign[m.class][m.origin || ''] = parseInt(code)
+}
+Object.freeze($objAssign)
+
+export async function get_matrix(q, req, res, ds, genome) {
+	if (q.getPlotDataByName) {
+		// send back the config for premade matrix plot
+		if (!ds.cohort?.matrixplots?.plots) throw 'ds.cohort.matrixplots.plots missing for the dataset'
+		const plot = ds.cohort.matrixplots.plots.find(p => p.name === q.getPlotDataByName)
+		if (!plot) throw 'invalid name of premade matrix plot' // invalid name could be attack string, avoid returning it so it won't be printed in html
+		res.send(plot.matrixConfig)
+		return
+	}
+	const data = await getData(q, ds, true) // FIXME hardcoded to true
+	if (data.error) {
+		console.trace(data)
+		res.send({ error: data.error })
+		return
+	}
+
+	const debug = serverconfig.debugmode
+	let jsonStrlen = ''
+	if (debug) {
+		jsonStrlen = JSON.stringify(data.refs).length
+		console.log('get_matrix() start jsonStr.length=', jsonStrlen)
+	}
+
+	const payload = {
+		samples: {},
+		refs: {
+			bySampleId: {},
+			byTermId: {},
+			$codes
+		}
+	}
+
+	const sampleEntries = Object.entries(data.samples || {})
+	let currShortId = 0,
+		sampleIndex = 0
+
+	if (authApi.canDisplaySampleIds(req, ds)) {
+		if (!data.refs.byTermId) data.refs.byTermId = {}
+
+		if (sampleEntries.length) {
+			for (const [sampleId, sample] of sampleEntries) {
+				payload.refs.bySampleId[sampleId] = Object.assign(
+					{
+						sample: sample.sample || sampleId,
+						label: sample.sampleName || undefined
+					},
+					data.refs.bySampleId[sampleId] || {}
+				)
+
+				delete sample.sample
+				delete sample.sampleName
+
+				if (!Object.keys(sample).length) {
+					delete payload.samples[sampleId]
+					delete payload.refs.bySampleId[sampleId]
+				} else {
+					for (const [termId, d] of Object.entries(sample)) {
+						delete d._SAMPLEID_ // not needed in client code
+						if (d.key) {
+							for (const c of $copyAs) {
+								if (d.key !== d[c[1]]) continue
+								delete d[c[1]] // can be copied as data[refs.$codes.copyAs[d.$]] = d.key
+								d.$ = c[0]
+							}
+						}
+
+						if (!payload.refs.byTermId[termId]) {
+							payload.refs.byTermId[termId] = data.refs.byTermId[termId] || {}
+							payload.refs.byTermId[termId].shortId = currShortId++
+							const gene = d.values?.[0]?.gene
+							if (gene) payload.refs.byTermId[termId].gene = gene
+						}
+
+						const { shortId, gene } = payload.refs.byTermId[termId]
+						// termId can be very long and repeated across all samples,
+						// use the shortId to lessen memory use
+						sample[shortId] = sample[termId]
+						delete sample[termId]
+
+						if (gene && d.values) {
+							for (const v of d.values) {
+								delete v._SAMPLEID_
+								const code = v.dt && $objAssign[v.class]?.[v.origin || '']
+								if (!code) continue
+								v.$ = code
+								// this can be rehydrated from refs.byTermId[tw.$id].gene
+								delete v.gene
+								// these props can be rehydrated from refs.$codes.objAssign[code]
+								delete v.class
+								delete v.label
+								delete v.origin
+							}
+						}
+					}
+				}
+
+				if (debug) {
+					jsonStrlen += JSON.stringify({ [sampleId]: sample }).length + 50
+					if (jsonStrlen > maxStrLength) break
+				}
+				payload.samples[sampleId] = sample
+				sampleIndex++
+			}
+		}
+	}
+
+	try {
+		if (debug) {
+			console.log('get_matrix() jsonStrlen=', jsonStrlen)
+			if (jsonStrlen > maxStrLength) throw new RangeError(`Invalid string length`)
+		}
+		const jsonStr = JSON.stringify(payload)
+		res.setHeader('Content-Type', 'application/json')
+		res.send(jsonStr)
+	} catch (e) {
+		if (e instanceof RangeError && e.message.includes('Invalid string length')) {
+			// create a more informative error message for end user
+			let message = `Response data too large - please narrow the cohort or limit the number of variables and/or genes.`
+			const n = sampleEntries.length
+			if (debug) message += `(Unable to encode data for ${n - sampleIndex} of ${n} cases/samples.)`
+			payload.error = {
+				code: 'RangeError: Invalid string length',
+				message,
+				jsonStrlen
+			}
+			payload.samples = {}
+			res.send(payload)
+		} else {
+			res.send({ error: e })
+		}
+	}
+}
