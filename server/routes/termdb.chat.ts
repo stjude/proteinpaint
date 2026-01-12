dd46a5e97f5cc386724b2a73d5e23b1b1d918e00
Robin Paul <robin.paul@stjude.org>
2026-01-12 17:29:06 -0600

    temp

--- server/routes/termdb.chat.ts
+++ server/routes/termdb.chat.ts
@@ -1,9 +1,11 @@
+import fs from 'fs'
 import type { ChatRequest, ChatResponse, RouteApi } from '#types'
 import { ChatPayload } from '#types/checkers'
-import { run_rust } from '@sjcrh/proteinpaint-rust'
+//import { run_rust } from '@sjcrh/proteinpaint-rust'
 import serverconfig from '../src/serverconfig.js'
 import { mayLog } from '#src/helpers.ts'
-import { run_python } from '@sjcrh/proteinpaint-python'
+//import { run_python } from '@sjcrh/proteinpaint-python'
+import got from 'got'
 
 export const api: RouteApi = {
 	endpoint: 'termdb/chat',
@@ -27,25 +29,6 @@ function init({ genomes }) {
 			if (!g) throw 'invalid genome'
 			const ds = g.datasets?.[q.dslabel]
 			if (!ds) throw 'invalid dslabel'
-
-			if (serverconfig.features.pythonChatBot) {
-				// Use the chat bot based on langGraph
-				//const df = path.join(serverconfig.tpmasterdir, ds.queries.chat.termsDescriptions)
-				const chatbot_input = {
-					prompt: q.prompt,
-					genome: q.genome,
-					dslabel: q.dslabel
-					//terms_tsv_path: df
-				}
-				try {
-					const ai_output_data = await run_python('chatBot.py', JSON.stringify(chatbot_input))
-					res.send(ai_output_data as ChatResponse)
-				} catch (error) {
-					const errmsg = 'Error running chatBot Python script:' + error
-					throw new Error(errmsg)
-				}
-				return
-			}
 			const serverconfig_ds_entries = serverconfig.genomes
 				.find(genome => genome.name == q.genome)
 				.datasets.find(dslabel => dslabel.name == ds.label)
@@ -56,122 +39,246 @@ function init({ genomes }) {
 
 			let apilink: string
 			let comp_model_name: string
-			let embedding_model_name: string
 			if (serverconfig.llm_backend == 'SJ') {
 				apilink = serverconfig.sj_apilink
 				comp_model_name = serverconfig.sj_comp_model_name
-				embedding_model_name = serverconfig.sj_embedding_model_name
 			} else if (serverconfig.llm_backend == 'ollama') {
 				apilink = serverconfig.ollama_apilink
 				comp_model_name = serverconfig.ollama_comp_model_name
-				embedding_model_name = serverconfig.ollama_embedding_model_name
 			} else {
 				throw "llm_backend either needs to be 'SJ' or 'ollama'" // Currently only 'SJ' and 'ollama' LLM backends are supported
 			}
 
-			const chatbot_input = {
-				user_input: q.prompt,
-				apilink: apilink,
-				tpmasterdir: serverconfig.tpmasterdir,
-				comp_model_name: comp_model_name,
-				embedding_model_name: embedding_model_name,
-				dataset_db: ds.cohort.db.file,
-				genedb: g.genedb.dbfile,
-				aiRoute: serverconfig.aiRoute, // Route file for classifying chat request into various routes
-				llm_backend_name: serverconfig.llm_backend, // The type of backend (engine) used for running the embedding and completion model. Currently "SJ" and "Ollama" are supported
-				aifiles: serverconfig_ds_entries.aifiles, // Dataset specific data containing data-specific routes, system prompts for agents and few-shot examples
-				binpath: serverconfig.binpath
-			}
+			//const chatbot_input = {
+			//    user_input: q.prompt,
+			//    apilink: apilink,
+			//    tpmasterdir: serverconfig.tpmasterdir,
+			//    comp_model_name: comp_model_name,
+			//    embedding_model_name: embedding_model_name,
+			//    dataset_db: ds.cohort.db.file,
+			//    genedb: g.genedb.dbfile,
+			//    aiRoute: serverconfig.aiRoute, // Route file for classifying chat request into various routes
+			//    llm_backend_name: serverconfig.llm_backend, // The type of backend (engine) used for running the embedding and completion model. Currently "SJ" and "Ollama" are supported
+			//    aifiles: serverconfig_ds_entries.aifiles, // Dataset specific data containing data-specific routes, system prompts for agents and few-shot examples
+			//    binpath: serverconfig.binpath
+			//}
 			//mayLog('chatbot_input:', JSON.stringify(chatbot_input))
 
 			const time1 = new Date().valueOf()
-			const classResult = JSON.parse(await run_rust('query_classification', JSON.stringify(chatbot_input)))
+			const classResult = await classify_query_by_dataset_type(
+				q.prompt,
+				comp_model_name,
+				serverconfig.llm_backend,
+				apilink,
+				serverconfig.aiRoute
+			)
 			const time2 = new Date().valueOf()
 			mayLog('Time taken for classification:', time2 - time1, 'ms')
+			mayLog('classResult:', classResult)
 
-			let ai_output_data: any
+			//let ai_output_data: any
 			let ai_output_json: any
-			if (classResult.route == 'summary') {
-				const time1 = new Date().valueOf()
-				ai_output_data = await run_rust('summary_agent', JSON.stringify(chatbot_input))
-				const time2 = new Date().valueOf()
-				mayLog('Time taken for running summary agent:', time2 - time1, 'ms')
+			//if (classResult == 'summary') {
+			//}
 
-				for (const line of ai_output_data.split('\n')) {
-					// The reason we are parsing each line from rust is because we want to debug what is causing the wrong output. As the AI pipeline matures, the rust code will be modified to always return a single JSON
-					if (line.startsWith('final_output:') == true) {
-						ai_output_json = JSON.parse(JSON.parse(line.replace('final_output:', '')))
-					} else {
-						mayLog(line)
-					}
-				}
-			} else if (classResult.route == 'dge') {
-				ai_output_json = { type: 'html', html: 'DE agent not implemented yet' }
-			} else {
-				// Will define all other agents later as desired
-				ai_output_json = { type: 'html', html: 'Unknown classification value' }
-			}
+			//const time1 = new Date().valueOf()
+			//const classResult = JSON.parse(await run_rust('query_classification', JSON.stringify(chatbot_input)))
+			//const time2 = new Date().valueOf()
+			//mayLog('Time taken for classification:', time2 - time1, 'ms')
 
-			if (ai_output_json.type == 'plot') {
-				if (typeof ai_output_json.plot != 'object') throw '.plot{} missing when .type=plot'
-				if (ai_output_json.plot.simpleFilter) {
-					// simpleFilter= [ {term:str, category:str} ]
-					if (!Array.isArray(ai_output_json.plot.simpleFilter)) throw 'ai_output_json.plot.simpleFilter is not array'
-					const localfilter = { type: 'tvslst', in: true, join: '', lst: [] as any[] }
-					if (ai_output_json.plot.simpleFilter.length > 1) localfilter.join = 'and' // For now hardcoding join as 'and' if number of filter terms > 1. Will later implement more comprehensive logic
-					for (const f of ai_output_json.plot.simpleFilter) {
-						const term = ds.cohort.termdb.q.termjsonByOneid(f.term)
-						if (!term) throw 'invalid term id from simpleFilter[].term'
-						if (term.type == 'categorical') {
-							let cat
-							for (const ck in term.values) {
-								if (ck == f.category) cat = ck
-								else if (term.values[ck].label == f.category) cat = ck
-							}
-							if (!cat) throw 'invalid category from ' + JSON.stringify(f)
-							// term and category validated
-							localfilter.lst.push({
-								type: 'tvs',
-								tvs: {
-									term,
-									values: [{ key: cat }]
-								}
-							})
-						} else if (term.type == 'float' || term.type == 'integer') {
-							const numeric: any = {
-								type: 'tvs',
-								tvs: {
-									term,
-									ranges: []
-								}
-							}
-							const range: any = {}
-							if (f.gt && !f.lt) {
-								range.start = Number(f.gt)
-								range.stopunbounded = true
-							} else if (f.lt && !f.gt) {
-								range.stop = Number(f.lt)
-								range.startunbounded = true
-							} else if (f.gt && f.lt) {
-								range.start = Number(f.gt)
-								range.stop = Number(f.lt)
-							} else {
-								throw 'Neither greater or lesser defined'
-							}
-							numeric.tvs.ranges.push(range)
-							localfilter.lst.push(numeric)
-						}
-					}
-					delete ai_output_json.plot.simpleFilter
-					ai_output_json.plot.filter = localfilter
-				}
-			}
+			//let ai_output_data: any
+			//let ai_output_json: any
+			//if (classResult.route == 'summary') {
+			//	const time1 = new Date().valueOf()
+			//	ai_output_data = await run_rust('summary_agent', JSON.stringify(chatbot_input))
+			//	const time2 = new Date().valueOf()
+			//	mayLog('Time taken for running summary agent:', time2 - time1, 'ms')
+
+			//	for (const line of ai_output_data.split('\n')) {
+			//		// The reason we are parsing each line from rust is because we want to debug what is causing the wrong output. As the AI pipeline matures, the rust code will be modified to always return a single JSON
+			//		if (line.startsWith('final_output:') == true) {
+			//			ai_output_json = JSON.parse(JSON.parse(line.replace('final_output:', '')))
+			//		} else {
+			//			mayLog(line)
+			//		}
+			//	}
+			//} else if (classResult.route == 'dge') {
+			//	ai_output_json = { type: 'html', html: 'DE agent not implemented yet' }
+			//} else {
+			//	// Will define all other agents later as desired
+			//	ai_output_json = { type: 'html', html: 'Unknown classification value' }
+			//}
+
+			//     		if (ai_output_json.type == 'plot') {
+			//	if (typeof ai_output_json.plot != 'object') throw '.plot{} missing when .type=plot'
+			//	if (ai_output_json.plot.simpleFilter) {
+			//		// simpleFilter= [ {term:str, category:str} ]
+			//		if (!Array.isArray(ai_output_json.plot.simpleFilter)) throw 'ai_output_json.plot.simpleFilter is not array'
+			//		const localfilter = { type: 'tvslst', in: true, join: '', lst: [] as any[] }
+			//		if (ai_output_json.plot.simpleFilter.length > 1) localfilter.join = 'and' // For now hardcoding join as 'and' if number of filter terms > 1. Will later implement more comprehensive logic
+			//		for (const f of ai_output_json.plot.simpleFilter) {
+			//			const term = ds.cohort.termdb.q.termjsonByOneid(f.term)
+			//			if (!term) throw 'invalid term id from simpleFilter[].term'
+			//			if (term.type == 'categorical') {
+			//				let cat
+			//				for (const ck in term.values) {
+			//					if (ck == f.category) cat = ck
+			//					else if (term.values[ck].label == f.category) cat = ck
+			//				}
+			//				if (!cat) throw 'invalid category from ' + JSON.stringify(f)
+			//				// term and category validated
+			//				localfilter.lst.push({
+			//					type: 'tvs',
+			//					tvs: {
+			//						term,
+			//						values: [{ key: cat }]
+			//					}
+			//				})
+			//			} else if (term.type == 'float' || term.type == 'integer') {
+			//				const numeric: any = {
+			//					type: 'tvs',
+			//					tvs: {
+			//						term,
+			//						ranges: []
+			//					}
+			//				}
+			//				const range: any = {}
+			//				if (f.gt && !f.lt) {
+			//					range.start = Number(f.gt)
+			//					range.stopunbounded = true
+			//				} else if (f.lt && !f.gt) {
+			//					range.stop = Number(f.lt)
+			//					range.startunbounded = true
+			//				} else if (f.gt && f.lt) {
+			//					range.start = Number(f.gt)
+			//					range.stop = Number(f.lt)
+			//				} else {
+			//					throw 'Neither greater or lesser defined'
+			//				}
+			//				numeric.tvs.ranges.push(range)
+			//				localfilter.lst.push(numeric)
+			//			}
+			//		}
+			//		delete ai_output_json.plot.simpleFilter
+			//		ai_output_json.plot.filter = localfilter
+			//	}
+			//}
 
 			//mayLog('ai_output_json:', ai_output_json)
 			res.send(ai_output_json as ChatResponse)
 		} catch (e: any) {
-			if (e.stack) console.log(e.stack)
+			if (e.stack) mayLog(e.stack)
 			res.send({ error: e?.message || e })
 		}
 	}
+}
+
+async function call_ollama(prompt: string, model_name: string, apilink: string) {
+	const temperature = 0.01
+	const top_p = 0.95
+	const timeout = 200000
+	const payload = {
+		model: model_name,
+		messages: [{ role: 'user', content: prompt }],
+		raw: false,
+		stream: false,
+		keep_alive: 15, //Keep the LLM loaded for 15mins
+		options: {
+			top_p: top_p,
+			temperature: temperature,
+			num_ctx: 10000
+		}
+	}
+
+	try {
+		const response = await got.post(apilink + '/api/chat', {
+			json: payload,
+			headers: { 'Content-Type': 'application/json' },
+			timeout: { request: timeout }
+		})
+		const result = JSON.parse(response.body)
+		mayLog('answer:', result.message.content)
+		if (result && result.message && result.message.content && result.message.content.length > 0)
+			return result.message.content
+		else {
+			throw 'Error: Received an unexpected response format:' + result
+		}
+	} catch (error) {
+		return 'Error: Ollama API request failed:' + error
+	}
+}
+
+async function call_sj_llm(prompt: string, model_name: string, apilink: string) {
+	const temperature = 0.01
+	const top_p = 0.95
+	const timeout = 200000
+	const max_new_tokens = 512
+	const payload = {
+		inputs: [
+			{
+				model_name: model_name,
+				inputs: {
+					text: prompt,
+					max_new_tokens: max_new_tokens,
+					temperature: temperature,
+					top_p: top_p
+				}
+			}
+		]
+	}
+
+	try {
+		const response = await got.post(apilink, {
+			json: payload,
+			headers: { 'Content-Type': 'application/json' },
+			timeout: { request: timeout }
+		})
+		mayLog('response:', response)
+		const result = JSON.parse(response.body)
+		mayLog('answer:', result.message.content)
+		if (result && result.outputs.length > 0) return result.message.content
+		else {
+			throw 'Error: Received an unexpected response format:' + result
+		}
+	} catch (error) {
+		return 'Error: SJ API request failed:' + error
+	}
+}
+
+async function readJSONFile(file: string) {
+	const json_file = await fs.promises.readFile(file)
+	return JSON.parse(json_file.toString())
+}
+
+async function classify_query_by_dataset_type(
+	user_prompt: string,
+	comp_model_name: string,
+	llm_backend_type: string,
+	apilink: string,
+	aiRoute: string
+) {
+	const data = await readJSONFile(aiRoute)
+	mayLog('ai_json:', data)
+
+	let contents = data['general'] // The general description should be right at the top of the system prompt
+	for (const key of Object.keys(data)) {
+		// Add descriptions of all other agents after the general description
+		if (key != 'general') {
+			contents += data[key]
+		}
+	}
+	const template = contents + ' Question: {' + user_prompt + '} Answer: {answer}'
+
+	let response: string
+	if (llm_backend_type == 'SJ') {
+		// Local SJ server
+		response = await call_sj_llm(template, comp_model_name, apilink)
+	} else if (llm_backend_type == 'ollama') {
+		// Ollama server
+		response = await call_ollama(template, comp_model_name, apilink)
+	} else {
+		// Will later add support for azure server also
+		throw 'Unknown LLM backend'
+	}
+	return JSON.parse(response)['answer']
 }
