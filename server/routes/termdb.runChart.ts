3c94d322832ea653b9e348f4fdd72955aca4db24
Xin Zhou <xzhou82@gmail.com>
2026-01-15 11:29:46 -0600

    fix to sort out request-response cycle

--- server/routes/termdb.runChart.ts
+++ server/routes/termdb.runChart.ts
@@ -17,17 +17,24 @@ export const api: RouteApi = {
 
 function init({ genomes }) {
 	return async (req, res): Promise<void> => {
-		const q: RunChartRequest = req.query
-		const genome = genomes[q.genome]
-		if (!genome) throw new Error('invalid genome name')
-		const ds = genome.datasets?.[q.dslabel]
-		if (!ds) throw new Error('invalid ds')
+		try {
+			const q: RunChartRequest = req.query
+			const genome = genomes[q.genome]
+			if (!genome) throw new Error('invalid genome name')
+			const ds = genome.datasets?.[q.dslabel]
+			if (!ds) throw new Error('invalid ds')
 
-		const result: RunChartResponse = {
-			status: 'ok',
-			series: []
+			console.log(q)
+
+			const result: RunChartResponse = {
+				status: 'ok',
+				series: []
+			}
+
+			res.send(result)
+		} catch (e: any) {
+			console.log(e.stack)
+			res.send({ error: e.message || e })
 		}
-
-		res.send(result)
 	}
 }
