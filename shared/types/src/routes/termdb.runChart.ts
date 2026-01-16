20c867a87d44573c5c69c515f1775b02d9f750e0
xzhou <xzhou82@gmail.com>
2026-01-16 13:39:21 -0600

    complete request/response type

--- shared/types/src/routes/termdb.runChart.ts
+++ shared/types/src/routes/termdb.runChart.ts
@@ -1,17 +1,39 @@
-import type { RoutePayload, Point } from './routeApi.ts'
+import type { RoutePayload } from './routeApi.ts'
 
 export type RunChartRequest = {
 	genome: string
 	dslabel: string
-	term: string
-	term2: string
+	/** date term for x axis */
+	term: {
+		id: string
+	}
+	/** timeline/duration term for y axis */
+	term2: {
+		id: string
+	}
 	filter?: any
 	__protected__?: any // auth token for accessing protected data
 }
 
 export type RunChartResponse = {
 	status: 'ok' | 'error'
-	series: Point[]
+	/** each series is one curve, with a median. a runchart may show 1 or multiple curves */
+	series: {
+		/** calculated Y median value for this curve */
+		median: number
+		points: Point[]
+	}[]
+}
+
+type Point = {
+	/** decimal year, e.g. 2024.21321321 */
+	x: number
+	/** text of human-readable x value, e.g. "Jan 2024" which may be by the months, depends on dataset customization */
+	xName: string
+	/** timeline, e.g. number of days */
+	y: number
+	/** number of samples with this timeline at this time point */
+	sampleCount: number
 }
 
 export const runChartPayload: RoutePayload = {
