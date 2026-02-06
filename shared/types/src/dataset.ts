52c0b79ec1b978beedcba67eafc9382eeb7ce6a1
gavrielm <gmatt@stjude.org>
2026-02-05 21:14:33 -0600

    Handle .maxGeneVariantGeneSetSize parameter from dataset to determine max geneset size (#4157)

--- shared/types/src/dataset.ts
+++ shared/types/src/dataset.ts
@@ -1586,6 +1586,8 @@ keep this setting here for reason of:
 		/** key is string as disease/survial etc, value is tw */
 		[index: string]: Tw
 	}
+	/** maximum number of genes to query in geneVariant gene set */
+	maxGeneVariantGeneSetSize?: number
 	//terms  are shown in the dictionary based on term and user role.
 	isTermVisible?: (clientAuthResult: any, ids: string) => boolean
 	hiddenIds?: string[]
