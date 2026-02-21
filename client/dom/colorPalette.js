82c9ce710fb8eccf6b537b32007812772a80c067
siosonel <siosonel@gmail.com>
2026-02-21 12:40:09 -0600

    temp tests

--- client/dom/colorPalette.js
+++ client/dom/colorPalette.js
@@ -5,7 +5,7 @@
 // - support a usedColors option that should not be reused in the generated color palette;
 //   this would require something like https://github.com/Evercoder/d3-color-difference,
 //   since two colors may still look very similar to each other even when not exactly the same
-export function getHslPalette(numColors) {
+export function getHslPalette(numColors, s = 70, l = 50) {
 	if (numColors <= 0) return []
 	// These preassigned angles are the corners of equilateral triangle, inside
 	// a 360 degree circle and touching its circumference. The angle sequence
@@ -13,15 +13,16 @@ export function getHslPalette(numColors) {
 	// - after the first 3 angles, flip the triangle upside down
 	// - then rotate to 270 which somewhat opposes 60
 	// - then rotate to 15 which somewhat opposes 150
-	const startAngles = [0, 120, 240, 180, 300, 60, 270, 30, 150, 15, 135, 255]
+	const startAngles = [0, 120, 240, 180, 300, 60, 270, 30, 150] //, 15, 135, 255]
 	// Keep saturation (70%) and lightness (50%) constant for consistency
-	const presetColors = startAngles.map((hue, i) => `hsl(${hue},${i < 9 ? 70 : 40}%,${i < 9 ? 50 : 70}%)`)
+	const presetColors = startAngles.map((hue, i) => `hsl(${hue},${s}%,${l}%)`) // `hsl(${hue},${i < 9 ? 70 : 40}%,${i < 9 ? 50 : 70}%)`)
 	const colors = new Set(presetColors)
+	console.log(19, presetColors)
 	const willCompute = numColors - colors.size
-	if (willCompute <= 0) return [...colors]
+	if (willCompute <= 0) return [...colors].slice(0, numColors)
 
 	// after the startAngles are used up, reuse it but pad each angle with a given offset
-	const huedelta = Math.trunc(360 / Math.max(30, Math.trunc(willCompute / 12))) // Divide 360° by unassigned colors
+	const huedelta = Math.trunc(360 / Math.max(30, Math.trunc(willCompute / startAngles.length))) // Divide 360° by unassigned colors
 	let pad = 0
 	while (colors.size < numColors) {
 		pad += huedelta
@@ -32,5 +33,6 @@ export function getHslPalette(numColors) {
 		}
 		if (colors.size >= numColors) break
 	}
+	console.log(34, colors)
 	return [...colors].slice(0, numColors)
 }
--- client/dom/test/colorPalette.unit.spec.js
+++ client/dom/test/colorPalette.unit.spec.js
@@ -1,6 +1,7 @@
 import tape from 'tape'
 import { getHslPalette } from '../colorPalette.js'
 import { select } from 'd3-selection'
+import { getColorScheme } from '#shared'
 
 /**************
  test sections
@@ -12,11 +13,29 @@ tape('\n', test => {
 })
 
 tape('default behavior', test => {
-	const numColors = 30
-	const colors = getHslPalette(30)
+	const n = 9 // target number of colors
+	compare(test, n, getHslPalette(n, 90, 70))
+	compare(test, n, getHslPalette(n, 10, 70))
+	compare(test, n, getHslPalette(n, 50, 70))
+	compare(test, n, getHslPalette(n, 90, 30))
+	compare(test, n, getHslPalette(n, 10, 30))
+	compare(test, n, getHslPalette(n, 50, 30))
+	//compare(test, n, getColorScheme(n))
+	test.end()
+})
+
+/**************
+ test helpers
+***************/
+
+function compare(test, numColors, colors) {
 	test.equal(colors.length, numColors, 'should give the expected number of colors')
 
-	const holder = select(document.body).append('div').style('border', '1px solid #000')
+	const holder = select(document.body)
+		.append('div')
+		.style('width', 'fit-content')
+		.style('margin', '5px')
+		.style('border', '1px solid #000')
 	const divs = holder
 		.selectAll('div')
 		.data(colors)
@@ -36,5 +55,4 @@ tape('default behavior', test => {
 
 	test.equal(uniqueColors.size, numColors, 'should generate unique colors')
 	//if (test._ok) holder.remove()
-	test.end()
-})
+}
