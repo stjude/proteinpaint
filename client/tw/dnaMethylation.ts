afee2bd2a64bd5a9a7b94edcde7d35e588faaa51
Gavriel Matt <gmatt@stjude.org>
2026-02-23 15:29:08 -0600

    Update term.name

--- client/tw/dnaMethylation.ts
+++ client/tw/dnaMethylation.ts
@@ -3,19 +3,19 @@ import type { RawDnaMethylationTerm } from '#types'
 const termType = 'dnaMethylation'
 
 export class DnaMethylationBase {
-	//gene: string
-	//name: string
-	//unit: string
+	id: string
+	name: string
+	unit: string
 
 	// option to fill-in/mutate the input raw term object in-place
 	// - does not have to construct, but may require forced type casting in consumer code
 	static async fill(term: RawDnaMethylationTerm) {
 		DnaMethylationBase.validate(term)
-		/*if (!term.name) {
-			term.unit = opts.vocabApi.termdbConfig.queries.geneExpression?.unit || 'Gene Expression'
-			const name = `${term.gene} ${term.unit}`
+		if (!term.name) {
+			term.unit = 'Average Beta Value'
+			const name = `${term.id} ${term.unit}`
 			term.name = name
-		}*/
+		}
 	}
 
 	static validate(term: RawDnaMethylationTerm) {
@@ -23,16 +23,17 @@ export class DnaMethylationBase {
 		if (term.type != termType) throw `incorrect term.type='${term?.type}', expecting '${termType}'`
 		if (term.type != 'dnaMethylation') throw 'unexpected term.type'
 		if (typeof term !== 'object') throw 'term is not an object'
-		//if (!term.gene && !term.name) throw 'no gene or name present'
-		//if (!term.gene || typeof term.gene != 'string') throw 'geneExpression term.gene must be non-empty string'
+		if (!term.id) throw 'term.id is missing'
+		if (!term.chr || !Number.isInteger(term.start) || !Number.isInteger(term.stop))
+			throw 'incomplete coordinate in term{}'
 	}
 
 	// option to construct an object instance and not mutate the input raw term
 	// - will be used instead of term literal object
 	constructor(term: RawDnaMethylationTerm) {
 		DnaMethylationBase.validate(term)
-		/*this.gene = term.gene || term.name
-		this.unit = term.unit || opts.vocabApi.termdbConfig.queries.geneExpression?.unit || 'Gene Expression'
-		this.name = term.name || `${term.gene} ${this.unit}`*/
+		this.id = term.id
+		this.unit = term.unit || 'Average Beta Value'
+		this.name = term.name || `${this.id} ${this.unit}`
 	}
 }
