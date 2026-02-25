881b0406fc80107c9912edf1876fd4d05a862f7c
Colleen Reilly <creilly@stjude.org>
2026-02-25 15:10:01 -0600

    Prototype scge handler

--- client/termdb/handlers/singleCellGeneExpression.ts
+++ client/termdb/handlers/singleCellGeneExpression.ts
@@ -1,5 +1,36 @@
+import { Menu, addGeneSearchbox } from '#dom'
+import type { AppApi } from '#rx'
+import { TermTypes } from '#shared/terms.js'
+
 export class SearchHandler {
-	init() {
-		//TODO: implement search handler for single cell gene expression term
+	callback?: (arg0: { gene: string; name: string; type: string }) => void
+	app?: AppApi
+
+	init(opts) {
+		this.validateOpts(opts)
+		this.callback = opts.callback
+		this.app = opts.app
+		const holder = opts.holder.append('div').style('padding', '10px 0px')
+		const geneSearch = addGeneSearchbox({
+			tip: new Menu({ padding: '0px' }),
+			genome: opts.genomeObj,
+			row: holder,
+			searchOnly: 'gene',
+			callback: () => this.selectGene(geneSearch.geneSymbol)
+		})
+	}
+
+	async selectGene(gene: string | undefined) {
+		if (!gene) throw new Error('No gene selected')
+		const unit = this.app!.vocabApi.termdbConfig.queries.singleCell.geneExpression?.unit || 'Gene Expression'
+		const name = `${gene} ${unit}`
+		this.callback!({ gene, name, type: TermTypes.SINGLECELL_GENE_EXPRESSION })
+	}
+
+	validateOpts(opts) {
+		if (opts.callback == null) throw new Error('callback is required')
+		if (opts.app == null) throw new Error('app is required')
+		if (opts.holder == null) throw new Error('holder is required')
+		if (opts.genomeObj == null) throw new Error('genomeObj is required')
 	}
 }
