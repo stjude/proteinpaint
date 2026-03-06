b20ccf01e7f4022e188a7eef3a0e37dd715d0b62
Robin Paul <rpaul1@stjude.org>
2026-03-05 21:29:17 -0600

    Changed routeSetter length = 6

--- server/src/test/serverconfig.unit.spec.js
+++ server/src/test/serverconfig.unit.spec.js
@@ -5,10 +5,9 @@ import path from 'path'
 const __dirname = import.meta.dirname
 
 const sc = structuredClone(serverconfig)
-
 tape('basic test', test => {
 	test.equal(sc.debugmode, true, 'should have debugmode=true in test environment')
 	test.equal(sc.binpath, path.join(__dirname, '../..'), 'should detect the correct binpath')
-	test.equal(sc.routeSetters?.length, 5, 'should set the expected routeSetters')
+	test.equal(sc.routeSetters?.length, 6, 'should set the expected routeSetters')
 	test.end()
 })
