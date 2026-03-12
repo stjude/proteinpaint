a0ae1c7a0a632d0e39114971460cfff46aef642d
siosonel <siosonel@gmail.com>
2026-03-12 10:55:02 -0500

    clean up rx/src/legacy.ts and rename to wrappers.ts

--- client/rx/src/wrappers.ts
+++ client/rx/src/wrappers.ts
@@ -1,6 +1,3 @@
-// !!! TODO: migrate all rx code to strictly-defined typescript and
-// to use classes (with private #Inner) instead of closures !!!
-import { copyMerge } from './utils.ts'
 import { AppApi } from './AppApi.ts'
 import { StoreApi } from './StoreApi.ts'
 import { ComponentApi } from './ComponentApi.ts'
@@ -8,15 +5,9 @@ import { ComponentApi } from './ComponentApi.ts'
 /*
 *********** Exported ***********
 getInitFxn()
-getComponentApi()
-Bus (class)
-notifyComponents()
-getComponents()
-copyMerge()
-fromJson()
-toJson()
-deepFreeze()
-deepEqual()
+getAppInit()
+getStoreInit()
+getComponentInit()
 *********** Internal ***********
 
 Instances, APIs, and callback arguments are documented at:
@@ -30,19 +21,19 @@ https://docs.google.com/document/d/1G3LqbtsCEkGw4ABA_VognhjVnUHnsVYAGdXyhYG374M/
 
 /* 
 	getInitFxn()
+	- !!! do not use for reactive components, use ComponentApi.getInitFxn() instead !!!
+	- use to wrap and protect _Class_ instance properties from being changed arbitrarily 
+	  by any code that has a reference to it 
+	- offers flexibility to use the generated instance outside of the rx framework/notification flow,
+		with or without an coordinating "app"
+	- as of 3/12/2026, this is used by FilterPrompt, FilterStateless, TermInfo
+
 	- returns a _Class_ initiator function that
 		- creates a _Class_ instance
 		- optionally attaches an api.Inner reference to the instance for debugging
 		- optionally creates an instance.bus property if there is an instance.eventTypes property
 		- freezes and returns an immutable instance API
-
-	Design goal: 
-	- to protect _Class_ instance properties from being changed arbitrarily 
-	  by any code that has a reference to it
-	- Flexibility to use the generated instance outside of the rx framework/notification flow,
-		with or without an coordinating "app"
 */
-// deprecated: use <AppApi | StoreApi | ComponentApi>.getInitFxn
 export function getInitFxn(_Class_) {
 	/*
 		opts
@@ -63,47 +54,25 @@ export function getInitFxn(_Class_) {
 	}
 }
 
-// should use AppApi.getInitFxn() directly
+// should use AppApi.getInitFxn() directly,
+// keeping this code for convenience as it's used in multiple places
 export function getAppInit(_Class_) {
 	if (_Class_.type) return AppApi.getInitFxn(_Class_)
 	throw `A rx app class must declare a static type='app'.`
 }
 
-// should use StoreApi.getInitFxn() directly
+// should use StoreApi.getInitFxn() directly,
+// keeping this code for convenience as it's used in multiple places
 export function getStoreInit(_Class_) {
 	if (_Class_.type) return StoreApi.getInitFxn(_Class_)
 	throw `A rx store class must declare a static type='store'.`
 }
 
-// should use ComponentApi.getInitFxn() directly
+// should use ComponentApi.getInitFxn() directly,
+// keeping this code for convenience as it's used in multiple places
 export function getCompInit(_Class_) {
 	if (_Class_.type) return ComponentApi.getInitFxn(_Class_)
 	throw `A rx component class must declare a static type.`
-}
-
-/*
-	may apply overrides to instance opts
-	if there is an instance.type key in app.opts
-*/
-export function getOpts(_opts, instance) {
-	const opts = Object.assign({}, instance.opts || {}, _opts)
-	if (!instance.app) return opts
-	if (instance.type in instance.app.opts) {
-		/*
-			Always override opts with any app.opts that is available
-			for the instance's component type, supplied as an appInit() argument.
-
-			TODO: May want the ability to NOT override an existing key-value
-			in opts, only apply app.opts[instance.type] override for key-values
-			that are not in opts. Need to see an actual use case before working on this.
-		*/
-		const overrides = instance.app.opts[instance.type]
-		if (instance.validateOpts) instance.validateOpts(overrides)
-		copyMerge(opts, overrides)
-	}
-	if ('debug' in instance.app) opts.debug = instance.app.debug
-	else if (instance.app.opts && 'debug' in instance.app.opts) opts.debug = instance.app.opts.debug
-	return opts
 }
 
 /*
