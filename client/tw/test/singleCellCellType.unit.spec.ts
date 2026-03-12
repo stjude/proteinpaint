2863896a4299efcb522c4e1cb36c3ef95df03ca8
Colleen Reilly <creilly@stjude.org>
2026-03-12 09:48:01 -0500

    Fix typos in test file

--- client/tw/test/singleCellCellType.unit.spec.ts
+++ client/tw/test/singleCellCellType.unit.spec.ts
@@ -8,7 +8,7 @@ import { TermTypes } from '#shared/terms.js'
 
 function getValidRawTerm(overrides: any = {}) {
 	return {
-		type: TermTypes.SINGLECELL_CELL_TYPE,
+		type: TermTypes.SINGLECELL_CELLTYPE,
 		sample: { sID: 'S1', eID: 'E1' },
 		plot: 'My plot',
 		...overrides
@@ -82,7 +82,7 @@ tape('constructor should set explicit term fields', test => {
 	})
 	const x = new SingleCellCellTypeBase(term as any)
 
-	test.equal(x.type, TermTypes.SINGLECELL_CELL_TYPE, 'Should set type')
+	test.equal(x.type, TermTypes.SINGLECELL_CELLTYPE, 'Should set type')
 	test.deepEqual(x.sample, { sID: 'S1', eID: 'E1' }, 'Should set sample')
 	test.equal(x.plot, 'My plot', 'Should set plot')
 	test.deepEqual(x.groupsetting, { disabled: true }, 'Should preserve provided groupsetting')
