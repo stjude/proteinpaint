9f94613cd00cedf27827fc21860d2beb3f600a42
Andrew Willems <andrew.willems@stjude.org>
2026-03-13 15:15:14 -0500

    Removed cache file creation, NaiveGP model branch, improved client code based on feedback. Other cleanups

---
+++ client/plots/dmr/settings/Settings.ts
@@ -0,0 +1,6 @@
+export type DMRSettings = {
+	/** Width argument for the block (default: 800) */
+	blockWidth: number
+	/** Base pairs to extend beyond the start and stop coordinates when querying for DMRs (default: 2000) */
+	pad: number
+}
