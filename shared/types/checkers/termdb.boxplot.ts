6095078e6835854a05da4f7e1c65663f50c2428c
Xin Zhou <xzhou82@gmail.com>
2026-04-01 21:59:52 -0500

    delete outdated files from shared/types/checkers/

--- shared/types/checkers/termdb.boxplot.ts
+++
@@ -1,8 +0,0 @@
-// this file was auto-generated using 'npm run generate' or 'npm run dev' from the shared/types dir
-import { createValidate } from 'typia'
-import type { BoxPlotRequest, BoxPlotResponse } from '../src/routes/termdb.boxplot.ts'
-
-export { boxplotPayload } from '../src/routes/termdb.boxplot.ts'
-
-export const validBoxPlotRequest = createValidate<BoxPlotRequest>()
-export const validBoxPlotResponse = createValidate<BoxPlotResponse>()
--- shared/types/checkers/termdb.violin.ts
+++
@@ -1,8 +0,0 @@
-// this file was auto-generated using 'npm run generate' or 'npm run dev' from the shared/types dir
-import { createValidate } from 'typia'
-import type { ViolinRequest, ViolinResponse } from '../src/routes/termdb.violin.ts'
-
-export { violinPayload } from '../src/routes/termdb.violin.ts'
-
-export const validViolinRequest = createValidate<ViolinRequest>()
-export const validViolinResponse = createValidate<ViolinResponse>()
