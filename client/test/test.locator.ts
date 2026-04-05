d619c0931d44c7b9d426e2f3a813111421767a6b
siosonel <siosonel@gmail.com>
2026-04-05 00:55:08 -0500

    support Locator method chaining with one await instead of nested awaits

--- client/test/test.locator.ts
+++ client/test/test.locator.ts
@@ -4,8 +4,10 @@ export class Locator {
 	elem: any //HTMLElement | SVGElement | SVGGElement | SVGLineElement | SVGRectElement | SVGSVGElement | SVGTextElement
 	opts = {
 		intervalWait: 25,
-		maxWait: 5000
+		maxWait: 3000
 	}
+	pending: ((elems: any[]) => Promise<any>)[] = []
+	result: any | any[] = []
 
 	// arg could be one of the following, must always
 	// - DOM element
@@ -24,6 +26,7 @@ export class Locator {
 		} else {
 			this.elem = arg
 		}
+		this.pending = []
 	}
 
 	// convenience method to allow await Locator.init(elem).whenVisible('')
@@ -32,28 +35,71 @@ export class Locator {
 	}
 
 	// selector: string selector such as '#my-id', '.my-class', `[data-testid='my-id-here']`, `[role='button']`, etc
-	shows(selector, targetVisibility = true) {
-		const intervalWait = this.opts.intervalWait
-		const maxWait = this.opts.maxWait
+	shows(selector, opts: { intervalWait?: number; maxWait?: number; visibility?: boolean; count?: number } = {}) {
+		//this.steps.push('shows', selector, )
+		const targetCount = opts.count || 1
+		const visibility = opts.visibility || true
+		const intervalWait = opts.intervalWait || this.opts.intervalWait
+		const maxWait = opts.maxWait || this.opts.maxWait
 		let elapsed = intervalWait
-		return new Promise((resolve, reject) => {
-			const i = setInterval(() => {
-				const descendants = this.elem.querySelectorAll(selector)
-				if (descendants?.[0]?.checkVisibility() === targetVisibility) {
-					clearInterval(i)
-					resolve(descendants)
-				} else {
-					elapsed += intervalWait
-					if (elapsed > maxWait) {
-						reject(`elem did not become visible within 5 seconds`)
-						clearInterval(i)
+		this.pending.push(async elems => {
+			//console.log(48, elems, targetCount, visibility)
+			const matchedElems = new Set()
+			return new Promise((resolve, _) => {
+				const i = setInterval(() => {
+					for (const elem of elems) {
+						for (const descendant of elem.querySelectorAll(selector)) {
+							if (matchedElems.has(descendant)) continue //; console.log(55, descendant, descendant.checkVisibility())
+							if (descendant.checkVisibility() === visibility) matchedElems.add(descendant)
+						}
+						if (matchedElems.size >= targetCount) {
+							clearInterval(i)
+							resolve([...matchedElems])
+						} else {
+							elapsed += intervalWait
+							if (elapsed > maxWait) {
+								clearInterval(i)
+								resolve([...matchedElems])
+								//const numMissing = targetCount - matchedElems.size
+								//reject(`${numMissing} of ${targetCount} elements did not become visible within ${maxWait} milliseconds`)
+							}
+						}
 					}
-				}
-			}, intervalWait)
+				}, intervalWait)
+			})
 		})
+		return this
+	}
+
+	find(selector) {
+		this.pending.push(async elems => {
+			for (const elem of elems) {
+				const match = elem.querySelector(selector)
+				if (match) {
+					return [match]
+				}
+			}
+			return []
+		})
+		return this
+	}
+
+	async all() {
+		const pending = this.pending
+		this.pending = [] // clear
+		let elems = [this.elem]
+		for (const fxn of pending) {
+			elems = await fxn(elems)
+		}
+		return [...elems]
+	}
+
+	async text() {
+		const elems = await this.all()
+		return elems.map(elem => elem.innerText)
 	}
 
 	hides(selector) {
-		return this.shows(selector, false)
+		return this.shows(selector, { visibility: false })
 	}
 }
