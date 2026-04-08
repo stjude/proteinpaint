3b5d7b1e88a642fa829347e41f64ac2ebf1b1ee0
Colleen Reilly <creilly@stjude.org>
2026-04-08 15:06:32 -0500

    Address copilot feedback

--- client/termdb/handlers/singleCellGeneExpression.ts
+++ client/termdb/handlers/singleCellGeneExpression.ts
@@ -37,8 +37,8 @@ export class SearchHandler {
 		if (opts.holder == null) throw new Error('holder is required')
 		if (opts.genomeObj == null) throw new Error('genomeObj is required')
 		if (opts.usecase == null) throw new Error('usecase is required')
-		// if (!opts.usecase?.specialCase?.config?.sample) {
-		// 	throw new Error('usecase.specialCase.config.sample is required for singleCellGeneExpression handler')
-		// }
+		if (!opts.usecase?.specialCase?.config?.sample) {
+			throw new Error('usecase.specialCase.config.sample is required for singleCellGeneExpression handler')
+		}
 	}
 }
