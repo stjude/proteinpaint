f6951c2d484de1a3864cbc611c64e2a1cdf8bd6f
Robin Paul <rpaul1@stjude.org>
2026-04-10 15:44:28 -0500

    Made Entity a union of TwEntity and FilterEntity, passed it back from phrase2entity.ts

--- server/routes/chat/scaffoldTypes.ts
+++ server/routes/chat/scaffoldTypes.ts
@@ -1,16 +1,27 @@
+export type MsgToUser = {
+	type: 'text'
+	text: string
+}
+
+// *** Scaffold Temp Types *** //
 export type SummaryScaffold = {
+	plotType: 'summary'
 	tw1: string
 	tw2?: string
 	tw3?: string
 	filter?: string
+	chartType?: 'violin' | 'boxplot' | 'sampleScatter' | 'barchart' // optional, only for summary plot for now
 }
 export type DEScaffold = {
+	plotType: 'dge'
 	filter1: string
 	filter2: string
 	filter?: string
+	method?: 'edgeR' | 'DESeq2' | 'limma' // optional, default to edgeR if not provided
 }
 
 export type MatrixScaffold = {
+	plotType: 'matrix'
 	twLst: string[]
 	divideBy?: string
 	filter?: string
@@ -31,34 +42,27 @@ export function isMatrixScaffold(s: Scaffold): s is MatrixScaffold {
 	return 'twLst' in s
 }
 
+// *** Entity Temp Types *** //
 type TermTypes = 'dictionary' | 'geneExpression' | 'dnaMethylation' | 'geneVariant' | 'proteomeAbundance'
 
 export type Entity = {
 	termType: TermTypes
 	phrase: string
+	logicalOperator?: '&' | '|' // optional, for filters
 }
-
-// JSON schema types for the filter tree returned by evaluateFilterTerm()
-export type FilterLeafNode = { leaf: string }
-export type FilterLeafNodeEntity = { leaf: Entity }
-export type FilterOperatorNode = { op: '&' | '|'; left: FilterTreeNode; right: FilterTreeNode }
-export type FilterTreeNode = FilterLeafNode | FilterOperatorNode
-export type FilterTreeResult = { sexpr: string; tree: FilterTreeNode }
-
-export type FilterTreeNodeEntity = FilterLeafNodeEntity | FilterOperatorNode
-export type FilterTreeResultEntity = { sexpr: string; tree: FilterTreeNodeEntity }
 
 export type SummaryPhrase2EntityResult = {
 	tw1: [Entity]
 	tw2?: [Entity]
 	tw3?: [Entity]
-	filter?: FilterTreeResultEntity
+	filter?: Entity[] // These might use logical operators
 }
 
 export type DEPhrase2EntityResult = {
-	filter1: FilterTreeResultEntity // will actually be nested structure later on
-	filter2: FilterTreeResultEntity // will actually be nested structure later on
-	filter?: FilterTreeResultEntity
+	filter1: Entity[] // will actually be nested structure later on
+	filter2: Entity[] // will actually be nested structure later on
+	filter?: Entity[]
+	method?: 'edgeR' | 'DESeq2' | 'limma' // optional, default to edgeR if not provided
 }
 
 export type Phrase2EntityResult = SummaryPhrase2EntityResult | DEPhrase2EntityResult // | MatrixPhrase2EntityResult (to be defined later)
