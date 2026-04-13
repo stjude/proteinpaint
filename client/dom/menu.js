5859bf32c3f592264e1962b67876b81060ead36f
xzhou <xzhou82@gmail.com>
2026-04-13 15:31:54 -0500

    geneset edit ui assigns its own testid to genesearch box

--- client/dom/menu.js
+++ client/dom/menu.js
@@ -46,7 +46,7 @@ export class Menu {
 		this.d = body
 			.append('div')
 			.attr('class', 'sja_menu_div')
-			.attr('data-testid', 'sjpp-menu-div')
+			.attr('data-testid', arg.testid || 'sjpp-menu-div')
 			.style('display', 'none')
 			.style('position', 'absolute')
 			.style('background-color', 'white')
