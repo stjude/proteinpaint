a3c27b44d16d1e6176b18e42cd862e8254ef2813
Pramesh Shakya <pramesh.shakya@stjude.org>
2026-04-15 16:38:34 -0500

    Added prelim dge support. Final plot state not configured properly yet.

--- server/routes/chat/scaffold2state.ts
+++ server/routes/chat/scaffold2state.ts
@@ -16,15 +16,27 @@ export function resolveToPlotState(input: any, plotType: string, subplotType?: s
 
 	if (plotType === 'summary') {
 		// default to violing for summary if not provided
-		plotState.childType = subplotType ? subplotType : 'violin'
+		plotState.plot.childType = subplotType ? subplotType : 'violin'
 
 		// for non-dict term, it needs to be within term: {}
 		// but, for dictionary term, it can be supplied as is
 		if (input.tw1) {
+			console.log('input.tw1:', input.tw1)
+			switch (plotState.plot.childType) {
+				case 'sampleScatter':
+					console.log('Setting mode to continuous for sampleScatter plot')
+					input.tw1.q.mode = 'continuous'
+			}
 			plotState.plot.term = isDictionaryTerm(input.tw1) ? input.tw1 : { term: input.tw1 }
 		}
 		if (input.tw2) {
 			// overlay term
+			console.log('input.tw2:', input.tw2)
+			switch (plotState.plot.childType) {
+				case 'sampleScatter':
+					console.log('Setting mode to continuous for sampleScatter plot')
+					input.tw2.q.mode = 'continuous'
+			}
 			plotState.plot.term2 = isDictionaryTerm(input.tw2) ? input.tw2 : { term: input.tw2 }
 		}
 		if (input.tw3) {
@@ -34,6 +46,14 @@ export function resolveToPlotState(input: any, plotType: string, subplotType?: s
 		if (input.filter) {
 			plotState.plot.filter = input.filter
 		}
+	} else if (plotType == 'dge') {
+		// default method for differential gene expression analysis
+		plotState.plot.chartType = 'differentialAnalysis'
+		plotState.plot.childType = 'volcano'
+		plotState.plot.termType = 'geneExpression' // placeholder(can this be something else as well?)
+		const groups = [input.filter1, input.filter2]
+		plotState.plot.samplelst = groups
+		plotState.method = 'edgeR'
 	} else {
 		throw 'Only summary plot type is supported for now'
 	}
