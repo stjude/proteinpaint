5c987cd5a4e186086f1797068bd85f15f890d4dc
siosonel <siosonel@gmail.com>
2026-04-22 17:30:25 -0500

    indicate if a demoToken secret was used in a jwt verification error; defer the check of dsAuthOk to when its checked for being in-session, to minimize confusing jwt-status requests

--- client/common/test/auth.unit.spec.js
+++ client/common/test/auth.unit.spec.js
@@ -30,8 +30,8 @@ tape('setDsAuthOk()', async test => {
 	}
 	//const fakeDofetch3 = () => ({ status: 'ok' }) // uncomment only if there is a saved token for one of the test dslabels
 	await auth.setDsAuthOk(opts /*, fakeDofetch3 */)
-	test.equal(auth.isInSession('abc', 'termdb'), false, 'should detect a dslabel that is not in session')
-	test.equal(auth.isInSession('xyz', 'fake-route'), true, 'should detect a dslabel that is in session')
+	test.equal(await auth.isInSession('abc', 'termdb'), false, 'should detect a dslabel that is not in session')
+	test.equal(await auth.isInSession('xyz', 'fake-route'), true, 'should detect a dslabel that is in session')
 	await auth.setTokenByDsRoute(opts.dsAuth[0].dslabel, opts.dsAuth[0].route) // clear jwtByDsRoute[dslabel][route]
 	await auth.setTokenByDsRoute(opts.dsAuth[1].dslabel, opts.dsAuth[1].route) // clear jwtByDsRoute[dslabel][route]
 	test.end()
