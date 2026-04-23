0dba4992e8718f32d191e901665f51ab2b8b0c8f
siosonel <siosonel@gmail.com>
2026-04-23 15:46:25 -0500

    deprecate serverconfig.dsCredentials.secrets in favor of dsCredentials: <filepath>

--- server/src/serverconfig.js
+++ server/src/serverconfig.js
@@ -162,6 +162,15 @@ if (serverconfig.debugmode && !serverconfig.binpath.includes('sjcrh/')) {
 	serverconfig.routeSetters = routeSetters
 }
 
+if (typeof serverconfig.dsCredentials == 'string') {
+	const json = fs.readFileSync(serverconfig.dsCredentials, { encoding: 'utf8' })
+	try {
+		serverconfig.dsCredentials = JSON.parse(json)
+	} catch (e) {
+		throw `invalid json file, serverconfig.dsCredentials='${json}'`
+	}
+}
+
 if (serverconfig.debugmode) {
 	// should be able to run this in local dev and test environments that use containers
 	const hg38test = serverconfig.genomes.find(g => g.name == 'hg38-test')
