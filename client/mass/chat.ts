a02b8ded399cb421d1d7a689f367a49954b7e976
Pramesh Shakya <pramesh.shakya@stjude.org>
2026-04-24 11:41:50 -0500

    Implement dataset related yes/no queries

--- client/mass/chat.ts
+++ client/mass/chat.ts
@@ -77,7 +77,8 @@ class MassAiChatBot implements RxComponent {
 							type: 'plot_create',
 							config: result.plot
 						})
-						serverBubble.html('Please refer to the plot generated above')
+						if (result.msg) serverBubble.html(result.msg + '.Please refer to the plot generated above.')
+						else serverBubble.html('Please refer to the plot generated above.')
 					}
 					/** may switch by data.type
 					 * type=chat: server returns a chat msg
