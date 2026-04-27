fe9feb8c800b78be2505db49755ccd85cacfb947
xzhou <xzhou82@gmail.com>
2026-04-27 13:19:58 -0500

    genomic alteration getter can quit by assessing hiddenmclass set

--- server/src/mds3.init.js
+++ server/src/mds3.init.js
@@ -974,6 +974,10 @@ export async function snvindelByRangeGetter_bcfMaf(ds, genome) {
 			{ <formatKey> : set of hidden values }
 	*/
 	return async param => {
+		if (param.hiddenmclass?.has(dtsnvindel)) {
+			// snvindel is skipped
+			return []
+		}
 		utils.validateRglst(param, genome)
 		const formatFilter = getFormatFilter(param)
 
@@ -1019,7 +1023,7 @@ export async function snvindelByRangeGetter_bcfMaf(ds, genome) {
 
 				// make a m{} for every alt allele
 				for (const m of m0.mlst) {
-					if (param.hiddenmclass && param.hiddenmclass.has(m.class)) continue
+					if (param.hiddenmclass?.has(m.class)) continue
 
 					// m should have .info{}
 					if (mayDropMbyInfoFilter(m, param)) continue // m is dropped due to info filter
@@ -1254,6 +1258,11 @@ export async function snvindelByRangeGetter_bcf(ds, genome) {
 			{ <formatKey> : set of hidden values }
 	*/
 	return async param => {
+		if (param.hiddenmclass?.has(dtsnvindel)) {
+			// snvindel is skipped. allows to avoid file read
+			return []
+		}
+
 		utils.validateRglst(param, genome)
 
 		const formatFilter = getFormatFilter(param)
@@ -1324,7 +1333,7 @@ export async function snvindelByRangeGetter_bcf(ds, genome) {
 
 				// make a m{} for every alt allele
 				for (const m of m0.mlst) {
-					if (param.hiddenmclass && param.hiddenmclass.has(m.class)) continue
+					if (param.hiddenmclass?.has(m.class)) continue
 
 					// m should have .info{}
 					if (mayDropMbyInfoFilter(m, param)) continue // m is dropped due to info filter
@@ -2309,6 +2318,10 @@ export async function svfusionByRangeGetter_file(ds, genome) {
 
 	// same parameter as snvindel.byrange.get()
 	return async param => {
+		if (param.hiddenmclass?.has(dtsv) && param.hiddenmclass.has(dtfusionrna)) {
+			// both sv & fusion are skipped (even if ds has only 1 type)
+			return []
+		}
 		utils.validateRglst(param, genome)
 
 		const formatFilter = getFormatFilter(param)
@@ -2354,7 +2367,7 @@ export async function svfusionByRangeGetter_file(ds, genome) {
 
 					j.class = j.dt == dtsv ? mclasssv : mclassfusionrna
 
-					if (param.hiddenmclass && param.hiddenmclass.has(j.class)) return
+					if (param.hiddenmclass?.has(j.class)) return
 
 					if (j.sample) {
 						j.sample = ds.cohort.termdb.q.sampleName2id(j.sample)
@@ -2485,6 +2498,10 @@ export async function svfusionByRangeGetter_file(ds, genome) {
 export async function svfusionByNameGetter_file(ds, genome) {
 	const q = ds.queries.svfusion.byname
 	return async param => {
+		if (param.hiddenmclass?.has(dtsv) && param.hiddenmclass.has(dtfusionrna)) {
+			// both sv & fusion are skipped (even if ds has only 1 type)
+			return []
+		}
 		utils.validateRglst(param, genome)
 
 		const formatFilter = getFormatFilter(param)
@@ -2542,7 +2559,7 @@ export async function svfusionByNameGetter_file(ds, genome) {
 					// FIXME file should use sample name but not integer id
 					j.sample = ds.cohort.termdb.q.sampleName2id(m['sample_name'])
 
-					if (param.hiddenmclass && param.hiddenmclass.has(j.class)) return
+					if (param.hiddenmclass?.has(j.class)) return
 
 					if (j.sample && limitSamples) {
 						// to filter sample
@@ -2686,6 +2703,10 @@ async function addCnvGetter(ds, genome) {
 	cnvLossCutoff: float
 	*/
 	return async param => {
+		if (param.hiddenmclass?.has(dtcnv)) {
+			// cnv is skipped
+			return { cnvs: [] }
+		}
 		utils.validateRglst(param, genome)
 		if (param.cnvMaxLength && !Number.isInteger(param.cnvMaxLength)) throw 'cnvMaxLength is not integer' // cutoff<=0 is ignored
 		if (param.cnvGainCutoff && !Number.isFinite(param.cnvGainCutoff)) throw 'cnvGainCutoff is not finite'
@@ -2755,7 +2776,7 @@ async function addCnvGetter(ds, genome) {
 						if (j.class != mclasscnvgain && j.class != mclasscnvloss) return // no valid class
 					}
 
-					if (param.hiddenmclass && param.hiddenmclass.has(j.class)) return
+					if (param.hiddenmclass?.has(j.class)) return
 
 					if (j.sample) {
 						j.sample = ds.cohort.termdb.q.sampleName2id(j.sample)
