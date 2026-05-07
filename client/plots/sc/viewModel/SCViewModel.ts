362e5808afda836ad8cf7682b019dd001d59530d
Colleen Reilly <creilly@stjude.org>
2026-05-07 11:25:02 -0500

    Sort table items before formatting the table data to avoid breaking selected row logic

--- client/plots/sc/viewModel/SCViewModel.ts
+++ client/plots/sc/viewModel/SCViewModel.ts
@@ -8,9 +8,13 @@ export class SCViewModel {
 	state: SCFormattedState
 	tableData: SCTableData
 
-	constructor(app: AppApi, config: SCConfig, items: SingleCellSample[], sampleColumns?: SampleColumn[]) {
+	constructor(app: AppApi, config: SCConfig, _items: SingleCellSample[], sampleColumns?: SampleColumn[]) {
 		this.app = app
 		this.state = this.app.getState()
+
+		//Sort meta analysis results to show at the beginning of the table.
+		//Prevents breaking the logic for selected rows after formating the table data.
+		const items = _items.sort((a, b) => (b.isMetaResult === a.isMetaResult ? 0 : b.isMetaResult ? 1 : -1))
 
 		//Should only be called once
 		const [rows, columns] = this.getTabelData(config, items, sampleColumns)
@@ -86,8 +90,7 @@ export class SCViewModel {
 					if (!value && item.isMetaResult) row.push({ value: 'All' })
 					else row.push({ value: item[col.termid] })
 				}
-				if (item.isMetaResult) rows.unshift(row)
-				else rows.push(row)
+				rows.push(row)
 			}
 		}
 		return [rows, columns]
