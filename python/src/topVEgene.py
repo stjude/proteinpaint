bbc88bc6522869505ce14fcfc2a292b70aa0d5b6
Jamel <jsimps@stjude.org>
2026-05-11 15:56:01 -0500

    Fixing some type checking and documentation

--- python/src/topVEgene.py
+++ python/src/topVEgene.py
@@ -12,12 +12,12 @@ import pandas as pd
 #    samples: Enter the sample ID(s) separated by comma
 #    input_file: Path to input file (HDF5 format)
 #    filter_extreme_values: boolean (true/false). When true, this filter removes genes that have very low expression in most samples, which can be considered as noise and can interfere with the variance calculation.
-#    max_genes: The max num of genes (for e.g 10) that need to be reported in the output.
+#    max_genes: The max num of genes between 10 and 1000 that need to be reported in the output.
 #    rank_type: var/iqr . This parameter decides whether to sort genes using variance or interquartile region. There is an article which states that its better to use interquartile region than variance for selecting genes for clustering https://www.frontiersin.org/articles/10.3389/fgene.2021.632620/full
 
 # echo '{"samples":"2646,2660,2898,3150,3178,3206,3220,3346,3360,1,3,7,21,22,23,37,38,39","input_file":"server/test/tp/files/hg38/TermdbTest/rnaseq/TermdbTest.fpkm.matrix.new.h5", "filter_extreme_values":true,"max_genes":10, "rank_type":"var"}' | python python/src/topVEgene.py
 
-def create_gene_variance_list(filename: str, sample_list: list, filter_extreme_values: bool, rank_type: str, max_genes: int) -> pd.DataFrame:
+def create_gene_variance_list(filename: str, sample_list: list, filter_extreme_values: bool, rank_type: str, max_genes: int) -> list[str]:
     sample_list = [str(s) for s in sample_list]
     with h5py.File(filename, "r") as hdf_data:
         gene_names = hdf_data["item"].asstr()[:]
@@ -40,7 +40,7 @@ def create_gene_variance_list(filename: str, sample_list: list, filter_extreme_v
         for i in range(matrix.shape[0]):
             gene_row=pd.Series(matrix[i], index=all_samples,name=gene_names[i]).loc[sample_list]
             selected_genes.append(calculate_variance(gene_row, filter_extreme_values, rank_type, len(sample_list)))
-        gene_dict={gene:score for (gene, score) in selected_genes}
+        gene_dict={gene:score for (gene, score) in selected_genes if score is not None}
         sorted_genes = sorted(gene_dict, key=gene_dict.get, reverse=True)
         return sorted_genes[:max_genes]
 
@@ -53,7 +53,7 @@ def calculate_variance(
     # Minimum required percentage of samples after dropout from low expression
     MIN_PROP = 0.7
     #using 10% based on https://pmc.ncbi.nlm.nih.gov/articles/PMC4983432/, other discussions/papers have suggested cutoff is mostly arbitrary
-    cutoffs = (gene_row.quantile(0.1)) if filter_extreme_values else pd.Series(0.0, index=gene_row.index)
+    cutoffs = gene_row.quantile(0.1) if filter_extreme_values else pd.Series(0.0, index=gene_row.index)
     #Finding genes that have enough samples with expression values above the cutoff and high enough expression
     gene_sample_count = gene_row.ge(cutoffs).sum()
     # Minimum sample size based on 70% of user-provided sample list size after dropout from low expression
