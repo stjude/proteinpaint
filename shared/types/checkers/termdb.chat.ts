0612ef47c06649a204c636564b449d51189b7b62
Robin Paul <rpaul1@stjude.org>
2026-05-19 16:52:43 -0500

    Changed termdb.chat2.ts to termdb.chat.ts in #shared directory

--- shared/types/checkers/termdb.chat.ts
+++ shared/types/checkers/termdb.chat.ts
@@ -1,8 +1,9 @@
 // this file was auto-generated using 'npm run generate' or 'npm run dev' from the shared/types dir
 import { createValidate } from 'typia'
-import type { ChatRequest, ChatResponse } from '../src/routes/termdb.chat2.ts'
+import type { ChatRequest, ChatResponse } from '../src/routes/termdb.chat.ts'
 
-export { ChatPayload } from '../src/routes/termdb.chat2.ts'
+export { ChatPayload } from '../src/routes/termdb.chat.ts'
+export { createAmbiguousPoints } from '../src/routes/termdb.chat.ts'
 
 export const validChatRequest = createValidate<ChatRequest>()
 export const validChatResponse = createValidate<ChatResponse>()
