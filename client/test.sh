6fe664b32a6893d733348af6c4060bc0a4aaf3e9
siosonel <siosonel@gmail.com>
2026-05-27 09:16:51 -0500

    do not skip chrome download in client/test.sh script

--- client/test.sh
+++ client/test.sh
@@ -56,11 +56,11 @@ node emitImports.mjs $SPECPATTERN > ./$TESTFILE
 ENV=test node esbuild.config.mjs
 
 # puppeteer needs headless chrome, install as needed
-set +u # disable unbound variable check
-if [[ "$PUPPETEER_SKIP_DOWNLOAD" != "" ]]; then
+# set +u # disable unbound variable check
+# if [[ "$PUPPETEER_SKIP_DOWNLOAD" != "" ]]; then
 	NODE_TLS_REJECT_UNAUTHORIZED=0 npx puppeteer browsers install chrome
-fi
-set -u # reenable unbound variable check
+# fi
+# set -u # reenable unbound variable check
 
 # rm -rf .coverage
 tsx --conditions=sjpp/dev test/puppet.js "$PATTERNSLIST" "$TESTPORT"
