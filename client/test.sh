ed02188205365fd970b87a768f227117c7dc5063
siosonel <siosonel@gmail.com>
2026-05-27 13:06:25 -0500

    remove another puppeteer cache dir

--- client/test.sh
+++ client/test.sh
@@ -58,7 +58,9 @@ ENV=test node esbuild.config.mjs
 # puppeteer needs headless chrome, install as needed
 # set +u # disable unbound variable check
 # if [[ "$PUPPETEER_SKIP_DOWNLOAD" != "" ]]; then
-	NODE_TLS_REJECT_UNAUTHORIZED=0 npx puppeteer browsers install chrome
+	# rm -rf ~/.cache/puppeteer
+  # rm ~rf /home/runner/.cache/puppeteer/chrome
+	# NODE_TLS_REJECT_UNAUTHORIZED=0 npx puppeteer browsers install chrome
 # fi
 # set -u # reenable unbound variable check
 
