0b48b8f66c61b616f452478cabb1b08416f3159b
Robin Paul <rpaul1@stjude.org>
2026-06-05 16:44:29 -0500

    Fixed override dir path

--- server/src/chat/route.ts
+++ server/src/chat/route.ts
@@ -76,7 +76,7 @@ export function init({ genomes }) {
 					text: 'Only search functionality supported for this data. No chat functionality supported.'
 				})
 			}
-			const overrideDir = path.join(process.cwd(), 'ai', q.dslabel)
+			const overrideDir = path.join(process.cwd(), 'dataset', 'ai', q.dslabel)
 			const aiFilesDir = fs.existsSync(overrideDir)
 				? overrideDir
 				: path.join(serverconfig.binpath, '../../dataset/ai', q.dslabel) // This is the directory where the AI JSON files are stored for this dataset. This will use this as the base directory for resolving all agent file paths specified in the dataset JSON file.
