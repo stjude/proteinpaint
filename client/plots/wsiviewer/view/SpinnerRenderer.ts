372f4662e2dca432001c7a589b2dc4c3b2f50f22
Jamel <jsimps@stjude.org>
2026-06-08 11:53:57 -0500

    Changing spinner logic

--- client/plots/wsiviewer/view/SpinnerRenderer.ts
+++
@@ -1,8 +0,0 @@
-export class SpinnerRenderer {
-	renderSpinner(holder: any) {
-		holder.selectAll('*').style('cursor', 'wait')
-	}
-	renderDefaultCursor(holder: any) {
-		holder.selectAll('*').style('cursor', 'default')
-	}
-}
