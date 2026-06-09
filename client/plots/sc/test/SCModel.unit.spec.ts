c4a3b84425908df367b5259f70f558f880e50341
Colleen Reilly <creilly@stjude.org>
2026-06-09 16:22:51 -0500

    Update the sc model unit tests to reflect code changes

--- client/plots/sc/test/SCModel.unit.spec.ts
+++ client/plots/sc/test/SCModel.unit.spec.ts
@@ -23,8 +23,8 @@ tape('constructor should set app, id, and state', test => {
 	const app = getMockSCApp()
 	const model = new SCModel(app)
 
-	test.equal(model.app, app, 'Should set app reference')
-	test.equal(model.id, 'plot1', 'Should set id')
+	test.equal(model.app, app.app, 'Should set app reference')
+	test.equal(model.id, 'testApp', 'Should set id')
 	test.deepEqual(
 		model.state.vocab,
 		{ genome: 'hg38-test', dslabel: 'TermdbTest' },
@@ -34,20 +34,35 @@ tape('constructor should set app, id, and state', test => {
 })
 
 tape('getDataRequestOpts() should return correct request body', test => {
-	const app = getMockSCApp()
+	const app = getMockSCApp({
+		plots: [{ id: 'testApp', settings: { sc: { item: { sID: 'S1', eID: 'EXP1' } } } }]
+	})
 	const model = new SCModel(app)
 	const opts = model.getDataRequestOpts()
 
 	test.equal(opts!.genome, 'hg38-test', 'Should include genome')
 	test.equal(opts!.dslabel, 'TermdbTest', 'Should include dslabel')
+	test.equal(opts!.checkPlotAvailability, true, 'Should default checkPlotAvailability to true')
 	test.deepEqual(opts!.plots, ['umap', 'tsne'], 'Should map plot names')
 	test.deepEqual(opts!.sample, { eID: 'EXP1', sID: 'S1' }, 'Should include sample with eID and sID')
 	test.end()
 })
 
+tape('getDataRequestOpts() should use provided plots and checkPlotAvailability value', test => {
+	const app = getMockSCApp({
+		plots: [{ id: 'testApp', settings: { sc: { item: { sID: 'S1', eID: 'EXP1' } } } }]
+	})
+	const model = new SCModel(app)
+	const opts = model.getDataRequestOpts(['violin'], false)
+
+	test.equal(opts!.checkPlotAvailability, false, 'Should pass through checkPlotAvailability argument')
+	test.deepEqual(opts!.plots, ['violin'], 'Should use explicitly provided plots')
+	test.end()
+})
+
 tape('getDataRequestOpts() should return undefined when item is not set', test => {
 	const app = getMockSCApp({
-		plots: [{ id: 'plot1', settings: { sc: { item: undefined } } }]
+		plots: [{ id: 'testApp', settings: { sc: { item: undefined } } }]
 	})
 	const model = new SCModel(app)
 	const opts = model.getDataRequestOpts()
