8e843d35a14e935c63426521557bb636ef8d3969
Xin Zhou <xzhou82@gmail.com>
2026-06-28 13:49:58 -0500

    apply testid to table <th>, add tests for tw column

--- client/dom/types/table.ts
+++ client/dom/types/table.ts
@@ -23,6 +23,8 @@ export type TableCell = {
 export type TableColumn = {
 	/** the text to show as header of a column */
 	label: string
+	/** optional data-testid for the column header <th> */
+	headerTestId?: string
 	/** show the column label vertically in the header */
 	labelVertical?: true
 	/** column width */
