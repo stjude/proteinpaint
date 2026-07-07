2f13912ae8e4f637e1ffc6d9b0820b86fc3742c2
Xin Zhou <xzhou82@gmail.com>
2026-07-07 09:16:22 -0500

    termdb/junctions route

--- shared/types/src/routes/termdb.junction.ts
+++ shared/types/src/routes/termdb.junction.ts
@@ -1,4 +1,30 @@
 import type { FileORURL } from '../fileOrUrl.ts'
+import type { ErrorResponse } from './errorResponse.ts'
+import type { Filter } from '../filter.ts'
+
+export type TermdbJunctionsRequest = {
+	genome: string
+	dslabel: string
+	rglst: any
+	filter?: Filter
+	filter0?: any
+	minReadCount?: number
+}
+export type TermdbJunctionsResponse =
+	| {
+			junctions: {
+				chr: string
+				start: number
+				stop: number
+				strand: string
+				type: string
+				// more properties
+				sampleCount: number
+				medianReadCount: number
+			}[]
+			error?: string
+	  }
+	| ErrorResponse
 
 export type TermdbJunctionOneSampleTkRequest = FileORURL & {
 	genome: string
