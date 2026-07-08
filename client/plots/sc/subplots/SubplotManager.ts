1f8bac487cb74d949daae8e4557134ba3fd24c51
Colleen Reilly <creilly@stjude.org>
2026-07-08 15:56:48 -0500

    Correct plot button labels

--- client/plots/sc/subplots/SubplotManager.ts
+++ client/plots/sc/subplots/SubplotManager.ts
@@ -114,17 +114,18 @@ export class SubplotManager {
 	}
 
 	getPlotName(subplot: any): string {
-		let plotName = subplot?.plotName || subplot?.singleCellPlot?.name
+		/** Hardcoding logic for some transient and parent plots for now. May consider
+		 * adding to the config if this becomes more complex. Must weight against
+		 * adding unnecessary complexity to the config for edge cases though.*/
+		const nameOverrides = new Map([
+			['GeneExpInput', 'Gene expression'],
+			['imagePlot', subplot?.imgDir?.label || 'Image'],
+			['dictionary', 'Summary'],
+			['summary', 'Summary']
+		])
+		let plotName = subplot?.plotName || nameOverrides.get(subplot?.chartType) || subplot?.singleCellPlot?.name
 		if (!plotName) {
-			/** Hardcoding logic for some transient and parent plots for now. May consider
-			 * adding to the config if this becomes more complex. Must weight against
-			 * adding unnecessary complexity to the config for edge cases though.*/
-			if (subplot.chartType === 'dictionary') plotName = 'Summary'
-			else if (subplot.chartType === 'summary') plotName = 'Summary'
-			else if (subplot.chartType === 'GeneExpInput') plotName = 'Gene expression'
-			else if (subplot.chartType === 'imagePlot') {
-				plotName = subplot?.imgDir?.label || 'Image'
-			} else if (subplot?.term?.term?.plot) plotName = subplot.term.term.plot
+			if (subplot?.term?.term?.plot) plotName = subplot.term.term.plot
 			else plotName = subplot.chartType || 'Plot'
 		}
 		return plotName
