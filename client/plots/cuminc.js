9520b6c408d74607d7eb75b788f688b7f63a8986
Jamel <jsimps@stjude.org>
2026-07-29 15:00:08 -0500

    Fix error handling in MassCumInc for empty results and clean up errorDiv styling

--- client/plots/cuminc.js
+++ client/plots/cuminc.js
@@ -466,6 +466,7 @@ class MassCumInc {
 	processResults(results) {
 		if (Object.keys(results.data).length === 0) {
 			sayerror(this.dom.errorDiv, 'No data available')
+			return
 		}
 		const s = this.settings
 		const c = this.config
