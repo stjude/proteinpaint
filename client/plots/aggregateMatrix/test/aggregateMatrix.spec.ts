70658349c77323ca06d72d6eb4b15a3136d3f731
Colleen Reilly <creilly@stjude.org>
2026-08-03 13:57:04 -0500

    Rn to `rows` and `columns` for clarity

--- client/plots/aggregateMatrix/test/aggregateMatrix.spec.ts
+++ client/plots/aggregateMatrix/test/aggregateMatrix.spec.ts
@@ -5,7 +5,8 @@ import * as helpers from '../../../test/front.helpers.js'
 DO NOT ENABLE THIS FILE ON CI. ITS FOR PROTOTYPING ONLY
 
 Tests:
-	- Default aggregate matrix
+	- Simple aggregate matrix
+	- Multiple sections and categories aggregate matrix
  */
 
 /*************************
@@ -37,7 +38,7 @@ tape('\n', function (test) {
 	test.end()
 })
 
-tape('Default aggregate matrix', async function (test) {
+tape('Simple aggregate matrix', async function (test) {
 	test.timeoutAfter(10000)
 
 	runpp({
@@ -45,7 +46,7 @@ tape('Default aggregate matrix', async function (test) {
 			plots: [
 				{
 					chartType: 'aggregateMatrix',
-					entries: {
+					rows: {
 						genes: [
 							{ name: 'TP53', id: 'TP53', type: 'geneExpression' },
 							{ name: 'KRAS', id: 'KRAS', type: 'geneExpression' },
@@ -55,9 +56,60 @@ tape('Default aggregate matrix', async function (test) {
 							{ name: 'MZB1', id: 'MZB1', type: 'geneExpression' },
 						]
 					},
-					categories: {
+					columns: {
 						'Cell type': [
 							{ name: 'B', id: 'B', type: 'pseudobulk', assay: 'geneExpression', memberId: 'Cell Type' },
+							{ name: 'CD4', id: 'CD4', type: 'pseudobulk', assay: 'geneExpression', memberId: 'Cell Type' }
+						]
+					},
+					settings: {
+						gradientMethod: 'mean',
+						sizeMethod: 'percent'
+					}
+				}
+			]
+		},
+		aggregateMatrix: {
+			callbacks: {
+				'postRender.test': runTests
+			}
+		}
+	})
+
+	async function runTests(aggregateMatrix: any) {
+		aggregateMatrix.on('postRender.test', null)
+		//TODO
+
+		// if (test['_ok']) aggregateMatrix.Inner.app.destroy()
+		test.end()
+	}
+})
+
+tape('Multiple sections and categories aggregate matrix', async function (test) {
+	test.timeoutAfter(10000)
+
+	runpp({
+		state: {
+			plots: [
+				{
+					chartType: 'aggregateMatrix',
+					rows: {
+						genesTest: [
+							{ name: 'TP53', id: 'TP53', type: 'geneExpression' },
+							{ name: 'KRAS', id: 'KRAS', type: 'geneExpression' },
+							{ name: 'EGFR', id: 'EGFR', type: 'geneExpression' }
+						],
+						genes2Test: [
+							{ name: 'XBP1', id: 'XBP1', type: 'geneExpression' },
+							{ name: 'CD138', id: 'CD138', type: 'geneExpression' },
+							{ name: 'MZB1', id: 'MZB1', type: 'geneExpression' }
+						]
+					},
+					columns: {
+						'Cell type': [
+							{ name: 'B', id: 'B', type: 'pseudobulk', assay: 'geneExpression', memberId: 'Cell Type' }
+						],
+						'Cell type2': [
 							{ name: 'CD4', id: 'CD4', type: 'pseudobulk', assay: 'geneExpression', memberId: 'Cell Type' }
 						]
 					},
