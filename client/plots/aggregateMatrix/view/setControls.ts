0f3993c5e2260ddbfcde73df59b06aa98b75e029
Colleen Reilly <creilly@stjude.org>
2026-08-04 13:56:27 -0500

    Add color pickers to the control panel

--- client/plots/aggregateMatrix/view/setControls.ts
+++ client/plots/aggregateMatrix/view/setControls.ts
@@ -1,15 +1,31 @@
 import type { AggregateMatrix } from "../AggregateMatrix"
 import { controlsInit } from '#plots/controls.js'
 import { validateMinMax } from '../settings/defaults.ts'
+import { capitalizeFirstLetter } from '#dom'
 
 export async function setControls(controlsDiv, ag: AggregateMatrix) {
 	const settings = ag.state.config.settings.aggregateMatrix
+	const chartType = ag.type
+	const gradienLabel = capitalizeFirstLetter(settings.gradientMethod)
+
 	const inputs: any = [
+		{
+				label: `${gradienLabel} start color`,
+				type: 'color',
+				chartType,
+				settingsKey: 'startColor'
+		},
+		{
+				label: `${gradienLabel} stop color`,
+				type: 'color',
+				chartType,
+				settingsKey: 'stopColor'
+		},
 		{
 			label: 'Min dot size',
 			title: 'Set the minimum dot size. Number must be between 2 and 35',
 			type: 'number',
-			chartType: 'aggregateMatrix',
+			chartType,
 			settingsKey: 'minDotSize',
 			step: 1,
 			max: settings.dotInputMax,
@@ -30,7 +46,7 @@ export async function setControls(controlsDiv, ag: AggregateMatrix) {
 			label: 'Max dot size',
 			title: 'Set the maximum dot size. Number must be between 2 and 35',
 			type: 'number',
-			chartType: 'aggregateMatrix',
+			chartType,
 			settingsKey: 'maxDotSize',
 			step: 1,
 			max: settings.dotInputMax,
