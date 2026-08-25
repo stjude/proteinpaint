6bb8fdd4ff1abdf2a251e7858c483ebcceec7276
Gavriel Matt <gmatt@stjude.org>
2026-08-25 16:37:19 -0500

    Return tw.term when launching geneExpression tvs menu

--- client/termdb/TermTypeSearch.ts
+++ client/termdb/TermTypeSearch.ts
@@ -476,7 +476,12 @@ export class TermTypeSearch {
 				type: 'submenu_set',
 				submenu: {
 					type: 'tvs',
-					term: term.term?.type == 'geneVariant' ? this.getDtTerm(term) : term
+					term:
+						term.term?.type == 'geneVariant'
+							? this.getDtTerm(term)
+							: term.term?.type == 'geneExpression'
+							? term.term
+							: term
 				}
 			})
 		}
