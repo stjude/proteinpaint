86984450cb6ac8d03dfee29211c0393022e8fe9c
Jamel <jsimps@stjude.org>
2026-09-02 12:26:11 -0500

    Refactor: Move scTestSample to testData.ts and update imports

---
+++ shared/utils/src/test/testData.ts
@@ -0,0 +1,6 @@
+/** This file contains const and methods to be shared between
+ * the client and e2e tests. */
+
+
+/** Use this for testing single cell sample */
+export const scTestSample = '2646'
