a786258af025586ff52a95c2ae04b7726f89e6e2
Colleen Reilly <creilly@stjude.org>
2026-09-09 16:58:34 -0500

    Show 'None' group by option last

--- client/plots/sc/settings/Settings.ts
+++ client/plots/sc/settings/Settings.ts
@@ -1,6 +1,6 @@
 import type { SCSample } from '../SCTypes'
 
-export const GroupByOptions = ['none', 'sample', 'plot'] as const
+export const GroupByOptions = ['sample', 'plot', 'none'] as const
 
 export type Settings = {
 	sc: SCSettings
